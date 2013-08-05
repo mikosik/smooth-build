5a877158d9b34ff2813ad066e1438cf40974cd87
mikoch <marcin.mikosik@gmail.com>
2013-08-05 15:21:26 +0200

    added TestingFileSystem.assertContentHasFilePath()

--- src/java/org/smoothbuild/testing/TestingFileSystem.java
+++ src/java/org/smoothbuild/testing/TestingFileSystem.java
@@ -1,10 +1,18 @@
 package org.smoothbuild.testing;
 
+import static com.google.common.base.Preconditions.checkArgument;
+import static org.smoothbuild.fs.base.PathUtils.isValid;
+import static org.smoothbuild.fs.base.PathUtils.toCanonical;
+
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 import org.smoothbuild.fs.base.PathUtils;
 import org.smoothbuild.fs.mem.InMemoryFileSystem;
+
+import com.google.common.io.LineReader;
 
 public class TestingFileSystem extends InMemoryFileSystem {
 
@@ -12,7 +20,8 @@ public class TestingFileSystem extends InMemoryFileSystem {
    * Creates textual file that contains its path.
    */
   public void createFile(String root, String path) throws IOException {
-    String fullPath = PathUtils.append(root, path);
+    String fullPath = fullPath(root, path);
+
     OutputStreamWriter writer = new OutputStreamWriter(createOutputStream(fullPath));
     writer.write(path);
     writer.close();
@@ -21,5 +30,26 @@ public class TestingFileSystem extends InMemoryFileSystem {
   public void createEmptyFile(String path) throws IOException {
     OutputStreamWriter writer = new OutputStreamWriter(createOutputStream(path));
     writer.close();
+  }
+
+  public void assertContentHasFilePath(String root, String path) throws IOException,
+      FileNotFoundException {
+    String fullPath = fullPath(root, path);
+
+    try (InputStreamReader readable = new InputStreamReader(this.createInputStream(fullPath));) {
+      LineReader reader = new LineReader(readable);
+      String actual = reader.readLine();
+      if (!actual.equals(path)) {
+        throw new AssertionError("File content is incorrect. Expected '" + path + "' but was '"
+            + actual + "'.");
+      }
+    }
+  }
+
+  private static String fullPath(String root, String path) {
+    checkArgument(isValid(root));
+    checkArgument(isValid(path));
+
+    return PathUtils.append(toCanonical(root), toCanonical(path));
   }
 }
