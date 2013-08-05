6035e765a73623e5b163b4a40e657ab24080a780
mikoch <marcin.mikosik@gmail.com>
2013-08-05 13:05:19 +0200

    added PathArgValidator

---
+++ src/java/org/smoothbuild/builtin/file/PathArgValidator.java
@@ -0,0 +1,22 @@
+package org.smoothbuild.builtin.file;
+
+import org.smoothbuild.fs.base.PathUtils;
+import org.smoothbuild.lang.function.Param;
+import org.smoothbuild.lang.function.exc.IllegalPathException;
+import org.smoothbuild.lang.function.exc.MissingArgException;
+import org.smoothbuild.lang.type.Path;
+
+public class PathArgValidator {
+  public static Path validatedPath(Param<String> path) throws MissingArgException,
+      IllegalPathException {
+    if (!path.isSet()) {
+      throw new MissingArgException(path);
+    }
+    String message = PathUtils.validationError(path.get());
+    if (message != null) {
+      throw new IllegalPathException(path, message);
+    }
+
+    return Path.path(path.get());
+  }
+}
---
+++ src/test/org/smoothbuild/builtin/file/PathArgValidatorTest.java
@@ -0,0 +1,52 @@
+package org.smoothbuild.builtin.file;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.smoothbuild.fs.base.PathUtilsTest;
+import org.smoothbuild.lang.function.Param;
+import org.smoothbuild.lang.function.exc.FunctionException;
+import org.smoothbuild.lang.function.exc.IllegalPathException;
+import org.smoothbuild.lang.function.exc.MissingArgException;
+
+public class PathArgValidatorTest {
+
+  @Test
+  public void missingDirArgIsReported() throws Exception {
+    Param<String> param = Param.stringParam("name");
+    try {
+      PathArgValidator.validatedPath(param);
+      Assert.fail("exception should be thrown");
+    } catch (MissingArgException e) {
+      // expected
+
+      assertThat(param).isSameAs(param);
+    }
+  }
+
+  @Test
+  public void illegalPathsAreReported() throws FunctionException {
+    for (String path : PathUtilsTest.listOfInvalidPaths()) {
+      Param<String> param = Param.stringParam("name");
+      param.set(path);
+      try {
+        PathArgValidator.validatedPath(param);
+        Assert.fail("exception should be thrown");
+      } catch (IllegalPathException e) {
+        // expected
+
+        assertThat(param).isSameAs(param);
+      }
+    }
+  }
+
+  @Test
+  public void validPathsAreAccepted() throws FunctionException {
+    for (String path : PathUtilsTest.listOfCorrectPaths()) {
+      Param<String> param = Param.stringParam("name");
+      param.set(path);
+      PathArgValidator.validatedPath(param);
+    }
+  }
+}
