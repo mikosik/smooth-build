932fd9876322f7c21a15a69d504a7183d8ab5179
mikoch <marcin.mikosik@gmail.com>
2013-08-05 12:05:54 +0200

    added TestingFileSystem.createEmptyFile()

--- src/java/org/smoothbuild/fs/mem/TestingFileSystem.java
+++ src/java/org/smoothbuild/fs/mem/TestingFileSystem.java
@@ -7,10 +7,18 @@ import org.smoothbuild.fs.base.PathUtils;
 
 public class TestingFileSystem extends InMemoryFileSystem {
 
+  /**
+   * Creates textual file that contains its path.
+   */
   public void createFile(String root, String path) throws IOException {
     String fullPath = PathUtils.append(root, path);
     OutputStreamWriter writer = new OutputStreamWriter(createOutputStream(fullPath));
     writer.write(path);
+    writer.close();
+  }
+
+  public void createEmptyFile(String path) throws IOException {
+    OutputStreamWriter writer = new OutputStreamWriter(createOutputStream(path));
     writer.close();
   }
 }
