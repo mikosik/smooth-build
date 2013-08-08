fef148302776cd3afba4e7a79422d7bca91eb3da
mikoch <marcin.mikosik@gmail.com>
2013-08-08 14:55:27 +0200

    FunctionInstanceId keeps hash of instance

--- src/java/org/smoothbuild/registry/instantiate/FunctionInstanceId.java
+++ src/java/org/smoothbuild/registry/instantiate/FunctionInstanceId.java
@@ -1,15 +1,38 @@
 package org.smoothbuild.registry.instantiate;
 
+import static org.smoothbuild.lang.type.Path.path;
+
 import org.smoothbuild.lang.type.Path;
 
+import com.google.common.annotations.VisibleForTesting;
+
 public class FunctionInstanceId {
+  @VisibleForTesting
+  static final Path BUILD_ROOT = path(".smooth");
+
+  private final String hash;
   private final Path resultDir;
 
-  public FunctionInstanceId(Path resultDir) {
-    this.resultDir = resultDir;
+  public FunctionInstanceId(String hash) {
+    this.hash = hash;
+    this.resultDir = BUILD_ROOT.append(path(hash));
   }
 
   public Path resultDir() {
     return resultDir;
+  }
+
+  @Override
+  public final boolean equals(Object object) {
+    if (!(object instanceof FunctionInstanceId)) {
+      return false;
+    }
+    FunctionInstanceId that = (FunctionInstanceId) object;
+    return this.hash.equals(that.hash);
+  }
+
+  @Override
+  public final int hashCode() {
+    return hash.hashCode();
   }
 }
