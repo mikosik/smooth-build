8957a6b5f3fadbeb458a18d2f7a9a7bf3754e717
mikoch <marcin.mikosik@gmail.com>
2013-08-08 16:04:49 +0200

    renamed FunctionInstance -> Expression, FunctionInstanceId -> ExpressionId, FunctionInstanceIdFactory -> ExpressionIdFactory

--- src/test/org/smoothbuild/registry/instantiate/ExpressionTest.java
+++ src/test/org/smoothbuild/registry/instantiate/ExpressionTest.java
@@ -6,15 +6,15 @@ import static org.mockito.Mockito.mock;
 import org.junit.Test;
 import org.smoothbuild.lang.function.FunctionDefinition;
 
-public class FunctionInstanceTest {
-  FunctionInstanceId id = new FunctionInstanceId("abc");
+public class ExpressionTest {
+  ExpressionId id = new ExpressionId("abc");
   FunctionDefinition definition = mock(FunctionDefinition.class);
 
-  FunctionInstance functionInstance = new FunctionInstance(id, definition);
+  Expression expression = new Expression(id, definition);
 
   @Test
   public void id() {
-    assertThat(functionInstance.id()).isEqualTo(id);
+    assertThat(expression.id()).isEqualTo(id);
   }
 
 }
