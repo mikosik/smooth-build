0f06807319b3b9928de7da91cfe8b6e269fdc317
mikoch <marcin.mikosik@gmail.com>
2013-08-08 17:58:28 +0200

    added Expression.type()

--- src/java/org/smoothbuild/registry/instantiate/Expression.java
+++ src/java/org/smoothbuild/registry/instantiate/Expression.java
@@ -1,19 +1,26 @@
 package org.smoothbuild.registry.instantiate;
 
 import org.smoothbuild.lang.function.FunctionDefinition;
+import org.smoothbuild.lang.function.Type;
 import org.smoothbuild.lang.function.exc.FunctionException;
 
 public class Expression {
   private final ExpressionId id;
+  private final Type<?> type;
   private final FunctionDefinition functionDefinition;
 
-  public Expression(ExpressionId id, FunctionDefinition functionDefinition) {
+  public Expression(ExpressionId id, Type<?> type, FunctionDefinition functionDefinition) {
     this.id = id;
+    this.type = type;
     this.functionDefinition = functionDefinition;
   }
 
   public ExpressionId id() {
     return id;
+  }
+
+  public Type<?> type() {
+    return type;
   }
 
   public void execute() throws FunctionException {
--- src/test/org/smoothbuild/registry/instantiate/ExpressionTest.java
+++ src/test/org/smoothbuild/registry/instantiate/ExpressionTest.java
@@ -5,16 +5,23 @@ import static org.mockito.Mockito.mock;
 
 import org.junit.Test;
 import org.smoothbuild.lang.function.FunctionDefinition;
+import org.smoothbuild.lang.function.Type;
 
 public class ExpressionTest {
   ExpressionId id = new ExpressionId("abc");
   FunctionDefinition definition = mock(FunctionDefinition.class);
 
-  Expression expression = new Expression(id, definition);
+  Expression expression = new Expression(id, Type.STRING, definition);
 
   @Test
   public void id() {
     assertThat(expression.id()).isEqualTo(id);
   }
 
+  @Test
+  public void type() throws Exception {
+    @SuppressWarnings("unchecked")
+    Type<String> actual = (Type<String>) expression.type();
+    assertThat(actual).isEqualTo(Type.STRING);
+  }
 }
