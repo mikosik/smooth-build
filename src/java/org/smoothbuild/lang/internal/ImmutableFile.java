f3e1ad6b877a866e7991086768e117eab70e9571
mikoch <marcin.mikosik@gmail.com>
2013-08-10 11:55:16 +0200

    added ImmutableFie, ImmutableFiles

---
+++ src/java/org/smoothbuild/lang/internal/ImmutableFile.java
@@ -0,0 +1,38 @@
+package org.smoothbuild.lang.internal;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import org.smoothbuild.lang.type.File;
+import org.smoothbuild.lang.type.Path;
+
+public class ImmutableFile implements File {
+  private final File file;
+
+  public static File immutableFile(File file) {
+    if (file instanceof ImmutableFile) {
+      return file;
+    } else {
+      return new ImmutableFile(file);
+    }
+  }
+
+  private ImmutableFile(File file) {
+    this.file = file;
+  }
+
+  @Override
+  public Path path() {
+    return file.path();
+  }
+
+  @Override
+  public InputStream createInputStream() {
+    return file.createInputStream();
+  }
+
+  @Override
+  public OutputStream createOutputStream() {
+    throw new UnsupportedOperationException();
+  }
+}
