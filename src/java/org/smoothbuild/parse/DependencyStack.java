8fea86c7dcf4af8dad122bea587a6499ba5de34e
mikoch <marcin.mikosik@gmail.com>
2013-08-16 15:08:18 +0200

    added DependencyStack

---
+++ src/java/org/smoothbuild/parse/DependencyStack.java
@@ -0,0 +1,53 @@
+package org.smoothbuild.parse;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+import org.smoothbuild.parse.err.CycleInCallGraphError;
+import org.smoothbuild.problem.SourceLocation;
+
+public class DependencyStack {
+  private final Deque<DependencyStackElem> stack = new ArrayDeque<DependencyStackElem>();
+
+  public boolean isEmpty() {
+    return stack.isEmpty();
+  }
+
+  public void push(DependencyStackElem element) {
+    stack.addLast(element);
+  }
+
+  public DependencyStackElem pop() {
+    return stack.removeLast();
+  }
+
+  public DependencyStackElem peek() {
+    return stack.getLast();
+  }
+
+  public CycleInCallGraphError createCycleProblem() {
+    String lastMissing = peek().missing().functionName();
+    int first = -1;
+    DependencyStackElem[] array = stack.toArray(new DependencyStackElem[stack.size()]);
+    for (int i = array.length - 1; 0 <= i; i--) {
+      if (array[i].name().equals(lastMissing)) {
+        first = i;
+        break;
+      }
+    }
+    if (first == -1) {
+      throw new IllegalStateException("Couldn't find expected cycle in call graph.");
+    }
+    StringBuilder builder = new StringBuilder();
+    for (int i = first; i < array.length; i++) {
+      DependencyStackElem current = array[i];
+      Dependency missing = current.missing();
+      builder.append(current.name() + missing.location() + " -> " + missing.functionName() + "\n");
+    }
+
+    SourceLocation location = array[first].missing().location();
+    String message = builder.toString();
+    CycleInCallGraphError problem = new CycleInCallGraphError(location, message);
+    return problem;
+  }
+}
