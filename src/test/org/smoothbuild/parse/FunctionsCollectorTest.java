d9d13a1e24348243e0bb504705dab8815f1c62a2
mikoch <marcin.mikosik@gmail.com>
2013-08-16 10:33:04 +0200

    refactored out TestingFuntionContext out of FunctionsCollectorTest

--- src/test/org/smoothbuild/parse/FunctionsCollectorTest.java
+++ src/test/org/smoothbuild/parse/FunctionsCollectorTest.java
@@ -1,13 +1,10 @@
 package org.smoothbuild.parse;
 
 import static org.fest.assertions.api.Assertions.assertThat;
+import static org.smoothbuild.testing.parse.TestingFunctionContext.testingFunctionContext;
 
-import org.antlr.v4.runtime.CommonToken;
-import org.antlr.v4.runtime.ParserRuleContext;
-import org.antlr.v4.runtime.Token;
 import org.junit.Test;
 import org.smoothbuild.antlr.SmoothParser.FunctionContext;
-import org.smoothbuild.antlr.SmoothParser.FunctionNameContext;
 import org.smoothbuild.lang.function.FullyQualifiedName;
 import org.smoothbuild.lang.function.Type;
 import org.smoothbuild.parse.err.DuplicateFunctionError;
@@ -31,8 +28,8 @@ public class FunctionsCollectorTest {
     String name1 = "functionA";
     String name2 = "functionB";
 
-    FunctionContext function1 = createFunction(name1);
-    FunctionContext function2 = createFunction(name2);
+    FunctionContext function1 = testingFunctionContext(name1);
+    FunctionContext function2 = testingFunctionContext(name2);
 
     functionsCollector.visitFunction(function1);
     functionsCollector.visitFunction(function2);
@@ -44,54 +41,28 @@ public class FunctionsCollectorTest {
 
   @Test
   public void illegalFunctionNameIsReported() {
-    functionsCollector.visitFunction(createFunction("function-name"));
+    functionsCollector.visitFunction((FunctionContext) testingFunctionContext("function-name"));
     problemsListener.assertOnlyProblem(IllegalFunctionNameError.class);
   }
 
   @Test
   public void duplicateFunction() throws Exception {
-    functionsCollector.visitFunction(createFunction("functionA"));
-    functionsCollector.visitFunction(createFunction("functionA"));
+    functionsCollector.visitFunction((FunctionContext) testingFunctionContext("functionA"));
+    functionsCollector.visitFunction((FunctionContext) testingFunctionContext("functionA"));
 
     problemsListener.assertOnlyProblem(DuplicateFunctionError.class);
   }
 
   @Test
   public void overridenImport() throws Exception {
-    functionsCollector.visitFunction(createFunction(IMPORTED_FUNCTION_NAME));
+    functionsCollector.visitFunction((FunctionContext) testingFunctionContext(IMPORTED_FUNCTION_NAME));
     problemsListener.assertOnlyProblem(OverridenImportWarning.class);
-  }
-
-  private FunctionContext createFunction(String name) {
-    FunctionContext function = new FunctionContext(null, 0);
-    FunctionNameContext functionName = new FunctionNameContext(function, 0);
-    functionName.addChild(stringChild(name));
-    functionName.start = token();
-    functionName.stop = token();
-    function.addChild(functionName);
-    return function;
-  }
-
-  private ParserRuleContext stringChild(final String string) {
-    return new ParserRuleContext() {
-      @Override
-      public String getText() {
-        return string;
-      }
-    };
-  }
-
-  private static Token token() {
-    CommonToken token = new CommonToken(0);
-    token.setStartIndex(13);
-    token.setLine(11);
-    token.setStopIndex(17);
-    return token;
   }
 
   private static ImportedFunctions createImportedFunctions() {
     ImportedFunctions imported = new ImportedFunctions();
-    imported.add(new Function(FullyQualifiedName.simpleName(IMPORTED_FUNCTION_NAME), Type.FILE, null));
+    imported.add(new Function(FullyQualifiedName.simpleName(IMPORTED_FUNCTION_NAME), Type.FILE,
+        null));
     return imported;
   }
 }
