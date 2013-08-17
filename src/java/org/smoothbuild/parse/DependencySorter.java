8b8eae153c0acf199d81407f0dac821a0ef2201b
mikoch <marcin.mikosik@gmail.com>
2013-08-17 10:42:13 +0200

    added DependencySorter

---
+++ src/java/org/smoothbuild/parse/DependencySorter.java
@@ -0,0 +1,109 @@
+package org.smoothbuild.parse;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+import org.smoothbuild.problem.ProblemsListener;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
+
+/**
+ * Sorts functions so all dependencies of each function are placed before that
+ * function in returned list. Detects cycles in dependency graph.
+ */
+public class DependencySorter {
+  private final ProblemsListener problemsListener;
+
+  public DependencySorter(ProblemsListener problemsListener) {
+    this.problemsListener = problemsListener;
+  }
+
+  public List<String> sortByDependency(SymbolTable importedFunctions,
+      Map<String, Set<Dependency>> dependenciesOrig) {
+    Worker worker = new Worker(importedFunctions, dependenciesOrig);
+    worker.work();
+    return worker.sorted();
+  }
+
+  private class Worker {
+    private final HashMap<String, Set<Dependency>> notSorted;
+    private final HashSet<String> reachableNames;
+    private final List<String> sorted;
+    private final DependencyStack stack;
+
+    public Worker(SymbolTable importedFunctions, Map<String, Set<Dependency>> dependenciesOrig) {
+      this.notSorted = Maps.newHashMap(dependenciesOrig);
+      this.reachableNames = Sets.newHashSet(importedFunctions.names());
+      this.sorted = Lists.newArrayListWithCapacity(dependenciesOrig.size());
+      this.stack = new DependencyStack();
+    }
+
+    public void work() {
+      while (!notSorted.isEmpty() || !stack.isEmpty()) {
+        if (stack.isEmpty()) {
+          stack.push(removeNext(notSorted));
+        }
+        processStackTop();
+      }
+    }
+
+    private void processStackTop() {
+      DependencyStackElem stackTop = stack.peek();
+      Dependency missing = findUnreachableDependency(reachableNames, stackTop.dependencies());
+      if (missing == null) {
+        addStackTopToSorted();
+      } else {
+        stackTop.setMissing(missing);
+        Set<Dependency> next = notSorted.remove(missing.functionName());
+        if (next == null) {
+          reportCycle();
+        } else {
+          stack.push(new DependencyStackElem(missing.functionName(), next));
+        }
+      }
+    }
+
+    private void reportCycle() {
+      // DependencyCollector made sure that all dependency exists so the
+      // only possibility at this point is that missing dependency is on
+      // stack and we have cycle in call graph.
+      problemsListener.report(stack.createCycleProblem());
+      addStackTopToSorted();
+    }
+
+    private void addStackTopToSorted() {
+      String name = stack.pop().name();
+      sorted.add(name);
+      reachableNames.add(name);
+    }
+
+    public List<String> sorted() {
+      return ImmutableList.copyOf(sorted);
+    }
+
+    private Dependency findUnreachableDependency(Set<String> reachableNames,
+        Set<Dependency> dependencies) {
+      for (Dependency dependency : dependencies) {
+        if (!reachableNames.contains(dependency.functionName())) {
+          return dependency;
+        }
+      }
+      return null;
+    }
+
+    private DependencyStackElem removeNext(Map<String, Set<Dependency>> dependencies) {
+      Iterator<Entry<String, Set<Dependency>>> it = dependencies.entrySet().iterator();
+      Entry<String, Set<Dependency>> element = it.next();
+      it.remove();
+      return new DependencyStackElem(element.getKey(), element.getValue());
+    }
+  }
+}
---
+++ src/test/org/smoothbuild/parse/DependencySorterTest.java
@@ -0,0 +1,119 @@
+package org.smoothbuild.parse;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyZeroInteractions;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.Matchers;
+import org.smoothbuild.parse.err.CycleInCallGraphError;
+import org.smoothbuild.problem.ProblemsListener;
+import org.smoothbuild.problem.SourceLocation;
+import org.smoothbuild.testing.parse.TestingImportedFunctions;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
+
+public class DependencySorterTest {
+  private static final String NAME1 = "funcation1";
+  private static final String NAME2 = "funcation2";
+  private static final String NAME3 = "funcation3";
+  private static final String NAME4 = "funcation4";
+  private static final String NAME5 = "funcation5";
+  private static final String NAME6 = "funcation6";
+
+  ProblemsListener problemsListener = mock(ProblemsListener.class);
+  SymbolTable importedFunctions = new TestingImportedFunctions();
+
+  DependencySorter sorter = new DependencySorter(problemsListener);
+
+  @Test
+  public void linearDependency() {
+    Map<String, Set<Dependency>> map = Maps.newHashMap();
+    map.put(NAME3, dependencies(NAME4));
+    map.put(NAME1, dependencies(NAME2));
+    map.put(NAME4, dependencies());
+    map.put(NAME2, dependencies(NAME3));
+
+    List<String> actual = sorter.sortByDependency(importedFunctions, map);
+
+    assertThat(actual).isEqualTo(ImmutableList.of(NAME4, NAME3, NAME2, NAME1));
+    verifyZeroInteractions(problemsListener);
+  }
+
+  @Test
+  public void treeDependency() {
+    Map<String, Set<Dependency>> map = Maps.newHashMap();
+    map.put(NAME1, dependencies(NAME2, NAME3));
+    map.put(NAME2, dependencies(NAME4));
+    map.put(NAME3, dependencies(NAME5));
+    map.put(NAME4, dependencies());
+    map.put(NAME5, dependencies(NAME6));
+    map.put(NAME6, dependencies());
+
+    List<String> actual = sorter.sortByDependency(importedFunctions, map);
+
+    assertContainsSubsequence(actual, NAME4, NAME2, NAME1);
+    assertContainsSubsequence(actual, NAME6, NAME5, NAME3, NAME1);
+
+    verifyZeroInteractions(problemsListener);
+  }
+
+  @Test
+  public void simpleRecursionIsReported() throws Exception {
+    Map<String, Set<Dependency>> map = Maps.newHashMap();
+    map.put(NAME1, dependencies(NAME1));
+
+    sorter.sortByDependency(importedFunctions, map);
+
+    verify(problemsListener).report(Matchers.isA(CycleInCallGraphError.class));
+  }
+
+  @Test
+  public void cycleIsReported() throws Exception {
+    Map<String, Set<Dependency>> map = Maps.newHashMap();
+    map.put(NAME1, dependencies(NAME2));
+    map.put(NAME2, dependencies(NAME3));
+    map.put(NAME3, dependencies(NAME1));
+
+    sorter.sortByDependency(importedFunctions, map);
+
+    verify(problemsListener).report(Matchers.isA(CycleInCallGraphError.class));
+  }
+
+  private void assertContainsSubsequence(List<String> list, String... names) {
+    int listIndex = 0;
+    int namesIndex = 0;
+
+    while (listIndex < list.size() && namesIndex < names.length) {
+      if (list.get(listIndex).equals(names[namesIndex])) {
+        namesIndex++;
+      }
+      listIndex++;
+    }
+    if (namesIndex < names.length) {
+      Assert.fail("List " + list.toString() + " should contain subsequence "
+          + Arrays.toString(names));
+    }
+  }
+
+  private static Set<Dependency> dependencies(String... names) {
+    Set<Dependency> result = Sets.newHashSet();
+    for (String name : names) {
+      result.add(dependency(name));
+    }
+    return result;
+  }
+
+  private static Dependency dependency(String name) {
+    return new Dependency(new SourceLocation(1, 2, 3), name);
+  }
+}
