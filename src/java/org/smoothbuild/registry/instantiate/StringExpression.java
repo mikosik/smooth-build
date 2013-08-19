18f151a2577a9910f4f4e91e9ee514573fe575b3
mikosik <marcin.mikosik@gmail.com>
2013-08-19 16:02:29 +0200

    added StringExpression

---
+++ src/java/org/smoothbuild/registry/instantiate/StringExpression.java
@@ -0,0 +1,32 @@
+package org.smoothbuild.registry.instantiate;
+
+import org.smoothbuild.lang.function.Type;
+import org.smoothbuild.lang.function.exc.FunctionException;
+
+public class StringExpression implements Expression {
+  private final ExpressionId id;
+  private final String string;
+
+  public StringExpression(ExpressionId id, String string) {
+    this.id = id;
+    this.string = string;
+  }
+
+  @Override
+  public ExpressionId id() {
+    return id;
+  }
+
+  @Override
+  public Type type() {
+    return Type.STRING;
+  }
+
+  @Override
+  public void execute() throws FunctionException {}
+
+  @Override
+  public Object result() {
+    return string;
+  }
+}
---
+++ src/test/org/smoothbuild/registry/instantiate/StringExpressionTest.java
@@ -0,0 +1,28 @@
+package org.smoothbuild.registry.instantiate;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
+import org.junit.Test;
+import org.smoothbuild.lang.function.Type;
+
+public class StringExpressionTest {
+  ExpressionId id = new ExpressionId("abc");
+  String string = "string-value";
+
+  StringExpression expression = new StringExpression(id, string);
+
+  @Test
+  public void id() {
+    assertThat(expression.id()).isSameAs(id);
+  }
+
+  @Test
+  public void type() {
+    assertThat(expression.type()).isEqualTo(Type.STRING);
+  }
+
+  @Test
+  public void result() {
+    assertThat(expression.result()).isEqualTo(string);
+  }
+}
