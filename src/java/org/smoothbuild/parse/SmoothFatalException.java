e68f351ceaba3b0fa845769807b12195c82617dc
mikosik <marcin.mikosik@gmail.com>
2013-08-22 13:01:40 +0200

    got rid of SmoothFailedException

--- src/java/org/smoothbuild/parse/SmoothFatalException.java
+++
@@ -1,8 +0,0 @@
-package org.smoothbuild.parse;
-
-@SuppressWarnings("serial")
-public class SmoothFatalException extends RuntimeException {
-  public SmoothFatalException(Throwable e) {
-    super(e);
-  }
-}
