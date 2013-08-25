a8c793bdde8ae5d0cac1f3381d41701b0362c2c4
mikosik <marcin.mikosik@gmail.com>
2013-08-25 15:43:20 +0200

    added Argument

---
+++ src/java/org/smoothbuild/parse/Argument.java
@@ -0,0 +1,34 @@
+package org.smoothbuild.parse;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import org.smoothbuild.expression.Expression;
+import org.smoothbuild.problem.SourceLocation;
+
+public class Argument {
+  private final String name;
+  private final Expression expression;
+  private final SourceLocation sourceLocation;
+
+  public Argument(String name, Expression expression, SourceLocation sourceLocation) {
+    this.name = name;
+    this.expression = checkNotNull(expression);
+    this.sourceLocation = checkNotNull(sourceLocation);
+  }
+
+  public String name() {
+    return name;
+  }
+
+  public Expression expression() {
+    return expression;
+  }
+
+  public SourceLocation sourceLocation() {
+    return sourceLocation;
+  }
+
+  public boolean isExplicit() {
+    return name != null;
+  }
+}
