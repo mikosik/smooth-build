1c2401e8d6132b7f2dc4939152614c75c333c25c
mikosik <marcin.mikosik@gmail.com>
2013-08-26 11:16:07 +0200

    refactored/simplified IllegalReturnTypeException

--- src/java/org/smoothbuild/function/exc/IllegalReturnTypeException.java
+++ src/java/org/smoothbuild/function/exc/IllegalReturnTypeException.java
@@ -2,17 +2,11 @@ package org.smoothbuild.function.exc;
 
 import org.smoothbuild.function.Type;
 
-import com.google.common.base.Joiner;
-
 @SuppressWarnings("serial")
 public class IllegalReturnTypeException extends FunctionImplementationException {
 
   public IllegalReturnTypeException(Class<?> klass, Class<?> returnType) {
     super(klass, "Return type of execute() method is illegal '" + returnType.getCanonicalName()
-        + "'.\n Only following types are allowed: " + allowedResultTypes() + ".");
-  }
-
-  private static String allowedResultTypes() {
-    return "{" + Joiner.on(", ").join(Type.javaTypesAllowedForResult()) + "}";
+        + "'.\n Only following types are allowed: " + Type.javaTypesAllowedForResult() + ".");
   }
 }
