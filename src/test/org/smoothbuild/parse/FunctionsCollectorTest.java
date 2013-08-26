13dcdf789e974b7073e1e4a7045717bd55160a1f
mikosik <marcin.mikosik@gmail.com>
2013-08-26 14:04:47 +0200

    refactored FunctionsCollector

--- src/test/org/smoothbuild/parse/FunctionsCollectorTest.java
+++ src/test/org/smoothbuild/parse/FunctionsCollectorTest.java
@@ -3,6 +3,7 @@ package org.smoothbuild.parse;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.smoothbuild.testing.parse.TestingFunction.function;
 import static org.smoothbuild.testing.parse.TestingImportedFunctions.IMPORTED_NAME;
+import static org.smoothbuild.testing.parse.TestingModule.testingModule;
 
 import org.junit.Test;
 import org.smoothbuild.antlr.SmoothParser.FunctionContext;
@@ -10,17 +11,19 @@ import org.smoothbuild.parse.err.DuplicateFunctionError;
 import org.smoothbuild.parse.err.IllegalFunctionNameError;
 import org.smoothbuild.parse.err.OverridenImportWarning;
 import org.smoothbuild.testing.parse.TestingImportedFunctions;
+import org.smoothbuild.testing.parse.TestingModule;
 import org.smoothbuild.testing.problem.TestingProblemsListener;
 
 import com.google.common.collect.ImmutableMap;
+import com.google.inject.util.Providers;
 
 public class FunctionsCollectorTest {
 
   TestingProblemsListener problemsListener = new TestingProblemsListener();
   SymbolTable importedFunctions = new TestingImportedFunctions();
 
-  FunctionsCollector functionsCollector = new FunctionsCollector(problemsListener,
-      importedFunctions);
+  FunctionsCollector functionsCollector = new FunctionsCollector(
+      Providers.of(new FunctionsCollector.FunctionVisitor(importedFunctions, problemsListener)));
 
   @Test
   public void visitedFunctionNamesAreReturned() throws Exception {
@@ -29,32 +32,28 @@ public class FunctionsCollectorTest {
 
     FunctionContext function1 = function(name1);
     FunctionContext function2 = function(name2);
-
-    functionsCollector.visitFunction(function1);
-    functionsCollector.visitFunction(function2);
+    TestingModule module = testingModule(function1, function2);
 
     ImmutableMap<String, FunctionContext> expected = ImmutableMap.of(name1, function1, name2,
         function2);
-    assertThat(functionsCollector.foundFunctions()).isEqualTo(expected);
+    assertThat(functionsCollector.parse(module)).isEqualTo(expected);
   }
 
   @Test
   public void illegalFunctionNameIsReported() {
-    functionsCollector.visitFunction(function("function-name"));
+    functionsCollector.parse(testingModule(function("function-name")));
     problemsListener.assertOnlyProblem(IllegalFunctionNameError.class);
   }
 
   @Test
   public void duplicateFunction() throws Exception {
-    functionsCollector.visitFunction(function("functionA"));
-    functionsCollector.visitFunction(function("functionA"));
-
+    functionsCollector.parse(testingModule(function("functionA"), function("functionA")));
     problemsListener.assertOnlyProblem(DuplicateFunctionError.class);
   }
 
   @Test
   public void overridenImport() throws Exception {
-    functionsCollector.visitFunction(function(IMPORTED_NAME));
+    functionsCollector.parse(testingModule(function(IMPORTED_NAME)));
     problemsListener.assertOnlyProblem(OverridenImportWarning.class);
   }
 
