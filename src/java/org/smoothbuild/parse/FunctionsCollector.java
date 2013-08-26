9009129de490e8aa5b0438a712d895b6a6187672
mikosik <marcin.mikosik@gmail.com>
2013-08-26 14:04:47 +0200

    refactored FunctionsCollector

--- src/java/org/smoothbuild/parse/FunctionsCollector.java
+++ src/java/org/smoothbuild/parse/FunctionsCollector.java
@@ -4,9 +4,13 @@ import static org.smoothbuild.function.FullyQualifiedName.isValidSimpleName;
 
 import java.util.Map;
 
+import javax.inject.Inject;
+import javax.inject.Provider;
+
 import org.smoothbuild.antlr.SmoothBaseVisitor;
 import org.smoothbuild.antlr.SmoothParser.FunctionContext;
 import org.smoothbuild.antlr.SmoothParser.FunctionNameContext;
+import org.smoothbuild.antlr.SmoothParser.ModuleContext;
 import org.smoothbuild.function.FullyQualifiedName;
 import org.smoothbuild.parse.err.DuplicateFunctionError;
 import org.smoothbuild.parse.err.IllegalFunctionNameError;
@@ -21,43 +25,60 @@ import com.google.common.collect.Maps;
  * FunctionContext. Detects illegal function names, duplicate function names,
  * overridden imports.
  */
-public class FunctionsCollector extends SmoothBaseVisitor<Void> {
-  private final ProblemsListener problemsListener;
-  private final SymbolTable importedFunctions;
-  private final Map<String, FunctionContext> functions;
+public class FunctionsCollector {
+  private final Provider<FunctionVisitor> visitorProvider;
 
-  public FunctionsCollector(ProblemsListener problemsListener, SymbolTable importedFunctions) {
-    this.problemsListener = problemsListener;
-    this.importedFunctions = importedFunctions;
-    this.functions = Maps.newHashMap();
+  @Inject
+  public FunctionsCollector(Provider<FunctionVisitor> visitor) {
+    this.visitorProvider = visitor;
   }
 
-  @Override
-  public Void visitFunction(FunctionContext functionContext) {
-    FunctionNameContext nameContext = functionContext.functionName();
-    String name = nameContext.getText();
-
-    if (!isValidSimpleName(name)) {
-      problemsListener.report(new IllegalFunctionNameError(Helpers.locationOf(nameContext), name));
-      return null;
-    }
-
-    if (functions.keySet().contains(name)) {
-      problemsListener.report(new DuplicateFunctionError(Helpers.locationOf(nameContext), name));
-      return null;
-    }
-    if (importedFunctions.containsFunction(name)) {
-      FullyQualifiedName importedName = importedFunctions.getFunction(name).name();
-      SourceLocation location = Helpers.locationOf(nameContext);
-      problemsListener.report(new OverridenImportWarning(location, name, importedName));
-      return null;
-    }
-
-    functions.put(name, functionContext);
-    return null;
+  public Map<String, FunctionContext> parse(ModuleContext module) {
+    FunctionVisitor functionVisitor = visitorProvider.get();
+    functionVisitor.visit(module);
+    return functionVisitor.foundFunctions();
   }
 
-  public Map<String, FunctionContext> foundFunctions() {
-    return functions;
+  public static class FunctionVisitor extends SmoothBaseVisitor<Void> {
+    private final SymbolTable importedFunctions;
+    private final ProblemsListener problemsListener;
+    private final Map<String, FunctionContext> functions;
+
+    @Inject
+    public FunctionVisitor(SymbolTable importedFunctions, ProblemsListener problemsListener) {
+      this.importedFunctions = importedFunctions;
+      this.problemsListener = problemsListener;
+      this.functions = Maps.newHashMap();
+    }
+
+    @Override
+    public Void visitFunction(FunctionContext functionContext) {
+      FunctionNameContext nameContext = functionContext.functionName();
+      String name = nameContext.getText();
+
+      if (!isValidSimpleName(name)) {
+        problemsListener
+            .report(new IllegalFunctionNameError(Helpers.locationOf(nameContext), name));
+        return null;
+      }
+
+      if (functions.keySet().contains(name)) {
+        problemsListener.report(new DuplicateFunctionError(Helpers.locationOf(nameContext), name));
+        return null;
+      }
+      if (importedFunctions.containsFunction(name)) {
+        FullyQualifiedName importedName = importedFunctions.getFunction(name).name();
+        SourceLocation location = Helpers.locationOf(nameContext);
+        problemsListener.report(new OverridenImportWarning(location, name, importedName));
+        return null;
+      }
+
+      functions.put(name, functionContext);
+      return null;
+    }
+
+    public Map<String, FunctionContext> foundFunctions() {
+      return functions;
+    }
   }
 }
