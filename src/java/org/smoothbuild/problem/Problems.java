4cee506509b1719089942589fdcbd20419d3a970
mikosik <marcin.mikosik@gmail.com>
2013-08-26 15:08:33 +0200

    added missing guice annotations to Problems/ProblemsListener

--- src/java/org/smoothbuild/problem/Problems.java
+++ src/java/org/smoothbuild/problem/Problems.java
@@ -2,9 +2,12 @@ package org.smoothbuild.problem;
 
 import java.util.List;
 
+import javax.inject.Singleton;
+
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
 
+@Singleton
 public class Problems implements ProblemsListener {
   private final List<Problem> list = Lists.newArrayList();
 
