7dcc2b1670ce4ae2ff1770067013d0ba2db2991d
mikosik <marcin.mikosik@gmail.com>
2013-08-26 11:36:55 +0200

    added more tests for FunctionFactory

--- src/java/org/smoothbuild/function/exc/NonPublicConstructorException.java
+++
@@ -1,9 +0,0 @@
-package org.smoothbuild.function.exc;
-
-@SuppressWarnings("serial")
-public class NonPublicConstructorException extends FunctionImplementationException {
-
-  public NonPublicConstructorException(Class<?> klass) {
-    super(klass, "Constructor should be public.");
-  }
-}
