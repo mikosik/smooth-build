0f89db3c09fcc08f141caf9e9ee3e91308dc7ba0
mikosik <marcin.mikosik@gmail.com>
2013-08-28 21:30:42 +0200

    added ScriptBuilder

---
+++ src/test/org/smoothbuild/testing/ScriptBuilderTest.java
@@ -0,0 +1,41 @@
+package org.smoothbuild.testing;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.smoothbuild.testing.ScriptBuilder.oneLineScript;
+
+import org.junit.Test;
+
+public class ScriptBuilderTest {
+
+  @Test
+  public void newLinesAreAppendedAtTheEndOfLines() {
+    ScriptBuilder builder = new ScriptBuilder();
+    builder.addLine("abc");
+    builder.addLine("def");
+    assertThat(builder.build()).isEqualTo("abc\ndef\n");
+  }
+
+  @Test
+  public void singleQuotesAreChangedIntoDoubleQuotes() throws Exception {
+    ScriptBuilder builder = new ScriptBuilder();
+    builder.addLine("a 'message' quoted");
+    assertThat(builder.build()).isEqualTo("a \"message\" quoted\n");
+  }
+
+  @Test
+  public void singleQuotesAreChangedIntoDoubleQuotesByOneLineScript() throws Exception {
+    assertThat(oneLineScript("a 'message' quoted")).isEqualTo("a \"message\" quoted");
+  }
+
+  @Test
+  public void doubleQuotesAreUnchanged() throws Exception {
+    ScriptBuilder builder = new ScriptBuilder();
+    builder.addLine("a \"message\" quoted");
+    assertThat(builder.build()).isEqualTo("a \"message\" quoted\n");
+  }
+
+  @Test
+  public void doubleQuotesAreUnchangedByOneLineScript() throws Exception {
+    assertThat(oneLineScript("a \"message\" quoted")).isEqualTo("a \"message\" quoted");
+  }
+}
