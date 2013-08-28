475915b73c6656552189eca65e73081b138bdcf9
mikosik <marcin.mikosik@gmail.com>
2013-08-28 21:30:42 +0200

    added ScriptBuilder

---
+++ src/testing/org/smoothbuild/testing/ScriptBuilder.java
@@ -0,0 +1,29 @@
+package org.smoothbuild.testing;
+
+/**
+ * Builder for easy creation of sample script in tests. All single quote
+ * characters (') are replaced by double quote characters ("). This way there's
+ * no need to use escape symbol when you need to place smooth string inside java
+ * string (Just use single quotes)..
+ */
+public class ScriptBuilder {
+  private final StringBuilder builder = new StringBuilder();
+
+  public static String oneLineScript(String string) {
+    return fromSingleToDoubleQuotes(string);
+  }
+
+  public ScriptBuilder addLine(String string) {
+    builder.append(fromSingleToDoubleQuotes(string));
+    builder.append("\n");
+    return this;
+  }
+
+  private static String fromSingleToDoubleQuotes(String string) {
+    return string.replace('\'', '"');
+  }
+
+  public String build() {
+    return builder.toString();
+  }
+}
