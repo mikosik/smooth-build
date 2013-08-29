68161b29f7b3648ba9d02a6992406da12d41dedc
mikosik <marcin.mikosik@gmail.com>
2013-08-29 12:26:01 +0200

    added NoSuchFileException

---
+++ src/java/org/smoothbuild/fs/base/exc/NoSuchFileException.java
@@ -0,0 +1,20 @@
+package org.smoothbuild.fs.base.exc;
+
+import java.io.FileNotFoundException;
+
+import org.smoothbuild.plugin.Path;
+
+@SuppressWarnings("serial")
+public class NoSuchFileException extends FileSystemException {
+  public NoSuchFileException(Path path, FileNotFoundException e) {
+    super(message(path), e);
+  }
+
+  public NoSuchFileException(Path path) {
+    super(message(path));
+  }
+
+  private static String message(Path path) {
+    return "Cannot find " + path.toString() + " file.";
+  }
+}
