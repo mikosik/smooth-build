bc30b99c2408bcedf0d9721143ba2f2294be7fa8
mikosik <marcin.mikosik@gmail.com>
2013-08-29 10:32:35 +0200

    fixed InMemoryFileSystemModule so FileSystem is @Singleton

--- src/test/org/smoothbuild/fs/mem/InMemoryFileSystemModule.java
+++ src/test/org/smoothbuild/fs/mem/InMemoryFileSystemModule.java
@@ -1,5 +1,7 @@
 package org.smoothbuild.fs.mem;
 
+import javax.inject.Singleton;
+
 import org.smoothbuild.fs.base.FileSystem;
 
 import com.google.inject.AbstractModule;
@@ -7,6 +9,7 @@ import com.google.inject.AbstractModule;
 public class InMemoryFileSystemModule extends AbstractModule {
   @Override
   protected void configure() {
+    bind(InMemoryFileSystem.class).in(Singleton.class);
     bind(FileSystem.class).to(InMemoryFileSystem.class);
   }
 }
