0028ce11ba239b6e71345c99e813cee82428551f
mikosik <marcin.mikosik@gmail.com>
2013-08-29 11:36:32 +0200

    fixed IllegalArgException

--- src/java/org/smoothbuild/plugin/exc/IllegalArgException.java
+++ src/java/org/smoothbuild/plugin/exc/IllegalArgException.java
@@ -1,9 +1,8 @@
 package org.smoothbuild.plugin.exc;
 
-
 @SuppressWarnings("serial")
 public class IllegalArgException extends ParamException {
   public IllegalArgException(String paramName, String message) {
-    super(paramName, "Parameter '' has been set to illegal value. " + message);
+    super(paramName, "Parameter '" + paramName + "' has been set to illegal value. " + message);
   }
 }
