4e87de6973003c76e048f524ff4fdd05a650b6b1
mikosik <marcin.mikosik@gmail.com>
2013-08-29 14:05:01 +0200

    added @ImplementedBy(PrintingProblemsListener.class) to ProblemsListener

--- src/java/org/smoothbuild/problem/ProblemsListener.java
+++ src/java/org/smoothbuild/problem/ProblemsListener.java
@@ -1,6 +1,8 @@
 package org.smoothbuild.problem;
 
+import com.google.inject.ImplementedBy;
 
+@ImplementedBy(PrintingProblemsListener.class)
 public interface ProblemsListener {
   public void report(Problem problem);
 }
