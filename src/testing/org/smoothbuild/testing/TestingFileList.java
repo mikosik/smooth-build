7af132fb4371935441a427e854bb3a3284ce5453
mikosik <marcin.mikosik@gmail.com>
2013-09-02 10:10:35 +0200

    added TestingFileList(TestingFileSystem) constructor

--- src/testing/org/smoothbuild/testing/TestingFileList.java
+++ src/testing/org/smoothbuild/testing/TestingFileList.java
@@ -1,12 +1,23 @@
 package org.smoothbuild.testing;
 
-import org.smoothbuild.fs.mem.MemoryFileSystem;
 import org.smoothbuild.fs.plugin.FileListImpl;
 import org.smoothbuild.plugin.Path;
 
 public class TestingFileList extends FileListImpl {
+  private final TestingFileSystem fileSystem;
+
   public TestingFileList() {
-    super(new MemoryFileSystem(), Path.rootPath());
+    this(new TestingFileSystem());
+  }
+
+  public TestingFileList(TestingFileSystem fileSystem) {
+    super(fileSystem, Path.rootPath());
+    this.fileSystem = fileSystem;
+  }
+
+  @Override
+  public TestingFileSystem fileSystem() {
+    return fileSystem;
   }
 
   @Override
