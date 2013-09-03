08714299d74021220f188ed07c4232988a893586
mikosik <marcin.mikosik@gmail.com>
2013-09-03 11:16:05 +0200

    refactored PluginTaskTest

--- src/test/org/smoothbuild/task/PluginTaskTest.java
+++ src/test/org/smoothbuild/task/PluginTaskTest.java
@@ -9,27 +9,28 @@ import java.lang.reflect.InvocationTargetException;
 
 import org.junit.Test;
 import org.smoothbuild.function.plugin.PluginInvoker;
-import org.smoothbuild.plugin.Path;
 import org.smoothbuild.plugin.TestingSandbox;
+import org.smoothbuild.problem.Problem;
 import org.smoothbuild.task.err.ReflexiveInternalError;
+import org.smoothbuild.util.Empty;
 
 import com.google.common.collect.ImmutableMap;
 
 public class PluginTaskTest {
   PluginInvoker pluginInvoker = mock(PluginInvoker.class);
-  Task subTask = mock(Task.class);
   TestingSandbox sandbox = new TestingSandbox();
-  String name = "param";
-  Object argValue = "argValue";
 
-  Path tempDir = Path.path("temp/dir");
-
-  PluginTask pluginTask = new PluginTask(testingSignature(), pluginInvoker, ImmutableMap.of(name,
-      subTask));
+  PluginTask pluginTask = new PluginTask(testingSignature(), pluginInvoker, Empty.stringTaskMap());
 
   @Test
   public void calculateResult() throws IllegalAccessException, InvocationTargetException {
+    Object argValue = "subTaskResult";
+    Task subTask = mock(Task.class);
     when(subTask.result()).thenReturn(argValue);
+
+    String name = "param";
+    PluginTask pluginTask = new PluginTask(testingSignature(), pluginInvoker, ImmutableMap.of(name,
+        subTask));
 
     String result = "result";
     when(pluginInvoker.invoke(sandbox, ImmutableMap.of(name, argValue))).thenReturn(result);
@@ -39,15 +40,17 @@ public class PluginTaskTest {
   }
 
   @Test
-  public void calculateResultReportErrorWhenExceptionIsThrownFromPluginInvoker()
-      throws IllegalAccessException, InvocationTargetException {
-    when(subTask.result()).thenReturn(argValue);
-    when(pluginInvoker.invoke(sandbox, ImmutableMap.of(name, argValue))).thenThrow(
-        new IllegalAccessException(""));
+  public void reflexiveInternalErrorIsReportedForIllegalAccessException() throws Exception {
+    assertExceptionIsReportedAsProblem(new IllegalAccessException(""), ReflexiveInternalError.class);
+  }
+
+  private void assertExceptionIsReportedAsProblem(IllegalAccessException thrown,
+      Class<? extends Problem> expected) throws Exception {
+    when(pluginInvoker.invoke(sandbox, Empty.stringObjectMap())).thenThrow(thrown);
 
     pluginTask.calculateResult(sandbox);
 
-    sandbox.problems().assertOnlyProblem(ReflexiveInternalError.class);
+    sandbox.problems().assertOnlyProblem(expected);
     assertThat(pluginTask.isResultCalculated()).isFalse();
   }
 
