de13cde2d8bf6a9ae0f73626c4abf42eb87655c7
mikosik <marcin.mikosik@gmail.com>
2013-09-03 11:02:08 +0200

    improved message in FileSystemError

--- src/java/org/smoothbuild/task/err/FileSystemError.java
+++ src/java/org/smoothbuild/task/err/FileSystemError.java
@@ -1,11 +1,29 @@
 package org.smoothbuild.task.err;
 
+import static org.smoothbuild.command.SmoothContants.BUILD_DIR;
+
+import org.smoothbuild.function.base.Name;
 import org.smoothbuild.problem.Error;
 
 import com.google.common.base.Throwables;
 
 public class FileSystemError extends Error {
-  public FileSystemError(Throwable e) {
-    super("FileSystem error: " + Throwables.getStackTraceAsString(e));
+  public FileSystemError(Name name, Throwable e) {
+    super(createMessage(name, e));
+  }
+
+  private static String createMessage(Name name, Throwable e) {
+    StringBuilder b = new StringBuilder();
+
+    b.append("Invoking function " + name + " caused internal exception:");
+    b.append("It was probably caused by:");
+    b.append(" - some other process (maybe other instance of Smooth) changing content of "
+        + BUILD_DIR + " dir");
+    b.append(" - bug in " + name + " function (plugin) implementation");
+    b.append(" - bug in Smooth tool");
+    b.append("Full java stacktrace below:");
+    b.append(Throwables.getStackTraceAsString(e));
+
+    return b.toString();
   }
 }
