02080390f37c6ce58133be6c7464b03f57e3ebe3
mikosik <marcin.mikosik@gmail.com>
2013-09-03 11:24:19 +0200

    added more tests to PluginTaskTest

--- src/test/org/smoothbuild/task/PluginTaskTest.java
+++ src/test/org/smoothbuild/task/PluginTaskTest.java
@@ -3,15 +3,22 @@ package org.smoothbuild.task;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+import static org.smoothbuild.function.base.Name.simpleName;
+import static org.smoothbuild.function.base.Type.VOID;
 import static org.smoothbuild.testing.TestingSignature.testingSignature;
 
 import java.lang.reflect.InvocationTargetException;
 
 import org.junit.Test;
+import org.smoothbuild.fs.base.exc.FileSystemException;
+import org.smoothbuild.function.base.Signature;
 import org.smoothbuild.function.plugin.PluginInvoker;
 import org.smoothbuild.plugin.TestingSandbox;
 import org.smoothbuild.problem.Problem;
+import org.smoothbuild.task.err.FileSystemError;
+import org.smoothbuild.task.err.NullResultError;
 import org.smoothbuild.task.err.ReflexiveInternalError;
+import org.smoothbuild.task.err.UnexpectedError;
 import org.smoothbuild.util.Empty;
 
 import com.google.common.collect.ImmutableMap;
@@ -40,11 +47,46 @@ public class PluginTaskTest {
   }
 
   @Test
+  public void nullResultErrorIsReportedWhenNullIsReturnByFunctionReturningNonVoidType()
+      throws Exception {
+    when(pluginInvoker.invoke(sandbox, Empty.stringObjectMap())).thenReturn(null);
+
+    pluginTask.calculateResult(sandbox);
+
+    sandbox.problems().assertOnlyProblem(NullResultError.class);
+    assertThat(pluginTask.isResultCalculated()).isFalse();
+  }
+
+  @Test
+  public void nullCanBeReturnedByFunctionOfVoidType() throws Exception {
+    pluginTask = new PluginTask(new Signature(VOID, simpleName("name"), Empty.stringParamMap()),
+        pluginInvoker, Empty.stringTaskMap());
+    when(pluginInvoker.invoke(sandbox, Empty.stringObjectMap())).thenReturn(null);
+
+    pluginTask.calculateResult(sandbox);
+
+    sandbox.problems().assertNoProblems();
+    assertThat(pluginTask.isResultCalculated()).isTrue();
+  }
+
+  @Test
   public void reflexiveInternalErrorIsReportedForIllegalAccessException() throws Exception {
     assertExceptionIsReportedAsProblem(new IllegalAccessException(""), ReflexiveInternalError.class);
   }
 
-  private void assertExceptionIsReportedAsProblem(IllegalAccessException thrown,
+  @Test
+  public void fileSystemErrorIsReportedForFileSystemException() throws Exception {
+    InvocationTargetException exception = new InvocationTargetException(new FileSystemException(""));
+    assertExceptionIsReportedAsProblem(exception, FileSystemError.class);
+  }
+
+  @Test
+  public void unexpectedErrorIsReportedForUnexpectedRuntimeException() throws Exception {
+    InvocationTargetException exception = new InvocationTargetException(new RuntimeException());
+    assertExceptionIsReportedAsProblem(exception, UnexpectedError.class);
+  }
+
+  private void assertExceptionIsReportedAsProblem(Throwable thrown,
       Class<? extends Problem> expected) throws Exception {
     when(pluginInvoker.invoke(sandbox, Empty.stringObjectMap())).thenThrow(thrown);
 
