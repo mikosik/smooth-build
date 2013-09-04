05d24b0190f6da00e2430f7b5bcbeddbdef0fbc8
mikosik <marcin.mikosik@gmail.com>
2013-09-04 18:58:19 +0200

    refactored StoredFile to use SubFileSystem

--- src/java/org/smoothbuild/fs/plugin/StoredFile.java
+++ src/java/org/smoothbuild/fs/plugin/StoredFile.java
@@ -4,18 +4,17 @@ import java.io.InputStream;
 import java.io.OutputStream;
 
 import org.smoothbuild.fs.base.FileSystem;
+import org.smoothbuild.fs.base.SubFileSystem;
 import org.smoothbuild.plugin.File;
 import org.smoothbuild.plugin.Path;
 
 public class StoredFile implements File {
   private final FileSystem fileSystem;
   private final Path path;
-  private final Path fullPath;
 
   public StoredFile(FileSystem fileSystem, Path root, Path path) {
-    this.fileSystem = fileSystem;
+    this.fileSystem = new SubFileSystem(fileSystem, root);
     this.path = path;
-    this.fullPath = root.append(path);
   }
 
   @Override
@@ -24,16 +23,16 @@ public class StoredFile implements File {
   }
 
   public Path fullPath() {
-    return fullPath;
+    return fileSystem.root().append(path);
   }
 
   @Override
   public InputStream createInputStream() {
-    return fileSystem.createInputStream(fullPath);
+    return fileSystem.createInputStream(path);
   }
 
   @Override
   public OutputStream createOutputStream() {
-    return fileSystem.createOutputStream(fullPath);
+    return fileSystem.createOutputStream(path);
   }
 }
