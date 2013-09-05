fb393ef688d94a508d6eea21b728a39f4cba403c
mikosik <marcin.mikosik@gmail.com>
2013-09-05 13:11:17 +0200

    added FileSetNode

---
+++ src/java/org/smoothbuild/function/def/FileSetNode.java
@@ -0,0 +1,32 @@
+package org.smoothbuild.function.def;
+
+import org.smoothbuild.function.base.Type;
+import org.smoothbuild.task.FileSetTask;
+import org.smoothbuild.task.Task;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.ImmutableSet.Builder;
+
+public class FileSetNode implements DefinitionNode {
+  private final ImmutableList<? extends DefinitionNode> elemNodes;
+
+  public FileSetNode(ImmutableList<? extends DefinitionNode> elemNodes) {
+    this.elemNodes = elemNodes;
+  }
+
+  @Override
+  public Type type() {
+    return Type.FILE_SET;
+  }
+
+  @Override
+  public Task generateTask() {
+    Builder<Task> builder = ImmutableSet.builder();
+    for (DefinitionNode node : elemNodes) {
+      builder.add(node.generateTask());
+    }
+    return new FileSetTask(builder.build());
+  }
+
+}
