8af07890b59178ba4da53dce2e06b62da85a054c
mikosik <marcin.mikosik@gmail.com>
2013-09-05 11:22:22 +0200

    fixed Type.STRING_SET, added missing tests

--- src/test/org/smoothbuild/function/base/TypeTest.java
+++ src/test/org/smoothbuild/function/base/TypeTest.java
@@ -8,6 +8,7 @@ import static org.smoothbuild.function.base.Type.JAVA_RESULT_TO_SMOOTH;
 import static org.smoothbuild.function.base.Type.PARAM_TYPES;
 import static org.smoothbuild.function.base.Type.RESULT_TYPES;
 import static org.smoothbuild.function.base.Type.STRING;
+import static org.smoothbuild.function.base.Type.STRING_SET;
 import static org.smoothbuild.function.base.Type.VOID;
 import static org.smoothbuild.function.base.Type.javaParamTypetoType;
 import static org.smoothbuild.function.base.Type.javaResultTypetoType;
@@ -15,6 +16,7 @@ import static org.smoothbuild.function.base.Type.javaResultTypetoType;
 import org.junit.Test;
 import org.smoothbuild.plugin.File;
 import org.smoothbuild.plugin.FileSet;
+import org.smoothbuild.plugin.StringSet;
 
 import com.google.inject.TypeLiteral;
 
@@ -23,6 +25,7 @@ public class TypeTest {
   @Test
   public void javaType() {
     assertThat((Object) STRING.javaType()).isEqualTo(type(String.class));
+    assertThat((Object) STRING_SET.javaType()).isEqualTo(type(StringSet.class));
     assertThat((Object) FILE.javaType()).isEqualTo(type(File.class));
     assertThat((Object) FILE_SET.javaType()).isEqualTo(type(FileSet.class));
     assertThat((Object) Type.VOID.javaType()).isEqualTo(type(Void.TYPE));
@@ -31,14 +34,22 @@ public class TypeTest {
   @Test
   public void isAssignableFrom() throws Exception {
     assertThat(STRING.isAssignableFrom(STRING)).isTrue();
+    assertThat(STRING.isAssignableFrom(STRING_SET)).isFalse();
     assertThat(STRING.isAssignableFrom(FILE)).isFalse();
     assertThat(STRING.isAssignableFrom(FILE_SET)).isFalse();
 
+    assertThat(STRING_SET.isAssignableFrom(STRING)).isFalse();
+    assertThat(STRING_SET.isAssignableFrom(STRING_SET)).isTrue();
+    assertThat(STRING_SET.isAssignableFrom(FILE)).isFalse();
+    assertThat(STRING_SET.isAssignableFrom(FILE_SET)).isFalse();
+
     assertThat(FILE.isAssignableFrom(STRING)).isFalse();
+    assertThat(FILE.isAssignableFrom(STRING_SET)).isFalse();
     assertThat(FILE.isAssignableFrom(FILE)).isTrue();
     assertThat(FILE.isAssignableFrom(FILE_SET)).isFalse();
 
     assertThat(FILE_SET.isAssignableFrom(STRING)).isFalse();
+    assertThat(FILE_SET.isAssignableFrom(STRING_SET)).isFalse();
     assertThat(FILE_SET.isAssignableFrom(FILE)).isFalse();
     assertThat(FILE_SET.isAssignableFrom(FILE_SET)).isTrue();
   }
@@ -58,13 +69,26 @@ public class TypeTest {
     assertThat(FILE_SET).isEqualTo(FILE_SET);
 
     assertThat(STRING.hashCode()).isNotEqualTo(FILE.hashCode());
+    assertThat(STRING.hashCode()).isNotEqualTo(STRING_SET.hashCode());
+    assertThat(STRING.hashCode()).isNotEqualTo(FILE_SET.hashCode());
+
+    assertThat(STRING_SET.hashCode()).isNotEqualTo(STRING.hashCode());
+    assertThat(STRING_SET.hashCode()).isNotEqualTo(FILE.hashCode());
+    assertThat(STRING_SET.hashCode()).isNotEqualTo(FILE_SET.hashCode());
+
+    assertThat(FILE.hashCode()).isNotEqualTo(STRING.hashCode());
+    assertThat(FILE.hashCode()).isNotEqualTo(STRING_SET.hashCode());
     assertThat(FILE.hashCode()).isNotEqualTo(FILE_SET.hashCode());
+
     assertThat(FILE_SET.hashCode()).isNotEqualTo(STRING.hashCode());
+    assertThat(FILE_SET.hashCode()).isNotEqualTo(STRING_SET.hashCode());
+    assertThat(FILE_SET.hashCode()).isNotEqualTo(FILE.hashCode());
   }
 
   @Test
   public void testJavaParamTypetoType() {
     assertThat(javaParamTypetoType(type(String.class))).isEqualTo(STRING);
+    assertThat(javaParamTypetoType(type(StringSet.class))).isEqualTo(STRING_SET);
     assertThat(javaParamTypetoType(type(File.class))).isEqualTo(FILE);
     assertThat(javaParamTypetoType(type(FileSet.class))).isEqualTo(FILE_SET);
   }
@@ -77,6 +101,7 @@ public class TypeTest {
   @Test
   public void testJavaResultTypetoType() {
     assertThat(javaResultTypetoType(type(String.class))).isEqualTo(STRING);
+    assertThat(javaResultTypetoType(type(StringSet.class))).isEqualTo(STRING_SET);
     assertThat(javaResultTypetoType(type(File.class))).isEqualTo(FILE);
     assertThat(javaResultTypetoType(type(FileSet.class))).isEqualTo(FILE_SET);
     assertThat(javaResultTypetoType(type(Void.TYPE))).isEqualTo(VOID);
