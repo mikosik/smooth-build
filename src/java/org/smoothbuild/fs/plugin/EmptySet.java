c170a97c89c586dd9f638078c3ced0fdedb72e5d
mikosik <marcin.mikosik@gmail.com>
2013-09-05 19:35:19 +0200

    removed unnecessary code from EmptySet

--- src/java/org/smoothbuild/fs/plugin/EmptySet.java
+++ src/java/org/smoothbuild/fs/plugin/EmptySet.java
@@ -1,11 +1,5 @@
 package org.smoothbuild.fs.plugin;
 
 public class EmptySet {
-  private static final EmptySet INSTANCE = new EmptySet();
-
   private EmptySet() {}
-
-  public static EmptySet emptySet() {
-    return INSTANCE;
-  }
 }
