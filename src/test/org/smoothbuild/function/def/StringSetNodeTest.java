b99ececc23c60caea6268b854f0c053c2e2911e0
mikosik <marcin.mikosik@gmail.com>
2013-09-05 12:50:57 +0200

    added StringSetNode

---
+++ src/test/org/smoothbuild/function/def/StringSetNodeTest.java
@@ -0,0 +1,35 @@
+package org.smoothbuild.function.def;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.smoothbuild.function.base.Type.STRING_SET;
+
+import org.junit.Test;
+import org.smoothbuild.plugin.StringSet;
+import org.smoothbuild.task.Task;
+
+import com.google.common.collect.ImmutableList;
+
+public class StringSetNodeTest {
+  String string1 = "string1";
+  String string2 = "string1";
+
+  StringNode node1 = new StringNode(string1);
+  StringNode node2 = new StringNode(string2);
+
+  ImmutableList<StringNode> elemNodes = ImmutableList.of(node1, node2);
+
+  StringSetNode stringSetNode = new StringSetNode(elemNodes);
+
+  @Test
+  public void type() {
+    assertThat(stringSetNode.type()).isEqualTo(STRING_SET);
+  }
+
+  @Test
+  public void generateTask() throws Exception {
+    Task task = stringSetNode.generateTask();
+    task.execute(null);
+    assertThat((StringSet) task.result()).containsOnly(string1, string2);
+  }
+
+}
