45266a2a6680b3118ef6273adf6b57e57feff653
mikosik <marcin.mikosik@gmail.com>
2013-09-06 17:27:31 +0200

    refactored FileSmoothTest

--- src/test/org/smoothbuild/integration/file/FileSmoothTest.java
+++ src/test/org/smoothbuild/integration/file/FileSmoothTest.java
@@ -10,44 +10,56 @@ import org.smoothbuild.plugin.api.Path;
 import org.smoothbuild.testing.ScriptBuilder;
 
 public class FileSmoothTest extends IntegrationTestCase {
-  Path file = path("file/path/file.txt");
-  Path destinationDir = path("destination/dir");
 
   @Test
   public void saveFile_pipe() throws IOException {
-    script("run : file(path='" + file.value() + "') | save(dir='" + destinationDir.value() + "');");
+    // given
+    Path file = path("file/path/file.txt");
+    Path dir = path("destination/dir");
+    script("run : file(path='" + file.value() + "') | save(dir='" + dir.value() + "');");
     fileSystem.createFileContainingItsPath(file);
 
+    // when
     smoothRunner.run("run");
 
+    // then
     problems.assertNoProblems();
-    fileSystem.assertFileContainsItsPath(destinationDir, file);
+    fileSystem.assertFileContainsItsPath(dir, file);
   }
 
   @Test
   public void saveFile_nestedCalls() throws IOException {
-    script("run : save(dir='" + destinationDir.value() + "', file=file(path='" + file.value()
-        + "') );");
+    // given
+    Path file = path("file/path/file.txt");
+    Path dir = path("destination/dir");
+    script("run : save(dir='" + dir.value() + "', file=file(path='" + file.value() + "') );");
     fileSystem.createFileContainingItsPath(file);
 
+    // when
     smoothRunner.run("run");
 
+    // then
     problems.assertNoProblems();
-    fileSystem.assertFileContainsItsPath(destinationDir, file);
+    fileSystem.assertFileContainsItsPath(dir, file);
   }
 
   @Test
   public void saveFile_separeteFunctions() throws IOException {
+    // given
+    Path file = path("file/path/file.txt");
+    Path dir = path("destination/dir");
     ScriptBuilder builder = new ScriptBuilder();
     builder.addLine("filename : '" + file.value() + "';");
     builder.addLine("myfile : file(path=filename);");
-    builder.addLine("run : save(file=myfile, dir='" + destinationDir.value() + "');");
+    builder.addLine("run : save(file=myfile, dir='" + dir.value() + "');");
     script(builder.build());
     fileSystem.createFileContainingItsPath(file);
 
+    // when
     smoothRunner.run("run");
 
+    // then
     problems.assertNoProblems();
-    fileSystem.assertFileContainsItsPath(destinationDir, file);
+    fileSystem.assertFileContainsItsPath(dir, file);
   }
 }
