51d53809700b1b5c3e32e109ad4664231436b0d5
mikosik <marcin.mikosik@gmail.com>
2013-09-06 12:00:24 +0200

    added IntegrationTestCase.script()

--- src/test/org/smoothbuild/integration/IntegrationTestCase.java
+++ src/test/org/smoothbuild/integration/IntegrationTestCase.java
@@ -1,9 +1,13 @@
 package org.smoothbuild.integration;
 
 import static com.google.inject.Guice.createInjector;
+import static org.smoothbuild.command.SmoothContants.DEFAULT_SCRIPT;
+
+import java.io.IOException;
 
 import org.junit.Before;
 import org.smoothbuild.run.SmoothRunner;
+import org.smoothbuild.testing.ScriptBuilder;
 import org.smoothbuild.testing.TestingFileSystem;
 import org.smoothbuild.testing.TestingFileSystemModule;
 import org.smoothbuild.testing.problem.TestingProblemsListener;
@@ -25,4 +29,9 @@ public class IntegrationTestCase {
     problems = injector.getInstance(TestingProblemsListener.class);
     smoothRunner = injector.getInstance(SmoothRunner.class);
   }
+
+  public void script(String script) throws IOException {
+    fileSystem.createFileWithContent(DEFAULT_SCRIPT, ScriptBuilder.script(script));
+  }
+
 }
