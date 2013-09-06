0f10c3d1191033282be96201188b9ebe996555dd
mikosik <marcin.mikosik@gmail.com>
2013-09-06 16:24:23 +0200

    added FileSetSmoothTest

---
+++ src/test/org/smoothbuild/integration/file/FileSetSmoothTest.java
@@ -0,0 +1,77 @@
+package org.smoothbuild.integration.file;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.smoothbuild.plugin.api.Path.path;
+
+import java.io.IOException;
+
+import org.junit.Test;
+import org.smoothbuild.integration.IntegrationTestCase;
+import org.smoothbuild.plugin.api.Path;
+
+public class FileSetSmoothTest extends IntegrationTestCase {
+
+  @Test
+  public void saveFileSetWithTwoFiles() throws IOException {
+    // given
+    Path dir = path("destination/dir");
+    Path file1 = path("file/path/file1.txt");
+    Path file2 = path("file/path/file2.txt");
+
+    StringBuilder builder = new StringBuilder();
+    builder.append("myfiles : [ file(path='" + file1.value() + "'), file(path='" + file2.value()
+        + "') ];\n");
+    builder.append("run : myfiles | save(dir='" + dir.value() + "');\n");
+    script(builder.toString());
+
+    fileSystem.createFileContainingItsPath(file1);
+    fileSystem.createFileContainingItsPath(file2);
+
+    // when
+    smoothRunner.run("run");
+
+    // then
+    problems.assertNoProblems();
+    fileSystem.assertFileContainsItsPath(dir, file1);
+    fileSystem.assertFileContainsItsPath(dir, file2);
+  }
+
+  @Test
+  public void saveFileSetWithOneFile() throws IOException {
+    // given
+    Path dir = path("destination/dir");
+    Path file1 = path("file/path/file1.txt");
+
+    StringBuilder builder = new StringBuilder();
+    builder.append("myfiles : [ file(path='" + file1.value() + "') ];\n");
+    builder.append("run : myfiles | save(dir='" + dir.value() + "');\n");
+    script(builder.toString());
+
+    fileSystem.createFileContainingItsPath(file1);
+
+    // when
+    smoothRunner.run("run");
+
+    // then
+    problems.assertNoProblems();
+    fileSystem.assertFileContainsItsPath(dir, file1);
+  }
+
+  @Test
+  public void saveEmptyFileSet() throws IOException {
+    // given
+    Path dir = path("destination/dir");
+
+    StringBuilder builder = new StringBuilder();
+    builder.append("myfiles : [ ];\n");
+    builder.append("run : myfiles | save(dir='" + dir.value() + "');\n");
+    script(builder.toString());
+
+    // when
+    smoothRunner.run("run");
+
+    // then
+    problems.assertNoProblems();
+    assertThat(fileSystem.pathExists(dir)).isFalse();
+  }
+}
