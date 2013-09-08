07c7174cd29ebbe6c27e3a34ac7fc87070715fc4
mikosik <marcin.mikosik@gmail.com>
2013-09-08 22:29:40 +0200

    refactored StoredFileTest

--- src/test/org/smoothbuild/plugin/internal/StoredFileTest.java
+++ src/test/org/smoothbuild/plugin/internal/StoredFileTest.java
@@ -5,16 +5,14 @@ import static org.smoothbuild.plugin.api.Path.path;
 import static org.smoothbuild.testing.TestingFile.assertContentHasFilePath;
 
 import org.junit.Test;
-import org.smoothbuild.fs.base.SubFileSystem;
 import org.smoothbuild.plugin.api.Path;
 import org.smoothbuild.testing.TestingFileSystem;
 
 public class StoredFileTest {
   TestingFileSystem fileSystem = new TestingFileSystem();
-  Path rootDir = path("abc/efg");
   Path filePath = path("xyz/test.txt");
 
-  StoredFile storedFile = new StoredFile(new SubFileSystem(fileSystem, rootDir), filePath);
+  StoredFile storedFile = new StoredFile(fileSystem, filePath);
 
   @Test
   public void testPath() throws Exception {
@@ -22,8 +20,13 @@ public class StoredFileTest {
   }
 
   @Test
+  public void fileSystem() throws Exception {
+    assertThat(storedFile.fileSystem()).isSameAs(fileSystem);
+  }
+
+  @Test
   public void createInputStream() throws Exception {
-    fileSystem.createFileContainingItsPath(rootDir, filePath);
+    fileSystem.createFileContainingItsPath(filePath);
     assertContentHasFilePath(storedFile);
   }
 
