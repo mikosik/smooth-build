314126615efc90b656ac64105725f566eb6329d2
mikosik <marcin.mikosik@gmail.com>
2013-09-10 13:50:51 +0200

    added OutputClassFile

---
+++ src/java/org/smoothbuild/builtin/java/javac/OutputClassFile.java
@@ -0,0 +1,23 @@
+package org.smoothbuild.builtin.java.javac;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.URI;
+
+import javax.tools.SimpleJavaFileObject;
+
+import org.smoothbuild.plugin.api.MutableFile;
+
+public class OutputClassFile extends SimpleJavaFileObject {
+  private final MutableFile file;
+
+  public OutputClassFile(MutableFile file) {
+    super(URI.create("class:///" + file.path().value()), Kind.CLASS);
+    this.file = file;
+  }
+
+  @Override
+  public OutputStream openOutputStream() throws IOException {
+    return file.openOutputStream();
+  }
+}
