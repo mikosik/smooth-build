f78a78693b6e09248c824a600f62191983f916a2
mikosik <marcin.mikosik@gmail.com>
2013-09-10 12:36:14 +0200

    added StreamTester.inputStreamWithContent()/inputStreamToString(), refactored tests

--- src/test/org/smoothbuild/testing/common/StreamTesterTest.java
+++ src/test/org/smoothbuild/testing/common/StreamTesterTest.java
@@ -1,16 +1,34 @@
 package org.smoothbuild.testing.common;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.fail;
 import static org.smoothbuild.testing.common.StreamTester.assertContent;
+import static org.smoothbuild.testing.common.StreamTester.inputStreamToString;
+import static org.smoothbuild.testing.common.StreamTester.inputStreamWithContent;
 import static org.smoothbuild.testing.common.StreamTester.writeAndClose;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
-import org.junit.Assert;
 import org.junit.Test;
 
 public class StreamTesterTest {
+
+  @Test
+  public void testInputStreamWithContent() throws Exception {
+    String content = "content";
+    InputStream inputStream = inputStreamWithContent(content);
+    assertThat(inputStreamToString(inputStream)).isEqualTo(content);
+  }
+
+  @Test
+  public void testInputStreamWithEmptyContent() throws Exception {
+    String content = "";
+    InputStream inputStream = inputStreamWithContent(content);
+    assertThat(inputStreamToString(inputStream)).isEqualTo(content);
+  }
 
   @Test
   public void testWriteAndClose() throws IOException {
@@ -19,13 +37,6 @@ public class StreamTesterTest {
     writeAndClose(outputStream, content);
 
     assertContent(inputStream(outputStream), content);
-
-    try {
-      assertContent(inputStream(outputStream), "different content");
-      Assert.fail("exception should be thrown");
-    } catch (AssertionError e) {
-      // expected
-    }
   }
 
   @Test
@@ -35,12 +46,48 @@ public class StreamTesterTest {
     writeAndClose(outputStream, content);
 
     assertContent(inputStream(outputStream), content);
+  }
+
+  @Test
+  public void testAssertContentSucceedsWhenContentIsEqual() throws Exception {
+    String content = "content";
+    InputStream inputStream = inputStreamWithContent(content);
+
+    assertContent(inputStream, content);
+  }
+
+  @Test
+  public void testAssertContentFailsWhenContentIsNotEqual() throws Exception {
+    String content = "content";
+    InputStream inputStream = inputStreamWithContent(content + "suffix");
+
     try {
-      assertContent(inputStream(outputStream), "different content");
-      Assert.fail("exception should be thrown");
+      assertContent(inputStream, content);
     } catch (AssertionError e) {
       // expected
+      return;
     }
+    fail("exception should be thrown");
+  }
+
+  @Test
+  public void testInputStreamToString() throws Exception {
+    String content = "content";
+    InputStream inputStream = inputStreamWithContent(content);
+
+    String actual = inputStreamToString(inputStream);
+
+    assertThat(actual).isEqualTo(content);
+  }
+
+  @Test
+  public void testEmptyInputStreamToString() throws Exception {
+    String content = "";
+    InputStream inputStream = inputStreamWithContent(content);
+
+    String actual = inputStreamToString(inputStream);
+
+    assertThat(actual).isEqualTo(content);
   }
 
   private static ByteArrayInputStream inputStream(ByteArrayOutputStream outputStream) {
