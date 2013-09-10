cbcb2cf9e36080cc36d943a2b8d4f91eefe827d4
mikosik <marcin.mikosik@gmail.com>
2013-09-10 14:15:16 +0200

    added SandboxedJavaFileManager

---
+++ src/test/org/smoothbuild/builtin/java/javac/SandboxedJavaFileManagerTest.java
@@ -0,0 +1,71 @@
+package org.smoothbuild.builtin.java.javac;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verifyZeroInteractions;
+import static org.smoothbuild.plugin.api.Path.path;
+
+import java.io.IOException;
+
+import javax.tools.JavaFileObject;
+import javax.tools.JavaFileObject.Kind;
+import javax.tools.StandardJavaFileManager;
+import javax.tools.StandardLocation;
+
+import org.junit.Test;
+import org.smoothbuild.builtin.java.javac.err.IncorrectClassNameGivenByJavaCompilerError;
+import org.smoothbuild.plugin.api.Path;
+import org.smoothbuild.testing.common.StreamTester;
+import org.smoothbuild.testing.plugin.internal.FileTester;
+import org.smoothbuild.testing.plugin.internal.TestSandbox;
+
+import com.google.common.collect.ImmutableSet;
+
+public class SandboxedJavaFileManagerTest {
+  StandardJavaFileManager sfm = mock(StandardJavaFileManager.class);
+  TestSandbox sandbox = new TestSandbox();
+
+  SandboxedJavaFileManager manager = new SandboxedJavaFileManager(sfm, sandbox);
+
+  @Test
+  public void getJavaFileOutput() throws IOException {
+    String content = "content";
+    String className = "my.package.MyClass";
+
+    JavaFileObject javaFile = manager.getJavaFileForOutput(StandardLocation.CLASS_OUTPUT,
+        className, Kind.CLASS, null);
+    StreamTester.writeAndClose(javaFile.openOutputStream(), content);
+
+    Path classFilePath = path("my/package/MyClass.class");
+    FileTester.assertContentContains(sandbox.resultFileSet().file(classFilePath), content);
+  }
+
+  @Test
+  public void getJavaFileOutputIsNotForwardedToStandardManagerForClassOutput() throws Exception {
+    manager.getJavaFileForOutput(StandardLocation.CLASS_OUTPUT, "className", Kind.CLASS, null);
+    verifyZeroInteractions(sfm);
+  }
+
+  @Test
+  public void getJavaFileOutputReportsProblemWhenClassNameIsIllegal() throws Exception {
+    String content = "content";
+    String className = ".illegal.MyClass";
+
+    JavaFileObject javaFile = manager.getJavaFileForOutput(StandardLocation.CLASS_OUTPUT,
+        className, Kind.CLASS, null);
+    StreamTester.writeAndClose(javaFile.openOutputStream(), content);
+
+    assertThat(javaFile).isInstanceOf(DummyOutputClassFile.class);
+    sandbox.problems().assertOnlyProblem(IncorrectClassNameGivenByJavaCompilerError.class);
+    verifyZeroInteractions(sfm);
+  }
+
+  @Test
+  public void listingClassPathAlwaysReturnsEmptyIterable() throws IOException {
+    Iterable<?> result = manager.list(StandardLocation.CLASS_PATH, "my.package",
+        ImmutableSet.of(Kind.CLASS), false);
+
+    assertThat(result).isEmpty();
+    verifyZeroInteractions(sfm);
+  }
+}
