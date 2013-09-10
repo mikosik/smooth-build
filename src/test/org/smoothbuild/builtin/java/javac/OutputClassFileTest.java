7dae708d426daa3fc84cd62d689408053ec99cb3
mikosik <marcin.mikosik@gmail.com>
2013-09-10 13:50:51 +0200

    added OutputClassFile

---
+++ src/test/org/smoothbuild/builtin/java/javac/OutputClassFileTest.java
@@ -0,0 +1,30 @@
+package org.smoothbuild.builtin.java.javac;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.smoothbuild.plugin.api.Path.path;
+
+import java.io.IOException;
+
+import org.junit.Test;
+import org.smoothbuild.testing.common.StreamTester;
+import org.smoothbuild.testing.plugin.internal.TestFile;
+
+public class OutputClassFileTest {
+  TestFile file = new TestFile(path("my/file"));
+
+  OutputClassFile outputClassFile = new OutputClassFile(file);
+
+  @Test
+  public void openOutputStream() throws IOException {
+    String content = "content";
+    StreamTester.writeAndClose(outputClassFile.openOutputStream(), content);
+    file.assertContentContains(content);
+  }
+
+  @Test
+  public void uri() throws Exception {
+    String pathString = "my/path/file";
+    OutputClassFile inputSourceFile = new OutputClassFile(new TestFile(path(pathString)));
+    assertThat(inputSourceFile.getName()).isEqualTo("/" + pathString);
+  }
+}
