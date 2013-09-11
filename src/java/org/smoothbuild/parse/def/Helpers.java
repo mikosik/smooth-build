70e5772d5f016d08fd65dc931f6d508a22ebfb00
mikosik <marcin.mikosik@gmail.com>
2013-09-11 20:56:54 +0200

    added Helpers.createMap()

---
+++ src/java/org/smoothbuild/parse/def/Helpers.java
@@ -0,0 +1,19 @@
+package org.smoothbuild.parse.def;
+
+import java.util.Set;
+
+import org.smoothbuild.function.base.Type;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Sets;
+
+public class Helpers {
+
+  public static <T> ImmutableMap<Type, Set<T>> createMap(Set<Type> types) {
+    ImmutableMap.Builder<Type, Set<T>> builder = ImmutableMap.builder();
+    for (Type type : types) {
+      builder.put(type, Sets.<T> newHashSet());
+    }
+    return builder.build();
+  }
+}
---
+++ src/test/org/smoothbuild/parse/def/HelpersTest.java
@@ -0,0 +1,33 @@
+package org.smoothbuild.parse.def;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.smoothbuild.function.base.Type.EMPTY_SET;
+import static org.smoothbuild.function.base.Type.FILE;
+import static org.smoothbuild.function.base.Type.FILE_SET;
+import static org.smoothbuild.function.base.Type.STRING;
+import static org.smoothbuild.function.base.Type.STRING_SET;
+import static org.smoothbuild.function.base.Type.VOID;
+
+import java.util.Set;
+
+import org.junit.Test;
+import org.smoothbuild.function.base.Type;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+
+public class HelpersTest {
+
+  @Test
+  public void createMap() {
+    ImmutableMap<Type, Set<Object>> map = Helpers.createMap(ImmutableSet.of(STRING, STRING_SET));
+
+    assertThat(map.get(STRING)).isEmpty();
+    assertThat(map.get(STRING_SET)).isEmpty();
+
+    assertThat(map).doesNotContainKey(FILE);
+    assertThat(map).doesNotContainKey(FILE_SET);
+    assertThat(map).doesNotContainKey(EMPTY_SET);
+    assertThat(map).doesNotContainKey(VOID);
+  }
+}
