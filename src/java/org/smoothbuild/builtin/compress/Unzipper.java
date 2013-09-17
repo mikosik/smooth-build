fb9388768210faeb515cd6a25b99c552d0bd4d19
mikosik <marcin.mikosik@gmail.com>
2013-09-17 17:58:22 +0200

    refactored out Unzipper out of UnzipFunction

---
+++ src/java/org/smoothbuild/builtin/compress/Unzipper.java
@@ -0,0 +1,37 @@
+package org.smoothbuild.builtin.compress;
+
+import static org.smoothbuild.plugin.api.Path.path;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+import org.smoothbuild.plugin.api.File;
+import org.smoothbuild.plugin.api.MutableFile;
+import org.smoothbuild.plugin.api.MutableFileSet;
+
+public class Unzipper {
+  private final byte[] buffer = new byte[1024];;
+
+  public void unzipFile(File zipFile, MutableFileSet resultFiles) throws IOException {
+    try (ZipInputStream zipInputStream = new ZipInputStream(zipFile.openInputStream());) {
+      ZipEntry entry = null;
+      while ((entry = zipInputStream.getNextEntry()) != null) {
+        unzipEntry(zipInputStream, entry, resultFiles);
+      }
+    }
+  }
+
+  private File unzipEntry(ZipInputStream zipInputStream, ZipEntry entry, MutableFileSet resultFiles)
+      throws IOException {
+    MutableFile file = resultFiles.createFile(path(entry.getName()));
+    try (OutputStream outputStream = file.openOutputStream()) {
+      int len = 0;
+      while ((len = zipInputStream.read(buffer)) > 0) {
+        outputStream.write(buffer, 0, len);
+      }
+    }
+    return file;
+  }
+}
