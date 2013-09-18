a7ed3ab169534d4b8428b9d900f9254969830937
mikosik <marcin.mikosik@gmail.com>
2013-09-18 10:43:30 +0200

    refactored out Unjarer from UnjarFunction, added handling duplicate file names, and illegal paths

--- src/java/org/smoothbuild/builtin/java/UnjarFunction.java
+++ src/java/org/smoothbuild/builtin/java/UnjarFunction.java
@@ -1,16 +1,14 @@
 package org.smoothbuild.builtin.java;
 
-import static org.smoothbuild.plugin.api.Path.path;
-
 import java.io.IOException;
-import java.io.OutputStream;
-import java.util.jar.JarEntry;
-import java.util.jar.JarInputStream;
 
+import org.smoothbuild.builtin.java.err.DuplicatePathInJarError;
+import org.smoothbuild.builtin.java.err.DuplicatePathInJarException;
+import org.smoothbuild.builtin.java.err.IllegalPathInJarError;
+import org.smoothbuild.builtin.java.err.IllegalPathInJarException;
 import org.smoothbuild.fs.base.exc.FileSystemException;
 import org.smoothbuild.plugin.api.File;
 import org.smoothbuild.plugin.api.FileSet;
-import org.smoothbuild.plugin.api.MutableFile;
 import org.smoothbuild.plugin.api.MutableFileSet;
 import org.smoothbuild.plugin.api.Required;
 import org.smoothbuild.plugin.api.Sandbox;
@@ -24,44 +22,32 @@ public class UnjarFunction {
 
   @SmoothFunction("unjar")
   public static FileSet execute(Sandbox sandbox, Parameters params) {
-    return new Worker(sandbox, params).execute();
+    return new Worker().execute(sandbox, params);
   }
 
-  private static class Worker {
-    private final Sandbox sandbox;
-    private final Parameters params;
-    byte[] buffer;
+  public static class Worker {
+    private final Unjarer unjarer;
 
-    public Worker(Sandbox sandbox, Parameters params) {
-      this.sandbox = sandbox;
-      this.params = params;
-      this.buffer = new byte[1024];
+    public Worker() {
+      this(new Unjarer());
     }
 
-    public FileSet execute() {
-      MutableFileSet resultFiles = sandbox.resultFileSet();
-      try (JarInputStream jarInputStream = new JarInputStream(params.file().openInputStream());) {
-        JarEntry entry = null;
-        while ((entry = jarInputStream.getNextJarEntry()) != null) {
-          unjarEntry(jarInputStream, entry, resultFiles);
-        }
+    public Worker(Unjarer unjarer) {
+      this.unjarer = unjarer;
+    }
+
+    public FileSet execute(Sandbox sandbox, Parameters params) {
+      MutableFileSet result = sandbox.resultFileSet();
+      try {
+        unjarer.unjarFile(params.file(), result);
       } catch (IOException e) {
         throw new FileSystemException(e);
+      } catch (DuplicatePathInJarException e) {
+        sandbox.report(new DuplicatePathInJarError(e.path()));
+      } catch (IllegalPathInJarException e) {
+        sandbox.report(new IllegalPathInJarError(e.fileName()));
       }
-
-      return resultFiles;
-    }
-
-    private File unjarEntry(JarInputStream jarInputStream, JarEntry entry,
-        MutableFileSet resultFiles) throws IOException {
-      MutableFile file = resultFiles.createFile(path(entry.getName()));
-      try (OutputStream outputStream = file.openOutputStream()) {
-        int len = 0;
-        while ((len = jarInputStream.read(buffer)) > 0) {
-          outputStream.write(buffer, 0, len);
-        }
-      }
-      return file;
+      return result;
     }
   }
 }
---
+++ src/java/org/smoothbuild/builtin/java/Unjarer.java
@@ -0,0 +1,52 @@
+package org.smoothbuild.builtin.java;
+
+import static org.smoothbuild.plugin.api.Path.path;
+import static org.smoothbuild.plugin.api.Path.validationError;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.jar.JarEntry;
+import java.util.jar.JarInputStream;
+
+import org.smoothbuild.builtin.java.err.DuplicatePathInJarException;
+import org.smoothbuild.builtin.java.err.IllegalPathInJarException;
+import org.smoothbuild.plugin.api.File;
+import org.smoothbuild.plugin.api.MutableFile;
+import org.smoothbuild.plugin.api.MutableFileSet;
+import org.smoothbuild.plugin.api.Path;
+
+public class Unjarer {
+  // TODO extract constant
+  private final byte[] buffer = new byte[1024];
+
+  public void unjarFile(File jarFile, MutableFileSet resultFiles) throws IOException,
+      DuplicatePathInJarException, IllegalPathInJarException {
+    try (JarInputStream jarInputStream = new JarInputStream(jarFile.openInputStream());) {
+      JarEntry entry = null;
+      while ((entry = jarInputStream.getNextJarEntry()) != null) {
+        unjarEntry(jarInputStream, entry, resultFiles);
+      }
+    }
+  }
+
+  private File unjarEntry(JarInputStream jarInputStream, JarEntry entry, MutableFileSet resultFiles)
+      throws IOException, DuplicatePathInJarException, IllegalPathInJarException {
+    String fileName = entry.getName();
+    String errorMessage = validationError(fileName);
+    if (errorMessage != null) {
+      throw new IllegalPathInJarException(fileName);
+    }
+    Path path = path(fileName);
+    if (resultFiles.contains(path)) {
+      throw new DuplicatePathInJarException(path);
+    }
+    MutableFile file = resultFiles.createFile(path);
+    try (OutputStream outputStream = file.openOutputStream()) {
+      int len = 0;
+      while ((len = jarInputStream.read(buffer)) > 0) {
+        outputStream.write(buffer, 0, len);
+      }
+    }
+    return file;
+  }
+}
