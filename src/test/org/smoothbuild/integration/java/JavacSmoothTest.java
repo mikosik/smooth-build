d0236377d501c41c136b475e8286f0529ea997ba
mikosik <marcin.mikosik@gmail.com>
2013-09-20 13:00:59 +0200

    added smooth test for javac testing usage of libs parameter

--- src/test/org/smoothbuild/integration/java/JavacSmoothTest.java
+++ src/test/org/smoothbuild/integration/java/JavacSmoothTest.java
@@ -6,13 +6,16 @@ import static org.smoothbuild.plugin.api.Path.path;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.reflect.InvocationTargetException;
 
 import org.junit.Test;
 import org.smoothbuild.builtin.java.javac.err.JavaCompilerError;
 import org.smoothbuild.integration.IntegrationTestCase;
 import org.smoothbuild.plugin.api.Path;
+import org.smoothbuild.testing.parse.ScriptBuilder;
 
 public class JavacSmoothTest extends IntegrationTestCase {
+  MyClassLoader classLoader = new MyClassLoader();
 
   @Test
   public void errorIsReportedForCompilationErrors() throws Exception {
@@ -44,9 +47,62 @@ public class JavacSmoothTest extends IntegrationTestCase {
 
     messages.assertNoProblems();
 
-    Class<?> klass = loadClass(byteCode(path("MyClass.class")));
-    Object result = klass.getMethod("myMethod").invoke(null);
+    String classFile = "MyClass.class";
+    String method = "myMethod";
+    Object result = invoke(classFile, method);
     assertThat(result).isEqualTo(returnedString);
+  }
+
+  @Test
+  public void compileOneFileWithLibraryDependency() throws Exception {
+    {
+      StringBuilder builder = new StringBuilder();
+      builder.append("package library;");
+      builder.append("public class LibraryClass {");
+      builder.append("  public static int add(int a, int b) {");
+      builder.append("    return a + b;");
+      builder.append("  }");
+      builder.append("}");
+
+      Path path = path("library/LibraryClass.java");
+      fileSystem.createFileWithContent(path, builder.toString());
+    }
+    {
+      StringBuilder builder = new StringBuilder();
+      builder.append("import library.LibraryClass;");
+      builder.append("public class MyClass2 {");
+      builder.append("  public static String myMethod() {");
+      builder.append("    return Integer.toString(LibraryClass.add(2, 3));");
+      builder.append("  }");
+      builder.append("}");
+
+      Path path = path("MyClass2.java");
+      fileSystem.createFileWithContent(path, builder.toString());
+    }
+    ScriptBuilder builder = new ScriptBuilder();
+    builder.addLine("libraryClasses : [ file(path='library/LibraryClass.java') ] | javac ;");
+    builder.addLine("libraryJar: libraryClasses | jar ;");
+    builder.addLine("saveLibraryClass : libraryClasses | save('.');");
+    builder
+        .addLine("run : [ file(path='MyClass2.java') ] | javac(libs=[libraryJar]) | save(dir='.');");
+    this.script(builder.build());
+
+    smoothRunner.run("saveLibraryClass");
+    smoothRunner.run("run");
+
+    messages.assertNoProblems();
+
+    loadClass(byteCode(path("library/LibraryClass.class")));
+    String classFile = "MyClass2.class";
+    String method = "myMethod";
+    Object result = invoke(classFile, method);
+    assertThat(result).isEqualTo("5");
+  }
+
+  private Object invoke(String classFile, String method) throws IOException,
+      IllegalAccessException, InvocationTargetException, NoSuchMethodException {
+    Class<?> klass = loadClass(byteCode(path(classFile)));
+    return klass.getMethod(method).invoke(null);
   }
 
   private byte[] byteCode(Path classFilePath) throws IOException {
@@ -56,8 +112,8 @@ public class JavacSmoothTest extends IntegrationTestCase {
     return result;
   }
 
-  private static Class<?> loadClass(byte[] bytes) {
-    return new MyClassLoader().defineClass(null, bytes);
+  private Class<?> loadClass(byte[] bytes) {
+    return classLoader.defineClass(null, bytes);
   }
 
   private static class MyClassLoader extends ClassLoader {
