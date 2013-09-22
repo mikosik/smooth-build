5855de3a6deb50475053245777acd84025fb8ee6
mikosik <marcin.mikosik@gmail.com>
2013-09-22 21:47:48 +0200

    fixed message in FileSystemError

--- src/java/org/smoothbuild/task/err/FileSystemError.java
+++ src/java/org/smoothbuild/task/err/FileSystemError.java
@@ -15,13 +15,13 @@ public class FileSystemError extends Error {
   private static String createMessage(Name name, Throwable e) {
     StringBuilder b = new StringBuilder();
 
-    b.append("Invoking function " + name + " caused internal exception:");
-    b.append("It was probably caused by:");
+    b.append("Invoking function " + name + " caused internal exception:\n");
+    b.append("It was probably caused by:\n");
     b.append(" - some other process (maybe other instance of Smooth) changing content of "
-        + BUILD_DIR + " dir");
-    b.append(" - bug in " + name + " function (plugin) implementation");
-    b.append(" - bug in Smooth tool");
-    b.append("Full java stacktrace below:");
+        + BUILD_DIR + " dir\n");
+    b.append(" - bug in " + name + " function (plugin) implementation\n");
+    b.append(" - bug in Smooth tool\n");
+    b.append("Full java stacktrace below:\n");
     b.append(Throwables.getStackTraceAsString(e));
 
     return b.toString();
