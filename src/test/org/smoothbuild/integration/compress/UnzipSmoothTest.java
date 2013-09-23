eb5762db490575482394374cf3b7f3554d5e9806
mikosik <marcin.mikosik@gmail.com>
2013-09-23 11:32:47 +0200

    refactored UnzipFunctionTest -> UnzipSmoothTest

---
+++ src/test/org/smoothbuild/integration/compress/UnzipSmoothTest.java
@@ -0,0 +1,36 @@
+package org.smoothbuild.integration.compress;
+
+import static org.smoothbuild.plugin.api.Path.path;
+
+import org.junit.Test;
+import org.smoothbuild.integration.IntegrationTestCase;
+import org.smoothbuild.plugin.api.Path;
+import org.smoothbuild.testing.common.ZipTester;
+import org.smoothbuild.testing.plugin.internal.FileTester;
+import org.smoothbuild.testing.plugin.internal.TestFile;
+import org.smoothbuild.testing.plugin.internal.TestFileSet;
+
+public class UnzipSmoothTest extends IntegrationTestCase {
+
+  @Test
+  public void testUnzipping() throws Exception {
+    // given
+    Path fileA = path("a/fileA.txt");
+    Path fileB = path("b/fileB.txt");
+    TestFile zipFile = fileSystem.createEmptyFile(path("input.zip"));
+    ZipTester.zipFiles(zipFile, fileA.value(), fileB.value());
+
+    script("run : file(" + zipFile.path() + ") | unzip | save('out');");
+
+    // when
+    smoothRunner.run("run");
+
+    // then
+    messages.assertNoProblems();
+
+    TestFileSet outFiles = new TestFileSet(fileSystem.subFileSystem(path("out")));
+    outFiles.contains(fileA);
+    outFiles.contains(fileB);
+    FileTester.assertContentContainsFilePath(outFiles.file(fileA));
+  }
+}
