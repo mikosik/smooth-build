369be2f51c4f1538695be60dc7cc2a28d1fb4e29
mikosik <marcin.mikosik@gmail.com>
2013-09-24 15:21:51 +0200

    refactored LibraryClasses

--- src/java/org/smoothbuild/builtin/java/javac/err/DuplicateClassFileError.java
+++ src/java/org/smoothbuild/builtin/java/javac/err/DuplicateClassFileError.java
@@ -4,8 +4,8 @@ import org.smoothbuild.message.Error;
 import org.smoothbuild.plugin.api.Path;
 
 public class DuplicateClassFileError extends Error {
-  public DuplicateClassFileError(Path path, String jarName1, String jarName2) {
-    super("File " + path + " is contained by both library jar files: '" + jarName1 + "' and '"
-        + jarName2 + "'.");
+  public DuplicateClassFileError(Path path, Path jar1, Path jar2) {
+    super("File " + path + " is contained by both library jar files: " + jar1 + " and " + jar2
+        + ".");
   }
 }
