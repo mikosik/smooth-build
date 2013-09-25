06262df715a5e4c64839714ac15a8ea9bd41a56b
mikosik <marcin.mikosik@gmail.com>
2013-09-25 20:02:02 +0200

    refactored Unzipper/UnzipFunction

--- src/java/org/smoothbuild/builtin/compress/Unzipper.java
+++ src/java/org/smoothbuild/builtin/compress/Unzipper.java
@@ -14,21 +14,25 @@ import org.smoothbuild.plugin.api.File;
 import org.smoothbuild.plugin.api.MutableFile;
 import org.smoothbuild.plugin.api.MutableFileSet;
 import org.smoothbuild.plugin.api.Path;
+import org.smoothbuild.task.err.FileSystemError;
 
 public class Unzipper {
   private final byte[] buffer = new byte[Constants.BUFFER_SIZE];
 
-  public void unzipFile(File zipFile, MutableFileSet resultFiles) throws IOException {
-    try (ZipInputStream zipInputStream = new ZipInputStream(zipFile.openInputStream());) {
-      ZipEntry entry = null;
-      while ((entry = zipInputStream.getNextEntry()) != null) {
-        unzipEntry(zipInputStream, entry, resultFiles);
+  public void unzipFile(File zipFile, MutableFileSet resultFiles) {
+    try {
+      try (ZipInputStream zipInputStream = new ZipInputStream(zipFile.openInputStream());) {
+        ZipEntry entry = null;
+        while ((entry = zipInputStream.getNextEntry()) != null) {
+          unzipEntry(zipInputStream, entry, resultFiles);
+        }
       }
+    } catch (IOException e) {
+      throw new FileSystemError(e);
     }
   }
 
-  private File unzipEntry(ZipInputStream zipInputStream, ZipEntry entry, MutableFileSet resultFiles)
-      throws IOException {
+  private File unzipEntry(ZipInputStream zipInputStream, ZipEntry entry, MutableFileSet resultFiles) {
     String fileName = entry.getName();
     String errorMessage = validationError(fileName);
     if (errorMessage != null) {
@@ -39,11 +43,15 @@ public class Unzipper {
       throw new DuplicatePathInZipError(path);
     }
     MutableFile file = resultFiles.createFile(path);
-    try (OutputStream outputStream = file.openOutputStream()) {
-      int len = 0;
-      while ((len = zipInputStream.read(buffer)) > 0) {
-        outputStream.write(buffer, 0, len);
+    try {
+      try (OutputStream outputStream = file.openOutputStream()) {
+        int len = 0;
+        while ((len = zipInputStream.read(buffer)) > 0) {
+          outputStream.write(buffer, 0, len);
+        }
       }
+    } catch (IOException e) {
+      throw new FileSystemError(e);
     }
     return file;
   }
