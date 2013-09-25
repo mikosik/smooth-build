232e768b5f57eff81823f4ec8d10ae562645461a
mikosik <marcin.mikosik@gmail.com>
2013-09-25 12:39:21 +0200

    refactored CommandLineParser to throw Message instead of adding it to MessageListener

--- src/test/org/smoothbuild/command/CommandLineParserTest.java
+++ src/test/org/smoothbuild/command/CommandLineParserTest.java
@@ -7,39 +7,31 @@ import static org.smoothbuild.function.base.Name.simpleName;
 import org.junit.Test;
 import org.smoothbuild.command.err.CommandLineError;
 import org.smoothbuild.command.err.NothingToDoError;
-import org.smoothbuild.testing.message.TestMessageListener;
 
 public class CommandLineParserTest {
   String functionName = "function1";
   CommandLineParser parser = new CommandLineParser();
-  TestMessageListener messages = new TestMessageListener();
 
   @Test
   public void functionToRun() {
-    CommandLineArguments args = parser.parse(messages, functionName);
-
+    CommandLineArguments args = parser.parse(functionName);
     assertThat(args.functionToRun()).isEqualTo(simpleName(functionName));
-    messages.assertNoProblems();
   }
 
-  @Test
+  @Test(expected = NothingToDoError.class)
   public void functionToRunMustBePresent() throws Exception {
-    parser.parse(messages);
-    messages.assertOnlyProblem(NothingToDoError.class);
+    parser.parse();
   }
 
-  @Test
+  @Test(expected = CommandLineError.class)
   public void atMostOneFunctionToRunCanBePresent() throws Exception {
-    parser.parse(messages, functionName, functionName);
-    messages.assertOnlyProblem(CommandLineError.class);
+    parser.parse(functionName, functionName);
   }
 
   @Test
   public void scriptFile() throws Exception {
-    CommandLineArguments args = parser.parse(messages, functionName);
-
+    CommandLineArguments args = parser.parse(functionName);
     assertThat(args.scriptFile()).isEqualTo(DEFAULT_SCRIPT);
-    messages.assertNoProblems();
   }
 
 }
