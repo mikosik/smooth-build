d2c4d77c07959b0aa9b791dee5788390a9a26b4c
mikosik <marcin.mikosik@gmail.com>
2013-09-26 11:08:46 +0200

    refactored out JavaNaming from InputClassFile

---
+++ src/java/org/smoothbuild/builtin/java/util/JavaNaming.java
@@ -0,0 +1,25 @@
+package org.smoothbuild.builtin.java.util;
+
+import javax.tools.JavaFileObject.Kind;
+
+import org.smoothbuild.plugin.api.Path;
+
+public class JavaNaming {
+  private static final String CLASS_FILE_EXTENSION = Kind.CLASS.extension;
+
+  public static String toBinaryName(Path path) {
+    String pathString = path.value();
+    int endIndex = pathString.length() - CLASS_FILE_EXTENSION.length();
+    String withoutExtension = pathString.substring(0, endIndex);
+    return withoutExtension.replace('/', '.');
+  }
+
+  public static String binaryNameToPackage(String binaryName) {
+    int lastIndex = binaryName.lastIndexOf('.');
+    if (lastIndex == -1) {
+      return "";
+    } else {
+      return binaryName.substring(0, lastIndex);
+    }
+  }
+}
