13f8db622764a7cd85a4f249a41610e728c4921f
mikosik <marcin.mikosik@gmail.com>
2013-09-26 15:31:21 +0200

    added FileClassLoader

---
+++ src/java/org/smoothbuild/builtin/java/junit/FileClassLoader.java
@@ -0,0 +1,34 @@
+package org.smoothbuild.builtin.java.junit;
+
+import java.io.IOException;
+import java.util.Map;
+
+import org.smoothbuild.plugin.api.File;
+
+import com.google.common.io.ByteStreams;
+
+public class FileClassLoader extends ClassLoader {
+  private final Map<String, File> binaryNameToFile;
+
+  public FileClassLoader(Map<String, File> binaryNameToFile) {
+    this.binaryNameToFile = binaryNameToFile;
+  }
+
+  @Override
+  public Class<?> findClass(String name) throws ClassNotFoundException {
+    File file = binaryNameToFile.get(name);
+    if (file == null) {
+      throw new ClassNotFoundException(name);
+    }
+    byte[] byteArray = fileToByteArray(file);
+    return defineClass(name, byteArray, 0, byteArray.length);
+  }
+
+  private byte[] fileToByteArray(File file) {
+    try {
+      return ByteStreams.toByteArray(file.openInputStream());
+    } catch (IOException e) {
+      throw new RuntimeException("Error reading from " + file.path() + ".", e);
+    }
+  }
+}
---
+++ src/test/org/smoothbuild/builtin/java/junit/FileClassLoaderTest.java
@@ -0,0 +1,51 @@
+package org.smoothbuild.builtin.java.junit;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.lang.reflect.Method;
+import java.util.Map;
+
+import org.junit.Test;
+import org.smoothbuild.plugin.api.File;
+import org.smoothbuild.plugin.api.Path;
+import org.smoothbuild.testing.plugin.internal.TestFile;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.io.ByteStreams;
+
+public class FileClassLoaderTest {
+
+  @Test
+  public void testClassLoading() throws Exception {
+    // Do not use MyClass.class literal to avoid loading MyClass by current
+    // ClassLoader.
+    String klassBinaryName = FileClassLoaderTest.class.getName() + "$MyClass";
+    String filePath = klassBinaryName.replace('.', '/') + ".class";
+
+    TestFile file = new TestFile(Path.path("this/path/is/ignored/anyway"));
+    InputStream classByteCode = this.getClass().getClassLoader().getResourceAsStream(filePath);
+
+    OutputStream outputStream = file.openOutputStream();
+    ByteStreams.copy(classByteCode, outputStream);
+    outputStream.close();
+
+    Map<String, File> binaryNameToFile = ImmutableMap.<String, File> of(klassBinaryName, file);
+    FileClassLoader fileClassLoader = new FileClassLoader(binaryNameToFile);
+
+    Class<?> klass = fileClassLoader.findClass(klassBinaryName);
+    assertThat(klass.getClassLoader()).isSameAs(fileClassLoader);
+
+    Method method = klass.getMethod("myMethod");
+    Object result = method.invoke(null);
+
+    assertThat(result).isEqualTo("myResult");
+  }
+
+  public static class MyClass {
+    public static String myMethod() {
+      return "myResult";
+    }
+  }
+}
