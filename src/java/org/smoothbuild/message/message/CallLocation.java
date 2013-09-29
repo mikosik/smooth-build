782d930b4694fa2b50896456a2fe4681d3fcf9b6
mikosik <marcin.mikosik@gmail.com>
2013-09-29 14:04:32 +0200

    added null checks to CallLocation

--- src/java/org/smoothbuild/message/message/CallLocation.java
+++ src/java/org/smoothbuild/message/message/CallLocation.java
@@ -1,14 +1,20 @@
 package org.smoothbuild.message.message;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+
 import org.smoothbuild.function.base.Name;
 
 public class CallLocation {
   private final Name name;
   private final CodeLocation codeLocation;
 
-  public CallLocation(Name name, CodeLocation codeLocation) {
-    this.name = name;
-    this.codeLocation = codeLocation;
+  public static CallLocation callLocation(Name name, CodeLocation codeLocation) {
+    return new CallLocation(name, codeLocation);
+  }
+
+  private CallLocation(Name name, CodeLocation codeLocation) {
+    this.name = checkNotNull(name);
+    this.codeLocation = checkNotNull(codeLocation);
   }
 
   public Name name() {
