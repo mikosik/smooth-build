44badc25baaf1434a904da433779087452945c2e
mikosik <marcin.mikosik@gmail.com>
2013-09-29 14:04:32 +0200

    added null checks to CallLocation

---
+++ src/test/org/smoothbuild/message/message/CallLocationTest.java
@@ -0,0 +1,40 @@
+package org.smoothbuild.message.message;
+
+import static org.testory.Testory.givenTest;
+import static org.testory.Testory.thenThrown;
+import static org.testory.Testory.when;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.smoothbuild.function.base.Name;
+import org.testory.common.Closure;
+
+public class CallLocationTest {
+  Name name;
+  CodeLocation codeLocation;
+
+  @Before
+  public void before() {
+    givenTest(this);
+  }
+
+  @Test
+  public void nullNameIsForbidden() {
+    when(callLocation(null, codeLocation));
+    thenThrown(NullPointerException.class);
+  }
+
+  @Test
+  public void nullCallLocationIsForbidden() {
+    when(callLocation(name, null));
+    thenThrown(NullPointerException.class);
+  }
+
+  private static Closure callLocation(final Name name, final CodeLocation codeLocation) {
+    return new Closure() {
+      public CallLocation invoke() {
+        return CallLocation.callLocation(name, codeLocation);
+      }
+    };
+  }
+}
