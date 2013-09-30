219ef1fc50459589c72d6f25735e55017b9a5fcb
mikosik <marcin.mikosik@gmail.com>
2013-09-30 22:46:58 +0200

    got rid of InfoCodeMessage

--- src/java/org/smoothbuild/message/message/InfoCodeMessage.java
+++
@@ -1,9 +0,0 @@
-package org.smoothbuild.message.message;
-
-import static org.smoothbuild.message.listen.MessageType.INFO;
-
-public class InfoCodeMessage extends CodeMessage {
-  public InfoCodeMessage(CodeLocation codeLocation, String message) {
-    super(INFO, codeLocation, message);
-  }
-}
