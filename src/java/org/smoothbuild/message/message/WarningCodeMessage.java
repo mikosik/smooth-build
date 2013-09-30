86fdc313da0945c6b8121edb75a4c56f92e0a354
mikosik <marcin.mikosik@gmail.com>
2013-09-30 22:48:39 +0200

    got rid of WarningCodeMessage

--- src/java/org/smoothbuild/message/message/WarningCodeMessage.java
+++
@@ -1,9 +0,0 @@
-package org.smoothbuild.message.message;
-
-import static org.smoothbuild.message.listen.MessageType.WARNING;
-
-public class WarningCodeMessage extends CodeMessage {
-  public WarningCodeMessage(CodeLocation codeLocation, String message) {
-    super(WARNING, codeLocation, message);
-  }
-}
