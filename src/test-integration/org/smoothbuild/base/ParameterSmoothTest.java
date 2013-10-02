c76bc671edd8c42425ae5115bccd5bda73911bbd
mikosik <marcin.mikosik@gmail.com>
2013-10-02 21:46:52 +0200

    changed grammar to allow trailing comma in parameter list

---
+++ src/test-integration/org/smoothbuild/base/ParameterSmoothTest.java
@@ -0,0 +1,20 @@
+package org.smoothbuild.base;
+
+import java.io.IOException;
+
+import org.junit.Test;
+import org.smoothbuild.integration.IntegrationTestCase;
+
+public class ParameterSmoothTest extends IntegrationTestCase {
+  @Test
+  public void trailingCommaIsAllowedInParameterList() throws IOException {
+    // given
+    script("run : newFile(path='file.txt', content='abc',) | save('outptuDir',);");
+
+    // when
+    smoothRunner.run("run");
+
+    // then
+    messages.assertNoProblems();
+  }
+}
