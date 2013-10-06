2feee46d3ce8b3ba6a2e85c8ca5b2d6bcedec1b6
mikosik <marcin.mikosik@gmail.com>
2013-10-06 19:17:08 +0200

    added NameMatcherMediumTest, MatchingNamesGenerator

---
+++ src/test/org/smoothbuild/fs/base/match/HelpTester.java
@@ -0,0 +1,11 @@
+package org.smoothbuild.fs.base.match;
+
+import com.google.common.collect.ImmutableList;
+
+public class HelpTester {
+
+  public static final ImmutableList<String> ALL = ImmutableList.of("a", "b", "c", "aa", "ab", "ac",
+      "ba", "bb", "bc", "ca", "cb", "cc");
+  public static final ImmutableList<String> ALL_WITH_EMPTY = ImmutableList.of("", "a", "b", "c",
+      "aa", "ab", "ac", "ba", "bb", "bc", "ca", "cb", "cc");
+}
