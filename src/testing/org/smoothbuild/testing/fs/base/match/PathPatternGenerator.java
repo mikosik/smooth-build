87ad61bad2a9d86f9075ca041f0cc706165e7543
mikosik <marcin.mikosik@gmail.com>
2013-10-07 10:48:00 +0200

    moved PathPatternGenerator to testing tree

--- src/testing/org/smoothbuild/testing/fs/base/match/PathPatternGenerator.java
+++ src/testing/org/smoothbuild/testing/fs/base/match/PathPatternGenerator.java
@@ -1,4 +1,4 @@
-package org.smoothbuild.fs.base.match;
+package org.smoothbuild.testing.fs.base.match;
 
 import static org.smoothbuild.fs.base.match.Constants.SINGLE_STAR;
 import static org.smoothbuild.testing.fs.base.match.HelpTester.endsWithThreeLetters;
