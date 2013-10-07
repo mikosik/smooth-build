cab7412a231324bc6426d32078bf873d3bb12802
mikosik <marcin.mikosik@gmail.com>
2013-10-07 12:17:06 +0200

    added MatchingPathsGenerator

--- src/testing/org/smoothbuild/testing/fs/base/match/HelpTester.java
+++ src/testing/org/smoothbuild/testing/fs/base/match/HelpTester.java
@@ -10,9 +10,21 @@ public class HelpTester {
       "ba", "bb", "bc", "ca", "cb", "cc");
   public static final ImmutableList<String> ALL_WITH_EMPTY = ImmutableList.of("", "a", "b", "c",
       "aa", "ab", "ac", "ba", "bb", "bc", "ca", "cb", "cc");
+  public static final ImmutableList<String> ALL_DOUBLE_STARS = allDoubleStars();
 
   public static boolean endsWithThreeLetters(String pattern) {
     boolean isLongEnough = 3 <= pattern.length();
     return isLongEnough && JAVA_LETTER.matchesAllOf(pattern.substring(pattern.length() - 3));
+  }
+
+  private static ImmutableList<String> allDoubleStars() {
+    ImmutableList.Builder<String> builder = ImmutableList.builder();
+    builder.add("");
+    for (String a : HelpTester.ALL) {
+      for (String b : HelpTester.ALL) {
+        builder.add(a + "/" + b);
+      }
+    }
+    return builder.build();
   }
 }
