e27e00e30424e715a2e256a78754eea1a29e9394
mikosik <marcin.mikosik@gmail.com>
2013-10-07 10:40:03 +0200

    fixed naming in NamePatternMediumTest

--- src/test/org/smoothbuild/fs/base/match/NamePatternMediumTest.java
+++ src/test/org/smoothbuild/fs/base/match/NamePatternMediumTest.java
@@ -9,12 +9,12 @@ import com.google.common.base.Function;
 public class NamePatternMediumTest {
   @Test
   public void all_generated_patterns_are_accepted() throws Exception {
-    Function<String, Void> function = new Function<String, Void>() {
+    Function<String, Void> consumer = new Function<String, Void>() {
       public Void apply(String pattern) {
         NamePattern.namePattern(pattern);
         return null;
       }
     };
-    generatePatterns(5, function);
+    generatePatterns(5, consumer);
   }
 }
