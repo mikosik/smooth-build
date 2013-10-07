4bdafd54b3f5391da84a731d6274c6214af2f669
mikosik <marcin.mikosik@gmail.com>
2013-10-07 10:46:25 +0200

    refactored out PathPatternMediumTest from PathPatternTest

---
+++ src/test/org/smoothbuild/fs/base/match/PathPatternMediumTest.java
@@ -0,0 +1,22 @@
+package org.smoothbuild.fs.base.match;
+
+import static org.smoothbuild.fs.base.match.PathPattern.pathPattern;
+import static org.smoothbuild.fs.base.match.PathPatternGenerator.generatePatterns;
+
+import org.junit.Test;
+
+import com.google.common.base.Function;
+
+public class PathPatternMediumTest {
+  @Test
+  public void allGeneratedPatternsAreValid() throws Exception {
+    Function<String, Void> consumer = new Function<String, Void>() {
+      public Void apply(String pattern) {
+        pathPattern(pattern);
+        return null;
+      }
+    };
+
+    generatePatterns(6, consumer);
+  }
+}
--- src/test/org/smoothbuild/fs/base/match/PathPatternTest.java
+++ src/test/org/smoothbuild/fs/base/match/PathPatternTest.java
@@ -1,28 +1,13 @@
 package org.smoothbuild.fs.base.match;
 
 import static org.junit.Assert.fail;
-import static org.smoothbuild.fs.base.match.PathPattern.pathPattern;
-import static org.smoothbuild.fs.base.match.PathPatternGenerator.generatePatterns;
 
 import org.junit.Test;
 
-import com.google.common.base.Function;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableList.Builder;
 
 public class PathPatternTest {
-
-  @Test
-  public void allGeneratedPatternsAreValid() throws Exception {
-    Function<String, Void> consumer = new Function<String, Void>() {
-      public Void apply(String pattern) {
-        pathPattern(pattern);
-        return null;
-      }
-    };
-
-    generatePatterns(6, consumer);
-  }
 
   @Test
   public void validationErrorReturnsMessageForInvalidPattern() {
