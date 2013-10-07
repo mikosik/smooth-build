c13e957a53a37c369e226e23f174a7cefc3bafaf
mikosik <marcin.mikosik@gmail.com>
2013-10-07 18:52:43 +0200

    added PathMatcher

---
+++ src/test/org/smoothbuild/fs/base/match/PathMatcherTestLarge.java
@@ -0,0 +1,47 @@
+package org.smoothbuild.fs.base.match;
+
+import static org.smoothbuild.fs.base.Path.path;
+import static org.smoothbuild.fs.base.match.PathMatcher.pathMatcher;
+import static org.smoothbuild.testing.fs.base.match.MatchingPathsGenerator.generatePaths;
+import static org.smoothbuild.testing.fs.base.match.PathPatternGenerator.generatePatterns;
+
+import org.junit.Test;
+import org.smoothbuild.fs.base.Path;
+
+import com.google.common.base.Function;
+import com.google.common.base.Predicate;
+
+/**
+ * This test is not automatically run by ant as it takes more than 1 hour to
+ * complete.
+ */
+public class PathMatcherTestLarge {
+
+  @Test
+  public void testGeneratedPatterns() throws Exception {
+    generatePatterns(5, doTestPatternConsumer());
+  }
+
+  private static Function<String, Void> doTestPatternConsumer() {
+    return new Function<String, Void>() {
+      private int count = 0;
+
+      public Void apply(String pattern) {
+        count++;
+        System.out.println(count + ": " + pattern);
+        generatePaths(pattern, assertThatPathMatchesPatternConsumer(pattern));
+        return null;
+      }
+    };
+  }
+
+  private static Function<String, Void> assertThatPathMatchesPatternConsumer(String pattern) {
+    final Predicate<Path> matcher = pathMatcher(pattern);
+    return new Function<String, Void>() {
+      public Void apply(String path) {
+        matcher.apply(path(path));
+        return null;
+      }
+    };
+  }
+}
