d3957c3a076969e20383a3c73d51ca2eae6fa1cd
mikosik <marcin.mikosik@gmail.com>
2013-10-08 14:31:14 +0200

    added COMMENT token to smooth grammar

---
+++ src/test-integration/org/smoothbuild/base/CommentSmoothTest.java
@@ -0,0 +1,36 @@
+package org.smoothbuild.base;
+
+import java.io.IOException;
+
+import org.junit.Test;
+import org.smoothbuild.integration.IntegrationTestCase;
+import org.smoothbuild.testing.parse.ScriptBuilder;
+
+public class CommentSmoothTest extends IntegrationTestCase {
+  @Test
+  public void comments_are_ignored() throws IOException {
+    // given
+    ScriptBuilder builder = new ScriptBuilder();
+    builder.addLine("# full line comment");
+    builder.addLine("run: newFile(path='file.txt', 'content'); # comment at the end of line");
+    script(builder.build());
+
+    // when
+    smoothRunner.run("run");
+
+    // then
+    messages.assertNoProblems();
+  }
+
+  @Test
+  public void comment_char_is_allowed_in_strings() throws IOException {
+    // given
+    script("run: newFile(path='file.txt', '###');");
+
+    // when
+    smoothRunner.run("run");
+
+    // then
+    messages.assertNoProblems();
+  }
+}
