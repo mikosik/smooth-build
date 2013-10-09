443f5330f4a8067edb73eee080de3f0423c88b23
mikosik <marcin.mikosik@gmail.com>
2013-10-09 16:08:01 +0200

    added Param.hash()

--- src/test/org/smoothbuild/function/base/ParamTest.java
+++ src/test/org/smoothbuild/function/base/ParamTest.java
@@ -6,7 +6,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.fail;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
-import static org.smoothbuild.function.base.Param.param;
 import static org.smoothbuild.function.base.Param.paramsToString;
 import static org.smoothbuild.function.base.Type.FILE_SET;
 import static org.smoothbuild.function.base.Type.STRING;
@@ -19,8 +18,9 @@ import nl.jqno.equalsverifier.EqualsVerifier;
 import org.junit.Test;
 import org.smoothbuild.function.def.DefinitionNode;
 
-public class ParamTest {
+import com.google.common.hash.HashCode;
 
+public class ParamTest {
   @Test
   public void creatingParamsMap() throws Exception {
     String name1 = "name1";
@@ -35,7 +35,7 @@ public class ParamTest {
 
   @Test
   public void creatingParamsMapWithDuplicateParamNamesThrowsExcpetion() throws Exception {
-    Param param1 = param(STRING, "name", false);
+    Param param1 = param(STRING, "name");
 
     try {
       Param.params(param1, param1);
@@ -47,22 +47,27 @@ public class ParamTest {
 
   @Test(expected = NullPointerException.class)
   public void nullTypeIsForbidden() throws Exception {
-    param(null, "name", true);
+    Param.param(null, "name", true, mock(HashCode.class));
   }
 
   @Test(expected = NullPointerException.class)
   public void nullNameIsForbidden() throws Exception {
-    param(Type.STRING, null, true);
+    Param.param(Type.STRING, null, true, mock(HashCode.class));
+  }
+
+  @Test(expected = NullPointerException.class)
+  public void nullHashCodeIsForbidden() throws Exception {
+    Param.param(Type.STRING, "name", true, null);
   }
 
   @Test(expected = IllegalArgumentException.class)
   public void creatingVoidParamIsForbidden() throws Exception {
-    param(Type.VOID, "name", true);
+    Param.param(Type.VOID, "name", true, mock(HashCode.class));
   }
 
   @Test(expected = IllegalArgumentException.class)
   public void creatingEmptySetParamIsForbidden() throws Exception {
-    param(Type.EMPTY_SET, "name", true);
+    Param.param(Type.EMPTY_SET, "name", true, mock(HashCode.class));
   }
 
   @Test
@@ -81,8 +86,17 @@ public class ParamTest {
   }
 
   @Test
+  public void hash() throws Exception {
+    HashCode hashCode = HashCode.fromInt(33);
+    Param param = Param.param(Type.STRING, "name", true, hashCode);
+    assertThat(param.hash()).isSameAs(hashCode);
+  }
+
+  @Test
   public void equalsAndHashCode() throws Exception {
-    EqualsVerifier.forClass(Param.class).suppress(NULL_FIELDS).verify();
+    EqualsVerifier.forClass(Param.class)
+        .withPrefabValues(HashCode.class, HashCode.fromInt(1), HashCode.fromInt(2))
+        .suppress(NULL_FIELDS).verify();
   }
 
   @Test
@@ -126,5 +140,14 @@ public class ParamTest {
     builder.append("  File* : param3               \n");
     builder.append("  String: param2-with-very-long\n");
     assertThat(actual).isEqualTo(builder.toString());
+  }
+
+  private static Param param(Type type, String name) {
+    return param(type, name, false);
+  }
+
+  private static Param param(Type type, String name, boolean isRequired) {
+    HashCode hashCode = mock(HashCode.class);
+    return Param.param(type, name, isRequired, hashCode);
   }
 }
--- src/testing/org/smoothbuild/testing/function/base/ParamTester.java
+++ src/testing/org/smoothbuild/testing/function/base/ParamTester.java
@@ -3,8 +3,14 @@ package org.smoothbuild.testing.function.base;
 import org.smoothbuild.function.base.Param;
 import org.smoothbuild.function.base.Type;
 
+import com.google.common.hash.HashCode;
+
 public class ParamTester {
   public static Param param(Type type, String name) {
-    return Param.param(type, name, false);
+    return param(type, name, false);
+  }
+
+  public static Param param(Type type, String name, boolean isRequired) {
+    return Param.param(type, name, isRequired, HashCode.fromInt(0));
   }
 }
