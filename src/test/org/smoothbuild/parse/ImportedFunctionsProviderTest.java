9bae516ceb6ba4581aacc4fb66f781a2f58552b1
mikosik <marcin.mikosik@gmail.com>
2013-10-11 22:43:14 +0200

    got rid of HashModule

--- src/test/org/smoothbuild/parse/ImportedFunctionsProviderTest.java
+++ src/test/org/smoothbuild/parse/ImportedFunctionsProviderTest.java
@@ -3,11 +3,10 @@ package org.smoothbuild.parse;
 import static com.google.inject.Guice.createInjector;
 
 import org.junit.Test;
-import org.smoothbuild.HashModule;
 
 public class ImportedFunctionsProviderTest {
   @Test
   public void creatingImportedFunctionsThrowsNoException() {
-    createInjector(new HashModule()).getInstance(ImportedFunctionsProvider.class).get();
+    createInjector().getInstance(ImportedFunctionsProvider.class).get();
   }
 }
