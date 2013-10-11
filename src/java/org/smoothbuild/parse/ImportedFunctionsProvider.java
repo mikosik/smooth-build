a865054000cf8c95e5dedf2a17b4ab665395830f
mikosik <marcin.mikosik@gmail.com>
2013-10-11 22:41:47 +0200

    refactored NativeFunctionFactory to use Hash.hashFunction(), changed create methods to statis

--- src/java/org/smoothbuild/parse/ImportedFunctionsProvider.java
+++ src/java/org/smoothbuild/parse/ImportedFunctionsProvider.java
@@ -1,7 +1,5 @@
 package org.smoothbuild.parse;
 
-import javax.inject.Inject;
-
 import org.smoothbuild.builtin.compress.UnzipFunction;
 import org.smoothbuild.builtin.compress.ZipFunction;
 import org.smoothbuild.builtin.file.FileFunction;
@@ -21,12 +19,6 @@ import org.smoothbuild.function.nativ.exc.NativeImplementationException;
 import com.google.inject.Provider;
 
 public class ImportedFunctionsProvider implements Provider<ImportedFunctions> {
-  private final NativeFunctionFactory nativeFunctionFactory;
-
-  @Inject
-  public ImportedFunctionsProvider(NativeFunctionFactory nativeFunctionFactory) {
-    this.nativeFunctionFactory = nativeFunctionFactory;
-  }
 
   @Override
   public ImportedFunctions get() {
@@ -55,7 +47,7 @@ public class ImportedFunctionsProvider implements Provider<ImportedFunctions> {
 
   private Function createFunction(Class<?> klass) {
     try {
-      return nativeFunctionFactory.create(klass, true);
+      return NativeFunctionFactory.create(klass, true);
     } catch (NativeImplementationException e) {
       throw new RuntimeException("Builtin function " + klass.getCanonicalName()
           + " has implementation problem.", e);
