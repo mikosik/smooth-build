dd56357a6b46a8b96660d6286e96ef20e013cea1
mikosik <marcin.mikosik@gmail.com>
2013-10-12 21:05:01 +0200

    added tests for StringSetTask hashes

--- src/test/org/smoothbuild/task/StringSetTaskTest.java
+++ src/test/org/smoothbuild/task/StringSetTaskTest.java
@@ -3,6 +3,7 @@ package org.smoothbuild.task;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.smoothbuild.message.message.CodeLocation.codeLocation;
 import static org.smoothbuild.testing.task.HashedTasksTester.hashedTasks;
+import static org.smoothbuild.testing.task.TaskTester.hashes;
 
 import java.util.List;
 
@@ -11,16 +12,17 @@ import org.smoothbuild.message.message.CodeLocation;
 import org.smoothbuild.testing.task.TestTask;
 import org.smoothbuild.type.api.StringSet;
 
-import com.google.common.collect.ImmutableList;
 import com.google.common.hash.HashCode;
 
 public class StringSetTaskTest {
   String string1 = "string1";
   String string2 = "string2";
+
   Task task1 = new TestTask(string1);
   Task task2 = new TestTask(string2);
+
   CodeLocation codeLocation = codeLocation(1, 2, 4);
-  private final List<HashCode> hashes = ImmutableList.of(task1.hash(), task2.hash());
+  List<HashCode> hashes = hashes(task1, task2);
 
   StringSetTask stringSetTask = new StringSetTask(hashes, codeLocation);
 
@@ -33,5 +35,31 @@ public class StringSetTaskTest {
   public void execute() {
     stringSetTask.execute(null, hashedTasks(task1, task2));
     assertThat((StringSet) stringSetTask.result()).containsOnly(string1, string2);
+  }
+
+  @Test
+  public void hashOfEmptyStringSetIsDifferentFromHashOfStringSetWithOneElement() throws Exception {
+    StringSetTask stringSetTask1 = new StringSetTask(hashes(task1), codeLocation);
+    StringSetTask stringSetTask2 = new StringSetTask(hashes(), codeLocation);
+
+    assertThat(stringSetTask1.hash()).isNotEqualTo(stringSetTask2.hash());
+  }
+
+  @Test
+  public void hashOfStringSetWithOneStringIsDifferentFromHashOfStringSetWithDifferentElement()
+      throws Exception {
+    StringSetTask stringSetTask1 = new StringSetTask(hashes(task1), codeLocation);
+    StringSetTask stringSetTask2 = new StringSetTask(hashes(task2), codeLocation);
+
+    assertThat(stringSetTask1.hash()).isNotEqualTo(stringSetTask2.hash());
+  }
+
+  @Test
+  public void hashOfStringSetWithOneStringIsDifferentFromHashOfStringSetWithAdditionalString()
+      throws Exception {
+    StringSetTask stringSetTask1 = new StringSetTask(hashes(task1), codeLocation);
+    StringSetTask stringSetTask2 = new StringSetTask(hashes(task1, task2), codeLocation);
+
+    assertThat(stringSetTask1.hash()).isNotEqualTo(stringSetTask2.hash());
   }
 }
