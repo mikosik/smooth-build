bf32cadf1494cf7f74b84dbdf84c5278c42641f4
mikosik <marcin.mikosik@gmail.com>
2013-10-13 22:34:15 +0200

    moved opening script file input stream from SmoothApp to ModuleParser

--- src/java/org/smoothbuild/app/SmoothApp.java
+++ src/java/org/smoothbuild/app/SmoothApp.java
@@ -2,16 +2,10 @@ package org.smoothbuild.app;
 
 import static org.smoothbuild.message.message.MessageType.INFO;
 
-import java.io.InputStream;
-
 import javax.inject.Inject;
 
-import org.smoothbuild.app.err.ScriptFileNotFoundError;
 import org.smoothbuild.command.CommandLineArguments;
 import org.smoothbuild.command.CommandLineParser;
-import org.smoothbuild.fs.base.FileSystem;
-import org.smoothbuild.fs.base.Path;
-import org.smoothbuild.fs.base.exc.NoSuchFileException;
 import org.smoothbuild.function.base.Module;
 import org.smoothbuild.message.listen.DetectingErrorsMessageListener;
 import org.smoothbuild.message.listen.MessageListener;
@@ -24,18 +18,15 @@ public class SmoothApp {
   private final DetectingErrorsMessageListener messages;
   private final Cleaner cleaner;
   private final CommandLineParser commandLineParser;
-  private final FileSystem fileSystem;
   private final ModuleParser moduleParser;
   private final SmoothExecutor smoothExecutor;
 
   @Inject
   public SmoothApp(MessageListener messageListener, Cleaner cleaner,
-      CommandLineParser commandLineParser, FileSystem fileSystem, ModuleParser moduleParser,
-      SmoothExecutor smoothExecutor) {
+      CommandLineParser commandLineParser, ModuleParser moduleParser, SmoothExecutor smoothExecutor) {
     this.messages = new DetectingErrorsMessageListener(messageListener);
     this.cleaner = cleaner;
     this.commandLineParser = commandLineParser;
-    this.fileSystem = fileSystem;
     this.moduleParser = moduleParser;
     this.smoothExecutor = smoothExecutor;
   }
@@ -45,11 +36,8 @@ public class SmoothApp {
 
     try {
       CommandLineArguments args = commandLineParser.parse(commandLine);
-      Path scriptFile = args.scriptFile();
 
-      InputStream inputStream = scriptInputStream(messages, scriptFile);
-
-      Module module = moduleParser.createModule(messages, inputStream, scriptFile);
+      Module module = moduleParser.createModule(messages, args);
       if (messages.errorDetected()) {
         return;
       }
@@ -63,14 +51,6 @@ public class SmoothApp {
       messages.report(new Message(INFO, "BUILD FAILED"));
     } else {
       messages.report(new Message(INFO, "SUCCESS"));
-    }
-  }
-
-  private InputStream scriptInputStream(MessageListener messages, Path scriptFile) {
-    try {
-      return fileSystem.openInputStream(scriptFile);
-    } catch (NoSuchFileException e) {
-      throw new ErrorMessageException(new ScriptFileNotFoundError(scriptFile));
     }
   }
 }
