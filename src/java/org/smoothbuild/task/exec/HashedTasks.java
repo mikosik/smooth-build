1617e1a7a5b169feeb24ef252df2a11943220c80
mikosik <marcin.mikosik@gmail.com>
2013-10-14 09:57:18 +0200

    added HashedTasks.add()

--- src/java/org/smoothbuild/task/exec/HashedTasks.java
+++ src/java/org/smoothbuild/task/exec/HashedTasks.java
@@ -4,14 +4,26 @@ import java.util.Map;
 
 import org.smoothbuild.task.base.Task;
 
-import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Maps;
 import com.google.common.hash.HashCode;
 
 public class HashedTasks {
-  private final ImmutableMap<HashCode, Task> tasks;
+  private final Map<HashCode, Task> tasks;
 
   public HashedTasks(Map<HashCode, Task> tasks) {
-    this.tasks = ImmutableMap.copyOf(tasks);
+    this();
+    this.tasks.putAll(tasks);
+  }
+
+  public HashedTasks() {
+    this.tasks = Maps.newHashMap();
+  }
+
+  public void add(Task task) {
+    HashCode hash = task.hash();
+    if (tasks.get(hash) == null) {
+      tasks.put(hash, task);
+    }
   }
 
   public Task get(HashCode hash) {
