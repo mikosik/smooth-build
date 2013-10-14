a36235ec36fa0a94ee42db6eafb95c599d0d3336
mikosik <marcin.mikosik@gmail.com>
2013-10-14 16:44:12 +0200

    added SmoothExecutorPhase

---
+++ src/java/org/smoothbuild/task/exec/SmoothExecutorPhase.java
@@ -0,0 +1,23 @@
+package org.smoothbuild.task.exec;
+
+import javax.inject.Inject;
+
+import org.smoothbuild.message.listen.MessageCatchingExecutor;
+import org.smoothbuild.message.listen.UserConsole;
+
+public class SmoothExecutorPhase extends MessageCatchingExecutor<ExecutionData, Void> {
+  private final SmoothExecutor smoothExecutor;
+
+  @Inject
+  public SmoothExecutorPhase(UserConsole userConsole, SmoothExecutor smoothExecutor,
+      SmoothExecutorMessages messages) {
+    super(userConsole, messages);
+    this.smoothExecutor = smoothExecutor;
+  }
+
+  @Override
+  public Void executeImpl(ExecutionData executionData) {
+    smoothExecutor.execute(executionData);
+    return null;
+  }
+}
