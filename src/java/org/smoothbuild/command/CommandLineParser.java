0a5687bca68678165026cbbebff9d6f5985eaba0
mikosik <marcin.mikosik@gmail.com>
2013-10-14 14:22:54 +0200

    changed CommandLineParser.parse() to accept List<String> not String[]

--- src/java/org/smoothbuild/command/CommandLineParser.java
+++ src/java/org/smoothbuild/command/CommandLineParser.java
@@ -4,23 +4,25 @@ import static org.smoothbuild.command.SmoothContants.DEFAULT_SCRIPT;
 import static org.smoothbuild.function.base.Name.isLegalSimpleName;
 import static org.smoothbuild.function.base.Name.qualifiedName;
 
+import java.util.List;
+
 import org.smoothbuild.command.err.CommandLineError;
 import org.smoothbuild.command.err.IllegalFunctionNameError;
 import org.smoothbuild.command.err.NothingToDoError;
 import org.smoothbuild.message.message.ErrorMessageException;
 
 public class CommandLineParser {
-  public CommandLineArguments parse(String... args) {
-    if (args.length == 0) {
+  public CommandLineArguments parse(List<String> args) {
+    if (args.size() == 0) {
       throw new ErrorMessageException(new NothingToDoError());
     }
 
-    if (args.length > 1) {
+    if (args.size() > 1) {
       throw new ErrorMessageException(new CommandLineError(
           "Too many functions. Only one can be specified. (This will change in future version)"));
     }
 
-    String functionString = args[0];
+    String functionString = args.get(0);
     if (!isLegalSimpleName(functionString)) {
       throw new ErrorMessageException(new IllegalFunctionNameError(functionString));
     }
