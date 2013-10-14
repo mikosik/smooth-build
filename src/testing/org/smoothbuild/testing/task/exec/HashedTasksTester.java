a2e82693d3cbfe925e42491c6f053472a9d06d09
mikosik <marcin.mikosik@gmail.com>
2013-10-14 09:59:00 +0200

    refactored HashedTasksTester to use HashedTasks.add() instead of legacy constructor

--- src/testing/org/smoothbuild/testing/task/exec/HashedTasksTester.java
+++ src/testing/org/smoothbuild/testing/task/exec/HashedTasksTester.java
@@ -1,21 +1,28 @@
 package org.smoothbuild.testing.task.exec;
 
+import java.util.Map;
+
 import org.smoothbuild.task.base.Task;
 import org.smoothbuild.task.exec.HashedTasks;
 
-import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.ImmutableMap.Builder;
+import com.google.common.collect.Maps;
 import com.google.common.hash.HashCode;
 
 public class HashedTasksTester {
 
   public static HashedTasks hashedTasks(Task... tasks) {
-    Builder<HashCode, Task> builder = ImmutableMap.builder();
+    Map<HashCode, Task> alreadyAdded = Maps.newHashMap();
+    HashedTasks result = new HashedTasks();
     for (Task task : tasks) {
-      builder.put(task.hash(), task);
-    }
-    ImmutableMap<HashCode, Task> map = builder.build();
+      HashCode hash = task.hash();
+      if (alreadyAdded.containsKey(hash)) {
+        throw new IllegalArgumentException("Two tasks with the same hash cannot be added: " + task
+            + " " + alreadyAdded.get(hash));
+      }
+      alreadyAdded.put(hash, task);
 
-    return new HashedTasks(map);
+      result.add(task);
+    }
+    return result;
   }
 }
