95e6c896835f8f0a247b6d87caa15e315d04a71e
mikosik <marcin.mikosik@gmail.com>
2013-10-14 16:46:23 +0200

    refactored SmoothApp

--- src/java/org/smoothbuild/app/SmoothApp.java
+++ src/java/org/smoothbuild/app/SmoothApp.java
@@ -1,15 +1,10 @@
 package org.smoothbuild.app;
 
-import java.util.List;
-
 import javax.inject.Inject;
 
 import org.smoothbuild.command.CommandLineArguments;
 import org.smoothbuild.command.CommandLineParserPhase;
 import org.smoothbuild.function.base.Module;
-import org.smoothbuild.message.listen.DetectingErrorsMessageListener;
-import org.smoothbuild.message.listen.ErrorMessageException;
-import org.smoothbuild.message.listen.MessageListener;
 import org.smoothbuild.message.listen.UserConsole;
 import org.smoothbuild.parse.ModuleParserPhase;
 import org.smoothbuild.task.exec.ExecutionData;
@@ -19,18 +14,16 @@ import com.google.common.collect.ImmutableList;
 
 public class SmoothApp {
   private final UserConsole userConsole;
-  private final DetectingErrorsMessageListener messages;
   private final Cleaner cleaner;
   private final CommandLineParserPhase commandLineParserPhase;
   private final ModuleParserPhase moduleParserPhase;
   private final SmoothExecutorPhase smoothExecutorPhase;
 
   @Inject
-  public SmoothApp(UserConsole userConsole, MessageListener messageListener, Cleaner cleaner,
+  public SmoothApp(UserConsole userConsole, Cleaner cleaner,
       CommandLineParserPhase commandLineParserPhase, ModuleParserPhase moduleParserPhase,
       SmoothExecutorPhase smoothExecutorPhase) {
     this.userConsole = userConsole;
-    this.messages = new DetectingErrorsMessageListener(messageListener);
     this.cleaner = cleaner;
     this.commandLineParserPhase = commandLineParserPhase;
     this.moduleParserPhase = moduleParserPhase;
@@ -40,26 +33,15 @@ public class SmoothApp {
   public void run(String... commandLine) {
     cleaner.clearBuildDir();
 
-    try {
-      runImpl(ImmutableList.copyOf(commandLine));
-    } catch (ErrorMessageException e) {
-      messages.report(e.errorMessage());
+    CommandLineArguments args = commandLineParserPhase.execute(ImmutableList.copyOf(commandLine));
+
+    if (args != null) {
+      Module module = moduleParserPhase.execute(args);
+      if (!userConsole.isErrorReported()) {
+        smoothExecutorPhase.execute(new ExecutionData(args, module));
+      }
     }
 
     userConsole.printFinalSummary();
-  }
-
-  private void runImpl(List<String> commandLine) {
-    CommandLineArguments args = commandLineParserPhase.execute(commandLine);
-    if (args == null) {
-      return;
-    }
-
-    Module module = moduleParserPhase.execute(args);
-    if (userConsole.isErrorReported()) {
-      return;
-    }
-
-    smoothExecutorPhase.execute(new ExecutionData(args, module));
   }
 }
