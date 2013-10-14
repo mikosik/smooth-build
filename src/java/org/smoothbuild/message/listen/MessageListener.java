40844f7d0ea323dc01576615a3bedac41590b510
mikosik <marcin.mikosik@gmail.com>
2013-10-14 16:48:07 +0200

    removed unused PrintingMessageListener

--- src/java/org/smoothbuild/message/listen/MessageListener.java
+++ src/java/org/smoothbuild/message/listen/MessageListener.java
@@ -2,9 +2,6 @@ package org.smoothbuild.message.listen;
 
 import org.smoothbuild.message.message.Message;
 
-import com.google.inject.ImplementedBy;
-
-@ImplementedBy(PrintingMessageListener.class)
 public interface MessageListener {
   public void report(Message message);
 }
