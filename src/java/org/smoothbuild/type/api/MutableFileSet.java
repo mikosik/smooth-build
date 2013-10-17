a22fd20e8a5afde69bd46a6f25f251b4e34a3e35
mikosik <marcin.mikosik@gmail.com>
2013-10-17 06:30:48 +0200

    added MutableStoredFileSet.openFileOutputStream()

--- src/java/org/smoothbuild/type/api/MutableFileSet.java
+++ src/java/org/smoothbuild/type/api/MutableFileSet.java
@@ -1,8 +1,11 @@
 package org.smoothbuild.type.api;
 
-import org.smoothbuild.fs.base.Path;
+import java.io.OutputStream;
 
+import org.smoothbuild.fs.base.Path;
 
 public interface MutableFileSet extends FileSet {
   public MutableFile createFile(Path path);
+
+  public OutputStream openFileOutputStream(Path path);
 }
--- src/test/org/smoothbuild/type/impl/MutableStoredFileSetTest.java
+++ src/test/org/smoothbuild/type/impl/MutableStoredFileSetTest.java
@@ -1,23 +1,43 @@
 package org.smoothbuild.type.impl;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.smoothbuild.fs.base.Path.path;
+import static org.smoothbuild.fs.base.PathState.NOTHING;
 import static org.smoothbuild.testing.type.impl.FileTester.createContentWithFilePath;
 
 import java.io.IOException;
+import java.io.OutputStream;
 
 import org.junit.Test;
 import org.smoothbuild.fs.base.Path;
+import org.smoothbuild.testing.common.StreamTester;
 import org.smoothbuild.testing.fs.base.TestFileSystem;
-import org.smoothbuild.type.impl.MutableStoredFileSet;
 
 public class MutableStoredFileSetTest {
+  Path path = path("my/file");
+  String content = "content";
+
   TestFileSystem fileSystem = new TestFileSystem();
   MutableStoredFileSet fileSet = new MutableStoredFileSet(fileSystem);
 
   @Test
   public void createFile() throws IOException {
-    Path path = path("my/file");
     createContentWithFilePath(fileSet.createFile(path));
     fileSystem.assertFileContainsItsPath(path);
+  }
+
+  @Test
+  public void openFileOutputStream() throws IOException {
+    StreamTester.writeAndClose(fileSet.openFileOutputStream(path), content);
+    fileSystem.assertFileContains(path, content);
+  }
+
+  // TODO uncomment when refactoring ends or add check in (to be created)
+  // FileSetBuilder that all opened OutputStreams have been closed.
+  // @Test
+  public void file_is_not_saved_when_output_stream_is_not_closed() throws IOException {
+    OutputStream outputStream = fileSet.openFileOutputStream(path);
+    outputStream.write("abc".getBytes());
+    assertThat(fileSystem.pathState(path)).isEqualTo(NOTHING);
   }
 }
