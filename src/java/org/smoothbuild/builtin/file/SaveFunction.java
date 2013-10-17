66e9786e71cf9fa161b10d1f54d62a584765ca49
mikosik <marcin.mikosik@gmail.com>
2013-10-17 16:10:52 +0200

    removed speed optimization from SaveFunction

--- src/java/org/smoothbuild/builtin/file/SaveFunction.java
+++ src/java/org/smoothbuild/builtin/file/SaveFunction.java
@@ -3,6 +3,10 @@ package org.smoothbuild.builtin.file;
 import static org.smoothbuild.builtin.file.PathArgValidator.validatedPath;
 import static org.smoothbuild.command.SmoothContants.BUILD_DIR;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 import org.smoothbuild.builtin.file.err.DirParamIsAFileError;
 import org.smoothbuild.builtin.file.err.DirParamSubdirIsAFileError;
 import org.smoothbuild.builtin.file.err.EitherFileOrFilesMustBeProvidedError;
@@ -12,13 +16,15 @@ import org.smoothbuild.builtin.file.err.FileOutputSubdirIsAFileError;
 import org.smoothbuild.builtin.file.err.WriteToSmoothDirError;
 import org.smoothbuild.fs.base.FileSystem;
 import org.smoothbuild.fs.base.Path;
+import org.smoothbuild.fs.base.exc.FileSystemException;
 import org.smoothbuild.message.listen.ErrorMessageException;
 import org.smoothbuild.plugin.api.Required;
 import org.smoothbuild.plugin.api.SmoothFunction;
 import org.smoothbuild.task.exec.SandboxImpl;
 import org.smoothbuild.type.api.File;
 import org.smoothbuild.type.api.FileSet;
-import org.smoothbuild.type.impl.StoredFile;
+
+import com.google.common.io.ByteStreams;
 
 public class SaveFunction {
   public interface Parameters {
@@ -79,10 +85,14 @@ public class SaveFunction {
     }
 
     private void saveImpl(Path dirPath, File file) {
-      Path fileSystemRoot = ((StoredFile) file).fileSystem().root();
-      Path source = fileSystemRoot.append(file.path());
       Path destination = dirPath.append(file.path());
-      sandbox.projectFileSystem().copy(source, destination);
+
+      try (InputStream inputStream = file.openInputStream();
+          OutputStream outputStream = sandbox.projectFileSystem().openOutputStream(destination);) {
+        ByteStreams.copy(inputStream, outputStream);
+      } catch (IOException e) {
+        throw new FileSystemException(e);
+      }
     }
 
     private void checkDirParam(Path dirPath) {
