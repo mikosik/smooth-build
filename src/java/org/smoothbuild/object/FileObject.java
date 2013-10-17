ade36c2e2116aefec0a32b812b4879921472ee3c
mikosik <marcin.mikosik@gmail.com>
2013-10-17 14:37:47 +0200

    added FileObject

---
+++ src/java/org/smoothbuild/object/FileObject.java
@@ -0,0 +1,34 @@
+package org.smoothbuild.object;
+
+import java.io.InputStream;
+
+import org.smoothbuild.fs.base.Path;
+import org.smoothbuild.type.api.File;
+
+import com.google.common.hash.HashCode;
+
+public class FileObject implements File {
+  private final Path path;
+  private final BlobObject content;
+  private final HashCode hash;
+
+  public FileObject(Path path, BlobObject content, HashCode hash) {
+    this.path = path;
+    this.content = content;
+    this.hash = hash;
+  }
+
+  @Override
+  public Path path() {
+    return path;
+  }
+
+  public HashCode hash() {
+    return hash;
+  }
+
+  @Override
+  public InputStream openInputStream() {
+    return content.openInputStream();
+  }
+}
