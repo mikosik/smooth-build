b3b17d675e32de9f4bb1abc15ca96cf74ad9079d
mikosik <marcin.mikosik@gmail.com>
2013-10-18 13:50:28 +0200

    added object.Unmarshaller

---
+++ src/java/org/smoothbuild/object/Unmarshaller.java
@@ -0,0 +1,89 @@
+package org.smoothbuild.object;
+
+import static org.smoothbuild.fs.base.Path.path;
+import static org.smoothbuild.object.HashedDb.STRING_CHARSET;
+
+import java.io.Closeable;
+import java.io.DataInputStream;
+import java.io.IOException;
+
+import org.smoothbuild.fs.base.Path;
+import org.smoothbuild.fs.base.exc.FileSystemException;
+import org.smoothbuild.hash.Hash;
+import org.smoothbuild.message.listen.ErrorMessageException;
+import org.smoothbuild.object.err.IllegalPathInObjectError;
+import org.smoothbuild.object.err.TooFewBytesToUnmarshallValue;
+
+import com.google.common.hash.HashCode;
+
+public class Unmarshaller implements Closeable {
+  private final HashCode hash;
+  private final DataInputStream inputStream;
+
+  public Unmarshaller(HashedDb hashedDb, HashCode hash) {
+    this.hash = hash;
+    this.inputStream = new DataInputStream(hashedDb.openInputStream(hash));
+  }
+
+  public Path readPath() {
+    try {
+      return readPathImpl();
+    } catch (IOException e) {
+      throw new FileSystemException(e);
+    }
+  }
+
+  private Path readPathImpl() throws IOException {
+    int size = inputStream.readInt();
+    byte[] bytes = readBytes(size, "path");
+
+    /*
+     * This method always replaces malformed-input and unmappable-character
+     * sequences with this charset's default replacement string. The {@link
+     * java.nio.charset.CharsetDecoder} class should be used when more control
+     * over the decoding process is required.
+     */
+    String value = new String(bytes, STRING_CHARSET);
+
+    return toPathSafely(value);
+  }
+
+  private Path toPathSafely(String value) {
+    try {
+      return path(value);
+    } catch (IllegalArgumentException e) {
+      throw new ErrorMessageException(new IllegalPathInObjectError(e.getMessage()));
+    }
+  }
+
+  public HashCode readHash() {
+    try {
+      return readHashImpl();
+    } catch (IOException e) {
+      throw new FileSystemException(e);
+    }
+  }
+
+  private HashCode readHashImpl() throws IOException {
+    byte[] bytes = readBytes(Hash.size(), "hash");
+    return HashCode.fromBytes(bytes);
+  }
+
+  private byte[] readBytes(int size, String valueName) throws IOException {
+    byte[] bytes = new byte[size];
+    int read = inputStream.read(bytes);
+    if (read < size) {
+      throw new ErrorMessageException(new TooFewBytesToUnmarshallValue(hash, valueName, size, read));
+    }
+    return bytes;
+  }
+
+  @Override
+  public void close() {
+    try {
+      inputStream.close();
+    } catch (IOException e) {
+      throw new FileSystemException(e);
+    }
+  }
+}
---
+++ src/test/org/smoothbuild/object/UnmarshallerTest.java
@@ -0,0 +1,133 @@
+package org.smoothbuild.object;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.smoothbuild.command.SmoothContants.OBJECTS_DIR;
+import static org.smoothbuild.fs.base.Path.path;
+import static org.smoothbuild.object.HashCodes.toPath;
+import static org.smoothbuild.testing.message.ErrorMessageMatchers.containsInstanceOf;
+
+import java.io.DataOutputStream;
+
+import org.junit.Test;
+import org.smoothbuild.fs.base.Path;
+import org.smoothbuild.hash.Hash;
+import org.smoothbuild.message.listen.ErrorMessageException;
+import org.smoothbuild.object.err.IllegalPathInObjectError;
+import org.smoothbuild.object.err.TooFewBytesToUnmarshallValue;
+import org.smoothbuild.testing.fs.base.TestFileSystem;
+
+import com.google.common.hash.HashCode;
+
+public class UnmarshallerTest {
+  TestFileSystem fileSystem = new TestFileSystem();
+  HashedDb hashedDb = new HashedDb(fileSystem);
+
+  @Test
+  public void marshalled_path_can_be_unmarshalled() {
+    Path path = path("my/path");
+
+    Marshaller marshaller = new Marshaller(hashedDb);
+    marshaller.addPath(path);
+    HashCode hash = marshaller.store();
+
+    Unmarshaller unmarshaller = new Unmarshaller(hashedDb, hash);
+    Path actual = unmarshaller.readPath();
+    unmarshaller.close();
+
+    assertThat(actual).isEqualTo(path);
+  }
+
+  @Test
+  public void marshalled_hash_can_be_unmarshalled() {
+    HashCode myHash = Hash.hashFunction().hashInt(33);
+
+    Marshaller marshaller = new Marshaller(hashedDb);
+    marshaller.addHash(myHash);
+    HashCode hash = marshaller.store();
+
+    Unmarshaller unmarshaller = new Unmarshaller(hashedDb, hash);
+    HashCode actual = unmarshaller.readHash();
+    unmarshaller.close();
+
+    assertThat(actual).isEqualTo(myHash);
+  }
+
+  @Test
+  public void marshalled_hash_and_path_can_be_unmarshalled() {
+    HashCode myHash = Hash.hashFunction().hashInt(33);
+    Path path = path("my/path");
+
+    Marshaller marshaller = new Marshaller(hashedDb);
+    marshaller.addHash(myHash);
+    marshaller.addPath(path);
+    HashCode hash = marshaller.store();
+
+    Unmarshaller unmarshaller = new Unmarshaller(hashedDb, hash);
+    HashCode actualHash = unmarshaller.readHash();
+    Path actualPath = unmarshaller.readPath();
+    unmarshaller.close();
+
+    assertThat(actualHash).isEqualTo(myHash);
+    assertThat(actualPath).isEqualTo(path);
+  }
+
+  @Test
+  public void too_short_hash_in_db_causes_exception() throws Exception {
+    HashCode objectHash = HashCode.fromInt(33);
+    Path objectPath = OBJECTS_DIR.append(toPath(objectHash));
+    try (DataOutputStream outputStream = new DataOutputStream(
+        fileSystem.openOutputStream(objectPath))) {
+      outputStream.write(new byte[Hash.size() - 1]);
+    }
+
+    try (Unmarshaller unmarshaller = new Unmarshaller(hashedDb, objectHash)) {
+      try {
+        unmarshaller.readHash();
+      } catch (ErrorMessageException e) {
+        assertThat(containsInstanceOf(TooFewBytesToUnmarshallValue.class).matches(e)).isTrue();
+      }
+    }
+  }
+
+  @Test
+  public void too_short_path_in_db_causes_exception() throws Exception {
+    HashCode objectHash = HashCode.fromInt(33);
+    Path objectPath = OBJECTS_DIR.append(toPath(objectHash));
+    try (DataOutputStream outputStream = new DataOutputStream(
+        fileSystem.openOutputStream(objectPath))) {
+      int size = 10;
+      outputStream.writeInt(size + 1);
+      outputStream.write(new byte[size]);
+    }
+
+    try (Unmarshaller unmarshaller = new Unmarshaller(hashedDb, objectHash)) {
+      try {
+        unmarshaller.readPath();
+      } catch (ErrorMessageException e) {
+        assertThat(containsInstanceOf(TooFewBytesToUnmarshallValue.class).matches(e)).isTrue();
+      }
+    }
+  }
+
+  @Test
+  public void illegal_path_causes_exception() throws Exception {
+    HashCode objectHash = HashCode.fromInt(33);
+    Path objectPath = OBJECTS_DIR.append(toPath(objectHash));
+    String illegalPathValue = "/";
+
+    try (DataOutputStream outputStream = new DataOutputStream(
+        fileSystem.openOutputStream(objectPath))) {
+      byte[] bytes = illegalPathValue.getBytes();
+      outputStream.writeInt(bytes.length);
+      outputStream.write(bytes);
+    }
+
+    try (Unmarshaller unmarshaller = new Unmarshaller(hashedDb, objectHash)) {
+      try {
+        unmarshaller.readPath();
+      } catch (ErrorMessageException e) {
+        assertThat(containsInstanceOf(IllegalPathInObjectError.class).matches(e)).isTrue();
+      }
+    }
+  }
+}
