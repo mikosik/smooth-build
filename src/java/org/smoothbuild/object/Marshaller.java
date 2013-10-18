d6b1c2c7c0386b7a45fa1a3d9fc82a94aee51afc
mikosik <marcin.mikosik@gmail.com>
2013-10-18 12:23:40 +0200

    added object.Marshaller

---
+++ src/java/org/smoothbuild/object/Marshaller.java
@@ -0,0 +1,33 @@
+package org.smoothbuild.object;
+
+import static org.smoothbuild.object.HashedDb.STRING_CHARSET;
+
+import org.smoothbuild.fs.base.Path;
+
+import com.google.common.hash.HashCode;
+import com.google.common.io.ByteArrayDataOutput;
+import com.google.common.io.ByteStreams;
+
+public class Marshaller {
+  private final HashedDb hashedDb;
+  private final ByteArrayDataOutput dataOutput;
+
+  public Marshaller(HashedDb hashedDb) {
+    this.hashedDb = hashedDb;
+    this.dataOutput = ByteStreams.newDataOutput(256);
+  }
+
+  public void addPath(Path path) {
+    byte[] pathBytes = path.value().getBytes(STRING_CHARSET);
+    dataOutput.writeInt(pathBytes.length);
+    dataOutput.write(pathBytes);
+  }
+
+  public void addHash(HashCode hash) {
+    dataOutput.write(hash.asBytes());
+  }
+
+  public HashCode store() {
+    return hashedDb.store(dataOutput.toByteArray());
+  }
+}
---
+++ src/test/org/smoothbuild/object/MarshallerTest.java
@@ -0,0 +1,51 @@
+package org.smoothbuild.object;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.smoothbuild.fs.base.Path.path;
+
+import org.junit.Test;
+import org.smoothbuild.fs.base.Path;
+
+import com.google.common.base.Charsets;
+import com.google.common.hash.HashCode;
+import com.google.common.primitives.Bytes;
+import com.google.common.primitives.Ints;
+
+public class MarshallerTest {
+  HashedDb hashedDb = mock(HashedDb.class);
+  private Marshaller marshaller;
+
+  @Test
+  public void storing_single_hash() {
+    HashCode hashCode = HashCode.fromInt(33);
+
+    marshaller = new Marshaller(hashedDb);
+    marshaller.addHash(hashCode);
+    marshaller.store();
+
+    verify(hashedDb).store(hashToBytes(hashCode));
+  }
+
+  @Test
+  public void storing_single_path() {
+    Path path = path("my/path");
+
+    marshaller = new Marshaller(hashedDb);
+    marshaller.addPath(path);
+    marshaller.store();
+
+    verify(hashedDb).store(pathToBytes(path));
+  }
+
+  private static byte[] pathToBytes(Path path) {
+    byte[] sizeBytes = Ints.toByteArray(path.value().length());
+    byte[] charBytes = path.value().getBytes(Charsets.UTF_8);
+    byte[] bytes = Bytes.concat(sizeBytes, charBytes);
+    return bytes;
+  }
+
+  private static byte[] hashToBytes(HashCode hashCode) {
+    return hashCode.asBytes();
+  }
+}
