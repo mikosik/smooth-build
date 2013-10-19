092e8a87b5ff06517f06be3cce694f1b0fd3610e
mikosik <marcin.mikosik@gmail.com>
2013-10-19 10:03:39 +0200

    added FileBuilder

---
+++ src/java/org/smoothbuild/object/FileBuilder.java
@@ -0,0 +1,39 @@
+package org.smoothbuild.object;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.common.base.Preconditions.checkState;
+
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+
+import org.smoothbuild.fs.base.Path;
+import org.smoothbuild.type.api.File;
+
+public class FileBuilder {
+  private final ObjectsDb objectsDb;
+
+  private Path path;
+  private ByteArrayOutputStream outputStream;
+
+  public FileBuilder(ObjectsDb objectsDb) {
+    this.objectsDb = objectsDb;
+  }
+
+  public void setPath(Path path) {
+    checkState(this.path == null, "Path has been already set.");
+    this.path = checkNotNull(path);
+  }
+
+  public OutputStream openOutputStream() {
+    checkState(this.outputStream == null, "Cannot open output stream twice.");
+    this.outputStream = new ByteArrayOutputStream();
+    return outputStream;
+  }
+
+  public File build() {
+    checkState(outputStream != null, "No file content available. Create one via openOutputStream()");
+    checkState(path != null, "No path set");
+
+    return objectsDb.file(path, outputStream.toByteArray());
+  }
+}
