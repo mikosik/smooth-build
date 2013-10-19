753662255564e680f30ad8ab8bc6d3938b2c99f9
mikosik <marcin.mikosik@gmail.com>
2013-10-19 10:37:55 +0200

    added Sandbox.fileBuilder()

--- src/java/org/smoothbuild/plugin/api/Sandbox.java
+++ src/java/org/smoothbuild/plugin/api/Sandbox.java
@@ -2,11 +2,14 @@ package org.smoothbuild.plugin.api;
 
 import org.smoothbuild.fs.base.Path;
 import org.smoothbuild.message.message.Message;
+import org.smoothbuild.object.FileBuilder;
 import org.smoothbuild.type.api.MutableFile;
 import org.smoothbuild.type.impl.FileSetBuilder;
 
 public interface Sandbox {
   public void report(Message message);
+
+  public FileBuilder fileBuilder();
 
   public FileSetBuilder fileSetBuilder();
 
--- src/test/org/smoothbuild/task/exec/SandboxImplTest.java
+++ src/test/org/smoothbuild/task/exec/SandboxImplTest.java
@@ -1,7 +1,6 @@
 package org.smoothbuild.task.exec;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
 import static org.smoothbuild.fs.base.Path.path;
 import static org.smoothbuild.function.base.Name.simpleName;
 import static org.smoothbuild.message.message.CallLocation.callLocation;
@@ -13,9 +12,12 @@ import org.junit.Test;
 import org.smoothbuild.fs.base.Path;
 import org.smoothbuild.message.message.CallLocation;
 import org.smoothbuild.message.message.Message;
+import org.smoothbuild.object.FileBuilder;
+import org.smoothbuild.object.HashedDb;
 import org.smoothbuild.object.ObjectsDb;
 import org.smoothbuild.testing.common.StreamTester;
 import org.smoothbuild.testing.fs.base.TestFileSystem;
+import org.smoothbuild.type.api.File;
 import org.smoothbuild.type.api.MutableFile;
 import org.smoothbuild.type.impl.FileSetBuilder;
 
@@ -27,19 +29,31 @@ public class SandboxImplTest {
   CallLocation callLocation = callLocation(simpleName("name"), codeLocation(1, 2, 4));
 
   TestFileSystem fileSystem = new TestFileSystem();
-  ObjectsDb objectsDb = mock(ObjectsDb.class);
+  ObjectsDb objectsDb = new ObjectsDb(new HashedDb(fileSystem));
 
   SandboxImpl sandbox = new SandboxImpl(fileSystem, objectsDb, root, callLocation);
 
   @Test
-  public void fileSetBuilderStoresFilesInSandboxFileSystem() throws Exception {
+  public void file_builder_stores_file_in_object_db() throws Exception {
+    FileBuilder fileBuilder = new FileBuilder(objectsDb);
+    fileBuilder.setPath(path1);
+    StreamTester.writeAndClose(fileBuilder.openOutputStream(), content);
+
+    File file = fileBuilder.build();
+
+    assertThat(file.path()).isEqualTo(path1);
+    assertThat(StreamTester.inputStreamToString(file.openInputStream())).isEqualTo(content);
+  }
+
+  @Test
+  public void file_set_builder_stores_files_in_sandbox_file_system() throws Exception {
     FileSetBuilder builder = sandbox.fileSetBuilder();
     StreamTester.writeAndClose(builder.openFileOutputStream(path1), content);
     fileSystem.assertFileContains(root.append(path1), content);
   }
 
   @Test
-  public void createFileCreatesFileOnFileSystem() throws Exception {
+  public void create_file_creates_file_on_file_system() throws Exception {
     MutableFile newFile = sandbox.createFile(path1);
     writeAndClose(newFile.openOutputStream(), path1.value());
 
