db7ff15498f7f044cce711bd354d7c6cc9526e90
mikosik <marcin.mikosik@gmail.com>
2013-10-20 18:04:57 +0200

    renamed TestUserConsole -> FakeUserConsole

--- src/testing/org/smoothbuild/testing/message/FakeUserConsoleModule.java
+++ src/testing/org/smoothbuild/testing/message/FakeUserConsoleModule.java
@@ -6,10 +6,10 @@ import org.smoothbuild.message.listen.UserConsole;
 
 import com.google.inject.AbstractModule;
 
-public class TestUserConsoleModule extends AbstractModule {
+public class FakeUserConsoleModule extends AbstractModule {
   @Override
   protected void configure() {
-    bind(TestUserConsole.class).in(Singleton.class);
-    bind(UserConsole.class).to(TestUserConsole.class);
+    bind(FakeUserConsole.class).in(Singleton.class);
+    bind(UserConsole.class).to(FakeUserConsole.class);
   }
 }
