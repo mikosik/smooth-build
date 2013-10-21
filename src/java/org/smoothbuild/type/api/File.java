2d40342d61aaa4949c9c430d7e945cd86e7ce958
mikosik <marcin.mikosik@gmail.com>
2013-10-21 15:14:22 +0200

    added hash() method to StoredFile/File

--- src/java/org/smoothbuild/type/api/File.java
+++ src/java/org/smoothbuild/type/api/File.java
@@ -4,8 +4,11 @@ import java.io.InputStream;
 
 import org.smoothbuild.fs.base.Path;
 
+import com.google.common.hash.HashCode;
 
 public interface File {
+  public HashCode hash();
+
   public Path path();
 
   public InputStream openInputStream();
--- src/test/org/smoothbuild/type/impl/StoredFileTest.java
+++ src/test/org/smoothbuild/type/impl/StoredFileTest.java
@@ -5,9 +5,14 @@ import static org.smoothbuild.fs.base.Path.path;
 
 import org.junit.Test;
 import org.smoothbuild.fs.base.Path;
+import org.smoothbuild.object.FileObject;
+import org.smoothbuild.object.HashedDb;
+import org.smoothbuild.object.ObjectsDb;
+import org.smoothbuild.testing.common.StreamTester;
 import org.smoothbuild.testing.fs.base.FakeFileSystem;
 import org.smoothbuild.testing.type.impl.FileTester;
-import org.smoothbuild.type.impl.StoredFile;
+
+import com.google.common.base.Charsets;
 
 public class StoredFileTest {
   FakeFileSystem fileSystem = new FakeFileSystem();
@@ -36,4 +41,17 @@ public class StoredFileTest {
     assertThat(storedFile.toString()).isEqualTo("StoredFile(" + filePath.toString() + ")");
   }
 
+  @Test
+  public void hashIsTheSameAsForFileObjectImplementation() throws Exception {
+    Path path = Path.path("my/file");
+    String content = "content";
+
+    MutableStoredFile mutableStoredFile = new MutableStoredFile(fileSystem, path);
+    StreamTester.writeAndClose(mutableStoredFile.openOutputStream(), content);
+
+    ObjectsDb objectsDb = new ObjectsDb(new HashedDb(new FakeFileSystem()));
+    FileObject fileObject = objectsDb.file(path, content.getBytes(Charsets.UTF_8));
+
+    assertThat(mutableStoredFile.hash()).isEqualTo(fileObject.hash());
+  }
 }
