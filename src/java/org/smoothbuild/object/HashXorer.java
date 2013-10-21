510018876a712bb06bf29dfb939d5826a0b6c517
mikosik <marcin.mikosik@gmail.com>
2013-10-21 17:10:02 +0200

    added HashXorer

---
+++ src/java/org/smoothbuild/object/HashXorer.java
@@ -0,0 +1,38 @@
+package org.smoothbuild.object;
+
+import org.smoothbuild.hash.Hash;
+
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.hash.HashCode;
+
+public class HashXorer {
+  private final byte[] bytes;
+
+  public HashXorer() {
+    this(Hash.size());
+  }
+
+  @VisibleForTesting
+  HashXorer(int size) {
+    this.bytes = new byte[size];
+  }
+
+  public void xorWith(HashCode hash) {
+    byte[] hashBytes = hash.asBytes();
+    checkSize(hashBytes);
+    for (int i = 0; i < bytes.length; i++) {
+      bytes[i] = (byte) (bytes[i] ^ hashBytes[i]);
+    }
+  }
+
+  private void checkSize(byte[] hashBytes) {
+    if (bytes.length != hashBytes.length) {
+      throw new IllegalArgumentException("Wrong hash size, expected " + bytes.length + " got "
+          + hashBytes.length);
+    }
+  }
+
+  public HashCode hash() {
+    return HashCode.fromBytes(bytes);
+  }
+}
