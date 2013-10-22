85f6497ceca7b856aa862deb27e47cd1ca3dc36f
mikosik <marcin.mikosik@gmail.com>
2013-10-22 16:03:24 +0200

    added FakeObjectsDb.createFileContainingItsPath()

--- src/testing/org/smoothbuild/testing/object/FakeObjectsDb.java
+++ src/testing/org/smoothbuild/testing/object/FakeObjectsDb.java
@@ -1,9 +1,13 @@
 package org.smoothbuild.testing.object;
 
 import org.smoothbuild.fs.base.FileSystem;
+import org.smoothbuild.fs.base.Path;
 import org.smoothbuild.object.HashedDb;
 import org.smoothbuild.object.ObjectsDb;
 import org.smoothbuild.testing.fs.base.FakeFileSystem;
+import org.smoothbuild.type.api.File;
+
+import com.google.common.base.Charsets;
 
 public class FakeObjectsDb extends ObjectsDb {
   public FakeObjectsDb() {
@@ -12,5 +16,9 @@ public class FakeObjectsDb extends ObjectsDb {
 
   public FakeObjectsDb(FileSystem fakeFileSystem) {
     super(new HashedDb(fakeFileSystem));
+  }
+
+  public File createFileContainingItsPath(Path path) {
+    return file(path, path.value().getBytes(Charsets.UTF_8));
   }
 }
