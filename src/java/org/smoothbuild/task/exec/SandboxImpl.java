03d7b2d043d63ff73b6fd9556043ab95fcf95d2f
mikosik <marcin.mikosik@gmail.com>
2013-10-22 12:09:43 +0200

    removed Sandbox.createFile()

--- src/java/org/smoothbuild/task/exec/SandboxImpl.java
+++ src/java/org/smoothbuild/task/exec/SandboxImpl.java
@@ -11,13 +11,10 @@ import org.smoothbuild.object.FileBuilder;
 import org.smoothbuild.object.FileSetBuilder;
 import org.smoothbuild.object.ObjectsDb;
 import org.smoothbuild.plugin.api.Sandbox;
-import org.smoothbuild.type.api.MutableFile;
-import org.smoothbuild.type.impl.MutableStoredFileSet;
 
 public class SandboxImpl implements Sandbox {
   private final FileSystem projectFileSystem;
   private final FileSystem sandboxFileSystem;
-  private final MutableStoredFileSet resultFileSet;
   private final MessageGroup messageGroup;
   private final CallLocation callLocation;
   private final ObjectsDb objectsDb;
@@ -33,7 +30,6 @@ public class SandboxImpl implements Sandbox {
     this.projectFileSystem = fileSystem;
     this.sandboxFileSystem = sandboxFileSystem;
     this.objectsDb = objectsDb;
-    this.resultFileSet = new MutableStoredFileSet(sandboxFileSystem);
     this.messageGroup = messageGroup;
     this.callLocation = callLocation;
   }
@@ -46,11 +42,6 @@ public class SandboxImpl implements Sandbox {
   @Override
   public FileSetBuilder fileSetBuilder() {
     return new FileSetBuilder(sandboxFileSystem);
-  }
-
-  @Override
-  public MutableFile createFile(Path path) {
-    return resultFileSet.createFile(path);
   }
 
   public FileSystem projectFileSystem() {
