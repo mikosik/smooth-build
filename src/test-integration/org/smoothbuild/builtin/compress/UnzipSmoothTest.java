1eea46001c77bded2b9b9219846c06f1ba706127
mikosik <marcin.mikosik@gmail.com>
2013-10-23 12:57:21 +0200

    refactored UnzipSmoothTest

--- src/test-integration/org/smoothbuild/builtin/compress/UnzipSmoothTest.java
+++ src/test-integration/org/smoothbuild/builtin/compress/UnzipSmoothTest.java
@@ -7,19 +7,18 @@ import org.smoothbuild.fs.base.Path;
 import org.smoothbuild.integration.IntegrationTestCase;
 import org.smoothbuild.testing.common.ZipTester;
 import org.smoothbuild.testing.type.impl.FakeFile;
-import org.smoothbuild.testing.type.impl.FakeFileSet;
-import org.smoothbuild.testing.type.impl.FileTester;
 
 public class UnzipSmoothTest extends IntegrationTestCase {
 
   @Test
   public void testUnzipping() throws Exception {
     // given
+    Path outPath = path("out");
     Path fileA = path("a/fileA.txt");
     Path fileB = path("b/fileB.txt");
     FakeFile zipFile = ZipTester.zippedFiles(fileSystem, fileA.value(), fileB.value());
 
-    script("run : file(" + zipFile.path() + ") | unzip | save('out');");
+    script("run : file(" + zipFile.path() + ") | unzip | save(" + outPath + ");");
 
     // when
     smoothApp.run("run");
@@ -27,9 +26,7 @@ public class UnzipSmoothTest extends IntegrationTestCase {
     // then
     messages.assertNoProblems();
 
-    FakeFileSet outFiles = fileSet(path("out"));
-    outFiles.contains(fileA);
-    outFiles.contains(fileB);
-    FileTester.assertContentContainsFilePath(outFiles.file(fileA));
+    fileSystem.assertFileContainsItsPath(outPath, fileA);
+    fileSystem.assertFileContainsItsPath(outPath, fileB);
   }
 }
