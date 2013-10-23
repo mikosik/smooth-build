a1e66c6ee69b634d978e04cca8b1204852f5bc69
mikosik <marcin.mikosik@gmail.com>
2013-10-23 14:34:35 +0200

    refactored MergeSmoothTest

--- src/test-integration/org/smoothbuild/builtin/file/MergeSmoothTest.java
+++ src/test-integration/org/smoothbuild/builtin/file/MergeSmoothTest.java
@@ -1,6 +1,5 @@
 package org.smoothbuild.builtin.file;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.smoothbuild.fs.base.Path.path;
 
 import org.junit.Test;
@@ -8,7 +7,6 @@ import org.smoothbuild.builtin.file.err.DuplicateMergedPathError;
 import org.smoothbuild.fs.base.Path;
 import org.smoothbuild.integration.IntegrationTestCase;
 import org.smoothbuild.testing.parse.ScriptBuilder;
-import org.smoothbuild.testing.type.impl.FakeFile;
 
 public class MergeSmoothTest extends IntegrationTestCase {
 
@@ -32,27 +30,24 @@ public class MergeSmoothTest extends IntegrationTestCase {
 
     // then
     messages.assertNoProblems();
-    fileSet(outputPath).file(path1).assertContentContainsFilePath();
-    fileSet(outputPath).file(path2).assertContentContainsFilePath();
-    assertThat(fileSet(outputPath)).hasSize(2);
+    fileSystem.assertFileContainsItsPath(outputPath, path1);
+    fileSystem.assertFileContainsItsPath(outputPath, path2);
   }
 
   @Test
   public void mergingFailsWhenSetsContainFileWithTheSamePath() throws Exception {
     // given
-    Path outputPath = path("output");
-    Path dirAPath = path("dirA");
-    Path dirBPath = path("dirB");
+    Path dir1Path = path("dirA");
+    Path dir2Path = path("dirB");
     Path filePath = path("def/fileA.txt");
+    Path outputPath = path("output");
 
-    FakeFile fileA = fileSet(dirAPath).file(filePath);
-    FakeFile fileB = fileSet(dirBPath).file(filePath);
-    fileA.createContentWithFilePath();
-    fileB.createContentWithFilePath();
+    fileSystem.createFileContainingItsPath(dir1Path, filePath);
+    fileSystem.createFileContainingItsPath(dir2Path, filePath);
 
     ScriptBuilder builder = new ScriptBuilder();
-    builder.addLine("a : files(" + dirAPath + ") ;");
-    builder.addLine("b : files(" + dirBPath + ") ;");
+    builder.addLine("a : files(" + dir1Path + ") ;");
+    builder.addLine("b : files(" + dir2Path + ") ;");
     builder.addLine("run : merge(files=a, with=b) | save(" + outputPath + ");");
     script(builder.build());
 
