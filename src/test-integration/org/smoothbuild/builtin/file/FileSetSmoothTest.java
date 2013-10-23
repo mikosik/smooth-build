353b0827cb3f333b96b56360c274f3f27e6c43fe
mikosik <marcin.mikosik@gmail.com>
2013-10-23 11:47:38 +0200

    refactored FileSetSmoothTest

--- src/test-integration/org/smoothbuild/builtin/file/FileSetSmoothTest.java
+++ src/test-integration/org/smoothbuild/builtin/file/FileSetSmoothTest.java
@@ -10,23 +10,22 @@ import org.smoothbuild.fs.base.Path;
 import org.smoothbuild.integration.IntegrationTestCase;
 import org.smoothbuild.object.err.DuplicatePathError;
 import org.smoothbuild.parse.err.SyntaxError;
-import org.smoothbuild.testing.type.impl.FakeFile;
 import org.smoothbuild.testing.type.impl.FakeFileSet;
 
 import com.google.common.collect.Iterables;
 
 public class FileSetSmoothTest extends IntegrationTestCase {
+  Path path1 = path("file/path/file1.txt");
+  Path path2 = path("file/path/file2.txt");
 
   @Test
   public void setWithTrailingCommaIsAllowed() throws Exception {
     Path dir = path("destination/dir");
-    FakeFile file1 = file(path("file/path/file1.txt"));
-    FakeFile file2 = file(path("file/path/file2.txt"));
-    file1.createContentWithFilePath();
-    file2.createContentWithFilePath();
+    fileSystem.createFileContainingItsPath(path1);
+    fileSystem.createFileContainingItsPath(path2);
 
     StringBuilder builder = new StringBuilder();
-    builder.append("myfiles : [ file(" + file1.path() + "), file(" + file2.path() + "), ];\n");
+    builder.append("myfiles : [ file(" + path1 + "), file(" + path2 + "), ];\n");
     builder.append("run : myfiles | save(" + dir + ");\n");
     script(builder.toString());
 
@@ -55,13 +54,11 @@ public class FileSetSmoothTest extends IntegrationTestCase {
   public void saveFileSetWithTwoFiles() throws IOException {
     // given
     Path dir = path("destination/dir");
-    FakeFile file1 = file(path("file/path/file1.txt"));
-    FakeFile file2 = file(path("file/path/file2.txt"));
-    file1.createContentWithFilePath();
-    file2.createContentWithFilePath();
+    fileSystem.createFileContainingItsPath(path1);
+    fileSystem.createFileContainingItsPath(path2);
 
     StringBuilder builder = new StringBuilder();
-    builder.append("myfiles : [ file(" + file1.path() + "), file(" + file2.path() + ") ];\n");
+    builder.append("myfiles : [ file(" + path1 + "), file(" + path2 + ") ];\n");
     builder.append("run : myfiles | save(" + dir + ");\n");
     script(builder.toString());
 
@@ -71,8 +68,8 @@ public class FileSetSmoothTest extends IntegrationTestCase {
     // then
     messages.assertNoProblems();
     FakeFileSet resultFiles = fileSet(dir);
-    resultFiles.file(file1.path()).assertContentContainsFilePath();
-    resultFiles.file(file2.path()).assertContentContainsFilePath();
+    resultFiles.file(path1).assertContentContainsFilePath();
+    resultFiles.file(path2).assertContentContainsFilePath();
     assertThat(Iterables.size(resultFiles)).isEqualTo(2);
   }
 
@@ -80,11 +77,11 @@ public class FileSetSmoothTest extends IntegrationTestCase {
   public void saveFileSetWithOneFile() throws IOException {
     // given
     Path dir = path("destination/dir");
-    FakeFile file1 = file(path("file/path/file1.txt"));
-    file1.createContentWithFilePath();
+
+    fileSystem.createFileContainingItsPath(path1);
 
     StringBuilder builder = new StringBuilder();
-    builder.append("myfiles : [ file(" + file1.path() + ") ];\n");
+    builder.append("myfiles : [ file(" + path1 + ") ];\n");
     builder.append("run : myfiles | save(" + dir + ");\n");
     script(builder.toString());
 
@@ -94,7 +91,7 @@ public class FileSetSmoothTest extends IntegrationTestCase {
     // then
     messages.assertNoProblems();
     FakeFileSet resultFiles = fileSet(dir);
-    resultFiles.file(file1.path()).assertContentContainsFilePath();
+    resultFiles.file(path1).assertContentContainsFilePath();
     assertThat(Iterables.size(resultFiles)).isEqualTo(1);
   }
 
@@ -120,10 +117,9 @@ public class FileSetSmoothTest extends IntegrationTestCase {
   @Test
   public void fileSetWithDuplicatedFiles() throws Exception {
     // given
-    FakeFile file1 = file(path("file/path/file1.txt"));
-    file1.createContentWithFilePath();
+    fileSystem.createFileContainingItsPath(path1);
 
-    script("run : [ file(" + file1.path() + "), file(" + file1.path() + ") ];\n");
+    script("run : [ file(" + path1 + "), file(" + path1 + ") ];\n");
 
     // when
     smoothApp.run("run");
