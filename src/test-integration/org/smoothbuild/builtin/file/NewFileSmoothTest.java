fd248780dcd3e7ff597fb96856f85281dbf94797
mikosik <marcin.mikosik@gmail.com>
2013-10-23 12:09:22 +0200

    refactored NewFileSmoothTest

--- src/test-integration/org/smoothbuild/builtin/file/NewFileSmoothTest.java
+++ src/test-integration/org/smoothbuild/builtin/file/NewFileSmoothTest.java
@@ -5,21 +5,22 @@ import static org.smoothbuild.fs.base.Path.path;
 import java.io.IOException;
 
 import org.junit.Test;
+import org.smoothbuild.fs.base.Path;
 import org.smoothbuild.integration.IntegrationTestCase;
-import org.smoothbuild.testing.type.impl.FakeFile;
 
 public class NewFileSmoothTest extends IntegrationTestCase {
 
   @Test
   public void saveFile_pipe() throws IOException {
-    FakeFile file = file(path("file/path/file.txt"));
+    Path path = path("file/path/file.txt");
     String content = "file content";
+    fileSystem.createFileWithContent(path, content);
 
-    script("run : newFile(path=" + file.path() + ", content='" + content + "') | save(dir='.');");
+    script("run : newFile(path=" + path + ", content='" + content + "') | save(dir='.');");
 
     smoothApp.run("run");
 
     messages.assertNoProblems();
-    file.assertContentContains(content);
+    fileSystem.assertFileContains(path, content);
   }
 }
