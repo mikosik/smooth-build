1b7daa9a8688449cc7b3f11251b1dea7027c6373
mikosik <marcin.mikosik@gmail.com>
2013-10-24 11:47:52 +0200

    added CannotAddDuplicatePathError and refactored FileSetBuilder to use it

---
+++ src/java/org/smoothbuild/plugin/api/err/CannotAddDuplicatePathError.java
@@ -0,0 +1,13 @@
+package org.smoothbuild.plugin.api.err;
+
+import static org.smoothbuild.message.message.MessageType.ERROR;
+
+import org.smoothbuild.fs.base.Path;
+import org.smoothbuild.message.message.Message;
+
+public class CannotAddDuplicatePathError extends Message {
+  public CannotAddDuplicatePathError(Path path) {
+    super(ERROR, "Error in function implementation: It tried to add two files with path = " + path
+        + " to File*");
+  }
+}
