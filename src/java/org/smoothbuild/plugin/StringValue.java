6d65dc6e3d654b3869ca7abe19628680d88b1c88
mikosik <marcin.mikosik@gmail.com>
2013-10-24 16:32:05 +0200

    added StringObject/StringValue

---
+++ src/java/org/smoothbuild/plugin/StringValue.java
@@ -0,0 +1,7 @@
+package org.smoothbuild.plugin;
+
+import org.smoothbuild.object.Hashed;
+
+public interface StringValue extends Hashed {
+  public String value();
+}
---
+++ src/test/org/smoothbuild/object/StringObjectTest.java
@@ -0,0 +1,64 @@
+package org.smoothbuild.object;
+
+import static com.google.common.base.Charsets.UTF_8;
+import static org.mockito.Mockito.mock;
+import static org.testory.Testory.given;
+import static org.testory.Testory.thenReturned;
+import static org.testory.Testory.thenThrown;
+import static org.testory.Testory.when;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.junit.Test;
+import org.mockito.BDDMockito;
+import org.testory.common.Closure;
+
+import com.google.common.hash.HashCode;
+
+public class StringObjectTest {
+  String content = "content";
+  InputStream inputStream = mock(InputStream.class);
+  HashedDb hashedDb = mock(HashedDb.class);
+  HashCode hash = HashCode.fromInt(1);
+
+  StringObject stringObject;
+
+  @Test
+  public void null_hash_is_forbidden() throws Exception {
+    when(stringObject(hashedDb, null));
+    thenThrown(NullPointerException.class);
+  }
+
+  @Test
+  public void null_file_system_is_forbidden() throws Exception {
+    when(stringObject(null, hash));
+    thenThrown(NullPointerException.class);
+  }
+
+  @Test
+  public void hash() {
+    given(stringObject = new StringObject(hashedDb, hash));
+    when(stringObject.hash());
+    thenReturned(hash);
+  }
+
+  @Test
+  public void open_input_stream_calls_hashed_db_open_input_stream() throws IOException {
+    BDDMockito.given(hashedDb.openInputStream(hash)).willReturn(
+        new ByteArrayInputStream(content.getBytes(UTF_8)));
+    given(stringObject = new StringObject(hashedDb, hash));
+    when(stringObject.value());
+    thenReturned(content);
+  }
+
+  private static Closure stringObject(final HashedDb hashedDb, final HashCode hash) {
+    return new Closure() {
+      @Override
+      public Object invoke() throws Throwable {
+        return new StringObject(hashedDb, hash);
+      }
+    };
+  }
+}
