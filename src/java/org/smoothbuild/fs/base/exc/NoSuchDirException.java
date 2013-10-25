64fde90a5fcd72d7f794152205f13b5b01a4f65f
mikosik <marcin.mikosik@gmail.com>
2013-10-25 16:44:09 +0200

    refactored NoSuchDirException, NoSuchFileException

--- src/java/org/smoothbuild/fs/base/exc/NoSuchDirException.java
+++ src/java/org/smoothbuild/fs/base/exc/NoSuchDirException.java
@@ -4,12 +4,7 @@ import org.smoothbuild.fs.base.Path;
 
 @SuppressWarnings("serial")
 public class NoSuchDirException extends FileSystemException {
-
   public NoSuchDirException(Path path) {
-    super(message(path));
-  }
-
-  private static String message(Path path) {
-    return "Path " + path + " is not a directory.";
+    super("Path " + path + " is not a directory.");
   }
 }
--- src/java/org/smoothbuild/fs/base/exc/NoSuchFileException.java
+++ src/java/org/smoothbuild/fs/base/exc/NoSuchFileException.java
@@ -5,10 +5,6 @@ import org.smoothbuild.fs.base.Path;
 @SuppressWarnings("serial")
 public class NoSuchFileException extends FileSystemException {
   public NoSuchFileException(Path path) {
-    super(message(path));
-  }
-
-  private static String message(Path path) {
-    return "Cannot find " + path.toString() + " file.";
+    super("Path " + path + " is not a file.");
   }
 }
