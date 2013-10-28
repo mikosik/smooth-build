582d1648ff95d3abfa63a1c22358c06b6f00c7ce
mikosik <marcin.mikosik@gmail.com>
2013-10-28 14:39:30 +0100

    name cleanup around ResultDb

--- src/java/org/smoothbuild/command/SmoothContants.java
+++ src/java/org/smoothbuild/command/SmoothContants.java
@@ -8,5 +8,5 @@ public class SmoothContants {
   public static final Path DEFAULT_SCRIPT = path("build.smooth");
   public static final Path BUILD_DIR = path(".smooth");
   public static final Path VALUE_DB_DIR = BUILD_DIR.append(path("values"));
-  public static final Path TASK_RESULT_DIR = BUILD_DIR.append(path("cache"));
+  public static final Path TASK_DB_DIR = BUILD_DIR.append(path("tasks"));
 }
--- src/java/org/smoothbuild/object/HashedDbWithResults.java
+++ src/java/org/smoothbuild/object/HashedDbWithResults.java
@@ -11,4 +11,4 @@ import com.google.inject.BindingAnnotation;
 @Retention(RUNTIME)
 @Target({ ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD })
 @BindingAnnotation
-public @interface Results {}
+public @interface HashedDbWithResults {}
