dfecf07f2889a6607aa547a7912f06bdb2a34d03
mikosik <marcin.mikosik@gmail.com>
2013-10-28 13:41:57 +0100

    refactored HashTask -> CallHashing

---
+++ src/java/org/smoothbuild/function/nativ/CallHashing.java
@@ -0,0 +1,25 @@
+package org.smoothbuild.function.nativ;
+
+import java.util.Map;
+
+import org.smoothbuild.function.base.Param;
+import org.smoothbuild.hash.Hash;
+import org.smoothbuild.plugin.Value;
+
+import com.google.common.hash.HashCode;
+import com.google.common.hash.Hasher;
+
+public class CallHashing {
+  public static HashCode hashCall(NativeFunction function, Map<String, Value> arguments) {
+    Hasher hasher = Hash.function().newHasher();
+    hasher.putBytes(Hash.string(function.signature().name().full()).asBytes());
+    for (Param param : function.params().values()) {
+      Value argument = arguments.get(param.name());
+      if (argument != null) {
+        hasher.putBytes(param.hash().asBytes());
+        hasher.putBytes(argument.hash().asBytes());
+      }
+    }
+    return hasher.hash();
+  }
+}
