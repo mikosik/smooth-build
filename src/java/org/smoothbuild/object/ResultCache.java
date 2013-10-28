fe7dd648a66b764af7093acc8d67209d60763c87
mikosik <marcin.mikosik@gmail.com>
2013-10-28 14:15:48 +0100

    merged TaskResultDb into ResultCache

--- src/java/org/smoothbuild/object/ResultCache.java
+++ src/java/org/smoothbuild/object/ResultCache.java
@@ -2,43 +2,83 @@ package org.smoothbuild.object;
 
 import javax.inject.Inject;
 
+import org.smoothbuild.plugin.File;
+import org.smoothbuild.plugin.FileSet;
+import org.smoothbuild.plugin.StringSet;
+import org.smoothbuild.plugin.StringValue;
+import org.smoothbuild.plugin.Value;
+
 import com.google.common.hash.HashCode;
 
 public class ResultCache {
-  private final TaskResultDb taskResultDb;
+  private static final int FILE_SET_FLAG = 1;
+  private static final int STRING_SET_FLAG = 2;
+  private static final int FILE_FLAG = 3;
+  private static final int STRING_FLAG = 4;
+
+  private final HashedDb taskResultDb;
   private final ObjectDb objectDb;
 
   @Inject
-  public ResultCache(TaskResultDb taskResultDb, ObjectDb objectDb) {
-    this.taskResultDb = taskResultDb;
+  public ResultCache(@TaskResults HashedDb taskResultsDb, ObjectDb objectDb) {
+    this.taskResultDb = taskResultsDb;
     this.objectDb = objectDb;
   }
 
-  public void store(HashCode taskHash, HashCode resultObjectHash) {
-    taskResultDb.store(taskHash, resultObjectHash);
+  public void store(HashCode taskHash, Value value) {
+    // TODO remove once null values are forbidden
+    if (value == null) {
+      return;
+    }
+
+    Marshaller marshaller = new Marshaller();
+    marshaller.addInt(flagFor(value));
+    marshaller.addHash(value.hash());
+
+    taskResultDb.store(taskHash, marshaller.getBytes());
   }
 
   public boolean contains(HashCode taskHash) {
     return taskResultDb.contains(taskHash);
   }
 
-  public FileSetObject readFileSet(HashCode taskHash) {
-    return objectDb.fileSet(resultHash(taskHash));
+  public Value read(HashCode taskHash) {
+    try (Unmarshaller unmarshaller = new Unmarshaller(taskResultDb, taskHash);) {
+      int flag = unmarshaller.readInt();
+      HashCode resultObjectHash = unmarshaller.readHash();
+      return readValue(flag, resultObjectHash);
+    }
   }
 
-  public StringSetObject readStringSet(HashCode taskHash) {
-    return objectDb.stringSet(resultHash(taskHash));
+  private Value readValue(int flag, HashCode resultObjectHash) {
+    switch (flag) {
+      case FILE_SET_FLAG:
+        return objectDb.fileSet(resultObjectHash);
+      case STRING_SET_FLAG:
+        return objectDb.stringSet(resultObjectHash);
+      case FILE_FLAG:
+        return objectDb.file(resultObjectHash);
+      case STRING_FLAG:
+        return objectDb.string(resultObjectHash);
+      default:
+        throw new RuntimeException("Internal error in smooth binary. Unknown value flag = " + flag);
+    }
   }
 
-  public FileObject readFile(HashCode taskHash) {
-    return objectDb.file(resultHash(taskHash));
-  }
-
-  public StringObject readString(HashCode taskHash) {
-    return objectDb.string(resultHash(taskHash));
-  }
-
-  private HashCode resultHash(HashCode taskHash) {
-    return taskResultDb.read(taskHash);
+  private static int flagFor(Value value) {
+    if (value instanceof FileSet) {
+      return FILE_SET_FLAG;
+    }
+    if (value instanceof StringSet) {
+      return STRING_SET_FLAG;
+    }
+    if (value instanceof File) {
+      return FILE_FLAG;
+    }
+    if (value instanceof StringValue) {
+      return STRING_FLAG;
+    }
+    throw new RuntimeException("Internal error in smooth binary. Unknown value type = "
+        + value.getClass().getName());
   }
 }
