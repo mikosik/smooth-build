dbc5265d06908d6ac8878b6716e0fdede2f41713
mikosik <marcin.mikosik@gmail.com>
2013-10-30 21:46:21 +0100

    removed unused CallLocation

--- src/testing/org/smoothbuild/testing/message/FakeCallLocation.java
+++
@@ -1,9 +0,0 @@
-package org.smoothbuild.testing.message;
-
-import org.smoothbuild.message.message.CallLocation;
-
-public class FakeCallLocation extends CallLocation {
-  public FakeCallLocation() {
-    super("name", new FakeCodeLocation());
-  }
-}
