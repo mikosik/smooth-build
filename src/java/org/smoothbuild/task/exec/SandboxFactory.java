dffdedb301c0fcf67b2464886485af65454fe499
mikosik <marcin.mikosik@gmail.com>
2013-10-31 13:14:25 +0100

    refactored out SandboxFactory from TaskExecutor

---
+++ src/java/org/smoothbuild/task/exec/SandboxFactory.java
@@ -0,0 +1,22 @@
+package org.smoothbuild.task.exec;
+
+import javax.inject.Inject;
+
+import org.smoothbuild.db.ValueDb;
+import org.smoothbuild.fs.base.FileSystem;
+import org.smoothbuild.task.base.LocatedTask;
+
+public class SandboxFactory {
+  private final FileSystem fileSystem;
+  private final ValueDb valueDb;
+
+  @Inject
+  public SandboxFactory(FileSystem fileSystem, ValueDb valueDb) {
+    this.fileSystem = fileSystem;
+    this.valueDb = valueDb;
+  }
+
+  public SandboxImpl createSandbox(LocatedTask task) {
+    return new SandboxImpl(fileSystem, valueDb, task.name(), task.codeLocation());
+  }
+}
--- src/test/org/smoothbuild/task/exec/TaskExecutorTest.java
+++ src/test/org/smoothbuild/task/exec/TaskExecutorTest.java
@@ -4,51 +4,28 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 
 import org.junit.Test;
-import org.mockito.BDDMockito;
-import org.mockito.Matchers;
 import org.mockito.Mockito;
-import org.mockito.invocation.InvocationOnMock;
-import org.mockito.stubbing.Answer;
-import org.smoothbuild.db.ValueDb;
-import org.smoothbuild.message.message.Message;
-import org.smoothbuild.message.message.MessageType;
-import org.smoothbuild.plugin.Sandbox;
+import org.smoothbuild.message.listen.MessageGroup;
 import org.smoothbuild.plugin.Value;
 import org.smoothbuild.task.base.LocatedTask;
-import org.smoothbuild.testing.db.FakeObjectDb;
-import org.smoothbuild.testing.fs.base.FakeFileSystem;
-import org.smoothbuild.testing.message.FakeCodeLocation;
 import org.smoothbuild.testing.message.FakeUserConsole;
 
 public class TaskExecutorTest {
   LocatedTask task = mock(LocatedTask.class);
   Value value = mock(Value.class);
-  FakeFileSystem fileSystem = new FakeFileSystem();
   FakeUserConsole userConsole = new FakeUserConsole();
-  ValueDb valueDb = new FakeObjectDb(fileSystem);
+  MessageGroup messageGroup = new MessageGroup("name");
+  SandboxImpl sandbox = mock(SandboxImpl.class);
+  SandboxFactory sandboxFactory = mock(SandboxFactory.class);
 
-  TaskExecutor taskExecutor = new TaskExecutor(fileSystem, valueDb, userConsole);
+  TaskExecutor taskExecutor = new TaskExecutor(sandboxFactory, userConsole);
 
   @Test
   public void execute_invokes_task_execute() {
-    Mockito.when(task.execute(Matchers.<Sandbox> any())).thenReturn(value);
+    Mockito.when(sandboxFactory.createSandbox(task)).thenReturn(sandbox);
+    Mockito.when(sandbox.messageGroup()).thenReturn(messageGroup);
+    Mockito.when(task.execute(sandbox)).thenReturn(value);
+
     assertThat(taskExecutor.execute(task)).isEqualTo(value);
-  }
-
-  @Test
-  public void message_can_be_reported_via_sandbox() throws Exception {
-    BDDMockito.given(task.codeLocation()).willReturn(new FakeCodeLocation());
-    Mockito.doAnswer(new Answer<Void>() {
-      @Override
-      public Void answer(InvocationOnMock invocation) throws Throwable {
-        Sandbox sandbox = (Sandbox) invocation.getArguments()[0];
-        sandbox.report(new Message(MessageType.ERROR, "message"));
-        return null;
-      }
-    }).when(task).execute(Matchers.<Sandbox> any());
-
-    taskExecutor.execute(task);
-
-    userConsole.assertProblemsFound();
   }
 }
