1cfcbca4c569fdc948948e8dd4242e21a4014b63
mikosik <marcin.mikosik@gmail.com>
2013-10-31 13:14:25 +0100

    refactored out SandboxFactory from TaskExecutor

--- src/java/org/smoothbuild/task/exec/TaskExecutor.java
+++ src/java/org/smoothbuild/task/exec/TaskExecutor.java
@@ -2,32 +2,24 @@ package org.smoothbuild.task.exec;
 
 import javax.inject.Inject;
 
-import org.smoothbuild.db.ValueDb;
-import org.smoothbuild.fs.base.FileSystem;
 import org.smoothbuild.message.listen.UserConsole;
 import org.smoothbuild.plugin.Value;
 import org.smoothbuild.task.base.LocatedTask;
 
 public class TaskExecutor {
-  private final FileSystem fileSystem;
-  private final ValueDb valueDb;
+  private final SandboxFactory sandboxFactory;
   private final UserConsole userConsole;
 
   @Inject
-  public TaskExecutor(FileSystem fileSystem, ValueDb valueDb, UserConsole userConsole) {
-    this.fileSystem = fileSystem;
-    this.valueDb = valueDb;
+  public TaskExecutor(SandboxFactory sandboxFactory, UserConsole userConsole) {
+    this.sandboxFactory = sandboxFactory;
     this.userConsole = userConsole;
   }
 
   public Value execute(LocatedTask task) {
-    SandboxImpl sandbox = createSandbox(task);
+    SandboxImpl sandbox = sandboxFactory.createSandbox(task);
     Value result = task.execute(sandbox);
     userConsole.report(sandbox.messageGroup());
     return result;
-  }
-
-  private SandboxImpl createSandbox(LocatedTask task) {
-    return new SandboxImpl(fileSystem, valueDb, task.name(), task.codeLocation());
   }
 }
