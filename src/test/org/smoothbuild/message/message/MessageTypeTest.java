e20c407b1b6f5f5bbd08c4818334c20e302d533c
mikosik <marcin.mikosik@gmail.com>
2013-11-01 10:10:55 +0100

    added MessageType.namePlural()

---
+++ src/test/org/smoothbuild/message/message/MessageTypeTest.java
@@ -0,0 +1,37 @@
+package org.smoothbuild.message.message;
+
+import static org.smoothbuild.message.message.MessageType.ERROR;
+import static org.smoothbuild.message.message.MessageType.INFO;
+import static org.smoothbuild.message.message.MessageType.SUGGESTION;
+import static org.smoothbuild.message.message.MessageType.WARNING;
+import static org.testory.Testory.thenReturned;
+import static org.testory.Testory.when;
+
+import org.junit.Test;
+
+public class MessageTypeTest {
+
+  @Test
+  public void name_plural_of_error() {
+    when(ERROR.namePlural());
+    thenReturned("errors");
+  }
+
+  @Test
+  public void name_plural_of_warning() {
+    when(WARNING.namePlural());
+    thenReturned("warnings");
+  }
+
+  @Test
+  public void name_plural_of_suggestion() {
+    when(SUGGESTION.namePlural());
+    thenReturned("suggestions");
+  }
+
+  @Test
+  public void name_plural_of_info() {
+    when(INFO.namePlural());
+    thenReturned("infos");
+  }
+}
