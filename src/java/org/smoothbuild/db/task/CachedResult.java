8b30d490bd03a17f9f7d19193dd06e0eee71ef2b
mikosik <marcin.mikosik@gmail.com>
2013-11-03 13:11:00 +0100

    added CachedResult

---
+++ src/java/org/smoothbuild/db/task/CachedResult.java
@@ -0,0 +1,24 @@
+package org.smoothbuild.db.task;
+
+import org.smoothbuild.message.message.Message;
+import org.smoothbuild.plugin.Value;
+
+import com.google.common.collect.ImmutableList;
+
+public class CachedResult {
+  private final Value value;
+  private final ImmutableList<Message> messages;
+
+  public CachedResult(Value result, Iterable<Message> messages) {
+    this.value = result;
+    this.messages = ImmutableList.copyOf(messages);
+  }
+
+  public Value value() {
+    return value;
+  }
+
+  public ImmutableList<Message> messages() {
+    return messages;
+  }
+}
