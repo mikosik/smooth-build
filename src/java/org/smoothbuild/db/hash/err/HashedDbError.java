177314dba140389646c1dd31ec8caa87dca9a774
mikosik <marcin.mikosik@gmail.com>
2013-11-04 14:04:46 +0100

    changed severity of HashedDbError to FATAL

--- src/java/org/smoothbuild/db/hash/err/HashedDbError.java
+++ src/java/org/smoothbuild/db/hash/err/HashedDbError.java
@@ -1,11 +1,11 @@
 package org.smoothbuild.db.hash.err;
 
-import static org.smoothbuild.message.message.MessageType.ERROR;
+import static org.smoothbuild.message.message.MessageType.FATAL;
 
 import org.smoothbuild.message.message.Message;
 
 public class HashedDbError extends Message {
   public HashedDbError(String message) {
-    super(ERROR, "Internal error in smooth hashed DB:\n" + message);
+    super(FATAL, "Internal error in smooth hashed DB:\n" + message);
   }
 }
