ffdfeba4616ff44e8634d412d3b9326009eedd3a
mikosik <marcin.mikosik@gmail.com>
2013-11-04 14:12:38 +0100

    added MessageType.isProblem()

--- src/java/org/smoothbuild/message/message/MessageType.java
+++ src/java/org/smoothbuild/message/message/MessageType.java
@@ -3,7 +3,17 @@ package org.smoothbuild.message.message;
 import com.google.common.base.Ascii;
 
 public enum MessageType {
-  FATAL, ERROR, WARNING, SUGGESTION, INFO;
+  FATAL(true), ERROR(true), WARNING(false), SUGGESTION(false), INFO(false);
+
+  private final boolean isProblem;
+
+  private MessageType(boolean isProblem) {
+    this.isProblem = isProblem;
+  }
+
+  public boolean isProblem() {
+    return isProblem;
+  }
 
   public String namePlural() {
     return Ascii.toLowerCase(name()) + "s";
