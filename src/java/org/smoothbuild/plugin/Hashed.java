0644377cdd2a35c177efd06b348681522205d0b0
mikosik <marcin.mikosik@gmail.com>
2013-11-04 14:30:10 +0100

    refactored out Hashed interface from Value interface

---
+++ src/java/org/smoothbuild/plugin/Hashed.java
@@ -0,0 +1,7 @@
+package org.smoothbuild.plugin;
+
+import com.google.common.hash.HashCode;
+
+public interface Hashed {
+  public HashCode hash();
+}
--- src/java/org/smoothbuild/plugin/Value.java
+++ src/java/org/smoothbuild/plugin/Value.java
@@ -2,13 +2,9 @@ package org.smoothbuild.plugin;
 
 import org.smoothbuild.function.base.Type;
 
-import com.google.common.hash.HashCode;
-
 /**
  * A value in smooth language.
  */
-public interface Value {
+public interface Value extends Hashed {
   public Type type();
-
-  public HashCode hash();
 }
