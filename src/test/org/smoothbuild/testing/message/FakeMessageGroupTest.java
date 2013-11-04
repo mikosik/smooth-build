0e2eca594a2c5e89a04201e72b670c4eef32168e
mikosik <marcin.mikosik@gmail.com>
2013-11-04 10:19:13 +0100

    refactored FakeMessageGroup

--- src/test/org/smoothbuild/testing/message/FakeMessageGroupTest.java
+++ src/test/org/smoothbuild/testing/message/FakeMessageGroupTest.java
@@ -1,7 +1,9 @@
 package org.smoothbuild.testing.message;
 
 import static org.smoothbuild.message.message.MessageType.ERROR;
+import static org.smoothbuild.message.message.MessageType.FATAL;
 import static org.smoothbuild.message.message.MessageType.INFO;
+import static org.smoothbuild.message.message.MessageType.SUGGESTION;
 import static org.smoothbuild.message.message.MessageType.WARNING;
 
 import org.junit.Test;
@@ -12,42 +14,58 @@ public class FakeMessageGroupTest {
   FakeMessageGroup testingProblemListener = new FakeMessageGroup();
   CodeLocation location = new FakeCodeLocation();
 
+  // assertProblemsFound()
+
   @Test(expected = AssertionError.class)
-  public void problemsFoundFailsWhenNothingFound() throws Exception {
+  public void assert_problems_found_fails_when_nothing_was_reported() throws Exception {
     testingProblemListener.assertProblemsFound();
   }
 
   @Test(expected = AssertionError.class)
-  public void problemsFoundFailsWhenInfoWasReported() throws Exception {
+  public void assert_problems_found_fails_when_info_was_reported() throws Exception {
     testingProblemListener.report(new Message(INFO, "message"));
     testingProblemListener.assertProblemsFound();
   }
 
+  @Test(expected = AssertionError.class)
+  public void assert_problems_found_fails_when_suggestion_was_reported() throws Exception {
+    testingProblemListener.report(new Message(SUGGESTION, "message"));
+    testingProblemListener.assertProblemsFound();
+  }
+
+  @Test(expected = AssertionError.class)
+  public void assert_problems_found_fails_when_warning_was_reported() throws Exception {
+    testingProblemListener.report(new Message(WARNING, "message"));
+    testingProblemListener.assertProblemsFound();
+  }
+
   @Test
-  public void problemsFoundSucceedsWhenErrorWasReported() throws Exception {
+  public void assert_problems_found_succeeds_when_error_was_reported() throws Exception {
     testingProblemListener.report(new Message(ERROR, "message"));
     testingProblemListener.assertProblemsFound();
   }
 
   @Test
-  public void problemsFoundSucceedsWhenWarningWasReported() throws Exception {
-    testingProblemListener.report(new Message(WARNING, "message"));
+  public void assert_problems_found_succeeds_when_fatal_was_reported() throws Exception {
+    testingProblemListener.report(new Message(FATAL, "message"));
     testingProblemListener.assertProblemsFound();
   }
 
+  // assertOnlyInfo()
+
   @Test
-  public void assertingThatOnlyOneInfoExistsSucceedsWhenOneExists() {
+  public void assert_only_info_succeeds_when_one_info_was_reported() {
     testingProblemListener.report(new MyInfo());
     testingProblemListener.assertOnlyInfo(MyInfo.class);
   }
 
   @Test(expected = AssertionError.class)
-  public void assertingThatOnlyOneInfoExistsFailsWhenNoOneExists() {
+  public void assert_only_info_fails_when_nothing_was_reported() {
     testingProblemListener.assertOnlyInfo(MyInfo.class);
   }
 
   @Test(expected = AssertionError.class)
-  public void assertingThatOnlyOneInfoExistsFailsWhenTwoProblemsExist() {
+  public void assert_only_info_fails_when_two_infos_were_reported() {
     testingProblemListener.report(new MyInfo());
     testingProblemListener.report(new MyInfo());
 
@@ -55,44 +73,65 @@ public class FakeMessageGroupTest {
   }
 
   @Test(expected = AssertionError.class)
-  public void assertingThatOnlyOneInfoExistsFailsWhenOneProblemOfWrongTypeExists() {
+  public void assert_only_info_fails_when_info_of_different_type_was_reported() {
     testingProblemListener.report(new Message(INFO, "message"));
     testingProblemListener.assertOnlyInfo(MyInfo.class);
   }
 
-  @Test
-  public void assertingThatOnlyOneProblemExistsSucceedsWhenOneExists() {
-    testingProblemListener.report(new MyProblem());
-    testingProblemListener.assertOnlyProblem(MyProblem.class);
-  }
-
   @Test(expected = AssertionError.class)
-  public void assertingThatOnlyOneProblemExistsFailsWhenNoOneExists() {
-    testingProblemListener.assertOnlyProblem(MyProblem.class);
-  }
-
-  @Test(expected = AssertionError.class)
-  public void assertingThatOnlyOneProblemExistsFailsWhenTwoProblemsExist() {
-    testingProblemListener.report(new MyProblem());
-    testingProblemListener.report(new MyProblem());
-
-    testingProblemListener.assertOnlyProblem(MyProblem.class);
-  }
-
-  @Test(expected = AssertionError.class)
-  public void assertingThatOnlyOneProblemExistsFailsWhenOneProblemOfWrongTypeExists() {
+  public void assert_only_info_fails_when_warning_was_reported() {
     testingProblemListener.report(new Message(WARNING, "message"));
+    testingProblemListener.assertOnlyInfo(MyInfo.class);
+  }
+
+  // assertOnlyProblem()
+
+  @Test
+  public void assert_only_problem_succeeds_when_one_problem_was_reported() {
+    testingProblemListener.report(new MyProblem());
+    testingProblemListener.assertOnlyProblem(MyProblem.class);
+  }
+
+  @Test(expected = AssertionError.class)
+  public void assert_only_problem_fails_when_nothing_was_reported() {
+    testingProblemListener.assertOnlyProblem(MyProblem.class);
+  }
+
+  @Test(expected = AssertionError.class)
+  public void assert_only_problem_fails_when_two_problems_were_reported() {
+    testingProblemListener.report(new MyProblem());
+    testingProblemListener.report(new MyProblem());
 
     testingProblemListener.assertOnlyProblem(MyProblem.class);
   }
 
+  @Test(expected = AssertionError.class)
+  public void assert_only_problem_fails_when_problem_of_different_type_was_reported() {
+    testingProblemListener.report(new Message(ERROR, "message"));
+    testingProblemListener.assertOnlyProblem(MyProblem.class);
+  }
+
+  @Test(expected = AssertionError.class)
+  public void assert_only_problem_fails_when_info_was_reported() {
+    testingProblemListener.report(new Message(INFO, "message"));
+    testingProblemListener.assertOnlyProblem(MyProblem.class);
+  }
+
+  // assertNoProblems()
+
   @Test
-  public void assertingThatNoProblemExistsSucceedsWhenZeroExists() throws Exception {
+  public void assert_no_problems_succeeds_when_nothing_was_reported() throws Exception {
+    testingProblemListener.assertNoProblems();
+  }
+
+  @Test
+  public void assert_no_problems_succeeds_when_warning_was_reported() throws Exception {
+    testingProblemListener.report(new Message(WARNING, "message"));
     testingProblemListener.assertNoProblems();
   }
 
   @Test(expected = AssertionError.class)
-  public void assertingThatNoProblemExistsFailsWhenOneExist() throws Exception {
+  public void assert_no_problems_fails_when_on_problem_was_reported() throws Exception {
     testingProblemListener.report(new MyProblem());
     testingProblemListener.assertNoProblems();
   }
--- src/testing/org/smoothbuild/testing/message/FakeMessageGroup.java
+++ src/testing/org/smoothbuild/testing/message/FakeMessageGroup.java
@@ -1,18 +1,19 @@
 package org.smoothbuild.testing.message;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.smoothbuild.message.message.MessageType.ERROR;
+import static org.smoothbuild.message.message.MessageType.FATAL;
 
 import java.util.List;
 
 import org.smoothbuild.message.listen.MessageGroup;
 import org.smoothbuild.message.message.Message;
-import org.smoothbuild.message.message.MessageType;
 
 import com.google.common.collect.Lists;
 
 public class FakeMessageGroup extends MessageGroup {
   private final List<Message> problems = Lists.newArrayList();
-  private final List<Message> infos = Lists.newArrayList();
+  private final List<Message> nonProblems = Lists.newArrayList();
 
   public FakeMessageGroup() {
     super("TestMessageGroup");
@@ -20,10 +21,10 @@ public class FakeMessageGroup extends MessageGroup {
 
   @Override
   public void report(Message message) {
-    if (message.type() == MessageType.INFO) {
-      infos.add(message);
-    } else {
+    if (message.type() == FATAL || message.type() == ERROR) {
       problems.add(message);
+    } else {
+      nonProblems.add(message);
     }
     super.report(message);
   }
@@ -33,10 +34,10 @@ public class FakeMessageGroup extends MessageGroup {
   }
 
   public void assertOnlyInfo(Class<? extends Message> klass) {
-    if (infos.size() != 1) {
-      throw new AssertionError("Expected one info ,\nbut got:\n" + infos.toString());
+    if (nonProblems.size() != 1) {
+      throw new AssertionError("Expected one info ,\nbut got:\n" + nonProblems.toString());
     }
-    assertThat(infos.get(0)).isInstanceOf(klass);
+    assertThat(nonProblems.get(0)).isInstanceOf(klass);
   }
 
   public void assertOnlyProblem(Class<? extends Message> klass) {
