ea8870cd0580020814f546c955008f38591f4560
mikosik <marcin.mikosik@gmail.com>
2013-11-06 13:23:05 +0100

    refactored NativeCallHasher

--- src/java/org/smoothbuild/task/base/NativeCallHasher.java
+++ src/java/org/smoothbuild/task/base/NativeCallHasher.java
@@ -3,25 +3,25 @@ package org.smoothbuild.task.base;
 import java.util.Map;
 
 import org.smoothbuild.db.hash.Hash;
+import org.smoothbuild.function.base.Function;
 import org.smoothbuild.function.base.Param;
-import org.smoothbuild.function.nativ.NativeFunction;
 
 import com.google.common.hash.HashCode;
 import com.google.common.hash.Hasher;
 
 public class NativeCallHasher {
-  private final NativeFunction nativeFunction;
+  private final Function function;
   private final Map<String, Result> args;
 
-  public NativeCallHasher(NativeFunction nativeFunction, Map<String, Result> args) {
-    this.nativeFunction = nativeFunction;
+  public NativeCallHasher(Function function, Map<String, Result> args) {
+    this.function = function;
     this.args = args;
   }
 
   public HashCode hash() {
     Hasher hasher = Hash.function().newHasher();
-    hasher.putBytes(Hash.string(nativeFunction.signature().name().value()).asBytes());
-    for (Param param : nativeFunction.params().values()) {
+    hasher.putBytes(Hash.string(function.signature().name().value()).asBytes());
+    for (Param param : function.params().values()) {
       Result argument = args.get(param.name());
       if (argument != null) {
         hasher.putBytes(Hash.string(param.name()).asBytes());
