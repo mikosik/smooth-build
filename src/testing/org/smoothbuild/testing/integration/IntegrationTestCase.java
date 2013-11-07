167484c394b8387f1b4abc1cf1f4a2fbc13534b9
mikosik <marcin.mikosik@gmail.com>
2013-11-07 16:56:27 +0100

    refactored IntegrationTestCase so it extends AbstractModule and tests that extend it can alter configure guice configuration

--- src/testing/org/smoothbuild/testing/integration/IntegrationTestCase.java
+++ src/testing/org/smoothbuild/testing/integration/IntegrationTestCase.java
@@ -11,16 +11,20 @@ import org.smoothbuild.testing.fs.base.FakeFileSystem;
 import org.smoothbuild.testing.message.FakeUserConsole;
 import org.smoothbuild.testing.parse.ScriptBuilder;
 
+import com.google.inject.AbstractModule;
 import com.google.inject.Injector;
+import com.google.inject.Module;
+import com.google.inject.util.Modules;
 
-public class IntegrationTestCase {
+public class IntegrationTestCase extends AbstractModule {
   protected FakeFileSystem fileSystem;
   protected SmoothApp smoothApp;
   protected FakeUserConsole userConsole;
 
   @Before
   public void before() {
-    Injector injector = createInjector(new IntegrationTestModule());
+    Module module = Modules.override(new IntegrationTestModule()).with(this);
+    Injector injector = createInjector(module);
     fileSystem = injector.getInstance(FakeFileSystem.class);
     userConsole = injector.getInstance(FakeUserConsole.class);
     smoothApp = injector.getInstance(SmoothApp.class);
@@ -29,4 +33,7 @@ public class IntegrationTestCase {
   public void script(String script) throws IOException {
     fileSystem.createFile(DEFAULT_SCRIPT, ScriptBuilder.script(script));
   }
+
+  @Override
+  protected void configure() {}
 }
