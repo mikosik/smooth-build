f9334283bb697720d40d36c3aa35a5df10bee811
mikosik <marcin.mikosik@gmail.com>
2013-11-07 14:11:19 +0100

    added Module.containsFunction()

--- src/java/org/smoothbuild/function/base/Module.java
+++ src/java/org/smoothbuild/function/base/Module.java
@@ -3,6 +3,7 @@ package org.smoothbuild.function.base;
 import com.google.common.collect.ImmutableSet;
 
 public interface Module {
+  public boolean containsFunction(Name name);
 
   public Function getFunction(Name name);
 
--- src/test/org/smoothbuild/function/base/ImmutableModuleTest.java
+++ src/test/org/smoothbuild/function/base/ImmutableModuleTest.java
@@ -1,14 +1,16 @@
 package org.smoothbuild.function.base;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.smoothbuild.function.base.Name.name;
-
-import java.util.Map;
+import static org.testory.Testory.given;
+import static org.testory.Testory.thenReturned;
+import static org.testory.Testory.when;
 
 import org.junit.Test;
+import org.smoothbuild.util.Empty;
 
 import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
 
 public class ImmutableModuleTest {
   Name name1 = name("name1");
@@ -16,25 +18,40 @@ public class ImmutableModuleTest {
   Function function1 = mock(Function.class);
   Function function2 = mock(Function.class);
 
-  Map<Name, Function> map = ImmutableMap.of(name1, function1, name2, function2);
+  ImmutableModule module;
 
   @Test
-  public void getFunction() {
-    Module immutableModule = new ImmutableModule(map);
-
-    assertThat(immutableModule.getFunction(name1)).isSameAs(function1);
-    assertThat(immutableModule.getFunction(name2)).isSameAs(function2);
+  public void empty_immutable_module_does_not_contain_function() throws Exception {
+    given(module = new ImmutableModule(Empty.nameToFunctionMap()));
+    when(module.containsFunction(name1));
+    thenReturned(false);
   }
 
   @Test
-  public void nullReturnedWhenFunctionDoesNotExist() throws Exception {
-    Module immutableModule = new ImmutableModule(ImmutableMap.<Name, Function> of());
-    assertThat(immutableModule.getFunction(name1)).isNull();
+  public void contains_function_returns_true_when_module_contains_function() throws Exception {
+    given(module = new ImmutableModule(ImmutableMap.of(name1, function1)));
+    when(module.containsFunction(name1));
+    thenReturned(true);
   }
 
   @Test
-  public void availableNames() throws Exception {
-    Module immutableModule = new ImmutableModule(map);
-    assertThat(immutableModule.availableNames()).containsOnly(name1, name2);
+  public void get_function_returns_function_with_given_name() {
+    given(module = new ImmutableModule(ImmutableMap.of(name1, function1, name2, function2)));
+    when(module.getFunction(name1));
+    thenReturned(function1);
+  }
+
+  @Test
+  public void get_function_returns_null_when_no_function_with_given_name_exists() throws Exception {
+    given(module = new ImmutableModule(ImmutableMap.of(name1, function1)));
+    when(module.getFunction(name2));
+    thenReturned(null);
+  }
+
+  @Test
+  public void available_names_returns_all_function_names() throws Exception {
+    given(module = new ImmutableModule(ImmutableMap.of(name1, function1, name2, function2)));
+    when(module.availableNames());
+    thenReturned(ImmutableSet.of(name1, name2));
   }
 }
