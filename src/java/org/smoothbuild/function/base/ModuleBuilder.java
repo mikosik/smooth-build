eeeb0a98dd8c7d92e76b4878d6e6ef33d76089cc
mikosik <marcin.mikosik@gmail.com>
2013-11-07 14:19:49 +0100

    added ModuleBuilder

---
+++ src/java/org/smoothbuild/function/base/ModuleBuilder.java
@@ -0,0 +1,43 @@
+package org.smoothbuild.function.base;
+
+import java.util.Map;
+
+import javax.inject.Inject;
+
+import org.smoothbuild.db.task.TaskDb;
+import org.smoothbuild.function.nativ.NativeFunction;
+
+import com.google.common.collect.Maps;
+
+public class ModuleBuilder {
+  private final TaskDb taskDb;
+  private final Map<Name, Function> functions;
+
+  @Inject
+  public ModuleBuilder(TaskDb taskDb) {
+    this.taskDb = taskDb;
+    this.functions = Maps.newHashMap();
+  }
+
+  public void addFunction(NativeFunction function) {
+    Name name = function.name();
+    if (functions.containsKey(name)) {
+      throw new IllegalArgumentException("Function " + name
+          + " has been already added to this module.");
+    } else {
+      functions.put(name, makeCacheable(function));
+    }
+  }
+
+  private Function makeCacheable(NativeFunction function) {
+    if (function.isCacheable()) {
+      return new CachableFunction(taskDb, function);
+    } else {
+      return function;
+    }
+  }
+
+  public Module build() {
+    return new ImmutableModule(functions);
+  }
+}
