a5fcd75608515ce56a27cd5ac97b73c03b3df315
mikosik <marcin.mikosik@gmail.com>
2013-11-09 12:40:08 +0100

    extracted ArtifactBuilder out of SmoothExecutor

---
+++ src/java/org/smoothbuild/task/exec/ArtifactBuilder.java
@@ -0,0 +1,52 @@
+package org.smoothbuild.task.exec;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.smoothbuild.function.base.Function;
+import org.smoothbuild.message.message.CodeLocation;
+import org.smoothbuild.task.base.Result;
+import org.smoothbuild.task.base.Task;
+import org.smoothbuild.task.base.Taskable;
+import org.smoothbuild.util.Empty;
+
+import com.google.common.collect.Lists;
+
+public class ArtifactBuilder {
+  private final TaskGenerator taskGenerator;
+  private final List<Result> artifacts = Lists.newArrayList();
+
+  @Inject
+  public ArtifactBuilder(TaskGenerator taskGenerator) {
+    this.taskGenerator = taskGenerator;
+  }
+
+  public void addArtifact(Function function) {
+    artifacts.add(taskGenerator.generateTask(new TaskableCall(function)));
+  }
+
+  public void runBuild() {
+    try {
+      for (Result result : artifacts) {
+        result.result();
+      }
+    } catch (BuildInterruptedException e) {
+      // Nothing to do. Just quit the build process.
+    }
+  }
+
+  private static class TaskableCall implements Taskable {
+    private final Function function;
+
+    public TaskableCall(Function function) {
+      this.function = function;
+    }
+
+    @Override
+    public Task generateTask(TaskGenerator taskGenerator) {
+      CodeLocation ignoredCodeLocation = null;
+      return function.generateTask(taskGenerator, Empty.stringTaskResultMap(), ignoredCodeLocation);
+    }
+  }
+}
--- src/java/org/smoothbuild/task/exec/SmoothExecutor.java
+++ src/java/org/smoothbuild/task/exec/SmoothExecutor.java
@@ -7,19 +7,14 @@ import org.smoothbuild.function.base.Function;
 import org.smoothbuild.function.base.Module;
 import org.smoothbuild.function.base.Name;
 import org.smoothbuild.message.listen.ErrorMessageException;
-import org.smoothbuild.message.message.CodeLocation;
-import org.smoothbuild.task.base.Result;
-import org.smoothbuild.task.base.Task;
-import org.smoothbuild.task.base.Taskable;
 import org.smoothbuild.task.exec.err.UnknownFunctionError;
-import org.smoothbuild.util.Empty;
 
 public class SmoothExecutor {
-  private final TaskGenerator taskGenerator;
+  private final ArtifactBuilder artifactBuilder;
 
   @Inject
-  public SmoothExecutor(TaskGenerator taskGenerator) {
-    this.taskGenerator = taskGenerator;
+  public SmoothExecutor(ArtifactBuilder artifactBuilder) {
+    this.artifactBuilder = artifactBuilder;
   }
 
   public void execute(ExecutionData executionData) {
@@ -31,25 +26,7 @@ public class SmoothExecutor {
     if (function == null) {
       throw new ErrorMessageException(new UnknownFunctionError(name, module.availableNames()));
     }
-    Result result = taskGenerator.generateTask(new TaskableCall(function));
-    try {
-      result.result();
-    } catch (BuildInterruptedException e) {
-      // Nothing to do. Just quit the build process.
-    }
-  }
-
-  private static class TaskableCall implements Taskable {
-    private final Function function;
-
-    public TaskableCall(Function function) {
-      this.function = function;
-    }
-
-    @Override
-    public Task generateTask(TaskGenerator taskGenerator) {
-      CodeLocation ignoredCodeLocation = null;
-      return function.generateTask(taskGenerator, Empty.stringTaskResultMap(), ignoredCodeLocation);
-    }
+    artifactBuilder.addArtifact(function);
+    artifactBuilder.runBuild();
   }
 }
