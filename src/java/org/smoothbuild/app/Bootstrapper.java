60d49a69106ad1a60d9abc85d06ec8e345c790b7
mikosik <marcin.mikosik@gmail.com>
2013-11-13 20:12:20 +0100

    refactored Bootstrapper

--- src/java/org/smoothbuild/app/Bootstrapper.java
+++ src/java/org/smoothbuild/app/Bootstrapper.java
@@ -3,12 +3,21 @@ package org.smoothbuild.app;
 import org.smoothbuild.builtin.BuiltinModule;
 import org.smoothbuild.db.DbModule;
 
+import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 
 public class Bootstrapper {
   public static <T> T bootstrap(Class<T> klass) {
-    Injector injector = Guice.createInjector(new DbModule(), new BuiltinModule());
+    Injector injector = Guice.createInjector(new MainModule());
     return injector.getInstance(klass);
+  }
+
+  public static class MainModule extends AbstractModule {
+    @Override
+    protected void configure() {
+      install(new DbModule());
+      install(new BuiltinModule());
+    }
   }
 }
