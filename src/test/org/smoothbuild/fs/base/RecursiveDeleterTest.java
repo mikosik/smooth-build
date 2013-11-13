fd3588bed1a571a58491fb286cdda75934b2aeb4
mikosik <marcin.mikosik@gmail.com>
2013-11-13 13:44:01 +0100

    refactored RecursiveDeleter, added more tests

--- src/test/org/smoothbuild/fs/base/RecursiveDeleterTest.java
+++ src/test/org/smoothbuild/fs/base/RecursiveDeleterTest.java
@@ -1,70 +1,103 @@
 package org.smoothbuild.fs.base;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.smoothbuild.testing.common.JdkFileTester.createDir;
-import static org.smoothbuild.testing.common.JdkFileTester.createEmptyFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.smoothbuild.testing.common.JdkFileTester;
 
-import com.google.common.io.Files;
+import com.google.common.collect.Lists;
 
 public class RecursiveDeleterTest {
-  private File tempDir;
-
-  private File fileOutside;
-  private File mainDir;
-  private File directFile;
-  private File directDir;
-  private File notDirectFile;
-  private File notDirectDir;
+  List<File> files;
+  File root;
 
   @Before
   public void before() throws IOException {
-    tempDir = Files.createTempDir();
-
-    File root = tempDir;
-    fileOutside = createEmptyFile(root, "fileOutsideMain");
-
-    String mainDirName = "mainDir";
-    mainDir = createDir(root, mainDirName);
-
-    String directFileName = "directFile";
-    directFile = createEmptyFile(mainDir, directFileName);
-
-    String directDirName = "directDir";
-    directDir = createDir(mainDir, directDirName);
-
-    String notDirectFileName = "notDirectFile";
-    notDirectFile = createEmptyFile(directDir, notDirectFileName);
-
-    String notDirectDirName = "notDirectDir";
-    notDirectDir = createDir(directDir, notDirectDirName);
+    files = Lists.newArrayList();
+    root = com.google.common.io.Files.createTempDir();
+    files.add(root);
   }
 
   @After
   public void after() {
-    deleteIfExists(notDirectDir);
-    deleteIfExists(notDirectFile);
-    deleteIfExists(directDir);
-    deleteIfExists(directFile);
-    deleteIfExists(mainDir);
-    deleteIfExists(fileOutside);
-    deleteIfExists(tempDir);
-  }
-
-  private static void deleteIfExists(File file) {
-    if (file.exists()) {
-      file.delete();
+    for (int i = files.size() - 1; 0 <= i; i--) {
+      File file = files.get(i);
+      if (file.exists()) {
+        file.delete();
+      }
     }
   }
 
   @Test
-  public void filesFromReturnsAllFilesRecursively() throws Exception {
+  public void deleting_sigle_file() throws Exception {
+    File file = createEmptyFile(root, "file");
+    RecursiveDeleter.deleteRecursively(file.toPath());
+    assertThat(file.exists()).isFalse();
+  }
+
+  @Test
+  public void deleting_sigle_file_does_not_delete_its_directory() throws Exception {
+    File directory = createDir(root, "dir");
+    File file = createEmptyFile(directory, "file");
+
+    RecursiveDeleter.deleteRecursively(file.toPath());
+
+    assertThat(directory.exists()).isTrue();
+  }
+
+  @Test
+  public void deleting_symbolic_link_to_file() throws Exception {
+    File file = createEmptyFile(root, "file");
+    File link = new File(root, "link");
+    Files.createSymbolicLink(link.toPath(), file.toPath());
+
+    RecursiveDeleter.deleteRecursively(link.toPath());
+
+    assertThat(link.exists()).isFalse();
+    assertThat(file.exists()).isTrue();
+  }
+
+  @Test
+  public void deleting_symbolic_link_to_directory() throws Exception {
+    File dir = createDir(root, "dir");
+    File file = createEmptyFile(dir, "file");
+    File link = new File(root, "link");
+    Files.createSymbolicLink(link.toPath(), dir.toPath());
+
+    RecursiveDeleter.deleteRecursively(link.toPath());
+
+    assertThat(dir.exists()).isTrue();
+    assertThat(file.exists()).isTrue();
+    assertThat(link.exists()).isFalse();
+  }
+
+  @Test
+  public void deleting_recursively_directory() throws Exception {
+    // given
+    File fileOutside = createEmptyFile(root, "fileOutsideMain");
+
+    String mainDirName = "mainDir";
+    File mainDir = createDir(root, mainDirName);
+
+    String directFileName = "directFile";
+    File directFile = createEmptyFile(mainDir, directFileName);
+
+    String directDirName = "directDir";
+    File directDir = createDir(mainDir, directDirName);
+
+    String notDirectFileName = "notDirectFile";
+    File notDirectFile = createEmptyFile(directDir, notDirectFileName);
+
+    String notDirectDirName = "notDirectDir";
+    File notDirectDir = createDir(directDir, notDirectDirName);
+
     // when
     RecursiveDeleter.deleteRecursively(mainDir.toPath());
 
@@ -76,5 +109,36 @@ public class RecursiveDeleterTest {
     assertThat(directDir.exists()).isFalse();
     assertThat(notDirectFile.exists()).isFalse();
     assertThat(notDirectDir.exists()).isFalse();
+  }
+
+  @Test
+  public void deleting_recursively_directory_that_contains_symbolic_link() throws Exception {
+    File file = createEmptyFile(root, "file");
+    File dir = createDir(root, "dir");
+    File link = new File(dir, "link");
+    Files.createSymbolicLink(link.toPath(), file.toPath());
+
+    RecursiveDeleter.deleteRecursively(dir.toPath());
+
+    assertThat(file.exists()).isTrue();
+    assertThat(dir.exists()).isFalse();
+    assertThat(link.exists()).isFalse();
+  }
+
+  @Test
+  public void deleting_directory_recursively_does_not_follow_symbolic_links() throws Exception {
+
+  }
+
+  private File createDir(File root, String dirName) {
+    File file = JdkFileTester.createDir(root, dirName);
+    files.add(file);
+    return file;
+  }
+
+  private File createEmptyFile(File root, String fileName) throws IOException {
+    File file = JdkFileTester.createEmptyFile(root, fileName);
+    files.add(file);
+    return file;
   }
 }
