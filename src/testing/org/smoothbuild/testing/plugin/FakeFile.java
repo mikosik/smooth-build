8f53e33bd677e00fc2ca8e3c9bf2d2bdd3d225c5
mikosik <marcin.mikosik@gmail.com>
2013-11-14 17:17:10 +0100

    refactored FakeFile to use FakeObject

--- src/testing/org/smoothbuild/testing/plugin/FakeFile.java
+++ src/testing/org/smoothbuild/testing/plugin/FakeFile.java
@@ -2,20 +2,21 @@ package org.smoothbuild.testing.plugin;
 
 import static org.smoothbuild.command.SmoothContants.CHARSET;
 
-import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 
 import org.smoothbuild.db.hash.Hash;
+import org.smoothbuild.db.hash.Marshaller;
 import org.smoothbuild.fs.base.Path;
 import org.smoothbuild.function.base.Type;
+import org.smoothbuild.plugin.Blob;
 import org.smoothbuild.plugin.File;
 
 import com.google.common.hash.HashCode;
 
 public class FakeFile implements File {
   private final Path path;
+  private final Blob blob;
   private final HashCode hash;
-  private final byte[] content;
 
   public FakeFile(Path path) {
     this(path, path.value());
@@ -27,8 +28,8 @@ public class FakeFile implements File {
 
   public FakeFile(Path path, byte[] bytes) {
     this.path = path;
-    this.hash = Hash.function().hashBytes(bytes);
-    this.content = bytes.clone();
+    this.blob = new FakeBlob(bytes);
+    this.hash = calculateHash(path, blob);
   }
 
   @Override
@@ -48,6 +49,13 @@ public class FakeFile implements File {
 
   @Override
   public InputStream openInputStream() {
-    return new ByteArrayInputStream(content);
+    return blob.openInputStream();
+  }
+
+  private static HashCode calculateHash(Path path, Blob blob) {
+    Marshaller marshaller = new Marshaller();
+    marshaller.write(blob.hash());
+    marshaller.write(path);
+    return Hash.bytes(marshaller.getBytes());
   }
 }
