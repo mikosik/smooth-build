dd6aee8cede5c8a0f5e3152328e52c38e28382c9
mikosik <marcin.mikosik@gmail.com>
2013-11-14 17:16:04 +0100

    added FakeBlob

---
+++ src/testing/org/smoothbuild/testing/plugin/FakeBlob.java
@@ -0,0 +1,35 @@
+package org.smoothbuild.testing.plugin;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import org.smoothbuild.db.hash.Hash;
+import org.smoothbuild.function.base.Type;
+import org.smoothbuild.plugin.Blob;
+
+import com.google.common.hash.HashCode;
+
+public class FakeBlob implements Blob {
+  private final HashCode hash;
+  private final byte[] data;
+
+  public FakeBlob(byte[] data) {
+    this.hash = Hash.function().hashBytes(data);
+    this.data = data.clone();
+  }
+
+  @Override
+  public Type type() {
+    return Type.BLOB;
+  }
+
+  @Override
+  public HashCode hash() {
+    return hash;
+  }
+
+  @Override
+  public InputStream openInputStream() {
+    return new ByteArrayInputStream(data);
+  }
+}
