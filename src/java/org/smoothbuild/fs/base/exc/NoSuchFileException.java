9078a1f6c2b1f3d74b36e2cb7c1ab3cdbe348faf
mikosik <marcin.mikosik@gmail.com>
2013-11-14 10:12:32 +0100

    fixed messages in NoSuchDirException, NoSuchFileException

--- src/java/org/smoothbuild/fs/base/exc/NoSuchFileException.java
+++ src/java/org/smoothbuild/fs/base/exc/NoSuchFileException.java
@@ -5,6 +5,6 @@ import org.smoothbuild.fs.base.Path;
 @SuppressWarnings("serial")
 public class NoSuchFileException extends FileSystemException {
   public NoSuchFileException(Path path) {
-    super("Path " + path + " is not a file.");
+    super("No such file: " + path);
   }
 }
