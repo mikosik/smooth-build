f3c959d812cad2a7a14f283937256e110dd7bbcb
mikosik <marcin.mikosik@gmail.com>
2013-11-20 14:48:32 +0100

    added BlobSetBuilder

---
+++ src/test/org/smoothbuild/lang/plugin/BlobSetBuilderTest.java
@@ -0,0 +1,36 @@
+package org.smoothbuild.lang.plugin;
+
+import static org.hamcrest.Matchers.emptyIterable;
+import static org.smoothbuild.command.SmoothContants.CHARSET;
+import static org.smoothbuild.util.Streams.inputStreamToString;
+import static org.testory.Testory.given;
+import static org.testory.Testory.thenReturned;
+import static org.testory.Testory.when;
+
+import java.io.IOException;
+
+import org.junit.Test;
+import org.smoothbuild.lang.function.value.Blob;
+import org.smoothbuild.testing.io.cache.value.FakeValueDb;
+
+public class BlobSetBuilderTest {
+  String content = "content";
+  FakeValueDb objectDb = new FakeValueDb();
+  Blob blob;
+
+  BlobSetBuilder blobSetBuilder = new BlobSetBuilder(objectDb);
+
+  @Test
+  public void build_returns_empty_blob_set_when_no_blob_has_been_added() throws IOException {
+    when(blobSetBuilder.build());
+    thenReturned(emptyIterable());
+  }
+
+  @Test
+  public void build_returns_set_containing_added_blob() throws Exception {
+    given(blob = objectDb.blob(content.getBytes(CHARSET)));
+    given(blobSetBuilder).add(blob);
+    when(inputStreamToString(blobSetBuilder.build().iterator().next().openInputStream()));
+    thenReturned(content);
+  }
+}
