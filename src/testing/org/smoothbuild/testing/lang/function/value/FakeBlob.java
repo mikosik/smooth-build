74b94c80a77b219b590d79f8e6a478ff154299b2
mikosik <marcin.mikosik@gmail.com>
2013-11-20 17:16:27 +0100

    reused AbstractValue in Fake[Blob/File/FileSet/String]

--- src/testing/org/smoothbuild/testing/lang/function/value/FakeBlob.java
+++ src/testing/org/smoothbuild/testing/lang/function/value/FakeBlob.java
@@ -4,28 +4,16 @@ import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 
 import org.smoothbuild.io.cache.hash.Hash;
+import org.smoothbuild.io.cache.value.AbstractValue;
 import org.smoothbuild.lang.function.base.Type;
 import org.smoothbuild.lang.function.value.Blob;
 
-import com.google.common.hash.HashCode;
-
-public class FakeBlob implements Blob {
-  private final HashCode hash;
+public class FakeBlob extends AbstractValue implements Blob {
   private final byte[] data;
 
   public FakeBlob(byte[] data) {
-    this.hash = Hash.function().hashBytes(data);
+    super(Type.BLOB, Hash.function().hashBytes(data));
     this.data = data.clone();
-  }
-
-  @Override
-  public Type type() {
-    return Type.BLOB;
-  }
-
-  @Override
-  public HashCode hash() {
-    return hash;
   }
 
   @Override
