fbfe469251e6e21740e7a5223171d1071527f1d1
mikosik <marcin.mikosik@gmail.com>
2013-11-20 12:06:14 +0100

    added BlobBuilder

---
+++ src/java/org/smoothbuild/lang/plugin/BlobBuilder.java
@@ -0,0 +1,30 @@
+package org.smoothbuild.lang.plugin;
+
+import static com.google.common.base.Preconditions.checkState;
+
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+
+import org.smoothbuild.io.cache.value.ValueDb;
+import org.smoothbuild.lang.function.value.Blob;
+
+public class BlobBuilder {
+  private final ValueDb valueDb;
+
+  private ByteArrayOutputStream outputStream;
+
+  public BlobBuilder(ValueDb valueDb) {
+    this.valueDb = valueDb;
+  }
+
+  public OutputStream openOutputStream() {
+    checkState(this.outputStream == null, "Cannot open output stream twice.");
+    this.outputStream = new ByteArrayOutputStream();
+    return outputStream;
+  }
+
+  public Blob build() {
+    checkState(outputStream != null, "No content available. Create one via openOutputStream()");
+    return valueDb.blob(outputStream.toByteArray());
+  }
+}
---
+++ src/test/org/smoothbuild/lang/plugin/BlobBuilderTest.java
@@ -0,0 +1,49 @@
+package org.smoothbuild.lang.plugin;
+
+import static org.mockito.Mockito.mock;
+import static org.testory.Testory.given;
+import static org.testory.Testory.thenReturned;
+import static org.testory.Testory.thenThrown;
+import static org.testory.Testory.when;
+
+import org.junit.Test;
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
+import org.smoothbuild.io.cache.value.CachedBlob;
+import org.smoothbuild.io.cache.value.ValueDb;
+
+public class BlobBuilderTest {
+  ValueDb valueDb = mock(ValueDb.class);
+  BlobBuilder blobBuilder = new BlobBuilder(valueDb);
+  byte[] bytes = new byte[] { 1, 2, 3 };
+  CachedBlob blob = Mockito.mock(CachedBlob.class);
+
+  @Test
+  public void opening_output_stream_twice_fails() throws Exception {
+    given(blobBuilder).openOutputStream();
+    when(blobBuilder).openOutputStream();
+    thenThrown(IllegalStateException.class);
+  }
+
+  @Test
+  public void build_fails_when_no_content_was_provided() {
+    when(blobBuilder).build();
+    thenThrown(IllegalStateException.class);
+  }
+
+  @Test
+  public void build_returns_blob_stored_in_object_db_with_empty_content() throws Exception {
+    BDDMockito.given(valueDb.blob(new byte[] {})).willReturn(blob);
+    given(blobBuilder).openOutputStream();
+    when(blobBuilder).build();
+    thenReturned(blob);
+  }
+
+  @Test
+  public void build_returns_blob_stored_in_object_db() throws Exception {
+    BDDMockito.given(valueDb.blob(bytes)).willReturn(blob);
+    given(blobBuilder.openOutputStream()).write(bytes);
+    when(blobBuilder).build();
+    thenReturned(blob);
+  }
+}
