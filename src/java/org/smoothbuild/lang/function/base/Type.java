a1fe46a23c48af0641b5d2fa3e206b17c1b7da05
mikosik <marcin.mikosik@gmail.com>
2013-11-21 11:44:20 +0100

    refactored Type class

--- src/java/org/smoothbuild/lang/function/base/Type.java
+++ src/java/org/smoothbuild/lang/function/base/Type.java
@@ -10,14 +10,36 @@ import com.google.common.collect.ImmutableSet;
 import com.google.inject.TypeLiteral;
 
 public class Type {
-  public static final Type STRING = new Type("String", StringValue.class);
-  public static final Type STRING_SET = new Type("String*",
-      new TypeLiteral<Array<StringValue>>() {});
-  public static final Type BLOB = new Type("Blob", Blob.class);
-  public static final Type BLOB_SET = new Type("BLOB*", new TypeLiteral<Array<Blob>>() {});
-  public static final Type FILE = new Type("File", File.class);
-  public static final Type FILE_SET = new Type("File*", new TypeLiteral<Array<File>>() {});
-  public static final Type EMPTY_SET = new Type("Any*", EmptySet.class);
+  /*
+   * Java types representing smooth types. These types are used by native
+   * function implementations in plugins code.
+   */
+
+  private static final TypeLiteral<?> STRING_T = TypeLiteral.get(StringValue.class);
+  private static final TypeLiteral<?> BLOB_T = TypeLiteral.get(Blob.class);
+  private static final TypeLiteral<?> FILE_T = TypeLiteral.get(File.class);
+  private static final TypeLiteral<?> STRING_A_T = new TypeLiteral<Array<StringValue>>() {};
+  private static final TypeLiteral<?> BLOB_A_T = new TypeLiteral<Array<Blob>>() {};
+  private static final TypeLiteral<?> FILE_A_T = new TypeLiteral<Array<File>>() {};
+  private static final TypeLiteral<?> EMPTY_A_T = TypeLiteral.get(EmptySet.class);
+
+  /*
+   * Smooth types. Used by smooth-build code to represent smooth types.
+   */
+
+  public static final Type STRING = new Type("String", STRING_T);
+  public static final Type STRING_SET = new Type("String*", STRING_A_T);
+  public static final Type BLOB = new Type("Blob", BLOB_T);
+  public static final Type BLOB_SET = new Type("BLOB*", BLOB_A_T);
+  public static final Type FILE = new Type("File", FILE_T);
+  public static final Type FILE_SET = new Type("File*", FILE_A_T);
+  public static final Type EMPTY_SET = new Type("Any*", EMPTY_A_T);
+
+  /*
+   * Not each type can be used in every place. Each set below represent one
+   * place where smooth type can be used and contains all smooth types that can
+   * be used there.
+   */
 
   static final ImmutableSet<Type> SET_ELEM_TYPES = ImmutableSet.of(STRING, BLOB, FILE);
   static final ImmutableSet<Type> RESULT_TYPES = ImmutableSet.of(STRING, STRING_SET, BLOB,
@@ -27,18 +49,26 @@ public class Type {
   static final ImmutableSet<Type> ALL_TYPES = ImmutableSet.of(STRING, STRING_SET, BLOB, BLOB_SET,
       FILE, FILE_SET, EMPTY_SET);
 
+  /*
+   * Some of the sets above converted to java types.
+   */
+
   static final ImmutableSet<TypeLiteral<?>> RESULT_JAVA_TYPES = toJavaTypes(RESULT_TYPES);
   static final ImmutableSet<TypeLiteral<?>> PARAM_JAVA_TYPES = toJavaTypes(PARAM_TYPES);
+
+  /*
+   * A few handy mappings.
+   */
 
   static final ImmutableMap<TypeLiteral<?>, Type> JAVA_PARAM_TO_SMOOTH = javaToTypeMap(PARAM_TYPES);
   static final ImmutableMap<TypeLiteral<?>, Type> JAVA_RESULT_TO_SMOOTH = javaToTypeMap(RESULT_TYPES);
 
+  /*
+   * Instance fields.
+   */
+
   private final String name;
   private final TypeLiteral<?> javaType;
-
-  private Type(String name, Class<?> javaType) {
-    this(name, TypeLiteral.get(javaType));
-  }
 
   private Type(String name, TypeLiteral<?> javaType) {
     this.name = name;
