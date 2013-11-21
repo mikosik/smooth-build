05b96f7033ff5e9dd76be95fdc8596298f54500a
mikosik <marcin.mikosik@gmail.com>
2013-11-21 15:32:15 +0100

    redesigned ParamsPool/TypedParamsPool/ArgumentNodesCreator to respect hierarchy imposed by Type.superTypes()

--- src/test/org/smoothbuild/lang/function/def/args/ParamsPoolTest.java
+++ src/test/org/smoothbuild/lang/function/def/args/ParamsPoolTest.java
@@ -3,8 +3,13 @@ package org.smoothbuild.lang.function.def.args;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.fail;
 import static org.smoothbuild.lang.function.base.Param.param;
+import static org.smoothbuild.lang.function.base.Type.BLOB;
+import static org.smoothbuild.lang.function.base.Type.BLOB_SET;
+import static org.smoothbuild.lang.function.base.Type.EMPTY_SET;
 import static org.smoothbuild.lang.function.base.Type.FILE;
+import static org.smoothbuild.lang.function.base.Type.FILE_SET;
 import static org.smoothbuild.lang.function.base.Type.STRING;
+import static org.smoothbuild.lang.function.base.Type.STRING_SET;
 import static org.smoothbuild.testing.lang.function.base.ParamTester.params;
 
 import org.junit.Test;
@@ -14,11 +19,24 @@ import com.google.common.collect.ImmutableMap;
 
 public class ParamsPoolTest {
   Param string = param(STRING, "string1");
-  Param stringRequired = param(STRING, "stringRequired", true);
+  Param blob = param(BLOB, "blob");
   Param file = param(FILE, "file1");
+
+  Param stringSet = param(STRING_SET, "stringSet");
+  Param blobSet = param(BLOB_SET, "blobSet");
+  Param fileSet = param(FILE_SET, "fileSet");
+
+  Param stringRequired = param(STRING, "stringRequired", true);
+  Param blobRequired = param(BLOB, "blobRequired", true);
   Param fileRequired = param(FILE, "fileRequired", true);
 
-  ImmutableMap<String, Param> params = params(string, stringRequired, file, fileRequired);
+  Param stringSetRequired = param(STRING_SET, "stringSetRequired", true);
+  Param blobSetRequired = param(BLOB_SET, "blobSetRequired", true);
+  Param fileSetRequired = param(FILE_SET, "fileSetRequired", true);
+
+  ImmutableMap<String, Param> params = params(string, stringRequired, blob, blobRequired, file,
+      fileRequired, stringSet, stringSetRequired, blobSet, blobSetRequired, fileSet,
+      fileSetRequired);
   ParamsPool paramsPool = new ParamsPool(params);
 
   @Test
@@ -73,13 +91,84 @@ public class ParamsPoolTest {
     }
   }
 
+  // availableForType()
+
   @Test
-  public void availableRequiredParams() throws Exception {
-    assertThat(paramsPool.availableRequiredParams()).containsOnly(stringRequired, fileRequired);
+  public void string_param_is_available_for_string_argument() throws Exception {
+    paramsPool = new ParamsPool(params);
+
+    assertThat(paramsPool.availableForType(STRING).optionalParams()).containsOnly(string);
+    assertThat(paramsPool.availableForType(STRING).requiredParams()).containsOnly(stringRequired);
   }
 
   @Test
-  public void availableRequiredParamsDoesNotContainsTakenParams() throws Exception {
+  public void blob_param_is_available_for_blob_argument() throws Exception {
+    paramsPool = new ParamsPool(params);
+
+    assertThat(paramsPool.availableForType(BLOB).optionalParams()).containsOnly(blob);
+    assertThat(paramsPool.availableForType(BLOB).requiredParams()).containsOnly(blobRequired);
+  }
+
+  @Test
+  public void blob_and_file_params_are_available_for_file_argument() throws Exception {
+    paramsPool = new ParamsPool(params);
+
+    assertThat(paramsPool.availableForType(FILE).optionalParams()).containsOnly(blob, file);
+    assertThat(paramsPool.availableForType(FILE).requiredParams()).containsOnly(blobRequired,
+        fileRequired);
+  }
+
+  @Test
+  public void string_set_param_is_available_for_string_set_argument() throws Exception {
+    paramsPool = new ParamsPool(params);
+
+    assertThat(paramsPool.availableForType(STRING_SET).optionalParams()).containsOnly(stringSet);
+    assertThat(paramsPool.availableForType(STRING_SET).requiredParams()).containsOnly(
+        stringSetRequired);
+  }
+
+  @Test
+  public void blob_set_param_is_available_for_blob_set_argument() throws Exception {
+    paramsPool = new ParamsPool(params);
+
+    assertThat(paramsPool.availableForType(BLOB_SET).optionalParams()).containsOnly(blobSet);
+    assertThat(paramsPool.availableForType(BLOB_SET).requiredParams())
+        .containsOnly(blobSetRequired);
+  }
+
+  @Test
+  public void blob_set_and_file_set_params_are_available_for_file_set_argument() throws Exception {
+    paramsPool = new ParamsPool(params);
+
+    assertThat(paramsPool.availableForType(FILE_SET).optionalParams()).containsOnly(blobSet,
+        fileSet);
+    assertThat(paramsPool.availableForType(FILE_SET).requiredParams()).containsOnly(
+        blobSetRequired, fileSetRequired);
+  }
+
+  @Test
+  public void set_params_are_available_for_empty_set_argument() throws Exception {
+    paramsPool = new ParamsPool(params);
+
+    assertThat(paramsPool.availableForType(EMPTY_SET).optionalParams()).containsOnly(stringSet,
+        blobSet, fileSet);
+    assertThat(paramsPool.availableForType(EMPTY_SET).requiredParams()).containsOnly(
+        stringSetRequired, blobSetRequired, fileSetRequired);
+  }
+
+  // availableRequiredParams()
+
+  @Test
+  public void availableRequiredParams() throws Exception {
+    assertThat(paramsPool.availableRequiredParams()).containsOnly(stringRequired, blobRequired,
+        fileRequired, stringSetRequired, blobSetRequired, fileSetRequired);
+  }
+
+  @Test
+  public void available_required_params_does_not_contain_taken_param() throws Exception {
+    ImmutableMap<String, Param> params = params(stringRequired, fileRequired);
+    ParamsPool paramsPool = new ParamsPool(params);
+
     paramsPool.take(stringRequired);
     assertThat(paramsPool.availableRequiredParams()).containsOnly(fileRequired);
   }
--- src/test/org/smoothbuild/lang/function/def/args/TypedParamsPoolTest.java
+++ src/test/org/smoothbuild/lang/function/def/args/TypedParamsPoolTest.java
@@ -1,52 +1,42 @@
 package org.smoothbuild.lang.function.def.args;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.Assert.fail;
 import static org.smoothbuild.lang.function.base.Param.param;
+import static org.smoothbuild.lang.function.base.Type.BLOB;
 import static org.smoothbuild.lang.function.base.Type.STRING;
+
+import java.util.Set;
 
 import org.junit.Test;
 import org.smoothbuild.lang.function.base.Param;
 
+import com.google.common.collect.Sets;
+
 public class TypedParamsPoolTest {
   Param string = param(STRING, "string1");
-  Param string2 = param(STRING, "string2");
-  Param string3 = param(STRING, "string3");
+  Param blob = param(BLOB, "blob");
 
   Param stringRequired = param(STRING, "stringRequired", true);
   Param stringRequired2 = param(STRING, "stringRequired2", true);
-  Param stringRequired3 = param(STRING, "stringRequired3", true);
 
-  TypedParamsPool pool = new TypedParamsPool();
-
-  TypedParamsPool pool1 = new TypedParamsPool();
-  TypedParamsPool pool2 = new TypedParamsPool();
-  TypedParamsPool pool3 = new TypedParamsPool();
-  TypedParamsPool combined = new TypedParamsPool(pool1, pool2, pool3);
+  Set<Param> optionalParams = Sets.newHashSet();
+  Set<Param> requiredParams = Sets.newHashSet();
+  TypedParamsPool pool = new TypedParamsPool(optionalParams, requiredParams);
 
   @Test
   public void requiredParams() throws Exception {
-    pool.add(string);
-    pool.add(string2);
-    pool.add(string3);
-    pool.add(stringRequired);
-    pool.add(stringRequired2);
-    pool.add(stringRequired3);
+    optionalParams.add(string);
+    requiredParams.add(stringRequired);
 
-    assertThat(pool.requiredParams())
-        .containsOnly(stringRequired, stringRequired2, stringRequired3);
+    assertThat(pool.requiredParams()).containsOnly(stringRequired);
   }
 
   @Test
   public void optionalParams() throws Exception {
-    pool.add(string);
-    pool.add(string2);
-    pool.add(string3);
-    pool.add(stringRequired);
-    pool.add(stringRequired2);
-    pool.add(stringRequired3);
+    optionalParams.add(string);
+    requiredParams.add(stringRequired);
 
-    assertThat(pool.optionalParams()).containsOnly(string, string2, string3);
+    assertThat(pool.optionalParams()).containsOnly(string);
   }
 
   @Test
@@ -60,50 +50,8 @@ public class TypedParamsPoolTest {
   }
 
   @Test
-  public void removeReturnsTrueWhenRequiredParamWasPresent() throws Exception {
-    pool.add(stringRequired);
-    assertThat(pool.remove(stringRequired)).isTrue();
-  }
-
-  @Test
-  public void removeReturnsFalseWhenRequiredParamWasNotPresent() throws Exception {
-    assertThat(pool.remove(stringRequired)).isFalse();
-  }
-
-  @Test
-  public void removedRequiredParam() throws Exception {
-    pool.add(stringRequired);
-    pool.add(stringRequired2);
-
-    pool.remove(stringRequired);
-
-    assertThat(pool.requiredParams()).containsOnly(stringRequired2);
-  }
-
-  @Test
-  public void removeReturnsTrueWhenOptionalParamWasPresent() throws Exception {
-    pool.add(string);
-    assertThat(pool.remove(string)).isTrue();
-  }
-
-  @Test
-  public void removeReturnsFalseWhenOptionalParamWasNotPresent() throws Exception {
-    assertThat(pool.remove(string)).isFalse();
-  }
-
-  @Test
-  public void removedOptionalParam() throws Exception {
-    pool.add(string);
-    pool.add(string2);
-
-    pool.remove(string);
-
-    assertThat(pool.optionalParams()).containsOnly(string2);
-  }
-
-  @Test
   public void hasCandidateForOptionalParam() throws Exception {
-    pool.add(stringRequired);
+    requiredParams.add(stringRequired);
 
     assertThat(pool.hasCandidate()).isTrue();
     assertThat(pool.candidate()).isSameAs(stringRequired);
@@ -111,7 +59,7 @@ public class TypedParamsPoolTest {
 
   @Test
   public void hasCandidateForOneOptionalParam() throws Exception {
-    pool.add(string);
+    optionalParams.add(string);
 
     assertThat(pool.hasCandidate()).isTrue();
     assertThat(pool.candidate()).isSameAs(string);
@@ -119,8 +67,8 @@ public class TypedParamsPoolTest {
 
   @Test
   public void hasCandidateForOptionalAndOneNonRequiredParam() throws Exception {
-    pool.add(stringRequired);
-    pool.add(string);
+    requiredParams.add(stringRequired);
+    optionalParams.add(string);
 
     assertThat(pool.hasCandidate()).isTrue();
     assertThat(pool.candidate()).isSameAs(stringRequired);
@@ -133,31 +81,31 @@ public class TypedParamsPoolTest {
 
   @Test
   public void doesNotHaveCandidateWhenTwoNonRequiredParamsExist() throws Exception {
-    pool.add(string);
-    pool.add(string2);
+    optionalParams.add(string);
+    optionalParams.add(blob);
     assertThat(pool.hasCandidate()).isFalse();
   }
 
   @Test
   public void doesNotHaveCandidateWhenTwoRequiredParamsExist() throws Exception {
-    pool.add(stringRequired);
-    pool.add(stringRequired2);
+    requiredParams.add(stringRequired);
+    requiredParams.add(stringRequired2);
     assertThat(pool.hasCandidate()).isFalse();
   }
 
   @Test
   public void doesNotHaveCandidateWhenTwoRequiredAndOneNonRequiredParamsExist() throws Exception {
-    pool.add(stringRequired);
-    pool.add(stringRequired2);
-    pool.add(string);
+    requiredParams.add(stringRequired);
+    requiredParams.add(stringRequired2);
+    optionalParams.add(string);
     assertThat(pool.hasCandidate()).isFalse();
   }
 
   @Test
   public void haveCandidateWhenOptionalAndTwoNonRequiredParamsExist() throws Exception {
-    pool.add(stringRequired);
-    pool.add(string);
-    pool.add(string2);
+    requiredParams.add(stringRequired);
+    optionalParams.add(string);
+    optionalParams.add(blob);
 
     assertThat(pool.hasCandidate()).isTrue();
     assertThat(pool.candidate()).isSameAs(stringRequired);
@@ -170,107 +118,9 @@ public class TypedParamsPoolTest {
 
   @Test
   public void size() throws Exception {
-    pool.add(string);
-    pool.add(stringRequired);
+    optionalParams.add(string);
+    requiredParams.add(stringRequired);
 
     assertThat(pool.size()).isEqualTo(2);
-  }
-
-  // test of combined version
-
-  @Test
-  public void combinedRequiredParams() throws Exception {
-    pool1.add(string);
-    pool1.add(stringRequired);
-    pool2.add(string2);
-    pool2.add(stringRequired2);
-    pool3.add(string3);
-    pool3.add(stringRequired3);
-
-    assertThat(combined.requiredParams()).containsOnly(stringRequired, stringRequired2,
-        stringRequired3);
-  }
-
-  @Test
-  public void removeDoesNotWorkOnCombined() throws Exception {
-    pool1.add(string);
-    try {
-      combined.remove(string);
-      fail("exception should be thrown");
-    } catch (UnsupportedOperationException e) {
-      // expected
-    }
-  }
-
-  @Test
-  public void hasCandidateForOptionalParamInFirstSubPool() throws Exception {
-    pool1.add(stringRequired);
-
-    assertThat(combined.hasCandidate()).isTrue();
-    assertThat(combined.candidate()).isSameAs(stringRequired);
-  }
-
-  @Test
-  public void hasCandidateForOneNonRequiredParamInFirstSubPool() throws Exception {
-    pool1.add(string);
-
-    assertThat(combined.hasCandidate()).isTrue();
-    assertThat(combined.candidate()).isSameAs(string);
-  }
-
-  @Test
-  public void doesNotHaveCandidateForOptionalParamInEachSubPool() throws Exception {
-    pool1.add(stringRequired);
-    pool2.add(stringRequired2);
-    pool3.add(stringRequired3);
-
-    assertThat(combined.hasCandidate()).isFalse();
-  }
-
-  @Test
-  public void doesNotHaveCandidateForOneNonRequiredParamInEachSubPool() throws Exception {
-    pool1.add(string);
-    pool2.add(string2);
-    pool3.add(string3);
-    assertThat(combined.hasCandidate()).isFalse();
-  }
-
-  @Test
-  public void hasCandidateForOptionalParamInFirstAndOneNonRequiredParamInSecondSubPool()
-      throws Exception {
-    pool1.add(stringRequired);
-    pool2.add(string);
-
-    assertThat(combined.hasCandidate()).isTrue();
-    assertThat(combined.candidate()).isSameAs(stringRequired);
-  }
-
-  @Test
-  public void hasCandidateForOptionalParamInFirstAndTwoNonRequiredParamInSecondSubPool()
-      throws Exception {
-    pool1.add(stringRequired);
-    pool2.add(string);
-    pool2.add(string2);
-
-    assertThat(combined.hasCandidate()).isTrue();
-    assertThat(combined.candidate()).isSameAs(stringRequired);
-  }
-
-  @Test
-  public void doesNotHaveCandidateWhenNoParamIsAdded() throws Exception {
-    assertThat(combined.hasCandidate()).isFalse();
-  }
-
-  @Test
-  public void sizeOfEmptyCombinedPoolIsZero() throws Exception {
-    assertThat(combined.size()).isEqualTo(0);
-  }
-
-  @Test
-  public void sizeOfCombinedPool() throws Exception {
-    pool1.add(string);
-    pool2.add(stringRequired);
-
-    assertThat(combined.size()).isEqualTo(2);
   }
 }
