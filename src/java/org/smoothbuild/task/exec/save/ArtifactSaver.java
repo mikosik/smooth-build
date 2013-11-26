1d681e6f5d20ae204f71ab5cc3be781b080bfc65
mikosik <marcin.mikosik@gmail.com>
2013-11-26 21:52:26 +0100

    refactored ArtifactSaver

--- src/java/org/smoothbuild/task/exec/save/ArtifactSaver.java
+++ src/java/org/smoothbuild/task/exec/save/ArtifactSaver.java
@@ -6,60 +6,54 @@ import static org.smoothbuild.lang.type.STypes.FILE;
 import static org.smoothbuild.lang.type.STypes.FILE_ARRAY;
 import static org.smoothbuild.lang.type.STypes.STRING;
 import static org.smoothbuild.lang.type.STypes.STRING_ARRAY;
+import static org.smoothbuild.message.base.MessageType.FATAL;
 
 import javax.inject.Inject;
 
 import org.smoothbuild.io.fs.SmoothDir;
 import org.smoothbuild.io.fs.base.FileSystem;
 import org.smoothbuild.lang.function.base.Name;
-import org.smoothbuild.lang.type.SArray;
 import org.smoothbuild.lang.type.SBlob;
-import org.smoothbuild.lang.type.SFile;
 import org.smoothbuild.lang.type.SString;
+import org.smoothbuild.lang.type.SType;
 import org.smoothbuild.lang.type.SValue;
 import org.smoothbuild.message.base.Message;
-import org.smoothbuild.message.base.MessageType;
 import org.smoothbuild.message.listen.ErrorMessageException;
 
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableMap.Builder;
+
 public class ArtifactSaver {
-  private final StringSaver stringSaver;
-  private final BlobSaver blobSaver;
-  private final FileSaver fileSaver;
-  private final HashedArraySaver<SString> stringArraySaver;
-  private final HashedArraySaver<SBlob> blobArraySaver;
-  private final FileArraySaver fileArraySaver;
+  private final ImmutableMap<SType<?>, Saver<?>> saversMap;
 
   @Inject
   public ArtifactSaver(@SmoothDir FileSystem smoothFileSystem) {
-    this.stringSaver = new StringSaver(smoothFileSystem);
-    this.blobSaver = new BlobSaver(smoothFileSystem);
-    this.fileSaver = new FileSaver(smoothFileSystem);
-    this.stringArraySaver = new HashedArraySaver<SString>(smoothFileSystem);
-    this.blobArraySaver = new HashedArraySaver<SBlob>(smoothFileSystem);
-    this.fileArraySaver = new FileArraySaver(smoothFileSystem);
+    Builder<SType<?>, Saver<?>> builder = ImmutableMap.builder();
+
+    builder.put(STRING, new StringSaver(smoothFileSystem));
+    builder.put(BLOB, new BlobSaver(smoothFileSystem));
+    builder.put(FILE, new FileSaver(smoothFileSystem));
+    builder.put(STRING_ARRAY, new HashedArraySaver<SString>(smoothFileSystem));
+    builder.put(BLOB_ARRAY, new HashedArraySaver<SBlob>(smoothFileSystem));
+    builder.put(FILE_ARRAY, new FileArraySaver(smoothFileSystem));
+
+    this.saversMap = builder.build();
   }
 
   public void save(Name name, SValue value) {
-    if (value.type() == BLOB) {
-      blobSaver.save(name, (SBlob) value);
-    } else if (value.type() == BLOB_ARRAY) {
-      @SuppressWarnings("unchecked")
-      SArray<SBlob> blobArray = (SArray<SBlob>) value;
-      blobArraySaver.save(name, blobArray);
-    } else if (value.type() == FILE) {
-      fileSaver.save(name, (SFile) value);
-    } else if (value.type() == FILE_ARRAY) {
-      @SuppressWarnings("unchecked")
-      SArray<SFile> fileArray = (SArray<SFile>) value;
-      fileArraySaver.save(name, fileArray);
-    } else if (value.type() == STRING) {
-      stringSaver.save(name, (SString) value);
-    } else if (value.type() == STRING_ARRAY) {
-      @SuppressWarnings("unchecked")
-      SArray<SString> stringArray = (SArray<SString>) value;
-      stringArraySaver.save(name, stringArray);
+    /*
+     * We have to cast to Saver<SValue> to avoid compilation warning. Generic
+     * type of object being cast is not actually Saver<SValue> but Saver<Xxx>
+     * where Xxx is subtype of SValue. As saversMap is immutable and constructed
+     * properly in constructor then cast added by erasure in Saver class will
+     * succeed.
+     */
+    @SuppressWarnings("unchecked")
+    Saver<SValue> saver = (Saver<SValue>) saversMap.get(value.type());
+    if (saver != null) {
+      saver.save(name, value);
     } else {
-      throw new ErrorMessageException(new Message(MessageType.FATAL,
+      throw new ErrorMessageException(new Message(FATAL,
           "Bug in smooth binary.\nUnknown value type " + value.getClass().getName()));
     }
   }
