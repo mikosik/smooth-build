930ca579ecea81290755ebcc8cac4eb26b4c075d
mikosik <marcin.mikosik@gmail.com>
2013-11-26 10:12:40 +0100

    renamed STypes.allowedForArrayElem() -> basicTypes()

--- src/java/org/smoothbuild/parse/err/ForbiddenArrayElemTypeError.java
+++ src/java/org/smoothbuild/parse/err/ForbiddenArrayElemTypeError.java
@@ -1,6 +1,6 @@
 package org.smoothbuild.parse.err;
 
-import static org.smoothbuild.lang.type.STypes.allowedForArrayElem;
+import static org.smoothbuild.lang.type.STypes.basicTypes;
 import static org.smoothbuild.message.base.MessageType.ERROR;
 
 import org.smoothbuild.lang.type.SType;
@@ -10,6 +10,6 @@ import org.smoothbuild.message.base.CodeMessage;
 public class ForbiddenArrayElemTypeError extends CodeMessage {
   public ForbiddenArrayElemTypeError(CodeLocation codeLocation, SType<?> type) {
     super(ERROR, codeLocation, "Array cannot contain element of type " + type
-        + ". Only following types are allowed: " + allowedForArrayElem());
+        + ". Only following types are allowed: " + basicTypes());
   }
 }
