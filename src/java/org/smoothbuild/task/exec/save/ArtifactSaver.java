2f34c51f356d2aba010e80c9dfca90736738fc3e
mikosik <marcin.mikosik@gmail.com>
2013-11-26 13:10:13 +0100

    refactored out BlobSaver from FileSaver

--- src/java/org/smoothbuild/task/exec/save/ArtifactSaver.java
+++ src/java/org/smoothbuild/task/exec/save/ArtifactSaver.java
@@ -1,5 +1,6 @@
 package org.smoothbuild.task.exec.save;
 
+import static org.smoothbuild.lang.type.STypes.BLOB;
 import static org.smoothbuild.lang.type.STypes.FILE;
 import static org.smoothbuild.lang.type.STypes.FILE_ARRAY;
 import static org.smoothbuild.lang.type.STypes.STRING;
@@ -11,6 +12,7 @@ import org.smoothbuild.io.fs.SmoothDir;
 import org.smoothbuild.io.fs.base.FileSystem;
 import org.smoothbuild.lang.function.base.Name;
 import org.smoothbuild.lang.type.SArray;
+import org.smoothbuild.lang.type.SBlob;
 import org.smoothbuild.lang.type.SFile;
 import org.smoothbuild.lang.type.SString;
 import org.smoothbuild.lang.type.SValue;
@@ -20,6 +22,7 @@ import org.smoothbuild.message.listen.ErrorMessageException;
 
 public class ArtifactSaver {
   private final StringSaver stringSaver;
+  private final BlobSaver blobSaver;
   private final FileSaver fileSaver;
   private final StringArraySaver stringArraySaver;
   private final FileArraySaver fileArraySaver;
@@ -27,13 +30,16 @@ public class ArtifactSaver {
   @Inject
   public ArtifactSaver(@SmoothDir FileSystem smoothFileSystem) {
     this.stringSaver = new StringSaver(smoothFileSystem);
+    this.blobSaver = new BlobSaver(smoothFileSystem);
     this.fileSaver = new FileSaver(smoothFileSystem);
     this.stringArraySaver = new StringArraySaver(smoothFileSystem);
     this.fileArraySaver = new FileArraySaver(smoothFileSystem);
   }
 
   public void save(Name name, SValue value) {
-    if (value.type() == FILE) {
+    if (value.type() == BLOB) {
+      blobSaver.save(name, (SBlob) value);
+    } else if (value.type() == FILE) {
       fileSaver.save(name, (SFile) value);
     } else if (value.type() == FILE_ARRAY) {
       @SuppressWarnings("unchecked")
--- src/java/org/smoothbuild/task/exec/save/FileSaver.java
+++ src/java/org/smoothbuild/task/exec/save/FileSaver.java
@@ -1,26 +1,18 @@
 package org.smoothbuild.task.exec.save;
 
-import static org.smoothbuild.task.exec.save.Savers.artifactPath;
-import static org.smoothbuild.task.exec.save.Savers.targetPath;
-
 import org.smoothbuild.io.fs.base.FileSystem;
-import org.smoothbuild.io.fs.base.Path;
 import org.smoothbuild.lang.function.base.Name;
 import org.smoothbuild.lang.type.SFile;
 
 public class FileSaver implements Saver<SFile> {
-  private final FileSystem smoothFileSystem;
+  private final BlobSaver blobSaver;
 
   public FileSaver(FileSystem smoothFileSystem) {
-    this.smoothFileSystem = smoothFileSystem;
+    this.blobSaver = new BlobSaver(smoothFileSystem);
   }
 
   @Override
-  public void save(Name name, SFile value) {
-    Path artifactPath = artifactPath(name);
-    Path targetPath = targetPath(value.content());
-
-    smoothFileSystem.delete(artifactPath);
-    smoothFileSystem.createLink(artifactPath, targetPath);
+  public void save(Name name, SFile file) {
+    blobSaver.save(name, file.content());
   }
 }
