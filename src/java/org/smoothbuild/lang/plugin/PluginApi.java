e543ea01aed295ebee9e95822f7e151afc91598e
mikosik <marcin.mikosik@gmail.com>
2013-11-27 16:23:45 +0100

    extracted SValueBuilders out of PluginApi

--- src/java/org/smoothbuild/lang/plugin/PluginApi.java
+++ src/java/org/smoothbuild/lang/plugin/PluginApi.java
@@ -1,21 +1,8 @@
 package org.smoothbuild.lang.plugin;
 
-import org.smoothbuild.io.cache.value.build.ArrayBuilder;
-import org.smoothbuild.io.cache.value.build.BlobBuilder;
-import org.smoothbuild.io.cache.value.build.FileBuilder;
-import org.smoothbuild.lang.type.SArrayType;
-import org.smoothbuild.lang.type.SString;
-import org.smoothbuild.lang.type.SValue;
+import org.smoothbuild.lang.type.SValueBuilders;
 import org.smoothbuild.message.base.Message;
 
-public interface PluginApi {
+public interface PluginApi extends SValueBuilders {
   public void report(Message message);
-
-  public <T extends SValue> ArrayBuilder<T> arrayBuilder(SArrayType<T> arrayType);
-
-  public FileBuilder fileBuilder();
-
-  public BlobBuilder blobBuilder();
-
-  public SString string(String string);
 }
