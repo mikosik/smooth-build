2516c8a7c568f04cf53abed828d8a0d6edb3246f
mikosik <marcin.mikosik@gmail.com>
2013-11-27 10:33:35 +0100

    added DuplicatesDetector

---
+++ src/java/org/smoothbuild/util/DuplicatesDetector.java
@@ -0,0 +1,33 @@
+package org.smoothbuild.util;
+
+import java.util.Set;
+
+import com.google.common.collect.HashMultiset;
+import com.google.common.collect.Multiset;
+import com.google.common.collect.Sets;
+
+public class DuplicatesDetector<T> {
+  private final Multiset<T> set = HashMultiset.create();
+  private boolean hasDuplicates = false;
+
+  public boolean add(T value) {
+    boolean alreadyContains = set.contains(value);
+    hasDuplicates = hasDuplicates || alreadyContains;
+    set.add(value);
+    return alreadyContains;
+  }
+
+  public boolean hasDuplicates() {
+    return hasDuplicates;
+  }
+
+  public Set<T> getDuplicates() {
+    Set<T> result = Sets.newHashSet();
+    for (Multiset.Entry<T> entry : set.entrySet()) {
+      if (1 < entry.getCount()) {
+        result.add(entry.getElement());
+      }
+    }
+    return result;
+  }
+}
