d25af8e245aa3374241ecd3e36001b464fad00a7
mikosik <marcin.mikosik@gmail.com>
2013-11-27 12:39:15 +0100

    refactored FakeArray to have generic argument

--- src/testing/org/smoothbuild/testing/lang/type/FakeArray.java
+++ src/testing/org/smoothbuild/testing/lang/type/FakeArray.java
@@ -6,25 +6,25 @@ import java.util.List;
 import org.smoothbuild.io.cache.hash.Hash;
 import org.smoothbuild.io.cache.value.instance.CachedValue;
 import org.smoothbuild.lang.type.SArray;
-import org.smoothbuild.lang.type.SFile;
 import org.smoothbuild.lang.type.SType;
+import org.smoothbuild.lang.type.SValue;
 
 import com.google.common.collect.Lists;
 import com.google.common.hash.HashCode;
 
-public class FakeArray extends CachedValue implements SArray<SFile> {
-  private final List<SFile> files = Lists.newArrayList();
+public class FakeArray<T extends SValue> extends CachedValue implements SArray<T> {
+  private final List<T> files = Lists.newArrayList();
 
   public FakeArray(SType<?> type) {
     super(type, HashCode.fromInt(0));
   }
 
-  public void add(SFile file) {
+  public void add(T file) {
     files.add(file);
   }
 
   @Override
-  public Iterator<SFile> iterator() {
+  public Iterator<T> iterator() {
     return files.iterator();
   }
 
