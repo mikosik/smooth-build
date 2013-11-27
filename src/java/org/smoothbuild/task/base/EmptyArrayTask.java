b5c4f62ee46b7d84cc33a0ed26706ca7abb53487
mikosik <marcin.mikosik@gmail.com>
2013-11-27 17:57:50 +0100

    refactored EmptyArrayTask

--- src/java/org/smoothbuild/task/base/EmptyArrayTask.java
+++ src/java/org/smoothbuild/task/base/EmptyArrayTask.java
@@ -1,7 +1,5 @@
 package org.smoothbuild.task.base;
 
-import static org.smoothbuild.lang.type.STypes.STRING_ARRAY;
-
 import org.smoothbuild.lang.type.SValue;
 import org.smoothbuild.message.base.CodeLocation;
 import org.smoothbuild.task.exec.PluginApiImpl;
@@ -14,24 +12,6 @@ public class EmptyArrayTask extends Task {
 
   @Override
   public SValue execute(PluginApiImpl pluginApi) {
-    /*
-     * We cheat here and return empty stringArray. Nobody will ever use this
-     * object.
-     * 
-     * Anyway as ArgumentNodesCreator detects nodes which type is equal to
-     * EMPTY_ARRAY and replaces them either with ArrayNode with type element
-     * depending on the type of parameter to which argument is assigned.
-     * 
-     * The only way this code could be execute is when you define smooth
-     * function like this
-     * 
-     * empty : [];
-     * 
-     * and request smooth to execute it directly. In that case it won't be
-     * passed as argument to any other function so workaround in
-     * ArgumentNodesCreator won't kick in.
-     */
-
-    return pluginApi.arrayBuilder(STRING_ARRAY).build();
+    return pluginApi.emptyArray();
   }
 }
