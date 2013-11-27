d9de50f9f7cf3399030f01a6f46c0f49b39b044c
mikosik <marcin.mikosik@gmail.com>
2013-11-27 20:11:07 +0100

    refactored EmptyArrayTask, StringTask to use name of its SType

--- src/java/org/smoothbuild/task/base/EmptyArrayTask.java
+++ src/java/org/smoothbuild/task/base/EmptyArrayTask.java
@@ -1,5 +1,7 @@
 package org.smoothbuild.task.base;
 
+import static org.smoothbuild.lang.type.STypes.EMPTY_ARRAY;
+
 import org.smoothbuild.lang.type.SValue;
 import org.smoothbuild.message.base.CodeLocation;
 import org.smoothbuild.task.exec.PluginApiImpl;
@@ -7,7 +9,7 @@ import org.smoothbuild.task.exec.PluginApiImpl;
 public class EmptyArrayTask extends Task {
 
   public EmptyArrayTask(CodeLocation codeLocation) {
-    super("Empty*", true, codeLocation);
+    super(EMPTY_ARRAY.name(), true, codeLocation);
   }
 
   @Override
