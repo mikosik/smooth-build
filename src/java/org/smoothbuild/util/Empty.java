dc17f9fdd6bb2d892b70722d914e619d5a790501
mikosik <marcin.mikosik@gmail.com>
2013-11-28 16:24:42 +0100

    added Empty.typeToConverterMap()

--- src/java/org/smoothbuild/util/Empty.java
+++ src/java/org/smoothbuild/util/Empty.java
@@ -1,8 +1,10 @@
 package org.smoothbuild.util;
 
+import org.smoothbuild.lang.convert.Converter;
 import org.smoothbuild.lang.function.base.Function;
 import org.smoothbuild.lang.function.base.Name;
 import org.smoothbuild.lang.function.def.Node;
+import org.smoothbuild.lang.type.SType;
 import org.smoothbuild.lang.type.SValue;
 import org.smoothbuild.message.base.Message;
 import org.smoothbuild.task.base.Result;
@@ -38,5 +40,9 @@ public class Empty {
     } else {
       return iterable;
     }
+  }
+
+  public static ImmutableMap<SType<?>, Converter<?>> typeToConverterMap() {
+    return ImmutableMap.of();
   }
 }
