b92eb098c3b6f5ca7a8d992286ae54caa4f2064d
mikosik <marcin.mikosik@gmail.com>
2013-11-28 18:01:21 +0100

    added Conversions

---
+++ src/java/org/smoothbuild/lang/convert/Conversions.java
@@ -0,0 +1,72 @@
+package org.smoothbuild.lang.convert;
+
+import static org.smoothbuild.lang.type.STypes.BLOB;
+import static org.smoothbuild.lang.type.STypes.BLOB_ARRAY;
+import static org.smoothbuild.lang.type.STypes.EMPTY_ARRAY;
+import static org.smoothbuild.lang.type.STypes.FILE;
+import static org.smoothbuild.lang.type.STypes.FILE_ARRAY;
+import static org.smoothbuild.lang.type.STypes.STRING;
+import static org.smoothbuild.lang.type.STypes.STRING_ARRAY;
+
+import org.smoothbuild.lang.type.SBlob;
+import org.smoothbuild.lang.type.SFile;
+import org.smoothbuild.lang.type.SString;
+import org.smoothbuild.lang.type.SType;
+import org.smoothbuild.util.Empty;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableMap.Builder;
+import com.google.common.collect.ImmutableSet;
+
+public class Conversions {
+
+  /**
+   * Maps smooth type to a map that contains keys being super-types of that type
+   * and values being converters to each super-type.
+   */
+  private static final ImmutableMap<SType<?>, ImmutableMap<SType<?>, Converter<?>>> map =
+      createMap();
+
+  public static ImmutableSet<SType<?>> superTypesOf(SType<?> type) {
+    return map.get(type).keySet();
+  }
+
+  public static boolean canAssign(SType<?> from, SType<?> to) {
+    if (from == to) {
+      return true;
+    }
+    return map.get(from).containsKey(to);
+  }
+
+  public static Converter<?> converter(SType<?> from, SType<?> to) {
+    return map.get(from).get(to);
+  }
+
+  private static ImmutableMap<SType<?>, ImmutableMap<SType<?>, Converter<?>>> createMap() {
+    Builder<SType<?>, ImmutableMap<SType<?>, Converter<?>>> builder = ImmutableMap.builder();
+
+    builder.put(STRING, Empty.typeToConverterMap());
+    builder.put(BLOB, Empty.typeToConverterMap());
+    builder.put(FILE, convertersMap(new FileToBlobConverter()));
+
+    builder.put(STRING_ARRAY, Empty.typeToConverterMap());
+    builder.put(BLOB_ARRAY, Empty.typeToConverterMap());
+    builder.put(FILE_ARRAY, convertersMap(new FileArrayToBlobArrayConverter()));
+
+    Converter<?> nilToStringArray = new EmptyArrayToTypedArrayConverter<SString>(STRING_ARRAY);
+    Converter<?> nilToBlobArray = new EmptyArrayToTypedArrayConverter<SBlob>(BLOB_ARRAY);
+    Converter<?> nilToFileArray = new EmptyArrayToTypedArrayConverter<SFile>(FILE_ARRAY);
+
+    builder.put(EMPTY_ARRAY, convertersMap(nilToStringArray, nilToBlobArray, nilToFileArray));
+
+    return builder.build();
+  }
+
+  private static ImmutableMap<SType<?>, Converter<?>> convertersMap(Converter<?>... converters) {
+    Builder<SType<?>, Converter<?>> builder = ImmutableMap.builder();
+    for (Converter<?> converter : converters) {
+      builder.put(converter.targetType(), converter);
+    }
+    return builder.build();
+  }
+}
---
+++ src/test/org/smoothbuild/lang/convert/ConversionsTest.java
@@ -0,0 +1,145 @@
+package org.smoothbuild.lang.convert;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.smoothbuild.io.fs.base.Path.path;
+import static org.smoothbuild.lang.type.STypes.BLOB;
+import static org.smoothbuild.lang.type.STypes.BLOB_ARRAY;
+import static org.smoothbuild.lang.type.STypes.EMPTY_ARRAY;
+import static org.smoothbuild.lang.type.STypes.FILE;
+import static org.smoothbuild.lang.type.STypes.FILE_ARRAY;
+import static org.smoothbuild.lang.type.STypes.STRING;
+import static org.smoothbuild.lang.type.STypes.STRING_ARRAY;
+import static org.smoothbuild.testing.lang.type.FakeArray.fakeArray;
+
+import org.junit.Test;
+import org.smoothbuild.lang.type.SArray;
+import org.smoothbuild.lang.type.SBlob;
+import org.smoothbuild.lang.type.SFile;
+import org.smoothbuild.lang.type.SString;
+import org.smoothbuild.testing.lang.type.FakeArray;
+import org.smoothbuild.testing.lang.type.FakeFile;
+import org.smoothbuild.testing.task.exec.FakePluginApi;
+
+public class ConversionsTest {
+
+  @Test
+  public void isAssignableFrom() throws Exception {
+    assertThat(Conversions.canAssign(STRING, STRING)).isTrue();
+    assertThat(Conversions.canAssign(STRING, STRING_ARRAY)).isFalse();
+    assertThat(Conversions.canAssign(STRING, BLOB)).isFalse();
+    assertThat(Conversions.canAssign(STRING, BLOB_ARRAY)).isFalse();
+    assertThat(Conversions.canAssign(STRING, FILE)).isFalse();
+    assertThat(Conversions.canAssign(STRING, FILE_ARRAY)).isFalse();
+    assertThat(Conversions.canAssign(STRING, EMPTY_ARRAY)).isFalse();
+
+    assertThat(Conversions.canAssign(BLOB, STRING)).isFalse();
+    assertThat(Conversions.canAssign(BLOB, BLOB)).isTrue();
+    assertThat(Conversions.canAssign(BLOB, FILE)).isFalse();
+    assertThat(Conversions.canAssign(BLOB, STRING_ARRAY)).isFalse();
+    assertThat(Conversions.canAssign(BLOB, BLOB_ARRAY)).isFalse();
+    assertThat(Conversions.canAssign(BLOB, FILE_ARRAY)).isFalse();
+    assertThat(Conversions.canAssign(BLOB, EMPTY_ARRAY)).isFalse();
+
+    assertThat(Conversions.canAssign(FILE, STRING)).isFalse();
+    assertThat(Conversions.canAssign(FILE, BLOB)).isTrue();
+    assertThat(Conversions.canAssign(FILE, FILE)).isTrue();
+    assertThat(Conversions.canAssign(FILE, STRING_ARRAY)).isFalse();
+    assertThat(Conversions.canAssign(FILE, BLOB_ARRAY)).isFalse();
+    assertThat(Conversions.canAssign(FILE, FILE_ARRAY)).isFalse();
+    assertThat(Conversions.canAssign(FILE, EMPTY_ARRAY)).isFalse();
+
+    assertThat(Conversions.canAssign(STRING_ARRAY, STRING)).isFalse();
+    assertThat(Conversions.canAssign(STRING_ARRAY, BLOB)).isFalse();
+    assertThat(Conversions.canAssign(STRING_ARRAY, FILE)).isFalse();
+    assertThat(Conversions.canAssign(STRING_ARRAY, STRING_ARRAY)).isTrue();
+    assertThat(Conversions.canAssign(STRING_ARRAY, BLOB_ARRAY)).isFalse();
+    assertThat(Conversions.canAssign(STRING_ARRAY, FILE_ARRAY)).isFalse();
+    assertThat(Conversions.canAssign(STRING_ARRAY, EMPTY_ARRAY)).isFalse();
+
+    assertThat(Conversions.canAssign(BLOB_ARRAY, STRING)).isFalse();
+    assertThat(Conversions.canAssign(BLOB_ARRAY, BLOB)).isFalse();
+    assertThat(Conversions.canAssign(BLOB_ARRAY, FILE)).isFalse();
+    assertThat(Conversions.canAssign(BLOB_ARRAY, STRING_ARRAY)).isFalse();
+    assertThat(Conversions.canAssign(BLOB_ARRAY, BLOB_ARRAY)).isTrue();
+    assertThat(Conversions.canAssign(BLOB_ARRAY, FILE_ARRAY)).isFalse();
+    assertThat(Conversions.canAssign(BLOB_ARRAY, EMPTY_ARRAY)).isFalse();
+
+    assertThat(Conversions.canAssign(FILE_ARRAY, STRING)).isFalse();
+    assertThat(Conversions.canAssign(FILE_ARRAY, BLOB)).isFalse();
+    assertThat(Conversions.canAssign(FILE_ARRAY, FILE)).isFalse();
+    assertThat(Conversions.canAssign(FILE_ARRAY, STRING_ARRAY)).isFalse();
+    assertThat(Conversions.canAssign(FILE_ARRAY, BLOB_ARRAY)).isTrue();
+    assertThat(Conversions.canAssign(FILE_ARRAY, FILE_ARRAY)).isTrue();
+    assertThat(Conversions.canAssign(FILE_ARRAY, EMPTY_ARRAY)).isFalse();
+
+    assertThat(Conversions.canAssign(EMPTY_ARRAY, STRING)).isFalse();
+    assertThat(Conversions.canAssign(EMPTY_ARRAY, BLOB)).isFalse();
+    assertThat(Conversions.canAssign(EMPTY_ARRAY, FILE)).isFalse();
+    assertThat(Conversions.canAssign(EMPTY_ARRAY, STRING_ARRAY)).isTrue();
+    assertThat(Conversions.canAssign(EMPTY_ARRAY, BLOB_ARRAY)).isTrue();
+    assertThat(Conversions.canAssign(EMPTY_ARRAY, FILE_ARRAY)).isTrue();
+    assertThat(Conversions.canAssign(EMPTY_ARRAY, EMPTY_ARRAY)).isTrue();
+  }
+
+  @Test
+  public void superTypes() throws Exception {
+    assertThat(Conversions.superTypesOf(STRING)).containsOnly();
+    assertThat(Conversions.superTypesOf(BLOB)).containsOnly();
+    assertThat(Conversions.superTypesOf(FILE)).containsOnly(BLOB);
+
+    assertThat(Conversions.superTypesOf(STRING_ARRAY)).containsOnly();
+    assertThat(Conversions.superTypesOf(BLOB_ARRAY)).containsOnly();
+    assertThat(Conversions.superTypesOf(FILE_ARRAY)).containsOnly(BLOB_ARRAY);
+    assertThat(Conversions.superTypesOf(EMPTY_ARRAY)).containsOnly(STRING_ARRAY, BLOB_ARRAY,
+        FILE_ARRAY);
+  }
+
+  @Test
+  public void convertFileToBlob() throws Exception {
+    FakeFile file = new FakeFile(path("abc"));
+    Object blob = Conversions.converter(FILE, BLOB).convert(new FakePluginApi(), file);
+    assertThat(blob).isSameAs(file.content());
+  }
+
+  @Test
+  public void convertFileArrayToBlobArray() throws Exception {
+    FakeFile file1 = new FakeFile(path("abc"));
+    FakeFile file2 = new FakeFile(path("abc"));
+
+    FakeArray<SFile> fileArray = fakeArray(FILE_ARRAY, file1, file2);
+    Converter<?> converter = Conversions.converter(FILE_ARRAY, BLOB_ARRAY);
+    @SuppressWarnings("unchecked")
+    SArray<SBlob> blobArray = (SArray<SBlob>) converter.convert(new FakePluginApi(), fileArray);
+    assertThat(blobArray).containsExactly(file1.content(), file2.content());
+  }
+
+  @Test
+  public void convertEmptyArrayToStringArray() throws Exception {
+    FakePluginApi pluginApi = new FakePluginApi();
+
+    Converter<?> converter = Conversions.converter(EMPTY_ARRAY, STRING_ARRAY);
+    @SuppressWarnings("unchecked")
+    SArray<SString> array = (SArray<SString>) converter.convert(pluginApi, pluginApi.emptyArray());
+    assertThat(array).isEmpty();
+  }
+
+  @Test
+  public void convertEmptyArrayToBlobArray() throws Exception {
+    FakePluginApi pluginApi = new FakePluginApi();
+
+    Converter<?> converter = Conversions.converter(EMPTY_ARRAY, BLOB_ARRAY);
+    @SuppressWarnings("unchecked")
+    SArray<SBlob> array = (SArray<SBlob>) converter.convert(pluginApi, pluginApi.emptyArray());
+    assertThat(array).isEmpty();
+  }
+
+  @Test
+  public void convertEmptyArrayToFileArray() throws Exception {
+    FakePluginApi pluginApi = new FakePluginApi();
+
+    Converter<?> converter = Conversions.converter(EMPTY_ARRAY, FILE_ARRAY);
+    @SuppressWarnings("unchecked")
+    SArray<SFile> array = (SArray<SFile>) converter.convert(pluginApi, pluginApi.emptyArray());
+    assertThat(array).isEmpty();
+  }
+}
