ca3f38bf9225a2c4e94ea93fde73c13fadf25c2b
mikosik <marcin.mikosik@gmail.com>
2013-11-29 16:06:03 +0100

    fixed storing empty arrays in ArtifactSaver

--- src/test-integration/org/smoothbuild/base/ArraysSmoothTest.java
+++ src/test-integration/org/smoothbuild/base/ArraysSmoothTest.java
@@ -1,8 +1,13 @@
 package org.smoothbuild.base;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.smoothbuild.io.fs.base.Path.path;
+
 import java.io.IOException;
 
 import org.junit.Test;
+import org.smoothbuild.io.fs.base.Path;
+import org.smoothbuild.io.fs.base.PathState;
 import org.smoothbuild.parse.err.SyntaxError;
 import org.smoothbuild.testing.integration.IntegrationTestCase;
 
@@ -17,5 +22,39 @@ public class ArraysSmoothTest extends IntegrationTestCase {
 
     // then
     userConsole.assertOnlyProblem(SyntaxError.class);
+  }
+
+  @Test
+  public void empty_array_can_be_saved() throws IOException {
+    // given
+    script("run : [];");
+
+    // when
+    build("run");
+
+    // then
+    userConsole.assertNoProblems();
+
+    Path artifactPath = RESULTS_PATH.append(path("run"));
+    assertThat(fileSystem.pathState(artifactPath)).isEqualTo(PathState.DIR);
+    assertThat(fileSystem.childNames(artifactPath)).isEmpty();
+  }
+
+  @Test
+  public void empty_file_array_can_be_saved() throws IOException {
+    // given
+    Path path = path("some/dir");
+    fileSystem.createDir(path);
+    script("run : files(" + path + ");");
+
+    // when
+    build("run");
+
+    // then
+    userConsole.assertNoProblems();
+
+    Path artifactPath = RESULTS_PATH.append(path("run"));
+    assertThat(fileSystem.pathState(artifactPath)).isEqualTo(PathState.DIR);
+    assertThat(fileSystem.childNames(artifactPath)).isEmpty();
   }
 }
