d3e1f52d1ec8aded6a72282d5719bbaab482ecc3
mikosik <marcin.mikosik@gmail.com>
2013-12-03 22:22:45 +0100

    reused LineBuilder in DuplicatePathsInFileArrayArtifactErrorTest

--- src/test/org/smoothbuild/task/exec/save/err/DuplicatePathsInFileArrayArtifactErrorTest.java
+++ src/test/org/smoothbuild/task/exec/save/err/DuplicatePathsInFileArrayArtifactErrorTest.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 import org.smoothbuild.io.fs.base.Path;
 import org.smoothbuild.lang.function.base.Name;
 import org.smoothbuild.message.base.Message;
+import org.smoothbuild.util.LineBuilder;
 
 import com.google.common.collect.ImmutableList;
 
@@ -19,12 +20,12 @@ public class DuplicatePathsInFileArrayArtifactErrorTest {
     Iterable<Path> fileSet = ImmutableList.of(path("path1"), path("path2"), path("path3"));
     Message error = new DuplicatePathsInFileArrayArtifactError(name, fileSet);
 
-    StringBuilder builder = new StringBuilder();
-    builder.append("Can't store result of 'func' as it contains files with duplicated paths:");
-    builder.append("\n  'path1'");
-    builder.append("\n  'path2'");
-    builder.append("\n  'path3'");
+    LineBuilder builder = new LineBuilder();
+    builder.addLine("Can't store result of 'func' as it contains files with duplicated paths:");
+    builder.addLine("  'path1'");
+    builder.addLine("  'path2'");
+    builder.add("  'path3'");
 
-    assertThat(error.message()).isEqualTo(builder.toString());
+    assertThat(error.message()).isEqualTo(builder.build());
   }
 }
