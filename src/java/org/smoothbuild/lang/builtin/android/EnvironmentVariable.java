9eae28c4649bcb0f8893a10a04c0b94e89c36d77
mikosik <marcin.mikosik@gmail.com>
2014-01-14 16:14:25 +0100

    added EnvironmentVariable

---
+++ src/java/org/smoothbuild/lang/builtin/android/EnvironmentVariable.java
@@ -0,0 +1,33 @@
+package org.smoothbuild.lang.builtin.android;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.common.base.Preconditions.checkState;
+
+public class EnvironmentVariable {
+  private final String name;
+  private final String value;
+  private final boolean isSet;
+
+  public static EnvironmentVariable environmentVariable(String variableName) {
+    return new EnvironmentVariable(variableName, System.getenv(variableName));
+  }
+
+  protected EnvironmentVariable(String name, String value) {
+    this.name = checkNotNull(name);
+    this.value = value;
+    this.isSet = value != null;
+  }
+
+  public String name() {
+    return name;
+  }
+
+  public String value() {
+    checkState(isSet, "Cannot call EnvironmentVariable.value() when isSet() == false");
+    return value;
+  }
+
+  public boolean isSet() {
+    return isSet;
+  }
+}
