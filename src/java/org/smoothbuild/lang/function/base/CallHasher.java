3769b5934f00f2984e836932e0eadb6367347b47
mikosik <marcin.mikosik@gmail.com>
2014-01-14 15:39:20 +0100

    added comment to CallHasher

--- src/java/org/smoothbuild/lang/function/base/CallHasher.java
+++ src/java/org/smoothbuild/lang/function/base/CallHasher.java
@@ -3,12 +3,17 @@ package org.smoothbuild.lang.function.base;
 import java.util.Map;
 
 import org.smoothbuild.io.cache.hash.Hash;
+import org.smoothbuild.io.cache.task.TaskDb;
 import org.smoothbuild.lang.function.nativ.NativeFunction;
 import org.smoothbuild.task.base.Result;
 
 import com.google.common.hash.HashCode;
 import com.google.common.hash.Hasher;
 
+/**
+ * Calculates hash of given function call (function + its arguments) so it can
+ * be used as a key for referencing its cached result in {@link TaskDb}.
+ */
 public class CallHasher {
   private final Function function;
   private final Map<String, Result> args;
@@ -19,8 +24,20 @@ public class CallHasher {
   }
 
   public HashCode hash() {
+
+    //
+    // Hash of given function call is calculated as a hash of byte array
+    // created by concatenating byte representation of following hashes:
+    // - hash of function being called
+    // - for each argument (sorted lexicographically by parameter name):
+    // ---- hash of parameter name
+    // ---- hash of argument value
+    //
+
     Hasher hasher = Hash.function().newHasher();
     hasher.putBytes(Hash.string(function.signature().name().value()).asBytes());
+
+    // function.params() are sorted lexicographically
     for (Param param : function.params().values()) {
       Result argument = args.get(param.name());
       if (argument != null) {
