a1f0b8a43b8b52ac389c0dc3593c1d36303089fd
mikosik <marcin.mikosik@gmail.com>
2014-01-14 14:58:41 +0100

    added Param.nameHash()

--- src/test/org/smoothbuild/lang/function/base/ParamTest.java
+++ src/test/org/smoothbuild/lang/function/base/ParamTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.smoothbuild.lang.function.base.Param.param;
 import static org.smoothbuild.lang.function.base.Param.paramsToString;
+import static org.smoothbuild.lang.type.STypes.BLOB;
 import static org.smoothbuild.lang.type.STypes.EMPTY_ARRAY;
 import static org.smoothbuild.lang.type.STypes.FILE_ARRAY;
 import static org.smoothbuild.lang.type.STypes.STRING;
@@ -18,6 +19,7 @@ import org.smoothbuild.lang.function.def.Node;
 import org.smoothbuild.lang.type.SType;
 import org.smoothbuild.util.LineBuilder;
 
+import com.google.common.hash.HashCode;
 import com.google.common.testing.EqualsTester;
 
 public class ParamTest {
@@ -50,6 +52,21 @@ public class ParamTest {
   @Test
   public void isRequired() throws Exception {
     assertThat(param(STRING, "name", true).isRequired()).isTrue();
+  }
+
+  @Test
+  public void params_with_different_names_have_different_name_hashes() throws Exception {
+    HashCode hash1 = param(STRING, "name1", true).nameHash();
+    HashCode hash2 = param(STRING, "name2", true).nameHash();
+    assertThat(hash1).isNotEqualTo(hash2);
+  }
+
+  @Test
+  public void params_with_same_names_but_different_types_have_the_same_name_hashes()
+      throws Exception {
+    HashCode hash1 = param(STRING, "name1", true).nameHash();
+    HashCode hash2 = param(BLOB, "name1", true).nameHash();
+    assertThat(hash1).isEqualTo(hash2);
   }
 
   @Test
