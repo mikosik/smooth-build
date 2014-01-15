948bd7d4e4cc8a6cd506e7db9285fa34328cc1b9
mikosik <marcin.mikosik@gmail.com>
2014-01-15 17:44:26 +0100

    style fixes in SValueBuilders

--- src/java/org/smoothbuild/lang/type/SValueBuilders.java
+++ src/java/org/smoothbuild/lang/type/SValueBuilders.java
@@ -5,13 +5,11 @@ import org.smoothbuild.io.cache.value.build.BlobBuilder;
 import org.smoothbuild.io.cache.value.build.FileBuilder;
 
 public interface SValueBuilders {
+  public <T extends SValue> ArrayBuilder<T> arrayBuilder(SArrayType<T> arrayType);
 
-  public abstract <T extends SValue> ArrayBuilder<T> arrayBuilder(SArrayType<T> arrayType);
+  public FileBuilder fileBuilder();
 
-  public abstract FileBuilder fileBuilder();
+  public BlobBuilder blobBuilder();
 
-  public abstract BlobBuilder blobBuilder();
-
-  public abstract SString string(String string);
-
+  public SString string(String string);
 }
