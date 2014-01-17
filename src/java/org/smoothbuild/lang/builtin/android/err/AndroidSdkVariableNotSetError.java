2a029b257950c7f54c3a781b71dfe6fba21f9312
mikosik <marcin.mikosik@gmail.com>
2014-01-17 12:20:19 +0100

    added AndroidSdk

---
+++ src/java/org/smoothbuild/lang/builtin/android/err/AndroidSdkVariableNotSetError.java
@@ -0,0 +1,13 @@
+package org.smoothbuild.lang.builtin.android.err;
+
+import static org.smoothbuild.message.base.MessageType.ERROR;
+
+import org.smoothbuild.message.base.Message;
+import org.smoothbuild.util.EnvironmentVariable;
+
+public class AndroidSdkVariableNotSetError extends Message {
+  public AndroidSdkVariableNotSetError(EnvironmentVariable androidSdkVar) {
+    super(ERROR, "Environment variable " + androidSdkVar.name() + " is not set.\n"
+        + "It should contain absolute path to android SDK.");
+  }
+}
