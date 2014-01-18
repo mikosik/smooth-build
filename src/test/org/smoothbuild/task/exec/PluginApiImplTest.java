9ac702fc30b12bff495a80d9cf2d27801a338fc2
mikosik <marcin.mikosik@gmail.com>
2014-01-18 07:28:54 +0100

    removed dead method from PluginApiImplTest

--- src/test/org/smoothbuild/task/exec/PluginApiImplTest.java
+++ src/test/org/smoothbuild/task/exec/PluginApiImplTest.java
@@ -1,24 +1,18 @@
 package org.smoothbuild.task.exec;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
 import static org.smoothbuild.message.base.MessageType.ERROR;
 
 import org.junit.Test;
-import org.mockito.Mockito;
 import org.smoothbuild.io.cache.value.build.SValueBuildersImpl;
 import org.smoothbuild.io.temp.TempDirectoryManager;
 import org.smoothbuild.lang.type.SValueBuilders;
 import org.smoothbuild.message.base.Message;
-import org.smoothbuild.task.base.Task;
 import org.smoothbuild.testing.io.cache.value.FakeValueDb;
 import org.smoothbuild.testing.io.fs.base.FakeFileSystem;
 import org.smoothbuild.testing.io.temp.FakeTempDirectoryManager;
-import org.smoothbuild.testing.message.FakeCodeLocation;
 
 public class PluginApiImplTest {
-  Task task = task();
-
   FakeFileSystem fileSystem = new FakeFileSystem();
   FakeValueDb valueDb = new FakeValueDb(fileSystem);
   SValueBuilders valueBuilders = new SValueBuildersImpl(valueDb);
@@ -36,12 +30,5 @@ public class PluginApiImplTest {
     Message errorMessage = new Message(ERROR, "message");
     pluginApi.log(errorMessage);
     assertThat(pluginApi.loggedMessages()).containsOnly(errorMessage);
-  }
-
-  private static Task task() {
-    Task task = mock(Task.class);
-    Mockito.when(task.name()).thenReturn("name");
-    Mockito.when(task.codeLocation()).thenReturn(new FakeCodeLocation());
-    return task;
   }
 }
