9147c5c7674b3239affd4ad0a125a4f36bbe083b
Marcin Mikosik <marcin.mikosik@gmail.com>
2014-03-23 12:28:31 +0100

    added BlobTester

---
+++ src/testing/org/smoothbuild/testing/lang/type/BlobTester.java
@@ -0,0 +1,13 @@
+package org.smoothbuild.testing.lang.type;
+
+import static org.smoothbuild.testing.common.StreamTester.assertContent;
+
+import java.io.IOException;
+
+import org.smoothbuild.lang.type.SBlob;
+
+public class BlobTester {
+  public static void assertContains(SBlob blob, String content) throws IOException {
+    assertContent(blob.openInputStream(), content);
+  }
+}
