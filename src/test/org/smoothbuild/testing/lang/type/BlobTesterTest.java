2dbb1c24e8f1d653baa8a4ec53c2c0ccb1d0766c
Marcin Mikosik <marcin.mikosik@gmail.com>
2014-03-23 12:28:31 +0100

    added BlobTester

---
+++ src/test/org/smoothbuild/testing/lang/type/BlobTesterTest.java
@@ -0,0 +1,34 @@
+package org.smoothbuild.testing.lang.type;
+
+import static org.junit.Assert.fail;
+import static org.smoothbuild.io.fs.base.Path.path;
+
+import java.io.IOException;
+
+import org.junit.Test;
+import org.smoothbuild.io.fs.base.Path;
+import org.smoothbuild.lang.type.SBlob;
+
+public class BlobTesterTest {
+  String content = "some content";
+  Path path = path("my/path");
+
+  @Test
+  public void asserting_content_succeeds_when_blob_content_equals_expected() throws IOException {
+    SBlob blob = new FakeBlob(content);
+    BlobTester.assertContains(blob, content);
+  }
+
+  @Test
+  public void asserting_content_fails_when_blob_content_does_not_equal_expected()
+      throws IOException {
+    SBlob blob = new FakeBlob(content);
+    try {
+      BlobTester.assertContains(blob, content + "suffix");
+    } catch (AssertionError e) {
+      // expected
+      return;
+    }
+    fail("exception should be thrown");
+  }
+}
