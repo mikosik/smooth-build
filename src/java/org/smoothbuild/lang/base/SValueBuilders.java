a7bbcbb72a52e148958db2dc1e9a86e8c0930662
Marcin Mikosik <marcin.mikosik@gmail.com>
2014-04-13 17:17:32 +0200

    renamed SValueBuildersImpl -> ObjectBuilders

--- src/java/org/smoothbuild/lang/base/SValueBuilders.java
+++ src/java/org/smoothbuild/lang/base/SValueBuilders.java
@@ -3,11 +3,11 @@ package org.smoothbuild.lang.base;
 import org.smoothbuild.db.objects.build.ArrayBuilder;
 import org.smoothbuild.db.objects.build.BlobBuilder;
 import org.smoothbuild.db.objects.build.FileBuilder;
-import org.smoothbuild.db.objects.build.SValueBuildersImpl;
+import org.smoothbuild.db.objects.build.ObjectBuilders;
 
 import com.google.inject.ImplementedBy;
 
-@ImplementedBy(SValueBuildersImpl.class)
+@ImplementedBy(ObjectBuilders.class)
 public interface SValueBuilders {
   public <T extends SValue> ArrayBuilder<T> arrayBuilder(SArrayType<T> arrayType);
 
