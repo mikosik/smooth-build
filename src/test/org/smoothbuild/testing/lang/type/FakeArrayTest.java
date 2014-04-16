5dac08bf96481421a721a45d45842457d8152a9d
Marcin Mikosik <marcin.mikosik@gmail.com>
2014-04-16 17:58:22 +0200

    changed FakeArray.hash() so it is compatible with ArrayObject.hash()

--- src/test/org/smoothbuild/testing/lang/type/FakeArrayTest.java
+++ src/test/org/smoothbuild/testing/lang/type/FakeArrayTest.java
@@ -9,35 +9,37 @@ import static org.testory.Testory.thenReturned;
 import static org.testory.Testory.when;
 
 import org.junit.Test;
+import org.smoothbuild.lang.base.SArray;
 import org.smoothbuild.lang.base.SFile;
+import org.smoothbuild.testing.db.objects.FakeObjectsDb;
 
 import com.google.common.collect.Iterables;
 
 public class FakeArrayTest {
   FakeFile file;
   FakeFile file2;
-  FakeArray<SFile> fileArray;
+  FakeArray<SFile> array;
 
   @Test
   public void type() throws Exception {
-    given(fileArray = new FakeArray<SFile>(FILE_ARRAY));
-    when(fileArray.type());
+    given(array = new FakeArray<SFile>(FILE_ARRAY));
+    when(array.type());
     thenReturned(FILE_ARRAY);
   }
 
   @Test
   public void initially_file_array_is_empty() throws Exception {
-    given(fileArray = new FakeArray<SFile>(FILE_ARRAY));
-    when(Iterables.size(fileArray));
+    given(array = new FakeArray<SFile>(FILE_ARRAY));
+    when(Iterables.size(array));
     thenReturned(0);
   }
 
   @Test
   public void fake_file_array_contains_added_file() throws Exception {
-    given(fileArray = new FakeArray<SFile>(FILE_ARRAY));
+    given(array = new FakeArray<SFile>(FILE_ARRAY));
     given(file = new FakeFile(path("my/file")));
-    when(fileArray).add(file);
-    then(fileArray, contains(file));
+    when(array).add(file);
+    then(array, contains(file));
   }
 
   @Test
@@ -46,5 +48,18 @@ public class FakeArrayTest {
     given(file2 = new FakeFile(path("my/file2")));
     when(FakeArray.fakeArray(FILE_ARRAY, file, file2));
     thenReturned(contains(file, file2));
+  }
+
+  @Test
+  public void fake_array_hash_is_compatible_with_array_object_hash() throws Exception {
+    given(file = new FakeFile(path("my/file")));
+    given(file2 = new FakeFile(path("my/file2")));
+    given(array = FakeArray.fakeArray(FILE_ARRAY, file, file2));
+    when(array).hash();
+    thenReturned(objectArray(file, file2).hash());
+  }
+
+  private static SArray<SFile> objectArray(SFile file1, SFile file2) {
+    return new FakeObjectsDb().arrayBuilder(FILE_ARRAY).add(file1).add(file2).build();
   }
 }
