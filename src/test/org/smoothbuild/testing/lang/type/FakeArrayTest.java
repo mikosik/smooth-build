4c3789ead2f5b9eecc5a3ebebe0f6caee8972e07
Marcin Mikosik <marcin.mikosik@gmail.com>
2014-04-16 18:17:31 +0200

    refactored FakeArrayTest

--- src/test/org/smoothbuild/testing/lang/type/FakeArrayTest.java
+++ src/test/org/smoothbuild/testing/lang/type/FakeArrayTest.java
@@ -1,8 +1,7 @@
 package org.smoothbuild.testing.lang.type;
 
 import static org.hamcrest.Matchers.contains;
-import static org.smoothbuild.io.fs.base.Path.path;
-import static org.smoothbuild.lang.base.STypes.FILE_ARRAY;
+import static org.smoothbuild.lang.base.STypes.STRING_ARRAY;
 import static org.testory.Testory.given;
 import static org.testory.Testory.then;
 import static org.testory.Testory.thenReturned;
@@ -10,56 +9,56 @@ import static org.testory.Testory.when;
 
 import org.junit.Test;
 import org.smoothbuild.lang.base.SArray;
-import org.smoothbuild.lang.base.SFile;
+import org.smoothbuild.lang.base.SString;
 import org.smoothbuild.testing.db.objects.FakeObjectsDb;
 
 import com.google.common.collect.Iterables;
 
 public class FakeArrayTest {
-  FakeFile file;
-  FakeFile file2;
-  FakeArray<SFile> array;
+  FakeString sstring1;
+  FakeString sstring2;
+  FakeArray<SString> array;
 
   @Test
   public void type() throws Exception {
-    given(array = new FakeArray<SFile>(FILE_ARRAY));
+    given(array = new FakeArray<>(STRING_ARRAY));
     when(array.type());
-    thenReturned(FILE_ARRAY);
+    thenReturned(STRING_ARRAY);
   }
 
   @Test
-  public void initially_file_array_is_empty() throws Exception {
-    given(array = new FakeArray<SFile>(FILE_ARRAY));
+  public void initially_array_is_empty() throws Exception {
+    given(array = new FakeArray<>(STRING_ARRAY));
     when(Iterables.size(array));
     thenReturned(0);
   }
 
   @Test
-  public void fake_file_array_contains_added_file() throws Exception {
-    given(array = new FakeArray<SFile>(FILE_ARRAY));
-    given(file = new FakeFile(path("my/file")));
-    when(array).add(file);
-    then(array, contains(file));
+  public void fake_array_contains_added_element() throws Exception {
+    given(array = new FakeArray<>(STRING_ARRAY));
+    given(sstring1 = new FakeString("my string"));
+    when(array).add(sstring1);
+    then(array, contains(sstring1));
   }
 
   @Test
-  public void fake_array_contains_files_passed_to_creation_method() throws Exception {
-    given(file = new FakeFile(path("my/file")));
-    given(file2 = new FakeFile(path("my/file2")));
-    when(FakeArray.fakeArray(FILE_ARRAY, file, file2));
-    thenReturned(contains(file, file2));
+  public void fake_array_contains_elements_passed_to_creation_method() throws Exception {
+    given(sstring1 = new FakeString("my string"));
+    given(sstring2 = new FakeString("my string B"));
+    when(FakeArray.fakeArray(STRING_ARRAY, sstring1, sstring2));
+    thenReturned(contains(sstring1, sstring2));
   }
 
   @Test
   public void fake_array_hash_is_compatible_with_array_object_hash() throws Exception {
-    given(file = new FakeFile(path("my/file")));
-    given(file2 = new FakeFile(path("my/file2")));
-    given(array = FakeArray.fakeArray(FILE_ARRAY, file, file2));
+    given(sstring1 = new FakeString("my string"));
+    given(sstring2 = new FakeString("my string B"));
+    given(array = FakeArray.fakeArray(STRING_ARRAY, sstring1, sstring2));
     when(array).hash();
-    thenReturned(objectArray(file, file2).hash());
+    thenReturned(objectArray(sstring1, sstring2).hash());
   }
 
-  private static SArray<SFile> objectArray(SFile file1, SFile file2) {
-    return new FakeObjectsDb().arrayBuilder(FILE_ARRAY).add(file1).add(file2).build();
+  private static SArray<SString> objectArray(SString elem1, SString elem2) {
+    return new FakeObjectsDb().arrayBuilder(STRING_ARRAY).add(elem1).add(elem2).build();
   }
 }
