16e98fb03ed44e0fa3fc0d487e27548d9aa797ce
Marcin Mikosik <marcin.mikosik@gmail.com>
2014-04-18 14:19:59 +0200

    refactored BinaryNameToClassFileTest

--- src/test/org/smoothbuild/lang/builtin/java/junit/BinaryNameToClassFileTest.java
+++ src/test/org/smoothbuild/lang/builtin/java/junit/BinaryNameToClassFileTest.java
@@ -1,33 +1,37 @@
 package org.smoothbuild.lang.builtin.java.junit;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.smoothbuild.io.fs.base.Path.path;
 import static org.smoothbuild.lang.builtin.java.junit.BinaryNameToClassFile.binaryNameToClassFile;
+import static org.testory.Testory.thenReturned;
+import static org.testory.Testory.when;
 
 import java.io.IOException;
 import java.util.Map;
 
 import org.junit.Test;
+import org.smoothbuild.io.fs.base.Path;
 import org.smoothbuild.lang.base.SBlob;
 import org.smoothbuild.lang.base.SFile;
 import org.smoothbuild.testing.common.JarTester;
-import org.smoothbuild.testing.lang.type.FileTester;
+import org.smoothbuild.testing.db.objects.FakeObjectsDb;
 import org.smoothbuild.testing.task.exec.FakeNativeApi;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
 
 public class BinaryNameToClassFileTest {
+  private final FakeObjectsDb objectsDb = new FakeObjectsDb();
 
   @Test
   public void binaryNamesAreMappedToProperClassFiles() throws IOException {
-    String file1 = "a/Klass.class";
-    String file2 = "b/Klass.class";
-    SBlob blob = JarTester.jar(file1, file2);
-    Map<String, SFile> x =
-        binaryNameToClassFile(new FakeNativeApi(), ImmutableList.<SBlob> of(blob));
+    Path path1 = path("a/Klass.class");
+    Path path2 = path("b/Klass.class");
+    SBlob blob = JarTester.jar(path1.value(), path2.value());
 
-    FileTester.assertContentContains(x.get("a.Klass"), file1);
-    FileTester.assertContentContains(x.get("b.Klass"), file2);
-    assertThat(x.size()).isEqualTo(2);
+    when(binaryNameToClassFile(new FakeNativeApi(), ImmutableList.of(blob)));
+    thenReturned(ImmutableMap
+        .of("a.Klass", objectsDb.file(path1), "b.Klass", objectsDb.file(path2)));
   }
 
   @Test
