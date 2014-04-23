bbea78361b45490f67768f49bc8e076ef46276a3
Marcin Mikosik <marcin.mikosik@gmail.com>
2014-04-23 16:56:39 +0200

    moved Signature.createParamsMap() to Params.map()

---
+++ src/java/org/smoothbuild/lang/function/base/Params.java
@@ -0,0 +1,37 @@
+package org.smoothbuild.lang.function.base;
+
+import java.util.Arrays;
+import java.util.Set;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Sets;
+
+public class Params {
+  public static ImmutableMap<String, Param> map(Param... params) {
+    return map(Arrays.asList(params));
+  }
+
+  /**
+   * @return Parameters ordered lexicographically by their names. Methods
+   *         values(), keySet(), entrySet() of returned map returns collections
+   *         which elements keep that order.
+   */
+  public static ImmutableMap<String, Param> map(Iterable<Param> params) {
+    Set<String> names = Sets.newHashSet();
+
+    /*
+     * ImmutableMap keeps order of elements.
+     */
+    ImmutableMap.Builder<String, Param> builder = ImmutableMap.builder();
+
+    for (Param param : ParamOrdering.PARAM_ORDERING.sortedCopy(params)) {
+      String name = param.name();
+      if (names.contains(name)) {
+        throw new IllegalArgumentException("Duplicate param name = '" + name + "'");
+      }
+      builder.put(name, param);
+      names.add(name);
+    }
+    return builder.build();
+  }
+}
---
+++ src/test/org/smoothbuild/lang/function/base/ParamsTest.java
@@ -0,0 +1,35 @@
+package org.smoothbuild.lang.function.base;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.smoothbuild.lang.base.STypes.STRING;
+import static org.smoothbuild.lang.function.base.Param.param;
+
+import org.junit.Test;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+
+public class ParamsTest {
+
+  @Test
+  public void paramsAreSortedAccordingToName() throws Exception {
+    String name1 = "aaa";
+    String name2 = "bbb";
+    String name3 = "ccc";
+    String name4 = "ddd";
+    String name5 = "eee";
+    String name6 = "fff";
+    Param param1 = param(STRING, name1, false);
+    Param param2 = param(STRING, name2, false);
+    Param param3 = param(STRING, name3, false);
+    Param param4 = param(STRING, name4, false);
+    Param param5 = param(STRING, name5, false);
+    Param param6 = param(STRING, name6, false);
+
+    ImmutableMap<String, Param> map =
+        Params.map(ImmutableList.of(param4, param6, param1, param3, param5, param2));
+
+    assertThat(map.values()).containsExactly(param1, param2, param3, param4, param5, param6);
+    assertThat(map.keySet()).containsExactly(name1, name2, name3, name4, name5, name6);
+  }
+}
