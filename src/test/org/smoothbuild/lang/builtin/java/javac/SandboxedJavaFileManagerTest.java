4c41551cd0f1ed05e7823f2dc1770f86942a20f5
Marcin Mikosik <marcin.mikosik@gmail.com>
2014-04-23 16:33:35 +0200

    refactored SandboxedJavaFileManagerTest to use testory

--- src/test/org/smoothbuild/lang/builtin/java/javac/SandboxedJavaFileManagerTest.java
+++ src/test/org/smoothbuild/lang/builtin/java/javac/SandboxedJavaFileManagerTest.java
@@ -1,15 +1,16 @@
 package org.smoothbuild.lang.builtin.java.javac;
 
-import static org.junit.Assert.fail;
+import static javax.tools.StandardLocation.CLASS_OUTPUT;
+import static org.testory.Testory.given;
 import static org.testory.Testory.mock;
 import static org.testory.Testory.onInstance;
 import static org.testory.Testory.thenCalledTimes;
+import static org.testory.Testory.thenThrown;
 import static org.testory.Testory.when;
 
 import javax.tools.JavaFileObject;
 import javax.tools.JavaFileObject.Kind;
 import javax.tools.StandardJavaFileManager;
-import javax.tools.StandardLocation;
 
 import org.junit.Test;
 import org.smoothbuild.lang.base.SValueFactory;
@@ -19,32 +20,26 @@ import org.smoothbuild.testing.db.objects.FakeObjectsDb;
 import com.google.common.collect.Multimap;
 
 public class SandboxedJavaFileManagerTest {
-  StandardJavaFileManager sfm = mock(StandardJavaFileManager.class);
-  SValueFactory valueFactory = new FakeObjectsDb();
+  private final StandardJavaFileManager sfm = mock(StandardJavaFileManager.class);
+  private final SValueFactory valueFactory = new FakeObjectsDb();
   @SuppressWarnings("unchecked")
-  Multimap<String, JavaFileObject> packagedJavaFileObjects = mock(Multimap.class);
+  private final Multimap<String, JavaFileObject> packagedJavaFileObjects = mock(Multimap.class);
 
-  SandboxedJavaFileManager manager = new SandboxedJavaFileManager(sfm, valueFactory,
-      packagedJavaFileObjects);
+  private SandboxedJavaFileManager manager;
 
   @Test
   public void getJavaFile_output_is_not_forwarded_to_standard_manager_for_class_output()
       throws Exception {
-    when(manager)
-        .getJavaFileForOutput(StandardLocation.CLASS_OUTPUT, "className", Kind.CLASS, null);
+    given(manager = new SandboxedJavaFileManager(sfm, valueFactory, packagedJavaFileObjects));
+    when(manager).getJavaFileForOutput(CLASS_OUTPUT, "className", Kind.CLASS, null);
     thenCalledTimes(0, onInstance(sfm));
   }
 
   @Test
   public void getJavaFileOutput_logs_error_when_class_name_is_illegal() throws Exception {
-    String className = ".illegal.MyClass";
-
-    try {
-      manager.getJavaFileForOutput(StandardLocation.CLASS_OUTPUT, className, Kind.CLASS, null);
-      fail("exception should be thrown");
-    } catch (IncorrectClassNameGivenByJavaCompilerError e) {
-      // expected
-    }
+    given(manager = new SandboxedJavaFileManager(sfm, valueFactory, packagedJavaFileObjects));
+    when(manager).getJavaFileForOutput(CLASS_OUTPUT, ".illegal.MyClass", Kind.CLASS, null);
+    thenThrown(IncorrectClassNameGivenByJavaCompilerError.class);
     thenCalledTimes(0, onInstance(sfm));
   }
 }
