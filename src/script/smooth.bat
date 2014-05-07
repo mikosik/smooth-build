6d2e09e96a159a2aab47dcabdaf10c98d0f7d16f
Marcin Mikosik <marcin.mikosik@gmail.com>
2014-05-07 15:03:19 +0200

    moved funcs.jar to lib directory in distribution zip file

--- src/script/smooth.bat
+++ src/script/smooth.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
 
 SET SMOOTH_HOME=%~dp
-java -cp %SMOOTH_HOME%\smooth.jar;%SMOOTH_HOME%\funcs.jar org.smoothbuild.Main %*
+java -cp %SMOOTH_HOME%\smooth.jar;%SMOOTH_HOME%\lib\funcs.jar org.smoothbuild.Main %*
 
