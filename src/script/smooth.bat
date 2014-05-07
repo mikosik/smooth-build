d92b30f651065b4732dea784b46504a5bf871c7f
Marcin Mikosik <marcin.mikosik@gmail.com>
2014-05-07 12:53:07 +0200

    renamed SMOOTH_DIR -> SMOOTH_HOME in bash/batch scripts

--- src/script/smooth.bat
+++ src/script/smooth.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
 
-SET SMOOTH_DIR=%~dp
-java -cp %SMOOTH_DIR%/smooth.jar;%SMOOTH_DIR%/funcs.jar org.smoothbuild.Main %*
+SET SMOOTH_HOME=%~dp
+java -cp %SMOOTH_HOME%/smooth.jar;%SMOOTH_HOME%/funcs.jar org.smoothbuild.Main %*
 
