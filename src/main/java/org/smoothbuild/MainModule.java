8ceb7e08d3c8dc129e82a4d596925b0205abb02b
Marcin Mikosik <marcin.mikosik@gmail.com>
2014-05-07 15:38:07 +0200

    extracted ModuleModule from MainModule

--- src/main/java/org/smoothbuild/MainModule.java
+++ src/main/java/org/smoothbuild/MainModule.java
@@ -1,17 +1,11 @@
 package org.smoothbuild;
 
-import javax.inject.Singleton;
-
 import org.smoothbuild.db.objects.ObjectsDbModule;
 import org.smoothbuild.db.taskoutputs.TaskOutputsDbModule;
 import org.smoothbuild.io.fs.FileSystemModule;
-import org.smoothbuild.lang.function.nativ.err.NativeImplementationException;
-import org.smoothbuild.lang.module.Module;
-import org.smoothbuild.lang.module.NativeModuleFactory;
-import org.smoothbuild.parse.Builtin;
+import org.smoothbuild.lang.module.ModuleModule;
 
 import com.google.inject.AbstractModule;
-import com.google.inject.Provides;
 
 public class MainModule extends AbstractModule {
   @Override
@@ -19,20 +13,6 @@ public class MainModule extends AbstractModule {
     install(new TaskOutputsDbModule());
     install(new ObjectsDbModule());
     install(new FileSystemModule());
-  }
-
-  @Provides
-  @Singleton
-  @Builtin
-  public Module provideBuiltinModule() throws NativeImplementationException {
-    return NativeModuleFactory.createNativeModule(builtinModuleClass(), true);
-  }
-
-  private Class<?> builtinModuleClass() {
-    try {
-      return Class.forName("org.smoothbuild.builtin.BuiltinSmoothModule");
-    } catch (ClassNotFoundException e) {
-      throw new RuntimeException(e);
-    }
+    install(new ModuleModule());
   }
 }
