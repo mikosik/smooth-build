6aa65272af54a56bf70108e44b670ca346c8c375
Marcin Mikosik <marcin.mikosik@gmail.com>
2014-05-09 11:31:30 +0200

    migrated InputSourceFileTest from assertj to testory

--- src/func/test/org/smoothbuild/builtin/java/javac/InputSourceFileTest.java
+++ src/func/test/org/smoothbuild/builtin/java/javac/InputSourceFileTest.java
@@ -1,7 +1,9 @@
 package org.smoothbuild.builtin.java.javac;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.smoothbuild.io.fs.base.Path.path;
+import static org.testory.Testory.given;
+import static org.testory.Testory.thenReturned;
+import static org.testory.Testory.when;
 
 import java.io.IOException;
 
@@ -12,22 +14,24 @@ import org.smoothbuild.testing.db.objects.FakeObjectsDb;
 
 public class InputSourceFileTest {
   private final FakeObjectsDb objectsDb = new FakeObjectsDb();
+  private Path path;
+  private String content;
+  private SFile file;
 
   @Test
-  public void getCharContent() throws IOException {
-    Path path = path("my/path");
-    SFile file = objectsDb.file(path);
-
-    CharSequence actual = new InputSourceFile(file).getCharContent(true);
-
-    assertThat(actual).isEqualTo(path.value());
+  public void get_char_content_returns_file_content() throws IOException {
+    given(path = path("my/path"));
+    given(content = "some content");
+    given(file = objectsDb.file(path, content));
+    when(new InputSourceFile(file)).getCharContent(true);
+    thenReturned(content);
   }
 
   @Test
   public void uri() throws Exception {
-    String pathString = "my/path/file";
-    SFile file = objectsDb.file(path(pathString));
-    InputSourceFile inputSourceFile = new InputSourceFile(file);
-    assertThat(inputSourceFile.getName()).isEqualTo("/" + pathString);
+    given(path = path("my/path"));
+    given(file = objectsDb.file(path));
+    when(new InputSourceFile(file)).getName();
+    thenReturned("/" + path.value());
   }
 }
