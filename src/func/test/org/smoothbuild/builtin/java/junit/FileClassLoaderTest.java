d247084720cfa3e9905975e9ce0a3ca47a89e7ed
Marcin Mikosik <marcin.mikosik@gmail.com>
2014-05-10 16:17:35 +0200

    migrated FileClassLoaderTest from assertj to testory

--- src/func/test/org/smoothbuild/builtin/java/junit/FileClassLoaderTest.java
+++ src/func/test/org/smoothbuild/builtin/java/junit/FileClassLoaderTest.java
@@ -1,12 +1,14 @@
 package org.smoothbuild.builtin.java.junit;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.smoothbuild.io.fs.base.Path.path;
 import static org.smoothbuild.testing.common.StreamTester.inputStreamToBytes;
+import static org.testory.Testory.given;
+import static org.testory.Testory.thenReturned;
+import static org.testory.Testory.when;
+import static org.testory.common.Matchers.same;
 
+import java.io.IOException;
 import java.io.InputStream;
-import java.lang.reflect.Method;
-import java.util.Map;
 
 import org.junit.Test;
 import org.smoothbuild.lang.base.SFile;
@@ -16,29 +18,25 @@ import com.google.common.collect.ImmutableMap;
 
 public class FileClassLoaderTest {
   private final FakeObjectsDb objectsDb = new FakeObjectsDb();
+  private FileClassLoader fileClassLoader;
+  private SFile file;
+  private Class<MyClass> klass;
 
   @Test
-  public void testClassLoading() throws Exception {
-    // Do not use MyClass.class literal to avoid loading MyClass by current
-    // ClassLoader.
-    String klassBinaryName = FileClassLoaderTest.class.getName() + "$MyClass";
-    String filePath = klassBinaryName.replace('.', '/') + ".class";
+  public void loads_class_from_binary() throws Exception {
+    given(klass = FileClassLoaderTest.MyClass.class);
+    given(file = createByteCodeFile(klass));
+    given(fileClassLoader = new FileClassLoader(ImmutableMap.of(klass.getName(), file)));
+    when(fileClassLoader.findClass(klass.getName()).getClassLoader());
+    thenReturned(same(fileClassLoader));
+  }
 
-    InputStream classByteCode = this.getClass().getClassLoader().getResourceAsStream(filePath);
-
-    SFile file =
-        objectsDb.file(path("this/path/is/ignored/anyway"), inputStreamToBytes(classByteCode));
-
-    Map<String, SFile> binaryNameToFile = ImmutableMap.<String, SFile> of(klassBinaryName, file);
-    FileClassLoader fileClassLoader = new FileClassLoader(binaryNameToFile);
-
-    Class<?> klass = fileClassLoader.findClass(klassBinaryName);
-    assertThat(klass.getClassLoader()).isSameAs(fileClassLoader);
-
-    Method method = klass.getMethod("myMethod");
-    Object result = method.invoke(null);
-
-    assertThat(result).isEqualTo("myResult");
+  private SFile createByteCodeFile(Class<?> klass) throws IOException {
+    String binaryName = klass.getName();
+    String binaryPath = binaryName.replace('.', '/') + ".class";
+    InputStream is = klass.getClassLoader().getResourceAsStream(binaryPath);
+    byte[] byteCode = inputStreamToBytes(is);
+    return objectsDb.file(path(binaryPath), byteCode);
   }
 
   public static class MyClass {
