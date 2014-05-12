4ea21f9608d335a354c4096dd8e2c7567d0cefd2
Marcin Mikosik <marcin.mikosik@gmail.com>
2014-05-12 17:45:58 +0200

    migrated TempDirectoryTest from assertj to testory

--- src/main/test/org/smoothbuild/io/temp/TempDirectoryTest.java
+++ src/main/test/org/smoothbuild/io/temp/TempDirectoryTest.java
@@ -1,18 +1,20 @@
 package org.smoothbuild.io.temp;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.contains;
-import static org.junit.Assert.fail;
 import static org.smoothbuild.io.fs.base.Path.path;
 import static org.smoothbuild.lang.base.STypes.FILE_ARRAY;
-import static org.smoothbuild.testing.common.StreamTester.assertContent;
+import static org.smoothbuild.util.Streams.inputStreamToString;
 import static org.testory.Testory.given;
+import static org.testory.Testory.then;
 import static org.testory.Testory.thenReturned;
+import static org.testory.Testory.thenThrown;
 import static org.testory.Testory.when;
 
+import java.io.IOException;
 import java.nio.file.Paths;
 
 import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.smoothbuild.io.fs.base.Path;
 import org.smoothbuild.lang.base.SArray;
@@ -20,15 +22,24 @@ import org.smoothbuild.lang.base.SBlob;
 import org.smoothbuild.lang.base.SFile;
 import org.smoothbuild.testing.db.objects.FakeObjectsDb;
 import org.smoothbuild.testing.io.fs.base.FakeFileSystem;
+import org.testory.common.Matcher;
 
 public class TempDirectoryTest {
   private final Path path = path("my/path");
   private final String content = "content";
-
-  private final FakeObjectsDb objectsDb = new FakeObjectsDb();
   private final java.nio.file.Path rootPath = Paths.get("/fake/path");
-  private final FakeFileSystem fileSystem = new FakeFileSystem();
-  private TempDirectory tempDirectory = new TempDirectory(objectsDb, rootPath, fileSystem);
+
+  private FakeObjectsDb objectsDb;
+  private FakeFileSystem fileSystem;
+  private TempDirectory tempDirectory;
+  private SArray<SFile> array;
+
+  @Before
+  public void before() {
+    objectsDb = new FakeObjectsDb();
+    fileSystem = new FakeFileSystem();
+    tempDirectory = new TempDirectory(objectsDb, rootPath, fileSystem);
+  }
 
   @After
   public void after() {
@@ -42,110 +53,90 @@ public class TempDirectoryTest {
 
   @Test
   public void rootOsPath() {
-    assertThat(tempDirectory.rootOsPath()).isEqualTo(rootPath.toString());
+    when(tempDirectory.rootOsPath());
+    thenReturned(rootPath.toString());
   }
 
   @Test
   public void file_is_written_to_file_system() throws Exception {
-    SFile file = objectsDb.file(path, content);
-    tempDirectory.writeFile(file);
-    fileSystem.assertFileContains(path, content);
+    when(tempDirectory).writeFile(objectsDb.file(path, content));
+    then(inputStreamToString(fileSystem.openInputStream(path)).equals(content));
   }
 
   @Test
   public void writing_file_after_destroy_throws_exception() throws Exception {
-    tempDirectory.destroy();
-
-    SFile file = objectsDb.file(path, content);
-
-    try {
-      tempDirectory.writeFile(file);
-      fail("exception should be thrown");
-    } catch (IllegalStateException e) {
-      // expected
-    }
+    given(tempDirectory).destroy();
+    when(tempDirectory).writeFile(objectsDb.file(path, content));
+    thenThrown(IllegalStateException.class);
   }
 
   @Test
-  public void content_and_path_is_written_to_file_system() throws Exception {
-    tempDirectory.writeFile(path, objectsDb.blob(content));
-    fileSystem.assertFileContains(path, content);
+  public void path_and_content_are_written_to_file_system() throws Exception {
+    when(tempDirectory).writeFile(path, objectsDb.blob(content));
+    then(inputStreamToString(fileSystem.openInputStream(path)).equals(content));
   }
 
   @Test
   public void writing_content_after_destroy_throws_exception() throws Exception {
-    tempDirectory.destroy();
-
-    try {
-      tempDirectory.writeFile(path, objectsDb.blob(content));
-      fail("exception should be thrown");
-    } catch (IllegalStateException e) {
-      // expected
-    }
+    given(tempDirectory).destroy();
+    when(tempDirectory).writeFile(path, objectsDb.blob(content));
+    thenThrown(IllegalStateException.class);
   }
 
   @Test
   public void files_are_written_to_file_system() throws Exception {
-    SFile file = objectsDb.file(path, content);
-    SArray<SFile> array = objectsDb.array(FILE_ARRAY, file);
-
-    tempDirectory.writeFiles(array);
-
-    fileSystem.assertFileContains(path, content);
+    given(array = objectsDb.array(FILE_ARRAY, objectsDb.file(path, content)));
+    when(tempDirectory).writeFiles(array);
+    then(inputStreamToString(fileSystem.openInputStream(path)).equals(content));
   }
 
   @Test
   public void writing_files_after_destroy_throws_exception() throws Exception {
-    tempDirectory.destroy();
-
-    SFile file = objectsDb.file(path, content);
-    SArray<SFile> array = objectsDb.array(FILE_ARRAY, file);
-
-    try {
-      tempDirectory.writeFiles(array);
-      fail("exception should be thrown");
-    } catch (IllegalStateException e) {
-      // expected
-    }
+    given(array = objectsDb.array(FILE_ARRAY, objectsDb.file(path, content)));
+    given(tempDirectory).destroy();
+    when(tempDirectory).writeFiles(array);
+    thenThrown(IllegalStateException.class);
   }
 
   @Test
   public void files_are_read_from_file_system() throws Exception {
     given(fileSystem).createFile(path, content);
-    given(tempDirectory = new TempDirectory(objectsDb, rootPath, fileSystem));
     when(tempDirectory.readFiles());
     thenReturned(contains(objectsDb.file(path, content)));
   }
 
   @Test
   public void reading_files_after_destroy_throws_exception() throws Exception {
-    tempDirectory.destroy();
-
-    try {
-      tempDirectory.readFiles();
-      fail("exception should be thrown");
-    } catch (IllegalStateException e) {
-      // expected
-    }
+    given(tempDirectory).destroy();
+    when(tempDirectory).readFiles();
+    thenThrown(IllegalStateException.class);
   }
 
   @Test
   public void content_is_read_from_file_system() throws Exception {
-    fileSystem.createFile(path, content);
-    SBlob blobContent = tempDirectory.readContent(path);
-    assertContent(blobContent.openInputStream(), content);
+    given(fileSystem).createFile(path, content);
+    when(tempDirectory).readContent(path);
+    thenReturned(blobContains(content));
   }
 
   @Test
   public void reading_content_after_destroy_throws_exception() throws Exception {
-    fileSystem.createFile(path, content);
-    tempDirectory.destroy();
+    given(fileSystem).createFile(path, content);
+    given(tempDirectory).destroy();
+    when(tempDirectory).readContent(path);
+    thenThrown(IllegalStateException.class);
+  }
 
-    try {
-      tempDirectory.readContent(path);
-      fail("exception should be thrown");
-    } catch (IllegalStateException e) {
-      // expected
-    }
+  private static Matcher blobContains(final String expected) {
+    return new Matcher() {
+      @Override
+      public boolean matches(Object object) {
+        try {
+          return inputStreamToString(((SBlob) object).openInputStream()).equals(expected);
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+    };
   }
 }
