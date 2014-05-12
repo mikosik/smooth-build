9a9f2df153b12423ff138dc124c08324e1f8a9fc
Marcin Mikosik <marcin.mikosik@gmail.com>
2014-05-12 17:51:45 +0200

    inlined .lang.function.def.args.Helpers.createMap()

--- src/main/java/org/smoothbuild/lang/function/def/args/Helpers.java
+++ src/main/java/org/smoothbuild/lang/function/def/args/Helpers.java
@@ -1,19 +1,6 @@
 package org.smoothbuild.lang.function.def.args;
 
-import java.util.Set;
-
-import org.smoothbuild.lang.base.SType;
-
-import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.Sets;
 
 public class Helpers {
 
-  public static <T> ImmutableMap<SType<?>, Set<T>> createMap(Set<SType<?>> types) {
-    ImmutableMap.Builder<SType<?>, Set<T>> builder = ImmutableMap.builder();
-    for (SType<?> type : types) {
-      builder.put(type, Sets.<T> newHashSet());
-    }
-    return builder.build();
-  }
 }
