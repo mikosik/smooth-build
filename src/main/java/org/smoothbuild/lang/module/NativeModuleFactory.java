f78812ed0b238bca8ddb267780da234e36a552dc
Marcin Mikosik <marcin.mikosik@gmail.com>
2014-05-15 14:28:43 +0200

    refactored NativeModuleFactory

--- src/main/java/org/smoothbuild/lang/module/NativeModuleFactory.java
+++ src/main/java/org/smoothbuild/lang/module/NativeModuleFactory.java
@@ -1,20 +1,16 @@
 package org.smoothbuild.lang.module;
 
-import static org.smoothbuild.lang.function.nativ.NativeFunctionFactory.createNativeFunction;
+import static org.smoothbuild.lang.function.nativ.NativeFunctionFactory.createNativeFunctions;
 
-import java.lang.reflect.Method;
-
+import org.smoothbuild.lang.function.nativ.NativeFunction;
 import org.smoothbuild.lang.function.nativ.err.NativeImplementationException;
-import org.smoothbuild.lang.plugin.SmoothFunction;
 
 public class NativeModuleFactory {
   public static Module createNativeModule(Class<?> moduleClass)
       throws NativeImplementationException {
     ModuleBuilder builder = new ModuleBuilder();
-    for (Method method : moduleClass.getDeclaredMethods()) {
-      if (method.isAnnotationPresent(SmoothFunction.class)) {
-        builder.addFunction(createNativeFunction(method));
-      }
+    for (NativeFunction<?> function : createNativeFunctions(moduleClass)) {
+      builder.addFunction(function);
     }
     return builder.build();
   }
