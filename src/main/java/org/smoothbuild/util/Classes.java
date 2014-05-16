7ad17cb956baaa1af506afa4e2d3e6b5f48cfb20
Marcin Mikosik <marcin.mikosik@gmail.com>
2014-05-16 12:26:53 +0200

    extracted Classes.byteCodeAsInputStream()

--- src/main/java/org/smoothbuild/util/Classes.java
+++ src/main/java/org/smoothbuild/util/Classes.java
@@ -1,9 +1,14 @@
 package org.smoothbuild.util;
 
+import java.io.InputStream;
+
 public class Classes {
 
   public static String binaryPath(Class<?> clazz) {
     return clazz.getName().replace('.', '/') + ".class";
   }
 
+  public static InputStream byteCodeAsInputStream(Class<?> clazz) {
+    return clazz.getClassLoader().getResourceAsStream(binaryPath(clazz));
+  }
 }
