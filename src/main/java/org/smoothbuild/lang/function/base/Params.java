bb021f7abd074559a872a258015f8d637460562b
Marcin Mikosik <marcin.mikosik@gmail.com>
2014-09-12 21:10:53 +0200

    added Params.filterRequiredParams()/filterOptionalParams()

--- src/main/java/org/smoothbuild/lang/function/base/Params.java
+++ src/main/java/org/smoothbuild/lang/function/base/Params.java
@@ -3,18 +3,39 @@ package org.smoothbuild.lang.function.base;
 import java.util.Arrays;
 import java.util.Set;
 
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Sets;
 
 public class Params {
+
+  public static ImmutableList<Param> filterRequiredParams(ImmutableList<Param> params) {
+    return filterParams(params, true);
+  }
+
+  public static ImmutableList<Param> filterOptionalParams(ImmutableList<Param> params) {
+    return filterParams(params, false);
+  }
+
+  private static ImmutableList<Param> filterParams(ImmutableList<Param> params,
+      boolean isRequired) {
+    ImmutableList.Builder<Param> builder = ImmutableList.builder();
+    for (Param param : params) {
+      if (param.isRequired() == isRequired) {
+        builder.add(param);
+      }
+    }
+    return builder.build();
+  }
+
   public static ImmutableMap<String, Param> map(Param... params) {
     return map(Arrays.asList(params));
   }
 
   /**
    * @return Parameters ordered lexicographically by their names. Methods
-   *         values(), keySet(), entrySet() of returned map returns collections
-   *         which elements keep that order.
+   * values(), keySet(), entrySet() of returned map returns collections
+   * which elements keep that order.
    */
   public static ImmutableMap<String, Param> map(Iterable<Param> params) {
     Set<String> names = Sets.newHashSet();
