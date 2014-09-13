361f77490360fa63f041a295191e9d2c7613a031
Marcin Mikosik <marcin.mikosik@gmail.com>
2014-09-13 22:34:39 +0200

    refactored ParamsPoolTest

--- src/main/test/org/smoothbuild/lang/function/def/args/ParamsPoolTest.java
+++ src/main/test/org/smoothbuild/lang/function/def/args/ParamsPoolTest.java
@@ -1,7 +1,5 @@
 package org.smoothbuild.lang.function.def.args;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.Assert.fail;
 import static org.smoothbuild.lang.base.STypes.BLOB;
 import static org.smoothbuild.lang.base.STypes.BLOB_ARRAY;
 import static org.smoothbuild.lang.base.STypes.FILE;
@@ -10,167 +8,307 @@ import static org.smoothbuild.lang.base.STypes.NIL;
 import static org.smoothbuild.lang.base.STypes.STRING;
 import static org.smoothbuild.lang.base.STypes.STRING_ARRAY;
 import static org.smoothbuild.lang.function.base.Param.param;
+import static org.testory.Testory.given;
+import static org.testory.Testory.thenReturned;
+import static org.testory.Testory.thenThrown;
+import static org.testory.Testory.when;
+import static org.testory.common.Matchers.same;
 
 import org.junit.Test;
 import org.smoothbuild.lang.function.base.Param;
+import org.smoothbuild.util.Empty;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
 
 public class ParamsPoolTest {
-  Param string = param(STRING, "string1");
-  Param blob = param(BLOB, "blob");
-  Param file = param(FILE, "file1");
+  Param param;
+  ParamsPool paramsPool;
 
-  Param stringArray = param(STRING_ARRAY, "stringArray");
-  Param blobArray = param(BLOB_ARRAY, "blobArray");
-  Param fileArray = param(FILE_ARRAY, "fileArray");
-
-  Param stringRequired = param(STRING, "stringRequired", true);
-  Param blobRequired = param(BLOB, "blobRequired", true);
-  Param fileRequired = param(FILE, "fileRequired", true);
-
-  Param stringArrayRequired = param(STRING_ARRAY, "stringArrayRequired", true);
-  Param blobArrayRequired = param(BLOB_ARRAY, "blobArrayRequired", true);
-  Param fileArrayRequired = param(FILE_ARRAY, "fileArrayRequired", true);
-
-  ImmutableList<Param> params = ImmutableList.of(string, stringRequired, blob, blobRequired, file,
-      fileRequired, stringArray, stringArrayRequired, blobArray, blobArrayRequired, fileArray,
-      fileArrayRequired);
-  ParamsPool paramsPool = new ParamsPool(params);
+  // take()
 
   @Test
-  public void paramCanBeTaken() {
-    assertThat(paramsPool.take(string)).isSameAs(string);
+  public void existing_param_can_be_taken_from_pool() {
+    given(param = param(STRING, "NAME"));
+    given(paramsPool = new ParamsPool(ImmutableList.of(param)));
+    when(paramsPool).take(param);
+    thenReturned(same(param));
   }
 
   @Test
-  public void takingUnknownParamThrowsException() {
-    try {
-      paramsPool.take(param(STRING, "someName"));
-      fail("exception should be thrown");
-    } catch (IllegalArgumentException e) {
-      // expected
-    }
+  public void taking_unknown_param_throws_exception() {
+    given(paramsPool = new ParamsPool(Empty.paramList()));
+    when(paramsPool).take(param(STRING, "unknownName"));
+    thenThrown(IllegalArgumentException.class);
   }
 
   @Test
-  public void paramCannotBeTakenTwice() {
-    paramsPool.take(string);
-    try {
-      paramsPool.take(string);
-      fail("exception should be thrown");
-    } catch (IllegalArgumentException e) {
-      // expected
-    }
+  public void param_cannot_be_taken_twice_from_pool() {
+    given(param = param(STRING, "NAME"));
+    given(paramsPool = new ParamsPool(ImmutableList.of(param)));
+    given(paramsPool).take(param);
+    when(paramsPool).take(param);
+    thenThrown(IllegalArgumentException.class);
+  }
+
+  // takeByName()
+
+  @Test
+  public void existing_param_can_be_taken_from_pool_by_name() {
+    given(param = param(STRING, "NAME"));
+    given(paramsPool = new ParamsPool(ImmutableList.of(param)));
+    when(paramsPool).takeByName(param.name());
+    thenReturned(same(param));
   }
 
   @Test
-  public void paramCanBeTakenByName() {
-    assertThat(paramsPool.takeByName(string.name())).isSameAs(string);
+  public void taking_unknown_param_by_name_throws_exception() {
+    given(paramsPool = new ParamsPool(Empty.paramList()));
+    when(paramsPool).takeByName("unknownName");
+    thenThrown(IllegalArgumentException.class);
   }
 
   @Test
-  public void takingUnknownParamByNameThrowsException() {
-    try {
-      paramsPool.takeByName("someName");
-      fail("exception should be thrown");
-    } catch (IllegalArgumentException e) {
-      // expected
-    }
-  }
-
-  @Test
-  public void paramCannotBeTakenByNameTwice() {
-    paramsPool.takeByName(string.name());
-    try {
-      paramsPool.takeByName(string.name());
-      fail("exception should be thrown");
-    } catch (IllegalArgumentException e) {
-      // expected
-    }
+  public void param_cannot_be_taken_by_name_twice() {
+    given(param = param(STRING, "NAME"));
+    given(paramsPool = new ParamsPool(ImmutableList.of(param)));
+    given(paramsPool).takeByName(param.name());
+    when(paramsPool).takeByName(param.name());
+    thenThrown(IllegalArgumentException.class);
   }
 
   // availableForType()
 
   @Test
-  public void string_param_is_available_for_string_argument() throws Exception {
-    paramsPool = new ParamsPool(params);
-
-    assertThat(paramsPool.availableForType(STRING).optionalParams()).containsOnly(string);
-    assertThat(paramsPool.availableForType(STRING).requiredParams()).containsOnly(stringRequired);
+  public void optional_string_param_is_available_in_optional_set_of_string_pool() throws Exception {
+    given(param = param(STRING, "NAME"));
+    given(paramsPool = new ParamsPool(ImmutableList.of(param)));
+    when(paramsPool.availableForType(STRING)).optionalParams();
+    thenReturned(ImmutableSet.of(param));
   }
 
   @Test
-  public void blob_param_is_available_for_blob_argument() throws Exception {
-    paramsPool = new ParamsPool(params);
-
-    assertThat(paramsPool.availableForType(BLOB).optionalParams()).containsOnly(blob);
-    assertThat(paramsPool.availableForType(BLOB).requiredParams()).containsOnly(blobRequired);
+  public void required_string_param_is_available_in_required_set_of_string_pool() throws Exception {
+    given(param = param(STRING, "NAME", true));
+    given(paramsPool = new ParamsPool(ImmutableList.of(param)));
+    when(paramsPool.availableForType(STRING)).requiredParams();
+    thenReturned(ImmutableSet.of(param));
   }
 
   @Test
-  public void blob_and_file_params_are_available_for_file_argument() throws Exception {
-    paramsPool = new ParamsPool(params);
-
-    assertThat(paramsPool.availableForType(FILE).optionalParams()).containsOnly(blob, file);
-    assertThat(paramsPool.availableForType(FILE).requiredParams()).containsOnly(blobRequired,
-        fileRequired);
+  public void optional_blob_param_is_available_in_optional_set_of_blob_pool() throws Exception {
+    given(param = param(BLOB, "NAME"));
+    given(paramsPool = new ParamsPool(ImmutableList.of(param)));
+    when(paramsPool.availableForType(BLOB)).optionalParams();
+    thenReturned(ImmutableSet.of(param));
   }
 
   @Test
-  public void string_array_param_is_available_for_string_array_argument() throws Exception {
-    paramsPool = new ParamsPool(params);
-
-    assertThat(paramsPool.availableForType(STRING_ARRAY).optionalParams()).containsOnly(
-        stringArray);
-    assertThat(paramsPool.availableForType(STRING_ARRAY).requiredParams()).containsOnly(
-        stringArrayRequired);
+  public void required_blob_param_is_available_in_required_set_of_blob_pool() throws Exception {
+    given(param = param(BLOB, "NAME", true));
+    given(paramsPool = new ParamsPool(ImmutableList.of(param)));
+    when(paramsPool.availableForType(BLOB)).requiredParams();
+    thenReturned(ImmutableSet.of(param));
   }
 
   @Test
-  public void blob_array_param_is_available_for_blob_array_argument() throws Exception {
-    paramsPool = new ParamsPool(params);
-
-    assertThat(paramsPool.availableForType(BLOB_ARRAY).optionalParams()).containsOnly(blobArray);
-    assertThat(paramsPool.availableForType(BLOB_ARRAY).requiredParams()).containsOnly(
-        blobArrayRequired);
+  public void optional_blob_param_is_available_in_optional_set_of_file_pool() throws Exception {
+    given(param = param(BLOB, "NAME"));
+    given(paramsPool = new ParamsPool(ImmutableList.of(param)));
+    when(paramsPool.availableForType(FILE)).optionalParams();
+    thenReturned(ImmutableSet.of(param));
   }
 
   @Test
-  public void blob_array_and_file_array_params_are_available_for_file_array_argument() throws
+  public void required_blob_param_is_available_in_required_set_of_file_pool() throws Exception {
+    given(param = param(BLOB, "NAME", true));
+    given(paramsPool = new ParamsPool(ImmutableList.of(param)));
+    when(paramsPool.availableForType(FILE)).requiredParams();
+    thenReturned(ImmutableSet.of(param));
+  }
+
+  @Test
+  public void optional_file_param_is_available_in_optional_set_of_file_pool() throws Exception {
+    given(param = param(FILE, "NAME"));
+    given(paramsPool = new ParamsPool(ImmutableList.of(param)));
+    when(paramsPool.availableForType(FILE)).optionalParams();
+    thenReturned(ImmutableSet.of(param));
+  }
+
+  @Test
+  public void required_file_param_is_available_in_required_set_of_file_pool() throws Exception {
+    given(param = param(FILE, "NAME", true));
+    given(paramsPool = new ParamsPool(ImmutableList.of(param)));
+    when(paramsPool.availableForType(FILE)).requiredParams();
+    thenReturned(ImmutableSet.of(param));
+  }
+
+  @Test
+  public void optional_string_array_param_is_available_in_optional_set_of_string_array_pool() throws
       Exception {
-    paramsPool = new ParamsPool(params);
-
-    assertThat(paramsPool.availableForType(FILE_ARRAY).optionalParams()).containsOnly(blobArray,
-        fileArray);
-    assertThat(paramsPool.availableForType(FILE_ARRAY).requiredParams()).containsOnly(
-        blobArrayRequired, fileArrayRequired);
+    given(param = param(STRING_ARRAY, "NAME"));
+    given(paramsPool = new ParamsPool(ImmutableList.of(param)));
+    when(paramsPool.availableForType(STRING_ARRAY)).optionalParams();
+    thenReturned(ImmutableSet.of(param));
   }
 
   @Test
-  public void array_params_are_available_for_nil_argument() throws Exception {
-    paramsPool = new ParamsPool(params);
+  public void required_string_array_param_is_available_in_required_set_of_string_array_pool() throws
+      Exception {
+    given(param = param(STRING_ARRAY, "NAME", true));
+    given(paramsPool = new ParamsPool(ImmutableList.of(param)));
+    when(paramsPool.availableForType(STRING_ARRAY)).requiredParams();
+    thenReturned(ImmutableSet.of(param));
+  }
 
-    assertThat(paramsPool.availableForType(NIL).optionalParams()).containsOnly(stringArray,
-        blobArray, fileArray);
-    assertThat(paramsPool.availableForType(NIL).requiredParams()).containsOnly(stringArrayRequired,
-        blobArrayRequired, fileArrayRequired);
+  @Test
+  public void optional_blob_array_param_is_available_in_optional_set_of_blob_array_pool() throws
+      Exception {
+    given(param = param(BLOB_ARRAY, "NAME"));
+    given(paramsPool = new ParamsPool(ImmutableList.of(param)));
+    when(paramsPool.availableForType(BLOB_ARRAY)).optionalParams();
+    thenReturned(ImmutableSet.of(param));
+  }
+
+  @Test
+  public void required_blob_array_param_is_available_in_required_set_of_blob_array_pool() throws
+      Exception {
+    given(param = param(BLOB_ARRAY, "NAME", true));
+    given(paramsPool = new ParamsPool(ImmutableList.of(param)));
+    when(paramsPool.availableForType(BLOB_ARRAY)).requiredParams();
+    thenReturned(ImmutableSet.of(param));
+  }
+
+  @Test
+  public void optional_blob_array_param_is_available_in_optional_set_of_file_array_pool() throws
+      Exception {
+    given(param = param(BLOB_ARRAY, "NAME"));
+    given(paramsPool = new ParamsPool(ImmutableList.of(param)));
+    when(paramsPool.availableForType(FILE_ARRAY)).optionalParams();
+    thenReturned(ImmutableSet.of(param));
+  }
+
+  @Test
+  public void required_blob_array_param_is_available_in_required_set_of_file_array_pool() throws
+      Exception {
+    given(param = param(BLOB_ARRAY, "NAME", true));
+    given(paramsPool = new ParamsPool(ImmutableList.of(param)));
+    when(paramsPool.availableForType(FILE_ARRAY)).requiredParams();
+    thenReturned(ImmutableSet.of(param));
+  }
+
+  @Test
+  public void optional_file_array_param_is_available_in_optional_set_of_file_array_pool() throws
+      Exception {
+    given(param = param(FILE_ARRAY, "NAME"));
+    given(paramsPool = new ParamsPool(ImmutableList.of(param)));
+    when(paramsPool.availableForType(FILE_ARRAY)).optionalParams();
+    thenReturned(ImmutableSet.of(param));
+  }
+
+  @Test
+  public void required_file_array_param_is_available_in_required_set_of_file_array_pool() throws
+      Exception {
+    given(param = param(FILE_ARRAY, "NAME", true));
+    given(paramsPool = new ParamsPool(ImmutableList.of(param)));
+    when(paramsPool.availableForType(FILE_ARRAY)).requiredParams();
+    thenReturned(ImmutableSet.of(param));
+  }
+
+  @Test
+  public void optional_string_array_param_is_available_in_optional_set_of_nil_pool() throws
+      Exception {
+    given(param = param(STRING_ARRAY, "NAME"));
+    given(paramsPool = new ParamsPool(ImmutableList.of(param)));
+    when(paramsPool.availableForType(NIL)).optionalParams();
+    thenReturned(ImmutableSet.of(param));
+  }
+
+  @Test
+  public void required_string_array_param_is_available_in_required_set_of_nil_pool() throws
+      Exception {
+    given(param = param(STRING_ARRAY, "NAME", true));
+    given(paramsPool = new ParamsPool(ImmutableList.of(param)));
+    when(paramsPool.availableForType(NIL)).requiredParams();
+    thenReturned(ImmutableSet.of(param));
+  }
+
+  @Test
+  public void optional_blob_array_param_is_available_in_optional_set_of_nil_pool() throws
+      Exception {
+    given(param = param(BLOB_ARRAY, "NAME"));
+    given(paramsPool = new ParamsPool(ImmutableList.of(param)));
+    when(paramsPool.availableForType(NIL)).optionalParams();
+    thenReturned(ImmutableSet.of(param));
+  }
+
+  @Test
+  public void required_blob_array_param_is_available_in_required_set_of_nil_pool() throws
+      Exception {
+    given(param = param(BLOB_ARRAY, "NAME", true));
+    given(paramsPool = new ParamsPool(ImmutableList.of(param)));
+    when(paramsPool.availableForType(NIL)).requiredParams();
+    thenReturned(ImmutableSet.of(param));
+  }
+
+  @Test
+  public void optional_file_array_param_is_available_in_optional_set_of_nil_pool() throws
+      Exception {
+    given(param = param(FILE_ARRAY, "NAME"));
+    given(paramsPool = new ParamsPool(ImmutableList.of(param)));
+    when(paramsPool.availableForType(NIL)).optionalParams();
+    thenReturned(ImmutableSet.of(param));
+  }
+
+  @Test
+  public void required_file_array_param_is_available_in_required_set_of_nil_pool() throws
+      Exception {
+    given(param = param(FILE_ARRAY, "NAME", true));
+    given(paramsPool = new ParamsPool(ImmutableList.of(param)));
+    when(paramsPool.availableForType(NIL)).requiredParams();
+    thenReturned(ImmutableSet.of(param));
   }
 
   // availableRequiredParams()
 
   @Test
-  public void availableRequiredParams() throws Exception {
-    assertThat(paramsPool.availableRequiredParams()).containsOnly(stringRequired, blobRequired,
-        fileRequired, stringArrayRequired, blobArrayRequired, fileArrayRequired);
+  public void available_required_params_contains_required_string_param() throws Exception {
+    given(param = param(STRING, "NAME", true));
+    given(paramsPool = new ParamsPool(ImmutableList.of(param)));
+    when(paramsPool.availableRequiredParams());
+    thenReturned(ImmutableSet.of(param));
+  }
+
+  @Test
+  public void available_required_params_contains_required_blob_param() throws Exception {
+    given(param = param(BLOB, "NAME", true));
+    given(paramsPool = new ParamsPool(ImmutableList.of(param)));
+    when(paramsPool.availableRequiredParams());
+    thenReturned(ImmutableSet.of(param));
+  }
+
+  @Test
+  public void available_required_params_contains_required_file_param() throws Exception {
+    given(param = param(FILE, "NAME", true));
+    given(paramsPool = new ParamsPool(ImmutableList.of(param)));
+    when(paramsPool.availableRequiredParams());
+    thenReturned(ImmutableSet.of(param));
+  }
+
+  @Test
+  public void available_required_params_contains_required_string_array_param() throws Exception {
+    given(param = param(STRING_ARRAY, "NAME", true));
+    given(paramsPool = new ParamsPool(ImmutableList.of(param)));
+    when(paramsPool.availableRequiredParams());
+    thenReturned(ImmutableSet.of(param));
   }
 
   @Test
   public void available_required_params_does_not_contain_taken_param() throws Exception {
-    ImmutableList<Param> params = ImmutableList.of(stringRequired, fileRequired);
-    ParamsPool paramsPool = new ParamsPool(params);
-
-    paramsPool.take(stringRequired);
-    assertThat(paramsPool.availableRequiredParams()).containsOnly(fileRequired);
+    given(param = param(STRING, "NAME", true));
+    given(paramsPool = new ParamsPool(ImmutableList.of(param)));
+    given(paramsPool.take(param));
+    when(paramsPool.availableRequiredParams());
+    thenReturned(ImmutableSet.<Param>of());
   }
 }
