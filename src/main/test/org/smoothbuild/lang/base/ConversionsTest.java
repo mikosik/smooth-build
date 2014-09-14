550423e2bfeb1d93bc11bbdaa7695237cd391205
Marcin Mikosik <marcin.mikosik@gmail.com>
2014-09-14 20:08:23 +0200

    added Conversions.convertFunctionName()

--- src/main/test/org/smoothbuild/lang/base/ConversionsTest.java
+++ src/main/test/org/smoothbuild/lang/base/ConversionsTest.java
@@ -3,6 +3,7 @@ package org.smoothbuild.lang.base;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.smoothbuild.lang.base.Conversions.canConvert;
+import static org.smoothbuild.lang.base.Conversions.convertFunctionName;
 import static org.smoothbuild.lang.base.STypes.BLOB;
 import static org.smoothbuild.lang.base.STypes.BLOB_ARRAY;
 import static org.smoothbuild.lang.base.STypes.FILE;
@@ -10,6 +11,9 @@ import static org.smoothbuild.lang.base.STypes.FILE_ARRAY;
 import static org.smoothbuild.lang.base.STypes.NIL;
 import static org.smoothbuild.lang.base.STypes.STRING;
 import static org.smoothbuild.lang.base.STypes.STRING_ARRAY;
+import static org.smoothbuild.lang.function.base.Name.name;
+import static org.testory.Testory.thenReturned;
+import static org.testory.Testory.when;
 
 import org.junit.Test;
 
@@ -72,5 +76,35 @@ public class ConversionsTest {
     assertTrue(canConvert(NIL, BLOB_ARRAY));
     assertTrue(canConvert(NIL, FILE_ARRAY));
     assertTrue(canConvert(NIL, NIL));
+  }
+
+  @Test
+  public void convert_function_name_for_file_to_blob_conversion() throws Exception {
+    when(convertFunctionName(FILE, BLOB));
+    thenReturned(name("fileToBlob"));
+  }
+
+  @Test
+  public void convert_function_name_for_file_array_to_blob_array_conversion() throws Exception {
+    when(convertFunctionName(FILE_ARRAY, BLOB_ARRAY));
+    thenReturned(name("fileArrayToBlobArray"));
+  }
+
+  @Test
+  public void convert_function_name_for_nil_to_string_array_conversion() throws Exception {
+    when(convertFunctionName(NIL, STRING_ARRAY));
+    thenReturned(name("nilToStringArray"));
+  }
+
+  @Test
+  public void convert_function_name_for_nil_to_blob_array_conversion() throws Exception {
+    when(convertFunctionName(NIL, BLOB_ARRAY));
+    thenReturned(name("nilToBlobArray"));
+  }
+
+  @Test
+  public void convert_function_name_for_nil_to_file_array_conversion() throws Exception {
+    when(convertFunctionName(NIL, FILE_ARRAY));
+    thenReturned(name("nilToFileArray"));
   }
 }
