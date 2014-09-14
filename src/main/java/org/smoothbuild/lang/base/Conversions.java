37fe8a42fd19a7e0f331923c9e00d3b7ba0a1511
Marcin Mikosik <marcin.mikosik@gmail.com>
2014-09-14 20:08:23 +0200

    added Conversions.convertFunctionName()

--- src/main/java/org/smoothbuild/lang/base/Conversions.java
+++ src/main/java/org/smoothbuild/lang/base/Conversions.java
@@ -6,25 +6,57 @@ import static org.smoothbuild.lang.base.STypes.FILE;
 import static org.smoothbuild.lang.base.STypes.FILE_ARRAY;
 import static org.smoothbuild.lang.base.STypes.NIL;
 import static org.smoothbuild.lang.base.STypes.STRING_ARRAY;
+import static org.smoothbuild.lang.function.base.Name.name;
 
-import com.google.common.collect.ImmutableMultimap;
+import org.smoothbuild.lang.function.base.Name;
+
+import com.google.common.collect.ImmutableMap;
 
 public class Conversions {
-  private static final ImmutableMultimap<SType<?>, SType<?>> CONVERSIONS = createConversions();
+  private static final ImmutableMap<TypeConversion, Name> CONVERSIONS = createConversions();
 
   public static boolean canConvert(SType<?> from, SType<?> to) {
-    return from == to || CONVERSIONS.containsEntry(from, to);
+    return from == to || CONVERSIONS.containsKey(new TypeConversion(from, to));
   }
 
-  private static ImmutableMultimap<SType<?>, SType<?>> createConversions() {
-    ImmutableMultimap.Builder<SType<?>, SType<?>> builder = ImmutableMultimap.builder();
+  public static Name convertFunctionName(SType<?> from, SType<?> to) {
+    return CONVERSIONS.get(new TypeConversion(from, to));
+  }
 
-    builder.put(FILE, BLOB);
-    builder.put(FILE_ARRAY, BLOB_ARRAY);
-    builder.put(NIL, STRING_ARRAY);
-    builder.put(NIL, BLOB_ARRAY);
-    builder.put(NIL, FILE_ARRAY);
+  private static ImmutableMap<TypeConversion, Name> createConversions() {
+    ImmutableMap.Builder<TypeConversion, Name> builder = ImmutableMap.builder();
+
+    builder.put(new TypeConversion(FILE, BLOB), name("fileToBlob"));
+    builder.put(new TypeConversion(FILE_ARRAY, BLOB_ARRAY), name("fileArrayToBlobArray"));
+    builder.put(new TypeConversion(NIL, STRING_ARRAY), name("nilToStringArray"));
+    builder.put(new TypeConversion(NIL, BLOB_ARRAY), name("nilToBlobArray"));
+    builder.put(new TypeConversion(NIL, FILE_ARRAY), name("nilToFileArray"));
 
     return builder.build();
+  }
+
+  private static class TypeConversion {
+    private final SType<?> from;
+    private final SType<?> to;
+
+    private TypeConversion(SType<?> from, SType<?> to) {
+      this.from = from;
+      this.to = to;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+      if (!(object instanceof TypeConversion)) {
+        return false;
+      }
+
+      TypeConversion that = (TypeConversion) object;
+      return this.from == that.from && this.to == that.to;
+    }
+
+    @Override
+    public int hashCode() {
+      return 31 * from.hashCode() + to.hashCode();
+    }
   }
 }
