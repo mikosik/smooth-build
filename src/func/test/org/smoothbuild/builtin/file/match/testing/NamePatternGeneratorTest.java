edbfd9f7ef7120cb4d3802f71450e2e7cecdad7a
Marcin Mikosik <marcin.mikosik@gmail.com>
2014-11-16 16:51:50 +0100

    refactored NamePatternGeneratorTest

--- src/func/test/org/smoothbuild/builtin/file/match/testing/NamePatternGeneratorTest.java
+++ src/func/test/org/smoothbuild/builtin/file/match/testing/NamePatternGeneratorTest.java
@@ -1,8 +1,9 @@
 package org.smoothbuild.builtin.file.match.testing;
 
 import static com.google.common.collect.Lists.newArrayList;
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.smoothbuild.builtin.file.match.testing.NamePatternGenerator.generatePatternsImpl;
+import static org.testory.Testory.*;
 
 import java.util.List;
 
@@ -14,7 +15,6 @@ public class NamePatternGeneratorTest {
 
   @Test
   public void all_possible_patterns_are_generated() {
-    // given
     final List<String> generatedPatterns = newArrayList();
     Function<String, Void> collectingConsumer = new Function<String, Void>() {
       public Void apply(String pattern) {
@@ -23,27 +23,8 @@ public class NamePatternGeneratorTest {
       }
     };
 
-    // when
     generatePatternsImpl(3, collectingConsumer);
-
-    // then
-    List<String> expected = newArrayList();
-    expected.add("aaa");
-    expected.add("aab");
-    expected.add("aa*");
-
-    expected.add("aba");
-    expected.add("abb");
-    expected.add("abc");
-    expected.add("ab*");
-
-    expected.add("a*a");
-    expected.add("a*b");
-
-    expected.add("*aa");
-    expected.add("*ab");
-    expected.add("*a*");
-
-    assertThat(generatedPatterns).containsOnly(expected.toArray(new String[expected.size()]));
+    then(generatedPatterns, containsInAnyOrder("aaa", "aab", "aa*", "aba", "abb", "abc", "ab*",
+        "a*a", "a*b", "*aa", "*ab", "*a*"));
   }
 }
