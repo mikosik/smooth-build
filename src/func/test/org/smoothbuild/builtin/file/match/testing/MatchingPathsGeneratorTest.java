25a03208abbeea0b27f79b63638650d1580640d2
Marcin Mikosik <marcin.mikosik@gmail.com>
2014-11-16 16:47:33 +0100

    refactored MatchingPathsGeneratorTest

--- src/func/test/org/smoothbuild/builtin/file/match/testing/MatchingPathsGeneratorTest.java
+++ src/func/test/org/smoothbuild/builtin/file/match/testing/MatchingPathsGeneratorTest.java
@@ -1,7 +1,9 @@
 package org.smoothbuild.builtin.file.match.testing;
 
 import static com.google.common.collect.Lists.newArrayList;
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.smoothbuild.builtin.file.match.testing.MatchingPathsGenerator.generatePaths;
+import static org.testory.Testory.*;
 
 import java.util.List;
 
@@ -22,28 +24,9 @@ public class MatchingPathsGeneratorTest {
       }
     };
 
-    // when
-    MatchingPathsGenerator.generatePaths("x/*/z", consumer);
-
-    // then
-    List<String> expected = newArrayList();
-    expected.add("x/a/z");
-    expected.add("x/b/z");
-    expected.add("x/c/z");
-
-    expected.add("x/aa/z");
-    expected.add("x/ab/z");
-    expected.add("x/ac/z");
-
-    expected.add("x/ba/z");
-    expected.add("x/bb/z");
-    expected.add("x/bc/z");
-
-    expected.add("x/ca/z");
-    expected.add("x/cb/z");
-    expected.add("x/cc/z");
-
-    assertThat(generatedPaths).containsOnly(expected.toArray(new String[expected.size()]));
+    generatePaths("x/*/z", consumer);
+    then(generatedPaths, containsInAnyOrder("x/a/z", "x/b/z", "x/c/z", "x/aa/z", "x/ab/z", "x/ac/z",
+        "x/ba/z", "x/bb/z", "x/bc/z", "x/ca/z", "x/cb/z", "x/cc/z"));
   }
 
   @Test
@@ -57,29 +40,9 @@ public class MatchingPathsGeneratorTest {
       }
     };
 
-    // when
-    MatchingPathsGenerator.generatePaths("x/1*9/z", consumer);
-
-    // then
-    List<String> expected = newArrayList();
-    expected.add("x/19/z");
-
-    expected.add("x/1a9/z");
-    expected.add("x/1b9/z");
-    expected.add("x/1c9/z");
-
-    expected.add("x/1aa9/z");
-    expected.add("x/1ab9/z");
-    expected.add("x/1ac9/z");
-
-    expected.add("x/1ba9/z");
-    expected.add("x/1bb9/z");
-    expected.add("x/1bc9/z");
-
-    expected.add("x/1ca9/z");
-    expected.add("x/1cb9/z");
-    expected.add("x/1cc9/z");
-
-    assertThat(generatedPaths).containsOnly(generatedPaths.toArray(new String[generatedPaths.size()]));
+    generatePaths("x/1*9/z", consumer);
+    then(generatedPaths, containsInAnyOrder("x/19/z", "x/1a9/z", "x/1b9/z", "x/1c9/z", "x/1aa9/z",
+        "x/1ab9/z", "x/1ac9/z", "x/1ba9/z", "x/1bb9/z", "x/1bc9/z", "x/1ca9/z", "x/1cb9/z",
+        "x/1cc9/z"));
   }
 }
