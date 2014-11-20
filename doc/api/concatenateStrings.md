735668eadbd9b26c5f03318099d07d1b1f6af03d
Marcin Mikosik <marcin.mikosik@gmail.com>
2014-11-20 16:36:18 +0100

    added concatenateStrings() smooth function

---
+++ doc/api/concatenateStrings.md
@@ -0,0 +1,19 @@
+## concatenateStrings
+
+Concatenates two String[].
+
+ * String[] __strings_ - Array of Strings to be concatenated with array passed
+via "with" parameter.
+ * String[] __with__ - Array of Strings to be concatenated with array passed
+via "blobs" parameter.
+
+Returns __String[]__ containing all Strings from first and second argument.
+
+### examples
+
+Returns array of all Strings combined from 'first' and 'second' array.
+```
+first: [ "abc", "def" ] ;
+second: [ "ghi", "jkl" ] ;
+all: concatenateStrings(first, with=second);
+```
---
+++ src/func/java/org/smoothbuild/builtin/string/ConcatenateStringsFunction.java
@@ -0,0 +1,36 @@
+package org.smoothbuild.builtin.string;
+
+import static org.smoothbuild.lang.base.Types.STRING_ARRAY;
+
+import org.smoothbuild.lang.base.Array;
+import org.smoothbuild.lang.base.ArrayBuilder;
+import org.smoothbuild.lang.base.NativeApi;
+import org.smoothbuild.lang.base.SString;
+import org.smoothbuild.lang.plugin.Required;
+import org.smoothbuild.lang.plugin.SmoothFunction;
+
+public class ConcatenateStringsFunction {
+
+  public interface ConcatenateStringsParameters {
+    @Required
+    public Array<SString> strings();
+
+    @Required
+    public Array<SString> with();
+  }
+
+  @SmoothFunction
+  public static Array<SString> concatenateStrings(NativeApi nativeApi,
+      ConcatenateStringsParameters params) {
+    ArrayBuilder<SString> builder = nativeApi.arrayBuilder(STRING_ARRAY);
+
+    for (SString string : params.strings()) {
+      builder.add(string);
+    }
+    for (SString string : params.with()) {
+      builder.add(string);
+    }
+
+    return builder.build();
+  }
+}
