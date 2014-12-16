ca3fc2ffde546ed137efb51a405092ef299356a3
Marcin Mikosik <marcin.mikosik@gmail.com>
2014-12-16 21:32:24 +0100

    improved generics in ArrayType

--- src/main/java/org/smoothbuild/lang/base/ArrayType.java
+++ src/main/java/org/smoothbuild/lang/base/ArrayType.java
@@ -5,11 +5,12 @@ import com.google.inject.TypeLiteral;
 public class ArrayType extends Type {
   private final Type elemType;
 
-  protected static ArrayType arrayType(Type elemType, TypeLiteral<? extends Value> jType) {
+  protected static ArrayType arrayType(Type elemType,
+      TypeLiteral<? extends Array<? extends Value>> jType) {
     return new ArrayType(elemType, jType);
   }
 
-  protected ArrayType(Type elemType, TypeLiteral<? extends Value> jType) {
+  protected ArrayType(Type elemType, TypeLiteral<? extends Array<? extends Value>> jType) {
     super(elemType.name() + "[]", jType);
     this.elemType = elemType;
   }
