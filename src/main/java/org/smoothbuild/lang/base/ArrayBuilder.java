1311445e116d829cc26c46336fae9c963c7c3721
Marcin Mikosik <marcin.mikosik@gmail.com>
2014-12-21 10:59:47 +0100

    merged BlobBuilderImpl into BlobBuilder, ArrayBuilderImpl into ArrayBuilder

--- src/main/java/org/smoothbuild/lang/base/ArrayBuilder.java
+++ src/main/java/org/smoothbuild/lang/base/ArrayBuilder.java
@@ -1,9 +1,35 @@
 package org.smoothbuild.lang.base;
 
-public interface ArrayBuilder<T extends Value> {
+import static com.google.common.base.Preconditions.checkNotNull;
 
-  public ArrayBuilder<T> add(T elem);
+import java.util.List;
 
-  public Array<T> build();
+import org.smoothbuild.db.objects.marshal.ArrayMarshaller;
 
+import com.google.common.collect.Lists;
+
+public class ArrayBuilder<T extends Value> {
+  private final ArrayMarshaller<T> marshaller;
+  private final Class<?> elementClass;
+  private final List<T> result;
+
+  public ArrayBuilder(ArrayMarshaller<T> arrayMarshaller, Class<?> elementClass) {
+    this.marshaller = arrayMarshaller;
+    this.elementClass = elementClass;
+    this.result = Lists.newArrayList();
+  }
+
+  public ArrayBuilder<T> add(T elem) {
+    checkNotNull(elem);
+    if (!elementClass.isAssignableFrom(elem.getClass())) {
+      throw new IllegalArgumentException("Element must be of type "
+          + elementClass.getCanonicalName());
+    }
+    result.add(elem);
+    return this;
+  }
+
+  public Array<T> build() {
+    return marshaller.write(result);
+  }
 }
--- src/main/java/org/smoothbuild/lang/base/BlobBuilder.java
+++ src/main/java/org/smoothbuild/lang/base/BlobBuilder.java
@@ -1,10 +1,29 @@
 package org.smoothbuild.lang.base;
 
+import static com.google.common.base.Preconditions.checkState;
+
+import java.io.ByteArrayOutputStream;
 import java.io.OutputStream;
 
-public interface BlobBuilder {
+import org.smoothbuild.db.objects.marshal.BlobMarshaller;
 
-  public OutputStream openOutputStream();
+public class BlobBuilder {
+  private final BlobMarshaller marshaller;
+  private ByteArrayOutputStream outputStream;
 
-  public Blob build();
+  public BlobBuilder(BlobMarshaller marshaller) {
+    this.marshaller = marshaller;
+  }
+
+  public OutputStream openOutputStream() {
+    checkState(this.outputStream == null, "Cannot open output stream twice.");
+    this.outputStream = new ByteArrayOutputStream();
+    return outputStream;
+  }
+
+  public Blob build() {
+    checkState(outputStream != null, "No content available. Create one via openOutputStream()");
+    byte[] bytes = outputStream.toByteArray();
+    return marshaller.write(bytes);
+  }
 }
