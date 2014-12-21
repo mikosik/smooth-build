dbf39e72b9e91a9bbb81bbd8e511388806db1fc6
Marcin Mikosik <marcin.mikosik@gmail.com>
2014-12-21 11:27:09 +0100

    merged ObjectBuilders into ObjectsDb

--- src/main/java/org/smoothbuild/db/objects/ObjectsDb.java
+++ src/main/java/org/smoothbuild/db/objects/ObjectsDb.java
@@ -1,13 +1,18 @@
 package org.smoothbuild.db.objects;
 
+import static org.smoothbuild.lang.base.Types.arrayTypeContaining;
+import static org.smoothbuild.lang.base.Types.jTypeToType;
+
 import javax.inject.Inject;
 
-import org.smoothbuild.db.objects.build.ObjectBuilders;
+import org.smoothbuild.db.objects.marshal.ArrayMarshaller;
 import org.smoothbuild.db.objects.marshal.ObjectMarshallers;
 import org.smoothbuild.io.fs.base.Path;
 import org.smoothbuild.lang.base.ArrayBuilder;
+import org.smoothbuild.lang.base.ArrayType;
 import org.smoothbuild.lang.base.Blob;
 import org.smoothbuild.lang.base.BlobBuilder;
+import org.smoothbuild.lang.base.Nothing;
 import org.smoothbuild.lang.base.SFile;
 import org.smoothbuild.lang.base.SString;
 import org.smoothbuild.lang.base.Type;
@@ -15,20 +20,29 @@ import org.smoothbuild.lang.base.Value;
 import org.smoothbuild.lang.base.ValueFactory;
 
 import com.google.common.hash.HashCode;
+import com.google.inject.TypeLiteral;
 
 public class ObjectsDb implements ValueFactory {
   private final ObjectMarshallers objectMarshallers;
-  private final ObjectBuilders objectBuilders;
 
   @Inject
-  public ObjectsDb(ObjectMarshallers objectMarshallers, ObjectBuilders objectBuilders) {
+  public ObjectsDb(ObjectMarshallers objectMarshallers) {
     this.objectMarshallers = objectMarshallers;
-    this.objectBuilders = objectBuilders;
   }
 
   @Override
-  public <T extends Value> ArrayBuilder<T> arrayBuilder(Class<T> elementType) {
-    return objectBuilders.arrayBuilder(elementType);
+  public <T extends Value> ArrayBuilder<T> arrayBuilder(Class<T> elementClass) {
+    if (!(elementClass == Nothing.class || elementClass == SString.class
+        || elementClass == Blob.class || elementClass == SFile.class)) {
+      throw new IllegalArgumentException("Illegal type " + elementClass.getCanonicalName());
+    }
+    Type type = jTypeToType(TypeLiteral.get(elementClass));
+    return createArrayBuilder(arrayTypeContaining(type), elementClass);
+  }
+
+  private <T extends Value> ArrayBuilder<T> createArrayBuilder(ArrayType type, Class<?> elementClass) {
+    ArrayMarshaller<T> marshaller = objectMarshallers.arrayMarshaller(type);
+    return new ArrayBuilder<>(marshaller, elementClass);
   }
 
   @Override
@@ -38,7 +52,7 @@ public class ObjectsDb implements ValueFactory {
 
   @Override
   public BlobBuilder blobBuilder() {
-    return objectBuilders.blobBuilder();
+    return new BlobBuilder(objectMarshallers.blobMarshaller());
   }
 
   @Override
