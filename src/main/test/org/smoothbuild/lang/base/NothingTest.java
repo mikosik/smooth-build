600f0f5ae1c36fa6d4a39436ccf2d1ce7e9984f4
Marcin Mikosik <marcin.mikosik@gmail.com>
2014-12-24 12:10:13 +0100

    added NothingTest

---
+++ src/main/test/org/smoothbuild/lang/base/NothingTest.java
@@ -0,0 +1,17 @@
+package org.smoothbuild.lang.base;
+
+import static org.junit.Assert.assertTrue;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Modifier;
+
+import org.junit.Test;
+
+public class NothingTest {
+  @Test
+  public void nothing_cannot_be_instantiated() {
+    for (Constructor<?> constructor : Nothing.class.getDeclaredConstructors()) {
+      assertTrue(Modifier.isPrivate(constructor.getModifiers()));
+    }
+  }
+}
