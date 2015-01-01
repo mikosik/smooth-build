0d0dbe99fd9ec019a8c52c1d08c48b1090636aca
Marcin Mikosik <marcin.mikosik@gmail.com>
2015-01-01 14:03:00 +0100

    merged Mapper into ArgumentExpressionCreator

--- src/main/java/org/smoothbuild/lang/function/def/args/ArgumentExpressionCreator.java
+++ src/main/java/org/smoothbuild/lang/function/def/args/ArgumentExpressionCreator.java
@@ -1,19 +1,33 @@
 package org.smoothbuild.lang.function.def.args;
 
+import static org.smoothbuild.lang.base.Conversions.canConvert;
+import static org.smoothbuild.lang.base.Types.allTypes;
+import static org.smoothbuild.lang.function.base.Parameters.parametersToNames;
+
 import java.util.Collection;
 import java.util.Map;
+import java.util.Set;
 
 import javax.inject.Inject;
 
+import org.smoothbuild.lang.base.Type;
 import org.smoothbuild.lang.expr.Expression;
 import org.smoothbuild.lang.expr.ImplicitConverter;
 import org.smoothbuild.lang.function.base.Function;
 import org.smoothbuild.lang.function.base.Parameter;
+import org.smoothbuild.lang.function.def.args.err.AmbiguousNamelessArgsError;
+import org.smoothbuild.lang.function.def.args.err.DuplicateArgNameError;
+import org.smoothbuild.lang.function.def.args.err.MissingRequiredArgsError;
+import org.smoothbuild.lang.function.def.args.err.TypeMismatchError;
+import org.smoothbuild.lang.function.def.args.err.UnknownParamNameError;
 import org.smoothbuild.message.base.CodeLocation;
 import org.smoothbuild.message.listen.LoggedMessages;
 
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableMap.Builder;
+import com.google.common.collect.ImmutableMultimap;
+import com.google.common.collect.Sets;
 
 public class ArgumentExpressionCreator {
   private final ImplicitConverter implicitConverter;
@@ -25,10 +39,97 @@ public class ArgumentExpressionCreator {
 
   public ImmutableMap<String, Expression> createArgExprs(CodeLocation codeLocation,
       LoggedMessages messages, Function function, Collection<Argument> arguments) {
-    Mapper mapper = new Mapper(codeLocation, messages, function, arguments);
-    Map<Parameter, Argument> paramToArgMap = mapper.detectMapping();
+    ParametersPool parametersPool = new ParametersPool(function.parameters());
+    ImmutableList<Argument> namedArguments = Argument.filterNamed(arguments);
+
+    detectDuplicatedAndUnknownArgumentNames(function, messages, namedArguments);
+    if (messages.containsProblems()) {
+      return null;
+    }
+
+    Builder<Parameter, Argument> mapBuilder = ImmutableMap.builder();
+    processNamedArguments(parametersPool, messages, mapBuilder, namedArguments);
+    if (messages.containsProblems()) {
+      return null;
+    }
+
+    processNamelessArguments(function, arguments, parametersPool, messages, mapBuilder);
+    if (messages.containsProblems()) {
+      return null;
+    }
+    ImmutableMap<Parameter, Argument> mapping = mapBuilder.build();
+
+    Set<Parameter> missingRequiredParameters = parametersPool.allRequired();
+    if (missingRequiredParameters.size() != 0) {
+      messages.log(new MissingRequiredArgsError(codeLocation, function, mapping,
+          missingRequiredParameters));
+      return null;
+    }
+
     messages.failIfContainsProblems();
-    return convert(paramToArgMap);
+    return convert(mapping);
+  }
+
+  private static void detectDuplicatedAndUnknownArgumentNames(Function function,
+      LoggedMessages messages, Collection<Argument> namedArguments) {
+    Set<String> unusedNames = Sets.newHashSet(parametersToNames(function.parameters()));
+    Set<String> usedNames = Sets.newHashSet();
+    for (Argument argument : namedArguments) {
+      if (argument.hasName()) {
+        String name = argument.name();
+        if (unusedNames.contains(name)) {
+          unusedNames.remove(name);
+          usedNames.add(name);
+        } else if (usedNames.contains(name)) {
+          messages.log(new DuplicateArgNameError(argument));
+        } else {
+          messages.log(new UnknownParamNameError(function.name(), argument));
+        }
+      }
+    }
+  }
+
+  private static void processNamedArguments(ParametersPool parametersPool, LoggedMessages messages,
+      Builder<Parameter, Argument> mapBuilder, Collection<Argument> namedArguments) {
+    for (Argument argument : namedArguments) {
+      if (argument.hasName()) {
+        String name = argument.name();
+        Parameter parameter = parametersPool.take(name);
+        Type paramType = parameter.type();
+        if (!canConvert(argument.type(), paramType)) {
+          messages.log(new TypeMismatchError(argument, paramType));
+        } else {
+          mapBuilder.put(parameter, argument);
+        }
+      }
+    }
+  }
+
+  private static void processNamelessArguments(Function function, Collection<Argument> arguments,
+      ParametersPool parametersPool, LoggedMessages messages,
+      Builder<Parameter, Argument> mapBuilder) {
+    ImmutableMultimap<Type, Argument> namelessArgs = Argument.filterNameless(arguments);
+
+    for (Type type : allTypes()) {
+      Collection<Argument> availableArguments = namelessArgs.get(type);
+      int argsSize = availableArguments.size();
+      if (0 < argsSize) {
+        TypedParametersPool availableTypedParams = parametersPool.assignableFrom(type);
+
+        if (argsSize == 1 && availableTypedParams.hasCandidate()) {
+          Argument onlyArgument = availableArguments.iterator().next();
+          Parameter candidateParameter = availableTypedParams.candidate();
+          mapBuilder.put(candidateParameter, onlyArgument);
+          parametersPool.take(candidateParameter);
+        } else {
+          AmbiguousNamelessArgsError error =
+              new AmbiguousNamelessArgsError(function.name(), mapBuilder.build(),
+                  availableArguments, availableTypedParams);
+          messages.log(error);
+          return;
+        }
+      }
+    }
   }
 
   private ImmutableMap<String, Expression> convert(Map<Parameter, Argument> paramToArgMap) {
