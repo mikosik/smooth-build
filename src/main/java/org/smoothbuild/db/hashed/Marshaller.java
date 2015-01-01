84abd79fc0ea9d1990b6b8ce4a126ba03d554f29
Marcin Mikosik <marcin.mikosik@gmail.com>
2015-01-01 18:43:09 +0100

    got rid of Hashed interface

--- src/main/java/org/smoothbuild/db/hashed/Marshaller.java
+++ src/main/java/org/smoothbuild/db/hashed/Marshaller.java
@@ -7,7 +7,7 @@ import static org.smoothbuild.db.hashed.Constants.TRUE_AS_BYTE;
 import java.util.List;
 
 import org.smoothbuild.io.fs.base.Path;
-import org.smoothbuild.lang.base.Hashed;
+import org.smoothbuild.lang.base.Value;
 
 import com.google.common.hash.HashCode;
 import com.google.common.io.ByteArrayDataOutput;
@@ -20,9 +20,9 @@ public class Marshaller {
     this.dataOutput = ByteStreams.newDataOutput(256);
   }
 
-  public void write(List<? extends Hashed> elements) {
+  public void write(List<? extends Value> elements) {
     write(elements.size());
-    for (Hashed element : elements) {
+    for (Value element : elements) {
       write(element.hash());
     }
   }
