183422314a3395bf8962220247697ee35d6c2760
Marcin Mikosik <marcin.mikosik@gmail.com>
2015-01-01 18:03:24 +0100

    refactored ConstantExpression

--- src/main/java/org/smoothbuild/lang/expr/ConstantExpression.java
+++ src/main/java/org/smoothbuild/lang/expr/ConstantExpression.java
@@ -2,7 +2,6 @@ package org.smoothbuild.lang.expr;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
-import org.smoothbuild.lang.base.Type;
 import org.smoothbuild.lang.base.Value;
 import org.smoothbuild.message.base.CodeLocation;
 import org.smoothbuild.task.work.ConstantWorker;
@@ -12,8 +11,8 @@ import org.smoothbuild.util.Empty;
 public class ConstantExpression extends Expression {
   private final Value value;
 
-  public ConstantExpression(Type type, Value value, CodeLocation codeLocation) {
-    super(type, Empty.expressionList(), codeLocation);
+  public ConstantExpression(Value value, CodeLocation codeLocation) {
+    super(value.type(), Empty.expressionList(), codeLocation);
     this.value = checkNotNull(value);
   }
 
