585c8eb62b7f7f172bc61347104f6215e9c8c170
Marcin Mikosik <marcin.mikosik@gmail.com>
2015-01-04 15:40:58 +0100

    renamed NativeCallExpression -> NativeCallExpressionLegacy, NativeCallWorker -> NativeCallWorkerLegacy

--- src/main/java/org/smoothbuild/lang/expr/NativeCallExpressionLegacy.java
+++ src/main/java/org/smoothbuild/lang/expr/NativeCallExpressionLegacy.java
@@ -5,18 +5,18 @@ import java.util.Map;
 import org.smoothbuild.lang.function.base.Function;
 import org.smoothbuild.lang.function.nativ.NativeFunctionLegacy;
 import org.smoothbuild.message.base.CodeLocation;
-import org.smoothbuild.task.work.NativeCallWorker;
+import org.smoothbuild.task.work.NativeCallWorkerLegacy;
 import org.smoothbuild.task.work.TaskWorker;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 
-public class NativeCallExpression extends Expression {
+public class NativeCallExpressionLegacy extends Expression {
   private final Function function;
   private final boolean isGenerated;
   private final ImmutableMap<String, ? extends Expression> args;
 
-  public NativeCallExpression(NativeFunctionLegacy function, boolean isGenerated,
+  public NativeCallExpressionLegacy(NativeFunctionLegacy function, boolean isGenerated,
       CodeLocation codeLocation, Map<String, ? extends Expression> args) {
     super(function.type(), ImmutableList.copyOf(args.values()), codeLocation);
 
@@ -29,6 +29,6 @@ public class NativeCallExpression extends Expression {
   public TaskWorker createWorker() {
     NativeFunctionLegacy nativeFunction = (NativeFunctionLegacy) function;
     ImmutableList<String> parameterNames = ImmutableList.copyOf(args.keySet());
-    return new NativeCallWorker(nativeFunction, parameterNames, isGenerated, codeLocation());
+    return new NativeCallWorkerLegacy(nativeFunction, parameterNames, isGenerated, codeLocation());
   }
 }
