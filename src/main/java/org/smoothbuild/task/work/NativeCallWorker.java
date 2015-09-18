22ad87cff01efb1dd0abb8267675b373c14aa7b7
Marcin Mikosik <marcin.mikosik@gmail.com>
2015-09-18 16:29:41 +0200

    refactored out NativeCallAlgorithm from NativeCallWorker

--- src/main/java/org/smoothbuild/task/work/NativeCallWorker.java
+++ src/main/java/org/smoothbuild/task/work/NativeCallWorker.java
@@ -1,23 +1,21 @@
 package org.smoothbuild.task.work;
 
-import java.util.List;
-
 import org.smoothbuild.lang.function.nativ.NativeFunction;
-import org.smoothbuild.lang.value.Value;
 import org.smoothbuild.message.base.CodeLocation;
 import org.smoothbuild.task.base.TaskInput;
 import org.smoothbuild.task.base.TaskOutput;
+import org.smoothbuild.task.compute.NativeCallAlgorithm;
 import org.smoothbuild.task.exec.ContainerImpl;
 
 import com.google.common.hash.HashCode;
 
 public class NativeCallWorker extends TaskWorker {
-  private final NativeFunction function;
+  private final NativeCallAlgorithm algorithm;
 
   public NativeCallWorker(NativeFunction function, boolean isInternal, CodeLocation codeLocation) {
     super(nativeCallWorkerHash(function), function.type(), function.name().value(), isInternal,
         function.isCacheable(), codeLocation);
-    this.function = function;
+    this.algorithm = new NativeCallAlgorithm(function);
   }
 
   private static HashCode nativeCallWorkerHash(NativeFunction function) {
@@ -26,11 +24,6 @@ public class NativeCallWorker extends TaskWorker {
 
   @Override
   public TaskOutput execute(TaskInput input, ContainerImpl container) {
-    Value result = function.invoke(container, calculateArguments(input));
-    return new TaskOutput(result, container.messages());
-  }
-
-  private List<Value> calculateArguments(TaskInput input) {
-    return input.values();
+    return algorithm.execute(input, container);
   }
 }
