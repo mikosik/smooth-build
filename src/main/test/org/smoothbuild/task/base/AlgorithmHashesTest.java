ca9b24835a91e99af7f20fcf78bb6a0f0234fb65
Marcin Mikosik <marcin.mikosik@gmail.com>
2015-09-20 11:30:10 +0200

    added missing tests in AlgorithmHashesTest
    Added tests that check that constantAlgorithmHash() and
    nativeCallAlgorithmHash() returns different hashes for different
    arguments.

--- src/main/test/org/smoothbuild/task/base/AlgorithmHashesTest.java
+++ src/main/test/org/smoothbuild/task/base/AlgorithmHashesTest.java
@@ -1,5 +1,6 @@
 package org.smoothbuild.task.base;
 
+import static org.hamcrest.Matchers.not;
 import static org.smoothbuild.task.base.AlgorithmHashes.arrayAlgorithmHash;
 import static org.smoothbuild.task.base.AlgorithmHashes.constantAlgorithmHash;
 import static org.smoothbuild.task.base.AlgorithmHashes.identityAlgorithmHash;
@@ -22,7 +23,9 @@ import com.google.common.hash.HashCode;
 public class AlgorithmHashesTest {
   private HashSet<HashCode> hashes;
   private NativeFunction function;
+  private NativeFunction function2;
   private Value value;
+  private Value value2;
 
   @Test
   public void each_algorithm_has_different_hash() {
@@ -37,5 +40,25 @@ public class AlgorithmHashesTest {
     given(hashes.add(nativeCallAlgorithmHash(function)));
     when(hashes).size();
     thenReturned(4);
+  }
+
+  @Test
+  public void constant_algorithm_has_different_hash_for_different_values() throws Exception {
+    given(value = mock(Value.class));
+    given(willReturn(Hash.integer(1)), value).hash();
+    given(value2 = mock(Value.class));
+    given(willReturn(Hash.integer(2)), value2).hash();
+    when(constantAlgorithmHash(value));
+    thenReturned(not(constantAlgorithmHash(value2)));
+  }
+
+  @Test
+  public void native_call_algorithm_has_different_hash_for_different_functions() throws Exception {
+    given(function = mock(NativeFunction.class));
+    given(willReturn(Hash.integer(1)), function).hash();
+    given(function2 = mock(NativeFunction.class));
+    given(willReturn(Hash.integer(2)), function2).hash();
+    when(nativeCallAlgorithmHash(function));
+    thenReturned(not(nativeCallAlgorithmHash(function2)));
   }
 }
