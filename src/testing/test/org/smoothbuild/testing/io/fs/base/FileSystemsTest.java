15c66b629a4c760670c0779bfcd4df8b79880fec
Marcin Mikosik <marcin.mikosik@gmail.com>
2015-09-20 18:04:00 +0200

    added FileSystems.fileContent()

--- src/testing/test/org/smoothbuild/testing/io/fs/base/FileSystemsTest.java
+++ src/testing/test/org/smoothbuild/testing/io/fs/base/FileSystemsTest.java
@@ -2,6 +2,7 @@ package org.smoothbuild.testing.io.fs.base;
 
 import static org.smoothbuild.io.fs.base.Path.path;
 import static org.smoothbuild.testing.io.fs.base.FileSystems.createFile;
+import static org.smoothbuild.testing.io.fs.base.FileSystems.fileContent;
 import static org.smoothbuild.util.Streams.inputStreamToString;
 import static org.testory.Testory.given;
 import static org.testory.Testory.thenEqual;
@@ -30,5 +31,13 @@ public class FileSystemsTest {
     given(path = path("some/path"));
     when(createFile(fileSystem, path, "content"));
     thenEqual(inputStreamToString(fileSystem.openInputStream(path)), "content");
+  }
+
+  @Test
+  public void file_content() throws Exception {
+    given(fileSystem = new MemoryFileSystem());
+    given(path = path("some/path"));
+    when(createFile(fileSystem, path, "content"));
+    thenEqual(fileContent(fileSystem, path), "content");
   }
 }
