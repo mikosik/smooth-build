ca68387098d93afcf9c496c6a2159dc9bb725ae1
Marcin Mikosik <marcin.mikosik@gmail.com>
2015-09-20 18:04:00 +0200

    added FileSystems.fileContent()

--- src/testing/java/org/smoothbuild/testing/io/fs/base/FileSystems.java
+++ src/testing/java/org/smoothbuild/testing/io/fs/base/FileSystems.java
@@ -1,5 +1,7 @@
 package org.smoothbuild.testing.io.fs.base;
 
+import static org.smoothbuild.util.Streams.inputStreamToString;
+
 import java.io.IOException;
 
 import org.smoothbuild.io.fs.base.FileSystem;
@@ -11,5 +13,9 @@ public class FileSystems {
       throws IOException {
     StreamTester.writeAndClose(fileSystem.openOutputStream(path), content);
     return path;
+  }
+
+  public static String fileContent(FileSystem fileSystem, Path path) throws IOException {
+    return inputStreamToString(fileSystem.openInputStream(path));
   }
 }
