f4a901a7e72ad4d65af62b1adeb6a1d92ecc9fb7
Marcin Mikosik <marcin.mikosik@gmail.com>
2015-09-21 17:50:39 +0200

    got rid of FakeTempDirectoryManager

--- src/testing/java/org/smoothbuild/testing/task/exec/FakeContainer.java
+++ src/testing/java/org/smoothbuild/testing/task/exec/FakeContainer.java
@@ -1,13 +1,16 @@
 package org.smoothbuild.testing.task.exec;
 
+import javax.inject.Provider;
+
 import org.smoothbuild.io.fs.base.FileSystem;
 import org.smoothbuild.io.fs.base.Path;
 import org.smoothbuild.io.fs.mem.MemoryFileSystem;
+import org.smoothbuild.io.util.TempDirectory;
+import org.smoothbuild.io.util.TempDirectoryManager;
 import org.smoothbuild.lang.value.SFile;
 import org.smoothbuild.message.base.Message;
 import org.smoothbuild.task.exec.ContainerImpl;
 import org.smoothbuild.testing.db.objects.FakeObjectsDb;
-import org.smoothbuild.testing.io.temp.FakeTempDirectoryManager;
 import org.smoothbuild.testing.message.FakeLoggedMessages;
 
 public class FakeContainer extends ContainerImpl {
@@ -24,10 +27,19 @@ public class FakeContainer extends ContainerImpl {
   }
 
   public FakeContainer(FileSystem fileSystem, FakeObjectsDb objectsDb) {
-    super(fileSystem, objectsDb, new FakeTempDirectoryManager(objectsDb));
+    super(fileSystem, objectsDb, createTempDirectoryManager(objectsDb));
     this.fileSystem = fileSystem;
     this.messages = new FakeLoggedMessages();
     this.objectsDb = objectsDb;
+  }
+
+  public static TempDirectoryManager createTempDirectoryManager(final FakeObjectsDb objectsDb) {
+    return new TempDirectoryManager(new Provider<TempDirectory>() {
+      @Override
+      public TempDirectory get() {
+        return new TempDirectory(objectsDb);
+      }
+    });
   }
 
   @Override
