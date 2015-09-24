6f1e128873ada7901d5ec9da4c1c6c0f6bd4e0bb
Marcin Mikosik <marcin.mikosik@gmail.com>
2015-09-24 20:59:43 +0200

    added tests verifying that named arguments can be passed in any order

--- src/acceptance/org/smoothbuild/acceptance/argument/ArgumentTest.java
+++ src/acceptance/org/smoothbuild/acceptance/argument/ArgumentTest.java
@@ -1,5 +1,7 @@
 package org.smoothbuild.acceptance.argument;
 
+import static org.smoothbuild.acceptance.FileContentMatcher.hasContent;
+
 import java.io.IOException;
 
 import org.junit.Test;
@@ -11,5 +13,21 @@ public class ArgumentTest extends AcceptanceTestCase {
     givenBuildScript(script("result : toBlob(string='abc',) ;"));
     whenRunSmoothBuild("result");
     thenReturnedCode(0);
+  }
+
+  @Test
+  public void arguments_can_be_passed_in_the_same_order_as_parameters() throws Exception {
+    givenBuildScript(script("result : twoStrings(stringA='abc', stringB='def') ;"));
+    whenRunSmoothBuild("result");
+    thenReturnedCode(0);
+    thenArtifact("result", hasContent("abc:def"));
+  }
+
+  @Test
+  public void arguments_can_be_passed_in_reversed_order_of_parameters() throws Exception {
+    givenBuildScript(script("result : twoStrings(stringB='def', stringA='abc') ;"));
+    whenRunSmoothBuild("result");
+    thenReturnedCode(0);
+    thenArtifact("result", hasContent("abc:def"));
   }
 }
