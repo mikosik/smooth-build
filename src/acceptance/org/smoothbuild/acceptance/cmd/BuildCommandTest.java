e7181ac637a621a4c388101882824f6ecc654ba1
Marcin Mikosik <marcin.mikosik@gmail.com>
2015-09-29 22:44:33 +0200

    Added BuildCommandTest checking 'build' command error message

---
+++ src/acceptance/org/smoothbuild/acceptance/cmd/BuildCommandTest.java
@@ -0,0 +1,46 @@
+package org.smoothbuild.acceptance.cmd;
+
+import static org.testory.Testory.thenEqual;
+
+import org.junit.Test;
+import org.smoothbuild.acceptance.AcceptanceTestCase;
+
+public class BuildCommandTest extends AcceptanceTestCase {
+  @Test
+  public void build_command_without_function_argument_prints_error() throws Exception {
+    givenScript("result: 'abc';");
+    whenSmoothBuild();
+    thenReturnedCode(1);
+    thenEqual(output(), noFunctionArgError());
+  }
+
+  private static String noFunctionArgError() {
+    StringBuilder builder = new StringBuilder();
+    builder.append(" + SMOOTH EXECUTOR\n");
+    builder.append("   + ERROR: No function passed to build command.\n");
+    builder.append("     Pass at least one from following available functions:\n");
+    builder.append("       'result'\n");
+    builder.append(" + FAILED :(\n");
+    builder.append("   + 1 error(s)\n");
+    return builder.toString();
+  }
+
+  @Test
+  public void build_command_with_nonexistent_function_argument_prints_error() throws Exception {
+    givenScript("result: 'abc';");
+    whenSmoothBuild("nonexistentFunction");
+    thenReturnedCode(1);
+    thenEqual(output(), nonexistentFunctionArgError());
+  }
+
+  private String nonexistentFunctionArgError() {
+    StringBuilder builder = new StringBuilder();
+    builder.append(" + SMOOTH EXECUTOR\n");
+    builder.append("   + ERROR: Unknown function 'nonexistentFunction' passed in command line.\n");
+    builder.append("     Only following function(s) are available:\n");
+    builder.append("       'result'\n");
+    builder.append(" + FAILED :(\n");
+    builder.append("   + 1 error(s)\n");
+    return builder.toString();
+  }
+}
