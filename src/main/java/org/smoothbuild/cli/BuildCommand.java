1b82f88c3ea28d9abff7a0f98ceaaa5a0b2323db
Marcin Mikosik <marcin.mikosik@gmail.com>
2015-10-01 20:34:14 +0200

    refactored build command parsing to not use argparse4j

--- src/main/java/org/smoothbuild/cli/BuildCommand.java
+++ src/main/java/org/smoothbuild/cli/BuildCommand.java
@@ -1,7 +1,19 @@
 package org.smoothbuild.cli;
 
-import org.smoothbuild.MainModule;
+import java.util.List;
 
+import javax.inject.Inject;
+
+import org.smoothbuild.MainModule;
+import org.smoothbuild.cli.work.build.CommandLineArguments;
+import org.smoothbuild.cli.work.build.CommandLineParserPhase;
+import org.smoothbuild.lang.module.Module;
+import org.smoothbuild.message.listen.UserConsole;
+import org.smoothbuild.parse.ModuleParserPhase;
+import org.smoothbuild.task.exec.ExecutionData;
+import org.smoothbuild.task.exec.SmoothExecutorPhase;
+
+import com.google.common.collect.ImmutableList;
 import com.google.inject.Guice;
 
 public class BuildCommand implements Command {
@@ -23,6 +35,32 @@ public class BuildCommand implements Command {
 
   @Override
   public int execute(String[] args) {
-    return Guice.createInjector(new MainModule()).getInstance(Argparse4j.class).parse(args);
+    return Guice.createInjector(new MainModule()).getInstance(Build.class).run(args);
+  }
+
+  public static class Build {
+    @Inject
+    private UserConsole userConsole;
+    @Inject
+    private CommandLineParserPhase commandLineParserPhase;
+    @Inject
+    private ModuleParserPhase moduleParserPhase;
+    @Inject
+    private SmoothExecutorPhase smoothExecutorPhase;
+
+    public int run(String... functions) {
+      List<String> functionList = ImmutableList.copyOf(functions).subList(1, functions.length);
+      CommandLineArguments args = commandLineParserPhase.execute(functionList);
+
+      if (!userConsole.isProblemReported()) {
+        Module module = moduleParserPhase.execute(args);
+        if (!userConsole.isProblemReported()) {
+          smoothExecutorPhase.execute(new ExecutionData(args, module));
+        }
+      }
+
+      userConsole.printFinalSummary();
+      return userConsole.isProblemReported() ? 1 : 0;
+    }
   }
 }
