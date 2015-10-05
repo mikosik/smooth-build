81526ae44e3446d0f0ddff4840c65dee3c5f7e67
Marcin Mikosik <marcin.mikosik@gmail.com>
2015-10-05 19:22:28 +0200

    added CommandFailedException

--- src/main/java/org/smoothbuild/Main.java
+++ src/main/java/org/smoothbuild/Main.java
@@ -1,10 +1,16 @@
 package org.smoothbuild;
 
+import static org.smoothbuild.SmoothConstants.EXIT_CODE_ERROR;
+
+import org.smoothbuild.cli.CommandFailedException;
 import org.smoothbuild.cli.Commands;
 
 public class Main {
   public static void main(String[] args) {
-    int exitCode = Commands.execute(args);
-    System.exit(exitCode);
+    try {
+      System.exit(Commands.execute(args));
+    } catch (CommandFailedException e) {
+      System.exit(EXIT_CODE_ERROR);
+    }
   }
 }
