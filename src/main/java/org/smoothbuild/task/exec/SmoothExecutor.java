0d1da1980f2da82d6d0be2dfcc8a8deacea77305
Marcin Mikosik <marcin.mikosik@gmail.com>
2015-10-24 14:19:59 +0200

    replaced NoFunctionSpecifiedError with ExecutionException

--- src/main/java/org/smoothbuild/task/exec/SmoothExecutor.java
+++ src/main/java/org/smoothbuild/task/exec/SmoothExecutor.java
@@ -1,5 +1,6 @@
 package org.smoothbuild.task.exec;
 
+import java.util.List;
 import java.util.Set;
 
 import javax.inject.Inject;
@@ -7,8 +8,10 @@ import javax.inject.Inject;
 import org.smoothbuild.lang.function.base.Function;
 import org.smoothbuild.lang.function.base.Name;
 import org.smoothbuild.lang.module.Module;
-import org.smoothbuild.task.exec.err.NoFunctionSpecifiedError;
 import org.smoothbuild.task.exec.err.UnknownFunctionError;
+
+import com.google.common.base.Joiner;
+import com.google.common.collect.Ordering;
 
 public class SmoothExecutor {
   private final ArtifactBuilder artifactBuilder;
@@ -23,7 +26,8 @@ public class SmoothExecutor {
     Module module = executionData.module();
 
     if (functions.isEmpty()) {
-      throw new NoFunctionSpecifiedError(module.availableNames());
+      throw new ExecutionException("error: No function passed to build command.\n"
+          + "  Pass at least one from following available functions:" + indentedNameList(module));
     }
     for (Name name : executionData.functions()) {
       Function function = module.getFunction(name);
@@ -34,5 +38,12 @@ public class SmoothExecutor {
     }
 
     artifactBuilder.runBuild();
+  }
+
+  private String indentedNameList(Module module) {
+    String prefix = "\n    ";
+    List<Name> sortedNames = Ordering.usingToString().sortedCopy(module
+        .availableNames());
+    return prefix + Joiner.on(prefix).join(sortedNames);
   }
 }
