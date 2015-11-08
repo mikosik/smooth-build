6070bb760d4661f32ed470a4a0db659d1a1d463f
Marcin Mikosik <marcin.mikosik@gmail.com>
2015-11-08 10:58:21 +0100

    refactored RecursiveDeleter

--- src/main/java/org/smoothbuild/io/fs/disk/RecursiveDeleter.java
+++ src/main/java/org/smoothbuild/io/fs/disk/RecursiveDeleter.java
@@ -10,7 +10,7 @@ public class RecursiveDeleter {
    * Deletes file or directory recursively. Symbolic links are deleted but not
    * followed.
    */
-  public static void deleteRecursively(java.nio.file.Path path) throws IOException {
+  public static void deleteRecursively(Path path) throws IOException {
     if (Files.isDirectory(path) && !Files.isSymbolicLink(path)) {
       deleteDirectoryContents(path);
     }
@@ -22,7 +22,7 @@ public class RecursiveDeleter {
     Files.delete(path);
   }
 
-  private static void deleteDirectoryContents(java.nio.file.Path directory) throws IOException {
+  private static void deleteDirectoryContents(Path directory) throws IOException {
     try (DirectoryStream<Path> children = Files.newDirectoryStream(directory)) {
       for (Path child : children) {
         deleteRecursively(child);
