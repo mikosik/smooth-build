44d94b343a370fad4f57f3784489d8e6adcb51a7
Marcin Mikosik <marcin.mikosik@gmail.com>
2016-01-01 17:55:41 +0100

    refactored JarFunction to not use Worker

--- src/func/java/org/smoothbuild/builtin/java/JarFunction.java
+++ src/func/java/org/smoothbuild/builtin/java/JarFunction.java
@@ -21,66 +21,45 @@ import org.smoothbuild.util.DuplicatesDetector;
 public class JarFunction {
   @SmoothFunction
   public static Blob jar(Container container, Array<SFile> files, Blob manifest) {
-    return new Worker(container, files, manifest).execute();
+    DuplicatesDetector<String> duplicatesDetector = new DuplicatesDetector<>();
+    byte[] buffer = new byte[Constants.BUFFER_SIZE];
+    BlobBuilder blobBuilder = container.create().blobBuilder();
+    try (JarOutputStream jarOutputStream = createOutputStream(blobBuilder, manifest)) {
+      for (SFile file : files) {
+        String path = file.path().value();
+        if (duplicatesDetector.addValue(path)) {
+          throw new ErrorMessage("Cannot jar two files with the same path = " + path);
+        }
+        jarFile(file, jarOutputStream, buffer);
+      }
+    } catch (IOException e) {
+      throw new FileSystemException(e);
+    }
+    return blobBuilder.build();
   }
 
-  private static class Worker {
-    private final Container container;
-    private final Array<SFile> files;
-    private final Blob manifest;
-
-    private final byte[] buffer = new byte[Constants.BUFFER_SIZE];
-    private final DuplicatesDetector<String> duplicatesDetector;
-
-    public Worker(Container container, Array<SFile> files, Blob manifest) {
-      this.container = container;
-      this.files = files;
-      this.manifest = manifest;
-      this.duplicatesDetector = new DuplicatesDetector<>();
-    }
-
-    public Blob execute() {
-      BlobBuilder blobBuilder = container.create().blobBuilder();
-      try (JarOutputStream jarOutputStream = createOutputStream(blobBuilder)) {
-        for (SFile file : files) {
-          addEntry(jarOutputStream, file);
-        }
-      } catch (IOException e) {
-        throw new FileSystemException(e);
-      }
-
-      return blobBuilder.build();
-    }
-
-    private JarOutputStream createOutputStream(BlobBuilder blobBuilder) throws IOException {
-      OutputStream outputStream = blobBuilder;
-      if (manifest == null) {
-        return new JarOutputStream(outputStream);
-      } else {
-        try (InputStream manifestStream = manifest.openInputStream()) {
-          Manifest manifest = new Manifest(manifestStream);
-          return new JarOutputStream(outputStream, manifest);
-        }
+  private static JarOutputStream createOutputStream(BlobBuilder blobBuilder, Blob manifest)
+      throws IOException {
+    OutputStream outputStream = blobBuilder;
+    if (manifest == null) {
+      return new JarOutputStream(outputStream);
+    } else {
+      try (InputStream manifestStream = manifest.openInputStream()) {
+        return new JarOutputStream(outputStream, new Manifest(manifestStream));
       }
     }
+  }
 
-    private void addEntry(JarOutputStream jarOutputStream, SFile file) throws IOException {
-      String path = file.path().value();
-      if (duplicatesDetector.addValue(path)) {
-        throw new ErrorMessage("Cannot jar two files with the same path = " + path);
+  private static void jarFile(SFile file, JarOutputStream jarOutputStream, byte[] buffer)
+      throws IOException {
+    jarOutputStream.putNextEntry(new JarEntry(file.path().value()));
+    try (InputStream inputStream = file.content().openInputStream()) {
+      int readCount = inputStream.read(buffer);
+      while (readCount > 0) {
+        jarOutputStream.write(buffer, 0, readCount);
+        readCount = inputStream.read(buffer);
       }
-      JarEntry entry = new JarEntry(path);
-      jarOutputStream.putNextEntry(entry);
-
-      try (InputStream inputStream = file.content().openInputStream()) {
-        int readCount = inputStream.read(buffer);
-        while (readCount > 0) {
-          jarOutputStream.write(buffer, 0, readCount);
-          readCount = inputStream.read(buffer);
-        }
-      }
-
-      jarOutputStream.closeEntry();
     }
+    jarOutputStream.closeEntry();
   }
 }
