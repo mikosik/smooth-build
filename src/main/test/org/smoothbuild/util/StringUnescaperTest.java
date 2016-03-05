25a676b2706014787a556f09a8d93c0d9cf5e691
Marcin Mikosik <marcin.mikosik@gmail.com>
2016-03-05 20:44:28 +0100

    refactored StringUnescaperTest to use Quackery

--- src/main/test/org/smoothbuild/util/StringUnescaperTest.java
+++ src/main/test/org/smoothbuild/util/StringUnescaperTest.java
@@ -2,114 +2,93 @@ package org.smoothbuild.util;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
+import static org.quackery.Case.newCase;
+import static org.quackery.Suite.suite;
 import static org.smoothbuild.util.StringUnescaper.unescaped;
 
 import java.util.Map;
 
-import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.quackery.Case;
+import org.quackery.Case.Body;
+import org.quackery.Quackery;
+import org.quackery.Suite;
+import org.quackery.junit.QuackeryRunner;
 
 import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.ImmutableMap.Builder;
 
+@RunWith(QuackeryRunner.class)
 public class StringUnescaperTest {
-  private static final String T = "\t";
-  private static final String B = "\b";
-  private static final String N = "\n";
-  private static final String C = "\r";
-  private static final String F = "\f";
-  private static final String D = "\"";
-  private static final String S = "\\";
-
-  private static final String ET = S + "t";
-  private static final String EB = S + "b";
-  private static final String EN = S + "n";
-  private static final String EC = S + "r";
-  private static final String EF = S + "f";
-  private static final String ED = S + D;
-  private static final String ES = S + S;
-
-  private static final Map<String, String> UNESCAPE = unescapeMap();
-
-  @Test
-  public void string_that_doesnt_need_unescaping_is_not_changed() {
-    assertUnescapedResult("", "");
-    assertUnescapedResult(" ", " ");
-    assertUnescapedResult("  ", "  ");
-
-    assertUnescapedResult("a", "a");
-    assertUnescapedResult("ab", "ab");
-    assertUnescapedResult("abc", "abc");
-    assertUnescapedResult("abcd", "abcd");
+  @Quackery
+  public static Suite preserves_non_escaped_characters() {
+    return suite("preserves non escaped characters")
+        .add(shouldUnescape(""))
+        .add(shouldUnescape(" "))
+        .add(shouldUnescape("  "))
+        .add(shouldUnescape("a"))
+        .add(shouldUnescape("ab"))
+        .add(shouldUnescape("abc"))
+        .add(shouldUnescape("abcd"));
   }
 
-  @Test
-  public void slash_at_the_end_of_string_cannot_be_unescaped() throws Exception {
-    assertUnescapingFails(S, 0);
-    assertUnescapingFails("abc" + S, 3);
+  @Quackery
+  public static Suite fails_for_illegal_escapes() {
+    return suite("fails for illegal escapes")
+        .add(shouldFailUnescaping("\\", 0))
+        .add(shouldFailUnescaping("abc\\", 3))
+        .add(shouldFailUnescaping("\\a", 1))
+        .add(shouldFailUnescaping("\\ ", 1));
   }
 
-  @Test
-  public void incorrect_unescape_code_cannot_be_unescaped() throws Exception {
-    assertUnescapingFails(S + "x", 1);
-    assertUnescapingFails(S + "a", 1);
-    assertUnescapingFails(S + "w", 1);
-    assertUnescapingFails(S + " ", 1);
-  }
+  @Quackery
+  public static Suite unsecaping_combined() throws Exception {
+    Suite suite = suite("three escaped in a row");
+    ImmutableMap<String, String> mappings = ImmutableMap.<String, String> builder()
+        .put("\\t", "\t")
+        .put("\\b", "\b")
+        .put("\\n", "\n")
+        .put("\\r", "\r")
+        .put("\\f", "\f")
+        .put("\\\"", "\"")
+        .put("\\\\", "\\")
+        .build();
 
-  private void assertUnescapingFails(String string, int index) {
-    try {
-      StringUnescaper.unescaped(string);
-      fail("exception should be thrown");
-    } catch (UnescapingFailedException e) {
-      // expected
-      assertEquals(index, e.charIndex());
-    }
-  }
-
-  @Test
-  public void unescaping_single_escaped_element() throws Exception {
-    for (Map.Entry<String, String> entry : UNESCAPE.entrySet()) {
-      assertUnescapedResult(entry.getKey(), entry.getValue());
-    }
-  }
-
-  @Test
-  public void unescaping_two_escaped_elements() throws Exception {
-    for (Map.Entry<String, String> entry1 : UNESCAPE.entrySet()) {
-      for (Map.Entry<String, String> entry2 : UNESCAPE.entrySet()) {
-        String escaped = entry1.getKey() + entry2.getKey();
-        String unescaped = entry1.getValue() + entry2.getValue();
-        assertUnescapedResult(escaped, unescaped);
-      }
-    }
-  }
-
-  @Test
-  public void unescaping_three_escaped_elements() throws Exception {
-    for (Map.Entry<String, String> entry1 : UNESCAPE.entrySet()) {
-      for (Map.Entry<String, String> entry2 : UNESCAPE.entrySet()) {
-        for (Map.Entry<String, String> entry3 : UNESCAPE.entrySet()) {
-          String escaped = entry1.getKey() + entry2.getKey() + entry3.getKey();
-          String unescaped = entry1.getValue() + entry2.getValue() + entry3.getValue();
-          assertUnescapedResult(escaped, unescaped);
+    for (Map.Entry<String, String> entry1 : mappings.entrySet()) {
+      suite = suite.add(shouldUnescape(entry1.getKey(), entry1.getValue()));
+      for (Map.Entry<String, String> entry2 : mappings.entrySet()) {
+        suite = suite.add(shouldUnescape(
+            entry1.getKey() + entry2.getKey(),
+            entry1.getValue() + entry2.getValue()));
+        for (Map.Entry<String, String> entry3 : mappings.entrySet()) {
+          suite = suite.add(shouldUnescape(
+              entry1.getKey() + entry2.getKey() + entry3.getKey(),
+              entry1.getValue() + entry2.getValue() + entry3.getValue()));
         }
       }
     }
+    return suite;
   }
 
-  private void assertUnescapedResult(String input, String expected) {
-    assertEquals(expected, unescaped(input));
+  private static Case shouldUnescape(String escaped) {
+    return shouldUnescape(escaped, escaped);
   }
 
-  private static Map<String, String> unescapeMap() {
-    Builder<String, String> builder = ImmutableMap.builder();
-    builder.put(ET, T);
-    builder.put(EB, B);
-    builder.put(EN, N);
-    builder.put(EC, C);
-    builder.put(EF, F);
-    builder.put(ED, D);
-    builder.put(ES, S);
-    return builder.build();
+  private static Case shouldUnescape(String escaped, String unescaped) {
+    return newCase("[" + escaped + "] should be unescaped",
+        () -> assertEquals(unescaped, unescaped(escaped)));
+  }
+
+  private static Case shouldFailUnescaping(String escaped, int index) {
+    return newCase("Unescaping [" + escaped + "] should fail at " + index,
+        new Body() {
+          public void run() throws Throwable {
+            try {
+              unescaped(escaped);
+              fail("exception should be thrown");
+            } catch (UnescapingFailedException e) {
+              assertEquals(index, e.charIndex());
+            }
+          }
+        });
   }
 }
