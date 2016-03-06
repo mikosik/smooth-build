ce1a5583467766726df9eedf4065ce6cdc7dd029
Marcin Mikosik <marcin.mikosik@gmail.com>
2016-03-06 14:11:56 +0100

    inlined PathArgValidator.validatedPath() in blob.FileFunction

--- src/func/test/org/smoothbuild/builtin/file/PathArgValidatorTest.java
+++ src/func/test/org/smoothbuild/builtin/file/PathArgValidatorTest.java
@@ -1,7 +1,6 @@
 package org.smoothbuild.builtin.file;
 
 import static org.junit.Assert.fail;
-import static org.smoothbuild.builtin.file.PathArgValidator.validatedPath;
 import static org.smoothbuild.builtin.file.PathArgValidator.validatedProjectPath;
 import static org.smoothbuild.db.values.ValuesDb.memoryValuesDb;
 
@@ -10,33 +9,12 @@ import java.util.List;
 import org.junit.Test;
 import org.smoothbuild.db.values.ValuesDb;
 import org.smoothbuild.lang.message.Message;
-import org.smoothbuild.testing.io.fs.base.PathTesting;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableList.Builder;
 
 public class PathArgValidatorTest {
   private final ValuesDb valuesDb = memoryValuesDb();
-
-  @Test
-  public void illegal_paths_are_reported() {
-    String name = "name";
-    for (String path : PathTesting.listOfInvalidPaths()) {
-      try {
-        validatedPath(name, valuesDb.string(path));
-        fail("exception should be thrown");
-      } catch (Message e) {
-        // expected
-      }
-    }
-  }
-
-  @Test
-  public void valid_paths_are_accepted() {
-    for (String path : PathTesting.listOfCorrectPaths()) {
-      validatedPath("name", valuesDb.string(path));
-    }
-  }
 
   @Test
   public void illegal_project_paths_are_reported() {
