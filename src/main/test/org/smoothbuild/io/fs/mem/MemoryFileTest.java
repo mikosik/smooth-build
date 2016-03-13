17c2305efd0bb1977470ea4149d8ac1a5279ebdf
Marcin Mikosik <marcin.mikosik@gmail.com>
2016-03-13 19:40:01 +0100

    fixed some tests to use raw byte arrays instead of Strings

--- src/main/test/org/smoothbuild/io/fs/mem/MemoryFileTest.java
+++ src/main/test/org/smoothbuild/io/fs/mem/MemoryFileTest.java
@@ -1,7 +1,7 @@
 package org.smoothbuild.io.fs.mem;
 
 import static org.hamcrest.Matchers.sameInstance;
-import static org.smoothbuild.util.Streams.inputStreamToString;
+import static org.smoothbuild.util.Streams.inputStreamToByteArray;
 import static org.testory.Testory.given;
 import static org.testory.Testory.mock;
 import static org.testory.Testory.thenEqual;
@@ -9,7 +9,7 @@ import static org.testory.Testory.thenReturned;
 import static org.testory.Testory.thenThrown;
 import static org.testory.Testory.when;
 
-import java.io.OutputStreamWriter;
+import java.io.OutputStream;
 
 import org.junit.Test;
 import org.smoothbuild.io.fs.base.FileSystemException;
@@ -19,9 +19,9 @@ public class MemoryFileTest {
   private final MemoryDir parent = mock(MemoryDir.class);
   private final Path name = Path.path("some/path");
   private final Path otherName = Path.path("other/path");
-  private final String line = "some string";
+  private final byte[] line = new byte[] { 1, 2, 3 };
   private MemoryFile file;
-  private OutputStreamWriter writer;
+  private OutputStream outputStream;
 
   @Test
   public void name() {
@@ -89,9 +89,9 @@ public class MemoryFileTest {
   @Test
   public void data_written_to_memory_file_can_be_read_back() throws Exception {
     given(file = new MemoryFile(parent, name));
-    given(writer = new OutputStreamWriter(file.openOutputStream()));
-    given(writer).write(line);
-    given(writer).close();
-    thenEqual(line, inputStreamToString(file.openInputStream()));
+    given(outputStream = file.openOutputStream());
+    given(outputStream).write(line);
+    given(outputStream).close();
+    thenEqual(line, inputStreamToByteArray(file.openInputStream()));
   }
 }
