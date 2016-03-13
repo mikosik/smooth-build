77e50ed36b810cf2130606da378a67061be75222
Marcin Mikosik <marcin.mikosik@gmail.com>
2016-03-13 11:15:07 +0100

    inlined FileSystems.createFile()

--- src/testing/java/org/smoothbuild/testing/io/fs/base/FileSystems.java
+++ src/testing/java/org/smoothbuild/testing/io/fs/base/FileSystems.java
@@ -6,15 +6,8 @@ import java.io.IOException;
 
 import org.smoothbuild.io.fs.base.FileSystem;
 import org.smoothbuild.io.fs.base.Path;
-import org.smoothbuild.util.Streams;
 
 public class FileSystems {
-  public static Path createFile(FileSystem fileSystem, Path path, String content)
-      throws IOException {
-    Streams.writeAndClose(fileSystem.openOutputStream(path), content);
-    return path;
-  }
-
   public static String fileContent(FileSystem fileSystem, Path path) throws IOException {
     return inputStreamToString(fileSystem.openInputStream(path));
   }
