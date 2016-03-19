06b0ffc36bc2eace7468acce2f2a549853a03ff9
Marcin Mikosik <marcin.mikosik@gmail.com>
2016-03-19 08:41:44 +0100

    inlined ValueCreators.string()

--- src/testing/test/org/smoothbuild/testing/db/values/ValueCreatorsTest.java
+++ src/testing/test/org/smoothbuild/testing/db/values/ValueCreatorsTest.java
@@ -1,11 +1,7 @@
 package org.smoothbuild.testing.db.values;
 
-import static org.smoothbuild.SmoothConstants.CHARSET;
 import static org.smoothbuild.db.values.ValuesDb.memoryValuesDb;
-import static org.smoothbuild.io.fs.base.Path.path;
 import static org.smoothbuild.testing.db.values.ValueCreators.blob;
-import static org.smoothbuild.testing.db.values.ValueCreators.file;
-import static org.smoothbuild.testing.db.values.ValueCreators.string;
 import static org.smoothbuild.util.Streams.inputStreamToByteArray;
 import static org.testory.Testory.given;
 import static org.testory.Testory.thenReturned;
@@ -16,22 +12,9 @@ import org.smoothbuild.db.values.ValuesDb;
 import org.smoothbuild.lang.value.Blob;
 
 public class ValueCreatorsTest {
-  private final String path = "my/file";
   private final ValuesDb valuesDb = memoryValuesDb();
   private Blob blob;
   private final byte[] bytes = new byte[] { 1, 2, 3 };
-
-  @Test
-  public void creates_file_with_path_as_content() throws Exception {
-    when(file(valuesDb, path(path)));
-    thenReturned(valuesDb.file(string(valuesDb, path), blob(valuesDb, path.getBytes(CHARSET))));
-  }
-
-  @Test
-  public void creates_file_with_bytes_as_content() throws Exception {
-    when(file(valuesDb, path(path), bytes));
-    thenReturned(valuesDb.file(string(valuesDb, path), blob(valuesDb, bytes)));
-  }
 
   @Test
   public void creates_blob_with_bytes_as_content() throws Exception {
