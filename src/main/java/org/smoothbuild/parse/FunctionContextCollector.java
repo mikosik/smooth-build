f7929689d7510affd8a327d206497d9879357931
Marcin Mikosik <marcin.mikosik@gmail.com>
2017-04-30 10:02:51 +0200

    inlined FunctionsCollector into FunctionContextCollector

--- src/main/java/org/smoothbuild/parse/FunctionContextCollector.java
+++ src/main/java/org/smoothbuild/parse/FunctionContextCollector.java
@@ -1,18 +1,24 @@
 package org.smoothbuild.parse;
 
 import static java.util.stream.Collectors.toList;
+import static org.smoothbuild.lang.function.base.Name.name;
 import static org.smoothbuild.parse.DependencyCollector.collectDependencies;
 import static org.smoothbuild.parse.DependencySorter.sortDependencies;
-import static org.smoothbuild.parse.FunctionsCollector.collectFunctions;
+import static org.smoothbuild.parse.LocationHelpers.locationOf;
 import static org.smoothbuild.parse.Maybe.errors;
 import static org.smoothbuild.parse.Maybe.invoke;
 import static org.smoothbuild.parse.Maybe.invokeWrap;
+import static org.smoothbuild.parse.Maybe.maybe;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import org.smoothbuild.antlr.SmoothBaseVisitor;
 import org.smoothbuild.antlr.SmoothParser.FunctionContext;
+import org.smoothbuild.antlr.SmoothParser.FunctionNameContext;
 import org.smoothbuild.antlr.SmoothParser.ModuleContext;
 import org.smoothbuild.lang.function.Functions;
 import org.smoothbuild.lang.function.base.Name;
@@ -20,7 +26,7 @@ import org.smoothbuild.lang.function.base.Name;
 public class FunctionContextCollector {
   public static Maybe<List<FunctionContext>> collectFunctionContexts(ModuleContext module,
       Functions functions) {
-    Maybe<Map<Name, FunctionContext>> functionContexts = collectFunctions(functions, module);
+    Maybe<Map<Name, FunctionContext>> functionContexts = collectFunctions(module, functions);
     if (!functionContexts.hasValue()) {
       return errors(functionContexts.errors());
     }
@@ -34,5 +40,31 @@ public class FunctionContextCollector {
     return names.stream()
         .map(n -> functionContexts.get(n))
         .collect(toList());
+  }
+
+  private static Maybe<Map<Name, FunctionContext>> collectFunctions(ModuleContext module,
+      Functions functions) {
+    Map<Name, FunctionContext> functionContexts = new HashMap<>();
+    List<ParseError> errors = new ArrayList<>();
+    new SmoothBaseVisitor<Void>() {
+      public Void visitFunction(FunctionContext functionContext) {
+        FunctionNameContext nameContext = functionContext.functionName();
+        Name name = name(nameContext.getText());
+        if (functionContexts.keySet().contains(name)) {
+          errors.add(new ParseError(
+              locationOf(nameContext), "Function " + name + " is already defined."));
+          return null;
+        }
+        if (functions.contains(name)) {
+          errors.add(new ParseError(locationOf(nameContext), "Function " + name
+              + " cannot override builtin function with the same name."));
+          return null;
+        }
+
+        functionContexts.put(name, functionContext);
+        return null;
+      }
+    }.visit(module);
+    return maybe(functionContexts, errors);
   }
 }
