82017fccf33ee5fa66c1d0f1008010f4f4228e40
Marcin Mikosik <marcin.mikosik@gmail.com>
2017-05-02 15:09:47 +0200

    added Lists.map()

--- src/main/test/org/smoothbuild/util/ListsTest.java
+++ src/main/test/org/smoothbuild/util/ListsTest.java
@@ -2,6 +2,7 @@ package org.smoothbuild.util;
 
 import static java.util.Arrays.asList;
 import static org.smoothbuild.util.Lists.concat;
+import static org.smoothbuild.util.Lists.map;
 import static org.testory.Testory.given;
 import static org.testory.Testory.thenEqual;
 import static org.testory.Testory.thenReturned;
@@ -34,5 +35,26 @@ public class ListsTest {
     given(list = new ArrayList<>(asList("first")));
     when(concat(list, "second"));
     thenEqual(list, new ArrayList<>(asList("first")));
+  }
+
+  @Test
+  public void mapping_empty_returns_empty() throws Exception {
+    given(list = asList());
+    when(() -> map(list, String::toUpperCase));
+    thenReturned(asList());
+  }
+
+  @Test
+  public void mapping_with_one_element() throws Exception {
+    given(list = asList("abc"));
+    when(() -> map(list, String::toUpperCase));
+    thenReturned(asList("ABC"));
+  }
+
+  @Test
+  public void mapping_with_two_elements() throws Exception {
+    given(list = asList("abc", "def"));
+    when(() -> map(list, String::toUpperCase));
+    thenReturned(asList("ABC", "DEF"));
   }
 }
