7152f99a586c94ca37a5861321f647ad1febb8ae
Marcin Mikosik <marcin.mikosik@gmail.com>
2017-05-02 15:09:47 +0200

    added Lists.map()

--- src/main/java/org/smoothbuild/util/Lists.java
+++ src/main/java/org/smoothbuild/util/Lists.java
@@ -1,12 +1,22 @@
 package org.smoothbuild.util;
 
+import static java.util.stream.Collectors.toList;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 
 public class Lists {
   public static <E> List<E> concat(List<E> list, E element) {
     List<E> result = new ArrayList<>(list);
     result.add(element);
     return result;
+  }
+
+  public static <T, R> List<R> map(List<T> list, Function<? super T, ? extends R> function) {
+    return list
+        .stream()
+        .map(function)
+        .collect(toList());
   }
 }
