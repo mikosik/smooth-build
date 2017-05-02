93921e5d65da8497be1dfd3e85ad14a0ef24f382
Marcin Mikosik <marcin.mikosik@gmail.com>
2017-05-02 15:04:31 +0200

    refactored ListsTest

--- src/main/test/org/smoothbuild/util/ListsTest.java
+++ src/main/test/org/smoothbuild/util/ListsTest.java
@@ -1,12 +1,13 @@
 package org.smoothbuild.util;
 
+import static java.util.Arrays.asList;
+import static org.smoothbuild.util.Lists.concat;
 import static org.testory.Testory.given;
 import static org.testory.Testory.thenEqual;
 import static org.testory.Testory.thenReturned;
 import static org.testory.Testory.when;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Test;
@@ -17,21 +18,21 @@ public class ListsTest {
   @Test
   public void concat_to_empty() throws Exception {
     given(list = new ArrayList<>());
-    when(Lists.concat(list, "element"));
-    thenReturned(Arrays.asList("element"));
+    when(concat(list, "element"));
+    thenReturned(asList("element"));
   }
 
   @Test
-  public void concat() throws Exception {
-    given(list = new ArrayList<>(Arrays.asList("first")));
-    when(Lists.concat(list, "second"));
-    thenReturned(Arrays.asList("first", "second"));
+  public void concat_to_non_empty() throws Exception {
+    given(list = new ArrayList<>(asList("first")));
+    when(concat(list, "second"));
+    thenReturned(asList("first", "second"));
   }
 
   @Test
   public void concat_doesnt_modify_list() throws Exception {
-    given(list = new ArrayList<>(Arrays.asList("first")));
-    when(Lists.concat(list, "second"));
-    thenEqual(list, new ArrayList<>(Arrays.asList("first")));
+    given(list = new ArrayList<>(asList("first")));
+    when(concat(list, "second"));
+    thenEqual(list, new ArrayList<>(asList("first")));
   }
 }
