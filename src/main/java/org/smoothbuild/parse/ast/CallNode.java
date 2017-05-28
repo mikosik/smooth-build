4da10161e5bf2de65627d85efa1782bbb3b89672
Marcin Mikosik <marcin.mikosik@gmail.com>
2017-05-28 14:07:44 +0200

    changed type of CallNode.name() from String to Name

--- src/main/java/org/smoothbuild/parse/ast/CallNode.java
+++ src/main/java/org/smoothbuild/parse/ast/CallNode.java
@@ -2,21 +2,22 @@ package org.smoothbuild.parse.ast;
 
 import java.util.List;
 
+import org.smoothbuild.lang.function.base.Name;
 import org.smoothbuild.lang.message.CodeLocation;
 
 import com.google.common.collect.ImmutableList;
 
 public class CallNode extends ExprNode {
-  private final String name;
+  private final Name name;
   private final List<ArgNode> args;
 
-  public CallNode(String name, List<ArgNode> args, CodeLocation codeLocation) {
+  public CallNode(Name name, List<ArgNode> args, CodeLocation codeLocation) {
     super(codeLocation);
     this.name = name;
     this.args = ImmutableList.copyOf(args);
   }
 
-  public String name() {
+  public Name name() {
     return name;
   }
 
