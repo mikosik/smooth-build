f91d3552e2b54683ca2b360b7765ed8c68e52da1
Marcin Mikosik <marcin.mikosik@gmail.com>
2017-07-16 16:43:03 +0200

    added Node.get(), .set()

---
+++ src/main/test/org/smoothbuild/parse/NodeTest.java
@@ -0,0 +1,30 @@
+package org.smoothbuild.parse;
+
+import static org.smoothbuild.lang.message.CodeLocation.codeLocation;
+import static org.testory.Testory.given;
+import static org.testory.Testory.thenEqual;
+import static org.testory.Testory.thenThrown;
+import static org.testory.Testory.when;
+
+import java.util.NoSuchElementException;
+
+import org.junit.Test;
+import org.smoothbuild.parse.ast.Node;
+
+public class NodeTest {
+  private Node node;
+
+  @Test
+  public void set_attribute_can_be_retrieved() {
+    given(node = new Node(codeLocation(1)));
+    when(() -> node.set(String.class, "abc"));
+    thenEqual(node.get(String.class), "abc");
+  }
+
+  @Test
+  public void getting_nonexistent_attribute_fails() throws Exception {
+    given(node = new Node(codeLocation(1)));
+    when(() -> node.get(String.class));
+    thenThrown(NoSuchElementException.class);
+  }
+}
