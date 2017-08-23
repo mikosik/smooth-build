9805a994603d039b12bac7e43a80c51e0b883cdb
Marcin Mikosik <marcin.mikosik@gmail.com>
2017-08-23 14:32:22 +0200

    removed Parameter factory methods, used constructor instead

--- src/main/test/org/smoothbuild/lang/function/base/ParameterOrderingTest.java
+++ src/main/test/org/smoothbuild/lang/function/base/ParameterOrderingTest.java
@@ -2,7 +2,6 @@ package org.smoothbuild.lang.function.base;
 
 import static java.util.Arrays.asList;
 import static org.hamcrest.Matchers.contains;
-import static org.smoothbuild.lang.function.base.Parameter.parameter;
 import static org.smoothbuild.lang.function.base.ParameterOrdering.PARAMETER_ORDERING;
 import static org.smoothbuild.lang.type.Types.STRING;
 import static org.testory.Testory.given;
@@ -18,18 +17,18 @@ public class ParameterOrderingTest {
 
   @Test
   public void ordering_params() {
-    given(parameter1 = parameter(STRING, "aaa"));
-    given(parameter2 = parameter(STRING, "bbb"));
-    given(parameter3 = parameter(STRING, "ccc"));
+    given(parameter1 = new Parameter(STRING, "aaa", null));
+    given(parameter2 = new Parameter(STRING, "bbb", null));
+    given(parameter3 = new Parameter(STRING, "ccc", null));
     when(PARAMETER_ORDERING.sortedCopy(asList(parameter3, parameter2, parameter1)));
     thenReturned(contains(parameter1, parameter2, parameter3));
   }
 
   @Test
   public void ordering_params_of_different_length() {
-    given(parameter1 = parameter(STRING, "a"));
-    given(parameter2 = parameter(STRING, "aa"));
-    given(parameter3 = parameter(STRING, "aaa"));
+    given(parameter1 = new Parameter(STRING, "a", null));
+    given(parameter2 = new Parameter(STRING, "aa", null));
+    given(parameter3 = new Parameter(STRING, "aaa", null));
     when(PARAMETER_ORDERING.sortedCopy(asList(parameter3, parameter2, parameter1)));
     thenReturned(contains(parameter1, parameter2, parameter3));
   }
