730a6f1c555aa60afeb1cf9ccefa02ba04c0520c
Marcin Mikosik <marcin.mikosik@gmail.com>
2017-08-23 15:42:35 +0200

    merged Argument into ArgNode

--- src/main/java/org/smoothbuild/parse/ast/ArgNode.java
+++ src/main/java/org/smoothbuild/parse/ast/ArgNode.java
@@ -1,8 +1,12 @@
 package org.smoothbuild.parse.ast;
 
 import static com.google.common.base.Preconditions.checkState;
+import static com.google.common.base.Strings.padEnd;
 
 import org.smoothbuild.lang.message.CodeLocation;
+import org.smoothbuild.lang.type.Type;
+
+import com.google.common.collect.Ordering;
 
 public class ArgNode extends Node {
   private final int position;
@@ -29,7 +33,42 @@ public class ArgNode extends Node {
     return name;
   }
 
+  public String nameSanitized() {
+    return hasName() ? name() : "<nameless>";
+  }
+
+  public String typeAndName() {
+    return get(Type.class).name() + ":" + nameSanitized();
+  }
+
   public ExprNode expr() {
     return expr;
   }
+
+  public String toPaddedString(int minTypeLength, int minNameLength, int minPositionLength) {
+    String type = padEnd(get(Type.class).name(), minTypeLength, ' ') + ": ";
+    String name = padEnd(nameSanitized(), minNameLength, ' ');
+    String position = padEnd(positionString(), minPositionLength, ' ');
+    String location = codeLocation().toString();
+    return type + name + " #" + position + " " + location;
+  }
+
+  private String positionString() {
+    return position() == 0 ? "|" : Integer.toString(position());
+  }
+
+  public static final Ordering<ArgNode> POSITION_ORDERING = new Ordering<ArgNode>() {
+    public int compare(ArgNode argument1, ArgNode argument2) {
+      int position1 = argument1.position();
+      int position2 = argument2.position();
+      if (position1 == position2) {
+        return 0;
+      }
+      if (position1 < position2) {
+        return -1;
+      } else {
+        return 1;
+      }
+    }
+  };
 }
