70a3e4182d4f5175a5803ce0b849764a67211bec
Marcin Mikosik <marcin.mikosik@gmail.com>
2017-08-26 14:22:31 +0200

    removed obsolete methods from Parameters

--- src/main/java/org/smoothbuild/lang/function/base/Parameters.java
+++ src/main/java/org/smoothbuild/lang/function/base/Parameters.java
@@ -1,9 +1,5 @@
 package org.smoothbuild.lang.function.base;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
-
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 
@@ -28,39 +24,10 @@ public class Parameters {
     return builder.build();
   }
 
-  public static ImmutableList<String> parametersToNames(Iterable<Parameter> params) {
-    ImmutableList.Builder<String> builder = ImmutableList.builder();
-    for (Parameter parameter : params) {
-      builder.add(parameter.name());
-    }
-    return builder.build();
-  }
-
-  public static ImmutableMap<String, Parameter> parametersToMap(Parameter... parameters) {
-    return parametersToMap(Arrays.asList(parameters));
-  }
-
   public static ImmutableMap<String, Parameter> parametersToMap(Iterable<Parameter> params) {
     ImmutableMap.Builder<String, Parameter> builder = ImmutableMap.builder();
     for (Parameter parameter : params) {
       builder.put(parameter.name(), parameter);
-    }
-    return builder.build();
-  }
-
-  /**
-   * @return Parameters ordered lexicographically by their names.
-   */
-  public static ImmutableList<Parameter> sortedParameters(Iterable<Parameter> params) {
-    Set<String> names = new HashSet<>();
-    ImmutableList.Builder<Parameter> builder = ImmutableList.builder();
-    for (Parameter parameter : ParameterOrdering.PARAMETER_ORDERING.sortedCopy(params)) {
-      String name = parameter.name();
-      if (names.contains(name)) {
-        throw new IllegalArgumentException("Duplicate param name = '" + name + "'");
-      }
-      builder.add(parameter);
-      names.add(name);
     }
     return builder.build();
   }
