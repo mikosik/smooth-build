0011f6017759a7a83364da5d1e011112aff5289b
Marcin Mikosik <marcin.mikosik@gmail.com>
2017-08-27 15:25:00 +0200

    Node.set() forbids null values

--- src/main/java/org/smoothbuild/parse/ast/Node.java
+++ src/main/java/org/smoothbuild/parse/ast/Node.java
@@ -1,5 +1,7 @@
 package org.smoothbuild.parse.ast;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.NoSuchElementException;
@@ -26,7 +28,7 @@ public class Node {
   }
 
   public <T> void set(Class<T> clazz, T value) {
-    map().put(clazz, value);
+    map().put(clazz, checkNotNull(value));
   }
 
   public boolean has(Class<?> clazz) {
