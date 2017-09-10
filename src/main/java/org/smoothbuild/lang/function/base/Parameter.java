db60ff3bfa84f6f01df169caa89a1931bdea8662
Marcin Mikosik <marcin.mikosik@gmail.com>
2017-09-10 19:32:42 +0200

    refactored Parameter to extend TypedName, improved ParameterTest

--- src/main/java/org/smoothbuild/lang/function/base/Parameter.java
+++ src/main/java/org/smoothbuild/lang/function/base/Parameter.java
@@ -1,28 +1,16 @@
 package org.smoothbuild.lang.function.base;
 
-import static com.google.common.base.Preconditions.checkNotNull;
 import static com.google.common.base.Strings.padEnd;
 
 import org.smoothbuild.lang.expr.Expression;
 import org.smoothbuild.lang.type.Type;
 
-public class Parameter {
-  private final Type type;
-  private final Name name;
+public class Parameter extends TypedName {
   private final Expression defaultValue;
 
   public Parameter(Type type, Name name, Expression defaultValue) {
-    this.type = checkNotNull(type);
-    this.name = checkNotNull(name);
+    super(type, name);
     this.defaultValue = defaultValue;
-  }
-
-  public Type type() {
-    return type;
-  }
-
-  public Name name() {
-    return name;
   }
 
   public boolean isRequired() {
@@ -33,27 +21,14 @@ public class Parameter {
     return defaultValue;
   }
 
-  public final boolean equals(Object object) {
-    if (!(object instanceof Parameter)) {
-      return false;
-    }
-    Parameter that = (Parameter) object;
-    return this.type.equals(that.type) && this.name.equals(that.name)
-        && this.isRequired() == that.isRequired();
-  }
-
-  public final int hashCode() {
-    return 17 * type.hashCode() + name.hashCode();
-  }
-
   public String toPaddedString(int minTypeLength, int minNameLength) {
-    String typePart = padEnd(type.name(), minTypeLength, ' ') + ": ";
-    String namePart = padEnd(name.toString(), minNameLength, ' ');
+    String typePart = padEnd(type().name(), minTypeLength, ' ') + ": ";
+    String namePart = padEnd(name().toString(), minNameLength, ' ');
     return typePart + namePart;
   }
 
   public String toString() {
-    return "Param(" + type.name() + ": " + name + ")";
+    return "Param(" + type().name() + ": " + name() + ")";
   }
 
   public static String parametersToString(Iterable<Parameter> parameters) {
