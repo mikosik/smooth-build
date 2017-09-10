ff77b51a8a04f6ea2f76e96e73a01bc6ccf44ef9
Marcin Mikosik <marcin.mikosik@gmail.com>
2017-09-10 18:58:35 +0200

    refactored Signature to extend TypedName

--- src/main/java/org/smoothbuild/lang/function/base/Signature.java
+++ src/main/java/org/smoothbuild/lang/function/base/Signature.java
@@ -1,7 +1,5 @@
 package org.smoothbuild.lang.function.base;
 
-import static com.google.common.base.Preconditions.checkNotNull;
-
 import org.smoothbuild.lang.type.Type;
 
 import com.google.common.collect.ImmutableList;
@@ -9,23 +7,12 @@ import com.google.common.collect.ImmutableList;
 /**
  * Function's signature.
  */
-public class Signature {
-  private final Type type;
-  private final Name name;
+public class Signature extends TypedName {
   private final ImmutableList<Parameter> parameters;
 
   public Signature(Type type, Name name, Iterable<Parameter> params) {
-    this.type = checkNotNull(type);
-    this.name = checkNotNull(name);
+    super(type, name);
     this.parameters = ImmutableList.copyOf(params);
-  }
-
-  public Type type() {
-    return type;
-  }
-
-  public Name name() {
-    return name;
   }
 
   public ImmutableList<Parameter> parameters() {
@@ -34,7 +21,7 @@ public class Signature {
 
   public String toString() {
     StringBuilder builder = new StringBuilder();
-    builder.append(type.name() + " " + name + "(");
+    builder.append(super.toString() + "(");
     int count = 0;
     for (Parameter parameter : parameters) {
       if (count != 0) {
