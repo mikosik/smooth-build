66b4c2dd9921d06e69fbfd9c515e88be63211428
Marcin Mikosik <marcin.mikosik@gmail.com>
2017-09-11 20:02:48 +0200

    added Lists.filter()

--- src/main/java/org/smoothbuild/util/Lists.java
+++ src/main/java/org/smoothbuild/util/Lists.java
@@ -6,12 +6,20 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class Lists {
   public static <E> List<E> concat(List<E> list, E element) {
     List<E> result = new ArrayList<>(list);
     result.add(element);
     return result;
+  }
+
+  public static <T> List<T> filter(List<T> list, Predicate<? super T> predicate) {
+    return list
+        .stream()
+        .filter(predicate)
+        .collect(toList());
   }
 
   public static <T, R> List<R> map(Collection<T> list, Function<? super T, ? extends R> function) {
