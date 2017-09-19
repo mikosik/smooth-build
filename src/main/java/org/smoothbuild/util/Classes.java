7fee417b147488e136b2e824bb0e0e2101d810a8
Marcin Mikosik <marcin.mikosik@gmail.com>
2017-09-19 13:48:51 +0200

    refactored out Classes.saveBytecodeInJar() from NativeLibraryLoaderTest

--- src/main/java/org/smoothbuild/util/Classes.java
+++ src/main/java/org/smoothbuild/util/Classes.java
@@ -2,8 +2,14 @@ package org.smoothbuild.util;
 
 import static com.google.common.io.ByteStreams.toByteArray;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.jar.JarOutputStream;
+import java.util.zip.ZipEntry;
+
+import com.google.common.io.ByteStreams;
 
 public class Classes {
   public static final String CLASS_FILE_EXTENSION = ".class";
@@ -19,6 +25,17 @@ public class Classes {
     }
     int newLength = binaryPath.length() - CLASS_FILE_EXTENSION.length();
     return binaryPath.substring(0, newLength).replace('/', '.');
+  }
+
+  public static void saveBytecodeInJar(File jarFile, Class<?>... classes) throws IOException {
+    try (JarOutputStream jarOutputStream = new JarOutputStream(new FileOutputStream(jarFile))) {
+      for (Class<?> clazz : classes) {
+        jarOutputStream.putNextEntry(new ZipEntry(binaryPath(clazz)));
+        try (InputStream byteCodeInputStream = byteCodeAsInputStream(clazz)) {
+          ByteStreams.copy(byteCodeInputStream, jarOutputStream);
+        }
+      }
+    }
   }
 
   public static InputStream byteCodeAsInputStream(Class<?> clazz) {
