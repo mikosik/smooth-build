6436f46b61ec4ad0130f42022ebd2b7825d6bec3
Marcin Mikosik <marcin.mikosik@gmail.com>
2017-09-29 15:33:09 +0200

    added Paths.changeExtension()

--- src/main/java/org/smoothbuild/util/Paths.java
+++ src/main/java/org/smoothbuild/util/Paths.java
@@ -10,4 +10,13 @@ public class Paths {
       throws FileNotFoundException {
     return new BufferedInputStream(new FileInputStream(path.toFile()));
   }
+
+  public static Path changeExtension(Path path, String extension) {
+    String string = removeExtension(path.toString()) + "." + extension;
+    return java.nio.file.Paths.get(string);
+  }
+
+  private static String removeExtension(String string) {
+    return string.substring(0, string.lastIndexOf('.'));
+  }
 }
---
+++ src/main/test/org/smoothbuild/util/PathsTest.java
@@ -0,0 +1,31 @@
+package org.smoothbuild.util;
+
+import static org.testory.Testory.given;
+import static org.testory.Testory.thenReturned;
+import static org.testory.Testory.when;
+
+import java.nio.file.Path;
+
+import org.junit.Test;
+
+public class PathsTest {
+  private Path path;
+
+  @Test
+  public void change_extension_to_different_one() throws Exception {
+    given(path = newPath("abc/def.txt"));
+    when(() -> Paths.changeExtension(path, "md"));
+    thenReturned(newPath("abc/def.md"));
+  }
+
+  @Test
+  public void change_extension_on_path_that_does_not_have_one() throws Exception {
+    given(path = newPath("abc/def.txt"));
+    when(() -> Paths.changeExtension(path, "md"));
+    thenReturned(newPath("abc/def.md"));
+  }
+
+  private static Path newPath(String path) {
+    return java.nio.file.Paths.get(path);
+  }
+}
