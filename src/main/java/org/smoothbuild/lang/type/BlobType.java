f69251a3035ec03a30473373684e27d01b2ac62b
Marcin Mikosik <marcin.mikosik@gmail.com>
2017-11-05 13:38:20 +0100

    remove dead code: implementations of Type.defaultValue()

--- src/main/java/org/smoothbuild/lang/type/BlobType.java
+++ src/main/java/org/smoothbuild/lang/type/BlobType.java
@@ -1,14 +1,9 @@
 package org.smoothbuild.lang.type;
 
-import org.smoothbuild.db.values.ValuesDb;
 import org.smoothbuild.lang.value.Blob;
 
 public class BlobType extends Type {
   protected BlobType() {
     super("Blob", Blob.class);
-  }
-
-  public Blob defaultValue(ValuesDb valuesDb) {
-    return valuesDb.blobBuilder().build();
   }
 }
