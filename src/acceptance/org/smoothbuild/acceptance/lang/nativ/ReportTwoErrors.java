115b01803197ab56da017698049aa8d251bde4e2
Marcin Mikosik <marcin.mikosik@gmail.com>
2017-11-25 18:11:35 +0100

    add test to NativeFunctionTest

---
+++ src/acceptance/org/smoothbuild/acceptance/lang/nativ/ReportTwoErrors.java
@@ -0,0 +1,16 @@
+package org.smoothbuild.acceptance.lang.nativ;
+
+import static org.smoothbuild.lang.message.MessageException.errorException;
+
+import org.smoothbuild.lang.message.ErrorMessage;
+import org.smoothbuild.lang.plugin.NativeApi;
+import org.smoothbuild.lang.plugin.SmoothFunction;
+import org.smoothbuild.lang.value.SString;
+
+public class ReportTwoErrors {
+  @SmoothFunction
+  public static SString reportTwoErrors(NativeApi nativeApi, SString message1, SString message2) {
+    nativeApi.log(new ErrorMessage(message1.value()));
+    throw errorException(message2.value());
+  }
+}
