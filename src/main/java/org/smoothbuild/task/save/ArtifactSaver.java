f6fd92c76493d7b3fc761f67866c69fc2604b5b4
Marcin Mikosik <marcin.mikosik@gmail.com>
2017-11-26 19:35:25 +0100

    merge all classes in .task.save into ArtifactSaver

--- src/main/java/org/smoothbuild/task/save/ArtifactSaver.java
+++ src/main/java/org/smoothbuild/task/save/ArtifactSaver.java
@@ -1,40 +1,108 @@
 package org.smoothbuild.task.save;
 
-import static org.smoothbuild.lang.type.ArrayType.arrayOf;
-import static org.smoothbuild.lang.type.Types.BLOB;
+import static java.util.stream.Collectors.joining;
+import static org.smoothbuild.io.fs.base.Path.path;
 import static org.smoothbuild.lang.type.Types.FILE;
-import static org.smoothbuild.lang.type.Types.NOTHING;
-import static org.smoothbuild.lang.type.Types.STRING;
+import static org.smoothbuild.task.save.ArtifactPaths.artifactPath;
+import static org.smoothbuild.task.save.ArtifactPaths.targetPath;
+
+import java.util.Set;
 
 import javax.inject.Inject;
 
 import org.smoothbuild.cli.Console;
 import org.smoothbuild.io.fs.base.FileSystem;
+import org.smoothbuild.io.fs.base.Path;
 import org.smoothbuild.lang.function.base.Name;
 import org.smoothbuild.lang.type.Type;
+import org.smoothbuild.lang.value.Array;
+import org.smoothbuild.lang.value.SFile;
 import org.smoothbuild.lang.value.Value;
-
-import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.ImmutableMap.Builder;
+import org.smoothbuild.util.DuplicatesDetector;
 
 public class ArtifactSaver {
-  private final ImmutableMap<Type, Saver<?>> saversMap;
+  private final FileSystem fileSystem;
+  private final Console console;
 
   @Inject
   public ArtifactSaver(FileSystem fileSystem, Console console) {
-    Builder<Type, Saver<?>> builder = ImmutableMap.builder();
-    builder.put(STRING, new StringSaver(fileSystem));
-    builder.put(BLOB, new BlobSaver(fileSystem));
-    builder.put(FILE, new FileSaver(fileSystem));
-    builder.put(arrayOf(STRING), new ArraySaver(fileSystem));
-    builder.put(arrayOf(BLOB), new ArraySaver(fileSystem));
-    builder.put(arrayOf(FILE), new FileArraySaver(fileSystem, console));
-    builder.put(arrayOf(NOTHING), new ArraySaver(fileSystem));
-    this.saversMap = builder.build();
+    this.fileSystem = fileSystem;
+    this.console = console;
   }
 
-  public <T extends Value> void save(Name name, T value) {
-    Saver<T> saver = (Saver<T>) saversMap.get(value.type());
-    saver.save(name, value);
+  public void save(Name name, Value value) {
+    if (value instanceof Array) {
+      saveArray(name, (Array) value);
+    } else if (value.type().equals(FILE)) {
+      saveValue(name, ((SFile) value).content());
+    } else {
+      saveValue(name, value);
+    }
+  }
+
+  private void saveArray(Name name, Array array) {
+    Type elemType = array.type().elemType();
+    if (elemType.equals(FILE)) {
+      saveFileArray(name, array);
+    } else {
+      saveValueArray(name, array);
+    }
+  }
+
+  private void saveValueArray(Name name, Array array) {
+    Path artifactPath = artifactPath(name);
+
+    fileSystem.delete(artifactPath);
+
+    // Create dir explicitly. When fileArray is empty for loop below won't
+    // create empty dir for us.
+    fileSystem.createDir(artifactPath);
+
+    int i = 0;
+    for (Value value : array.asIterable(Value.class)) {
+      Path filePath = path(Integer.valueOf(i).toString());
+      Path sourcePath = artifactPath.append(filePath);
+      Path targetPath = targetPath(value);
+      fileSystem.createLink(sourcePath, targetPath);
+      i++;
+    }
+  }
+
+  private void saveFileArray(Name name, Array fileArray) {
+    DuplicatesDetector<String> duplicatesDetector = new DuplicatesDetector<>();
+
+    Path artifactPath = artifactPath(name);
+    fileSystem.delete(artifactPath);
+
+    // Create dir explicitly. When fileArray is empty for loop below won't
+    // create empty dir for us.
+    fileSystem.createDir(artifactPath);
+
+    for (SFile file : fileArray.asIterable(SFile.class)) {
+      Path sourcePath = artifactPath.append(path(file.path().value()));
+      if (!duplicatesDetector.addValue(file.path().value())) {
+        Path targetPath = targetPath(file.content());
+        fileSystem.createLink(sourcePath, targetPath);
+      }
+    }
+
+    if (duplicatesDetector.hasDuplicates()) {
+      Set<String> duplicates = duplicatesDetector.getDuplicateValues();
+      console.error(duplicatedPathsMessage(name, duplicates));
+    }
+  }
+
+  private String duplicatedPathsMessage(Name name, Set<String> duplicates) {
+    String separator = "\n  ";
+    String list = separator + duplicates.stream().collect(joining(separator));
+    return "Can't store result of '" + name + "' as it contains files with duplicated paths:"
+        + list;
+  }
+
+  private void saveValue(Name name, Value value) {
+    Path artifactPath = artifactPath(name);
+    Path targetPath = targetPath(value);
+    fileSystem.delete(artifactPath);
+    fileSystem.createLink(artifactPath, targetPath);
   }
 }
--- src/main/java/org/smoothbuild/task/save/Saver.java
+++
@@ -1,8 +0,0 @@
-package org.smoothbuild.task.save;
-
-import org.smoothbuild.lang.function.base.Name;
-import org.smoothbuild.lang.value.Value;
-
-public interface Saver<T extends Value> {
-  public void save(Name name, T value);
-}
