3ff3ca5aa2d21bc1d343584b0c28c3d558f67655
Marcin Mikosik <marcin.mikosik@gmail.com>
2017-11-27 12:57:29 +0100

    extend ArrayMatcher to work with arrays of gepth > 1

--- src/acceptance/org/smoothbuild/acceptance/ArrayMatcher.java
+++ src/acceptance/org/smoothbuild/acceptance/ArrayMatcher.java
@@ -1,49 +1,90 @@
 package org.smoothbuild.acceptance;
 
+import static java.util.Arrays.stream;
 import static org.smoothbuild.util.Streams.inputStreamToString;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.UncheckedIOException;
 import java.util.Arrays;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
 
 public class ArrayMatcher extends TypeSafeMatcher<File> {
-  private final String[] expectedElements;
+  private final Object[] expected;
 
-  public static Matcher<File> isArrayWith(String... expectedElements) {
+  public static Matcher<File> isArrayWith(Object... expectedElements) {
     return new ArrayMatcher(expectedElements);
   }
 
-  private ArrayMatcher(String... expectedElements) {
-    this.expectedElements = expectedElements;
+  private ArrayMatcher(Object... expectedElements) {
+    this.expected = expectedElements;
   }
 
   @Override
   public void describeTo(Description description) {
-    description.appendText("is array dir with elements = " + Arrays.toString(expectedElements));
+    description.appendText("is array dir with elements = " + objectToString(expected));
   }
 
   @Override
   protected boolean matchesSafely(File item) {
     try {
-      return item.isDirectory() && containsExpectedElements(item)
-          && item.list().length == expectedElements.length;
+      Object actual = actual(item);
+      return actual != null && actual.getClass().isArray() &&
+          Arrays.deepEquals(expected, (Object[]) actual);
     } catch (IOException e) {
       throw new RuntimeException(e);
     }
   }
 
-  private boolean containsExpectedElements(File dir) throws IOException {
-    for (int i = 0; i < expectedElements.length; i++) {
-      File file = new File(dir, Integer.toString(i));
-      if (!expectedElements[i].equals(inputStreamToString(new FileInputStream(file)))) {
-        return false;
-      }
+  @Override
+  protected void describeMismatchSafely(File item, Description mismatchDescription) {
+    try {
+      mismatchDescription
+          .appendText("was\n")
+          .appendText(objectToString(actual(item)));
+    } catch (IOException e) {
+      throw new UncheckedIOException(e);
     }
-    return true;
+  }
+
+  private static Object actual(File file) throws IOException {
+    if (!file.exists()) {
+      return null;
+    }
+    if (file.isDirectory()) {
+      return actualArray(file);
+    }
+    return inputStreamToString(new FileInputStream(file));
+  }
+
+  private static Object actualArray(File file) throws IOException {
+    int count = file.list().length;
+    Object[] result = new Object[count];
+    for (int i = 0; i < count; i++) {
+      result[i] = actual(new File(file, Integer.toString(i)));
+    }
+    return result;
+  }
+
+  private static String objectToString(Object object) {
+    if (object == null) {
+      return "null";
+    } else if (object.getClass().isArray()) {
+      return arrayToString((Object[]) object);
+    } else {
+      return Objects.toString(object);
+    }
+  }
+
+  private static String arrayToString(Object[] array) {
+    return "[" + stream(array)
+        .map(ArrayMatcher::objectToString)
+        .collect(Collectors.joining(", ")) + "]";
   }
 }
