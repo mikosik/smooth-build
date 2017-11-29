f8ee8ebc685351891815c8894b94e63ad1430d19
Marcin Mikosik <marcin.mikosik@gmail.com>
2017-11-29 01:56:03 +0100

    make SFile implementation more generic to simplify redesigning it into generic Struct Value

--- src/main/java/org/smoothbuild/lang/value/SFile.java
+++ src/main/java/org/smoothbuild/lang/value/SFile.java
@@ -6,38 +6,41 @@ import org.smoothbuild.db.hashed.HashedDb;
 import org.smoothbuild.db.hashed.Marshaller;
 import org.smoothbuild.db.hashed.Unmarshaller;
 
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableMap.Builder;
 import com.google.common.hash.HashCode;
 
 public class SFile extends Value {
-  private final SString path;
-  private final Blob content;
+  private final ImmutableMap<String, Value> fields;
 
   public SFile(HashCode hash, HashedDb hashedDb) {
     super(FILE, hash);
     try (Unmarshaller unmarshaller = hashedDb.newUnmarshaller(hash)) {
-      this.path = new SString(unmarshaller.readHash(), hashedDb);
-      this.content = new Blob(unmarshaller.readHash(), hashedDb);
+      Builder<String, Value> builder = ImmutableMap.builder();
+      builder.put("content", new Blob(unmarshaller.readHash(), hashedDb));
+      builder.put("path", new SString(unmarshaller.readHash(), hashedDb));
+      fields = builder.build();
     }
   }
 
   public static SFile storeFileInDb(SString path, Blob content, HashedDb hashedDb) {
     Marshaller marshaller = hashedDb.newMarshaller();
-    marshaller.writeHash(path.hash());
     marshaller.writeHash(content.hash());
+    marshaller.writeHash(path.hash());
     marshaller.close();
     return new SFile(marshaller.hash(), hashedDb);
   }
 
   public SString path() {
-    return path;
+    return (SString) fields.get("path");
   }
 
   public Blob content() {
-    return content;
+    return (Blob) fields.get("content");
   }
 
   @Override
   public String toString() {
-    return "File(" + path + " " + content.toString() + ")";
+    return "File(" + path().toString() + " " + content().toString() + ")";
   }
 }
