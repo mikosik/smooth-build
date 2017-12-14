ae39a1ad195086a847263aff2d2ca07eaad172e7
Marcin Mikosik <marcin.mikosik@gmail.com>
2017-12-14 17:46:07 +0100

    refactor Signature to not extend TypedName

--- src/main/java/org/smoothbuild/lang/function/base/Signature.java
+++ src/main/java/org/smoothbuild/lang/function/base/Signature.java
@@ -1,5 +1,9 @@
 package org.smoothbuild.lang.function.base;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import java.util.Objects;
+
 import org.smoothbuild.lang.type.Type;
 
 import com.google.common.collect.ImmutableList;
@@ -7,12 +11,23 @@ import com.google.common.collect.ImmutableList;
 /**
  * Function's signature.
  */
-public class Signature extends TypedName {
+public class Signature {
+  private final Type type;
+  private final Name name;
   private final ImmutableList<Parameter> parameters;
 
   public Signature(Type type, Name name, Iterable<Parameter> params) {
-    super(type, name);
+    this.type = checkNotNull(type);
+    this.name = checkNotNull(name);
     this.parameters = ImmutableList.copyOf(params);
+  }
+
+  public Type type() {
+    return type;
+  }
+
+  public Name name() {
+    return name;
   }
 
   public ImmutableList<Parameter> parameters() {
@@ -20,9 +35,24 @@ public class Signature extends TypedName {
   }
 
   @Override
+  public boolean equals(Object object) {
+    if (!(object instanceof Signature)) {
+      return false;
+    }
+    Signature that = (Signature) object;
+    return type.equals(that.type)
+        && name.equals(that.name);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(type, name);
+  }
+
+  @Override
   public String toString() {
     StringBuilder builder = new StringBuilder();
-    builder.append(super.toString() + "(");
+    builder.append(type.toString() + " " + name.toString() + "(");
     int count = 0;
     for (Parameter parameter : parameters) {
       if (count != 0) {
