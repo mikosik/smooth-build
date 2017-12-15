67f1b61e0468479030d554c015c72bcda2200e9e
Marcin Mikosik <marcin.mikosik@gmail.com>
2017-12-15 14:51:12 +0100

    fix parameter types and names in Lists/Sets methods

--- src/main/java/org/smoothbuild/util/Sets.java
+++ src/main/java/org/smoothbuild/util/Sets.java
@@ -2,21 +2,20 @@ package org.smoothbuild.util;
 
 import static java.util.stream.Collectors.toSet;
 
-import java.util.Collection;
 import java.util.Set;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class Sets {
-  public static <T, R> Set<R> map(Collection<T> set, Function<? super T, ? extends R> function) {
+  public static <T, R> Set<R> map(Set<T> set, Function<? super T, ? extends R> function) {
     return set
         .stream()
         .map(function)
         .collect(toSet());
   }
 
-  public static <T> Set<T> filter(Set<T> list, Predicate<? super T> predicate) {
-    return list
+  public static <T> Set<T> filter(Set<T> set, Predicate<? super T> predicate) {
+    return set
         .stream()
         .filter(predicate)
         .collect(toSet());
