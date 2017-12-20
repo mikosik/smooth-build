f8190c38b4aac6f86cee6672f971722b1b596957
Marcin Mikosik <marcin.mikosik@gmail.com>
2017-12-20 13:07:01 +0100

    refactor Types.closesCommonConvertibleTo() to use Type.hierarchy()

--- src/main/java/org/smoothbuild/lang/type/Types.java
+++ src/main/java/org/smoothbuild/lang/type/Types.java
@@ -1,9 +1,9 @@
 package org.smoothbuild.lang.type;
 
+import static com.google.common.collect.Lists.reverse;
 import static org.smoothbuild.lang.type.ArrayType.arrayOf;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.List;
 
 import org.smoothbuild.lang.value.Blob;
 import org.smoothbuild.lang.value.Nothing;
@@ -26,9 +26,8 @@ public class Types {
   }
 
   /*
-   * Not each type can be used in every place. Each set below represent one place
-   * where smooth type can be used and contains all smooth types that can be used
-   * there.
+   * Not each type can be used in every place. Each set below represent one place where smooth type
+   * can be used and contains all smooth types that can be used there.
    */
 
   private static final ImmutableSet<Type> BASIC_TYPES = ImmutableSet.of(STRING, BLOB, FILE,
@@ -38,8 +37,8 @@ public class Types {
       arrayOf(STRING), arrayOf(BLOB), arrayOf(FILE), arrayOf(NOTHING));
 
   /**
-   * All smooth types available in smooth language. Returned list is sorted using
-   * type - subtype relationship. Each type comes before all of its subtypes.
+   * All smooth types available in smooth language. Returned list is sorted using type - subtype
+   * relationship. Each type comes before all of its subtypes.
    */
   public static ImmutableSet<Type> allTypes() {
     return ALL_TYPES;
@@ -56,49 +55,45 @@ public class Types {
 
   public static Type closestCommonConvertibleTo(Type type1, Type type2) {
     /*
-     * Algorithm below works correctly for all smooth types currently existing in
-     * smooth but doesn't work when it is possible to define user struct types. It
-     * will fail when conversion chain contains cycle (for example struct type is
-     * convertible to itself) or conversion chain has infinite length (for example
-     * structure X is convertible to its array [X])
+     * Algorithm below works correctly for all smooth types currently existing in smooth but doesn't
+     * work when it is possible to define user struct types. It will fail when conversion chain
+     * (hierarchy) contains cycle (for example struct type is convertible to itself) or conversion
+     * chain has infinite length (for example structure X is convertible to its array [X]). This
+     * comment will become obsolete once we get rid of conversion chains (and direct-convertible-to)
+     * and instead create normal object oriented type hierarchy with one root (Value type).
      */
-    Deque<Type> chain1 = conversionChain(type1);
-    Deque<Type> chain2 = conversionChain(type2);
-
-    Type type = null;
-    while (!chain1.isEmpty() && !chain2.isEmpty() && chain1.peekLast().equals(chain2.peekLast())) {
-      type = chain1.peekLast();
-      chain1.removeLast();
-      chain2.removeLast();
-    }
-
+    List<Type> hierarchy1 = type1.hierarchy();
+    List<Type> hierarchy2 = type2.hierarchy();
+    Type type = closesCommonSuperType(hierarchy1, hierarchy2);
     if (type == null) {
-      Type last1 = chain1.peekLast();
-      Type last2 = chain2.peekLast();
+      Type last1 = hierarchy1.get(0);
+      Type last2 = hierarchy2.get(0);
       boolean isNothing1 = last1.coreType().equals(NOTHING);
       boolean isNothing2 = last2.coreType().equals(NOTHING);
       if (isNothing1 && isNothing2) {
         type = last1.coreDepth() < last2.coreDepth() ? last2 : last1;
       } else if (isNothing1) {
-        type = firstWithDepthNotLowerThan(chain2, last1.coreDepth());
+        type = firstWithDepthNotLowerThan(hierarchy2, last1.coreDepth());
       } else if (isNothing2) {
-        type = firstWithDepthNotLowerThan(chain1, last2.coreDepth());
+        type = firstWithDepthNotLowerThan(hierarchy1, last2.coreDepth());
       }
     }
     return type;
   }
 
-  private static Deque<Type> conversionChain(Type type) {
-    Deque<Type> chain = new ArrayDeque<>();
-    while (type != null) {
-      chain.add(type);
-      type = type.directConvertibleTo();
+  private static Type closesCommonSuperType(List<Type> hierarchy1, List<Type> hierarchy2) {
+    int index = 0;
+    Type type = null;
+    while (index < hierarchy1.size() && index < hierarchy2.size()
+        && hierarchy1.get(index).equals(hierarchy2.get(index))) {
+      type = hierarchy1.get(index);
+      index++;
     }
-    return chain;
+    return type;
   }
 
-  private static Type firstWithDepthNotLowerThan(Deque<Type> chain, int depth) {
-    return chain
+  private static Type firstWithDepthNotLowerThan(List<Type> hierarchy, int depth) {
+    return reverse(hierarchy)
         .stream()
         .filter(t -> depth <= t.coreDepth())
         .findFirst()
