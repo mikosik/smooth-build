470f6a1748d32589d33a2da888129420a80531be
Marcin Mikosik <marcin.mikosik@gmail.com>
2017-12-26 18:34:09 +0100

    refactor SString and ValuesDb to use HashedDb.writeString()/readString()

--- src/main/java/org/smoothbuild/lang/value/SString.java
+++ src/main/java/org/smoothbuild/lang/value/SString.java
@@ -2,14 +2,8 @@ package org.smoothbuild.lang.value;
 
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
-import static org.smoothbuild.SmoothConstants.CHARSET;
-import static org.smoothbuild.util.Streams.inputStreamToString;
-
-import java.io.IOException;
 
 import org.smoothbuild.db.hashed.HashedDb;
-import org.smoothbuild.db.hashed.HashedDbException;
-import org.smoothbuild.db.hashed.Marshaller;
 import org.smoothbuild.lang.type.Type;
 
 import com.google.common.hash.HashCode;
@@ -23,19 +17,8 @@ public class SString extends Value {
     this.hashedDb = checkNotNull(hashedDb);
   }
 
-  public static SString storeStringInDb(Type type, String string, HashedDb hashedDb) {
-    Marshaller marshaller = hashedDb.newMarshaller();
-    marshaller.write(string.getBytes(CHARSET));
-    marshaller.close();
-    return new SString(type, marshaller.hash(), hashedDb);
-  }
-
   public String value() {
-    try {
-      return inputStreamToString(hashedDb.newUnmarshaller(hash()));
-    } catch (IOException e) {
-      throw new HashedDbException("IO error occurred while reading " + hash() + " value.");
-    }
+    return hashedDb.readString(hash());
   }
 
   @Override
