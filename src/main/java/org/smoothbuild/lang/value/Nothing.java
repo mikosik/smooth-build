19b03ca93817d574ac23ebbb9f046a2756d27717
Marcin Mikosik <marcin.mikosik@gmail.com>
2018-01-05 04:54:34 +0100

    Nothing overwrites all methods from Value and throws Exception

--- src/main/java/org/smoothbuild/lang/value/Nothing.java
+++ src/main/java/org/smoothbuild/lang/value/Nothing.java
@@ -5,7 +5,6 @@ import org.smoothbuild.lang.type.Type;
 import com.google.common.hash.HashCode;
 
 public final class Nothing extends Value {
-
   private Nothing() {
     super(null, null, null);
   }
@@ -16,7 +15,22 @@ public final class Nothing extends Value {
   }
 
   @Override
+  public HashCode dataHash() {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
   public Type type() {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public boolean equals(Object object) {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public int hashCode() {
     throw new UnsupportedOperationException();
   }
 }
