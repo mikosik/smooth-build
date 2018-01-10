5a77143f0736212f1258619e1e19e989c4f08387
Marcin Mikosik <marcin.mikosik@gmail.com>
2018-01-10 15:36:26 +0100

    got rid of convert.smooth and its native implementations

--- src/main/java/org/smoothbuild/SmoothPaths.java
+++ src/main/java/org/smoothbuild/SmoothPaths.java
@@ -6,16 +6,11 @@ import java.nio.file.Paths;
 public class SmoothPaths {
   private static final String LIB_DIR_NAME = "lib";
   private static final String FUNCS_MODULE = "funcs.smooth";
-  private static final String CONVERT_MODULE = "convert.smooth";
 
   private final Path home;
 
   public SmoothPaths(Path home) {
     this.home = home;
-  }
-
-  public Path convertModule() {
-    return libDir().resolve(CONVERT_MODULE);
   }
 
   public Path funcsModule() {
