61813b73e493c6d4d8bab4598e4dad7d1c271a41
Marcin Mikosik <marcin.mikosik@gmail.com>
2018-02-22 11:28:28 +0100

    replace hardcoded Message struct with one defined in funcs.smooth

--- src/main/java/org/smoothbuild/lang/message/MessagesDb.java
+++ src/main/java/org/smoothbuild/lang/message/MessagesDb.java
@@ -7,8 +7,9 @@ import javax.inject.Inject;
 
 import org.smoothbuild.db.hashed.HashedDb;
 import org.smoothbuild.db.values.ValuesDb;
+import org.smoothbuild.lang.plugin.Types;
+import org.smoothbuild.lang.runtime.RuntimeTypes;
 import org.smoothbuild.lang.type.StructType;
-import org.smoothbuild.lang.type.Type;
 import org.smoothbuild.lang.type.TypesDb;
 import org.smoothbuild.lang.value.Value;
 
@@ -18,31 +19,29 @@ public class MessagesDb {
   public static final String SEVERITY = "severity";
   public static final String TEXT = "text";
   private final ValuesDb valuesDb;
-  private final StructType messageType;
+  private final Types types;
 
   public MessagesDb() {
     this(new HashedDb());
   }
 
   public MessagesDb(HashedDb hashedDb) {
-    this(new ValuesDb(hashedDb), new TypesDb(hashedDb));
+    this(new ValuesDb(hashedDb), new RuntimeTypes(new TypesDb(hashedDb)));
+  }
+
+  public MessagesDb(ValuesDb valuesDb, RuntimeTypes types) {
+    this(valuesDb, (Types) types);
+    types.struct("Message", ImmutableMap.of(TEXT, types.string(), SEVERITY, types.string()));
   }
 
   @Inject
-  public MessagesDb(ValuesDb valuesDb, TypesDb typesDb) {
+  public MessagesDb(ValuesDb valuesDb, Types types) {
     this.valuesDb = valuesDb;
-    this.messageType = createMessageType(typesDb);
-  }
-
-  private StructType createMessageType(TypesDb typesDb) {
-    ImmutableMap<String, Type> fields = ImmutableMap.of(
-        TEXT, typesDb.string(),
-        SEVERITY, typesDb.string());
-    return typesDb.struct("Message", fields);
+    this.types = types;
   }
 
   public StructType messageType() {
-    return messageType;
+    return (StructType) types.getType("Message");
   }
 
   public Message error(String message) {
@@ -60,7 +59,7 @@ public class MessagesDb {
   private Message newMessage(String text, String severity) {
     Value textValue = valuesDb.string(text);
     Value severityValue = valuesDb.string(severity);
-    Value message = valuesDb.structBuilder(messageType)
+    Value message = valuesDb.structBuilder(messageType())
         .set(TEXT, textValue)
         .set(SEVERITY, severityValue)
         .build();
