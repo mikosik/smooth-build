d2e2d757d1d5614d2a600f06380ea1d38967345b
Marcin Mikosik <marcin.mikosik@gmail.com>
2018-02-23 16:21:10 +0100

    inline NativeFunction.invoke()

--- src/main/java/org/smoothbuild/lang/function/NativeFunction.java
+++ src/main/java/org/smoothbuild/lang/function/NativeFunction.java
@@ -1,19 +1,8 @@
 package org.smoothbuild.lang.function;
 
-import static com.google.common.base.Throwables.getStackTraceAsString;
-import static org.smoothbuild.lang.message.Messages.containsErrors;
-
-import java.lang.reflect.InvocationTargetException;
-import java.util.List;
-
 import org.smoothbuild.lang.expr.Expression;
 import org.smoothbuild.lang.expr.NativeCallExpression;
 import org.smoothbuild.lang.message.Location;
-import org.smoothbuild.lang.plugin.AbortException;
-import org.smoothbuild.lang.plugin.NativeApi;
-import org.smoothbuild.lang.value.Value;
-import org.smoothbuild.task.base.Output;
-import org.smoothbuild.task.exec.Container;
 
 import com.google.common.hash.HashCode;
 
@@ -35,6 +24,10 @@ public class NativeFunction extends Function {
     this.isCacheable = isCacheable;
   }
 
+  public Native nativ() {
+    return nativ;
+  }
+
   public HashCode hash() {
     return hash;
   }
@@ -46,50 +39,5 @@ public class NativeFunction extends Function {
   @Override
   public Expression createCallExpression(Location location) {
     return new NativeCallExpression(this, location);
-  }
-
-  public Output invoke(Container container, List<Value> arguments) {
-    try {
-      Value result = (Value) nativ.method().invoke(null, createArguments(container, arguments));
-      if (result == null) {
-        return nullOutput(container);
-      }
-      if (!type().equals(result.type())) {
-        container.log().error("Function " + name()
-            + " has faulty native implementation: Its result type is " + type().name()
-            + " but it returned value of type " + result.type().name() + ".");
-        return new Output(null, container.messages());
-      }
-      return new Output(result, container.messages());
-    } catch (IllegalAccessException e) {
-      throw new RuntimeException(e);
-    } catch (InvocationTargetException e) {
-      Throwable cause = e.getCause();
-      if (cause instanceof AbortException) {
-        return nullOutput(container);
-      } else {
-        container.log().error("Function " + name()
-            + " threw java exception from its native code:\n"
-            + getStackTraceAsString(cause));
-        return new Output(null, container.messages(), false);
-      }
-    }
-  }
-
-  private Output nullOutput(Container container) {
-    if (!containsErrors(container.messages())) {
-      container.log().error("Function " + name()
-          + " has faulty native implementation: it returned 'null' but logged no error.");
-    }
-    return new Output(null, container.messages());
-  }
-
-  private static Object[] createArguments(NativeApi nativeApi, List<Value> arguments) {
-    Object[] nativeArguments = new Object[1 + arguments.size()];
-    nativeArguments[0] = nativeApi;
-    for (int i = 0; i < arguments.size(); i++) {
-      nativeArguments[i + 1] = arguments.get(i);
-    }
-    return nativeArguments;
   }
 }
--- src/main/java/org/smoothbuild/task/base/NativeCallComputation.java
+++ src/main/java/org/smoothbuild/task/base/NativeCallComputation.java
@@ -1,9 +1,17 @@
 package org.smoothbuild.task.base;
 
+import static com.google.common.base.Throwables.getStackTraceAsString;
+import static org.smoothbuild.lang.message.Messages.containsErrors;
 import static org.smoothbuild.task.base.ComputationHashes.nativeCallComputationHash;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.List;
+
 import org.smoothbuild.lang.function.NativeFunction;
+import org.smoothbuild.lang.plugin.AbortException;
+import org.smoothbuild.lang.plugin.NativeApi;
 import org.smoothbuild.lang.type.Type;
+import org.smoothbuild.lang.value.Value;
 import org.smoothbuild.task.exec.Container;
 
 import com.google.common.hash.HashCode;
@@ -27,6 +35,49 @@ public class NativeCallComputation implements Computation {
 
   @Override
   public Output execute(Input input, Container container) {
-    return function.invoke(container, input.values());
+    try {
+      Value result = (Value) function.nativ().method()
+          .invoke(null, createArguments(container, input.values()));
+      if (result == null) {
+        return nullOutput(container);
+      }
+      if (!function.type().equals(result.type())) {
+        container.log().error("Function " + function.name()
+            + " has faulty native implementation: Its result type is " + function.type().name()
+            + " but it returned value of type " + result.type().name() + ".");
+        return new Output(null, container.messages());
+      }
+      return new Output(result, container.messages());
+    } catch (IllegalAccessException e) {
+      throw new RuntimeException(e);
+    } catch (InvocationTargetException e) {
+      Throwable cause = e.getCause();
+      if (cause instanceof AbortException) {
+        return nullOutput(container);
+      } else {
+        container.log().error("Function " + function.name()
+            + " threw java exception from its native code:\n"
+            + getStackTraceAsString(cause));
+        return new Output(null, container.messages(), false);
+      }
+    }
   }
+
+  private Output nullOutput(Container container) {
+    if (!containsErrors(container.messages())) {
+      container.log().error("Function " + function.name()
+          + " has faulty native implementation: it returned 'null' but logged no error.");
+    }
+    return new Output(null, container.messages());
+  }
+
+  private static Object[] createArguments(NativeApi nativeApi, List<Value> arguments) {
+    Object[] nativeArguments = new Object[1 + arguments.size()];
+    nativeArguments[0] = nativeApi;
+    for (int i = 0; i < arguments.size(); i++) {
+      nativeArguments[i + 1] = arguments.get(i);
+    }
+    return nativeArguments;
+  }
+
 }
