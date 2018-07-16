5cecd1d60fbddc26f2fd77a9c1eabf902590685c
Marcin Mikosik <marcin.mikosik@gmail.com>
2018-07-16 15:27:17 +0200

    refactored ModuleLoader, RuntimeLoader

--- src/main/java/org/smoothbuild/parse/RuntimeLoader.java
+++ src/main/java/org/smoothbuild/parse/RuntimeLoader.java
@@ -6,23 +6,26 @@ import java.util.List;
 import javax.inject.Inject;
 
 import org.smoothbuild.SmoothPaths;
+import org.smoothbuild.lang.runtime.SRuntime;
 
 public class RuntimeLoader {
+  private final SRuntime runtime;
   private final ModuleLoader moduleLoader;
   private final SmoothPaths paths;
 
   @Inject
-  public RuntimeLoader(ModuleLoader moduleLoader, SmoothPaths paths) {
+  public RuntimeLoader(SRuntime runtime, ModuleLoader moduleLoader, SmoothPaths paths) {
+    this.runtime = runtime;
     this.moduleLoader = moduleLoader;
     this.paths = paths;
   }
 
   public List<? extends Object> load() {
-    List<? extends Object> errors = moduleLoader.loadModule(paths.funcsModule());
+    List<? extends Object> errors = moduleLoader.loadModule(runtime, paths.funcsModule());
     if (!errors.isEmpty()) {
       return errors;
     }
-    errors = moduleLoader.loadModule(paths.defaultScript());
+    errors = moduleLoader.loadModule(runtime, paths.defaultScript());
     if (!errors.isEmpty()) {
       return errors;
     }
