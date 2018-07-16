0520ae0184941e1cac2d61a6c7466fe4d97c9417
Marcin Mikosik <marcin.mikosik@gmail.com>
2018-07-16 15:11:48 +0200

    fixed Version to use console instead of System.out

--- src/main/java/org/smoothbuild/cli/Version.java
+++ src/main/java/org/smoothbuild/cli/Version.java
@@ -2,12 +2,21 @@ package org.smoothbuild.cli;
 
 import static org.smoothbuild.SmoothConstants.EXIT_CODE_SUCCESS;
 
+import javax.inject.Inject;
+
 import org.smoothbuild.SmoothConstants;
 
 public class Version implements Command {
+  private final Console console;
+
+  @Inject
+  public Version(Console console) {
+    this.console = console;
+  }
+
   @Override
   public int run(String... names) {
-    System.out.println("smooth build version " + SmoothConstants.VERSION);
+    console.println("smooth build version " + SmoothConstants.VERSION);
     return EXIT_CODE_SUCCESS;
   }
 }
