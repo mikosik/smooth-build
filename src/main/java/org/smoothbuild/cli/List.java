bcbd768e3a3ab7c9320bf24454800b87a294cabb
Marcin Mikosik <marcin.mikosik@gmail.com>
2018-07-16 15:48:13 +0200

    redesigned RuntimeLoader into RuntimeController

--- src/main/java/org/smoothbuild/cli/List.java
+++ src/main/java/org/smoothbuild/cli/List.java
@@ -1,32 +1,25 @@
 package org.smoothbuild.cli;
 
-import static org.smoothbuild.SmoothConstants.EXIT_CODE_ERROR;
-import static org.smoothbuild.SmoothConstants.EXIT_CODE_SUCCESS;
-
 import javax.inject.Inject;
 
 import org.smoothbuild.SmoothPaths;
-import org.smoothbuild.lang.runtime.SRuntime;
-import org.smoothbuild.parse.RuntimeLoader;
+import org.smoothbuild.parse.RuntimeController;
 
 public class List implements Command {
-  private final SRuntime runtime;
   private final SmoothPaths paths;
   private final Console console;
-  private final RuntimeLoader runtimeLoader;
+  private final RuntimeController runtimeController;
 
   @Inject
-  public List(SRuntime runtime, SmoothPaths paths, Console console, RuntimeLoader runtimeLoader) {
-    this.runtime = runtime;
+  public List(SmoothPaths paths, Console console, RuntimeController runtimeController) {
     this.paths = paths;
     this.console = console;
-    this.runtimeLoader = runtimeLoader;
+    this.runtimeController = runtimeController;
   }
 
   @Override
   public int run(String... args) {
-    java.util.List<? extends Object> errors = runtimeLoader.load();
-    if (errors.isEmpty()) {
+    return runtimeController.setUpRuntimeAndRun(runtime -> {
       runtime
           .functions()
           .all()
@@ -35,11 +28,7 @@ public class List implements Command {
           .filter(f -> f.parameters().size() == 0)
           .map(t -> t.name().toString())
           .sorted()
-          .forEach(n -> System.out.println(n));
-    } else {
-      console.rawErrors(errors);
-    }
-    console.printFinalSummary();
-    return console.isErrorReported() ? EXIT_CODE_ERROR : EXIT_CODE_SUCCESS;
+          .forEach(n -> console.println(n));
+    });
   }
 }
