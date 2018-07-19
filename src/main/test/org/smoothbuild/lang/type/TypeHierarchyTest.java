8ce6ea27c8a7431cbaea16d681e8d16d5ed33cd8
Marcin Mikosik <marcin.mikosik@gmail.com>
2018-07-19 15:33:37 +0200

    refactored TypeHierarchyTest, ParametersPoolTest to use TestingTypes

--- src/main/test/org/smoothbuild/lang/type/TypeHierarchyTest.java
+++ src/main/test/org/smoothbuild/lang/type/TypeHierarchyTest.java
@@ -1,6 +1,7 @@
 package org.smoothbuild.lang.type;
 
 import static com.google.common.collect.Collections2.permutations;
+import static org.smoothbuild.lang.type.TestingTypes.typesDb;
 import static org.smoothbuild.lang.type.TypeHierarchy.sortedTypes;
 import static org.smoothbuild.util.Lists.list;
 import static org.testory.Testory.thenReturned;
@@ -11,10 +12,6 @@ import java.util.List;
 import org.junit.Test;
 
 public class TypeHierarchyTest {
-  private final TypesDb typesDb = new TestingTypesDb();
-  private final Type string = typesDb.string();
-  private final Type nothing = typesDb.nothing();
-
   @Test
   public void sorted_types_for_empty_hierarchy() throws Exception {
     assertSortedOrder(list());
@@ -22,50 +19,52 @@ public class TypeHierarchyTest {
 
   @Test
   public void sorted_types_with_one_type() throws Exception {
-    assertSortedOrder(list(string));
+    assertSortedOrder(list(TestingTypes.string));
   }
 
   @Test
   public void sorted_types_for_two_related_types() throws Exception {
-    assertSortedOrder(list(string, personType()));
+    assertSortedOrder(list(TestingTypes.string, personType()));
   }
 
   @Test
   public void sorted_types_for_two_related_array_types() throws Exception {
-    assertSortedOrder(list(typesDb.array(string), typesDb.array(personType())));
+    assertSortedOrder(list(typesDb.array(TestingTypes.string), typesDb.array(personType())));
   }
 
   @Test
   public void sorted_types_for_two_related_array_of_array_types() throws Exception {
-    assertSortedOrder(list(typesDb.array(typesDb.array(string)), typesDb.array(
+    assertSortedOrder(list(typesDb.array(typesDb.array(TestingTypes.string)), typesDb.array(
         typesDb.array(personType()))));
   }
 
   @Test
   public void sorted_nothings() throws Exception {
-    assertSortedOrder(list(typesDb.array(typesDb.array(nothing)), typesDb.array(
-        nothing), nothing));
+    assertSortedOrder(list(typesDb.array(typesDb.array(TestingTypes.nothing)), typesDb.array(
+        TestingTypes.nothing), TestingTypes.nothing));
   }
 
   @Test
   public void sorted_nothings_without_middle_one() throws Exception {
-    assertSortedOrder(list(typesDb.array(typesDb.array(nothing)), nothing));
+    assertSortedOrder(list(typesDb.array(typesDb.array(TestingTypes.nothing)),
+        TestingTypes.nothing));
   }
 
   @Test
   public void sorted_types_comes_before_nothing() throws Exception {
-    assertSortedOrder(list(string, nothing));
+    assertSortedOrder(list(TestingTypes.string, TestingTypes.nothing));
   }
 
   @Test
   public void sorted_array_types_comes_before_array_of_nothing() throws Exception {
-    assertSortedOrder(list(typesDb.array(string), typesDb.array(nothing)));
+    assertSortedOrder(list(typesDb.array(TestingTypes.string), typesDb.array(
+        TestingTypes.nothing)));
   }
 
   @Test
   public void sorted_types_does_not_return_duplicated_types() throws Exception {
-    when(() -> sortedTypes(list(string, string, string)));
-    thenReturned(list(string));
+    when(() -> sortedTypes(list(TestingTypes.string, TestingTypes.string, TestingTypes.string)));
+    thenReturned(list(TestingTypes.string));
   }
 
   private static void assertSortedOrder(List<Type> expected) {
