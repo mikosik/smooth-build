e431958c5e232d626aff00eba08883472ea34deb
Marcin Mikosik <marcin.mikosik@gmail.com>
2018-07-26 13:55:16 +0200

    added TypeChooser.actualCallType()

--- src/main/java/org/smoothbuild/lang/expr/TypeChooser.java
+++ src/main/java/org/smoothbuild/lang/expr/TypeChooser.java
@@ -1,7 +1,10 @@
 package org.smoothbuild.lang.expr;
 
+import java.util.List;
 import java.util.function.IntFunction;
 
+import org.smoothbuild.lang.base.ParameterInfo;
+import org.smoothbuild.lang.base.Signature;
 import org.smoothbuild.lang.type.ConcreteType;
 import org.smoothbuild.lang.type.Type;
 
@@ -10,11 +13,38 @@ public interface TypeChooser<T extends Type> {
 
   public abstract T choose(IntFunction<T> childrenType);
 
-  public static TypeChooser<ConcreteType> fixedTypeChooser(ConcreteType type) {
-    return (IntFunction<ConcreteType> childrenType) -> type;
+  public static <T extends Type> TypeChooser<T> fixedTypeChooser(T type) {
+    return (IntFunction<T> childrenType) -> type;
   }
 
   public static TypeChooser<ConcreteType> arrayOfFirstChildType() {
     return childrenType -> childrenType.apply(0).increaseCoreDepthBy(1);
+  }
+
+  public static TypeChooser<ConcreteType> callEvaluatorType(Signature signature) {
+    return (arguments) -> inferCallType(signature.type(), signature.parameters(), arguments);
+  }
+
+  public static <T extends Type> T inferCallType(Type resultType,
+      List<? extends ParameterInfo> parameters, IntFunction<T> arguments) {
+    if (resultType.isGeneric()) {
+      int matchingParameter =
+          indexOfParameterWithMatchingCoreType(resultType.coreType(), parameters);
+      Type actualParameterCoreType = arguments.apply(matchingParameter).decreaseCoreDepthBy(
+          parameters.get(matchingParameter).type().coreDepth());
+      return (T) resultType.replaceCoreType(actualParameterCoreType);
+    } else {
+      return (T) resultType;
+    }
+  }
+
+  static int indexOfParameterWithMatchingCoreType(Type coreType,
+      List<? extends ParameterInfo> parameters) {
+    for (int i = 0; i < parameters.size(); i++) {
+      if (parameters.get(i).type().coreType().equals(coreType)) {
+        return i;
+      }
+    }
+    throw new IllegalArgumentException("Cannot find parameter with matching type.");
   }
 }
---
+++ src/main/test/org/smoothbuild/lang/expr/TypeChooserTest.java
@@ -0,0 +1,63 @@
+package org.smoothbuild.lang.expr;
+
+import static org.smoothbuild.lang.expr.TypeChooser.inferCallType;
+import static org.smoothbuild.lang.type.TestingTypes.a;
+import static org.smoothbuild.lang.type.TestingTypes.arrayA;
+import static org.smoothbuild.lang.type.TestingTypes.arrayString;
+import static org.smoothbuild.lang.type.TestingTypes.blob;
+import static org.smoothbuild.lang.type.TestingTypes.string;
+import static org.smoothbuild.util.Lists.list;
+import static org.testory.Testory.thenReturned;
+import static org.testory.Testory.when;
+
+import java.util.function.IntFunction;
+
+import org.junit.Test;
+import org.smoothbuild.lang.base.ParameterInfo;
+import org.smoothbuild.lang.type.Type;
+
+public class TypeChooserTest {
+  @Test
+  public void concrete_result_type_is_returned_as_inferred() throws Exception {
+    when(inferCallType(string, list(), arguments(-1, null)));
+    thenReturned(string);
+  }
+
+  @Test
+  public void generic_result_type_is_infered_from_generic_parameter() throws Exception {
+    when(inferCallType(a, list(param(a), param(blob)), arguments(0, string)));
+    thenReturned(string);
+  }
+
+  @Test
+  public void generic_result_type_is_infered_from_generic_array_parameter() throws Exception {
+    when(inferCallType(a, list(param(arrayA), param(blob)), arguments(0, arrayString)));
+    thenReturned(string);
+  }
+
+  @Test
+  public void generic_array_result_type_is_infered_from_generic_parameter() throws Exception {
+    when(inferCallType(arrayA, list(param(a), param(blob)), arguments(0, string)));
+    thenReturned(arrayString);
+  }
+
+  @Test
+  public void generic_array_result_type_is_infered_from_generic_array_parameter() throws Exception {
+    when(inferCallType(arrayA, list(param(arrayA), param(blob)), arguments(0, arrayString)));
+    thenReturned(arrayString);
+  }
+
+  private ParameterInfo param(Type type) {
+    return new ParameterInfo(type, "name", true);
+  }
+
+  private IntFunction<Type> arguments(int assignedArg, Type type) {
+    return (index) -> {
+      if (index == assignedArg) {
+        return type;
+      } else {
+        throw new IllegalArgumentException("unexpected argument index");
+      }
+    };
+  }
+}
