c5d241a14bd7abb05d5c35a443e6ac8396b424e2
Marcin Mikosik <marcin.mikosik@gmail.com>
2018-08-20 11:20:50 +0200

    added GenericTest wit some corner cases tests

---
+++ src/acceptance/org/smoothbuild/acceptance/lang/nativ/Flatten.java
@@ -0,0 +1,21 @@
+package org.smoothbuild.acceptance.lang.nativ;
+
+import org.smoothbuild.lang.plugin.NativeApi;
+import org.smoothbuild.lang.plugin.SmoothFunction;
+import org.smoothbuild.lang.type.ConcreteArrayType;
+import org.smoothbuild.lang.type.ConcreteType;
+import org.smoothbuild.lang.value.Array;
+import org.smoothbuild.lang.value.ArrayBuilder;
+import org.smoothbuild.lang.value.Value;
+
+public class Flatten {
+  @SmoothFunction
+  public static Array testFlatten(NativeApi nativeApi, Array array) {
+    ConcreteType resultArrayElemType = ((ConcreteArrayType) array.type().elemType()).elemType();
+    ArrayBuilder builder = nativeApi.create().arrayBuilder(resultArrayElemType);
+    for (Array innerArray : array.asIterable(Array.class)) {
+      builder.addAll(innerArray.asIterable(Value.class));
+    }
+    return builder.build();
+  }
+}
