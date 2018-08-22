4dc8ec09041b66f7be30be27a57a168b9a3aee7a
Marcin Mikosik <marcin.mikosik@gmail.com>
2018-08-22 13:45:59 +0200

    refactored Marshaller (and friends) to use Sinks instead of OutputStreams

--- src/main/java/org/smoothbuild/db/hashed/Marshaller.java
+++ src/main/java/org/smoothbuild/db/hashed/Marshaller.java
@@ -1,16 +1,24 @@
 package org.smoothbuild.db.hashed;
 
+import static okio.Okio.buffer;
+
+import java.io.IOException;
 import java.io.OutputStream;
 import java.util.function.Supplier;
 
 import com.google.common.hash.HashCode;
 
+import okio.BufferedSink;
+import okio.Sink;
+
 public class Marshaller extends OutputStream {
-  private final StoringOutputStream outputStream;
+  private final BufferedSink bufferedSink;
+  private final OutputStream outputStream;
   private final Supplier<HashCode> hashSupplier;
 
-  public Marshaller(StoringOutputStream outputStream, Supplier<HashCode> hashSupplier) {
-    this.outputStream = outputStream;
+  public Marshaller(Sink sink, Supplier<HashCode> hashSupplier) {
+    this.bufferedSink = buffer(sink);
+    this.outputStream = bufferedSink.outputStream();
     this.hashSupplier = hashSupplier;
   }
 
@@ -21,25 +29,45 @@ public class Marshaller extends OutputStream {
 
   @Override
   public void write(int b) {
-    outputStream.write(b);
+    try {
+      outputStream.write(b);
+    } catch (IOException e) {
+      rethrowAsHashedDbException(e);
+    }
   }
 
   @Override
   public void write(byte b[]) {
-    outputStream.write(b);
+    try {
+      outputStream.write(b);
+    } catch (IOException e) {
+      rethrowAsHashedDbException(e);
+    }
   }
 
   @Override
   public void write(byte bytes[], int off, int len) {
-    outputStream.write(bytes, off, len);
+    try {
+      outputStream.write(bytes, off, len);
+    } catch (IOException e) {
+      rethrowAsHashedDbException(e);
+    }
   }
 
   @Override
   public void close() {
-    outputStream.close();
+    try {
+      outputStream.close();
+    } catch (IOException e) {
+      rethrowAsHashedDbException(e);
+    }
   }
 
   public HashCode hash() {
     return hashSupplier.get();
+  }
+
+  private void rethrowAsHashedDbException(Throwable e) {
+    throw new HashedDbException("IO error occurred while writing object.", e);
   }
 }
