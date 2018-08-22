b259e9af925278c639ab5f389e25c8ad4cb50454
Marcin Mikosik <marcin.mikosik@gmail.com>
2018-08-22 13:05:53 +0200

    refactored Marshaller into chain of InputStreams

---
+++ src/main/java/org/smoothbuild/db/hashed/FileStorer.java
@@ -0,0 +1,33 @@
+package org.smoothbuild.db.hashed;
+
+import static org.smoothbuild.db.hashed.Hash.toPath;
+import static org.smoothbuild.io.fs.base.PathState.NOTHING;
+
+import java.util.function.Supplier;
+
+import org.smoothbuild.io.fs.base.FileSystem;
+import org.smoothbuild.io.fs.base.Path;
+
+import com.google.common.hash.HashCode;
+
+public class FileStorer {
+  private final FileSystem fileSystem;
+  private final Path hashedDbRoot;
+  private final Path sourceFile;
+  private final Supplier<HashCode> hashSupplier;
+
+  public FileStorer(FileSystem fileSystem, Path hashedDbRoot, Path sourceFile,
+      Supplier<HashCode> hashSupplier) {
+    this.fileSystem = fileSystem;
+    this.hashedDbRoot = hashedDbRoot;
+    this.sourceFile = sourceFile;
+    this.hashSupplier = hashSupplier;
+  }
+
+  public void store() {
+    Path path = hashedDbRoot.append(toPath(hashSupplier.get()));
+    if (fileSystem.pathState(path) == NOTHING) {
+      fileSystem.move(sourceFile, path);
+    }
+  }
+}
--- src/main/java/org/smoothbuild/db/hashed/Marshaller.java
+++ src/main/java/org/smoothbuild/db/hashed/Marshaller.java
@@ -1,32 +1,17 @@
 package org.smoothbuild.db.hashed;
 
-import static org.smoothbuild.db.hashed.Hash.toPath;
-import static org.smoothbuild.io.fs.base.PathState.NOTHING;
-
-import java.io.IOException;
 import java.io.OutputStream;
-
-import org.smoothbuild.io.fs.base.FileSystem;
-import org.smoothbuild.io.fs.base.Path;
+import java.util.function.Supplier;
 
 import com.google.common.hash.HashCode;
-import com.google.common.hash.Hasher;
 
 public class Marshaller extends OutputStream {
-  private final FileSystem fileSystem;
-  private final Path rootPath;
-  private final Path tempPath;
-  private HashCode hash;
-  private final OutputStream outputStream;
-  private final Hasher hasher;
+  private final StoringOutputStream outputStream;
+  private final Supplier<HashCode> hashSupplier;
 
-  public Marshaller(FileSystem fileSystem, Path rootPath, Path tempPath, HashCode hash) {
-    this.fileSystem = fileSystem;
-    this.rootPath = rootPath;
-    this.tempPath = tempPath;
-    this.hash = hash;
-    this.hasher = Hash.newHasher();
-    this.outputStream = fileSystem.sink(tempPath).outputStream();
+  public Marshaller(StoringOutputStream outputStream, Supplier<HashCode> hashSupplier) {
+    this.outputStream = outputStream;
+    this.hashSupplier = hashSupplier;
   }
 
   public Marshaller writeHash(HashCode hash) {
@@ -36,45 +21,25 @@ public class Marshaller extends OutputStream {
 
   @Override
   public void write(int b) {
-    write(new byte[] { (byte) b });
+    outputStream.write(b);
   }
 
   @Override
   public void write(byte b[]) {
-    write(b, 0, b.length);
+    outputStream.write(b);
   }
 
   @Override
   public void write(byte bytes[], int off, int len) {
-    hasher.putBytes(bytes, off, len);
-    try {
-      outputStream.write(bytes, off, len);
-    } catch (IOException e) {
-      throw newHashedDbException(hash, e);
-    }
+    outputStream.write(bytes, off, len);
   }
 
   @Override
   public void close() {
-    if (hash == null) {
-      hash = hasher.hash();
-    }
-    try {
-      outputStream.close();
-    } catch (IOException e) {
-      newHashedDbException(hash, e);
-    }
-    Path path = rootPath.append(toPath(hash));
-    if (fileSystem.pathState(path) == NOTHING) {
-      fileSystem.move(tempPath, path);
-    }
+    outputStream.close();
   }
 
   public HashCode hash() {
-    return hash;
-  }
-
-  private static HashedDbException newHashedDbException(HashCode hash, Throwable e) {
-    return new HashedDbException("IO error occurred while writing " + hash + " object.", e);
+    return hashSupplier.get();
   }
 }
