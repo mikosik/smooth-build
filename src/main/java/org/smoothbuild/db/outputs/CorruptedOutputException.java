aa75c962268126a5990f88953c0c28c09169ca92
Marcin Mikosik <marcin.mikosik@gmail.com>
2018-08-24 10:18:59 +0200

    merged all OutputsDb related exceptions into OutputsDbException

--- src/main/java/org/smoothbuild/db/outputs/CorruptedOutputException.java
+++
@@ -1,9 +0,0 @@
-package org.smoothbuild.db.outputs;
-
-import com.google.common.hash.HashCode;
-
-public class CorruptedOutputException extends RuntimeException {
-  public CorruptedOutputException(HashCode hash, String message) {
-    super(hash.toString() + " value in OutputsDb is corrupted. " + message);
-  }
-}
