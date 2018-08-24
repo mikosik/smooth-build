f9f72a3947a92483b9e3eb87ab7a75858e880b32
Marcin Mikosik <marcin.mikosik@gmail.com>
2018-08-24 11:25:32 +0200

    merged all ValuesDb related exceptions into ValuesDbException

--- src/main/java/org/smoothbuild/db/values/CorruptedHashSequenceValueException.java
+++
@@ -1,9 +0,0 @@
-package org.smoothbuild.db.values;
-
-import com.google.common.hash.HashCode;
-
-public class CorruptedHashSequenceValueException extends CorruptedValueException {
-  public CorruptedHashSequenceValueException(HashCode hash) {
-    super(hash, "Expected sequence of hashes but number of bytes is not multiple of hash size.");
-  }
-}
---
+++ src/main/java/org/smoothbuild/db/values/ValuesDbException.java
@@ -0,0 +1,18 @@
+package org.smoothbuild.db.values;
+
+import com.google.common.hash.HashCode;
+
+public class ValuesDbException extends RuntimeException {
+  public static ValuesDbException corruptedHashSequenceException(HashCode hash) {
+    return corruptedValueException(hash,
+        "Expected sequence of hashes but number of bytes is not multiple of hash size.");
+  }
+
+  public static ValuesDbException corruptedValueException(HashCode hash, String message) {
+    return new ValuesDbException(hash.toString() + "value in ValuesDb is corrupted. " + message);
+  }
+
+  public ValuesDbException(String message) {
+    super(message);
+  }
+}
