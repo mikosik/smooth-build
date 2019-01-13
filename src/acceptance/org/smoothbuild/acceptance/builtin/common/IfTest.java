4d4c034fdc27933fc4f7d1a583ef6c1e234d9473
Marcin Mikosik <marcin.mikosik@gmail.com>
2019-01-13 11:58:17 +0100

    implemented short-circiut in 'if' function (#106)

--- src/acceptance/org/smoothbuild/acceptance/builtin/common/IfTest.java
+++ src/acceptance/org/smoothbuild/acceptance/builtin/common/IfTest.java
@@ -1,13 +1,11 @@
 package org.smoothbuild.acceptance.builtin.common;
 
 import static org.hamcrest.Matchers.equalTo;
-import static org.smoothbuild.testing.db.values.ValueCreators.falseByteString;
-import static org.smoothbuild.testing.db.values.ValueCreators.trueByteString;
-import static org.smoothbuild.util.Lists.list;
 import static org.testory.Testory.then;
 
 import org.junit.Test;
 import org.smoothbuild.acceptance.AcceptanceTestCase;
+import org.smoothbuild.acceptance.lang.nativ.ThrowException;
 
 public class IfTest extends AcceptanceTestCase {
   @Test
@@ -24,5 +22,25 @@ public class IfTest extends AcceptanceTestCase {
     whenSmoothBuild("result");
     thenFinishedWithSuccess();
     then(artifactContent("result"), equalTo("else clause"));
+  }
+
+  @Test
+  public void first_value_should_not_be_evaluated_when_condition_is_false() throws Exception {
+    givenNativeJar(ThrowException.class);
+    givenScript("String throwException();" +
+        "        result = if(false(), throwException(), 'else clause');");
+    whenSmoothBuild("result");
+    thenFinishedWithSuccess();
+    then(artifactContent("result"), equalTo("else clause"));
+  }
+
+  @Test
+  public void second_value_should_not_be_evaluated_when_condition_is_true() throws Exception {
+    givenNativeJar(ThrowException.class);
+    givenScript("String throwException();" +
+        "        result = if(true(), 'then clause', throwException());");
+    whenSmoothBuild("result");
+    thenFinishedWithSuccess();
+    then(artifactContent("result"), equalTo("then clause"));
   }
 }
