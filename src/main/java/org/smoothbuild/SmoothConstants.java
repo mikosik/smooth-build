78227f5a644a40580f549cbbb8c12fc066369e10
Marcin Mikosik <marcin.mikosik@gmail.com>
2019-03-01 20:15:40 +0100

    refactored SmoothConstants

--- src/main/java/org/smoothbuild/SmoothConstants.java
+++ src/main/java/org/smoothbuild/SmoothConstants.java
@@ -3,10 +3,9 @@ package org.smoothbuild;
 import static org.smoothbuild.io.fs.base.Path.path;
 
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 import org.smoothbuild.io.fs.base.Path;
-
-import com.google.common.base.Charsets;
 
 public class SmoothConstants {
   public static final String VERSION = "0.17.0";
@@ -15,7 +14,7 @@ public class SmoothConstants {
   public static final int EXIT_CODE_ERROR = 2;
   public static final String SMOOTH_HOME_ENV_VARIABLE = "SMOOTH_HOME";
 
-  public static final Charset CHARSET = Charsets.UTF_8;
+  public static final Charset CHARSET = StandardCharsets.UTF_8;
 
   public static final Path SMOOTH_DIR = path(".smooth");
   public static final Path VALUES_DIR = path("values");
