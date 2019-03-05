a5bc2900e25346c0b38ea31edb8662309d24e295
Marcin Mikosik <marcin.mikosik@gmail.com>
2019-03-05 17:31:23 +0100

    changed FileSystemModule so FileSystem is @Singleton and is synchronized

--- src/main/java/org/smoothbuild/io/fs/FileSystemModule.java
+++ src/main/java/org/smoothbuild/io/fs/FileSystemModule.java
@@ -2,17 +2,20 @@ package org.smoothbuild.io.fs;
 
 import org.smoothbuild.io.fs.base.FileSystem;
 import org.smoothbuild.io.fs.base.Path;
+import org.smoothbuild.io.fs.base.SynchronizedFileSystem;
 import org.smoothbuild.io.fs.disk.DiskFileSystem;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
+import com.google.inject.Singleton;
 
 public class FileSystemModule extends AbstractModule {
   @Override
   protected void configure() {}
 
   @Provides
+  @Singleton
   public FileSystem provideFileSystem() {
-    return new DiskFileSystem(Path.root());
+    return new SynchronizedFileSystem(new DiskFileSystem(Path.root()));
   }
 }
