43e9ed7201519c4234cc9937e67011011066ec95
Marcin Mikosik <marcin.mikosik@gmail.com>
2019-03-05 17:18:19 +0100

    added SynchronizedFileSystem

---
+++ src/main/java/org/smoothbuild/io/fs/base/SynchronizedFileSystem.java
@@ -0,0 +1,54 @@
+package org.smoothbuild.io.fs.base;
+
+import java.io.IOException;
+
+import okio.BufferedSink;
+import okio.BufferedSource;
+
+public class SynchronizedFileSystem implements FileSystem {
+  private final FileSystem fileSystem;
+
+  public SynchronizedFileSystem(FileSystem fileSystem) {
+    this.fileSystem = fileSystem;
+  }
+
+  @Override
+  public synchronized PathState pathState(Path path) {
+    return fileSystem.pathState(path);
+  }
+
+  @Override
+  public synchronized Iterable<Path> files(Path dir) throws IOException {
+    return fileSystem.files(dir);
+  }
+
+  @Override
+  public synchronized void move(Path source, Path target) throws IOException {
+    fileSystem.move(source, target);
+  }
+
+  @Override
+  public synchronized void delete(Path path) throws IOException {
+    fileSystem.delete(path);
+  }
+
+  @Override
+  public synchronized BufferedSource source(Path path) throws IOException {
+    return fileSystem.source(path);
+  }
+
+  @Override
+  public synchronized BufferedSink sink(Path path) throws IOException {
+    return fileSystem.sink(path);
+  }
+
+  @Override
+  public synchronized void createLink(Path link, Path target) throws IOException {
+    fileSystem.createLink(link, target);
+  }
+
+  @Override
+  public synchronized void createDir(Path path) throws IOException {
+    fileSystem.createDir(path);
+  }
+}
