08310a81bd7247f0c81d007ec628345f64ec82c7
Marcin Mikosik <marcin.mikosik@gmail.com>
2019-03-19 20:55:42 +0100

    refactored Okios to make use of shorter (java11) try-with-resources

--- src/main/java/org/smoothbuild/util/Okios.java
+++ src/main/java/org/smoothbuild/util/Okios.java
@@ -7,13 +7,13 @@ import okio.Sink;
 
 public class Okios {
   public static void copyAllAndClose(BufferedSource source, Sink sink) throws IOException {
-    try (BufferedSource toClose1 = source; Sink toClose2 = sink) {
+    try (source; sink) {
       source.readAll(sink);
     }
   }
 
   public static <T> T readAndClose(BufferedSource source, DataReader<T> reader) throws IOException {
-    try (BufferedSource toClose = source) {
+    try (source) {
       return reader.readFrom(source);
     }
   }
