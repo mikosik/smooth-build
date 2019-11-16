50dbfaed9545bf5534d9929625d0081106cee714
Marcin Mikosik <marcin.mikosik@gmail.com>
2019-11-16 19:25:45 +0100

    added CorruptedNothingTest

---
+++ src/main/test/org/smoothbuild/lang/object/corrupt/CorruptedNothingTest.java
@@ -0,0 +1,26 @@
+package org.smoothbuild.lang.object.corrupt;
+
+import static org.smoothbuild.testing.common.ExceptionMatcher.exception;
+import static org.testory.Testory.given;
+import static org.testory.Testory.thenThrown;
+import static org.testory.Testory.when;
+
+import org.junit.Test;
+import org.smoothbuild.db.hashed.Hash;
+import org.smoothbuild.lang.object.base.SString;
+import org.smoothbuild.lang.object.db.ObjectsDbException;
+
+public class CorruptedNothingTest extends AbstractCorruptedTestCase {
+  private Hash instanceHash;
+
+  @Test
+  public void learning_test_create_string() throws Exception {
+    given(instanceHash =
+        hash(
+            hash(nothingType()),
+            hash("aaa")));
+    when(() -> ((SString) objectsDb().get(instanceHash)).data());
+    thenThrown(exception(new ObjectsDbException(instanceHash,
+        "Object type is Nothing so such object cannot exist.")));
+  }
+}
