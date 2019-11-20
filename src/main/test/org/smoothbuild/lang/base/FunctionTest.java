55b157eca922fe9aca77de0668646cbaf9249965
Marcin Mikosik <marcin.mikosik@gmail.com>
2019-11-20 13:05:40 +0100

    added Function.canBeCalledArgless()

--- src/main/test/org/smoothbuild/lang/base/FunctionTest.java
+++ src/main/test/org/smoothbuild/lang/base/FunctionTest.java
@@ -2,6 +2,7 @@ package org.smoothbuild.lang.base;
 
 import static org.smoothbuild.util.Lists.list;
 import static org.testory.Testory.given;
+import static org.testory.Testory.mock;
 import static org.testory.Testory.thenReturned;
 import static org.testory.Testory.when;
 
@@ -21,13 +22,13 @@ public class FunctionTest extends TestingContext {
 
   @Test(expected = NullPointerException.class)
   public void nullSignatureIsForbidden() throws Exception {
-    new MyAbstractFunction(null);
+    new MyFunction(null);
   }
 
   @Test
   public void type_returns_signature_type() {
     given(signature = new Signature(STRING, "name", list()));
-    given(function = new MyAbstractFunction(signature));
+    given(function = new MyFunction(signature));
     when(function).type();
     thenReturned(STRING);
   }
@@ -36,7 +37,7 @@ public class FunctionTest extends TestingContext {
   public void name_returns_signature_name() {
     given(name = "name");
     given(signature = new Signature(STRING, name, list()));
-    given(function = new MyAbstractFunction(signature));
+    given(function = new MyFunction(signature));
     when(function).type();
     thenReturned(STRING);
   }
@@ -45,13 +46,48 @@ public class FunctionTest extends TestingContext {
   public void params_returns_signature_params() {
     given(parameters = list(new Parameter(0, STRING, "name", null)));
     given(signature = new Signature(STRING, "name", parameters));
-    given(function = new MyAbstractFunction(signature));
+    given(function = new MyFunction(signature));
     when(function).parameters();
     thenReturned(parameters);
   }
 
-  public static class MyAbstractFunction extends Function {
-    public MyAbstractFunction(Signature signature) {
+  @Test
+  public void function_without_params_can_be_called_without_args() {
+    given(parameters = list());
+    given(signature = new Signature(STRING, "name", parameters));
+    given(function = new MyFunction(signature));
+    when(() -> function.canBeCalledArgless());
+    thenReturned(true);
+  }
+
+  @Test
+  public void function_with_all_params_with_default_values_can_be_called_without_args() {
+    given(parameters = list(paramWithDefault(), paramWithDefault()));
+    given(signature = new Signature(STRING, "name", parameters));
+    given(function = new MyFunction(signature));
+    when(() -> function.canBeCalledArgless());
+    thenReturned(true);
+  }
+
+  @Test
+  public void function_with_one_param_without_default_values_cannot_be_called_without_args() {
+    given(parameters = list(paramWithDefault(), paramWithoutDefault()));
+    given(signature = new Signature(STRING, "name", parameters));
+    given(function = new MyFunction(signature));
+    when(() -> function.canBeCalledArgless());
+    thenReturned(false);
+  }
+
+  private Parameter paramWithDefault() {
+    return new Parameter(0, stringType(), "a", mock(Expression.class));
+  }
+
+  private Parameter paramWithoutDefault() {
+    return new Parameter(0, stringType(), "a", null);
+  }
+
+  public static class MyFunction extends Function {
+    public MyFunction(Signature signature) {
       super(signature, Location.unknownLocation());
     }
 
