ac0a166fc378eac743ecf1cc8a92e2ca89c6378e
Marcin Mikosik <marcin.mikosik@gmail.com>
2020-01-31 15:25:52 +0100

    added Collections.toMap() that allows null values

--- src/main/test/org/smoothbuild/util/CollectionsTest.java
+++ src/main/test/org/smoothbuild/util/CollectionsTest.java
@@ -6,25 +6,54 @@ import static org.testory.Testory.thenReturned;
 import static org.testory.Testory.when;
 
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.junit.Test;
-
-import com.google.common.collect.ImmutableMap;
 
 public class CollectionsTest {
   private Collection<String> collection;
 
   @Test
-  public void to_map() throws Exception {
+  public void to_map_with_key_function() {
     given(collection = list("abc", "defg", "hijkl"));
     when(() -> Collections.toMap(collection, String::length));
-    thenReturned(ImmutableMap.of(3, "abc", 4, "defg", 5, "hijkl"));
+    thenReturned(Map.of(3, "abc", 4, "defg", 5, "hijkl"));
   }
 
   @Test
-  public void empty_collection_to_map() throws Exception {
+  public void empty_collection_to_map_with_key_function() {
     given(collection = list());
     when(() -> Collections.toMap(collection, String::length));
-    thenReturned(ImmutableMap.of());
+    thenReturned(Map.of());
+  }
+
+  @Test
+  public void to_map_with_key_and_value_function() {
+    given(collection = list("abc", "defg", "hijkl"));
+    when(() -> Collections.toMap(collection, String::toUpperCase, String::length));
+    thenReturned(Map.of("ABC", 3, "DEFG", 4, "HIJKL", 5));
+  }
+
+  @Test
+  public void to_map_with_key_and_value_function_allows_null_values() {
+    given(collection = list("abc", "defg", "hijkl"));
+    when(() -> Collections.toMap(collection, String::toUpperCase, (x) -> null));
+    thenReturned(expectedMapWithNulls());
+  }
+
+  private static HashMap<String, Object> expectedMapWithNulls() {
+    HashMap<String, Object> expected = new HashMap<>();
+    expected.put("ABC", null);
+    expected.put("DEFG", null);
+    expected.put("HIJKL", null);
+    return expected;
+  }
+
+  @Test
+  public void empty_collection_to_map_with_key_and_value_function() {
+    given(collection = list());
+    when(() -> Collections.toMap(collection, String::toUpperCase, String::length));
+    thenReturned(Map.of());
   }
 }
