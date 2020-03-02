e2f7faeffdc03a8d1a76599a5b33690ddc1d16b4
Marcin Mikosik <marcin.mikosik@gmail.com>
2020-03-02 16:26:13 +0100

    added Strings.escaped()

--- src/main/test/org/smoothbuild/util/StringsTest.java
+++ src/main/test/org/smoothbuild/util/StringsTest.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.assertEquals;
 import static org.quackery.Case.newCase;
 import static org.quackery.Suite.suite;
 import static org.quackery.run.Runners.expect;
+import static org.smoothbuild.util.Strings.escaped;
 import static org.smoothbuild.util.Strings.unescaped;
 
 import java.util.HashMap;
@@ -19,17 +20,34 @@ import org.quackery.Test;
 import org.quackery.junit.QuackeryRunner;
 import org.quackery.report.AssumeException;
 
-import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableBiMap;
 
 @RunWith(QuackeryRunner.class)
 public class StringsTest {
 
   @Quackery
   public static Suite mainSuite() {
-    return suite("Test StringUnescaper")
+    return suite("Test Strings")
         .add(suite("unescapes escaped")
-            .addAll(createConversionMap().entrySet().stream()
+            .addAll(createTestDataForUnescaping().entrySet().stream()
                 .map(e -> unescapes(e.getKey(), e.getValue()))
+                .collect(toList())))
+        .add(suite("preserves non escaped characters")
+            .add(unescapingDoesntChange(""))
+            .add(unescapingDoesntChange(" "))
+            .add(unescapingDoesntChange("  "))
+            .add(unescapingDoesntChange("a"))
+            .add(unescapingDoesntChange("ab"))
+            .add(unescapingDoesntChange("abc"))
+            .add(unescapingDoesntChange("abcd")))
+        .add(suite("fails when escaping")
+            .add(failsUnescaping("\\", 0))
+            .add(failsUnescaping("abc\\", 3))
+            .add(failsUnescaping("\\a", 1))
+            .add(failsUnescaping("\\ ", 1)))
+        .add(suite("escapes escaped")
+            .addAll(createTestDataForEscaping().entrySet().stream()
+                .map(e -> escapes(e.getKey(), e.getValue()))
                 .collect(toList())))
         .add(suite("preserves non escaped characters")
             .add(escapingDoesntChange(""))
@@ -38,26 +56,18 @@ public class StringsTest {
             .add(escapingDoesntChange("a"))
             .add(escapingDoesntChange("ab"))
             .add(escapingDoesntChange("abc"))
-            .add(escapingDoesntChange("abcd")))
-        .add(suite("fails when escaping")
-            .add(failsUnescaping("\\", 0))
-            .add(failsUnescaping("abc\\", 3))
-            .add(failsUnescaping("\\a", 1))
-            .add(failsUnescaping("\\ ", 1)));
+            .add(escapingDoesntChange("abcd")));
   }
 
-  private static HashMap<String, String> createConversionMap() {
-    ImmutableMap<String, String> mappings = ImmutableMap.<String, String>builder()
-        .put("\\t", "\t")
-        .put("\\b", "\b")
-        .put("\\n", "\n")
-        .put("\\r", "\r")
-        .put("\\f", "\f")
-        .put("\\\"", "\"")
-        .put("\\\\", "\\")
-        .put("", "")
-        .build();
+  private static HashMap<String, String> createTestDataForEscaping() {
+    return createTestData(escapeMapping());
+  }
 
+  private static HashMap<String, String> createTestDataForUnescaping() {
+    return createTestData(unescapeMapping());
+  }
+
+  private static HashMap<String, String> createTestData(ImmutableBiMap<String, String> mappings) {
     HashMap<String, String> conversionMap = new HashMap<>();
     for (Entry<String, String> entry1 : mappings.entrySet()) {
       for (Entry<String, String> entry2 : mappings.entrySet()) {
@@ -71,7 +81,33 @@ public class StringsTest {
     return conversionMap;
   }
 
+  private static ImmutableBiMap<String, String> unescapeMapping() {
+    return escapeMapping().inverse();
+  }
+
+  private static ImmutableBiMap<String, String> escapeMapping() {
+    return ImmutableBiMap.<String, String>builder()
+        .put("\t", "\\t")
+        .put("\b", "\\b")
+        .put("\n", "\\n")
+        .put("\r", "\\r")
+        .put("\f", "\\f")
+        .put("\"", "\\\"")
+        .put("\\", "\\\\")
+        .put("", "")
+        .build();
+  }
+
   private static Case escapingDoesntChange(String escaped) {
+    return escapes(escaped, escaped);
+  }
+
+  private static Case escapes(String string, String escaped) {
+    return newCase(format("[%s] should be escaped as [%s]", string, escaped),
+        () -> assertEquals(escaped, escaped(string)));
+  }
+
+  private static Case unescapingDoesntChange(String escaped) {
     return unescapes(escaped, escaped);
   }
 
