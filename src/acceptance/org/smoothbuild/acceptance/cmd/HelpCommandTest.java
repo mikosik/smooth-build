c5657d57ba3ba6e167cbe47ef2112e07074b3b16
Marcin Mikosik <marcin.mikosik@gmail.com>
2020-03-03 15:00:52 +0100

    refactored HelpCommandTest

--- src/acceptance/org/smoothbuild/acceptance/cmd/HelpCommandTest.java
+++ src/acceptance/org/smoothbuild/acceptance/cmd/HelpCommandTest.java
@@ -1,5 +1,7 @@
 package org.smoothbuild.acceptance.cmd;
 
+import static org.smoothbuild.util.Strings.unlines;
+
 import org.junit.Test;
 import org.smoothbuild.acceptance.AcceptanceTestCase;
 
@@ -8,46 +10,49 @@ public class HelpCommandTest extends AcceptanceTestCase {
   public void help_command_prints_general_help() {
     whenSmoothHelp();
     thenFinishedWithSuccess();
-    thenOutputContains(generalHelp());
+    thenOutputContains(expectedGeneralHelp());
   }
 
-  private static String generalHelp() {
-    return "usage: smooth <command> <arg>...\n"
-        + "\n"
-        + "All available commands are:\n"
-        + "  build   Build artifact(s) by running specified function(s)\n"
-        + "  clean   Remove all cached objects and artifacts calculated during previous builds\n"
-        + "  help    Print help about given command\n"
-        + "  list    Print arg-less user defined functions\n"
-        + "  tree    Prints execution tree for specified function(s)\n"
-        + "  version Print smooth build version number\n";
+  private static String expectedGeneralHelp() {
+    return unlines(
+        "usage: smooth <command> <arg>...",
+        "",
+        "All available commands are:",
+        "  build   Build artifact(s) by running specified function(s)",
+        "  clean   Remove all cached objects and artifacts calculated during previous builds",
+        "  help    Print help about given command",
+        "  list    Print arg-less user defined functions",
+        "  tree    Prints execution tree for specified function(s)",
+        "  version Print smooth build version number");
   }
 
   @Test
   public void help_build_command_prints_help_for_build() {
     whenSmoothHelp("build");
     thenFinishedWithSuccess();
-    thenOutputContains(buildHelp());
+    thenOutputContains(expectedBuildCommandHelp());
   }
 
-  private static String buildHelp() {
-    return "usage: smooth build <function>...\n"
-        + "\n"
-        + "Build artifact(s) by running specified function(s)\n"
-        + "\n"
-        + "  <function>  function which execution result is saved as artifact\n";
+  private static String expectedBuildCommandHelp() {
+    return unlines(
+        "usage: smooth build <function>...",
+        "",
+        "Build artifact(s) by running specified function(s)",
+        "",
+        "  <function>  function which execution result is saved as artifact");
   }
 
   @Test
   public void help_clean_command_prints_help_for_clean() {
     whenSmoothHelp("clean");
     thenFinishedWithSuccess();
-    thenOutputContains(cleanHelp());
+    thenOutputContains(expectedCleanCommandHelp());
   }
 
-  private static String cleanHelp() {
-    return "usage: smooth clean\n"
-        + "\n"
-        + "Remove all cached objects and artifacts calculated during previous builds\n";
+  private static String expectedCleanCommandHelp() {
+    return unlines(
+        "usage: smooth clean",
+        "",
+        "Remove all cached objects and artifacts calculated during previous builds");
   }
 }
