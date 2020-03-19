601607810756a196dab57eccdeba4aec976336f3
Marcin Mikosik <marcin.mikosik@gmail.com>
2020-03-19 17:49:57 +0100

    refactored InputTest to use Truth framework

--- src/main/test/org/smoothbuild/exec/comp/InputTest.java
+++ src/main/test/org/smoothbuild/exec/comp/InputTest.java
@@ -1,11 +1,8 @@
 package org.smoothbuild.exec.comp;
 
-import static org.hamcrest.Matchers.not;
+import static com.google.common.truth.Truth.assertThat;
 import static org.smoothbuild.exec.comp.Input.input;
 import static org.smoothbuild.util.Lists.list;
-import static org.testory.Testory.given;
-import static org.testory.Testory.thenReturned;
-import static org.testory.Testory.when;
 
 import org.junit.Test;
 import org.smoothbuild.lang.object.base.SString;
@@ -19,39 +16,43 @@ public class InputTest extends TestingContext{
 
   @Test
   public void different_inputs_have_different_hashes() {
-    given(sstring1 = string("abc"));
-    given(sstring2 = string("def"));
-    given(input1 = input(list(sstring1)));
-    given(input2 = input(list(sstring2)));
-    when(input1).hash();
-    thenReturned(not(input2.hash()));
+    sstring1 = string("abc");
+    sstring2 = string("def");
+    input1 = input(list(sstring1));
+    input2 = input(list(sstring2));
+
+    assertThat(input1.hash())
+        .isNotEqualTo(input2.hash());
   }
 
   @Test
   public void inputs_with_same_values_but_in_different_order_have_different_hashes() {
-    given(sstring1 = string("abc"));
-    given(sstring2 = string("def"));
-    given(input1 = input(list(sstring1, sstring2)));
-    given(input2 = input(list(sstring2, sstring1)));
-    when(input1).hash();
-    thenReturned(not(input2.hash()));
+    sstring1 = string("abc");
+    sstring2 = string("def");
+    input1 = input(list(sstring1, sstring2));
+    input2 = input(list(sstring2, sstring1));
+
+    assertThat(input1.hash())
+        .isNotEqualTo(input2.hash());
   }
 
   @Test
   public void equal_inputs_have_equal_hashes() {
-    given(sstring1 = string("abc"));
-    given(input1 = input(list(sstring1)));
-    given(input2 = input(list(sstring1)));
-    when(input1).hash();
-    thenReturned(input2.hash());
+    sstring1 = string("abc");
+    input1 = input(list(sstring1));
+    input2 = input(list(sstring1));
+
+    assertThat(input1.hash())
+        .isEqualTo(input2.hash());
   }
 
   @Test
   public void input_with_no_values_has_hash_different_from_input_with_one_value() {
-    given(sstring1 = string("abc"));
-    given(input1 = input(list(sstring1)));
-    given(input2 = input(list()));
-    when(input1).hash();
-    thenReturned(not(input2.hash()));
+    sstring1 = string("abc");
+    input1 = input(list(sstring1));
+    input2 = input(list());
+
+    assertThat(input1.hash())
+        .isNotEqualTo(input2.hash());
   }
 }
