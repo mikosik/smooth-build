bec2186f668b2cc01648e3f527d5ab0dca07f847
Marcin Mikosik <marcin.mikosik@gmail.com>
2020-03-20 11:28:16 +0100

    refactored ScopeTest to use Truth framework

--- src/main/test/org/smoothbuild/lang/base/ScopeTest.java
+++ src/main/test/org/smoothbuild/lang/base/ScopeTest.java
@@ -1,92 +1,86 @@
 package org.smoothbuild.lang.base;
 
+import static com.google.common.truth.Truth.assertThat;
 import static org.smoothbuild.lang.base.Scope.scope;
-import static org.testory.Testory.given;
-import static org.testory.Testory.thenReturned;
-import static org.testory.Testory.thenThrown;
-import static org.testory.Testory.when;
-import static org.testory.common.Matchers.same;
+import static org.smoothbuild.testing.common.AssertCall.assertCall;
 
 import java.util.NoSuchElementException;
 
 import org.junit.Test;
 
 public class ScopeTest {
-  private final String name = "name";
   private Scope<String> scope;
   private Scope<String> outerScope;
 
   @Test
-  public void empty_scope_doesnt_contain_any_binding() throws Exception {
-    given(scope = scope());
-    when(() -> scope.contains(name));
-    thenReturned(false);
+  public void empty_scope_doesnt_contain_any_binding() {
+    scope = scope();
+    assertThat(scope.contains("name"))
+        .isFalse();
   }
 
   @Test
-  public void getting_not_added_binding_throws_exception() throws Exception {
-    given(scope = scope());
-    when(() -> scope.get(name));
-    thenThrown(NoSuchElementException.class);
+  public void getting_not_added_binding_throws_exception() {
+    scope = scope();
+    assertCall(() -> scope.get("name"))
+        .throwsException(NoSuchElementException.class);
   }
 
   @Test
-  public void scope_contains_added_binding() throws Exception {
-    given(scope = scope());
-    given(scope).add(name, "bound");
-    when(() -> scope.contains(name));
-    thenReturned(true);
+  public void scope_contains_added_binding() {
+    scope = scope();
+    scope.add("name", "bound");
+    assertThat(scope.contains("name"))
+        .isTrue();
   }
 
   @Test
-  public void added_binding_can_be_retrieved() throws Exception {
-    given(scope = scope());
-    given(scope).add(name, "bound");
-    when(() -> scope.get(name));
-    thenReturned("bound");
+  public void added_binding_can_be_retrieved() {
+    scope = scope();
+    scope.add("name", "bound");
+    assertThat(scope.get("name"))
+        .isEqualTo("bound");
   }
 
   @Test
-  public void adding_same_binding_twice_throws_exception() throws Exception {
-    given(scope = scope());
-    given(scope).add(name, "bound");
-    when(() -> scope.add(name, "bound"));
-    thenThrown(IllegalStateException.class);
+  public void adding_same_binding_twice_throws_exception() {
+    scope = scope();
+    scope.add("name", "bound");
+    assertCall(() -> scope.add("name", "bound"))
+        .throwsException(IllegalStateException.class);
   }
 
   @Test
-  public void when_no_binding_in_current_scope_then_binding_from_outer_scope_is_returned()
-      throws Exception {
-    given(outerScope = scope());
-    given(outerScope).add(name, "bound");
-    given(scope = scope(outerScope));
-    when(() -> scope.get(name));
-    thenReturned("bound");
+  public void when_no_binding_in_current_scope_then_binding_from_outer_scope_is_returned() {
+    outerScope = scope();
+    outerScope.add("name", "bound");
+    scope = scope(outerScope);
+    assertThat(scope.get("name"))
+        .isEqualTo("bound");
   }
 
   @Test
-  public void binding_in_current_scope_hides_binding_from_outer_scope()
-      throws Exception {
-    given(outerScope = scope());
-    given(outerScope).add(name, "bound in outer");
-    given(scope = scope(outerScope));
-    given(scope).add(name, "bound in inner");
-    when(() -> scope.get(name));
-    thenReturned("bound in inner");
+  public void binding_in_current_scope_hides_binding_from_outer_scope() {
+    outerScope = scope();
+    outerScope.add("name", "bound in outer");
+    scope = scope(outerScope);
+    scope.add("name", "bound in inner");
+    assertThat(scope.get("name"))
+        .isEqualTo("bound in inner");
   }
 
   @Test
-  public void top_level_scope_doesnt_have_outer_scope() throws Exception {
-    given(scope = scope());
-    when(() -> scope.outerScope());
-    thenThrown(UnsupportedOperationException.class);
+  public void top_level_scope_doesnt_have_outer_scope() {
+    scope = scope();
+    assertCall(() -> scope.outerScope())
+        .throwsException(UnsupportedOperationException.class);
   }
 
   @Test
-  public void inner_scope_can_return_outer_scope() throws Exception {
-    given(outerScope = scope());
-    given(scope = scope(outerScope));
-    when(() -> scope.outerScope());
-    thenReturned(same(outerScope));
+  public void inner_scope_can_return_outer_scope() {
+    outerScope = scope();
+    scope = scope(outerScope);
+    assertThat(scope.outerScope())
+        .isSameInstanceAs(outerScope);
   }
 }
