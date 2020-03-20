792dcd3f305430b32ffabf91abf2f5193ba27332
Marcin Mikosik <marcin.mikosik@gmail.com>
2020-03-20 10:34:12 +0100

    refactored JarFileTest to use Truth framework

--- src/main/test/org/smoothbuild/io/util/JarFileTest.java
+++ src/main/test/org/smoothbuild/io/util/JarFileTest.java
@@ -1,15 +1,15 @@
 package org.smoothbuild.io.util;
 
-import static org.testory.Testory.given;
-import static org.testory.Testory.thenReturned;
-import static org.testory.Testory.when;
+import static com.google.common.truth.Truth.assertThat;
 
 import java.io.File;
 
 import org.junit.Test;
 import org.smoothbuild.db.hashed.Hash;
+import org.smoothbuild.io.fs.base.Path;
 
 import com.google.common.io.Files;
+import com.google.common.truth.Truth;
 
 import okio.ByteString;
 
@@ -20,18 +20,18 @@ public class JarFileTest {
 
   @Test
   public void jar_file_hash_is_equal_to_hash_of_file_bytes() throws Exception {
-    given(file = File.createTempFile("temp", ".tmp"));
+    file = File.createTempFile("temp", ".tmp");
     Files.write(bytes.toByteArray(), file);
-    given(jarFile = JarFile.jarFile(file.toPath()));
-    when(jarFile).hash();
-    thenReturned(Hash.of(bytes));
+    jarFile = JarFile.jarFile(file.toPath());
+    assertThat(jarFile.hash())
+        .isEqualTo(Hash.of(bytes));
   }
 
   @Test
   public void path_passed_during_construction_is_returned_by_path() throws Exception {
-    given(file = File.createTempFile("temp", ".tmp"));
-    given(jarFile = JarFile.jarFile(file.toPath()));
-    when(jarFile).path();
-    thenReturned(file.toPath());
+    file = File.createTempFile("temp", ".tmp");
+    jarFile = JarFile.jarFile(file.toPath());
+    Truth.<Path>assertThat(jarFile.path())
+        .isEqualTo(file.toPath());
   }
 }
