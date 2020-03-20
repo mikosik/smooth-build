5beb78a764d6e0688a03e25eb939a7d32c02fb2e
Marcin Mikosik <marcin.mikosik@gmail.com>
2020-03-20 10:10:25 +0100

    refactored GenericFileSystemTestCase to use Truth framework

--- src/main/test/org/smoothbuild/io/fs/base/GenericFileSystemTestCase.java
+++ src/main/test/org/smoothbuild/io/fs/base/GenericFileSystemTestCase.java
@@ -1,16 +1,11 @@
 package org.smoothbuild.io.fs.base;
 
-import static org.hamcrest.Matchers.containsInAnyOrder;
+import static com.google.common.truth.Truth.assertThat;
 import static org.smoothbuild.io.fs.base.Path.path;
 import static org.smoothbuild.io.fs.base.PathState.DIR;
 import static org.smoothbuild.io.fs.base.PathState.FILE;
 import static org.smoothbuild.io.fs.base.PathState.NOTHING;
-import static org.smoothbuild.testing.common.ExceptionMatcher.exception;
-import static org.testory.Testory.given;
-import static org.testory.Testory.thenEqual;
-import static org.testory.Testory.thenReturned;
-import static org.testory.Testory.thenThrown;
-import static org.testory.Testory.when;
+import static org.smoothbuild.testing.common.AssertCall.assertCall;
 
 import java.io.IOException;
 import java.nio.file.FileAlreadyExistsException;
@@ -32,113 +27,113 @@ public abstract class GenericFileSystemTestCase {
   // pathKind()
 
   @Test
-  public void rooth_path_is_a_dir() throws Exception {
-    when(fileSystem.pathState(Path.root()));
-    thenReturned(DIR);
+  public void rooth_path_is_a_dir() {
+    assertThat(fileSystem.pathState(Path.root()))
+        .isEqualTo(DIR);
   }
 
   @Test
-  public void non_root_path_are_initially_nothing_state() throws Exception {
-    when(fileSystem.pathState(path("abc")));
-    thenReturned(NOTHING);
+  public void non_root_path_are_initially_nothing_state() {
+    assertThat(fileSystem.pathState(path("abc")))
+        .isEqualTo(NOTHING);
   }
 
   @Test
   public void file_path_has_file_state() throws Exception {
-    given(this).createEmptyFile(path);
-    when(fileSystem).pathState(path);
-    thenReturned(FILE);
+    createEmptyFile(path);
+    assertThat(fileSystem.pathState(path))
+        .isEqualTo(FILE);
   }
 
   @Test
   public void dir_path_has_dir_state() throws Exception {
-    given(this).createEmptyFile(dir.append(path));
-    when(fileSystem).pathState(dir);
-    thenReturned(DIR);
+    createEmptyFile(dir.append(path));
+    assertThat(fileSystem.pathState(dir))
+        .isEqualTo(DIR);
   }
 
   @Test
   public void path_state_is_nothing_even_when_its_first_part_is_a_file() throws Exception {
-    given(this).createEmptyFile(path);
-    when(fileSystem).pathState(path.append(path("something")));
-    thenReturned(NOTHING);
+    createEmptyFile(path);
+    assertThat(fileSystem.pathState(path.append(path("something"))))
+        .isEqualTo(NOTHING);
   }
 
   @Test
   public void creating_file_twice_is_possible() throws Exception {
-    given(this).createEmptyFile(path);
-    when(this).createEmptyFile(path);
-    thenReturned();
+    createEmptyFile(path);
+    createEmptyFile(path);
+    assertThat(fileSystem.pathState(path))
+        .isEqualTo(FILE);
   }
 
   // filesFrom()
 
   @Test
-  public void files_throws_exception_when_dir_does_not_exist() throws Exception {
-    when(fileSystem).files(path("abc"));
-    thenThrown(exception(new IOException("Dir " + path("abc") + " doesn't exist.")));
+  public void files_throws_exception_when_dir_does_not_exist() {
+    assertCall(() -> fileSystem.files(path("abc")))
+        .throwsException(new IOException("Dir " + path("abc") + " doesn't exist."));
   }
 
   @Test
   public void files_throws_exception_when_path_is_a_file() throws Exception {
-    given(this).createEmptyFile(path);
-    when(fileSystem).files(path);
-    thenThrown(exception(new IOException("Dir " + path + " doesn't exist. It is a file.")));
+    createEmptyFile(path);
+    assertCall(() -> fileSystem.files(path))
+        .throwsException(new IOException("Dir " + path + " doesn't exist. It is a file."));
   }
 
   @Test
   public void files_returns_all_children() throws Exception {
-    given(this).createEmptyFile("abc/dir1/file1.txt");
-    given(this).createEmptyFile("abc/dir2/file2.txt");
-    given(this).createEmptyFile("abc/text.txt");
-    when(fileSystem).files(path("abc"));
-    thenReturned(containsInAnyOrder(path("dir1"), path("dir2"), path("text.txt")));
+    createEmptyFile("abc/dir1/file1.txt");
+    createEmptyFile("abc/dir2/file2.txt");
+    createEmptyFile("abc/text.txt");
+    assertThat(fileSystem.files(path("abc")))
+        .containsExactly(path("dir1"), path("dir2"), path("text.txt"));
   }
 
   @Test
-  public void files_throws_exception_when_path_does_not_exist() throws Exception {
-    when(fileSystem).files(path("abc"));
-    thenThrown(IOException.class);
+  public void files_throws_exception_when_path_does_not_exist() {
+    assertCall(() -> fileSystem.files(path("abc")))
+        .throwsException(IOException.class);
   }
 
   // source()
 
   @Test
   public void source_reads_file_content() throws Exception {
-    given(this).createFile(path, bytes);
-    when(() -> fileSystem.source(path).readByteString());
-    thenReturned(bytes);
+    createFile(path, bytes);
+    assertThat(fileSystem.source(path).readByteString())
+        .isEqualTo(bytes);
   }
 
   @Test
-  public void source_throws_exception_when_file_does_not_exist() throws Exception {
-    when(() -> fileSystem.source(path("dir/file")));
-    thenThrown(exception(new IOException("File " + path("dir/file") + " doesn't exist.")));
+  public void source_throws_exception_when_file_does_not_exist() {
+    assertCall(() -> fileSystem.source(path("dir/file")))
+        .throwsException(new IOException("File " + path("dir/file") + " doesn't exist."));
   }
 
   @Test
   public void source_throws_exception_when_path_is_dir() throws Exception {
-    given(this).createEmptyFile(path);
-    when(() -> fileSystem.source(path.parent()));
-    thenThrown(exception(new IOException("File 'some/dir' doesn't exist. It is a dir.")));
+    createEmptyFile(path);
+    assertCall(() -> fileSystem.source(path.parent()))
+        .throwsException(new IOException("File 'some/dir' doesn't exist. It is a dir."));
   }
 
   @Test
-  public void source_throws_exception_when_path_is_root_dir() throws Exception {
-    when(() -> fileSystem.source(Path.root()));
-    thenThrown(exception(new IOException("File '' doesn't exist. It is a dir.")));
+  public void source_throws_exception_when_path_is_root_dir() {
+    assertCall(() -> fileSystem.source(Path.root()))
+        .throwsException(new IOException("File '' doesn't exist. It is a dir."));
   }
 
   // sink()
 
   @Test
-  public void data_written_via_sink_can_be_read_by_source()
-      throws Exception {
+  public void data_written_via_sink_can_be_read_by_source() throws Exception {
     try (BufferedSink sink = fileSystem.sink(path)) {
       sink.write(bytes);
     }
-    when(() -> fileSystem.source(path).readByteString());
-    thenReturned(bytes);
+    assertThat(fileSystem.source(path).readByteString())
+        .isEqualTo(bytes);
   }
 
   @Test
@@ -149,196 +144,208 @@ public abstract class GenericFileSystemTestCase {
     try (BufferedSink sink = fileSystem.sink(path)) {
       sink.write(ByteString.encodeUtf8("def"));
     }
-    when(() -> fileSystem.source(path).readByteString());
-    thenReturned(ByteString.encodeUtf8("def"));
+    assertThat(fileSystem.source(path).readByteString())
+        .isEqualTo(ByteString.encodeUtf8("def"));
   }
 
   @Test
   public void sink_fails_when_target_file_is_a_dir() throws Exception {
-    given(this).createEmptyFile(dir.append(path));
-    when(() -> fileSystem.sink(dir));
-    thenThrown(IOException.class);
+    createEmptyFile(dir.append(path));
+    assertCall(() -> fileSystem.sink(dir))
+        .throwsException(IOException.class);
   }
 
   // move()
 
   @Test
-  public void moving_nonexistent_file_fails() throws Exception {
-    when(fileSystem).move(path("source"), path("target"));
-    thenThrown(exception(new IOException(
-        "Cannot move " + path("source") + ". It doesn't exist.")));
+  public void moving_nonexistent_file_fails() {
+    assertCall(() -> fileSystem.move(path("source"), path("target")))
+        .throwsException(new IOException("Cannot move " + path("source") + ". It doesn't exist."));
   }
 
   @Test
   public void moving_directory_fails() throws Exception {
-    given(this).createEmptyFile(path("source/file"));
-    when(fileSystem).move(path("source"), path("target"));
-    thenThrown(exception(new IOException(
-        "Cannot move " + path("source") + ". It is directory.")));
+    createEmptyFile(path("source/file"));
+    assertCall(() -> fileSystem.move(path("source"), path("target")))
+        .throwsException(new IOException("Cannot move " + path("source") + ". It is directory."));
   }
 
   @Test
   public void moving_to_directory_fails() throws Exception {
-    given(this).createEmptyFile(path("source"));
-    given(this).createEmptyFile(path("target/file"));
-    when(fileSystem).move(path("source"), path("target"));
-    thenThrown(exception(new IOException(
-        "Cannot move to " + path("target") + ". It is directory.")));
+    createEmptyFile(path("source"));
+    createEmptyFile(path("target/file"));
+    assertCall(() -> fileSystem.move(path("source"), path("target")))
+        .throwsException(new IOException(
+            "Cannot move to " + path("target") + ". It is directory."));
   }
 
   @Test
   public void moved_file_is_deleted_from_source() throws Exception {
-    given(this).createEmptyFile(path("source"));
-    when(fileSystem).move(path("source"), path("target"));
-    thenEqual(fileSystem.pathState(path("source")), NOTHING);
+    createEmptyFile(path("source"));
+    fileSystem.move(path("source"), path("target"));
+    assertThat(fileSystem.pathState(path("source")))
+        .isEqualTo(NOTHING);
   }
 
   @Test
   public void moved_file_is_copied_to_target() throws Exception {
-    given(this).createFile(path("source"), bytes);
-    when(() -> fileSystem.move(path("source"), path("target")));
-    thenEqual(fileSystem.pathState(path("source")), NOTHING);
-    when(() -> fileSystem.source(path("target")).readByteString());
-    thenReturned(bytes);
+    createFile(path("source"), bytes);
+    fileSystem.move(path("source"), path("target"));
+    assertThat(fileSystem.pathState(path("source")))
+        .isEqualTo(NOTHING);
+    assertThat(fileSystem.source(path("target")).readByteString())
+        .isEqualTo(bytes);
   }
 
   @Test
   public void moved_file_overwrites_target_file() throws Exception {
-    given(this).createFile(path("source"), bytes);
-    given(this).createEmptyFile(path("target"));
-    when(() -> fileSystem.move(path("source"), path("target")));
-    thenEqual(fileSystem.pathState(path("source")), NOTHING);
-    when(() -> fileSystem.source(path("target")).readByteString());
-    thenReturned(bytes);
+    createFile(path("source"), bytes);
+    createEmptyFile(path("target"));
+    fileSystem.move(path("source"), path("target"));
+    assertThat(fileSystem.pathState(path("source")))
+        .isEqualTo(NOTHING);
+    assertThat(fileSystem.source(path("target")).readByteString())
+        .isEqualTo(bytes);
   }
 
   @Test
   public void moving_creates_missing_parent_directories_in_target_path() throws Exception {
-    given(this).createFile(path("source"), bytes);
-    when(fileSystem).move(path("source"), path("dir/target"));
-    when(() -> fileSystem.source(path("dir/target")).readByteString());
-    thenReturned(bytes);
+    createFile(path("source"), bytes);
+    fileSystem.move(path("source"), path("dir/target"));
+    assertThat(fileSystem.source(path("dir/target")).readByteString())
+        .isEqualTo(bytes);
   }
 
   // delete()
 
   @Test
   public void deleting_dir_removes_its_files() throws Exception {
-    given(this).createEmptyFile(path);
-    when(fileSystem).delete(path.parent());
-    thenEqual(fileSystem.pathState(path), NOTHING);
+    createEmptyFile(path);
+    fileSystem.delete(path.parent());
+    assertThat(fileSystem.pathState(path))
+        .isEqualTo(NOTHING);
   }
 
   @Test
   public void delete_file() throws Exception {
-    given(this).createEmptyFile(path);
-    given(fileSystem).delete(path);
-    when(fileSystem).pathState(path);
-    thenReturned(NOTHING);
+    createEmptyFile(path);
+    fileSystem.delete(path);
+    assertThat(fileSystem.pathState(path))
+        .isEqualTo(NOTHING);
   }
 
   @Test
   public void delete_does_nothing_for_nonexistet_path() throws Exception {
-    when(fileSystem).delete(path);
-    thenReturned();
+    fileSystem.delete(path);
+    assertThat(fileSystem.pathState(path))
+        .isEqualTo(NOTHING);
   }
 
   @Test
   public void deleting_root_path_removes_all_files() throws Exception {
-    given(this).createEmptyFile(path);
-    given(this).createEmptyFile(path2);
-    when(fileSystem).delete(Path.root());
-    thenEqual(fileSystem.pathState(path), NOTHING);
-    thenEqual(fileSystem.pathState(path2), NOTHING);
+    createEmptyFile(path);
+    createEmptyFile(path2);
+    fileSystem.delete(Path.root());
+    assertThat(fileSystem.pathState(path))
+        .isEqualTo(NOTHING);
+    assertThat(fileSystem.pathState(path2))
+        .isEqualTo(NOTHING);
   }
 
   // links
 
   @Test
   public void link_contains_data_from_target() throws Exception {
-    given(this).createFile(path, bytes);
-    when(fileSystem).createLink(linkPath, path);
-    thenEqual(fileSystem.source(linkPath).readByteString(), bytes);
+    createFile(path, bytes);
+    fileSystem.createLink(linkPath, path);
+    assertThat(fileSystem.source(linkPath).readByteString())
+        .isEqualTo(bytes);
   }
 
   @Test
   public void creating_links_creates_missing_dirs() throws Exception {
-    given(this).createFile(path, bytes);
-    when(fileSystem).createLink(linkPath, path);
-    thenEqual(fileSystem.pathState(linkPath), FILE);
+    createFile(path, bytes);
+    fileSystem.createLink(linkPath, path);
+    assertThat(fileSystem.pathState(linkPath))
+        .isEqualTo(FILE);
   }
 
   @Test
   public void deleted_link_is_removed() throws Exception {
-    given(this).createFile(path, bytes);
-    given(fileSystem).createLink(linkPath, path);
-    when(fileSystem).delete(linkPath);
-    thenEqual(fileSystem.pathState(linkPath), NOTHING);
+    createFile(path, bytes);
+    fileSystem.createLink(linkPath, path);
+    fileSystem.delete(linkPath);
+    assertThat(fileSystem.pathState(linkPath))
+        .isEqualTo(NOTHING);
   }
 
   @Test
   public void deleting_link_to_file_does_not_delete_target() throws Exception {
-    given(this).createFile(path, bytes);
-    given(fileSystem).createLink(linkPath, path);
-    when(fileSystem).delete(linkPath);
-    thenEqual(fileSystem.pathState(path), FILE);
+    createFile(path, bytes);
+    fileSystem.createLink(linkPath, path);
+    fileSystem.delete(linkPath);
+    assertThat(fileSystem.pathState(path))
+        .isEqualTo(FILE);
   }
 
   @Test
   public void link_to_dir_can_be_used_to_access_its_file() throws Exception {
-    given(this).createFile(path, bytes);
-    when(fileSystem).createLink(linkPath, path.parent());
-    thenEqual(fileSystem.source(linkPath.append(path.lastPart())).readByteString(), bytes);
+    createFile(path, bytes);
+    fileSystem.createLink(linkPath, path.parent());
+    assertThat(fileSystem.source(linkPath.append(path.lastPart())).readByteString())
+        .isEqualTo(bytes);
   }
 
   @Test
   public void deleting_link_to_dir_does_not_delete_target() throws Exception {
-    given(this).createEmptyFile(dir.append(path("ignore")));
-    given(fileSystem).createLink(path, dir);
-    when(fileSystem).delete(path);
-    thenEqual(fileSystem.pathState(path), NOTHING);
-    thenEqual(fileSystem.pathState(dir), DIR);
+    createEmptyFile(dir.append(path("ignore")));
+    fileSystem.createLink(path, dir);
+    fileSystem.delete(path);
+    assertThat(fileSystem.pathState(path))
+        .isEqualTo(NOTHING);
+    assertThat(fileSystem.pathState(dir))
+        .isEqualTo(DIR);
   }
 
   @Test
   public void cannot_create_link_when_path_is_taken_by_file() throws Exception {
-    given(this).createEmptyFile(path);
-    given(this).createEmptyFile(linkPath);
-    when(fileSystem).createLink(linkPath, path);
-    thenThrown(exception(new IOException("Cannot use " + linkPath
-        + " path. It is already taken.")));
+    createEmptyFile(path);
+    createEmptyFile(linkPath);
+    assertCall(() -> fileSystem.createLink(linkPath, path))
+        .throwsException(new IOException("Cannot use " + linkPath + " path. It is already taken."));
   }
 
   @Test
   public void cannot_create_link_when_path_is_taken_by_dir() throws Exception {
-    given(this).createEmptyFile(path);
-    given(this).createEmptyFile(linkPath);
-    when(fileSystem).createLink(linkPath.parent(), path);
-    thenThrown(exception(new IOException("Cannot use " + linkPath.parent()
-        + " path. It is already taken.")));
+    createEmptyFile(path);
+    createEmptyFile(linkPath);
+    assertCall(() -> fileSystem.createLink(linkPath.parent(), path))
+        .throwsException(new IOException(
+            "Cannot use " + linkPath.parent() + " path. It is already taken."));
   }
 
   // createDir()
 
   @Test
   public void created_dir_exists() throws Exception {
-    given(fileSystem).createDir(path);
-    when(fileSystem).pathState(path);
-    thenReturned(DIR);
+    fileSystem.createDir(path);
+    assertThat(fileSystem.pathState(path))
+        .isEqualTo(DIR);
   }
 
   @Test
   public void creating_existing_dir_does_not_cause_errors() throws Exception {
-    given(fileSystem).createDir(path);
-    when(fileSystem).createDir(path);
-    thenReturned();
+    fileSystem.createDir(path);
+    fileSystem.createDir(path);
+    assertThat(fileSystem.pathState(path))
+        .isEqualTo(DIR);
   }
 
   @Test
   public void cannot_create_dir_if_such_file_already_exists() throws Exception {
-    given(this).createEmptyFile(path);
-    when(fileSystem).createDir(path);
-    thenThrown(FileAlreadyExistsException.class);
+    createEmptyFile(path);
+    assertCall(() -> fileSystem.createDir(path))
+        .throwsException(FileAlreadyExistsException.class);
   }
 
   // helpers
