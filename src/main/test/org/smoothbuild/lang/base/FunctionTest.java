57178cf88e025eadb92c5e64ae8e72005e3b0e1d
Marcin Mikosik <marcin.mikosik@gmail.com>
2020-03-20 10:53:30 +0100

    refactored FunctionTest to use Truth framework

--- src/main/test/org/smoothbuild/lang/base/FunctionTest.java
+++ src/main/test/org/smoothbuild/lang/base/FunctionTest.java
@@ -1,10 +1,9 @@
 package org.smoothbuild.lang.base;
 
+import static com.google.common.truth.Truth.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.smoothbuild.testing.common.AssertCall.assertCall;
 import static org.smoothbuild.util.Lists.list;
-import static org.testory.Testory.given;
-import static org.testory.Testory.mock;
-import static org.testory.Testory.thenReturned;
-import static org.testory.Testory.when;
 
 import java.util.List;
 
@@ -15,67 +14,56 @@ import org.smoothbuild.testing.TestingContext;
 
 public class FunctionTest extends TestingContext {
   private final ConcreteType STRING = stringType();
-  private String name;
-  private List<Parameter> parameters;
-  private Signature signature;
-  private Function function;
 
-  @Test(expected = NullPointerException.class)
-  public void nullSignatureIsForbidden() throws Exception {
-    new MyFunction(null);
+  @Test
+  public void nullSignatureIsForbidden() {
+    assertCall(() -> new MyFunction(null))
+        .throwsException(NullPointerException.class);
   }
 
   @Test
   public void type_returns_signature_type() {
-    given(signature = new Signature(STRING, "name", list()));
-    given(function = new MyFunction(signature));
-    when(function).type();
-    thenReturned(STRING);
+    Function function = new MyFunction(new Signature(STRING, "name", list()));
+    assertThat(function.type())
+        .isEqualTo(STRING);
   }
 
   @Test
   public void name_returns_signature_name() {
-    given(name = "name");
-    given(signature = new Signature(STRING, name, list()));
-    given(function = new MyFunction(signature));
-    when(function).type();
-    thenReturned(STRING);
+    Function function = new MyFunction(new Signature(STRING, "name", list()));
+    assertThat(function.type())
+        .isEqualTo(STRING);
   }
 
   @Test
   public void params_returns_signature_params() {
-    given(parameters = list(new Parameter(0, STRING, "name", null)));
-    given(signature = new Signature(STRING, "name", parameters));
-    given(function = new MyFunction(signature));
-    when(function).parameters();
-    thenReturned(parameters);
+    List<Parameter> parameters = list(new Parameter(0, STRING, "name", null));
+    Function function = new MyFunction(new Signature(STRING, "name", parameters));
+    assertThat(function.parameters())
+        .isEqualTo(parameters);
   }
 
   @Test
   public void function_without_params_can_be_called_without_args() {
-    given(parameters = list());
-    given(signature = new Signature(STRING, "name", parameters));
-    given(function = new MyFunction(signature));
-    when(() -> function.canBeCalledArgless());
-    thenReturned(true);
+    Function function = new MyFunction(new Signature(STRING, "name", list()));
+    assertThat(function.canBeCalledArgless())
+        .isTrue();
   }
 
   @Test
   public void function_with_all_params_with_default_values_can_be_called_without_args() {
-    given(parameters = list(paramWithDefault(), paramWithDefault()));
-    given(signature = new Signature(STRING, "name", parameters));
-    given(function = new MyFunction(signature));
-    when(() -> function.canBeCalledArgless());
-    thenReturned(true);
+    List<Parameter> parameters = list(paramWithDefault(), paramWithDefault());
+    Function function = new MyFunction(new Signature(STRING, "name", parameters));
+    assertThat(function.canBeCalledArgless())
+        .isTrue();
   }
 
   @Test
   public void function_with_one_param_without_default_values_cannot_be_called_without_args() {
-    given(parameters = list(paramWithDefault(), paramWithoutDefault()));
-    given(signature = new Signature(STRING, "name", parameters));
-    given(function = new MyFunction(signature));
-    when(() -> function.canBeCalledArgless());
-    thenReturned(false);
+    List<Parameter> parameters = list(paramWithDefault(), paramWithoutDefault());
+    Function function = new MyFunction(new Signature(STRING, "name", parameters));
+    assertThat(function.canBeCalledArgless())
+        .isFalse();
   }
 
   private Parameter paramWithDefault() {
