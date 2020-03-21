0c2307b25a1371084df1a5d866929b4eca50ece5
Marcin Mikosik <marcin.mikosik@gmail.com>
2020-03-21 11:53:54 +0100

    refactored BoolArrayTest to use Truth framework

--- src/main/test/org/smoothbuild/lang/object/base/BoolArrayTest.java
+++ src/main/test/org/smoothbuild/lang/object/base/BoolArrayTest.java
@@ -1,19 +1,15 @@
 package org.smoothbuild.lang.object.base;
 
-import static org.testory.Testory.given;
-import static org.testory.Testory.thenReturned;
-import static org.testory.Testory.when;
+import static com.google.common.truth.Truth.assertThat;
 
 import org.junit.Test;
 import org.smoothbuild.testing.TestingContext;
 
 public class BoolArrayTest extends TestingContext {
-  private Array array;
-
   @Test
-  public void type_of_bool_array_is_bool_array() throws Exception {
-    given(array = arrayBuilder(boolType()).build());
-    when(array.type());
-    thenReturned(arrayType(boolType()));
+  public void type_of_bool_array_is_bool_array() {
+    Array array = arrayBuilder(boolType()).build();
+    assertThat(array.type())
+        .isEqualTo(arrayType(boolType()));
   }
 }
