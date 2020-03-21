6c48d8b596fc4504eacab655e5bc144f4bb792df
Marcin Mikosik <marcin.mikosik@gmail.com>
2020-03-21 13:38:51 +0100

    refactored StructArrayTest to use Truth framework

--- src/main/test/org/smoothbuild/lang/object/base/StructArrayTest.java
+++ src/main/test/org/smoothbuild/lang/object/base/StructArrayTest.java
@@ -1,19 +1,16 @@
 package org.smoothbuild.lang.object.base;
 
-import static org.testory.Testory.given;
-import static org.testory.Testory.thenReturned;
-import static org.testory.Testory.when;
+import static com.google.common.truth.Truth.assertThat;
 
 import org.junit.Test;
 import org.smoothbuild.testing.TestingContext;
 
 public class StructArrayTest extends TestingContext {
-  private Array array;
 
   @Test
-  public void type_of_struct_array_is_struct_array() throws Exception {
-    given(array = arrayBuilder(personType()).build());
-    when(array.type());
-    thenReturned(arrayType(personType()));
+  public void type_of_struct_array_is_struct_array() {
+    Array array = arrayBuilder(personType()).build();
+    assertThat(array.type())
+        .isEqualTo(arrayType(personType()));
   }
 }
