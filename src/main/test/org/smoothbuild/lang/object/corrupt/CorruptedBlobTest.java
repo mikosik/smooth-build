68f9ecd3909c5bb9ab2f1fc4991747b9c8629aa9
Marcin Mikosik <marcin.mikosik@gmail.com>
2020-03-23 10:43:30 +0100

    refactored CorruptedBlobTest to use Truth framework

--- src/main/test/org/smoothbuild/lang/object/corrupt/CorruptedBlobTest.java
+++ src/main/test/org/smoothbuild/lang/object/corrupt/CorruptedBlobTest.java
@@ -1,10 +1,7 @@
 package org.smoothbuild.lang.object.corrupt;
 
-import static org.smoothbuild.testing.common.ExceptionMatcher.exception;
-import static org.testory.Testory.given;
-import static org.testory.Testory.thenReturned;
-import static org.testory.Testory.thenThrown;
-import static org.testory.Testory.when;
+import static com.google.common.truth.Truth.assertThat;
+import static org.smoothbuild.testing.common.AssertCall.assertCall;
 
 import org.junit.jupiter.api.Test;
 import org.smoothbuild.db.hashed.Hash;
@@ -15,33 +12,30 @@ import org.smoothbuild.lang.object.db.ObjectDbException;
 import okio.ByteString;
 
 public class CorruptedBlobTest  extends AbstractCorruptedTestCase {
-  private Hash instanceHash;
-  private Hash dataHash;
-  private ByteString byteString;
-
   @Test
-  public void learning_test_create_bool() {
+  public void learning_test_create_bool() throws Exception {
     /*
      * This test makes sure that other tests in this class use proper scheme to save smooth blob
      * in HashedDb.
      */
-    given(byteString = ByteString.of((byte) 1, (byte) 2));
-    given(() -> instanceHash =
+    ByteString byteString = ByteString.of((byte) 1, (byte) 2);
+    Hash instanceHash =
         hash(
             hash(blobType()),
-            hash(byteString)));
-    when(() -> ((Blob) objectDb().get(instanceHash)).source().readByteString());
-    thenReturned(byteString);
+            hash(byteString));
+    assertThat(((Blob) objectDb().get(instanceHash)).source().readByteString())
+        .isEqualTo(byteString);
   }
 
   @Test
-  public void bool_with_data_hash_pointing_nowhere_is_corrupted() {
-    given(() -> dataHash = Hash.of(33));
-    given(() -> instanceHash =
+  public void bool_with_data_hash_pointing_nowhere_is_corrupted() throws Exception {
+    Hash dataHash = Hash.of(33);
+    Hash instanceHash =
         hash(
             hash(blobType()),
-            dataHash));
-    when(() -> ((Blob) objectDb().get(instanceHash)).source());
-    thenThrown(exception(new ObjectDbException(instanceHash, new NoSuchDataException(dataHash))));
+            dataHash);
+    assertCall(() -> ((Blob) objectDb().get(instanceHash)).source())
+        .throwsException(new ObjectDbException(instanceHash))
+        .withCause(new NoSuchDataException(dataHash));
   }
 }
