fc1515811712bdfa5a5f4e899b997260b98ec185
Marcin Mikosik <marcin.mikosik@gmail.com>
2020-03-23 13:42:32 +0100

    refactored AbstractTypeTestCase and subclasses to use Truth framework

--- src/main/test/org/smoothbuild/lang/object/type/AbstractTypeTestCase.java
+++ src/main/test/org/smoothbuild/lang/object/type/AbstractTypeTestCase.java
@@ -1,10 +1,6 @@
 package org.smoothbuild.lang.object.type;
 
-import static org.smoothbuild.lang.object.type.ThoroughTypeMatcher.typeMatchingThoroughly;
-import static org.testory.Testory.given;
-import static org.testory.Testory.thenReturned;
-import static org.testory.Testory.when;
-import static org.testory.common.Matchers.same;
+import static com.google.common.truth.Truth.assertThat;
 
 import org.junit.jupiter.api.Test;
 import org.smoothbuild.db.hashed.Hash;
@@ -12,39 +8,47 @@ import org.smoothbuild.lang.object.db.ObjectDb;
 import org.smoothbuild.testing.TestingContext;
 
 public abstract class AbstractTypeTestCase extends TestingContext {
-  protected ConcreteType type;
-  protected Hash hash;
-  private ObjectDb objectDbOther;
-
   protected abstract ConcreteType getType(ObjectDb objectDb);
 
   @Test
-  public void type_can_be_read_back() throws Exception {
-    given(type = getType(objectDb()));
-    when(() -> objectDbOther().get(type.hash()));
-    thenReturned(typeMatchingThoroughly(type));
+  public void type_can_be_read_back() {
+    ConcreteType type = getType(objectDb());
+    assertTypesAreDeeplyEqual((ConcreteType) objectDbOther().get(type.hash()), type);
   }
 
   @Test
-  public void type_is_cached() throws Exception {
-    given(type = getType(objectDb()));
-    when(() -> getType(objectDb()));
-    thenReturned(same(type));
+  public void type_is_cached() {
+    assertThat(getType(objectDb()))
+        .isSameInstanceAs(getType(objectDb()));
   }
 
   @Test
-  public void type_is_cached_when_read_by_hash() throws Exception {
-    given(type = getType(objectDb()));
-    when(() -> objectDb().get(type.hash()));
-    thenReturned(same(type));
+  public void type_is_cached_when_read_by_hash() {
+    ConcreteType type = getType(objectDb());
+    assertThat(objectDb().get(type.hash()))
+        .isSameInstanceAs(type);
   }
 
   @Test
-  public void type_is_cached_when_read_twice_by_hash() throws Exception {
-    given(hash = getType(objectDb()).hash());
-    given(objectDbOther = objectDbOther());
-    given(type = (ConcreteType) objectDbOther.get(hash));
-    when(() -> objectDbOther.get(hash));
-    thenReturned(same(type));
+  public void type_is_cached_when_read_twice_by_hash() {
+    Hash hash = getType(objectDb()).hash();
+    ObjectDb objectDbOther = objectDbOther();
+    ConcreteType type = (ConcreteType) objectDbOther.get(hash);
+    assertThat(objectDbOther.get(hash))
+        .isSameInstanceAs(type);
+  }
+
+  public static void assertTypesAreDeeplyEqual(ConcreteType actual, ConcreteType expected) {
+    assertThat(expected.name().equals(actual.name())
+        && expected.hash().equals(actual.hash())
+        && expected.dataHash().equals(actual.dataHash())
+        && expected.isGeneric() == actual.isGeneric()
+        && expected.isNothing() == actual.isNothing()
+        && expected.coreDepth() == actual.coreDepth()
+        && expected.hierarchy().equals(actual.hierarchy())
+        && expected.jType().equals(actual.jType())
+        && expected.type().equals(expected.type())
+        && expected.toString().equals(expected.toString()))
+        .isTrue();
   }
 }
