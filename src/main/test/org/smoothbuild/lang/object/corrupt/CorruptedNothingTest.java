8e81900bd4aea1980ee622046a86461b16c29066
Marcin Mikosik <marcin.mikosik@gmail.com>
2020-03-23 11:04:18 +0100

    refactored CorruptedNothingTest to use Truth framework

--- src/main/test/org/smoothbuild/lang/object/corrupt/CorruptedNothingTest.java
+++ src/main/test/org/smoothbuild/lang/object/corrupt/CorruptedNothingTest.java
@@ -1,9 +1,6 @@
 package org.smoothbuild.lang.object.corrupt;
 
-import static org.smoothbuild.testing.common.ExceptionMatcher.exception;
-import static org.testory.Testory.given;
-import static org.testory.Testory.thenThrown;
-import static org.testory.Testory.when;
+import static org.smoothbuild.testing.common.AssertCall.assertCall;
 
 import org.junit.jupiter.api.Test;
 import org.smoothbuild.db.hashed.Hash;
@@ -11,16 +8,14 @@ import org.smoothbuild.lang.object.base.SString;
 import org.smoothbuild.lang.object.db.ObjectDbException;
 
 public class CorruptedNothingTest extends AbstractCorruptedTestCase {
-  private Hash instanceHash;
-
   @Test
   public void learning_test_create_string() throws Exception {
-    given(instanceHash =
+    Hash instanceHash =
         hash(
             hash(nothingType()),
-            hash("aaa")));
-    when(() -> ((SString) objectDb().get(instanceHash)).jValue());
-    thenThrown(exception(new ObjectDbException(instanceHash,
-        "Object type is Nothing so such object cannot exist.")));
+            hash("aaa"));
+    assertCall(() -> ((SString) objectDb().get(instanceHash)).jValue())
+        .throwsException(new ObjectDbException(
+            instanceHash, "Object type is Nothing so such object cannot exist."));
   }
 }
