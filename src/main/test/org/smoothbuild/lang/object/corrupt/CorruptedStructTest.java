2948014719b651eb4732b98d023836e49da5cb19
Marcin Mikosik <marcin.mikosik@gmail.com>
2020-03-23 12:43:20 +0100

    refactored CorruptedStuctTest to use Truth framework

--- src/main/test/org/smoothbuild/lang/object/corrupt/CorruptedStructTest.java
+++ src/main/test/org/smoothbuild/lang/object/corrupt/CorruptedStructTest.java
@@ -1,10 +1,7 @@
 package org.smoothbuild.lang.object.corrupt;
 
-import static org.smoothbuild.testing.common.ExceptionMatcher.exception;
-import static org.testory.Testory.given;
-import static org.testory.Testory.thenReturned;
-import static org.testory.Testory.thenThrown;
-import static org.testory.Testory.when;
+import static com.google.common.truth.Truth.assertThat;
+import static org.smoothbuild.testing.common.AssertCall.assertCall;
 
 import org.junit.jupiter.api.Test;
 import org.smoothbuild.db.hashed.DecodingHashSequenceException;
@@ -13,70 +10,66 @@ import org.smoothbuild.lang.object.base.Struct;
 import org.smoothbuild.lang.object.db.ObjectDbException;
 
 public class CorruptedStructTest extends AbstractCorruptedTestCase {
-  private Hash structHash;
-  private Struct struct;
-  private Hash fieldValuesHash;
-
   @Test
   public void learning_test_create_struct() throws Exception {
     /*
      * This test makes sure that other tests in this class use proper scheme to save smooth struct
      * in HashedDb.
      */
-    when(() ->
+    assertThat(
         hash(
             hash(personType()),
             hash(
                 hash(string("John")),
-                hash(string("Doe")))));
-    thenReturned(person("John", "Doe").hash());
+                hash(string("Doe")))))
+        .isEqualTo(person("John", "Doe").hash());
   }
 
   @Test
-  public void struct_with_too_few_fields_is_corrupted() {
-    given(() -> fieldValuesHash =
+  public void struct_with_too_few_fields_is_corrupted() throws Exception {
+    Hash fieldValuesHash =
         hash(
-          hash(string("John"))));
-    given(() -> structHash =
+            hash(string("John")));
+    Hash structHash =
         hash(
             hash(personType()),
-            fieldValuesHash));
-    given(struct = (Struct) objectDb().get(structHash));
-    when(() -> struct.get("firstName"));
-    thenThrown(exception(new ObjectDbException(structHash,
-        new DecodingHashSequenceException(fieldValuesHash, 2, 1))));
+            fieldValuesHash);
+    Struct struct = (Struct) objectDb().get(structHash);
+    assertCall(() -> struct.get("firstName"))
+        .throwsException(new ObjectDbException(structHash))
+        .withCause(new DecodingHashSequenceException(fieldValuesHash, 2, 1));
   }
 
   @Test
   public void struct_with_too_many_fields_is_corrupted() throws Exception {
-    given(() -> fieldValuesHash =
+    Hash fieldValuesHash =
         hash(
             hash(string("John")),
             hash(string("Doe")),
-            hash(string("junk"))));
-    given(() -> structHash =
+            hash(string("junk")));
+    Hash structHash =
         hash(
             hash(personType()),
-            fieldValuesHash));
-    given(struct = (Struct) objectDb().get(structHash));
-    when(() -> struct.get("firstName"));
-    thenThrown(exception(new ObjectDbException(structHash,
-        new DecodingHashSequenceException(fieldValuesHash, 2, 3))));
+            fieldValuesHash);
+    Struct struct = (Struct) objectDb().get(structHash);
+    assertCall(() -> struct.get("firstName"))
+        .throwsException(new ObjectDbException(structHash))
+        .withCause(new DecodingHashSequenceException(fieldValuesHash, 2, 3));
   }
 
   @Test
   public void struct_with_field_of_wrong_type_is_corrupted() throws Exception {
-    given(structHash =
+    Hash structHash =
         hash(
             hash(personType()),
             hash(
                 hash(string("John")),
-                hash(bool(true)))));
-    given(struct = (Struct) objectDb().get(structHash));
-    when(() -> struct.get("firstName"));
-    thenThrown(exception(new ObjectDbException(structHash, "Its type specifies field 'lastName' "
+                hash(bool(true))));
+    Struct struct = (Struct) objectDb().get(structHash);
+    assertCall(() -> struct.get("firstName"))
+        .throwsException(new ObjectDbException(structHash, "Its type specifies field 'lastName' "
         + "with type Type(\"String\"):7561a6b22d5fe8e18dec31904e0e9cdf6644ca96 but its data "
         + "has object of type Type(\"Bool\"):912e97481a6f232997c26729f48c14d33540c9e1 assigned "
-        + "to that field.")));
+        + "to that field."));
   }
 }
