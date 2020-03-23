3c31688daf80b20227cd93ef8f89e46366e22dc6
Marcin Mikosik <marcin.mikosik@gmail.com>
2020-03-23 08:02:35 +0100

    added .withCall() to assertCall(...)

--- src/testing/java/org/smoothbuild/testing/common/AssertCall.java
+++ src/testing/java/org/smoothbuild/testing/common/AssertCall.java
@@ -3,7 +3,13 @@ package org.smoothbuild.testing.common;
 import static com.google.common.truth.Fact.fact;
 import static com.google.common.truth.Fact.simpleFact;
 import static com.google.common.truth.Truth.assertAbout;
+import static java.util.Arrays.asList;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import com.google.common.truth.Fact;
 import com.google.common.truth.FailureMetadata;
 import com.google.common.truth.Subject;
 import com.google.common.truth.Subject.Factory;
@@ -34,24 +40,82 @@ public class AssertCall {
       this.actual = actual;
     }
 
-    public void throwsException(Throwable expected) {
+    public ExceptionCauseSubject throwsException(Throwable expected) {
+      String expectedClassName = expected.getClass().getCanonicalName();
       if (actual == null) {
         failWithoutActual(simpleFact("expected call to throw exception"));
-      }
-      String expectedClassName = expected.getClass().getCanonicalName();
-      if (actual.getClass() != expected.getClass()) {
+      } else if (actual.getClass() != expected.getClass()) {
         failWithActual(fact("expected call to throw", expectedClassName));
+      } else {
+        Throwable actualThrowable = (Throwable) actual;
+        check("getMessage()").that(actualThrowable.getMessage()).isEqualTo(expected.getMessage());
       }
-      Throwable actualThrowable = (Throwable) this.actual;
-      check("getMessage()").that(actualThrowable.getMessage()).isEqualTo(expected.getMessage());
+      return new ExceptionCauseSubject(List.of(
+          fact("expected call to throw", expectedClassName),
+          fact("with message", ((Throwable) actual).getMessage())));
     }
 
-    public void throwsException(Class<? extends Throwable> expected) {
+    public ExceptionCauseSubject throwsException(Class<? extends Throwable> expected) {
       if (actual == null) {
         failWithoutActual(simpleFact("expected call to throw exception"));
-      }
-      if (actual.getClass() != expected) {
+      } else if (actual.getClass() != expected) {
         failWithActual(fact("expected call to throw", expected.getCanonicalName()));
+      }
+      return new ExceptionCauseSubject(List.of(
+          fact("expected call to throw", expected.getCanonicalName())));
+    }
+
+    public class ExceptionCauseSubject {
+      private final List<Fact> facts;
+
+      public ExceptionCauseSubject(List<Fact> facts) {
+        this.facts = facts;
+      }
+
+      public void withCause(Throwable expectedCause) {
+        Throwable actualCause = ((Throwable) actual).getCause();
+        String expectedCauseName = expectedCause.getClass().getCanonicalName();
+        if (actualCause == null) {
+          callFailWithoutActual(
+              facts,
+              fact("with cause", expectedCauseName),
+              simpleFact("but was no cause"));
+        } else if (!Objects.equals(actualCause.getClass(), expectedCause.getClass())) {
+          callFailWithoutActual(
+              facts,
+              fact("with cause", expectedCauseName),
+              fact("but was cause", actualCause.getClass().getCanonicalName()));
+        } else if (!Objects.equals(actualCause.getMessage(), expectedCause.getMessage())) {
+          callFailWithoutActual(
+              facts,
+              fact("with cause", expectedCauseName),
+              fact("with message", expectedCause.getMessage()),
+              fact("but was message", actualCause.getMessage()));
+        }
+      }
+
+      public void withCause(Class<? extends Throwable> expectedCause) {
+        Throwable actualCause = ((Throwable) actual).getCause();
+        String expectedCauseName = expectedCause.getCanonicalName();
+        if (actualCause == null) {
+          callFailWithoutActual(
+              facts,
+              fact("with cause", expectedCauseName),
+              simpleFact("but was exception without cause"));
+        } else if (!actualCause.getClass().equals(expectedCause)) {
+          callFailWithoutActual(
+              facts,
+              fact("with cause", expectedCauseName),
+              fact("but was cause", actualCause.getClass().getCanonicalName()));
+        }
+      }
+
+      private void callFailWithoutActual(List<Fact> facts1, Fact... facts2) {
+        Fact first = facts1.get(0);
+        ArrayList<Fact> allWithoutFirst = new ArrayList<>();
+        allWithoutFirst.addAll(facts1.subList(1, facts1.size()));
+        allWithoutFirst.addAll(asList(facts2));
+        failWithoutActual(first, allWithoutFirst.toArray(Fact[]::new));
       }
     }
   }
