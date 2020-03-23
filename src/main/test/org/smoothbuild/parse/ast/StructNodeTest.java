caa410f583bb9effbe0aaa733952f9c74f5885ea
Marcin Mikosik <marcin.mikosik@gmail.com>
2020-03-23 16:14:02 +0100

    refactored StructNodeTest to use Truth framework

--- src/main/test/org/smoothbuild/parse/ast/StructNodeTest.java
+++ src/main/test/org/smoothbuild/parse/ast/StructNodeTest.java
@@ -1,34 +1,30 @@
 package org.smoothbuild.parse.ast;
 
+import static com.google.common.truth.Truth.assertThat;
 import static org.smoothbuild.lang.base.Location.unknownLocation;
 import static org.smoothbuild.parse.ast.StructNode.constructorNameToTypeName;
 import static org.smoothbuild.util.Lists.list;
-import static org.testory.Testory.given;
-import static org.testory.Testory.thenReturned;
-import static org.testory.Testory.when;
 
 import org.junit.jupiter.api.Test;
 
 public class StructNodeTest {
-  private StructNode struct;
-
   @Test
   public void constructor_name_is_lower_camelcase_of_type_name() {
-    given(struct = new StructNode("MyType", list(), unknownLocation()));
-    when(() -> struct.constructor().name());
-    thenReturned("myType");
+    StructNode struct = new StructNode("MyType", list(), unknownLocation());
+    assertThat(struct.constructor().name())
+        .isEqualTo("myType");
   }
 
   @Test
   public void constructor_name_is_lower_camelcase_of_type_name_preserving_underscores() {
-    given(struct = new StructNode("My_Pretty_Type", list(), unknownLocation()));
-    when(() -> struct.constructor().name());
-    thenReturned("my_Pretty_Type");
+    StructNode struct = new StructNode("My_Pretty_Type", list(), unknownLocation());
+    assertThat(struct.constructor().name())
+        .isEqualTo("my_Pretty_Type");
   }
 
   @Test
   public void type_Name_is_upper_camelcase_of_constructor_name() {
-    when(() -> constructorNameToTypeName("myType"));
-    thenReturned("MyType");
+    assertThat(constructorNameToTypeName("myType"))
+        .isEqualTo("MyType");
   }
 }
