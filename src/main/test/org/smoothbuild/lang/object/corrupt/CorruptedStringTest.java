7d336739b81c1c9e686376406b4f37ebe53b79e6
Marcin Mikosik <marcin.mikosik@gmail.com>
2020-03-23 12:38:44 +0100

    refactored CorruptedStringTest to use Truth framework

--- src/main/test/org/smoothbuild/lang/object/corrupt/CorruptedStringTest.java
+++ src/main/test/org/smoothbuild/lang/object/corrupt/CorruptedStringTest.java
@@ -1,10 +1,7 @@
 package org.smoothbuild.lang.object.corrupt;
 
-import static org.smoothbuild.testing.common.ExceptionMatcher.exception;
-import static org.testory.Testory.given;
-import static org.testory.Testory.thenReturned;
-import static org.testory.Testory.thenThrown;
-import static org.testory.Testory.when;
+import static com.google.common.truth.Truth.assertThat;
+import static org.smoothbuild.testing.common.AssertCall.assertCall;
 
 import org.junit.jupiter.api.Test;
 import org.smoothbuild.db.hashed.DecodingStringException;
@@ -15,32 +12,29 @@ import org.smoothbuild.lang.object.db.ObjectDbException;
 import okio.ByteString;
 
 public class CorruptedStringTest extends AbstractCorruptedTestCase {
-  private Hash instanceHash;
-  private Hash notStringHash;
-
   @Test
   public void learning_test_create_string() throws Exception {
     /*
      * This test makes sure that other tests in this class use proper scheme to save smooth bool
      * in HashedDb.
      */
-    given(instanceHash =
+    Hash instanceHash =
         hash(
             hash(stringType()),
-            hash("aaa")));
-    when(() -> ((SString) objectDb().get(instanceHash)).jValue());
-    thenReturned("aaa");
+            hash("aaa"));
+    assertThat(((SString) objectDb().get(instanceHash)).jValue())
+        .isEqualTo("aaa");
   }
 
   @Test
   public void string_with_data_being_invalid_utf8_sequence_is_corrupted() throws Exception {
-    given(() -> notStringHash = hash(ByteString.of((byte) -64)));
-    given(() -> instanceHash =
+    Hash notStringHash = hash(ByteString.of((byte) -64));
+    Hash instanceHash =
         hash(
             hash(stringType()),
-            notStringHash));
-    when(() -> ((SString) objectDb().get(instanceHash)).jValue());
-    thenThrown(exception(new ObjectDbException(instanceHash,
-        new DecodingStringException(notStringHash, null))));
+            notStringHash);
+    assertCall(() -> ((SString) objectDb().get(instanceHash)).jValue())
+        .throwsException(new ObjectDbException(instanceHash))
+        .withCause(new DecodingStringException(notStringHash, null));
   }
 }
