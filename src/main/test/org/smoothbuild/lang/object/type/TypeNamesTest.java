330de3396d607c414f0a413a2a4c94310e305595
Marcin Mikosik <marcin.mikosik@gmail.com>
2020-03-23 15:27:25 +0100

    refactored TypeNamesTest to use Truth framework

--- src/main/test/org/smoothbuild/lang/object/type/TypeNamesTest.java
+++ src/main/test/org/smoothbuild/lang/object/type/TypeNamesTest.java
@@ -1,51 +1,50 @@
 package org.smoothbuild.lang.object.type;
 
+import static com.google.common.truth.Truth.assertThat;
 import static org.smoothbuild.lang.object.type.TypeNames.isGenericTypeName;
-import static org.testory.Testory.thenReturned;
-import static org.testory.Testory.when;
 
 import org.junit.jupiter.api.Test;
 
 public class TypeNamesTest {
   @Test
   public void empty_string_is_not_generic_type_name() {
-    when(() -> isGenericTypeName(""));
-    thenReturned(false);
+    assertThat(isGenericTypeName(""))
+        .isFalse();
   }
 
   @Test
   public void lowercase_a_character_is_not_generic_type_name() {
-    when(() -> isGenericTypeName("a"));
-    thenReturned(false);
+    assertThat(isGenericTypeName("a"))
+        .isFalse();
   }
 
   @Test
   public void lowercase_b_character_is_not_generic_type_name() {
-    when(() -> isGenericTypeName("b"));
-    thenReturned(false);
+    assertThat(isGenericTypeName("b"))
+        .isFalse();
   }
 
   @Test
   public void uppercase_a_character_is_generic_type_name() {
-    when(() -> isGenericTypeName("A"));
-    thenReturned(true);
+    assertThat(isGenericTypeName("A"))
+        .isTrue();
   }
 
   @Test
   public void uppercase_b_character_is_generic_type_name() {
-    when(() -> isGenericTypeName("B"));
-    thenReturned(true);
+    assertThat(isGenericTypeName("B"))
+        .isTrue();
   }
 
   @Test
   public void longer_string_starting_with_lowercase_is_not_generic_type_name() {
-    when(() -> isGenericTypeName("alphabet"));
-    thenReturned(false);
+    assertThat(isGenericTypeName("alphabet"))
+        .isFalse();
   }
 
   @Test
   public void longer_string_starting_with_uppercase_is_not_generic_type_name() {
-    when(() -> isGenericTypeName("Alphabet"));
-    thenReturned(false);
+    assertThat(isGenericTypeName("Alphabet"))
+        .isFalse();
   }
 }
