b93611e9e814afdad8f9d3482aa0f82081d3722c
Marcin Mikosik <marcin.mikosik@gmail.com>
2020-03-24 16:36:20 +0100

    refactored ListsTest to use Truth framework

--- src/main/test/org/smoothbuild/util/ListsTest.java
+++ src/main/test/org/smoothbuild/util/ListsTest.java
@@ -1,140 +1,143 @@
 package org.smoothbuild.util;
 
+import static com.google.common.truth.Truth.assertThat;
 import static java.util.Arrays.asList;
-import static org.hamcrest.Matchers.contains;
-import static org.hamcrest.Matchers.empty;
 import static org.smoothbuild.util.Lists.concat;
 import static org.smoothbuild.util.Lists.filter;
 import static org.smoothbuild.util.Lists.list;
 import static org.smoothbuild.util.Lists.map;
 import static org.smoothbuild.util.Lists.sane;
-import static org.testory.Testory.given;
-import static org.testory.Testory.thenEqual;
-import static org.testory.Testory.thenReturned;
-import static org.testory.Testory.when;
-import static org.testory.common.Matchers.same;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-import com.google.common.base.Predicates;
-
+@SuppressWarnings("ClassCanBeStatic")
 public class ListsTest {
-  private List<String> list;
+  @Nested
+  class list {
+    @Test
+    public void with_no_elements(){
+      assertThat(list())
+          .isEmpty();
+    }
 
-  @Test
-  public void list_with_no_elements() throws Exception {
-    when(() -> list());
-    thenReturned(empty());
+    @Test
+    public void with_one_element(){
+      assertThat(list("abc"))
+          .containsExactly("abc");
+    }
+
+    @Test
+    public void with_two_elements(){
+      assertThat(list("abc", "def"))
+          .containsExactly("abc", "def")
+          .inOrder();
+    }
+
+    @Test
+    public void with_three_elements(){
+      assertThat(list("abc", "def", "ghi"))
+          .containsExactly("abc", "def", "ghi")
+          .inOrder();
+    }
   }
 
-  @Test
-  public void list_with_one_element() throws Exception {
-    when(() -> list("abc"));
-    thenReturned(contains("abc"));
+  @Nested
+  class concat {
+    @Test
+    public void with_empty(){
+      assertThat(concat(new ArrayList<>(), "element"))
+          .containsExactly("element");
+    }
+
+    @Test
+    public void with_non_empty(){
+      assertThat(concat(asList("first"), "second"))
+          .containsExactly("first", "second")
+          .inOrder();
+    }
+
+    @Test
+    public void concat_doesnt_modify_list(){
+      List<String> list = asList("first");
+      concat(list, "second");
+      assertThat(list)
+          .containsExactly("first");
+    }
   }
 
-  @Test
-  public void list_with_two_elements() throws Exception {
-    when(() -> list("abc", "def"));
-    thenReturned(contains("abc", "def"));
+  @Nested
+  class filter {
+    @Test
+    public void returns_empty_for_empty_list(){
+      assertThat(filter(new ArrayList<>(), x -> true))
+          .isEmpty();
+    }
+
+    @Test
+    public void returns_unmodified_list_when_predicate_is_always_true(){
+      assertThat(filter(asList("first", "second", "third"), x -> true))
+          .containsExactly("first", "second", "third")
+          .inOrder();
+    }
+
+    @Test
+    public void returns_empty_list_when_predicate_is_always_false(){
+      assertThat(filter(asList("first", "second", "third"), x -> false))
+          .isEmpty();
+    }
+
+    @Test
+    public void leaves_only_elements_matching_predicate(){
+      assertThat(filter(asList("first", "second", "third"), s -> s.startsWith("s")))
+          .containsExactly("second")
+          .inOrder();
+    }
   }
 
-  @Test
-  public void list_with_three_elements() throws Exception {
-    when(() -> list("abc", "def", "ghi"));
-    thenReturned(contains("abc", "def", "ghi"));
+  @Nested
+  class map {
+    @Test
+    public void returns_empty_list_for_empty_argument(){
+      assertThat(map(new ArrayList<String>(), String::toUpperCase))
+          .isEmpty();
+    }
+
+    @Test
+    public void returns_mapped_one_element(){
+      assertThat(map(asList("abc"), String::toUpperCase))
+          .containsExactly("ABC");
+    }
+
+    @Test
+    public void mapping_with_two_elements(){
+      assertThat(map(asList("abc", "def"), String::toUpperCase))
+          .containsExactly("ABC", "DEF");
+    }
   }
 
-  @Test
-  public void concat_to_empty() throws Exception {
-    given(list = new ArrayList<>());
-    when(concat(list, "element"));
-    thenReturned(asList("element"));
-  }
+  @Nested
+  class sane {
+    @Test
+    public void converts_null_to_empty_list(){
+      assertThat(sane(null))
+          .isEmpty();
+    }
 
-  @Test
-  public void concat_to_non_empty() throws Exception {
-    given(list = new ArrayList<>(asList("first")));
-    when(concat(list, "second"));
-    thenReturned(asList("first", "second"));
-  }
+    @Test
+    public void returns_empty_list_for_empty_list_argument(){
+      assertThat(sane(new ArrayList<>()))
+          .isEmpty();
+    }
 
-  @Test
-  public void concat_doesnt_modify_list() throws Exception {
-    given(list = new ArrayList<>(asList("first")));
-    when(concat(list, "second"));
-    thenEqual(list, new ArrayList<>(asList("first")));
-  }
-
-  @Test
-  public void filter_empty_returns_empty() throws Exception {
-    given(list = asList());
-    when(() -> filter(list, Predicates.alwaysTrue()));
-    thenReturned(asList());
-  }
-
-  @Test
-  public void filter_with_always_true_predicate_returns_same_list() throws Exception {
-    given(list = asList("first", "second", "third"));
-    when(() -> filter(list, Predicates.alwaysTrue()));
-    thenReturned(asList("first", "second", "third"));
-  }
-
-  @Test
-  public void filter_with_always_false_predicate_returns_empty_list() throws Exception {
-    given(list = asList("first", "second", "third"));
-    when(() -> filter(list, Predicates.alwaysFalse()));
-    thenReturned(asList());
-  }
-
-  @Test
-  public void filter_filters_elements() throws Exception {
-    given(list = asList("first", "second", "third"));
-    when(() -> filter(list, s -> s.startsWith("s")));
-    thenReturned(asList("second"));
-  }
-
-  @Test
-  public void mapping_empty_returns_empty() throws Exception {
-    given(list = asList());
-    when(() -> map(list, String::toUpperCase));
-    thenReturned(asList());
-  }
-
-  @Test
-  public void mapping_with_one_element() throws Exception {
-    given(list = asList("abc"));
-    when(() -> map(list, String::toUpperCase));
-    thenReturned(asList("ABC"));
-  }
-
-  @Test
-  public void mapping_with_two_elements() throws Exception {
-    given(list = asList("abc", "def"));
-    when(() -> map(list, String::toUpperCase));
-    thenReturned(asList("ABC", "DEF"));
-  }
-
-  @Test
-  public void sane_null_is_converted_to_empty_list() throws Exception {
-    when(() -> sane(null));
-    thenReturned(asList());
-  }
-
-  @Test
-  public void sane_empty_list_is_just_returned() throws Exception {
-    given(list = new ArrayList<>());
-    when(() -> sane(list));
-    thenReturned(same(list));
-  }
-
-  @Test
-  public void sane_non_empty_list_is_just_returned() throws Exception {
-    given(list = asList("abc", "def"));
-    when(() -> sane(list));
-    thenReturned(same(list));
+    @Test
+    public void returns_unchanged_list_when_it_has_elements(){
+      assertThat(sane(asList("abc", "def")))
+          .containsExactly("abc", "def")
+          .inOrder();
+    }
   }
 }
