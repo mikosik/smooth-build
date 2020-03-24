0d65e64c1e22944525aaf7542105db6d29553d4a
Marcin Mikosik <marcin.mikosik@gmail.com>
2020-03-24 13:49:25 +0100

    refactored ClassesTest to use Truth framework

--- src/main/test/org/smoothbuild/util/reflect/ClassesTest.java
+++ src/main/test/org/smoothbuild/util/reflect/ClassesTest.java
@@ -1,34 +1,33 @@
 package org.smoothbuild.util.reflect;
 
+import static com.google.common.truth.Truth.assertThat;
 import static org.smoothbuild.testing.common.AssertCall.assertCall;
-import static org.testory.Testory.thenReturned;
-import static org.testory.Testory.when;
 
 import org.junit.jupiter.api.Test;
 
 public class ClassesTest {
   @Test
   public void binary_path_of_top_level_class() {
-    when(Classes.binaryPath(String.class));
-    thenReturned("java/lang/String.class");
+    assertThat(Classes.binaryPath(String.class))
+        .isEqualTo("java/lang/String.class");
   }
 
   @Test
-  public void binary_path_of_inner_class() throws Exception {
-    when(Classes.binaryPath(Character.Subset.class));
-    thenReturned("java/lang/Character$Subset.class");
+  public void binary_path_of_inner_class() {
+    assertThat(Classes.binaryPath(Character.Subset.class))
+        .isEqualTo("java/lang/Character$Subset.class");
   }
 
   @Test
-  public void binary_path_to_binary_name_for_top_level_class() throws Exception {
-    when(Classes.binaryPathToBinaryName("java/lang/String.class"));
-    thenReturned("java.lang.String");
+  public void binary_path_to_binary_name_for_top_level_class() {
+    assertThat(Classes.binaryPathToBinaryName("java/lang/String.class"))
+        .isEqualTo("java.lang.String");
   }
 
   @Test
-  public void binary_path_to_binary_name_for_inner_class() throws Exception {
-    when(Classes.binaryPathToBinaryName("java/lang/Character$Subset.class"));
-    thenReturned("java.lang.Character$Subset");
+  public void binary_path_to_binary_name_for_inner_class() {
+    assertThat(Classes.binaryPathToBinaryName("java/lang/Character$Subset.class"))
+        .isEqualTo("java.lang.Character$Subset");
   }
 
   @Test
