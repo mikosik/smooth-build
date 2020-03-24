b2bd940551d1648a7b2c6f8fee38726fcb11f383
Marcin Mikosik <marcin.mikosik@gmail.com>
2020-03-24 13:45:25 +0100

    refactored DependencyStackTest to use Truth framework

--- src/main/test/org/smoothbuild/parse/deps/DependencyStackTest.java
+++ src/main/test/org/smoothbuild/parse/deps/DependencyStackTest.java
@@ -1,12 +1,8 @@
 package org.smoothbuild.parse.deps;
 
-import static org.hamcrest.core.IsSame.sameInstance;
+import static com.google.common.truth.Truth.assertThat;
 import static org.smoothbuild.lang.base.Location.location;
-import static org.testory.Testory.given;
-import static org.testory.Testory.thenEqual;
-import static org.testory.Testory.thenReturned;
-import static org.testory.Testory.thenThrown;
-import static org.testory.Testory.when;
+import static org.smoothbuild.testing.common.AssertCall.assertCall;
 
 import java.nio.file.Paths;
 import java.util.NoSuchElementException;
@@ -18,136 +14,131 @@ import org.smoothbuild.parse.ast.NamedNode;
 import com.google.common.collect.ImmutableSet;
 
 public class DependencyStackTest {
-  private final String stackName = "My Stack";
-  private DependencyStack dependencyStack;
-  private StackElem elem1;
-  private StackElem elem2;
-  private StackElem elem3;
-
   @Test
   public void stack_is_empty_initially() {
-    given(dependencyStack = new DependencyStack(stackName));
-    when(dependencyStack).isEmpty();
-    thenReturned(true);
+    DependencyStack dependencyStack = new DependencyStack("my stack");
+    assertThat(dependencyStack.isEmpty())
+        .isTrue();
   }
 
   @Test
-  public void stack_is_not_empty_after_pushing_one_element() throws Exception {
-    given(dependencyStack = new DependencyStack(stackName));
-    given(dependencyStack).push(elem());
-    when(dependencyStack).isEmpty();
-    thenReturned(false);
+  public void stack_is_not_empty_after_pushing_one_element() {
+    DependencyStack dependencyStack = new DependencyStack("my stack");
+    dependencyStack.push(elem());
+    assertThat(dependencyStack.isEmpty())
+        .isFalse();
   }
 
   @Test
   public void stack_is_empty_after_pushing_and_popping() {
-    given(dependencyStack = new DependencyStack(stackName));
-    given(dependencyStack).push(elem());
-    given(dependencyStack).pop();
-    when(dependencyStack).isEmpty();
-    thenReturned(true);
+    DependencyStack dependencyStack = new DependencyStack("my stack");
+    dependencyStack.push(elem());
+    dependencyStack.pop();
+    assertThat(dependencyStack.isEmpty())
+        .isTrue();
   }
 
   @Test
-  public void pushed_element_is_returned_by_pop() throws Exception {
-    given(dependencyStack = new DependencyStack(stackName));
-    given(elem1 = elem());
-    given(dependencyStack).push(elem1);
-    when(dependencyStack).pop();
-    thenReturned(elem1);
+  public void pushed_element_is_returned_by_pop() {
+    DependencyStack dependencyStack = new DependencyStack("my stack");
+    StackElem elem = elem();
+    dependencyStack.push(elem);
+    assertThat(dependencyStack.pop())
+        .isEqualTo(elem);
   }
 
   @Test
-  public void elements_are_poped_in_filo_order() throws Exception {
-    given(dependencyStack = new DependencyStack(stackName));
-    given(elem1 = elem());
-    given(elem2 = elem());
-    given(elem3 = elem());
-    given(dependencyStack).push(elem1);
-    given(dependencyStack).push(elem2);
-    given(dependencyStack).push(elem3);
-    when(dependencyStack).pop();
-    thenReturned(sameInstance(elem3));
-    when(dependencyStack).pop();
-    thenReturned(sameInstance(elem2));
-    when(dependencyStack).pop();
-    thenReturned(sameInstance(elem1));
+  public void elements_are_poped_in_filo_order() {
+    DependencyStack dependencyStack = new DependencyStack("my stack");
+    StackElem elem1 = elem();
+    StackElem elem2 = elem();
+    StackElem elem3 = elem();
+    dependencyStack.push(elem1);
+    dependencyStack.push(elem2);
+    dependencyStack.push(elem3);
+    assertThat(dependencyStack.pop())
+        .isEqualTo(elem3);
+    assertThat(dependencyStack.pop())
+        .isEqualTo(elem2);
+    assertThat(dependencyStack.pop())
+        .isEqualTo(elem1);
   }
 
   @Test
-  public void poping_from_empty_stack_throws_exception() throws Exception {
-    given(dependencyStack = new DependencyStack(stackName));
-    when(dependencyStack).pop();
-    thenThrown(NoSuchElementException.class);
+  public void poping_from_empty_stack_throws_exception() {
+    DependencyStack dependencyStack = new DependencyStack("my stack");
+    assertCall(() -> dependencyStack.pop())
+        .throwsException(NoSuchElementException.class);
   }
 
   @Test
-  public void poping_after_all_elements_has_been_removed_throws_exception() throws Exception {
-    given(dependencyStack = new DependencyStack(stackName));
-    given(dependencyStack).push(elem());
-    given(dependencyStack).pop();
-    when(dependencyStack).pop();
-    thenThrown(NoSuchElementException.class);
+  public void poping_after_all_elements_has_been_removed_throws_exception() {
+    DependencyStack dependencyStack = new DependencyStack("my stack");
+    dependencyStack.push(elem());
+    dependencyStack.pop();
+    assertCall(() -> dependencyStack.pop())
+        .throwsException(NoSuchElementException.class);
   }
 
   @Test
-  public void peek_returns_top_element() throws Exception {
-    given(dependencyStack = new DependencyStack(stackName));
-    given(dependencyStack).push(elem());
-    given(elem2 = elem());
-    given(dependencyStack).push(elem2);
-    when(dependencyStack).peek();
-    thenReturned(sameInstance(elem2));
+  public void peek_returns_top_element() {
+    DependencyStack dependencyStack = new DependencyStack("my stack");
+    dependencyStack.push(elem());
+    StackElem elem2 = elem();
+    dependencyStack.push(elem2);
+    assertThat(dependencyStack.peek())
+        .isSameInstanceAs(elem2);
   }
 
   @Test
-  public void peek_does_not_remove_element() throws Exception {
-    given(dependencyStack = new DependencyStack(stackName));
-    given(elem1 = elem());
-    given(dependencyStack).push(elem1);
-    when(dependencyStack).peek();
-    thenEqual(dependencyStack.pop(), elem1);
+  public void peek_does_not_remove_element() {
+    DependencyStack dependencyStack = new DependencyStack("my stack");
+    StackElem elem1 = elem();
+    dependencyStack.push(elem1);
+    dependencyStack.peek();
+    assertThat(dependencyStack.pop())
+        .isSameInstanceAs(elem1);
   }
 
   @Test
-  public void peeking_on_empty_stack_throws_exception() throws Exception {
-    given(dependencyStack = new DependencyStack(stackName));
-    when(dependencyStack).peek();
-    thenThrown(NoSuchElementException.class);
+  public void peeking_on_empty_stack_throws_exception() {
+    DependencyStack dependencyStack = new DependencyStack("my stack");
+    assertCall(dependencyStack::peek)
+        .throwsException(NoSuchElementException.class);
   }
 
   @Test
-  public void peeking_after_all_elements_has_been_removed_throws_exception() throws Exception {
-    given(dependencyStack = new DependencyStack(stackName));
-    given(dependencyStack).push(elem());
-    given(dependencyStack).pop();
-    when(dependencyStack).peek();
-    thenThrown(NoSuchElementException.class);
+  public void peeking_after_all_elements_has_been_removed_throws_exception() {
+    DependencyStack dependencyStack = new DependencyStack("my stack");
+    dependencyStack.push(elem());
+    dependencyStack.pop();
+    assertCall(dependencyStack::peek)
+        .throwsException(NoSuchElementException.class);
   }
 
   @Test
-  public void create_cycle_error() throws Exception {
-    given(dependencyStack = new DependencyStack(stackName));
-    given(dependencyStack).push(elem("name1", "name2", 1));
-    given(dependencyStack).push(elem("name2", "name3", 2));
-    given(dependencyStack).push(elem("name3", "name4", 3));
-    given(dependencyStack).push(elem("name4", "name2", 4));
-    when(() -> dependencyStack.createCycleError().toString());
-    thenReturned(new ParseError(location(Paths.get("script.smooth"), 2),
-        "My Stack contains cycle:\n"
+  public void create_cycle_error() {
+    DependencyStack dependencyStack = new DependencyStack("my stack");
+    dependencyStack.push(elem("name1", "name2", 1));
+    dependencyStack.push(elem("name2", "name3", 2));
+    dependencyStack.push(elem("name3", "name4", 3));
+    dependencyStack.push(elem("name4", "name2", 4));
+    assertThat(dependencyStack.createCycleError().toString())
+        .isEqualTo(new ParseError(location(Paths.get("script.smooth"), 2),
+        "my stack contains cycle:\n"
             + "script.smooth:2: name2 -> name3\n"
             + "script.smooth:3: name3 -> name4\n"
             + "script.smooth:4: name4 -> name2\n").toString());
   }
 
   @Test
-  public void create_cycle_error_for_recursive_call() throws Exception {
-    given(dependencyStack = new DependencyStack(stackName));
-    given(dependencyStack).push(elem("name1", "name2", 1));
-    given(dependencyStack).push(elem("name2", "name2", 2));
-    when(() -> dependencyStack.createCycleError().toString());
-    thenReturned(new ParseError(location(Paths.get("script.smooth"), 2),
-        "My Stack contains cycle:\n"
+  public void create_cycle_error_for_recursive_call() {
+    DependencyStack dependencyStack = new DependencyStack("my stack");
+    dependencyStack.push(elem("name1", "name2", 1));
+    dependencyStack.push(elem("name2", "name2", 2));
+    assertThat(dependencyStack.createCycleError().toString())
+        .isEqualTo(new ParseError(location(Paths.get("script.smooth"), 2),
+        "my stack contains cycle:\n"
             + "script.smooth:2: name2 -> name2\n").toString());
   }
 
