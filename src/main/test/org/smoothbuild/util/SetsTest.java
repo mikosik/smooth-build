5d299d1185debce6e627bdad1a0e62a490b5f2c7
Marcin Mikosik <marcin.mikosik@gmail.com>
2020-03-25 10:51:43 +0100

    refactored SetsTest to use Truth framework

--- src/main/test/org/smoothbuild/util/SetsTest.java
+++ src/main/test/org/smoothbuild/util/SetsTest.java
@@ -1,97 +1,89 @@
 package org.smoothbuild.util;
 
+import static com.google.common.truth.Truth.assertThat;
 import static org.smoothbuild.util.Sets.filter;
 import static org.smoothbuild.util.Sets.map;
-import static org.testory.Testory.given;
-import static org.testory.Testory.thenReturned;
-import static org.testory.Testory.when;
+import static org.smoothbuild.util.Sets.set;
 
 import java.util.HashSet;
-import java.util.Set;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-import com.google.common.base.Predicates;
-import com.google.common.collect.ImmutableSet;
-
+@SuppressWarnings("ClassCanBeStatic")
 public class SetsTest {
-  private Set<String> set;
+  @Nested
+  class create {
+    @Test
+    public void set_with_no_elements() {
+      assertThat(set())
+          .isEmpty();
+    }
 
-  @Test
-  public void set_with_no_elements() throws Exception {
-    when(() -> set());
-    thenReturned(new HashSet<>());
+    @Test
+    public void set_with_one_element() {
+      assertThat(set("abc"))
+          .containsExactly("abc");
+    }
+
+    @Test
+    public void set_with_two_elements() {
+      assertThat(set("abc", "def"))
+          .containsExactly("abc", "def");
+    }
+
+    @Test
+    public void set_with_three_elements() {
+      assertThat(set("abc", "def", "ghi"))
+          .containsExactly("abc", "def", "ghi");
+    }
   }
 
-  @Test
-  public void set_with_one_element() throws Exception {
-    when(() -> set("abc"));
-    thenReturned(ImmutableSet.of("abc"));
+  @Nested
+  class map {
+    @Test
+    public void returns_empty_for_empty_argument() {
+      assertThat(map(new HashSet<String>(), String::toUpperCase))
+          .isEmpty();
+    }
+
+    @Test
+    public void one_element() {
+      assertThat(map(set("abc"), String::toUpperCase))
+          .containsExactly("ABC");
+    }
+
+    @Test
+    public void two_elements() {
+      assertThat(map(set("abc", "def"), String::toUpperCase))
+          .containsExactly("ABC", "DEF");
+    }
   }
 
-  @Test
-  public void set_with_two_elements() throws Exception {
-    when(() -> set("abc", "def"));
-    thenReturned(ImmutableSet.of("abc", "def"));
-  }
+  @Nested
+  class filter {
+    @Test
+    public void returns_empty_for_empty_argument() {
+      assertThat(filter(new HashSet<>(), e -> true))
+          .isEmpty();
+    }
 
-  @Test
-  public void set_with_three_elements() throws Exception {
-    when(() -> set("abc", "def", "ghi"));
-    thenReturned(ImmutableSet.of("abc", "def", "ghi"));
-  }
+    @Test
+    public void with_always_true_predicate_returns_unchanged_list() {
+      assertThat(filter(set("first", "second", "third"), s -> true))
+          .containsExactly("first", "second", "third");
+    }
 
-  @Test
-  public void mapping_empty_returns_empty() throws Exception {
-    given(set = new HashSet<>());
-    when(() -> map(set, String::toUpperCase));
-    thenReturned(new HashSet<>());
-  }
+    @Test
+    public void with_always_false_predicate_returns_empty_list() {
+      assertThat(filter(set("first", "second", "third"), s -> false))
+          .isEmpty();
+    }
 
-  @Test
-  public void mapping_with_one_element() throws Exception {
-    given(set = set("abc"));
-    when(() -> map(set, String::toUpperCase));
-    thenReturned(set("ABC"));
-  }
-
-  @Test
-  public void mapping_with_two_elements() throws Exception {
-    given(set = set("abc", "def"));
-    when(() -> map(set, String::toUpperCase));
-    thenReturned(set("ABC", "DEF"));
-  }
-
-  @Test
-  public void filter_empty_returns_empty() throws Exception {
-    given(set = set());
-    when(() -> filter(set, Predicates.alwaysTrue()));
-    thenReturned(set());
-  }
-
-  @Test
-  public void filter_with_always_true_predicate_returns_same_list() throws Exception {
-    given(set = set("first", "second", "third"));
-    when(() -> filter(set, Predicates.alwaysTrue()));
-    thenReturned(set("first", "second", "third"));
-  }
-
-  @Test
-  public void filter_with_always_false_predicate_returns_empty_list() throws Exception {
-    given(set = set("first", "second", "third"));
-    when(() -> filter(set, Predicates.alwaysFalse()));
-    thenReturned(set());
-  }
-
-  @Test
-  public void filter_filters_elements() throws Exception {
-    given(set = set("first", "second", "third"));
-    when(() -> filter(set, s -> s.startsWith("s")));
-    thenReturned(set("second"));
-  }
-
-  @SafeVarargs
-  private static <T> Set<T> set(T... elements) {
-    return com.google.common.collect.Sets.newHashSet(elements);
+    @Test
+    public void filter_elements_matching_predicate() {
+      assertThat(filter(set("first", "second", "third"), s -> s.startsWith("s")))
+          .containsExactly("second");
+    }
   }
 }
