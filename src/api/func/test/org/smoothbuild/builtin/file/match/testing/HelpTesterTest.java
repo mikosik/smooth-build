cadcb35ce7865a02feacc4871794a6e6b801315f
Marcin Mikosik <marcin.mikosik@gmail.com>
2020-03-26 16:58:46 +0100

    refactored HelpTesterTest to use Truth framework

--- src/api/func/test/org/smoothbuild/builtin/file/match/testing/HelpTesterTest.java
+++ src/api/func/test/org/smoothbuild/builtin/file/match/testing/HelpTesterTest.java
@@ -1,39 +1,38 @@
 package org.smoothbuild.builtin.file.match.testing;
 
+import static com.google.common.truth.Truth.assertThat;
 import static org.smoothbuild.builtin.file.match.testing.HelpTester.endsWithThreeLetters;
-import static org.testory.Testory.thenReturned;
-import static org.testory.Testory.when;
 
 import org.junit.jupiter.api.Test;
 
 public class HelpTesterTest {
   @Test
   public void empty_string_does_not_end_with_three_letters() {
-    when(endsWithThreeLetters(""));
-    thenReturned(false);
+    assertThat(endsWithThreeLetters(""))
+        .isFalse();
   }
 
   @Test
   public void two_letters_string_does_not_end_with_three_letters() {
-    when(endsWithThreeLetters("ab"));
-    thenReturned(false);
+    assertThat(endsWithThreeLetters("ab"))
+        .isFalse();
   }
 
   @Test
   public void three_letters_string_ends_with_three_letters() {
-    when(endsWithThreeLetters("abc"));
-    thenReturned(true);
+    assertThat(endsWithThreeLetters("abc"))
+        .isTrue();
   }
 
   @Test
   public void long_word_ends_with_three_letters() {
-    when(endsWithThreeLetters("abcdefghi"));
-    thenReturned(true);
+    assertThat(endsWithThreeLetters("abcdefghi"))
+        .isTrue();
   }
 
   @Test
   public void long_word_with_digit_at_the_end_does_not_end_with_three_letters() {
-    when(endsWithThreeLetters("abcdefghi1"));
-    thenReturned(false);
+    assertThat(endsWithThreeLetters("abcdefghi1"))
+        .isFalse();
   }
 }
