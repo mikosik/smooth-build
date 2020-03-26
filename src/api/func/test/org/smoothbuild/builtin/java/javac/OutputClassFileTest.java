b61b5ab4b85c8f3349c700cc1e560147ad1f26b8
Marcin Mikosik <marcin.mikosik@gmail.com>
2020-03-26 16:41:21 +0100

    refactored OutputClassFileTest to use Truth framework

--- src/api/func/test/org/smoothbuild/builtin/java/javac/OutputClassFileTest.java
+++ src/api/func/test/org/smoothbuild/builtin/java/javac/OutputClassFileTest.java
@@ -1,11 +1,8 @@
 package org.smoothbuild.builtin.java.javac;
 
+import static com.google.common.truth.Truth.assertThat;
 import static okio.Okio.buffer;
 import static okio.Okio.sink;
-import static org.hamcrest.Matchers.contains;
-import static org.testory.Testory.given;
-import static org.testory.Testory.thenReturned;
-import static org.testory.Testory.when;
 
 import java.io.IOException;
 
@@ -22,24 +19,22 @@ public class OutputClassFileTest extends TestingContext {
   private final Path path = Path.path("my/path");
   private final ByteString bytes = ByteString.encodeUtf8("abc");
 
-  private ArrayBuilder fileArrayBuilder;
-  private OutputClassFile outputClassFile;
-
   @Test
   public void open_output_stream() throws IOException {
-    given(fileArrayBuilder = arrayBuilder(nativeApi().factory().fileType()));
-    given(outputClassFile = new OutputClassFile(fileArrayBuilder, path, nativeApi()));
+    ArrayBuilder fileArrayBuilder = arrayBuilder(nativeApi().factory().fileType());
+    OutputClassFile outputClassFile = new OutputClassFile(fileArrayBuilder, path, nativeApi());
     try (BufferedSink sink = buffer(sink(outputClassFile.openOutputStream()))) {
       sink.write(bytes);
     }
-    when(() -> fileArrayBuilder.build().asIterable(Struct.class));
-    thenReturned(contains(file(path, bytes)));
+    assertThat(fileArrayBuilder.build().asIterable(Struct.class))
+        .containsExactly(file(path, bytes));
   }
 
   @Test
-  public void get_name_returns_file_path() throws Exception {
-    given(outputClassFile = new OutputClassFile(arrayBuilder(fileType()), path, nativeApi()));
-    when(outputClassFile.getName());
-    thenReturned("/" + path.value());
+  public void get_name_returns_file_path() {
+    OutputClassFile outputClassFile =
+        new OutputClassFile(arrayBuilder(fileType()), path, nativeApi());
+    assertThat(outputClassFile.getName())
+        .isEqualTo("/" + path.value());
   }
 }
