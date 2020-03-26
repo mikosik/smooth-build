b43acd6914db94b3c0478795deae6905c5159e2e
Marcin Mikosik <marcin.mikosik@gmail.com>
2020-03-26 16:36:28 +0100

    refactored FileClassLoaderTest to use Truth framework

--- src/api/func/test/org/smoothbuild/builtin/java/junit/FileClassLoaderTest.java
+++ src/api/func/test/org/smoothbuild/builtin/java/junit/FileClassLoaderTest.java
@@ -1,14 +1,10 @@
 package org.smoothbuild.builtin.java.junit;
 
+import static com.google.common.truth.Truth.assertThat;
 import static org.smoothbuild.io.fs.base.Path.path;
 import static org.smoothbuild.util.reflect.Classes.binaryPath;
-import static org.testory.Testory.given;
-import static org.testory.Testory.thenReturned;
-import static org.testory.Testory.when;
-import static org.testory.common.Matchers.same;
 
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.Map;
 
 import org.junit.jupiter.api.Test;
@@ -17,23 +13,13 @@ import org.smoothbuild.testing.TestingContext;
 import org.smoothbuild.util.reflect.Classes;
 
 public class FileClassLoaderTest extends TestingContext {
-  private FileClassLoader fileClassLoader;
-  private Struct file;
-  private Class<MyClass> klass;
-
   @Test
   public void loads_class_from_binary() throws Exception {
-    given(klass = FileClassLoaderTest.MyClass.class);
-    given(file = createByteCodeFile(klass));
-    given(fileClassLoader = new FileClassLoader(map(klass.getName(), file)));
-    when(fileClassLoader.findClass(klass.getName()).getClassLoader());
-    thenReturned(same(fileClassLoader));
-  }
-
-  private static Map<String, Struct> map(String name, Struct file) {
-    HashMap<String, Struct> result = new HashMap<>();
-    result.put(name, file);
-    return result;
+    Class<MyClass> klass = MyClass.class;
+    Struct file = createByteCodeFile(klass);
+    FileClassLoader fileClassLoader = new FileClassLoader(Map.of(klass.getName(), file));
+    assertThat(fileClassLoader.findClass(klass.getName()).getClassLoader())
+        .isSameInstanceAs(fileClassLoader);
   }
 
   private Struct createByteCodeFile(Class<?> klass) throws IOException {
