301e8223ace4d56392c4b9dd56c8dbf12cfe65f4
Marcin Mikosik <marcin.mikosik@gmail.com>
2020-03-26 16:51:44 +0100

    refactored InputClassFileTest to use Truth framework

--- src/api/func/test/org/smoothbuild/builtin/java/javac/InputClassFileTest.java
+++ src/api/func/test/org/smoothbuild/builtin/java/javac/InputClassFileTest.java
@@ -1,13 +1,11 @@
 package org.smoothbuild.builtin.java.javac;
 
+import static com.google.common.truth.Truth.assertThat;
 import static okio.Okio.buffer;
 import static okio.Okio.source;
-import static org.hamcrest.Matchers.not;
 import static org.smoothbuild.io.fs.base.Path.path;
+import static org.smoothbuild.testing.common.AssertCall.assertCall;
 import static org.smoothbuild.util.Okios.readAndClose;
-import static org.testory.Testory.given;
-import static org.testory.Testory.thenReturned;
-import static org.testory.Testory.when;
 
 import java.net.URI;
 
@@ -15,95 +13,89 @@ import org.junit.jupiter.api.Test;
 import org.smoothbuild.io.fs.base.Path;
 import org.smoothbuild.lang.object.base.Struct;
 import org.smoothbuild.testing.TestingContext;
-import org.smoothbuild.testing.common.AssertCall;
 
+import okio.BufferedSource;
 import okio.ByteString;
 
 public class InputClassFileTest extends TestingContext {
   private final ByteString bytes = ByteString.encodeUtf8("abc");
-  private InputClassFile inputClassFile;
   private final Path path = path("a/b/MyClass.class");
-  private Struct file;
 
   @Test
-  public void file_without_class_extension_is_forbidden() throws Exception {
-    AssertCall.assertCall(() -> inputClassFile("abc"))
+  public void file_without_class_extension_is_forbidden() {
+    assertCall(() -> new InputClassFile(file(path("abc"))))
         .throwsException(IllegalArgumentException.class);
   }
 
   @Test
-  public void input_class_files_with_equal_paths_but_different_content_are_equal()
-      throws Exception {
-    when(new InputClassFile(file(path, ByteString.encodeUtf8("abc"))));
-    thenReturned(new InputClassFile(file(path, ByteString.encodeUtf8("def"))));
+  public void input_class_files_with_equal_paths_but_different_content_are_equal() {
+    assertThat(new InputClassFile(file(path, ByteString.encodeUtf8("abc"))))
+        .isEqualTo(new InputClassFile(file(path, ByteString.encodeUtf8("def"))));
   }
 
   @Test
-  public void input_class_files_with_different_paths_are_not_equal() throws Exception {
-    when(new InputClassFile(file(path("a/b/MyClass.class"), bytes)));
-    thenReturned(not(new InputClassFile(file(path("a/b/OtherClass.class"), bytes))));
+  public void input_class_files_with_different_paths_are_not_equal() {
+    assertThat(new InputClassFile(file(path("a/b/MyClass.class"), bytes)))
+        .isNotEqualTo(new InputClassFile(file(path("a/b/OtherClass.class"), bytes)));
   }
 
   @Test
-  public void class_in_default_package_has_empty_package() throws Exception {
-    given(inputClassFile = new InputClassFile(file(path("Myclass.class"))));
-    when(inputClassFile).aPackage();
-    thenReturned("");
+  public void class_in_default_package_has_empty_package() {
+    InputClassFile inputClassFile = new InputClassFile(file(path("Myclass.class")));
+    assertThat(inputClassFile.aPackage())
+        .isEqualTo("");
   }
 
   @Test
-  public void aPackage_returns_class_package() throws Exception {
-    given(inputClassFile = new InputClassFile(file(path("a/b/Myclass.class"))));
-    when(inputClassFile).aPackage();
-    thenReturned("a.b");
+  public void aPackage_returns_class_package() {
+    InputClassFile inputClassFile = new InputClassFile(file(path("a/b/Myclass.class")));
+    assertThat(inputClassFile.aPackage())
+        .isEqualTo("a.b");
   }
 
   @Test
-  public void aPackage_for_inner_class_returns_package_of_enclosing_class() throws Exception {
-    given(inputClassFile = new InputClassFile(file(path("a/b/MyClass$Inner.class"))));
-    when(inputClassFile).aPackage();
-    thenReturned("a.b");
+  public void aPackage_for_inner_class_returns_package_of_enclosing_class() {
+    InputClassFile inputClassFile = new InputClassFile(file(path("a/b/MyClass$Inner.class")));
+    assertThat(inputClassFile.aPackage())
+        .isEqualTo("a.b");
   }
 
   @Test
-  public void binary_name_for_default_class_returns_class_name() throws Exception {
-    given(inputClassFile = new InputClassFile(file(path("MyClass.class"))));
-    when(inputClassFile).binaryName();
-    thenReturned("MyClass");
+  public void binary_name_for_default_class_returns_class_name() {
+    InputClassFile inputClassFile = new InputClassFile(file(path("MyClass.class")));
+    assertThat(inputClassFile.binaryName())
+        .isEqualTo("MyClass");
   }
 
   @Test
-  public void binary_name_for_class_returns_package_plus_class_name() throws Exception {
-    given(inputClassFile = new InputClassFile(file(path("a/b/MyClass.class"))));
-    when(inputClassFile).binaryName();
-    thenReturned("a.b.MyClass");
+  public void binary_name_for_class_returns_package_plus_class_name() {
+    InputClassFile inputClassFile = new InputClassFile(file(path("a/b/MyClass.class")));
+    assertThat(inputClassFile.binaryName())
+        .isEqualTo("a.b.MyClass");
   }
 
   @Test
   public void binary_name_for_inner_class_returns_package_plus_outer_class_plus_inner_class_name()
-      throws Exception {
-    given(inputClassFile = new InputClassFile(file(path("a/b/MyClass$Inner.class"))));
-    when(inputClassFile).binaryName();
-    thenReturned("a.b.MyClass$Inner");
+      {
+    InputClassFile inputClassFile = new InputClassFile(file(path("a/b/MyClass$Inner.class")));
+    assertThat(inputClassFile.binaryName())
+        .isEqualTo("a.b.MyClass$Inner");
   }
 
   @Test
-  public void to_uri() throws Exception {
-    given(inputClassFile = new InputClassFile(file(path("a/b/MyClass.class"))));
-    when(inputClassFile).toUri();
-    thenReturned(URI.create("jar:///:a/b/MyClass.class"));
+  public void to_uri() {
+    InputClassFile inputClassFile = new InputClassFile(file(path("a/b/MyClass.class")));
+    assertThat(inputClassFile.toUri())
+        .isEqualTo(URI.create("jar:///:a/b/MyClass.class"));
   }
 
   @Test
   public void open_input_stream_returns_file_content() throws Exception {
-    given(file = file(path, bytes));
-    given(inputClassFile = new InputClassFile(file));
-    when(() -> readAndClose(buffer(source(inputClassFile.openInputStream())),
-        s -> s.readByteString()));
-    thenReturned(bytes);
-  }
-
-  private InputClassFile inputClassFile(String path) {
-    return new InputClassFile(file(path(path)));
+    Struct file = file(path, bytes);
+    InputClassFile inputClassFile = new InputClassFile(file);
+    BufferedSource buffer = buffer(source(inputClassFile.openInputStream()));
+    ByteString byteString = readAndClose(buffer, BufferedSource::readByteString);
+    assertThat(byteString)
+        .isEqualTo(bytes);
   }
 }
