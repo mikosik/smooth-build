19ebf81d959037c542e9b503e3a45c78cce91507
Marcin Mikosik <marcin.mikosik@gmail.com>
2020-03-26 12:11:53 +0100

    refactored NamePatternGeneratorTest to use Truth framework

--- src/api/func/test/org/smoothbuild/builtin/file/match/testing/NamePatternGeneratorTest.java
+++ src/api/func/test/org/smoothbuild/builtin/file/match/testing/NamePatternGeneratorTest.java
@@ -1,8 +1,7 @@
 package org.smoothbuild.builtin.file.match.testing;
 
-import static org.hamcrest.Matchers.containsInAnyOrder;
+import static com.google.common.truth.Truth.assertThat;
 import static org.smoothbuild.builtin.file.match.testing.NamePatternGenerator.generatePatternsImpl;
-import static org.testory.Testory.then;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,8 +12,9 @@ public class NamePatternGeneratorTest {
   @Test
   public void all_possible_patterns_are_generated() {
     final List<String> generatedPatterns = new ArrayList<>();
-    generatePatternsImpl(3, (pattern) -> generatedPatterns.add(pattern));
-    then(generatedPatterns, containsInAnyOrder("aaa", "aab", "aa*", "aba", "abb", "abc", "ab*",
-        "a*a", "a*b", "*aa", "*ab", "*a*"));
+    generatePatternsImpl(3, generatedPatterns::add);
+    assertThat(generatedPatterns)
+        .containsExactly("aaa", "aab", "aa*", "aba", "abb", "abc", "ab*", "a*a", "a*b", "*aa",
+            "*ab", "*a*");
   }
 }
