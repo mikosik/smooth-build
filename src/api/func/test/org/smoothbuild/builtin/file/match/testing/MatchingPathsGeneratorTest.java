7f592f809fec7ecec41f251ae438aff825b573d8
Marcin Mikosik <marcin.mikosik@gmail.com>
2020-03-26 12:10:14 +0100

    refactored MatchingPathsGeneratorTest to use Truth framework

--- src/api/func/test/org/smoothbuild/builtin/file/match/testing/MatchingPathsGeneratorTest.java
+++ src/api/func/test/org/smoothbuild/builtin/file/match/testing/MatchingPathsGeneratorTest.java
@@ -1,8 +1,7 @@
 package org.smoothbuild.builtin.file.match.testing;
 
-import static org.hamcrest.Matchers.containsInAnyOrder;
+import static com.google.common.truth.Truth.assertThat;
 import static org.smoothbuild.builtin.file.match.testing.MatchingPathsGenerator.generatePaths;
-import static org.testory.Testory.then;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,18 +13,19 @@ public class MatchingPathsGeneratorTest {
   public void single_star_set_for_whole_name() {
     // given
     final List<String> generatedPaths = new ArrayList<>();
-    generatePaths("x/*/z", (string) -> generatedPaths.add(string));
-    then(generatedPaths, containsInAnyOrder("x/a/z", "x/b/z", "x/c/z", "x/aa/z", "x/ab/z",
-        "x/ac/z", "x/ba/z", "x/bb/z", "x/bc/z", "x/ca/z", "x/cb/z", "x/cc/z"));
+    generatePaths("x/*/z", generatedPaths::add);
+    assertThat(generatedPaths)
+        .containsExactly("x/a/z", "x/b/z", "x/c/z", "x/aa/z", "x/ab/z", "x/ac/z", "x/ba/z",
+            "x/bb/z", "x/bc/z", "x/ca/z", "x/cb/z", "x/cc/z");
   }
 
   @Test
   public void single_star_set_as_part_of_name() {
     // given
     final List<String> generatedPaths = new ArrayList<>();
-    generatePaths("x/1*9/z", (string) -> generatedPaths.add(string));
-    then(generatedPaths, containsInAnyOrder("x/19/z", "x/1a9/z", "x/1b9/z", "x/1c9/z", "x/1aa9/z",
-        "x/1ab9/z", "x/1ac9/z", "x/1ba9/z", "x/1bb9/z", "x/1bc9/z", "x/1ca9/z", "x/1cb9/z",
-        "x/1cc9/z"));
+    generatePaths("x/1*9/z", generatedPaths::add);
+    assertThat(generatedPaths)
+        .containsExactly("x/19/z", "x/1a9/z", "x/1b9/z", "x/1c9/z", "x/1aa9/z", "x/1ab9/z",
+            "x/1ac9/z", "x/1ba9/z", "x/1bb9/z", "x/1bc9/z", "x/1ca9/z", "x/1cb9/z", "x/1cc9/z");
   }
 }
