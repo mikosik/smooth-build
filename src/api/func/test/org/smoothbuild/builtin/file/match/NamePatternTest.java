7b1646446e7611ba9b84f71f463b388bf95b6e78
Marcin Mikosik <marcin.mikosik@gmail.com>
2020-03-27 07:04:51 +0100

    refactored NamePatternTest to use Truth framework

--- src/api/func/test/org/smoothbuild/builtin/file/match/NamePatternTest.java
+++ src/api/func/test/org/smoothbuild/builtin/file/match/NamePatternTest.java
@@ -1,157 +1,147 @@
 package org.smoothbuild.builtin.file.match;
 
+import static com.google.common.truth.Truth.assertThat;
 import static org.smoothbuild.builtin.file.match.Constants.DOUBLE_STAR;
 import static org.smoothbuild.builtin.file.match.NamePattern.namePattern;
-import static org.smoothbuild.util.Lists.list;
-import static org.testory.Testory.given;
-import static org.testory.Testory.thenReturned;
-import static org.testory.Testory.thenThrown;
-import static org.testory.Testory.when;
+import static org.smoothbuild.testing.common.AssertCall.assertCall;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+@SuppressWarnings("ClassCanBeStatic")
 public class NamePatternTest {
-  private NamePattern pattern;
+  @Nested
+  class creating_pattern {
+    @Test
+    public void null_is_forbidden() {
+      assertCall(() -> namePattern(null))
+          .throwsException(NullPointerException.class);
+    }
 
-  @Test
-  public void null_pattern_is_forbidden() {
-    when(() -> namePattern(null));
-    thenThrown(NullPointerException.class);
+    @Test
+    public void with_slash_is_forbidden() {
+      assertCall(() -> namePattern("abc/def"))
+          .throwsException(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void for_empty_string_is_forbidden() {
+      assertCall(() -> namePattern(""))
+          .throwsException(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void double_star_pattern_is_allowed() {
+      namePattern(DOUBLE_STAR);
+    }
+
+    @Test
+    public void double_star_with_suffix_is_not_allowed() {
+      assertCall(() -> namePattern(DOUBLE_STAR + "a"))
+          .throwsException(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void double_star_with_prefix_is_not_allowed() {
+      assertCall(() -> namePattern("a" + DOUBLE_STAR))
+          .throwsException(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void three_star_pattern_is_not_allowed() {
+      assertCall(() -> namePattern("***"))
+          .throwsException(IllegalArgumentException.class);
+    }
   }
 
-  @Test
-  public void slash_in_pattern_is_forbidden() {
-    when(() -> namePattern("abc/def"));
-    thenThrown(IllegalArgumentException.class);
+  @Nested
+  class hasStars {
+    @Test
+    public void returns_false_for_pattern_that_contains_no_stars() {
+      assertThat(namePattern("abcdef").hasStars())
+          .isFalse();
+    }
+
+    @Test
+    public void returns_true_for_one_star_pattern() {
+      assertThat(namePattern("*").hasStars())
+          .isTrue();
+    }
+
+    @Test
+    public void returns_true_for_pattern_that_contains_one_star() {
+      assertThat(namePattern("abc*def").hasStars())
+          .isTrue();
+    }
+
+    @Test
+    public void returns_true_for_pattern_that_contains_two_stars() {
+      assertThat(namePattern("abc*def*ghi").hasStars())
+          .isTrue();
+    }
   }
 
-  @Test
-  public void empty_pattern_is_forbidden() throws Exception {
-    when(() -> namePattern(""));
-    thenThrown(IllegalArgumentException.class);
+  @Nested
+  class isSingleStar {
+    @Test
+    public void returns_false_for_pattern_that_contains_one_star() {
+      assertThat(namePattern("*abc").isSingleStar())
+          .isFalse();
+    }
+
+    @Test
+    public void returns_true_for_one_star_pattern() {
+      assertThat(namePattern("*").isSingleStar())
+          .isTrue();
+    }
   }
 
-  @Test
-  public void double_star_pattern_is_allowed() throws Exception {
-    when(namePattern(DOUBLE_STAR));
-    thenReturned();
+  @Nested
+  class isDoubleStart {
+    @Test
+    public void returns_false_for_pattern_that_contains_no_star() {
+      assertThat(namePattern("abc").isDoubleStar())
+          .isFalse();
+    }
+
+    @Test
+    public void returns_false_for_pattern_that_contains_one_star() {
+      assertThat(namePattern("*abc").isDoubleStar())
+          .isFalse();
+    }
+
+    @Test
+    public void returns_false_for_pattern_that_contains_two_disjoint_stars() {
+      assertThat(namePattern("*abc*def").isDoubleStar())
+          .isFalse();
+    }
+
+    @Test
+    public void returns_true_for_double_star_pattern() {
+      assertThat(namePattern("**").isDoubleStar())
+          .isTrue();
+    }
   }
 
-  @Test
-  public void double_star_with_suffix_is_not_allowed() throws Exception {
-    when(() -> namePattern(DOUBLE_STAR + "a"));
-    thenThrown(IllegalArgumentException.class);
-  }
+  @Nested
+  class parts {
+    @Test
+    public void returns_pattern_parts() {
+      assertThat(namePattern("abc*def*ghi").parts())
+          .containsExactly("abc", "*", "def", "*", "ghi")
+          .inOrder();
+    }
 
-  @Test
-  public void double_star_with_prefix_is_not_allowed() throws Exception {
-    when(() -> namePattern("a" + DOUBLE_STAR));
-    thenThrown(IllegalArgumentException.class);
-  }
+    @Test
+    public void single_star_pattern_has_one_part() {
+      assertThat(namePattern("*").parts())
+          .containsExactly("*");
+    }
 
-  @Test
-  public void three_star_pattern_is_not_allowed() throws Exception {
-    when(() -> namePattern("***"));
-    thenThrown(IllegalArgumentException.class);
-  }
-
-  // hasStars()
-
-  @Test
-  public void hasStars_returns_false_for_pattern_that_contains_no_stars() throws Exception {
-    given(pattern = namePattern("abcdef"));
-    when(pattern.hasStars());
-    thenReturned(false);
-  }
-
-  @Test
-  public void hasStars_returns_true_for_one_star_pattern() throws Exception {
-    given(pattern = namePattern("*"));
-    when(pattern.hasStars());
-    thenReturned(true);
-  }
-
-  @Test
-  public void hasStars_returns_true_for_pattern_that_contains_one_star() throws Exception {
-    given(pattern = namePattern("abc*def"));
-    when(pattern.hasStars());
-    thenReturned(true);
-  }
-
-  @Test
-  public void hasStars_returns_true_for_pattern_that_contains_two_stars() throws Exception {
-    given(pattern = namePattern("abc*def*ghi"));
-    when(pattern.hasStars());
-    thenReturned(true);
-  }
-
-  // isSingleStar()
-
-  @Test
-  public void isSingleStar_returns_false_for_pattern_that_contains_one_star() throws Exception {
-    given(pattern = namePattern("*abc"));
-    when(pattern.isSingleStar());
-    thenReturned(false);
-  }
-
-  @Test
-  public void isSingleStar_returns_true_for_one_star_pattern() throws Exception {
-    given(pattern = namePattern("*"));
-    when(pattern.isSingleStar());
-    thenReturned(true);
-  }
-
-  // isDoubleStar()
-
-  @Test
-  public void isDoubleStar_returns_false_for_pattern_that_contains_no_star() throws Exception {
-    given(pattern = namePattern("abc"));
-    when(pattern.isDoubleStar());
-    thenReturned(false);
-  }
-
-  @Test
-  public void isDoubleStar_returns_false_for_pattern_that_contains_one_star() throws Exception {
-    given(pattern = namePattern("*abc"));
-    when(pattern.isDoubleStar());
-    thenReturned(false);
-  }
-
-  @Test
-  public void isDoubleStar_returns_false_for_pattern_that_contains_two_disjoint_stars()
-      throws Exception {
-    given(pattern = namePattern("*abc*def"));
-    when(pattern.isDoubleStar());
-    thenReturned(false);
-  }
-
-  @Test
-  public void isDoubleStar_returns_true_for_double_star_pattern() throws Exception {
-    given(pattern = namePattern("**"));
-    when(pattern.isDoubleStar());
-    thenReturned(true);
-  }
-
-  // parts()
-
-  @Test
-  public void parts_returns_pattern_parts() throws Exception {
-    given(pattern = namePattern("abc*def*ghi"));
-    when(pattern.parts());
-    thenReturned(list("abc", "*", "def", "*", "ghi"));
-  }
-
-  @Test
-  public void single_star_pattern_has_one_part() throws Exception {
-    given(pattern = namePattern("*"));
-    when(pattern.parts());
-    thenReturned(list("*"));
-  }
-
-  @Test
-  public void double_star_pattern_has_one_part() throws Exception {
-    given(pattern = namePattern("**"));
-    when(pattern.parts());
-    thenReturned(list("**"));
+    @Test
+    public void double_star_pattern_has_one_part() {
+      assertThat(namePattern("**").parts())
+          .containsExactly("**");
+    }
   }
 }
