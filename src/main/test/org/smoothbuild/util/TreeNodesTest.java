18d398855a10ee48928bfb70749ef958ba459079
Marcin Mikosik <marcin.mikosik@gmail.com>
2020-03-27 08:31:47 +0100

    refactored TreeNodesTest and TreeOrderMatcher to use Truth framework

--- src/main/test/org/smoothbuild/util/TreeNodesTest.java
+++ src/main/test/org/smoothbuild/util/TreeNodesTest.java
@@ -1,46 +1,38 @@
 package org.smoothbuild.util;
 
+import static com.google.common.truth.Truth.assertThat;
+import static org.mockito.Mockito.verifyNoInteractions;
 import static org.smoothbuild.testing.common.TestTreeNode.node;
 import static org.smoothbuild.util.Lists.list;
 import static org.smoothbuild.util.TreeNodes.walk;
-import static org.testory.Testory.given;
-import static org.testory.Testory.mock;
-import static org.testory.Testory.onInstance;
-import static org.testory.Testory.thenCalledNever;
-import static org.testory.Testory.thenEqual;
-import static org.testory.Testory.thenReturned;
-import static org.testory.Testory.when;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
 
 import org.junit.jupiter.api.Test;
-import org.smoothbuild.testing.common.TestTreeNode;
+import org.mockito.Mockito;
 
 public class TreeNodesTest {
-  private Consumer<TestTreeNode> consumer;
-  private List<TestTreeNode> nodes;
-
   @Test
   public void to_string_for_null_generates_empty_string_with_EOL() {
-    when(TreeNodes.treeToString(null));
-    thenReturned("\n");
+    assertThat(TreeNodes.treeToString(null))
+        .isEqualTo("\n");
   }
 
   @Test
   public void to_string_for_childless_root() {
-    when(TreeNodes.treeToString(node("name")));
-    thenReturned("name\n");
+    assertThat(TreeNodes.treeToString(node("name")))
+        .isEqualTo("name\n");
   }
 
   @Test
   public void to_string_for_tree_with_2_layers() {
-    when(TreeNodes.treeToString(
+    assertThat(TreeNodes.treeToString(
         node("name", list(
             node("child 1"),
-            node("child 2")))));
-    thenReturned(
+            node("child 2")))))
+        .isEqualTo(
         "name\n" +
         "  child 1\n" +
         "  child 2\n");
@@ -48,14 +40,14 @@ public class TreeNodesTest {
 
   @Test
   public void to_string_for_tree_with_3_layers() {
-    when(TreeNodes.treeToString(
+    assertThat(TreeNodes.treeToString(
         node("name", list(
             node("child 1", list(
                 node("child A"),
                 node("child B")
             )),
-            node("child 2")))));
-    thenReturned(
+            node("child 2")))))
+        .isEqualTo(
         "name\n" +
         "  child 1\n" +
         "    child A\n" +
@@ -65,57 +57,67 @@ public class TreeNodesTest {
 
   @Test
   public void to_list_for_null_returns_empty_list() {
-    when(TreeNodes.treeToList(null));
-    thenReturned(list());
+    assertThat(TreeNodes.treeToList(null))
+        .isEmpty();
   }
 
   @Test
   public void to_list_for_childless_root_returns_root_only_list() {
-    when(TreeNodes.treeToList(node("name")));
-    thenReturned(list(node("name")));
+    assertThat(TreeNodes.treeToList(node("name")))
+        .containsExactly(node("name"));
   }
 
   @Test
   public void to_list_for_tree_with_2_layers() {
-    when(TreeNodes.treeToList(
+    assertThat(TreeNodes.treeToList(
         node("name", list(
             node("child 1"),
-            node("child 2")))));
-    thenReturned(list(node("name"), node("child 1"), node("child 2")));
+            node("child 2")))))
+        .containsExactly(node("name"), node("child 1"), node("child 2"))
+        .inOrder();
   }
 
   @Test
   public void to_list_for_tree_with_3_layers() {
-    when(TreeNodes.treeToList(
+    assertThat(TreeNodes.treeToList(
         node("name", list(
             node("child 1", list(
                 node("child A"),
                 node("child B")
             )),
-            node("child 2")))));
-    thenReturned(list(
-        node("name"), node("child 1"), node("child A"), node("child B"), node("child 2")));
+            node("child 2")))))
+        .containsExactly(
+            node("name"), node("child 1"), node("child A"), node("child B"), node("child 2"))
+        .inOrder();
   }
 
   @Test
   public void walk_for_null_never_calls_consumer() {
-    given(consumer = mock(Consumer.class));
-    when(() -> walk(null, consumer));
-    thenCalledNever(onInstance(consumer));
+    @SuppressWarnings("unchecked")
+    Consumer<MyNode> consumer = Mockito.mock(Consumer.class);
+    walk(null, consumer);
+    verifyNoInteractions(consumer);
   }
 
+  private static class MyNode implements TreeNode<MyNode> {
+    @Override
+    public List<MyNode> children() {
+      return null;
+    }
+  }
   @Test
   public void walk_visits_all_nodes_in_parent_child_order() {
-    given(nodes = new ArrayList<>());
-    when(() -> walk(
+    List<Object> nodes = new ArrayList<>();
+    walk(
         node("name", list(
             node("child 1", list(
                 node("child A"),
                 node("child B")
             )),
-            node("child 2"))), nodes::add)
-    );
-    thenEqual(nodes, list(
-        node("name"), node("child 1"), node("child A"), node("child B"), node("child 2")));
+            node("child 2"))), nodes::add);
+    assertThat(nodes)
+        .containsExactly(
+            node("name"), node("child 1"), node("child A"), node("child B"), node("child 2"))
+        .inOrder();
   }
 }
