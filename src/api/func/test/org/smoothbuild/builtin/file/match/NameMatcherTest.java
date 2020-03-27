60ec20cd2b3609788909cb1ec2931442811afbe2
Marcin Mikosik <marcin.mikosik@gmail.com>
2020-03-27 07:34:16 +0100

    refactored NameMatcherTest to use Truth framework

--- src/api/func/test/org/smoothbuild/builtin/file/match/NameMatcherTest.java
+++ src/api/func/test/org/smoothbuild/builtin/file/match/NameMatcherTest.java
@@ -1,268 +1,225 @@
 package org.smoothbuild.builtin.file.match;
 
+import static com.google.common.truth.Truth.assertThat;
 import static org.smoothbuild.builtin.file.match.NamePattern.namePattern;
 import static org.smoothbuild.io.fs.base.Path.path;
-import static org.testory.Testory.given;
-import static org.testory.Testory.thenReturned;
-import static org.testory.Testory.thenThrown;
-import static org.testory.Testory.when;
+import static org.smoothbuild.testing.common.AssertCall.assertCall;
 
 import org.junit.jupiter.api.Test;
 
 public class NameMatcherTest {
-  private NameMatcher nameMatcher;
-  private NamePattern pattern;
-
   @Test()
   public void double_star_pattern_is_forbidden() {
-    given(pattern = namePattern("**"));
-    when(() -> new NameMatcher(pattern));
-    thenThrown(IllegalArgumentException.class);
+    assertCall(() -> new NameMatcher(namePattern("**")))
+        .throwsException(IllegalArgumentException.class);
   }
-
-  // single star pattern
 
   @Test
   public void single_star_matches_one_character_string() {
-    given(nameMatcher = nameMatcher("*"));
-    when(nameMatcher.test(path("a")));
-    thenReturned(true);
+    assertThat(nameMatcher("*").test(path("a")))
+        .isTrue();
   }
 
   @Test
   public void single_star_matches_long_string() {
-    given(nameMatcher = nameMatcher("*"));
-    when(nameMatcher.test(path("abcdefghijklmnop")));
-    thenReturned(true);
+    assertThat(nameMatcher("*").test(path("abcdefghijklmnop")))
+        .isTrue();
   }
-
-  // pattern that starts with star
 
   @Test
   public void pattern_that_have_star_plus_ending_matches_lonely_ending() {
-    given(nameMatcher = nameMatcher("*abc"));
-    when(nameMatcher.test(path("abc")));
-    thenReturned(true);
+    assertThat(nameMatcher("*abc").test(path("abc")))
+        .isTrue();
   }
 
   @Test
   public void pattern_that_have_star_plus_ending_matches_lonely_ending_prefixed_by_half_of_ending() {
-    given(nameMatcher = nameMatcher("*abc"));
-    when(nameMatcher.test(path("ababc")));
-    thenReturned(true);
+    assertThat(nameMatcher("*abc").test(path("ababc")))
+        .isTrue();
   }
 
   @Test
   public void pattern_that_have_star_plus_ending_matches_doubled_lonely_ending() {
-    given(nameMatcher = nameMatcher("*abc"));
-    when(nameMatcher.test(path("abcabc")));
-    thenReturned(true);
+    assertThat(nameMatcher("*abc").test(path("abcabc")))
+        .isTrue();
   }
 
   // pattern that starts with star
 
   @Test
   public void pattern_that_have_prefixed_star_matches_lonely_prefix() {
-    given(nameMatcher = nameMatcher("abc*"));
-    when(nameMatcher.test(path("abc")));
-    thenReturned(true);
+    assertThat(nameMatcher("abc*").test(path("abc")))
+        .isTrue();
   }
 
   @Test
   public void pattern_that_have_prefixed_star_matches_lonely_prefix_suffixed_by_half_of_prefix() {
-    given(nameMatcher = nameMatcher("abc*"));
-    when(nameMatcher.test(path("abcab")));
-    thenReturned(true);
+    assertThat(nameMatcher("abc*").test(path("abcab")))
+        .isTrue();
   }
 
   @Test
   public void pattern_that_have_prefixed_star_matches_doubled_lonely_prefix() {
-    given(nameMatcher = nameMatcher("abc*"));
-    when(nameMatcher.test(path("abcabc")));
-    thenReturned(true);
+    assertThat(nameMatcher("abc*").test(path("abcabc")))
+        .isTrue();
   }
 
   // pattern with one star in the middle
 
   @Test
   public void pattern_that_have_middle_star_does_not_match_lonely_prefix() {
-    given(nameMatcher = nameMatcher("abc*def"));
-    when(nameMatcher.test(path("abc")));
-    thenReturned(false);
+    assertThat(nameMatcher("abc*def").test(path("abc")))
+        .isFalse();
   }
 
   @Test
   public void pattern_that_have_middle_star_does_not_match_lonely_ending() {
-    given(nameMatcher = nameMatcher("abc*def"));
-    when(nameMatcher.test(path("def")));
-    thenReturned(false);
+    assertThat(nameMatcher("abc*def").test(path("def")))
+        .isFalse();
   }
 
   @Test
   public void pattern_that_have_middle_star_matches_prefix_plus_ending() {
-    given(nameMatcher = nameMatcher("abc*def"));
-    when(nameMatcher.test(path("abcdef")));
-    thenReturned(true);
+    assertThat(nameMatcher("abc*def").test(path("abcdef")))
+        .isTrue();
   }
 
   @Test
-  public void pattern_that_have_middle_star_matches_prefix_and_ending_split_by_its_halfs() {
-    given(nameMatcher = nameMatcher("abc*def"));
-    when(nameMatcher.test(path("abcabdedef")));
-    thenReturned(true);
+  public void pattern_that_have_middle_star_matches_prefix_and_ending_separated_by_its_halfs() {
+    assertThat(nameMatcher("abc*def").test(path("abcabdedef")))
+        .isTrue();
   }
 
   @Test
   public void pattern_that_have_middle_star_matches_doubled_prefix_plus_doubled_suffix() {
-    given(nameMatcher = nameMatcher("abc*def"));
-    when(nameMatcher.test(path("abcabcdefdef")));
-    thenReturned(true);
+    assertThat(nameMatcher("abc*def").test(path("abcabcdefdef")))
+        .isTrue();
   }
 
   // pattern with trailing stars
 
   @Test
   public void pattern_that_have_trailing_stars_matches_lonely_middle_part() {
-    given(nameMatcher = nameMatcher("*abc*"));
-    when(nameMatcher.test(path("abc")));
-    thenReturned(true);
+    assertThat(nameMatcher("*abc*").test(path("abc")))
+        .isTrue();
   }
 
   @Test
   public void pattern_that_have_trailing_stars_matches_prefixed_middle_part() {
-    given(nameMatcher = nameMatcher("*abc*"));
-    when(nameMatcher.test(path("xxxabc")));
-    thenReturned(true);
+    assertThat(nameMatcher("*abc*").test(path("xxxabc")))
+        .isTrue();
   }
 
   @Test
   public void pattern_that_have_trailing_stars_matches_suffixed_middle_part() {
-    given(nameMatcher = nameMatcher("*abc*"));
-    when(nameMatcher.test(path("abcyyy")));
-    thenReturned(true);
+    assertThat(nameMatcher("*abc*").test(path("abcyyy")))
+        .isTrue();
   }
 
   @Test
   public void pattern_that_have_trailing_stars_matches_surrounded_middle_part() {
-    given(nameMatcher = nameMatcher("*abc*"));
-    when(nameMatcher.test(path("xxxabcyyy")));
-    thenReturned(true);
+    assertThat(nameMatcher("*abc*").test(path("xxxabcyyy")))
+        .isTrue();
   }
 
   @Test
   public void pattern_that_have_trailing_stars_matches_surrounded_middle_part_prefixed_with_half_of_middle() {
-    given(nameMatcher = nameMatcher("*abc*"));
-    when(nameMatcher.test(path("xxxababcyyy")));
-    thenReturned(true);
+    assertThat(nameMatcher("*abc*").test(path("xxxababcbcyyy")))
+        .isTrue();
   }
 
   // pattern with two inner stars
 
   @Test
   public void pattern_that_have_two_inner_stars_matches_string_with_nothing_for_stars() {
-    given(nameMatcher = nameMatcher("abc*def*ghi"));
-    when(nameMatcher.test(path("abcdefghi")));
-    thenReturned(true);
+    assertThat(nameMatcher("*abc*def*ghi").test(path("abcdefghi")))
+        .isTrue();
   }
 
   @Test
   public void pattern_that_have_two_inner_stars_does_not_match_when_middle_is_missing() {
-    given(nameMatcher = nameMatcher("abc*def*ghi"));
-    when(nameMatcher.test(path("abcghi")));
-    thenReturned(false);
+    assertThat(nameMatcher("*abc*def*ghi").test(path("abcghi")))
+        .isFalse();
   }
 
   @Test
   public void pattern_that_have_two_inner_stars_matches_string_with_nothing_for_first_star() {
-    given(nameMatcher = nameMatcher("abc*def*ghi"));
-    when(nameMatcher.test(path("abcdefyyyghi")));
-    thenReturned(true);
+    assertThat(nameMatcher("*abc*def*ghi").test(path("abcdef___ghi")))
+        .isTrue();
   }
 
   @Test
   public void pattern_that_have_two_inner_stars_matches_string_with_nothing_for_second_star() {
-    given(nameMatcher = nameMatcher("abc*def*ghi"));
-    when(nameMatcher.test(path("abcxxxdefghi")));
-    thenReturned(true);
+    assertThat(nameMatcher("*abc*def*ghi").test(path("abc___defghi")))
+        .isTrue();
   }
 
   @Test
   public void pattern_that_have_two_inner_stars_matches_string_with_something_for_each_star() {
-    given(nameMatcher = nameMatcher("abc*def*ghi"));
-    when(nameMatcher.test(path("abcxxxdefyyyghi")));
-    thenReturned(true);
+    assertThat(nameMatcher("*abc*def*ghi").test(path("abc___def___ghi")))
+        .isTrue();
   }
 
   @Test
   public void pattern_that_have_two_inner_stars_does_not_matches_string_with_not_full_middle_part() {
-    given(nameMatcher = nameMatcher("abc*def*ghi"));
-    when(nameMatcher.test(path("abcdeghi")));
-    thenReturned(false);
+    assertThat(nameMatcher("*abc*def*ghi").test(path("abcdeghi")))
+        .isFalse();
   }
 
   // pattern with three inner stars
 
   @Test
   public void pattern_that_have_three_inner_stars_matches_string_with_nothing_for_stars() {
-    given(nameMatcher = nameMatcher("abc*def*ghi*jkl"));
-    when(nameMatcher.test(path("abcdefghijkl")));
-    thenReturned(true);
+    assertThat(nameMatcher("abc*def*ghi*jkl").test(path("abcdefghijkl")))
+        .isTrue();
   }
 
   @Test
   public void pattern_that_have_three_inner_stars_does_not_match_when_left_middle_is_missing() {
-    given(nameMatcher = nameMatcher("abc*def*ghi*jkl"));
-    when(nameMatcher.test(path("abcghijkl")));
-    thenReturned(false);
+    assertThat(nameMatcher("abc*def*ghi*jkl").test(path("abcghijkl")))
+        .isFalse();
   }
 
   @Test
   public void pattern_that_have_three_inner_stars_does_not_match_when_right_middle_is_missing() {
-    given(nameMatcher = nameMatcher("abc*def*ghi*jkl"));
-    when(nameMatcher.test(path("abcdefjkl")));
-    thenReturned(false);
+    assertThat(nameMatcher("abc*def*ghi*jkl").test(path("abcdefjkl")))
+        .isFalse();
   }
 
   @Test
   public void pattern_that_have_three_inner_stars_matches_string_with_nothing_for_first_star() {
-    given(nameMatcher = nameMatcher("abc*def*ghi*jkl"));
-    when(nameMatcher.test(path("abcdefyyyghizzzjkl")));
-    thenReturned(true);
+    assertThat(nameMatcher("abc*def*ghi*jkl").test(path("abcdef___ghi___jkl")))
+        .isTrue();
   }
 
   @Test
   public void pattern_that_have_three_inner_stars_matches_string_with_nothing_for_second_star() {
-    given(nameMatcher = nameMatcher("abc*def*ghi*jkl"));
-    when(nameMatcher.test(path("abcxxxdefghizzzjkl")));
-    thenReturned(true);
+    assertThat(nameMatcher("abc*def*ghi*jkl").test(path("abc___defghi___jkl")))
+        .isTrue();
   }
 
   @Test
   public void pattern_that_have_three_inner_stars_matches_string_with_nothing_for_third_star() {
-    given(nameMatcher = nameMatcher("abc*def*ghi*jkl"));
-    when(nameMatcher.test(path("abcxxxdefyyyghijkl")));
-    thenReturned(true);
+    assertThat(nameMatcher("abc*def*ghi*jkl").test(path("abc___def___ghijkl")))
+        .isTrue();
   }
 
   @Test
   public void pattern_that_have_three_inner_stars_matches_string_with_something_for_each_star() {
-    given(nameMatcher = nameMatcher("abc*def*ghi*jkl"));
-    when(nameMatcher.test(path("abcxxxdefyyyghizzzjkl")));
-    thenReturned(true);
+    assertThat(nameMatcher("abc*def*ghi*jkl").test(path("abc___def___ghi___jkl")))
+        .isTrue();
   }
 
   @Test
-  public void pattern_that_have_two_inner_stars_does_not_match_string_with_not_full_left_middle_part() {
-    given(nameMatcher = nameMatcher("abc*def*ghi*jkl"));
-    when(nameMatcher.test(path("abcdeghijkl")));
-    thenReturned(false);
+  public void pattern_that_have_three_inner_stars_does_not_match_string_with_not_full_left_middle_part() {
+    assertThat(nameMatcher("abc*def*ghi*jkl").test(path("abcdeghijkl")))
+        .isFalse();
   }
 
   @Test
-  public void pattern_that_have_two_inner_stars_does_not_match_string_with_not_full_right_middle_part() {
-    given(nameMatcher = nameMatcher("abc*def*ghi*jkl"));
-    when(nameMatcher.test(path("abcdefghjkl")));
-    thenReturned(false);
+  public void pattern_that_have_three_inner_stars_does_not_match_string_with_not_full_right_middle_part() {
+    assertThat(nameMatcher("abc*def*ghi*jkl").test(path("abcdefghjkl")))
+        .isFalse();
   }
 
   private static NameMatcher nameMatcher(final String pattern) {
