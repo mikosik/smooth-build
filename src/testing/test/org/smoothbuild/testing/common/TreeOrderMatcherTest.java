75bec35017a9f0964cf1c726b7f2e2f9ca2050e4
Marcin Mikosik <marcin.mikosik@gmail.com>
2020-03-27 08:31:47 +0100

    refactored TreeNodesTest and TreeOrderMatcher to use Truth framework

--- src/testing/test/org/smoothbuild/testing/common/TreeOrderMatcherTest.java
+++ src/testing/test/org/smoothbuild/testing/common/TreeOrderMatcherTest.java
@@ -1,10 +1,8 @@
 package org.smoothbuild.testing.common;
 
+import static com.google.common.truth.Truth.assertThat;
 import static org.smoothbuild.testing.common.TestTreeNode.node;
 import static org.smoothbuild.util.Lists.list;
-import static org.testory.Testory.given;
-import static org.testory.Testory.thenReturned;
-import static org.testory.Testory.when;
 
 import org.junit.jupiter.api.Test;
 
@@ -13,43 +11,43 @@ public class TreeOrderMatcherTest {
 
   @Test
   public void empty_list_matches_empty_tree() {
-    given(matcher = new TreeOrderMatcher<>(null));
-    when(() -> matcher.matchesSafely(list()));
-    thenReturned(true);
+    matcher = new TreeOrderMatcher<>(null);
+    assertThat(matcher.matchesSafely(list()))
+        .isTrue();
   }
 
   @Test
   public void empty_list_matches_non_empty_tree() {
-    given(matcher = new TreeOrderMatcher<>(node("root")));
-    when(() -> matcher.matchesSafely(list()));
-    thenReturned(true);
+    matcher = new TreeOrderMatcher<>(node("root"));
+    assertThat(matcher.matchesSafely(list()))
+        .isTrue();
   }
 
   @Test
   public void list_matches_tree_which_does_not_contain_list_elements() {
-    given(matcher = new TreeOrderMatcher<>(node("root")));
-    when(() -> matcher.matchesSafely(list(node("A"), node("B"))));
-    thenReturned(true);
+    matcher = new TreeOrderMatcher<>(node("root"));
+    assertThat(matcher.matchesSafely(list(node("A"), node("B"))))
+        .isTrue();
   }
 
   @Test
   public void list_matches_tree_when_elements_match_parent_child_relationship() {
-    given(matcher = new TreeOrderMatcher<>(node("root", list(node("child A"), node("child B")))));
-    when(() -> matcher.matchesSafely(list(node("child A"), node("child B"), node("root"))));
-    thenReturned(true);
+    matcher = new TreeOrderMatcher<>(node("root", list(node("child A"), node("child B"))));
+    assertThat(matcher.matchesSafely(list(node("child A"), node("child B"), node("root"))))
+        .isTrue();
   }
 
   @Test
   public void list_matches_tree_when_elements_match_parent_child_relationship_with_children_reversed() {
-    given(matcher = new TreeOrderMatcher<>(node("root", list(node("child A"), node("child B")))));
-    when(() -> matcher.matchesSafely(list(node("child B"), node("child A"), node("root"))));
-    thenReturned(true);
+    matcher = new TreeOrderMatcher<>(node("root", list(node("child A"), node("child B"))));
+    assertThat(matcher.matchesSafely(list(node("child B"), node("child A"), node("root"))))
+        .isTrue();
   }
 
   @Test
   public void list_does_not_matche_tree_when_parent_is_before_child() {
-    given(matcher = new TreeOrderMatcher<>(node("root", list(node("child A"), node("child B")))));
-    when(() -> matcher.matchesSafely(list(node("root"), node("child A"))));
-    thenReturned(false);
+    matcher = new TreeOrderMatcher<>(node("root", list(node("child A"), node("child B"))));
+    assertThat(matcher.matchesSafely(list(node("root"), node("child A"))))
+        .isFalse();
   }
 }
