f68f532546c54eef27aa3ee83c6d862d89eaff52
Marcin Mikosik <marcin.mikosik@gmail.com>
2020-04-01 14:35:45 +0200

    refactored SoftTerminationExecutor to take threadCount as constructor parameter

--- src/main/test/org/smoothbuild/util/concurrent/SoftTerminationExecutorTwoThreadsTest.java
+++ src/main/test/org/smoothbuild/util/concurrent/SoftTerminationExecutorTwoThreadsTest.java
@@ -2,7 +2,7 @@ package org.smoothbuild.util.concurrent;
 
 public class SoftTerminationExecutorTwoThreadsTest extends AbstractSoftTerminationExecutorTestCase {
   @Override
-  protected SoftTerminationExecutor createJobExecutor() {
-    return new SoftTerminationExecutor(2);
+  protected int threadCount() {
+    return 2;
   }
 }
