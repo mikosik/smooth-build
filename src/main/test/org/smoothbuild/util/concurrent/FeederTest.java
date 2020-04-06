86978ed0c7d88d688ce8e9ab3de333bc3e02b440
Marcin Mikosik <marcin.mikosik@gmail.com>
2020-04-06 13:27:54 +0200

    added Feeder.runWhenAllAvailable()

--- src/main/test/org/smoothbuild/util/concurrent/FeederTest.java
+++ src/main/test/org/smoothbuild/util/concurrent/FeederTest.java
@@ -3,16 +3,21 @@ package org.smoothbuild.util.concurrent;
 import static com.google.common.truth.Truth.assertThat;
 import static org.junit.Assert.assertThrows;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoInteractions;
 import static org.smoothbuild.util.Strings.unlines;
+import static org.smoothbuild.util.concurrent.Feeder.runWhenAllAvailable;
 
+import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Consumer;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+@SuppressWarnings("ClassCanBeStatic")
 public class FeederTest {
   private Feeder<String> feeder;
 
@@ -100,6 +105,45 @@ public class FeederTest {
       assertThat(feeder.isCurrentThreadHoldingALock())
           .isFalse();
     });
+  }
+
+  @Nested
+  class run_when_children_available {
+    @Test
+    void calls_runnable_when_all_children_become_available() {
+      Runnable parent = mock(Runnable.class);
+      List<Feeder<String>> feeders = List.of(new Feeder<>(), new Feeder<>(), new Feeder<>());
+      runWhenAllAvailable(feeders, parent);
+      for (Feeder<String> child : feeders) {
+        child.accept("abc");
+      }
+
+      verify(parent, times(1)).run();
+    }
+
+    @Test
+    void calls_runnable_immediately_when_all_children_were_available_before_call() {
+      Runnable parent = mock(Runnable.class);
+      List<Feeder<String>> feeders = List.of(new Feeder<>(), new Feeder<>(), new Feeder<>());
+      for (Feeder<String> child : feeders) {
+        child.accept("abc");
+      }
+      runWhenAllAvailable(feeders, parent);
+
+      verify(parent, times(1)).run();
+    }
+
+    @Test
+    void is_not_run_when_not_all_children_are_available() {
+      Runnable parent = mock(Runnable.class);
+      List<Feeder<String>> feeders = List.of(new Feeder<>(), new Feeder<>(), new Feeder<>());
+      runWhenAllAvailable(feeders, parent);
+      for (int i = 1; i < feeders.size(); i++) {
+        feeders.get(i).accept("abc");
+      }
+
+      verifyNoInteractions(parent);
+    }
   }
 
   @SuppressWarnings("unchecked")
