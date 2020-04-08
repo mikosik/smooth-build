f5ff32b5a94da17c7295efd189dcc773d0438957
Marcin Mikosik <marcin.mikosik@gmail.com>
2020-04-08 15:11:51 +0200

    added hashes to 'smooth version' output

--- src/acceptance/org/smoothbuild/acceptance/cmd/VersionCommandTest.java
+++ src/acceptance/org/smoothbuild/acceptance/cmd/VersionCommandTest.java
@@ -1,14 +1,28 @@
 package org.smoothbuild.acceptance.cmd;
 
+import static com.google.common.truth.Truth.assertThat;
+
 import org.junit.jupiter.api.Test;
 import org.smoothbuild.SmoothConstants;
 import org.smoothbuild.acceptance.AcceptanceTestCase;
 
 public class VersionCommandTest extends AcceptanceTestCase {
   @Test
-  public void version_command_prints_tool_version() throws Exception {
+  public void version_command_prints_tool_version() {
     whenSmoothVersion();
     thenFinishedWithSuccess();
+    thenOutputContains("smooth build version " + SmoothConstants.VERSION + "\n");
+  }
+
+  @Test
+  public void version_command_prints_file_hashes() {
+    whenSmoothVersion();
+    thenFinishedWithSuccess();
+    String hexNumberPattern = "[a-f0-9]+";
+    assertThat(output()).containsMatch("sandbox *" + hexNumberPattern);
+    assertThat(output()).containsMatch("  smooth.jar *" + hexNumberPattern);
+    assertThat(output()).containsMatch("  java platform *" + hexNumberPattern);
+    assertThat(output()).containsMatch("funcs.jar *" + hexNumberPattern);
     thenOutputContains("smooth build version " + SmoothConstants.VERSION + "\n");
   }
 }
