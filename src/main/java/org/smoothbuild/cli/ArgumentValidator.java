aec6014e6c476f7e36f6aafc0ab58ae65c251ae5
Marcin Mikosik <marcin.mikosik@gmail.com>
2020-04-14 15:35:37 +0200

    fixed error reporting in ArgumentValidator

--- src/main/java/org/smoothbuild/cli/ArgumentValidator.java
+++ src/main/java/org/smoothbuild/cli/ArgumentValidator.java
@@ -2,7 +2,6 @@ package org.smoothbuild.cli;
 
 import static com.google.common.collect.ImmutableList.toImmutableList;
 import static org.smoothbuild.lang.base.Name.isLegalName;
-import static org.smoothbuild.util.Maybe.error;
 import static org.smoothbuild.util.Maybe.maybe;
 import static org.smoothbuild.util.Maybe.value;
 
@@ -18,14 +17,7 @@ public class ArgumentValidator {
   public static Maybe<Set<String>> validateFunctionNames(List<String> args) {
     DuplicatesDetector<String> duplicatesDetector = new DuplicatesDetector<>();
     args.forEach(duplicatesDetector::addValue);
-    Set<String> uniques = duplicatesDetector.getUniqueValues();
-
-    if (uniques.isEmpty()) {
-      return error("error: Specify at least one function to be executed.\n"
-          + "Try 'smooth list' to see all available functions.");
-    }
-
-    return value(uniques)
+    return value(duplicatesDetector.getUniqueValues())
         .map(as -> maybe(as, illegalFunctionNameErrors(args)))
         .map(as -> maybe(as, duplicateFunctionNameErrors(duplicatesDetector)));
   }
@@ -34,15 +26,14 @@ public class ArgumentValidator {
     return args
         .stream()
         .filter(a -> !isLegalName(a))
-        .map(a -> "error: Illegal function name '" + a
-            + "' passed in command line.")
+        .map(a -> "Illegal function name '" + a + "' passed in command line.")
         .collect(toImmutableList());
   }
 
   private static ImmutableList<String> duplicateFunctionNameErrors(
       DuplicatesDetector<String> duplicatesDetector) {
     return duplicatesDetector.getDuplicateValues().stream().map(
-        name -> "error: Function '" + name + "' has been specified more than once.")
+        name -> "Function '" + name + "' has been specified more than once.")
         .collect(toImmutableList());
   }
 }
