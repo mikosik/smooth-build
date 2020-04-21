67c20c8bf7934a8fc73e3fd3da8e5ab9cc23247e
Marcin Mikosik <marcin.mikosik@gmail.com>
2020-04-21 12:16:58 +0200

    removed obsolete methods from LocationHelpers

--- src/main/java/org/smoothbuild/parse/LocationHelpers.java
+++ src/main/java/org/smoothbuild/parse/LocationHelpers.java
@@ -2,35 +2,14 @@ package org.smoothbuild.parse;
 
 import static org.smoothbuild.lang.base.Location.location;
 
-import java.nio.file.Path;
-
 import org.antlr.v4.runtime.ParserRuleContext;
 import org.antlr.v4.runtime.Token;
 import org.smoothbuild.ModulePath;
-import org.smoothbuild.antlr.SmoothParser.ArgContext;
-import org.smoothbuild.antlr.SmoothParser.NameContext;
 import org.smoothbuild.lang.base.Location;
 
 public class LocationHelpers {
-  public static Location locationOf(Path file, ArgContext arg) {
-    NameContext name = arg.name();
-    if (name == null) {
-      return locationOf(file, arg.pipe());
-    } else {
-      return locationOf(file, name);
-    }
-  }
-
-  public static Location locationOf(Path path, ParserRuleContext parserRuleContext) {
-    return locationOf(path, parserRuleContext.getStart());
-  }
-
   public static Location locationOf(ModulePath path, ParserRuleContext parserRuleContext) {
     return locationOf(path, parserRuleContext.getStart());
-  }
-
-  public static Location locationOf(Path path, Token token) {
-    return location(new ModulePath(path, path.toString()), token.getLine());
   }
 
   public static Location locationOf(ModulePath path, Token token) {
--- src/main/test/org/smoothbuild/parse/LocationHelpersTest.java
+++ src/main/test/org/smoothbuild/parse/LocationHelpersTest.java
@@ -10,42 +10,16 @@ import java.nio.file.Paths;
 import org.antlr.v4.runtime.ParserRuleContext;
 import org.antlr.v4.runtime.Token;
 import org.junit.jupiter.api.Test;
-import org.smoothbuild.antlr.SmoothParser.ArgContext;
-import org.smoothbuild.antlr.SmoothParser.NameContext;
-import org.smoothbuild.antlr.SmoothParser.PipeContext;
+import org.smoothbuild.ModulePath;
 import org.smoothbuild.lang.base.Location;
 
 public class LocationHelpersTest {
-  @Test
-  public void location_of_arg_context_with_param_name() {
-    Token token = token(13);
-    NameContext nameContext = mock(NameContext.class);
-    when(nameContext.getStart()).thenReturn(token);
-    ArgContext argContext = mock(ArgContext.class);
-    when(argContext.name()).thenReturn(nameContext);
-    Location location = locationOf(Paths.get("script.smooth"), argContext);
-    assertThat(location.line())
-        .isEqualTo(13);
-  }
-
-  @Test
-  public void location_of_arg_context_without_param_name() {
-    PipeContext pipeContext = mock(PipeContext.class);
-    Token token = token(13);
-    when(pipeContext.getStart()).thenReturn(token);
-    ArgContext argContext = mock(ArgContext.class);
-    when(argContext.pipe()).thenReturn(pipeContext);
-    Location location = locationOf(Paths.get("script.smooth"), argContext);
-    assertThat(location.line())
-        .isEqualTo(13);
-  }
-
   @Test
   public void location_of_parser_rule_context() {
     ParserRuleContext parserRuleContext = mock(ParserRuleContext.class);
     Token token = token(13);
     when(parserRuleContext.getStart()).thenReturn(token);
-    Location location = locationOf(Paths.get("script.smooth"), parserRuleContext);
+    Location location = locationOf(modulePath(), parserRuleContext);
     assertThat(location.line())
         .isEqualTo(13);
   }
@@ -53,7 +27,7 @@ public class LocationHelpersTest {
   @Test
   public void location_of_token() {
     Token token = token(13);
-    Location location = locationOf(Paths.get("script.smooth"), token);
+    Location location = locationOf(modulePath(), token);
     assertThat(location.line())
         .isEqualTo(13);
   }
@@ -62,5 +36,9 @@ public class LocationHelpersTest {
     Token token = mock(Token.class);
     when(token.getLine()).thenReturn(l);
     return token;
+  }
+
+  private static ModulePath modulePath() {
+    return new ModulePath(Paths.get("script.smooth"), "{u}/script.smooth");
   }
 }
