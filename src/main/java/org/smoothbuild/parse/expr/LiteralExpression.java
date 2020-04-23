4bc630437b86adc395b9be829c212f16a27bb390
Marcin Mikosik <marcin.mikosik@gmail.com>
2020-04-23 07:25:51 +0200

    removed obsolete param from FixedTask constructor

--- src/main/java/org/smoothbuild/parse/expr/LiteralExpression.java
+++ src/main/java/org/smoothbuild/parse/expr/LiteralExpression.java
@@ -6,23 +6,20 @@ import org.smoothbuild.exec.task.base.BuildTask;
 import org.smoothbuild.exec.task.base.FixedTask;
 import org.smoothbuild.lang.base.Location;
 import org.smoothbuild.lang.base.Scope;
-import org.smoothbuild.lang.object.base.Array;
 import org.smoothbuild.lang.object.base.SObject;
 import org.smoothbuild.lang.object.base.SString;
 
 public class LiteralExpression extends Expression {
   private final SObject object;
-  private final Array messages;
 
-  public LiteralExpression(SObject object, Array messages, Location location) {
+  public LiteralExpression(SObject object, Location location) {
     super(location);
     this.object = object;
-    this.messages = messages;
   }
 
   @Override
   public BuildTask createTask(Scope<BuildTask> scope) {
     String name = escapedAndLimitedWithEllipsis(((SString) this.object).jValue(), 20);
-    return new FixedTask(object, name, messages, location());
+    return new FixedTask(object, name, location());
   }
 }
