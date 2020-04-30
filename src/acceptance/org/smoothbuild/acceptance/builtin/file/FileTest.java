cef64c02baecce5dee824e379084e7f480ba3e3a
Marcin Mikosik <marcin.mikosik@gmail.com>
2020-04-30 13:32:04 +0200

    added missing test for File constructor

---
+++ src/acceptance/org/smoothbuild/acceptance/builtin/file/FileTest.java
@@ -0,0 +1,17 @@
+package org.smoothbuild.acceptance.builtin.file;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.smoothbuild.acceptance.AcceptanceTestCase;
+
+public class FileTest extends AcceptanceTestCase {
+  @Test
+  public void file_constructor() throws Exception {
+    givenScript("result = file(toBlob('abc'), 'name.txt');");
+    whenSmoothBuild("result");
+    thenFinishedWithSuccess();
+    assertThat(artifactContent("result"))
+        .isEqualTo("abc");
+  }
+}
