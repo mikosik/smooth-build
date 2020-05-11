1fd5c0c10fe6a3cdffdd00c2a0ee35732b366a8c
Marcin Mikosik <marcin.mikosik@gmail.com>
2020-05-11 12:37:24 +0200

    added Feeder.chain()

--- src/main/java/org/smoothbuild/util/concurrent/Feeder.java
+++ src/main/java/org/smoothbuild/util/concurrent/Feeder.java
@@ -1,8 +1,11 @@
 package org.smoothbuild.util.concurrent;
 
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 public interface Feeder<T> extends Supplier<T> {
   public void addConsumer(Consumer<T> consumer);
+
+  public <U> Feeder<U> chain(Function<T, U> function);
 }
