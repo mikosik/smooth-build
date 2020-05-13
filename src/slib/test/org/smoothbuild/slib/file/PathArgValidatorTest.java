a6c71ac24c323260baf1f462b6d9386e2e169b6c
Marcin Mikosik <marcin.mikosik@gmail.com>
2020-05-13 11:08:26 +0200

    refactored PathArgValidatorTest

--- src/slib/test/org/smoothbuild/slib/file/PathArgValidatorTest.java
+++ src/slib/test/org/smoothbuild/slib/file/PathArgValidatorTest.java
@@ -3,14 +3,12 @@ package org.smoothbuild.slib.file;
 import static org.smoothbuild.slib.file.PathArgValidator.validatedProjectPath;
 import static org.smoothbuild.testing.common.AssertCall.assertCall;
 
-import java.util.List;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.smoothbuild.lang.plugin.AbortException;
 import org.smoothbuild.testing.TestingContext;
-
-import com.google.common.collect.ImmutableList;
 
 public class PathArgValidatorTest extends TestingContext {
   @ParameterizedTest
@@ -19,25 +17,22 @@ public class PathArgValidatorTest extends TestingContext {
     validatedProjectPath(container(), "name", string(path));
   }
 
-  public static List<String> listOfCorrectProjectPaths() {
-    ImmutableList.Builder<String> builder = ImmutableList.builder();
+  public static Stream<String> listOfCorrectProjectPaths() {
+    return Stream.of(
+        ".",
 
-    builder.add(".");
+        "abc",
+        "abc/def",
+        "abc/def/ghi",
+        "abc/def/ghi/ijk",
 
-    builder.add("abc");
-    builder.add("abc/def");
-    builder.add("abc/def/ghi");
-    builder.add("abc/def/ghi/ijk");
-
-    // These paths look really strange but Linux allows creating them.
-    // I cannot see any good reason for forbidding them.
-    builder.add("**");
-    builder.add("...");
-    builder.add(".../abc");
-    builder.add("abc/...");
-    builder.add("abc/.../def");
-
-    return builder.build();
+        // These paths look really strange but Linux allows creating them.
+        // I cannot see any good reason for forbidding them.
+        "**",
+        "...",
+        ".../abc",
+        "abc/...",
+        "abc/.../def");
   }
 
   @ParameterizedTest
@@ -47,43 +42,40 @@ public class PathArgValidatorTest extends TestingContext {
         .throwsException(AbortException.class);
   }
 
-  public static ImmutableList<String> listOfInvalidProjectPaths() {
-    ImmutableList.Builder<String> builder = ImmutableList.builder();
+  public static Stream<String> listOfInvalidProjectPaths() {
+    return Stream.of(
+        "",
 
-    builder.add("");
+        "./",
+        "./.",
+        "././",
 
-    builder.add("./");
-    builder.add("./.");
-    builder.add("././");
+        "abc/",
+        "abc/def/",
+        "abc/def/ghi/",
 
-    builder.add("abc/");
-    builder.add("abc/def/");
-    builder.add("abc/def/ghi/");
+        "./abc",
+        "./abc/def",
+        "./abc/def/ghi",
 
-    builder.add("./abc");
-    builder.add("./abc/def");
-    builder.add("./abc/def/ghi");
+        "..",
+        "../",
+        "./../",
+        "../abc",
+        "abc/..",
+        "abc/../def",
+        "../..",
 
-    builder.add("..");
-    builder.add("../");
-    builder.add("./../");
-    builder.add("../abc");
-    builder.add("abc/..");
-    builder.add("abc/../def");
-    builder.add("../..");
+        "/",
+        "///",
 
-    builder.add("/");
-    builder.add("///");
+        "/abc",
+        "///abc",
 
-    builder.add("/abc");
-    builder.add("///abc");
+        "abc//",
+        "abc///",
 
-    builder.add("abc//");
-    builder.add("abc///");
-
-    builder.add("abc//def");
-    builder.add("abc///def");
-
-    return builder.build();
+        "abc//def",
+        "abc///def");
   }
 }
