b0470d05e2377aff12185ca53f3845fc033b12d7
Marcin Mikosik <marcin.mikosik@gmail.com>
2020-05-28 10:14:31 +0200

    refactored out LoggerModule from ConsoleModule so VersionCommand can run without instantiating ObjectDb and creating .smooth/

---
+++ src/main/java/org/smoothbuild/cli/console/LoggerModule.java
@@ -0,0 +1,21 @@
+package org.smoothbuild.cli.console;
+
+import org.smoothbuild.cli.taskmatcher.TaskMatcher;
+
+import com.google.inject.AbstractModule;
+
+public class LoggerModule extends AbstractModule {
+  private final Level logLevel;
+  private final TaskMatcher taskMatcher;
+
+  public LoggerModule(Level logLevel, TaskMatcher taskMatcher) {
+    this.logLevel = logLevel;
+    this.taskMatcher = taskMatcher;
+  }
+
+  @Override
+  protected void configure() {
+    bind(Level.class).toInstance(logLevel);
+    bind(TaskMatcher.class).toInstance(taskMatcher);
+  }
+}
