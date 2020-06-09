2f571d8a3f9d52c7ff43c30858377fef43863065
Marcin Mikosik <marcin.mikosik@gmail.com>
2020-06-09 14:46:53 +0200

    extracted ProcessBuilder code from AcceptanceTestCase to CommandExecutor

--- src/main/java/org/smoothbuild/util/CommandExecutor.java
+++ src/main/java/org/smoothbuild/util/CommandExecutor.java
@@ -1,14 +1,29 @@
 package org.smoothbuild.util;
 
+import static com.google.common.io.ByteStreams.toByteArray;
+import static java.nio.charset.StandardCharsets.UTF_8;
+
 import java.io.IOException;
-import java.util.List;
+import java.nio.file.Path;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class CommandExecutor {
-
-  public static int execute(List<String> command) throws InterruptedException, IOException {
-    ProcessBuilder processBuilder = new ProcessBuilder(command);
-    processBuilder.inheritIO();
+  public static CommandResult execute(Path workingDir, String[] allArguments)
+      throws IOException, InterruptedException, ExecutionException {
+    ProcessBuilder processBuilder = new ProcessBuilder(allArguments);
+    processBuilder.directory(workingDir.toFile());
     Process process = processBuilder.start();
-    return process.waitFor();
+    ExecutorService executor = Executors.newFixedThreadPool(2);
+    Future<byte[]> inputStream = executor.submit(() -> toByteArray(process.getInputStream()));
+    Future<byte[]> errorStream = executor.submit(() -> toByteArray(process.getErrorStream()));
+    int exitCode = process.waitFor();
+    String sysOut = new String(inputStream.get(), UTF_8);
+    String sysErr = new String(errorStream.get(), UTF_8);
+    return new CommandResult(exitCode, sysOut, sysErr);
   }
+
+  public static record CommandResult(int exitCode, String sysOut, String sysErr) {}
 }
