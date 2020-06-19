21c791599426fdc32355d54c1de06b9c161b570b
Marcin Mikosik <marcin.mikosik@gmail.com>
2020-06-19 11:16:18 +0200

    added TopologicalSorter

---
+++ src/main/java/org/smoothbuild/util/graph/SortTopologically.java
@@ -0,0 +1,230 @@
+package org.smoothbuild.util.graph;
+
+import static com.google.common.collect.ImmutableList.toImmutableList;
+import static com.google.common.collect.ImmutableMap.toImmutableMap;
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toSet;
+import static org.smoothbuild.util.graph.SortTopologically.Node.State.BEING_PROCESSED;
+import static org.smoothbuild.util.graph.SortTopologically.Node.State.NOT_VISITED;
+import static org.smoothbuild.util.graph.SortTopologically.Node.State.PROCESSED;
+
+import java.util.ArrayDeque;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+import com.google.common.collect.ImmutableList;
+
+public class SortTopologically {
+  public static <K, N, E> TopologicalSortingResult<K, N, E> sortTopologically(
+      Collection<GraphNode<K, N, E>> nodes) {
+    if (nodes.isEmpty()) {
+      return new TopologicalSortingResult<>(ImmutableList.of(), null);
+    }
+    ImmutableList<Node<K, N, E>> wrappedNodes = nodes.stream()
+        .map(Node::new)
+        .collect(toImmutableList());
+    return sortTopologicallyImpl(wrappedNodes);
+  }
+
+  public static <K, N, E> TopologicalSortingResult<K, N, E> sortTopologicallyImpl(
+      ImmutableList<Node<K, N, E>> nodes) {
+
+    // For each root node (node without incoming edges) algorithm processes it with the following
+    // VISITING sub-algorithm.
+    // (If there's no root node then any node is chosen as root and algorithm will detect a cycle.)
+    //
+    // VISITING sub-algorithm:
+    //   (1) Mark current node as BEING-PROCESSED.
+    //   (2) Add current node to the end of `currentPath`.
+    //   (3) For each node reachable via direct edge from that node:
+    //      IF current node is marked as BEING-PROCESSED
+    //        THEN End algorithm and return current  node and all nodes that succeed it in
+    //             `currentPath` as a cycle.
+    //      ELSE IF current node is marked as PROCESSED
+    //        THEN Do nothing.
+    //      ELSE
+    //        Invoke VISITING sub-algorithm for that reachable node. This is iterative
+    //        implementation of recursive algorithm. Recursion is simulated by stack handled in
+    //        `currentPath`.
+    //   (4) Mark current node as PROCESSED.
+    //   (5) Remove current node from `currentPath`.
+    //   (6) Add current node to the beginning of `resultSequence`.
+    //
+    // If algorithm completes and all nodes are marked as PROCESSED then `currentPath` contains
+    // topologically sorted nodes.
+    // If some nodes are not marked as PROCESSED then they form an island (or many islands)
+    // that doesn't have root node thus contains a cycle(s). Running algorithm just for those
+    // nodes will detect that cycle.
+
+    var rootKeys = findRootNodes(nodes);
+    if (rootKeys.isEmpty()) {
+      rootKeys = Set.of(nodes.iterator().next().key());
+    }
+
+    var keyToNode = nodes.stream().collect(toImmutableMap(Node::key, n -> n));
+    var currentPath = new LinkedList<PathElem<K, N, E>>();
+    var resultSequence = new ArrayDeque<Node<K, N, E>>(nodes.size());
+
+    for (K rootKey : rootKeys) {
+      addToPath(currentPath, keyToNode.get(rootKey));
+      while (!currentPath.isEmpty()) {
+        var pathEnd = currentPath.peekLast();
+        int edgeIndex = pathEnd.incrementAndGetEdgeIndex();
+        if (edgeIndex < pathEnd.node().edges().size()) {
+          K targetKey = pathEnd.node().edges().get(edgeIndex).targetKey();
+          var targetNode = keyToNode.get(targetKey);
+          switch (targetNode.state()) {
+            case NOT_VISITED:
+              addToPath(currentPath, targetNode);
+              break;
+            case BEING_PROCESSED:
+              return createCycleResult(currentPath, targetKey);
+            case PROCESSED:
+              break;
+          }
+        } else {
+          var processedNode = currentPath.removeLast().node();
+          processedNode.setState(PROCESSED);
+          resultSequence.addFirst(processedNode);
+        }
+      }
+    }
+
+    if (resultSequence.size() == nodes.size()) {
+      return createSortedResult(resultSequence);
+    } else {
+      return findCycleInUnprocessedNodes(nodes);
+    }
+  }
+
+  private static <K, N, E> Set<K> findRootNodes(Collection<Node<K, N, E>> nodes) {
+    var result = nodes.stream().map(Node::key).collect(toSet());
+    assertAllEdgesPointToExistingNodes(nodes, result);
+    for (var node : nodes) {
+      for (var edge : node.edges()) {
+        result.remove(edge.targetKey());
+      }
+    }
+    return result;
+  }
+
+  private static <K, N, E> void assertAllEdgesPointToExistingNodes(
+      Collection<Node<K, N, E>> nodes, Set<K> keys) {
+    for (var node : nodes) {
+      for (var edge : node.edges()) {
+        if (!keys.contains(edge.targetKey())) {
+          throw new IllegalArgumentException("Node '" + node.key()
+              + "' has edge pointing to node '" + edge.targetKey() + "' which does not exist.");
+        }
+      }
+    }
+  }
+
+  private static <K, N, E> void addToPath(LinkedList<PathElem<K, N, E>> currentPath,
+      Node<K, N, E> node) {
+    node.setState(BEING_PROCESSED);
+    currentPath.addLast(new PathElem<>(node));
+  }
+
+  private static <K, N, E> TopologicalSortingResult<K, N, E> createSortedResult(
+      ArrayDeque<Node<K, N, E>> resultSequence) {
+    var graphNodes = resultSequence.stream()
+        .map(Node::node)
+        .collect(toImmutableList());
+    return new TopologicalSortingResult<>(graphNodes, null);
+  }
+
+  private static <K, N, E> TopologicalSortingResult<K, N, E> createCycleResult(
+      LinkedList<PathElem<K, N, E>> currentPath, K key) {
+    var cycle = currentPath.stream()
+        .dropWhile(e -> !e.node().key().equals(key))
+        .map(elem -> elem.node().edges().get(elem.edgeIndex()))
+        .collect(toList());
+    return new TopologicalSortingResult<>(null, cycle);
+  }
+
+  private static <K, N, E> TopologicalSortingResult<K, N, E> findCycleInUnprocessedNodes(
+      ImmutableList<Node<K, N, E>> nodes) {
+    var notVisited = nodes.stream()
+        .filter(n -> n.state() == NOT_VISITED)
+        .collect(toImmutableList());
+    return sortTopologicallyImpl(notVisited);
+  }
+
+  private static class PathElem<K, N, E> {
+    private final Node<K, N, E> node;
+    private int edgeIndex;
+
+    public PathElem(Node<K, N, E> node) {
+      this.node = node;
+      this.edgeIndex = -1;
+    }
+
+    public Node<K, N, E> node() {
+      return node;
+    }
+
+    public int edgeIndex() {
+      return edgeIndex;
+    }
+
+    public int incrementAndGetEdgeIndex() {
+      return ++edgeIndex;
+    }
+  }
+
+  public static class Node<K, N, E> {
+    public enum State { NOT_VISITED, BEING_PROCESSED, PROCESSED }
+
+    private final GraphNode<K, N, E> node;
+    private State state;
+
+    public Node(GraphNode<K, N, E> node) {
+      this.node = node;
+      this.state = NOT_VISITED;
+    }
+
+    public GraphNode<K, N, E> node() {
+      return node;
+    }
+
+    public State state() {
+      return state;
+    }
+
+    public void setState(State state) {
+      this.state = state;
+    }
+
+    public K key() {
+      return node.key();
+    }
+
+    public ImmutableList<GraphEdge<E, K>> edges() {
+      return node.edges();
+    }
+
+    @Override
+    public int hashCode() {
+      return key().hashCode();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+      if (this == o) {
+        return true;
+      }
+      if (o instanceof Node<?, ?, ?> that) {
+        return this.node.equals(that.node);
+      }
+      return false;
+    }
+  }
+
+  public static record TopologicalSortingResult<K, N, E>(
+      List<GraphNode<K, N, E>>sorted,
+      List<GraphEdge<E, K>> cycle) {
+  }
+}
---
+++ src/main/test/org/smoothbuild/util/graph/SortTopologicallyTest.java
@@ -0,0 +1,340 @@
+package org.smoothbuild.util.graph;
+
+import static com.google.common.collect.Collections2.permutations;
+import static com.google.common.collect.ImmutableList.toImmutableList;
+import static com.google.common.truth.Truth.assertWithMessage;
+import static java.util.stream.Collectors.toList;
+import static org.junit.jupiter.api.Assertions.assertTimeoutPreemptively;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.smoothbuild.testing.common.AssertCall.assertCall;
+import static org.smoothbuild.util.Lists.map;
+import static org.smoothbuild.util.graph.SortTopologically.sortTopologically;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+public class SortTopologicallyTest {
+  @Nested
+  class sort {
+    @Test
+    public void empty() {
+      assertSortTopologically(
+          List.of(),
+          List.of(
+              List.of()));
+    }
+
+    @Test
+    public void single_node() {
+      var n1 = node(1);
+      assertSortTopologically(
+          List.of(n1),
+          List.of(
+              List.of(n1)));
+    }
+
+    @Test
+    public void two_single_nodes() {
+      var n1 = node(1);
+      var n2 = node(2);
+      assertSortTopologically(
+          List.of(n1, n2),
+          List.of(
+              List.of(n1)));
+    }
+
+    @Test
+    public void three_single_nodes() {
+      var n1 = node(1);
+      var n2 = node(2);
+      var n3 = node(3);
+      assertSortTopologically(
+          List.of(n1, n2, n3),
+          List.of(
+              List.of(n1)));
+    }
+
+    @Test
+    public void single_line() {
+      var n1 = node(1, List.of(2));
+      var n2 = node(2, List.of(3));
+      var n3 = node(3);
+      assertSortTopologically(
+          List.of(n1, n2, n3),
+          List.of(
+              List.of(n1, n2, n3)));
+    }
+
+    @Test
+    public void single_line_with_island() {
+      var n1 = node(1, List.of(2));
+      var n2 = node(2, List.of(3));
+      var n3 = node(3);
+      var n4 = node(4);
+      assertSortTopologically(
+          List.of(n1, n2, n3, n4),
+          List.of(
+              List.of(n1, n2, n3)));
+    }
+
+    @Test
+    public void two_lines() {
+      var n1 = node(1, List.of(2));
+      var n2 = node(2, List.of(3));
+      var n3 = node(3);
+      var n4 = node(4, List.of(5));
+      var n5 = node(5, List.of(6));
+      var n6 = node(6);
+      assertSortTopologically(
+          List.of(n1, n2, n3, n4, n5, n6),
+          List.of(
+              List.of(n1, n2, n3),
+              List.of(n4, n5, n6)));
+    }
+
+    @Test
+    public void two_lines_with_island() {
+      var n1 = node(1, List.of(2));
+      var n2 = node(2, List.of(3));
+      var n3 = node(3);
+      var n4 = node(4, List.of(5));
+      var n5 = node(5, List.of(6));
+      var n6 = node(6);
+      var n7 = node(7);
+      assertSortTopologically(
+          List.of(n1, n2, n3, n4, n5, n6, n7),
+          List.of(
+              List.of(n1, n2, n3),
+              List.of(n4, n5, n6)));
+    }
+
+    @Test
+    public void simple_tree() {
+      var n1 = node(1, List.of(2, 3));
+      var n2 = node(2);
+      var n3 = node(3);
+      assertSortTopologically(
+          List.of(n1, n2, n3),
+          List.of(
+              List.of(n1, n2),
+              List.of(n1, n3)));
+    }
+
+    @Test
+    public void two_simple_trees() {
+      var n1 = node(1, List.of(2, 3));
+      var n2 = node(2);
+      var n3 = node(3);
+      var n4 = node(4, List.of(5, 6));
+      var n5 = node(5);
+      var n6 = node(6);
+      assertSortTopologically(
+          List.of(n1, n2, n3, n4, n5, n6),
+          List.of(
+              List.of(n1, n2),
+              List.of(n1, n3),
+              List.of(n4, n5),
+              List.of(n4, n6)));
+    }
+
+    @Test
+    public void simple_tree_with_island() {
+      var n1 = node(1, List.of(2, 3));
+      var n2 = node(2);
+      var n3 = node(3);
+      var n4 = node(4);
+      assertSortTopologically(
+          List.of(n1, n2, n3, n4),
+          List.of(
+              List.of(n1, n2),
+              List.of(n1, n3)));
+    }
+
+    @Test
+    public void two_roots() {
+      var n1 = node(1, List.of(2, 3));
+      var n2 = node(2);
+      var n3 = node(3);
+      var n4 = node(4, List.of(2, 3));
+      assertSortTopologically(
+          List.of(n1, n2, n3, n4),
+          List.of(
+              List.of(n1, n2),
+              List.of(n1, n3),
+              List.of(n4, n2),
+              List.of(n4, n3)));
+    }
+
+    @Test
+    public void parallel_edges() {
+      var n1 = node(1, List.of(2, 2));
+      var n2 = node(2);
+      assertSortTopologically(
+          List.of(n1, n2),
+          List.of(
+              List.of(n1, n2)));
+    }
+
+    @Test
+    public void parallel_path_between_nodes() {
+      var n1 = node(1, List.of(2, 3));
+      var n2 = node(2, List.of(4));
+      var n3 = node(3, List.of(4));
+      var n4 = node(4);
+      assertSortTopologically(
+          List.of(n1, n2, n3, n4),
+          List.of(
+              List.of(n1, n2, n4),
+              List.of(n1, n3, n4)));
+    }
+  }
+
+  @Nested
+  class detects_cycle {
+    @Test
+    public void from_single_node_to_itself() {
+      var n1 = node(1, List.of(1));
+      assertCycleDetected(
+          List.of(n1),
+          List.of(n1));
+    }
+
+    @Test
+    public void between_two_nodes_when_no_root_exists() {
+      var n1 = node(1, List.of(2));
+      var n2 = node(2, List.of(1));
+      assertCycleDetected(
+          List.of(n1, n2),
+          List.of(n1, n2));
+    }
+
+    @Test
+    public void between_two_nodes_when_island_exists() {
+      var n1 = node(1);
+      var n2 = node(2, List.of(3));
+      var n3 = node(3, List.of(2));
+      assertCycleDetected(
+          List.of(n1, n2, n3),
+          List.of(n2, n3)
+      );
+    }
+
+    @Test
+    public void between_two_nodes_when_root_exists() {
+      var n1 = node(1, List.of(2));
+      var n2 = node(2, List.of(3));
+      var n3 = node(3, List.of(2));
+      var nodes = List.of(n1, n2, n3);
+      assertCycleDetected(
+          nodes,
+          List.of(n2, n3)
+      );
+    }
+
+    private void assertCycleDetected(List<GraphNode<Integer, String, String>> nodes,
+        List<GraphNode<Integer, String, String>> expectedCycle) {
+      var permutations = permutations(nodes);
+      for (List<GraphNode<Integer, String, String>> permutation : permutations) {
+        var actual = sortTopologically(permutation).cycle();
+
+        List<GraphEdge<String, Integer>> cycle = buildCycle(expectedCycle);
+        for (int i = 0; i < cycle.size(); i++) {
+          var rotated = new ArrayList<>(cycle);
+          Collections.rotate(rotated, i);
+          if (actual.equals(rotated)) {
+            return;
+          }
+        }
+        fail("For tested permutation: " + map(permutation, GraphNode::key) + "\n"
+            + "actual cycle = " + actual + "\n"
+            + "doesn't match expected cycle (even rotated) = " + cycle);
+      }
+    }
+
+    private List<GraphEdge<String, Integer>> buildCycle(
+        List<GraphNode<Integer, String, String>> nodes) {
+      return nodes.stream()
+          .map(n -> n.edges().get(0))
+          .collect(toList());
+    }
+  }
+
+  @Nested
+  class fails {
+    @Test
+    public void when_edge_points_to_nonexistent_node() {
+      var n1 = node(1, List.of(2));
+      var nodes = List.of(n1);
+
+      assertCall(() -> sortTopologically(nodes))
+          .throwsException(new IllegalArgumentException(
+              "Node '1' has edge pointing to node '2' which does not exist."));
+    }
+
+    @Test
+    public void when_two_nodes_has_same_key() {
+      var n1 = node(1);
+      var n1b = node(1);
+      var nodes = List.of(n1, n1b);
+
+      assertCall(() -> sortTopologically(nodes))
+          .throwsException(IllegalArgumentException.class);
+    }
+  }
+
+  @Test
+  public void sorting_algorithm_has_linear_complexity() {
+    AtomicInteger key = new AtomicInteger();
+    var topLayer = createLayer(key, List.of());
+    var nodes = new ArrayList<>(topLayer);
+    int layerCount = 100;
+    for (int i = 0; i < layerCount; i++) {
+      topLayer = createLayer(key, map(topLayer, GraphNode::key));
+      nodes.addAll(topLayer);
+    }
+    assertTimeoutPreemptively(Duration.ofSeconds(5), () -> sortTopologically(nodes));
+  }
+
+  private ArrayList<GraphNode<Integer, String, String>> createLayer(AtomicInteger key,
+      List<Integer> edges) {
+    var layer = new ArrayList<GraphNode<Integer, String, String>>();
+    for (int j = 0; j < 10; j++) {
+      layer.add(node(key.getAndIncrement(), edges));
+    }
+    return layer;
+  }
+
+  private static void assertSortTopologically(List<GraphNode<Integer, String, String>> nodes,
+      List<List<GraphNode<Integer, String, String>>> expectedOrders) {
+    var permutations = permutations(nodes);
+    for (List<GraphNode<Integer, String, String>> permutation : permutations) {
+      var actual = sortTopologically(permutation).sorted();
+
+      String message = "tested permutation: " + map(permutation, GraphNode::key);
+      assertWithMessage(message).that(actual).containsExactlyElementsIn(nodes);
+      for (List<GraphNode<Integer, String, String>> expectedOrder : expectedOrders) {
+        assertWithMessage(message)
+            .that(actual)
+            .containsAtLeastElementsIn(expectedOrder)
+            .inOrder();
+      }
+    }
+  }
+
+  private static GraphNode<Integer, String, String> node(int n1) {
+    return node(n1, new ArrayList<>());
+  }
+
+  private static GraphNode<Integer, String, String> node(Integer key, List<Integer> targetKeys) {
+    var edges = targetKeys.stream()
+        .map(k -> new GraphEdge<>("->" + k, k))
+        .collect(toImmutableList());
+    return new GraphNode<>(key, "node" + key, edges);
+  }
+}
