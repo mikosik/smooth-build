2a7a88a21f2f42da5e8668268d12ab898963e64d
Marcin Mikosik <marcin.mikosik@gmail.com>
2020-06-25 17:10:41 +0200

    fixed style in Computer

--- src/main/test/org/smoothbuild/exec/task/base/ComputationHashTest.java
+++ src/main/test/org/smoothbuild/exec/task/base/ComputationHashTest.java
@@ -32,8 +32,8 @@ public class ComputationHashTest extends TestingContext {
   public void hashes_of_computations_with_same_algorithm_runtime_and_input_are_equal() {
     Algorithm algorithm = computation(Hash.of(1));
     Input input = input(list(string("input")));
-    assertThat(computationHash(algorithm, input, Hash.of(13)))
-        .isEqualTo(computationHash(algorithm, input, Hash.of(13)));
+    assertThat(computationHash(Hash.of(13), algorithm, input))
+        .isEqualTo(computationHash(Hash.of(13), algorithm, input));
   }
 
   @Test
@@ -41,16 +41,16 @@ public class ComputationHashTest extends TestingContext {
     Algorithm algorithm = computation(Hash.of(1));
     Algorithm algorithm2 = computation(Hash.of(2));
     Input input = input(list(string("input")));
-    assertThat(computationHash(algorithm, input, Hash.of(13)))
-        .isNotEqualTo(computationHash(algorithm2, input, Hash.of(13)));
+    assertThat(computationHash(Hash.of(13), algorithm, input))
+        .isNotEqualTo(computationHash(Hash.of(13), algorithm2, input));
   }
 
   @Test
   public void hashes_of_computations_with_same_algorithm_and_input_but_different_runtime_are_not_equal() {
     Algorithm algorithm = computation(Hash.of(1));
     Input input = input(list(string("input")));
-    assertThat(computationHash(algorithm, input, Hash.of(13)))
-        .isNotEqualTo(computationHash(algorithm, input, Hash.of(14)));
+    assertThat(computationHash(Hash.of(13), algorithm, input))
+        .isNotEqualTo(computationHash(Hash.of(14), algorithm, input));
   }
 
   @Test
@@ -58,15 +58,15 @@ public class ComputationHashTest extends TestingContext {
     Algorithm algorithm = computation(Hash.of(1));
     Input input = input(list(string("input")));
     Input input2 = input(list(string("input2")));
-    assertThat(computationHash(algorithm, input, Hash.of(13)))
-        .isNotEqualTo(computationHash(algorithm, input2, Hash.of(13)));
+    assertThat(computationHash(Hash.of(13), algorithm, input))
+        .isNotEqualTo(computationHash(Hash.of(13), algorithm, input2));
   }
 
   @Test
   public void hash_of_computation_with_array_algorithm_and_empty_input_is_stable() {
     Algorithm algorithm = new ArrayLiteralAlgorithm(arrayType(stringType()));
     Input input = input(list());
-    assertThat(computationHash(algorithm, input, Hash.of(13)))
+    assertThat(computationHash(Hash.of(13), algorithm, input))
         .isEqualTo(Hash.decode("16457f3457ec260bb4be0161933c32010b162123"));
   }
 
@@ -74,7 +74,7 @@ public class ComputationHashTest extends TestingContext {
   public void hash_of_computation_with_array_algorithm_and_non_empty_input_is_stable() {
     Algorithm algorithm = new ArrayLiteralAlgorithm(arrayType(stringType()));
     Input input = input(list(string("abc"), string("def")));
-    assertThat(computationHash(algorithm, input, Hash.of(13)))
+    assertThat(computationHash(Hash.of(13), algorithm, input))
         .isEqualTo(Hash.decode("8dc297a86b455810ee2558f9e6eb8893c524f861"));
   }
 
@@ -82,7 +82,7 @@ public class ComputationHashTest extends TestingContext {
   public void hash_of_computation_with_native_call_algorithm_and_empty_input_is_stable() {
     Algorithm algorithm = new NativeCallAlgorithm(stringType(), mockNativeFunction());
     Input input = input(list());
-    assertThat(computationHash(algorithm, input, Hash.of(13)))
+    assertThat(computationHash(Hash.of(13), algorithm, input))
         .isEqualTo(Hash.decode("d66663fdde3d70dda784d5b035578cbd7bd6c184"));
   }
 
@@ -90,7 +90,7 @@ public class ComputationHashTest extends TestingContext {
   public void hash_of_computation_with_native_call_algorithm_and_non_empty_input_is_stable() {
     Algorithm algorithm = new NativeCallAlgorithm(stringType(), mockNativeFunction());
     Input input = input(list(string("abc"), string("def")));
-    assertThat(computationHash(algorithm, input, Hash.of(13)))
+    assertThat(computationHash(Hash.of(13), algorithm, input))
         .isEqualTo(Hash.decode("425c7c9760d1e93908cc13c04bdb4092eee8561d"));
   }
 
@@ -98,7 +98,7 @@ public class ComputationHashTest extends TestingContext {
   public void hash_of_computation_with_convert_from_nothing_algorithm_and_one_element_input_is_stable() {
     Algorithm algorithm = new ConvertAlgorithm(stringType(), stringType());
     Input input = input(list(string("abc")));
-    assertThat(computationHash(algorithm, input, Hash.of(13)))
+    assertThat(computationHash(Hash.of(13), algorithm, input))
         .isEqualTo(Hash.decode("1ec66039449159837c5e5e82ce3da7bbf89ed417"));
   }
 
@@ -106,7 +106,7 @@ public class ComputationHashTest extends TestingContext {
   public void hash_of_computation_with_constructor_call_algorithm_and_empty_input_is_stable() {
     Algorithm algorithm = new ConstructorCallAlgorithm(constructor());
     Input input = input(list());
-    assertThat(computationHash(algorithm, input, Hash.of(13)))
+    assertThat(computationHash(Hash.of(13), algorithm, input))
         .isEqualTo(Hash.decode("af8f490d36d092f1c660585e557ff988b1048c17"));
   }
 
@@ -114,7 +114,7 @@ public class ComputationHashTest extends TestingContext {
   public void hash_of_computation_with_constructor_call_algorithm_and_one_element_input_is_stable() {
     Algorithm algorithm = new ConstructorCallAlgorithm(constructor());
     Input input = input(list(string("abc")));
-    assertThat(computationHash(algorithm, input, Hash.of(13)))
+    assertThat(computationHash(Hash.of(13), algorithm, input))
         .isEqualTo(Hash.decode("0df5916d25416379005a8e4700fca183b7ae86da"));
   }
 
@@ -122,7 +122,7 @@ public class ComputationHashTest extends TestingContext {
   public void hash_of_computation_with_constructor_call_algorithm_and_two_elements_input_is_stable() {
     Algorithm algorithm = new ConstructorCallAlgorithm(constructor());
     Input input = input(list(string("abc"), string("def")));
-    assertThat(computationHash(algorithm, input, Hash.of(13)))
+    assertThat(computationHash(Hash.of(13), algorithm, input))
         .isEqualTo(Hash.decode("55076a75491bc119310d29f3d4239ebeba07360f"));
   }
 
@@ -134,7 +134,7 @@ public class ComputationHashTest extends TestingContext {
   public void hash_of_computation_with_accessor_call_algorithm_and_one_element_input_is_stable() {
     Algorithm algorithm = new AccessorCallAlgorithm(accessor());
     Input input = input(list(string("abc")));
-    assertThat(computationHash(algorithm, input, Hash.of(13)))
+    assertThat(computationHash(Hash.of(13), algorithm, input))
         .isEqualTo(Hash.decode("d4ef1a2529ad47aca82b72dd736dccc1ea4c1f01"));
   }
 
