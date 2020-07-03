fda02ff450d1e2c73c1d3e4d67f07fa159cb6aff
Marcin Mikosik <marcin.mikosik@gmail.com>
2020-07-03 14:33:49 +0200

    removed no longer used Node.get()/set()

--- src/main/java/org/smoothbuild/parse/ast/Node.java
+++ src/main/java/org/smoothbuild/parse/ast/Node.java
@@ -1,8 +1,5 @@
 package org.smoothbuild.parse.ast;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.NoSuchElementException;
 import java.util.Optional;
 
 import org.smoothbuild.lang.base.Location;
@@ -10,7 +7,6 @@ import org.smoothbuild.lang.object.type.Type;
 
 public class Node {
   private final Location location;
-  private Map<Class<?>, Object> map;
   private Optional<Type> type;
 
   public Node(Location location) {
@@ -31,26 +27,5 @@ public class Node {
 
   public void setType(Optional<Type> type) {
     this.type = type;
-  }
-
-  public <T> T get(Class<T> clazz) {
-    if (!map().containsKey(clazz)) {
-      throw new NoSuchElementException("Cannot find attribute of type" + clazz.getName()
-          + " in AST Node " + this.getClass().getCanonicalName() + " " + toString());
-    }
-    @SuppressWarnings("unchecked")
-    T result = (T) map().get(clazz);
-    return result;
-  }
-
-  public <T> void set(Class<T> clazz, T value) {
-    map().put(clazz, value);
-  }
-
-  private Map<Class<?>, Object> map() {
-    if (map == null) {
-      map = new HashMap<>();
-    }
-    return map;
   }
 }
