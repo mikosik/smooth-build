ed70730f43a071b7686529f350ce3c0c33071037
Marcin Mikosik <marcin.mikosik@gmail.com>
2020-07-03 11:38:57 +0200

    added StringNode.calculateUnescaped()/unescapedValue()

--- src/main/java/org/smoothbuild/parse/ast/StringNode.java
+++ src/main/java/org/smoothbuild/parse/ast/StringNode.java
@@ -1,9 +1,12 @@
 package org.smoothbuild.parse.ast;
 
 import org.smoothbuild.lang.base.Location;
+import org.smoothbuild.util.Strings;
+import org.smoothbuild.util.UnescapingFailedException;
 
 public class StringNode extends ExprNode {
   private final String value;
+  private String unescaped;
 
   public StringNode(String value, Location location) {
     super(location);
@@ -12,5 +15,13 @@ public class StringNode extends ExprNode {
 
   public String value() {
     return value;
+  }
+
+  public String unescapedValue() {
+    return unescaped;
+  }
+
+  public void calculateUnescaped() throws UnescapingFailedException {
+    unescaped = Strings.unescaped(value);
   }
 }
