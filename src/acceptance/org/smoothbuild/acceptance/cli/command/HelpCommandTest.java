0bbe932eddeff39af2a0a6b4600c9c6f7b06f1fd
Marcin Mikosik <marcin.mikosik@gmail.com>
2020-07-10 20:38:15 +0200

    updated library dependencies to latest versions

--- src/acceptance/org/smoothbuild/acceptance/cli/command/HelpCommandTest.java
+++ src/acceptance/org/smoothbuild/acceptance/cli/command/HelpCommandTest.java
@@ -19,8 +19,6 @@ public class HelpCommandTest extends AcceptanceTestCase {
         times significantly.
         More info at https://github.com/mikosik/smooth-build/blob/master/doc/tutorial.md
         
-        Options:
-        
         Commands:
           build    Build artifact(s) by running specified function(s)
           clean    Remove all cached objects and artifacts calculated during all
