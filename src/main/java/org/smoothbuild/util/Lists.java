310225950104d3c38a4c5ab81a7e1c8de1924a0c
Marcin Mikosik <marcin.mikosik@gmail.com>
2020-07-25 00:18:06 +0200

    refactored Lists/Sets filter()/map() methods to return guava's immutable collections

--- src/main/java/org/smoothbuild/util/Lists.java
+++ src/main/java/org/smoothbuild/util/Lists.java
@@ -1,12 +1,14 @@
 package org.smoothbuild.util;
 
-import static java.util.stream.Collectors.toList;
+import static com.google.common.collect.ImmutableList.toImmutableList;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Function;
 import java.util.function.Predicate;
+
+import com.google.common.collect.ImmutableList;
 
 public class Lists {
   @SafeVarargs
@@ -20,18 +22,19 @@ public class Lists {
     return result;
   }
 
-  public static <E> List<E> filter(List<E> list, Predicate<? super E> predicate) {
+  public static <E> ImmutableList<E> filter(List<E> list, Predicate<? super E> predicate) {
     return list
         .stream()
         .filter(predicate)
-        .collect(toList());
+        .collect(toImmutableList());
   }
 
-  public static <E, R> List<R> map(List<E> list, Function<? super E, ? extends R> function) {
+  public static <E, R> ImmutableList<R> map(
+      List<E> list, Function<? super E, ? extends R> function) {
     return list
         .stream()
         .map(function)
-        .collect(toList());
+        .collect(toImmutableList());
   }
 
   public static <E> List<E> sane(List<E> list) {
--- src/main/java/org/smoothbuild/util/Sets.java
+++ src/main/java/org/smoothbuild/util/Sets.java
@@ -1,5 +1,6 @@
 package org.smoothbuild.util;
 
+import static com.google.common.collect.ImmutableSet.toImmutableSet;
 import static java.util.stream.Collectors.toSet;
 
 import java.util.Arrays;
@@ -7,23 +8,25 @@ import java.util.Set;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
+import com.google.common.collect.ImmutableSet;
+
 public class Sets {
   @SafeVarargs
   public static <E> Set<E> set(E... elements) {
     return Arrays.stream(elements).collect(toSet());
   }
 
-  public static <E, R> Set<R> map(Set<E> set, Function<? super E, ? extends R> function) {
+  public static <E, R> ImmutableSet<R> map(Set<E> set, Function<? super E, ? extends R> function) {
     return set
         .stream()
         .map(function)
-        .collect(toSet());
+        .collect(toImmutableSet());
   }
 
-  public static <E> Set<E> filter(Set<E> set, Predicate<? super E> predicate) {
+  public static <E> ImmutableSet<E> filter(Set<E> set, Predicate<? super E> predicate) {
     return set
         .stream()
         .filter(predicate)
-        .collect(toSet());
+        .collect(toImmutableSet());
   }
 }
