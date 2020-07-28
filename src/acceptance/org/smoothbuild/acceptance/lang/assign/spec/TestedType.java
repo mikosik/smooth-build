58304e0d7555b96c19fffa7dd628073b82009fd6
Marcin Mikosik <marcin.mikosik@gmail.com>
2020-07-28 12:57:14 +0200

    refactored StructTest to use TestedType

--- src/acceptance/org/smoothbuild/acceptance/lang/assign/spec/TestedType.java
+++ src/acceptance/org/smoothbuild/acceptance/lang/assign/spec/TestedType.java
@@ -6,6 +6,10 @@ import static org.smoothbuild.lang.base.type.Types.nothing;
 import static org.smoothbuild.lang.base.type.Types.string;
 import static org.smoothbuild.util.Lists.list;
 
+import java.util.List;
+
+import com.google.common.collect.ImmutableList;
+
 public class TestedType {
   public static final TestedType BLOB = new TestedType(
       blob().name(),
@@ -27,6 +31,11 @@ public class TestedType {
       "'abc'",
       "abc"
   );
+  public static final TestedType STRUCT_WITH_BLOB = new TestedType(
+      "Data",
+      "data(0xAB)",
+      null,
+      "Data{ Blob value }");
   public static final TestedType STRUCT_WITH_BOOL = new TestedType(
       "Flag",
       "flag(true())",
@@ -46,6 +55,7 @@ public class TestedType {
       list()
   );
   public static final TestedType STRING_ARRAY = array(STRING);
+  public static final TestedType STRUCT_WITH_BLOB_ARRAY = array(STRUCT_WITH_BLOB);
   public static final TestedType STRUCT_WITH_BOOL_ARRAY = array(STRUCT_WITH_BOOL);
   public static final TestedType STRUCT_WITH_STRING_ARRAY = array(STRUCT_WITH_STRING);
 
@@ -53,8 +63,33 @@ public class TestedType {
   public static final TestedType BOOL_ARRAY2 = array(BOOL_ARRAY);
   public static final TestedType NOTHING_ARRAY2 = array(NOTHING_ARRAY);
   public static final TestedType STRING_ARRAY2 = array(STRING_ARRAY);
+  public static final TestedType STRUCT_WITH_BLOB_ARRAY2 = array(STRUCT_WITH_BLOB_ARRAY);
   public static final TestedType STRUCT_WITH_BOOL_ARRAY2 = array(STRUCT_WITH_BOOL_ARRAY);
   public static final TestedType STRUCT_WITH_STRING_ARRAY2 = array(STRUCT_WITH_STRING_ARRAY);
+
+  public static final List<TestedType> TESTED_TYPES = ImmutableList.of(
+      BLOB,
+      BOOL,
+      NOTHING,
+      STRING,
+      STRUCT_WITH_BLOB,
+      STRUCT_WITH_BOOL,
+      STRUCT_WITH_STRING,
+      BLOB_ARRAY,
+      BOOL_ARRAY,
+      NOTHING_ARRAY,
+      STRING_ARRAY,
+      STRUCT_WITH_BLOB_ARRAY,
+      STRUCT_WITH_BOOL_ARRAY,
+      STRUCT_WITH_STRING_ARRAY,
+      BLOB_ARRAY2,
+      BOOL_ARRAY2,
+      NOTHING_ARRAY2,
+      STRING_ARRAY2,
+      STRUCT_WITH_BLOB_ARRAY2,
+      STRUCT_WITH_BOOL_ARRAY2,
+      STRUCT_WITH_STRING_ARRAY2
+  );
 
   public final String name;
   public final String literal;
