f2f96b08911732209b87edb6843a6623da4d0aa4
Marcin Mikosik <marcin.mikosik@gmail.com>
2020-07-28 12:24:18 +0200

    fixed naming in codebase sstring -> rstring

--- src/main/test/org/smoothbuild/record/base/StringTest.java
+++ src/main/test/org/smoothbuild/record/base/StringTest.java
@@ -10,7 +10,7 @@ public class StringTest extends TestingContext {
   private final String otherString = "my string 2";
 
   @Test
-  public void spec_of_sstring_is_sstring() {
+  public void spec_of_rstring_is_rstring() {
     assertThat(string(string).spec())
         .isEqualTo(stringSpec());
   }
@@ -22,82 +22,82 @@ public class StringTest extends TestingContext {
   }
 
   @Test
-  public void jvalue_returns_empty_java_string_for_empty_sstring() {
+  public void jvalue_returns_empty_java_string_for_empty_rstring() {
     assertThat(string("").jValue())
         .isEqualTo("");
   }
 
   @Test
-  public void sstrings_with_equal_values_are_equal() {
+  public void rstrings_with_equal_values_are_equal() {
     assertThat(string(string))
         .isEqualTo(string(string));
   }
 
   @Test
-  public void sstrings_with_different_values_are_not_equal() {
+  public void rstrings_with_different_values_are_not_equal() {
     assertThat(string(string))
         .isNotEqualTo(string(otherString));
   }
 
   @Test
-  public void hash_of_sstrings_with_equal_values_is_the_same() {
+  public void hash_of_rstrings_with_equal_values_is_the_same() {
     assertThat(string(string).hash())
         .isEqualTo(string(string).hash());
   }
 
   @Test
-  public void hash_of_sstrings_with_different_values_is_not_the_same() {
+  public void hash_of_rstrings_with_different_values_is_not_the_same() {
     assertThat(string(string).hash())
         .isNotEqualTo(string(otherString).hash());
   }
 
   @Test
-  public void hash_code_of_sstrings_with_equal_values_is_the_same() {
+  public void hash_code_of_rstrings_with_equal_values_is_the_same() {
     assertThat(string(string).hashCode())
         .isEqualTo(string(string).hashCode());
   }
 
   @Test
-  public void hash_code_of_sstrings_with_different_values_is_not_the_same() {
+  public void hash_code_of_rstrings_with_different_values_is_not_the_same() {
     assertThat(string(string).hashCode())
         .isNotEqualTo(string(otherString).hashCode());
   }
 
   @Test
-  public void sstring_can_be_read_back_by_hash() {
-    RString sstring = string(string);
-    assertThat(recordDbOther().get(sstring.hash()))
-        .isEqualTo(sstring);
+  public void rstring_can_be_read_back_by_hash() {
+    RString rstring = string(string);
+    assertThat(recordDbOther().get(rstring.hash()))
+        .isEqualTo(rstring);
   }
 
   @Test
-  public void sstring_read_back_by_hash_has_same_javlue() {
-    RString sstring = string(string);
-    assertThat(((RString) recordDbOther().get(sstring.hash())).jValue())
+  public void rstring_read_back_by_hash_has_same_javlue() {
+    RString rstring = string(string);
+    assertThat(((RString) recordDbOther().get(rstring.hash())).jValue())
         .isEqualTo(string);
   }
 
   @Test
   public void to_string_contains_string_value() {
-    RString sstring = string(string);
-    assertThat(sstring.toString())
+    RString rstring = string(string);
+    assertThat(rstring.toString())
         .isEqualTo("""
-            "my string":""" + sstring.hash());
+            "my string":""" + rstring.hash());
   }
 
   @Test
   public void to_string_contains_shortened_string_value_for_long_strings() {
-    RString sstring = string("123456789012345678901234567890");
-    assertThat(sstring.toString())
+    RString rstring = string("123456789012345678901234567890");
+    assertThat(rstring.toString())
         .isEqualTo("""
-            "1234567890123456789012345"...:""" + sstring.hash());
+            "1234567890123456789012345"...:""" + rstring.hash());
   }
 
   @Test
   public void to_string_contains_properly_escaped_special_characters() {
-    RString sstring = string("\t \b \n \r \f \" \\");
-    assertThat(sstring.toString())
+    RString rstring = string("\t \b \n \r \f \" \\");
+    assertThat(rstring.toString())
         .isEqualTo("""
-            "\\t \\b \\n \\r \\f \\" \\\\":""" + sstring.hash());
+            "\\t \\b \\n \\r \\f \\" \\\\":""" + rstring.hash());
   }
 }
