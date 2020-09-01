4007c42e332577efce9fe7ff0a55c837072cd28f
Marcin Mikosik <marcin.mikosik@gmail.com>
2020-09-01 09:39:32 +0200

    added more tests to ReferenceTest, fixed found bug in FindSemanticErrors

--- src/acceptance/org/smoothbuild/acceptance/lang/ReferenceTest.java
+++ src/acceptance/org/smoothbuild/acceptance/lang/ReferenceTest.java
@@ -1,16 +1,63 @@
 package org.smoothbuild.acceptance.lang;
 
+import static com.google.common.truth.Truth.assertThat;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.smoothbuild.acceptance.AcceptanceTestCase;
 
 public class ReferenceTest extends AcceptanceTestCase {
-  @Test
-  public void unknown_reference_causes_error() throws Exception {
-    createUserModule(
-        "  func(String existingParam) = unknown;  ",
-        "  result = 'def';                        ");
-    runSmoothBuild("result");
-    assertFinishedWithError();
-    assertSysOutContainsParseError(1, "'unknown' is undefined.\n");
+  @Nested
+  class reference_to_local {
+    @Test
+    public void value_succeeds() throws IOException {
+      createUserModule("""
+          String myValue = "abc";
+          result = myValue;
+          """);
+      runSmoothBuild("result");
+      assertFinishedWithSuccess();
+      assertThat(artifactFileContentAsString("result"))
+          .isEqualTo("abc");
+    }
+
+    @Test
+    public void function_causes_error() throws IOException {
+      createUserModule("""
+          String myFunction() = "abc";
+          result = myFunction;
+          """);
+      runSmoothList();
+      assertFinishedWithError();
+      assertSysOutContainsParseError(
+          2, "'myFunction' is a function and cannot be accessed as a value.");
+    }
+  }
+
+  @Nested
+  class reference_to_imported {
+    @Test
+    public void value_succeeds() throws IOException {
+      createUserModule("""
+          result = true;
+          """);
+      runSmoothBuild("result");
+      assertFinishedWithSuccess();
+      assertThat(artifactAsBoolean("result"))
+          .isEqualTo(true);
+    }
+
+    @Test
+    public void function_causes_error() throws IOException {
+      createUserModule("""
+          result = and;
+          """);
+      runSmoothList();
+      assertFinishedWithError();
+      assertSysOutContainsParseError(
+          1, "'and' is a function and cannot be accessed as a value.");
+    }
   }
 }
