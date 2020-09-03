0211c24059b1d4299b71b5d23d99bf7f4fedc4ec
Marcin Mikosik <marcin.mikosik@gmail.com>
2020-09-03 13:16:48 +0200

    added ParameterDefaultValueConversionTest

---
+++ src/acceptance/org/smoothbuild/acceptance/lang/assign/convert/ParameterDefaultValueConversionTest.java
@@ -0,0 +1,14 @@
+package org.smoothbuild.acceptance.lang.assign.convert;
+
+import static org.smoothbuild.util.Strings.unlines;
+
+public class ParameterDefaultValueConversionTest extends AbstractConversionTestCase {
+  @Override
+  protected String createTestScript(ConversionTestSpec testSpec) {
+    return unlines(
+        "  fun(" + testSpec.target.name + " param = " + testSpec.source.literal + ") = param; ",
+        "  result = fun();  ",
+        testSpec.declarations()
+    );
+  }
+}
