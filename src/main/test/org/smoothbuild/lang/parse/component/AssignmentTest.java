9deeadc4bcccc36c85261b173f860e30007eb384
Marcin Mikosik <marcin.mikosik@gmail.com>
2020-09-23 19:46:48 +0200

    merged AssignmentGenericTest with AssignmentTest

--- src/main/test/org/smoothbuild/lang/parse/component/AssignmentTest.java
+++ src/main/test/org/smoothbuild/lang/parse/component/AssignmentTest.java
@@ -2,6 +2,7 @@ package org.smoothbuild.lang.parse.component;
 
 import static org.smoothbuild.lang.base.type.TestedAssignmentSpec.assignment_test_specs;
 import static org.smoothbuild.lang.base.type.TestedAssignmentSpec.assignment_without_generics_test_specs;
+import static org.smoothbuild.lang.base.type.TestedAssignmentSpec.parameter_assignment_test_specs;
 import static org.smoothbuild.lang.parse.component.TestModuleLoader.module;
 import static org.smoothbuild.util.Strings.unlines;
 
@@ -11,6 +12,7 @@ import java.util.Map;
 import java.util.stream.Stream;
 
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.smoothbuild.lang.base.type.TestedAssignmentSpec;
 import org.smoothbuild.lang.base.type.TestedType;
@@ -95,6 +97,30 @@ public class AssignmentTest {
           .loadsWithError(2, "In call to `myFunction`: Cannot assign argument of type "
               + source.q() + " to parameter `param` of type " + target.q() + ".");
     }
+  }
+
+  @ParameterizedTest
+  @MethodSource("parameter_assignment_test_data")
+  public void generic_parameter_assignment(
+      boolean allowed, TestedType targetType, TestedType sourceType) {
+    String target = targetType.name();
+    String source = sourceType.name();
+    TestModuleLoader module = module(unlines(
+        "String innerFunction(" + target + " target);                          ",
+        "outerFunction(" + source + " source) = innerFunction(target=source);  ",
+        targetType.declarations(),
+        sourceType.declarations()));
+    if (allowed) {
+      module.loadsSuccessfully();
+    } else {
+      module.loadsWithError(2,
+          "In call to `innerFunction`: Cannot assign argument of type " + sourceType.q()
+              + " to parameter `target` of type " + targetType.q() + ".");
+    }
+  }
+
+  private static Stream<Arguments> parameter_assignment_test_data() {
+    return parameter_assignment_test_specs();
   }
 
   @ParameterizedTest
