bd47f8cb822361c9c618db9546c778f778ebf776
Marcin Mikosik <marcin.mikosik@gmail.com>
2020-09-23 19:42:02 +0200

    moved AssignmentGenericTest.generic_parameter_assignment_test_data() to TestedAssignmentSpec

--- src/testing/java/org/smoothbuild/lang/base/type/TestedAssignmentSpec.java
+++ src/testing/java/org/smoothbuild/lang/base/type/TestedAssignmentSpec.java
@@ -1,5 +1,6 @@
 package org.smoothbuild.lang.base.type;
 
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 import static org.smoothbuild.lang.base.type.TestedType.A;
 import static org.smoothbuild.lang.base.type.TestedType.A_ARRAY;
 import static org.smoothbuild.lang.base.type.TestedType.A_ARRAY2;
@@ -26,6 +27,8 @@ import static org.smoothbuild.lang.base.type.TestedType.STRUCT_WITH_STRING_ARRAY
 import static org.smoothbuild.lang.base.type.TestedType.STRUCT_WITH_STRING_ARRAY2;
 
 import java.util.stream.Stream;
+
+import org.junit.jupiter.params.provider.Arguments;
 
 public class TestedAssignmentSpec extends TestedAssignment {
   public final boolean allowed;
@@ -472,5 +475,56 @@ public class TestedAssignmentSpec extends TestedAssignment {
         illegalAssignment(STRUCT_WITH_STRING_ARRAY2, STRUCT_WITH_BOOL_ARRAY),
         illegalAssignment(STRUCT_WITH_STRING_ARRAY2, STRUCT_WITH_BOOL_ARRAY2)
     );
+  }
+
+  public static Stream<Arguments> parameter_assignment_test_specs() {
+    return Stream.of(
+        arguments(true, A, STRING),
+        arguments(true, A, STRUCT_WITH_STRING),
+        arguments(true, A, NOTHING),
+        arguments(true, A, A),
+        arguments(true, A, B),
+        arguments(true, A, STRING_ARRAY),
+        arguments(true, A, STRUCT_WITH_STRING_ARRAY),
+        arguments(true, A, NOTHING_ARRAY),
+        arguments(true, A, A_ARRAY),
+        arguments(true, A, B_ARRAY),
+        arguments(true, A, STRING_ARRAY2),
+        arguments(true, A, STRUCT_WITH_STRING_ARRAY2),
+        arguments(true, A, NOTHING_ARRAY2),
+        arguments(true, A, A_ARRAY2),
+        arguments(true, A, B_ARRAY2),
+
+        arguments(false, A_ARRAY, STRING),
+        arguments(false, A_ARRAY, STRUCT_WITH_STRING),
+        arguments(true, A_ARRAY, NOTHING),
+        arguments(false, A_ARRAY, A),
+        arguments(false, A_ARRAY, B),
+        arguments(true, A_ARRAY, STRING_ARRAY),
+        arguments(true, A_ARRAY, STRUCT_WITH_STRING_ARRAY),
+        arguments(true, A_ARRAY, NOTHING_ARRAY),
+        arguments(true, A_ARRAY, A_ARRAY),
+        arguments(true, A_ARRAY, B_ARRAY),
+        arguments(true, A_ARRAY, STRING_ARRAY2),
+        arguments(true, A_ARRAY, STRUCT_WITH_STRING_ARRAY2),
+        arguments(true, A_ARRAY, NOTHING_ARRAY2),
+        arguments(true, A_ARRAY, A_ARRAY2),
+        arguments(true, A_ARRAY, B_ARRAY2),
+
+        arguments(false, A_ARRAY2, STRING),
+        arguments(false, A_ARRAY2, STRUCT_WITH_STRING),
+        arguments(true, A_ARRAY2, NOTHING),
+        arguments(false, A_ARRAY2, A),
+        arguments(false, A_ARRAY2, B),
+        arguments(false, A_ARRAY2, STRING_ARRAY),
+        arguments(false, A_ARRAY2, STRUCT_WITH_STRING_ARRAY),
+        arguments(true, A_ARRAY2, NOTHING_ARRAY),
+        arguments(false, A_ARRAY2, A_ARRAY),
+        arguments(false, A_ARRAY2, B_ARRAY),
+        arguments(true, A_ARRAY2, STRING_ARRAY2),
+        arguments(true, A_ARRAY2, STRUCT_WITH_STRING_ARRAY2),
+        arguments(true, A_ARRAY2, NOTHING_ARRAY2),
+        arguments(true, A_ARRAY2, A_ARRAY2),
+        arguments(true, A_ARRAY2, B_ARRAY2));
   }
 }
