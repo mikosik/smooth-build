4790331a44f1807567884c51cca8a3d4a70fd4f8
Marcin Mikosik <marcin.mikosik@gmail.com>
2020-09-23 19:29:22 +0200

    fixed style in TestedAssignment

--- src/testing/java/org/smoothbuild/lang/base/type/TestedAssignment.java
+++ src/testing/java/org/smoothbuild/lang/base/type/TestedAssignment.java
@@ -4,20 +4,20 @@ import static java.lang.String.join;
 import static org.smoothbuild.util.Sets.set;
 
 public class TestedAssignment {
-    public final TestedType target;
-    public final TestedType source;
+  public final TestedType target;
+  public final TestedType source;
 
-    public TestedAssignment(TestedType target, TestedType source) {
-      this.target = target;
-      this.source = source;
-    }
+  public TestedAssignment(TestedType target, TestedType source) {
+    this.target = target;
+    this.source = source;
+  }
 
-    public String declarations() {
-      return join("\n", set(target.declarations(), source.declarations()));
-    }
+  public String declarations() {
+    return join("\n", set(target.declarations(), source.declarations()));
+  }
 
-    @Override
-    public String toString() {
-      return target.name() + " <- " + source.name();
-    }
+  @Override
+  public String toString() {
+    return target.name() + " <- " + source.name();
+  }
 }
