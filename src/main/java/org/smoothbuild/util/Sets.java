cb92ca20ccccb3bd725298747e6b50b04b7ad1da
Marcin Mikosik <marcin.mikosik@gmail.com>
2020-09-30 12:56:36 +0200

    refactored Sets.map()/filter() to accept Collection instead of Map

--- src/main/java/org/smoothbuild/util/Sets.java
+++ src/main/java/org/smoothbuild/util/Sets.java
@@ -4,6 +4,7 @@ import static com.google.common.collect.ImmutableSet.toImmutableSet;
 import static java.util.stream.Collectors.toSet;
 
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Set;
 import java.util.function.Function;
 import java.util.function.Predicate;
@@ -16,14 +17,15 @@ public class Sets {
     return Arrays.stream(elements).collect(toSet());
   }
 
-  public static <E, R> ImmutableSet<R> map(Set<E> set, Function<? super E, ? extends R> function) {
+  public static <E, R> ImmutableSet<R> map(
+      Collection<E> set, Function<? super E, ? extends R> function) {
     return set
         .stream()
         .map(function)
         .collect(toImmutableSet());
   }
 
-  public static <E> ImmutableSet<E> filter(Set<E> set, Predicate<? super E> predicate) {
+  public static <E> ImmutableSet<E> filter(Collection<E> set, Predicate<? super E> predicate) {
     return set
         .stream()
         .filter(predicate)
