cb0324fa68ab1bc6c1e27dafa07142526455fa5d
Marcin Mikosik <marcin.mikosik@gmail.com>
2020-11-21 09:53:37 +0100

    renamed TestingItemSignature.item() -> itemSignature()

--- src/testing/java/org/smoothbuild/lang/base/type/TestingItemSignature.java
+++ src/testing/java/org/smoothbuild/lang/base/type/TestingItemSignature.java
@@ -5,7 +5,7 @@ import static org.smoothbuild.testing.common.TestingLocation.loc;
 import java.util.Optional;
 
 public class TestingItemSignature {
-  public static ItemSignature item(Type type, String name) {
+  public static ItemSignature itemSignature(Type type, String name) {
     return new ItemSignature(type, name, Optional.empty(), loc(7));
   }
 }
