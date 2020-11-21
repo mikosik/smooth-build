fbe24ebb4c060dde873485cb25ff706033cec1f2
Marcin Mikosik <marcin.mikosik@gmail.com>
2020-11-21 10:46:16 +0100

    refactored AbstractConversionTestCase

--- src/acceptance/org/smoothbuild/acceptance/lang/assign/convert/ValueResultConversionTest.java
+++ src/acceptance/org/smoothbuild/acceptance/lang/assign/convert/ValueResultConversionTest.java
@@ -2,9 +2,11 @@ package org.smoothbuild.acceptance.lang.assign.convert;
 
 import static org.smoothbuild.util.Strings.unlines;
 
+import org.smoothbuild.lang.base.type.TestedAssignment;
+
 public class ValueResultConversionTest extends AbstractConversionTestCase {
   @Override
-  protected String createTestScript(ConversionTestSpec testSpec) {
+  protected String createTestScript(TestedAssignment testSpec) {
     return unlines(
         testSpec.target.name() + " value = " + testSpec.source.literal() + ";",
         "result = value;",
