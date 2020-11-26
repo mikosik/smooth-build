42749148f89c4ecf0235313f0ecf16063bc6ce1e
Marcin Mikosik <marcin.mikosik@gmail.com>
2020-11-26 11:18:21 +0100

    added Maybe.of() .withLogsFrom() .equals() .hashCode() .toString()

--- src/main/test/org/smoothbuild/cli/console/MaybeTest.java
+++ src/main/test/org/smoothbuild/cli/console/MaybeTest.java
@@ -6,12 +6,30 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import com.google.common.testing.EqualsTester;
+
 public class MaybeTest {
   private Maybe<String> value;
 
   @BeforeEach
   public void before() {
     value = new Maybe<>();
+  }
+
+  @Test
+  public void of_value() {
+    var maybe = Maybe.of("abc");
+    assertThat(maybe.value())
+        .isEqualTo("abc");
+  }
+
+  @Test
+  public void with_logs_from() {
+    MemoryLogger logger = new MemoryLogger();
+    logger.error("message");
+    var maybe = Maybe.withLogsFrom(logger);
+    assertThat(maybe.logs())
+        .isEqualTo(logger.logs());
   }
 
   @Nested
@@ -28,5 +46,29 @@ public class MaybeTest {
       assertThat(value.value())
           .isEqualTo("abc");
     }
+  }
+
+  @Test
+  public void test_equals_and_hashcode() {
+    new EqualsTester()
+        .addEqualityGroup(new Maybe<>(), new Maybe<>())
+        .addEqualityGroup(Maybe.of("abc"), Maybe.of("abc"))
+        .addEqualityGroup(Maybe.of("def"), Maybe.of("def"))
+        .addEqualityGroup(withLog("abc"), withLog("abc"))
+        .addEqualityGroup(withLog("def"), withLog("def"));
+  }
+
+  @Test
+  public void to_string() {
+    Maybe<String> maybe = Maybe.of("abc");
+    maybe.error("message");
+    assertThat(maybe.toString())
+        .isEqualTo("Maybe{abc, [Log{ERROR, 'message'}]}");
+  }
+
+  private static Maybe<String> withLog(String message) {
+    Maybe<String> maybe = new Maybe<>();
+    maybe.error(message);
+    return maybe;
   }
 }
