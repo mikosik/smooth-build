77723acb2edbf4f3d810a4cb2f1303d9454c971b
Marcin Mikosik <marcin.mikosik@gmail.com>
2020-12-07 18:07:56 +0100

    merged AbstractObjectSpecTestCase and its subclasses into SpecCachingTest

--- src/main/test/org/smoothbuild/db/object/spec/SpecCachingTest.java
+++ src/main/test/org/smoothbuild/db/object/spec/SpecCachingTest.java
@@ -1,42 +1,58 @@
 package org.smoothbuild.db.object.spec;
 
 import static com.google.common.truth.Truth.assertThat;
+import static org.smoothbuild.util.Lists.list;
 
 import java.util.List;
-import java.util.function.Supplier;
+import java.util.function.Function;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.smoothbuild.db.hashed.Hash;
 import org.smoothbuild.db.object.db.ObjectDb;
 import org.smoothbuild.testing.TestingContext;
 
 public class SpecCachingTest extends TestingContext {
-
-  @Test
-  public void creating_array_spec_reuses_cached_instance() {
-    assertReturnsSameInstanceEachTime(() -> arraySpec(stringSpec()));
+  @ParameterizedTest
+  @MethodSource("spec_creators")
+  public void created_spec_is_cached(Function<ObjectDb, Spec> specCreator) {
+    assertThat(specCreator.apply(objectDb()))
+        .isSameInstanceAs(specCreator.apply(objectDb()));
   }
 
-  @Test
-  public void reading_array_spec_reuses_cached_instance() {
-    Spec spec = arraySpec(stringSpec());
-    ObjectDb objectDbOther = objectDbOther();
-    assertReturnsSameInstanceEachTime(() -> objectDbOther.getSpec(spec.hash()));
+  @ParameterizedTest
+  @MethodSource("spec_creators")
+  public void read_spec_is_cached(Function<ObjectDb, Spec> specCreator) {
+    Hash hash = specCreator.apply(objectDb()).hash();
+    ObjectDb otherDb = objectDbOther();
+    assertThat(otherDb.getSpec(hash))
+        .isSameInstanceAs(otherDb.getSpec(hash));
   }
 
-  @Test
-  public void creating_tuple_spec_reuses_cached_instance() {
-    assertReturnsSameInstanceEachTime(() -> tupleSpec(List.of(stringSpec())));
+  private static List<Function<ObjectDb, Spec>> spec_creators() {
+    return list(
+        ObjectDb::anySpec,
+        ObjectDb::blobSpec,
+        ObjectDb::boolSpec,
+        ObjectDb::nothingSpec,
+        ObjectDb::stringSpec,
+        SpecCachingTest::tupleSpec,
+        (objectDb) -> objectDb.arraySpec(objectDb.anySpec()),
+        (objectDb) -> objectDb.arraySpec(objectDb.blobSpec()),
+        (objectDb) -> objectDb.arraySpec(objectDb.boolSpec()),
+        (objectDb) -> objectDb.arraySpec(objectDb.nothingSpec()),
+        (objectDb) -> objectDb.arraySpec(objectDb.stringSpec()),
+        (objectDb) -> objectDb.arraySpec(tupleSpec(objectDb)),
+        (objectDb) -> objectDb.arraySpec(objectDb.arraySpec(objectDb.anySpec())),
+        (objectDb) -> objectDb.arraySpec(objectDb.arraySpec(objectDb.blobSpec())),
+        (objectDb) -> objectDb.arraySpec(objectDb.arraySpec(objectDb.boolSpec())),
+        (objectDb) -> objectDb.arraySpec(objectDb.arraySpec(objectDb.nothingSpec())),
+        (objectDb) -> objectDb.arraySpec(objectDb.arraySpec(objectDb.stringSpec())),
+        (objectDb) -> objectDb.arraySpec(objectDb.arraySpec(tupleSpec(objectDb)))
+    );
   }
 
-  @Test
-  public void reading_tuple_spec_reuses_cached_instance() {
-    Spec spec = tupleSpec(List.of(stringSpec()));
-    ObjectDb objectDbOther = objectDbOther();
-
-    assertReturnsSameInstanceEachTime(() -> objectDbOther.getSpec(spec.hash()));
-  }
-
-  private static void assertReturnsSameInstanceEachTime(Supplier<Object> supplier) {
-    assertThat(supplier.get()).isSameInstanceAs(supplier.get());
+  private static TupleSpec tupleSpec(ObjectDb objectDb) {
+    return objectDb.tupleSpec(list(objectDb.stringSpec(), objectDb.stringSpec()));
   }
 }
