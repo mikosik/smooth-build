28b0529ff2535e06fbe95f4b4cd2133b65d5ce11
Marcin Mikosik <marcin.mikosik@gmail.com>
2020-12-27 11:13:14 +0100

    fixed name TestingConstraints -> TestingVariableToBounds

--- src/testing/java/org/smoothbuild/lang/base/type/constraint/TestingVariableToBounds.java
+++ src/testing/java/org/smoothbuild/lang/base/type/constraint/TestingVariableToBounds.java
@@ -7,8 +7,8 @@ import org.smoothbuild.lang.base.type.Type;
 import org.smoothbuild.lang.base.type.TypeVariable;
 import org.smoothbuild.lang.base.type.VariableToBounds;
 
-public class TestingConstraints {
-  public static VariableToBounds constraints(TypeVariable var, Type lowerBound) {
+public class TestingVariableToBounds {
+  public static VariableToBounds variableToBounds(TypeVariable var, Type lowerBound) {
     return VariableToBounds.empty().addBounds(var, oneSideBound(LOWER, lowerBound));
   }
 }
