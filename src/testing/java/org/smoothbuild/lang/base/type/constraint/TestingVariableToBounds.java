36efded07063f75295b043de16e72b30534a15fa
Marcin Mikosik <marcin.mikosik@gmail.com>
2020-12-28 10:21:25 +0100

    improved naming in TestingVariableToBounds

--- src/testing/java/org/smoothbuild/lang/base/type/constraint/TestingVariableToBounds.java
+++ src/testing/java/org/smoothbuild/lang/base/type/constraint/TestingVariableToBounds.java
@@ -1,14 +1,21 @@
 package org.smoothbuild.lang.base.type.constraint;
 
 import static org.smoothbuild.lang.base.type.Bounds.oneSideBound;
-import static org.smoothbuild.lang.base.type.Side.LOWER;
 
+import org.smoothbuild.lang.base.type.Side;
 import org.smoothbuild.lang.base.type.Type;
 import org.smoothbuild.lang.base.type.TypeVariable;
 import org.smoothbuild.lang.base.type.VariableToBounds;
 
 public class TestingVariableToBounds {
-  public static VariableToBounds variableToBounds(TypeVariable var, Type lowerBound) {
-    return VariableToBounds.empty().addBounds(var, oneSideBound(LOWER, lowerBound));
+  public static VariableToBounds vtb(TypeVariable var, Side side, Type bound) {
+    return VariableToBounds.empty().addBounds(var, oneSideBound(side, bound));
+  }
+
+  public static VariableToBounds vtb(
+      TypeVariable var1, Side side1, Type bound1,
+      TypeVariable var2, Side side2, Type bound2) {
+    return vtb(var1, side1, bound1)
+        .addBounds(var2, oneSideBound(side2, bound2));
   }
 }
