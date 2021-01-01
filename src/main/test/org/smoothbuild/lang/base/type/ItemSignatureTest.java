52eba172327c62fccb3fcd7f7fc8ff91ecb9d626
Marcin Mikosik <marcin.mikosik@gmail.com>
2021-01-01 10:57:18 +0100

    removed ItemSignature.location()

--- src/main/test/org/smoothbuild/lang/base/type/ItemSignatureTest.java
+++ src/main/test/org/smoothbuild/lang/base/type/ItemSignatureTest.java
@@ -1,8 +1,6 @@
 package org.smoothbuild.lang.base.type;
 
 import static com.google.common.truth.Truth.assertThat;
-import static org.smoothbuild.lang.base.Location.commandLineLocation;
-import static org.smoothbuild.lang.base.Location.internal;
 import static org.smoothbuild.lang.base.type.TestingTypes.BLOB;
 import static org.smoothbuild.lang.base.type.TestingTypes.BOOL;
 import static org.smoothbuild.lang.base.type.TestingTypes.NOTHING;
@@ -26,26 +24,26 @@ public class ItemSignatureTest {
 
   @Test
   public void null_type_is_forbidden() {
-    assertCall(() -> new ItemSignature(null, name, Optional.of(STRING), internal()))
+    assertCall(() -> new ItemSignature(null, name, Optional.of(STRING)))
         .throwsException(NullPointerException.class);
   }
 
   @Test
   public void null_name_is_forbidden() {
-    assertCall(() -> new ItemSignature(STRING, null, Optional.of(STRING), internal()))
+    assertCall(() -> new ItemSignature(STRING, null, Optional.of(STRING)))
         .throwsException(NullPointerException.class);
   }
 
   @Test
   public void type_getter() {
-    item = new ItemSignature(STRING, name, Optional.of(STRING), internal());
+    item = new ItemSignature(STRING, name, Optional.of(STRING));
     assertThat(item.type())
         .isEqualTo(STRING);
   }
 
   @Test
   public void name_getter() {
-    item = new ItemSignature(STRING, name, Optional.of(STRING), internal());
+    item = new ItemSignature(STRING, name, Optional.of(STRING));
     assertThat(item.name())
         .isEqualTo(name);
   }
@@ -53,36 +51,32 @@ public class ItemSignatureTest {
   @Test
   public void equals_and_hash_code() {
     EqualsTester tester = new EqualsTester();
-    tester.addEqualityGroup(
-        new ItemSignature(STRING, "equal", Optional.of(STRING), internal()));
-    tester.addEqualityGroup(
-        new ItemSignature(STRING, "equal", Optional.of(STRING), commandLineLocation()));
-    tester.addEqualityGroup(
-        new ItemSignature(STRING, "equal", Optional.empty(), internal()));
+    tester.addEqualityGroup(new ItemSignature(STRING, "equal", Optional.of(STRING)));
+    tester.addEqualityGroup(new ItemSignature(STRING, "equal", Optional.empty()));
     for (Type type : List.of(BOOL, STRING, a(STRING), BLOB, NOTHING, PERSON)) {
-      tester.addEqualityGroup(new ItemSignature(type, name, Optional.of(STRING), internal()));
-      tester.addEqualityGroup(new ItemSignature(type, "name2", Optional.of(STRING), internal()));
+      tester.addEqualityGroup(new ItemSignature(type, name, Optional.of(STRING)));
+      tester.addEqualityGroup(new ItemSignature(type, "name2", Optional.of(STRING)));
     }
     tester.testEquals();
   }
 
   @Test
   public void to_padded_string() {
-    item = new ItemSignature(STRING, "myName", Optional.of(STRING), internal());
+    item = new ItemSignature(STRING, "myName", Optional.of(STRING));
     assertThat(item.toPaddedString(10, 13))
         .isEqualTo("String    : myName       ");
   }
 
   @Test
   public void to_padded_string_for_short_limits() {
-    item = new ItemSignature(STRING, "myName", Optional.of(STRING), internal());
+    item = new ItemSignature(STRING, "myName", Optional.of(STRING));
     assertThat(item.toPaddedString(1, 1))
         .isEqualTo("String: myName");
   }
 
   @Test
   public void to_string() {
-    item = new ItemSignature(STRING, "myName", Optional.of(STRING), internal());
+    item = new ItemSignature(STRING, "myName", Optional.of(STRING));
     assertThat(item.toString())
         .isEqualTo("String myName");
   }
@@ -90,9 +84,9 @@ public class ItemSignatureTest {
   @Test
   public void params_to_string() {
     List<ItemSignature> names = new ArrayList<>();
-    names.add(new ItemSignature(STRING, "param1", Optional.of(STRING), internal()));
-    names.add(new ItemSignature(STRING, "param2-with-very-long", Optional.of(STRING), internal()));
-    names.add(new ItemSignature(a(BLOB), "param3", Optional.of(STRING), internal()));
+    names.add(new ItemSignature(STRING, "param1", Optional.of(STRING)));
+    names.add(new ItemSignature(STRING, "param2-with-very-long", Optional.of(STRING)));
+    names.add(new ItemSignature(a(BLOB), "param3", Optional.of(STRING)));
 
     assertThat(ItemSignature.iterableToString(names))
         .isEqualTo(unlines(
--- src/testing/java/org/smoothbuild/lang/base/type/TestingItemSignature.java
+++ src/testing/java/org/smoothbuild/lang/base/type/TestingItemSignature.java
@@ -1,11 +1,9 @@
 package org.smoothbuild.lang.base.type;
 
-import static org.smoothbuild.testing.common.TestingLocation.loc;
-
 import java.util.Optional;
 
 public class TestingItemSignature {
   public static ItemSignature itemSignature(Type type, String name) {
-    return new ItemSignature(type, name, Optional.empty(), loc(7));
+    return new ItemSignature(type, name, Optional.empty());
   }
 }
