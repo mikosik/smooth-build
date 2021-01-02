5665d6e5e00b38a0284af01e4dbfdcc2a4b4328c
Marcin Mikosik <marcin.mikosik@gmail.com>
2021-01-02 14:01:32 +0100

    fixed naming issues with `Item` term

--- src/main/test/org/smoothbuild/lang/base/ItemTest.java
+++ src/main/test/org/smoothbuild/lang/base/ItemTest.java
@@ -8,7 +8,7 @@ import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 
-public class ParameterTest {
+public class ItemTest {
   private final String name = "name";
   private Item parameter;
 
