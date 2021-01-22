59677f28c173fb792aa813de245fce76217983ba
Marcin Mikosik <marcin.mikosik@gmail.com>
2021-01-22 11:18:33 +0100

    added Expression.toTypes()

--- src/main/java/org/smoothbuild/lang/expr/Expression.java
+++ src/main/java/org/smoothbuild/lang/expr/Expression.java
@@ -1,6 +1,12 @@
 package org.smoothbuild.lang.expr;
 
+import static org.smoothbuild.util.Lists.map;
+
+import java.util.List;
+
 import org.smoothbuild.lang.base.type.Type;
+
+import com.google.common.collect.ImmutableList;
 
 /**
  * Expression in smooth language.
@@ -9,4 +15,8 @@ public interface Expression {
   public Type type();
 
   public abstract <T> T visit(ExpressionVisitor<T> visitor) throws ExpressionVisitorException;
+
+  public static ImmutableList<Type> toTypes(List<? extends Expression> expressions) {
+    return map(expressions, Expression::type);
+  }
 }
