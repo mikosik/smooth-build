205278a5cc58600e2862bf76e4c41aa20b0f2793
Marcin Mikosik <marcin.mikosik@gmail.com>
2021-01-24 13:43:37 +0100

    refactored UnescapingFailedException, improved tests

--- src/main/java/org/smoothbuild/util/UnescapingFailedException.java
+++ src/main/java/org/smoothbuild/util/UnescapingFailedException.java
@@ -1,23 +1,17 @@
 package org.smoothbuild.util;
 
 public class UnescapingFailedException extends RuntimeException {
-  private final int charIndex;
-
   public static UnescapingFailedException missingEscapeCodeException(int charIndex) {
-    return new UnescapingFailedException(charIndex, "Missing escape code after backslash \\");
+    return new UnescapingFailedException(
+        "Missing escape code after backslash \\ at char index = " + charIndex + ".");
   }
 
   public static UnescapingFailedException illegalEscapeSequenceException(int charIndex) {
-    return new UnescapingFailedException(charIndex,
-        "Illegal escape sequence. Legal sequences are: \\t \\b \\n \\r \\f \\\" \\\\.");
+    return new UnescapingFailedException("Illegal escape sequence at char index = " + charIndex
+        + ". Legal sequences are: \\t \\b \\n \\r \\f \\\" \\\\.");
   }
 
-  public UnescapingFailedException(int charIndex, String message) {
+  public UnescapingFailedException(String message) {
     super(message);
-    this.charIndex = charIndex;
-  }
-
-  public int charIndex() {
-    return charIndex;
   }
 }
