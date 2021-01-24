1583b3caba133fe1a145c983f8a4330860a89ff3
Marcin Mikosik <marcin.mikosik@gmail.com>
2021-01-24 14:46:57 +0100

    refactored Referencables to hold Definitions.referencables() instead of Definitions

--- src/main/java/org/smoothbuild/lang/parse/Referencables.java
+++ src/main/java/org/smoothbuild/lang/parse/Referencables.java
@@ -5,7 +5,7 @@ import static java.util.Objects.requireNonNullElseGet;
 import java.util.Map;
 import java.util.Optional;
 
-import org.smoothbuild.lang.base.define.Definitions;
+import org.smoothbuild.lang.base.define.Referencable;
 import org.smoothbuild.lang.base.like.CallableLike;
 import org.smoothbuild.lang.base.like.ItemLike;
 import org.smoothbuild.lang.base.like.ReferencableLike;
@@ -13,12 +13,14 @@ import org.smoothbuild.lang.base.type.ItemSignature;
 import org.smoothbuild.lang.base.type.Type;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
 
 public class Referencables {
-  private final Definitions imported;
+  private final ImmutableMap<String, Referencable> imported;
   private final Map<String, ? extends ReferencableLike> local;
 
-  public Referencables(Definitions imported, Map<String, ? extends ReferencableLike> local) {
+  public Referencables(ImmutableMap<String, Referencable> imported,
+      Map<String, ? extends ReferencableLike> local) {
     this.imported = imported;
     this.local = local;
   }
@@ -40,8 +42,6 @@ public class Referencables {
   }
 
   public ReferencableLike findReferencableLike(String name) {
-    return requireNonNullElseGet(
-        imported.referencables().get(name),
-        () -> local.get(name));
+    return requireNonNullElseGet(imported.get(name), () -> local.get(name));
   }
 }
