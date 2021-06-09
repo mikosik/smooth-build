b134a2b602ae607cae2ee033fa55915423d48e52
Marcin Mikosik <marcin.mikosik@gmail.com>
2021-06-09 13:21:37 +0200

    converted JarFile class to record

--- src/main/java/org/smoothbuild/io/util/JarFile.java
+++ src/main/java/org/smoothbuild/io/util/JarFile.java
@@ -5,26 +5,5 @@ import java.nio.file.Path;
 import org.smoothbuild.db.hashed.Hash;
 import org.smoothbuild.lang.base.define.ModuleLocation;
 
-public class JarFile {
-  private final ModuleLocation moduleLocation;
-  private final Path resolvedPath;
-  private final Hash hash;
-
-  public JarFile(ModuleLocation moduleLocation, Path resolvedPath, Hash hash) {
-    this.moduleLocation = moduleLocation;
-    this.resolvedPath = resolvedPath;
-    this.hash = hash;
-  }
-
-  public Hash hash() {
-    return hash;
-  }
-
-  public ModuleLocation location() {
-    return moduleLocation;
-  }
-
-  public Path path() {
-    return resolvedPath;
-  }
+public record JarFile(ModuleLocation location, Path resolvedPath, Hash hash) {
 }
