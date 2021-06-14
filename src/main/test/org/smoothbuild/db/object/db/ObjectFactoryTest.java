422f397ba25c86fbf7c3f02ff5c4488f2682598d
Marcin Mikosik <marcin.mikosik@gmail.com>
2021-06-14 12:32:42 +0200

    cleaned up imports in ObjectFactoryTest

--- src/main/test/org/smoothbuild/db/object/db/ObjectFactoryTest.java
+++ src/main/test/org/smoothbuild/db/object/db/ObjectFactoryTest.java
@@ -1,5 +1,6 @@
 package org.smoothbuild.db.object.db;
 
+import static com.google.common.truth.Truth.assertThat;
 import static org.smoothbuild.cli.console.Level.ERROR;
 import static org.smoothbuild.cli.console.Level.INFO;
 import static org.smoothbuild.cli.console.Level.WARNING;
@@ -8,8 +9,6 @@ import org.junit.jupiter.api.Test;
 import org.smoothbuild.exec.base.MessageTuple;
 import org.smoothbuild.testing.TestingContext;
 
-import com.google.common.truth.Truth;
-
 import okio.ByteString;
 
 public class ObjectFactoryTest extends TestingContext {
@@ -17,31 +16,31 @@ public class ObjectFactoryTest extends TestingContext {
 
   @Test
   public void blob_data_can_be_read_back() throws Exception {
-    Truth.assertThat(objectFactory().blob(sink -> sink.write(bytes)).source().readByteString())
+    assertThat(objectFactory().blob(sink -> sink.write(bytes)).source().readByteString())
         .isEqualTo(bytes);
   }
 
   @Test
   public void error_severity_is_error() {
-    Truth.assertThat(MessageTuple.severity(objectFactory().errorMessage("text")))
+    assertThat(MessageTuple.severity(objectFactory().errorMessage("text")))
         .isEqualTo(ERROR.name());
   }
 
   @Test
   public void warning_severity_is_warning() {
-    Truth.assertThat(MessageTuple.severity(objectFactory().warningMessage("text")))
+    assertThat(MessageTuple.severity(objectFactory().warningMessage("text")))
         .isEqualTo(WARNING.name());
   }
 
   @Test
   public void info_severity_is_info() {
-    Truth.assertThat(MessageTuple.severity(objectFactory().infoMessage("text")))
+    assertThat(MessageTuple.severity(objectFactory().infoMessage("text")))
         .isEqualTo(INFO.name());
   }
 
   @Test
   public void text_returns_text() {
-    Truth.assertThat(MessageTuple.text(objectFactory().errorMessage("text")))
+    assertThat(MessageTuple.text(objectFactory().errorMessage("text")))
         .isEqualTo("text");
   }
 }
