f827f61b6bcfa8be8f72cf12b50837cc199ff715
Marcin Mikosik <marcin.mikosik@gmail.com>
2021-06-15 14:20:10 +0200

    refactored SpecStableHashTest

--- src/main/test/org/smoothbuild/db/object/spec/SpecStableHashTest.java
+++ src/main/test/org/smoothbuild/db/object/spec/SpecStableHashTest.java
@@ -12,15 +12,16 @@ public class SpecStableHashTest extends TestingContext {
   @Test
   public void hashes_of_specs_are_stable() {
     assertHash(anySpec(), "8aacfde27b33a25ea9797815353fa580f744b3cb");
-    assertHash(boolSpec(), "cf1a0a6c0b2bd3fb9bc6dd67c6c2497cf94cdedf");
-    assertHash(stringSpec(), "de248ad7b14cbd4e62207297826e21c2aaae36f4");
     assertHash(blobSpec(), "8be891c7170d4d1bbca0ffcb3fce71285c55aee4");
+    assertHash(boolSpec(), "cf1a0a6c0b2bd3fb9bc6dd67c6c2497cf94cdedf");
     assertHash(nothingSpec(), "0a2b2a825165ae9742c63b0c6ddafc22f0bd3b1e");
-    assertHash(arraySpec(boolSpec()), "4d42bef63f74197a8780495faf97f1e6c2b9cccd");
-    assertHash(arraySpec(stringSpec()), "a4cdb35e5fac7a1152a3cd73a1b66fb960a8b4e9");
-    assertHash(arraySpec(blobSpec()), "8800c7594a868985fe01a2ab00a29083df788fc7");
-    assertHash(arraySpec(nothingSpec()), "3455dd4106f9763675c88becf9161eb31912cbe3");
+    assertHash(stringSpec(), "de248ad7b14cbd4e62207297826e21c2aaae36f4");
     assertHash(tupleSpec(objectDb()), "5dd814fefb53a67d3fd0ec7fadcca7e1aa9d9b3b");
+
+    assertHash(arraySpec(blobSpec()), "8800c7594a868985fe01a2ab00a29083df788fc7");
+    assertHash(arraySpec(boolSpec()), "4d42bef63f74197a8780495faf97f1e6c2b9cccd");
+    assertHash(arraySpec(nothingSpec()), "3455dd4106f9763675c88becf9161eb31912cbe3");
+    assertHash(arraySpec(stringSpec()), "a4cdb35e5fac7a1152a3cd73a1b66fb960a8b4e9");
   }
 
   private TupleSpec tupleSpec(ObjectDb objectDb) {
