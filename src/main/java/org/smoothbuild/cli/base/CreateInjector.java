2405e6fa9adc7466dccdfe9b633b268c7a667589
Marcin Mikosik <marcin.mikosik@gmail.com>
2021-06-18 13:33:50 +0200

    added FileSystemModule.provideSdkFileSystem()

--- src/main/java/org/smoothbuild/cli/base/CreateInjector.java
+++ src/main/java/org/smoothbuild/cli/base/CreateInjector.java
@@ -29,7 +29,7 @@ public class CreateInjector {
     return Guice.createInjector(PRODUCTION,
         new ExecuteModule(),
         new ObjectDbModule(),
-        new FileSystemModule(projectDir),
+        new FileSystemModule(projectDir, installationDir),
         new PathsModule(installationDir, projectDir),
         new LoggerModule(logLevel, taskMatcher),
         new ConsoleModule(out));
--- src/main/java/org/smoothbuild/install/InstallationPaths.java
+++ src/main/java/org/smoothbuild/install/InstallationPaths.java
@@ -12,7 +12,7 @@ import org.smoothbuild.io.fs.base.FilePath;
 import com.google.common.collect.ImmutableList;
 
 public class InstallationPaths {
-  private static final String LIB_DIR_NAME = "lib";
+  public static final String LIB_DIR_NAME = "lib";
   public static final ImmutableList<FilePath> SDK_MODULES = list(filePath(SDK, path("api.smooth")));
   private static final String SMOOTH_JAR = "smooth.jar";
   private final java.nio.file.Path installationDir;
