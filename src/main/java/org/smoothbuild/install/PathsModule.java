3121d18d79a677a721e3a895278dbe51a1542eb2
Marcin Mikosik <marcin.mikosik@gmail.com>
2021-07-06 13:02:53 +0200

    refactored PathsModule

--- src/main/java/org/smoothbuild/install/PathsModule.java
+++ src/main/java/org/smoothbuild/install/PathsModule.java
@@ -9,6 +9,8 @@ import org.smoothbuild.io.fs.base.Space;
 
 import com.google.common.collect.ImmutableMap;
 import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
+import com.google.inject.Singleton;
 
 public class PathsModule extends AbstractModule {
   private final Path projectDir;
@@ -27,12 +29,12 @@ public class PathsModule extends AbstractModule {
   protected void configure() {
     InstallationPaths installationPaths = new InstallationPaths(installationDir);
     bind(InstallationPaths.class).toInstance(installationPaths);
-
-    bind(FullPathResolver.class).toInstance(
-        new FullPathResolver(resolvers(installationPaths.standardLibraryDir(), projectDir)));
   }
 
-  private ImmutableMap<Space, Path> resolvers(Path sdkApiDir, Path projectDir) {
+  @Provides
+  @Singleton
+  public ImmutableMap<Space, Path> provideSpaceToPathMap(InstallationPaths installationPaths) {
+    Path sdkApiDir = installationPaths.standardLibraryDir();
     if (projectDir == null) {
       return ImmutableMap.of(SDK, sdkApiDir);
     } else {
