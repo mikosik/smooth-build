e6692f600c82a378b4d507c2f4239e35e17b3fe8
Marcin Mikosik <marcin.mikosik@gmail.com>
2021-07-19 10:36:26 +0200

    refactored OkiosTest to use @Nested

--- src/main/test/org/smoothbuild/util/io/OkiosTest.java
+++ src/main/test/org/smoothbuild/util/io/OkiosTest.java
@@ -6,6 +6,7 @@ import static org.mockito.Mockito.verify;
 import static org.smoothbuild.util.io.Okios.copyAllAndClose;
 import static org.smoothbuild.util.io.Okios.readAndClose;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import okio.Buffer;
@@ -16,61 +17,63 @@ import okio.Sink;
 public class OkiosTest {
   private static final ByteString bytes = ByteString.encodeUtf8("test string");
 
-  // copyAllAndClose()
+  @Nested
+  class _copy_all_and_close {
+    @Test
+    public void copies_bytes() throws Exception {
+      Buffer buffer = new Buffer();
+      copyAllAndClose(bufferWith(bytes), buffer);
+      assertThat(buffer.readByteString())
+          .isEqualTo(bytes);
+    }
 
-  @Test
-  public void copy_all_and_close_copies_bytes() throws Exception {
-    Buffer buffer = new Buffer();
-    copyAllAndClose(bufferWith(bytes), buffer);
-    assertThat(buffer.readByteString())
-        .isEqualTo(bytes);
+    @Test
+    public void works_for_empty_source() throws Exception {
+      Buffer buffer = new Buffer();
+      copyAllAndClose(bufferWith(ByteString.of()), buffer);
+      assertThat(buffer.readByteString())
+          .isEqualTo(ByteString.of());
+    }
+
+    @Test
+    public void closes_source() throws Exception {
+      Buffer buffer = new Buffer();
+      BufferedSource source = mock(BufferedSource.class);
+      copyAllAndClose(source, buffer);
+      verify(source).close();
+    }
+
+    @Test
+    public void closes_sink() throws Exception {
+      Sink sink = mock(Sink.class);
+      copyAllAndClose(bufferWith(ByteString.of()), sink);
+      verify(sink).close();
+    }
   }
 
-  @Test
-  public void copy_all_and_close_works_for_empty_source() throws Exception {
-    Buffer buffer = new Buffer();
-    copyAllAndClose(bufferWith(ByteString.of()), buffer);
-    assertThat(buffer.readByteString())
-        .isEqualTo(ByteString.of());
-  }
+  @Nested
+  class _read_and_close {
+    @Test
+    public void copies_bytes() throws Exception {
+      ByteString byteString = readAndClose(bufferWith(bytes), BufferedSource::readByteString);
+      assertThat(byteString)
+          .isEqualTo(bytes);
+    }
 
-  @Test
-  public void copy_all_and_close_closes_source() throws Exception {
-    Buffer buffer = new Buffer();
-    BufferedSource source = mock(BufferedSource.class);
-    copyAllAndClose(source, buffer);
-    verify(source).close();
-  }
+    @Test
+    public void works_for_empty_source() throws Exception {
+      ByteString byteString =
+          readAndClose(bufferWith(ByteString.of()), BufferedSource::readByteString);
+      assertThat(byteString)
+          .isEqualTo(ByteString.of());
+    }
 
-  @Test
-  public void copy_all_and_close_closes_sink() throws Exception {
-    Sink sink = mock(Sink.class);
-    copyAllAndClose(bufferWith(ByteString.of()), sink);
-    verify(sink).close();
-  }
-
-  // readAndClose()
-
-  @Test
-  public void read_and_close_copies_bytes() throws Exception {
-    ByteString byteString = readAndClose(bufferWith(bytes), BufferedSource::readByteString);
-    assertThat(byteString)
-        .isEqualTo(bytes);
-  }
-
-  @Test
-  public void read_and_close_works_for_empty_source() throws Exception {
-    ByteString byteString =
-        readAndClose(bufferWith(ByteString.of()), BufferedSource::readByteString);
-    assertThat(byteString)
-        .isEqualTo(ByteString.of());
-  }
-
-  @Test
-  public void read_and_close_closes_source() throws Exception {
-    BufferedSource source = mock(BufferedSource.class);
-    readAndClose(source, s -> "");
-    verify(source).close();
+    @Test
+    public void closes_source() throws Exception {
+      BufferedSource source = mock(BufferedSource.class);
+      readAndClose(source, s -> "");
+      verify(source).close();
+    }
   }
 
   private static Buffer bufferWith(ByteString bytes) {
