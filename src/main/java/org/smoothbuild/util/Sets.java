b0d24bfdcb6c0325e54a31b71da9bef147d98619
Marcin Mikosik <marcin.mikosik@gmail.com>
2021-08-06 12:16:07 +0200

    changed Sets.set() to return ImmutableSet

--- src/main/java/org/smoothbuild/util/Sets.java
+++ src/main/java/org/smoothbuild/util/Sets.java
@@ -1,11 +1,8 @@
 package org.smoothbuild.util;
 
 import static com.google.common.collect.ImmutableSet.toImmutableSet;
-import static java.util.stream.Collectors.toSet;
 
-import java.util.Arrays;
 import java.util.Collection;
-import java.util.Set;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
@@ -13,8 +10,8 @@ import com.google.common.collect.ImmutableSet;
 
 public class Sets {
   @SafeVarargs
-  public static <E> Set<E> set(E... elements) {
-    return Arrays.stream(elements).collect(toSet());
+  public static <E> ImmutableSet<E> set(E... elements) {
+    return ImmutableSet.copyOf(elements);
   }
 
   public static <E, R> ImmutableSet<R> map(
