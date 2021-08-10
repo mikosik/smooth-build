2cb42df58aabb9cffcd02da271869a4aede51dd6
Marcin Mikosik <marcin.mikosik@gmail.com>
2021-08-10 18:17:45 +0200

    added Maps.map(), .mapValues()

--- src/main/java/org/smoothbuild/util/Maps.java
+++ src/main/java/org/smoothbuild/util/Maps.java
@@ -3,6 +3,7 @@ package org.smoothbuild.util;
 import static com.google.common.collect.ImmutableMap.toImmutableMap;
 import static com.google.common.collect.Streams.stream;
 
+import java.util.Map;
 import java.util.function.Function;
 
 import com.google.common.collect.ImmutableMap;
@@ -18,5 +19,18 @@ public class Maps {
       Function<? super E, K> keyFunction, Function<? super E, V> valueFunction) {
     return stream(keys)
         .collect(toImmutableMap(keyFunction, valueFunction));
+  }
+
+  public static <K, V, V2> ImmutableMap<K, V2> mapValues(Map<K, V> map,
+      Function<? super V, V2> valueFunction) {
+    return map(map, k -> k, valueFunction);
+  }
+
+  public static <K, V, K2, V2> ImmutableMap<K2, V2> map(Map<K, V> map,
+      Function<? super K, K2> keyFunction, Function<? super V, V2> valueFunction) {
+    return map.entrySet().stream()
+        .collect(toImmutableMap(
+            e -> keyFunction.apply(e.getKey()),
+            e -> valueFunction.apply(e.getValue())));
   }
 }
--- src/main/test/org/smoothbuild/util/MapsTest.java
+++ src/main/test/org/smoothbuild/util/MapsTest.java
@@ -4,6 +4,7 @@ import static com.google.common.truth.Truth.assertThat;
 import static java.util.Arrays.asList;
 import static org.smoothbuild.testing.common.AssertCall.assertCall;
 import static org.smoothbuild.util.Lists.list;
+import static org.smoothbuild.util.Maps.map;
 import static org.smoothbuild.util.Maps.toMap;
 
 import java.util.List;
@@ -36,6 +37,12 @@ public class MapsTest {
       }
 
       @Test
+      public void duplicate_keys() {
+        assertCall(() -> toMap(list(1, 1), Object::toString))
+            .throwsException(IllegalArgumentException.class);
+      }
+
+      @Test
       public void null_element() {
         List<Object> iterableWithNull = asList(null, null);
         assertCall(() -> toMap(iterableWithNull, Object::toString))
@@ -48,6 +55,7 @@ public class MapsTest {
             .throwsException(NullPointerException.class);
       }
     }
+
     @Nested
     class _with_key_and_value_function {
       @Test
@@ -69,6 +77,12 @@ public class MapsTest {
       }
 
       @Test
+      public void duplicate_keys() {
+        assertCall(() -> toMap(list(1,2), k -> "key", Object::toString))
+            .throwsException(IllegalArgumentException.class);
+      }
+
+      @Test
       public void null_element() {
         List<Object> iterableWithNull = asList(null, null);
         assertCall(() -> toMap(iterableWithNull, Object::toString, Object::toString))
@@ -86,6 +100,39 @@ public class MapsTest {
         assertCall(() -> toMap(list(13), Object::toString, e -> null))
             .throwsException(NullPointerException.class);
       }
+    }
+  }
+
+  @Nested
+  class _map {
+    @Test
+    public void empty_map() {
+      assertThat(map(Map.of(), Object::toString, Object::toString))
+          .isEqualTo(Map.of());
+    }
+
+    @Test
+    public void many_elements() {
+      assertThat(map(Map.of(2, "2", 3, "3"), i -> i * i, v -> v + "^2"))
+          .isEqualTo(Map.of(4, "2^2", 9, "3^2"));
+    }
+
+    @Test
+    public void duplicate_keys() {
+      assertCall(() -> map(Map.of(1, "1", 2, "2"), k -> "key", Object::toString))
+          .throwsException(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void null_key() {
+      assertCall(() -> map(Map.of(2, "2"), e -> null, Object::toString))
+          .throwsException(NullPointerException.class);
+    }
+
+    @Test
+    public void null_value() {
+      assertCall(() -> map(Map.of(2, "2"), Object::toString, e -> null))
+          .throwsException(NullPointerException.class);
     }
   }
 }
