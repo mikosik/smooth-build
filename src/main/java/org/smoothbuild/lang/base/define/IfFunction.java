152958ae18692cf4a87161dcb39504732d018689
Marcin Mikosik <marcin.mikosik@gmail.com>
2021-08-11 07:22:48 +0200

    refactored IfFunction

--- src/main/java/org/smoothbuild/lang/base/define/IfFunction.java
+++ src/main/java/org/smoothbuild/lang/base/define/IfFunction.java
@@ -13,7 +13,7 @@ import com.google.common.collect.ImmutableList;
 
 public class IfFunction extends Function {
   public static final String IF_FUNCTION_NAME = "if";
-  private static final Variable RESULT_TYPE = new Variable("A");
+  private static final Type RESULT_TYPE = new Variable("A");
 
   public IfFunction(ModulePath modulePath) {
     super(RESULT_TYPE, modulePath, IF_FUNCTION_NAME, createParameters(modulePath), internal());
