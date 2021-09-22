0bd209af1c110c5c6924b4a169691f5205e6b008
Marcin Mikosik <marcin.mikosik@gmail.com>
2021-09-22 07:45:46 +0200

    added missing methods to ObjectFactory

--- src/main/java/org/smoothbuild/db/object/db/ObjectFactory.java
+++ src/main/java/org/smoothbuild/db/object/db/ObjectFactory.java
@@ -3,28 +3,42 @@ package org.smoothbuild.db.object.db;
 import static org.smoothbuild.cli.console.Level.ERROR;
 import static org.smoothbuild.cli.console.Level.INFO;
 import static org.smoothbuild.cli.console.Level.WARNING;
+import static org.smoothbuild.db.object.db.Helpers.wrapHashedDbExceptionAsObjectDbException;
 import static org.smoothbuild.util.Lists.list;
 
 import java.io.IOException;
 import java.math.BigInteger;
+import java.util.List;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
 import org.smoothbuild.db.object.exc.ObjectDbException;
+import org.smoothbuild.db.object.obj.base.Expr;
 import org.smoothbuild.db.object.obj.base.Obj;
+import org.smoothbuild.db.object.obj.base.Val;
+import org.smoothbuild.db.object.obj.expr.Call;
+import org.smoothbuild.db.object.obj.expr.Const;
+import org.smoothbuild.db.object.obj.expr.EArray;
+import org.smoothbuild.db.object.obj.expr.FieldRead;
+import org.smoothbuild.db.object.obj.expr.Null;
+import org.smoothbuild.db.object.obj.expr.Ref;
 import org.smoothbuild.db.object.obj.val.ArrayBuilder;
 import org.smoothbuild.db.object.obj.val.Blob;
 import org.smoothbuild.db.object.obj.val.BlobBuilder;
 import org.smoothbuild.db.object.obj.val.Bool;
+import org.smoothbuild.db.object.obj.val.DefinedLambda;
 import org.smoothbuild.db.object.obj.val.Int;
+import org.smoothbuild.db.object.obj.val.NativeLambda;
 import org.smoothbuild.db.object.obj.val.Rec;
 import org.smoothbuild.db.object.obj.val.Str;
 import org.smoothbuild.db.object.spec.base.ValSpec;
 import org.smoothbuild.db.object.spec.val.ArraySpec;
 import org.smoothbuild.db.object.spec.val.BlobSpec;
 import org.smoothbuild.db.object.spec.val.BoolSpec;
+import org.smoothbuild.db.object.spec.val.DefinedLambdaSpec;
 import org.smoothbuild.db.object.spec.val.IntSpec;
+import org.smoothbuild.db.object.spec.val.NativeLambdaSpec;
 import org.smoothbuild.db.object.spec.val.NothingSpec;
 import org.smoothbuild.db.object.spec.val.RecSpec;
 import org.smoothbuild.db.object.spec.val.StrSpec;
@@ -82,8 +96,18 @@ public class ObjectFactory {
     return objectDb.boolVal(value);
   }
 
+  public DefinedLambda definedLambda(
+      DefinedLambdaSpec spec, Expr body, List<Expr> defaultArguments) {
+    return objectDb.definedLambdaVal(spec, body, defaultArguments);
+  }
+
   public Int intValue(BigInteger value) {
     return objectDb.intVal(value);
+  }
+
+  public NativeLambda nativeLambda(
+      NativeLambdaSpec spec, Str classBinaryName, Blob nativeJar, List<Expr> defaultArguments) {
+    return objectDb.nativeLambdaVal(spec, classBinaryName, nativeJar, defaultArguments);
   }
 
   public Rec file(Str path, Blob content) {
@@ -96,6 +120,30 @@ public class ObjectFactory {
 
   public Rec rec(RecSpec spec, Iterable<? extends Obj> items) {
     return objectDb.recVal(spec, items);
+  }
+
+  public Const constExpr(Val val) {
+    return objectDb.constExpr(val);
+  }
+
+  public Call callExpr(Expr function, Iterable<? extends Expr> arguments) {
+    return objectDb.callExpr(function, arguments);
+  }
+
+  public EArray eArrayExpr(Iterable<? extends Expr> elements) {
+    return objectDb.eArrayExpr(elements);
+  }
+
+  public FieldRead fieldReadExpr(Expr rec, Int index) {
+    return objectDb.fieldReadExpr(rec, index);
+  }
+
+  public Null nullExpr() {
+    return objectDb.nullExpr();
+  }
+
+  public Ref refExpr(BigInteger value) {
+    return objectDb.refExpr(value);
   }
 
   public ArraySpec arraySpec(ValSpec elementSpec) {
