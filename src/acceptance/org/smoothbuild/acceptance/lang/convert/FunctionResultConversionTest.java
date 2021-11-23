9a029bb2348807d14a45adefb0a2afacf756585f
Marcin Mikosik <marcin.mikosik@gmail.com>
2021-11-23 14:10:08 +0100

    fixed test classes names

--- src/acceptance/org/smoothbuild/acceptance/lang/convert/FunctionResultConversionTest.java
+++ src/acceptance/org/smoothbuild/acceptance/lang/convert/FunctionResultConversionTest.java
@@ -4,12 +4,12 @@ import static org.smoothbuild.util.Strings.unlines;
 
 import org.smoothbuild.lang.base.type.TestedAssignment;
 
-public class ValueSResultConversionTest extends AbstractConversionTestCase {
+public class FunctionResultConversionTest extends AbstractConversionTestCase {
   @Override
   protected String createTestScript(TestedAssignment testSpec) {
     return unlines(
-        testSpec.target().name() + " value = " + testSpec.source().literal() + ";",
-        "result = value;",
+        testSpec.target().name() + " myFunction() = " + testSpec.source().literal() + ";",
+        "result = myFunction();",
         testSpec.declarations()
     );
   }
--- src/acceptance/org/smoothbuild/acceptance/lang/convert/ValueResultConversionTest.java
+++ src/acceptance/org/smoothbuild/acceptance/lang/convert/ValueResultConversionTest.java
@@ -4,12 +4,12 @@ import static org.smoothbuild.util.Strings.unlines;
 
 import org.smoothbuild.lang.base.type.TestedAssignment;
 
-public class FunctionSResultConversionTest extends AbstractConversionTestCase {
+public class ValueResultConversionTest extends AbstractConversionTestCase {
   @Override
   protected String createTestScript(TestedAssignment testSpec) {
     return unlines(
-        testSpec.target().name() + " myFunction() = " + testSpec.source().literal() + ";",
-        "result = myFunction();",
+        testSpec.target().name() + " value = " + testSpec.source().literal() + ";",
+        "result = value;",
         testSpec.declarations()
     );
   }
