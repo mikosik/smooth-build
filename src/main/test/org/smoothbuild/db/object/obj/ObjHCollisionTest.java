860fd313aa441382df579080baf5399e5c978b42
Marcin Mikosik <marcin.mikosik@gmail.com>
2021-12-10 12:33:20 +0100

    fixed ObjHCollisionTest to test all subclasses of ExprH and ValH

--- src/main/test/org/smoothbuild/db/object/obj/ObjHCollisionTest.java
+++ src/main/test/org/smoothbuild/db/object/obj/ObjHCollisionTest.java
@@ -29,10 +29,18 @@ public class ObjHCollisionTest extends TestingContext {
   }
 
   private List<ObjH> objectsH() {
-    BlobH blob1 = blobH(encodeUtf8("abc"));
-    BlobH blob2 = blobH(encodeUtf8("def"));
+    BlobH blob1 = blobH(0);
+    BlobH blob2 = blobH(1);
     BlobH blobEmpty = blobH(ByteString.EMPTY);
+    // @formatter:off
     return List.of(
+        // values
+        arrayH(blobTH()),
+        arrayH(nothingTH()),
+        arrayH(stringTH()),
+        arrayH(blob1),
+        arrayH(blob2),
+        arrayH(blobEmpty),
         blobEmpty,
         blob1,
         blob2,
@@ -43,6 +51,27 @@ public class ObjHCollisionTest extends TestingContext {
         funcH(funcTH(stringTH(), list()), stringH("abc")),
         funcH(funcTH(intTH(), list(intTH())), intH(0)),
         funcH(funcTH(intTH(), list(stringTH())), intH(0)),
+        intH(0),
+        intH(1),
+        methodH(methodTH(intTH(), list()), blob1, stringH("binary name"), boolH(true)),
+        methodH(methodTH(intTH(), list()), blob1, stringH("binary name"), boolH(false)),
+        methodH(methodTH(intTH(), list()), blob1, stringH("binary name 2"), boolH(true)),
+        methodH(methodTH(intTH(), list()), blob2, stringH("binary name"), boolH(true)),
+        methodH(methodTH(stringTH(), list()), blob1, stringH("binary name"), boolH(true)),
+        stringH("abc"),
+        stringH("def"),
+        tupleH(list()),
+        tupleH(list(intH(0))),
+        tupleH(list(intH(1))),
+        tupleH(list(stringH("abc"))),
+        tupleH(list(stringH("def"))),
+        // expressions
+        callH(funcH(funcTH(stringTH(), list(intTH())), stringH("a")), list(intH(1))),
+        callH(funcH(funcTH(stringTH(), list(intTH())), stringH("a")), list(intH(2))),
+        callH(funcH(funcTH(stringTH(), list(intTH())), stringH("b")), list(intH(1))),
+        combineH(list()),
+        combineH(list(intH(0))),
+        combineH(list(intH(1))),
         ifH(boolH(false), intH(1), intH(2)),
         ifH(boolH(false), intH(1), intH(3)),
         ifH(boolH(false), intH(3), intH(2)),
@@ -53,20 +82,16 @@ public class ObjHCollisionTest extends TestingContext {
         mapH(arrayH(intH(1)), funcH(funcTH(intTH(), list(intTH())), paramRefH(intTH(), 0))),
         mapH(arrayH(intH(1)), funcH(funcTH(intTH(), list(intTH())), intH(3))),
         mapH(arrayH(intH(2)), funcH(funcTH(intTH(), list(intTH())), paramRefH(intTH(), 0))),
-        methodH(methodTH(intTH(), list()), blob1, stringH("binary name"), boolH(true)),
-        methodH(methodTH(intTH(), list()), blob1, stringH("binary name"), boolH(false)),
-        methodH(methodTH(intTH(), list()), blob1, stringH("binary name 2"), boolH(true)),
-        methodH(methodTH(intTH(), list()), blob2, stringH("binary name"), boolH(true)),
-        methodH(methodTH(stringTH(), list()), blob1, stringH("binary name"), boolH(true)),
-        intH(0),
-        intH(1),
-        stringH("abc"),
-        stringH("def"),
-        tupleH(list()),
-        tupleH(list(intH(0))),
-        tupleH(list(intH(1))),
-        tupleH(list(stringH("abc"))),
-        tupleH(list(stringH("def")))
+        orderH(list()),
+        orderH(list(intH(1))),
+        orderH(list(intH(2))),
+        paramRefH(intTH(), 0),
+        paramRefH(intTH(), 1),
+        paramRefH(stringTH(), 0),
+        selectH(tupleH(list(intH(1), stringH("a"))), intH(0)),
+        selectH(tupleH(list(intH(1), stringH("a"))), intH(1)),
+        selectH(tupleH(list(intH(1), stringH("b"))), intH(0))
     );
+    // @formatter:on
   }
 }
