95134d68a414bea8c6d845f4bf3f0a7aab228fbd
Marcin Mikosik <marcin.mikosik@gmail.com>
2021-12-15 11:03:54 +0100

    added TypingHMergeBoundsTest

---
+++ src/main/test/org/smoothbuild/lang/base/type/TypingHMergeBoundsTest.java
@@ -0,0 +1,9 @@
+package org.smoothbuild.lang.base.type;
+
+import org.smoothbuild.db.object.type.base.TypeH;
+
+public class TypingHMergeBoundsTest extends TypingMergeBoundsTestCase<TypeH> {
+  public TypingHMergeBoundsTest() {
+    super(new TestingTH());
+  }
+}
