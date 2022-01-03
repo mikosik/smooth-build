3cb8fb4820e0c2b22cfe7002feb30197a70071ea
Marcin Mikosik <marcin.mikosik@gmail.com>
2022-01-03 15:23:08 +0100

    fixed static imports in AlgorithmHashesCollisionTest

--- src/main/test/org/smoothbuild/vm/job/algorithm/AlgorithmHashesCollisionTest.java
+++ src/main/test/org/smoothbuild/vm/job/algorithm/AlgorithmHashesCollisionTest.java
@@ -2,6 +2,11 @@ package org.smoothbuild.vm.job.algorithm;
 
 import static org.junit.jupiter.api.Assertions.fail;
 import static org.smoothbuild.util.collect.Lists.list;
+import static org.smoothbuild.vm.job.algorithm.AlgorithmHashes.combineAlgorithmHash;
+import static org.smoothbuild.vm.job.algorithm.AlgorithmHashes.convertAlgorithmHash;
+import static org.smoothbuild.vm.job.algorithm.AlgorithmHashes.invokeAlgorithmHash;
+import static org.smoothbuild.vm.job.algorithm.AlgorithmHashes.orderAlgorithmHash;
+import static org.smoothbuild.vm.job.algorithm.AlgorithmHashes.selectAlgorithmHash;
 
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -18,25 +23,25 @@ public class AlgorithmHashesCollisionTest extends TestingContext {
   public void each_algorithm_has_different_hash() {
     List<Hash> list = new ArrayList<>();
     Set<Hash> set = new HashSet<>();
-    addHash(list, set, AlgorithmHashes.combineAlgorithmHash(tupleTB(list())));
-    addHash(list, set, AlgorithmHashes.combineAlgorithmHash(tupleTB(list(intTB()))));
-    addHash(list, set, AlgorithmHashes.combineAlgorithmHash(tupleTB(list(stringTB()))));
-    addHash(list, set, AlgorithmHashes.combineAlgorithmHash(tupleTB(list(intTB(), stringTB()))));
-    addHash(list, set, AlgorithmHashes.convertAlgorithmHash(intTB()));
-    addHash(list, set, AlgorithmHashes.convertAlgorithmHash(stringTB()));
-    addHash(list, set, AlgorithmHashes.invokeAlgorithmHash(
+    addHash(list, set, combineAlgorithmHash(tupleTB(list())));
+    addHash(list, set, combineAlgorithmHash(tupleTB(list(intTB()))));
+    addHash(list, set, combineAlgorithmHash(tupleTB(list(stringTB()))));
+    addHash(list, set, combineAlgorithmHash(tupleTB(list(intTB(), stringTB()))));
+    addHash(list, set, convertAlgorithmHash(intTB()));
+    addHash(list, set, convertAlgorithmHash(stringTB()));
+    addHash(list, set, invokeAlgorithmHash(
         methodB(methodTB(intTB(), list()), blobB(1), stringB("1"), boolB(true))));
-    addHash(list, set, AlgorithmHashes.invokeAlgorithmHash(
+    addHash(list, set, invokeAlgorithmHash(
         methodB(methodTB(intTB(), list()), blobB(1), stringB("1"), boolB(false))));
-    addHash(list, set, AlgorithmHashes.invokeAlgorithmHash(
+    addHash(list, set, invokeAlgorithmHash(
         methodB(methodTB(intTB(), list()), blobB(1), stringB("2"), boolB(true))));
-    addHash(list, set, AlgorithmHashes.invokeAlgorithmHash(
+    addHash(list, set, invokeAlgorithmHash(
         methodB(methodTB(intTB(), list()), blobB(2), stringB("1"), boolB(true))));
-    addHash(list, set, AlgorithmHashes.invokeAlgorithmHash(
+    addHash(list, set, invokeAlgorithmHash(
         methodB(methodTB(boolTB(), list()), blobB(1), stringB("1"), boolB(true))));
-    addHash(list, set, AlgorithmHashes.orderAlgorithmHash(arrayTB(intTB())));
-    addHash(list, set, AlgorithmHashes.orderAlgorithmHash(arrayTB(boolTB())));
-    addHash(list, set, AlgorithmHashes.selectAlgorithmHash());
+    addHash(list, set, orderAlgorithmHash(arrayTB(intTB())));
+    addHash(list, set, orderAlgorithmHash(arrayTB(boolTB())));
+    addHash(list, set, selectAlgorithmHash());
   }
 
   private void addHash(List<Hash> list, Set<Hash> set, Hash hash) {
