efe467450413fccd3a82127d412f1fb63aa9ec7b
Marcin Mikosik <marcin.mikosik@gmail.com>
2022-01-05 15:58:36 +0100

    increased task name length limit in logs to 43 to hold whole hash + `()` in case of function calls

--- src/main/java/org/smoothbuild/vm/job/job/JobInfo.java
+++ src/main/java/org/smoothbuild/vm/job/job/JobInfo.java
@@ -4,7 +4,7 @@ import org.smoothbuild.lang.base.define.Loc;
 import org.smoothbuild.lang.base.define.Nal;
 
 public record JobInfo(JobKind kind, String name, Loc loc) implements Nal {
-  public static final int NAME_LENGTH_LIMIT = 41;
+  public static final int NAME_LENGTH_LIMIT = 43;
 
   public JobInfo(JobKind kind, Nal nal) {
     this(kind, nal.name(), nal.loc());
