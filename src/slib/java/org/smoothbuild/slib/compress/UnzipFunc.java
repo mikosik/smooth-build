979778e905aa568ab0d902f14a5baa69286baca2
Marcin Mikosik <marcin.mikosik@gmail.com>
2022-02-01 12:36:35 +0100

    refactored UnzipFunc, UnjarFunc to share common code

--- src/slib/java/org/smoothbuild/slib/compress/UnzipFunc.java
+++ src/slib/java/org/smoothbuild/slib/compress/UnzipFunc.java
@@ -1,6 +1,7 @@
 package org.smoothbuild.slib.compress;
 
 import java.io.IOException;
+import java.util.function.Predicate;
 
 import org.smoothbuild.bytecode.obj.val.ArrayB;
 import org.smoothbuild.bytecode.obj.val.BlobB;
@@ -10,8 +11,13 @@ import net.lingala.zip4j.exception.ZipException;
 
 public class UnzipFunc {
   public static ArrayB func(NativeApi nativeApi, BlobB blob) throws IOException {
+    return unzip(nativeApi, blob, x -> true);
+  }
+
+  public static ArrayB unzip(NativeApi nativeApi, BlobB blob,
+      Predicate<String> includePredicate) throws IOException {
     try {
-      return nativeApi.unzipper().unzip(blob, x -> true);
+      return nativeApi.unzipper().unzip(blob, includePredicate);
     } catch (ZipException e) {
       nativeApi.log().error(
           "Cannot read archive. Corrupted data? Internal message: " + e.getMessage());
--- src/slib/java/org/smoothbuild/slib/java/UnjarFunc.java
+++ src/slib/java/org/smoothbuild/slib/java/UnjarFunc.java
@@ -1,21 +1,15 @@
 package org.smoothbuild.slib.java;
 
+import static org.smoothbuild.slib.compress.UnzipFunc.unzip;
+
 import java.io.IOException;
 
 import org.smoothbuild.bytecode.obj.val.ArrayB;
 import org.smoothbuild.bytecode.obj.val.BlobB;
 import org.smoothbuild.plugin.NativeApi;
 
-import net.lingala.zip4j.exception.ZipException;
-
 public class UnjarFunc {
   public static ArrayB func(NativeApi nativeApi, BlobB jar) throws IOException {
-    try {
-      return nativeApi.unzipper().unzip(jar, string -> !string.equals("META-INF/MANIFEST.MF"));
-    } catch (ZipException e) {
-      nativeApi.log().error(
-          "Cannot read archive. Corrupted data? Internal message: " + e.getMessage());
-      return null;
-    }
+    return unzip(nativeApi, jar, string -> !string.equals("META-INF/MANIFEST.MF"));
   }
 }
