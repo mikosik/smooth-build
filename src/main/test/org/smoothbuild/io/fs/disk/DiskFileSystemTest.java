ff1636932c91d22a69f0ddf750bcf3077288e00a
Marcin Mikosik <marcin.mikosik@gmail.com>
2022-02-02 13:35:57 +0100

    refactored DiskFileSystemTest to use junit @TempDir

--- src/main/test/org/smoothbuild/io/fs/disk/DiskFileSystemTest.java
+++ src/main/test/org/smoothbuild/io/fs/disk/DiskFileSystemTest.java
@@ -1,16 +1,14 @@
 package org.smoothbuild.io.fs.disk;
 
+import static java.nio.file.Files.createDirectories;
 import static okio.Okio.buffer;
 import static okio.Okio.sink;
-import static org.smoothbuild.io.fs.disk.RecursiveDeleter.deleteRecursively;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.io.TempDir;
 import org.smoothbuild.io.fs.base.GenericFileSystemTestCase;
 import org.smoothbuild.io.fs.base.PathS;
 
@@ -18,32 +16,20 @@ import okio.BufferedSink;
 import okio.ByteString;
 
 public class DiskFileSystemTest extends GenericFileSystemTestCase {
-  private File tempDir;
+  private Path tempDir;
 
   @BeforeEach
-  public void before() {
-    tempDir = com.google.common.io.Files.createTempDir();
-    fileSystem = new DiskFileSystem(Path.of(tempDir.getAbsolutePath()));
-  }
-
-  @AfterEach
-  public void after() throws IOException {
-    Path tempPath = tempDir.toPath();
-    if (Files.isDirectory(tempPath)) {
-      deleteRecursively(tempPath);
-    }
+  public void before(@TempDir Path tempDir) {
+    this.tempDir = tempDir;
+    fileSystem = new DiskFileSystem(tempDir);
   }
 
   @Override
   protected void createFile(PathS path, ByteString content) throws IOException {
-    File file = stringPathToFile(path.toString());
-    file.getParentFile().mkdirs();
-    try (BufferedSink sink = buffer(sink(file))) {
+    Path filePath = tempDir.resolve(path.toString());
+    createDirectories(filePath.getParent());
+    try (BufferedSink sink = buffer(sink(filePath))) {
       sink.write(content);
     }
-  }
-
-  private File stringPathToFile(String stringPath) {
-    return new File(tempDir.getAbsoluteFile() + "/" + stringPath);
   }
 }
