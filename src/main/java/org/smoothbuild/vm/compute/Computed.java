2a44593ca52e8cde7b37f77a8b72b4c6750a58cd
Marcin Mikosik <marcin.mikosik@gmail.com>
2022-02-03 12:40:49 +0100

    refactored Computed

--- src/main/java/org/smoothbuild/vm/compute/Computed.java
+++ src/main/java/org/smoothbuild/vm/compute/Computed.java
@@ -1,16 +1,11 @@
 package org.smoothbuild.vm.compute;
 
+import static com.google.common.base.Preconditions.checkArgument;
 import static java.util.Objects.requireNonNull;
 
 import org.smoothbuild.vm.job.algorithm.Output;
 
 public record Computed(Output output, Exception exception, ResSource resSource) {
-
-  public Computed(Output output, Exception exception, ResSource resSource) {
-    this.output = output;
-    this.exception = exception;
-    this.resSource = requireNonNull(resSource);
-  }
 
   public Computed(Output output, ResSource resSource) {
     this(output, null, resSource);
@@ -18,6 +13,13 @@ public record Computed(Output output, Exception exception, ResSource resSource) 
 
   public Computed(Exception exception, ResSource resSource) {
     this(null, exception, resSource);
+  }
+
+  public Computed(Output output, Exception exception, ResSource resSource) {
+    this.output = output;
+    this.exception = exception;
+    this.resSource = requireNonNull(resSource);
+    checkArgument(output == null ^ exception == null);
   }
 
   public boolean hasOutput() {
