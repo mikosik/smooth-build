7cf0a4e74cbf04a6821bacaad47ba97ee9c8d6e7
Marcin Mikosik <marcin.mikosik@gmail.com>
2022-02-04 17:36:23 +0100

    refactored FindTopEvals

--- src/main/java/org/smoothbuild/run/FindTopEvals.java
+++ src/main/java/org/smoothbuild/run/FindTopEvals.java
@@ -1,14 +1,12 @@
 package org.smoothbuild.run;
 
-import static org.smoothbuild.cli.console.Log.error;
 import static org.smoothbuild.lang.base.define.Loc.commandLineLoc;
 
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
 
-import org.smoothbuild.cli.console.Log;
+import org.smoothbuild.cli.console.LogBuffer;
 import org.smoothbuild.cli.console.Reporter;
 import org.smoothbuild.lang.base.define.DefsS;
 import org.smoothbuild.lang.base.define.ValS;
@@ -21,26 +19,26 @@ public class FindTopEvals {
       Reporter reporter, DefsS defs, List<String> names) {
     var topEvals = defs.topEvals();
     var topRefs = new HashSet<TopRefS>();
-    var logs = new ArrayList<Log>();
+    var logs = new LogBuffer();
     for (String name : names) {
       var topEval = topEvals.get(name);
       if (topEval != null) {
         if (topEval instanceof ValS value) {
           topRefs.add(new TopRefS(value.type(), value.name(), commandLineLoc()));
         } else {
-          logs.add(error(
-              "`" + name + "` cannot be calculated as it is not a value but a function."));
+          logs.error(
+              "`" + name + "` cannot be calculated as it is not a value but a function.");
         }
       } else {
-        logs.add(error("Unknown value `" + name + "`.\n"
-            + "Try 'smooth list' to see all available values that can be calculated."));
+        logs.error("Unknown value `" + name + "`.\n"
+            + "Try 'smooth list' to see all available values that can be calculated.");
       }
     }
-    reporter.report("command line arguments", logs);
-    if (logs.isEmpty()) {
-      return Optional.of(ImmutableList.copyOf(topRefs));
-    } else {
+    reporter.report("command line arguments", logs.toList());
+    if (logs.containsProblem()) {
       return Optional.empty();
+    } else {
+      return Optional.of(ImmutableList.copyOf(topRefs));
     }
   }
 }
