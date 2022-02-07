6b844272149bc651fd92452983d24215433e3377
Marcin Mikosik <marcin.mikosik@gmail.com>
2022-02-07 11:39:10 +0100

    fixed style in Reporter

--- src/main/java/org/smoothbuild/out/report/Reporter.java
+++ src/main/java/org/smoothbuild/out/report/Reporter.java
@@ -5,11 +5,11 @@ import java.util.List;
 import org.smoothbuild.out.log.Log;
 
 public interface Reporter {
-  void startNewPhase(String name);
+  public void startNewPhase(String name);
 
-  void report(List<Log> logs);
+  public void report(List<Log> logs);
 
-  void report(String taskHeader, List<Log> logs);
+  public void report(String taskHeader, List<Log> logs);
 
-  void printSummary();
+  public void printSummary();
 }
