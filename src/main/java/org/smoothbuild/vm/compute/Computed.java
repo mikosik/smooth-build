fc1639d4ce090db63edf58c316a3d151f67f485d
Marcin Mikosik <marcin.mikosik@gmail.com>
2022-02-07 14:02:45 +0100

    refactored Computed

--- src/main/java/org/smoothbuild/vm/compute/Computed.java
+++ src/main/java/org/smoothbuild/vm/compute/Computed.java
@@ -19,7 +19,7 @@ public record Computed(Output output, Exception exception, ResSource resSource) 
     this.output = output;
     this.exception = exception;
     this.resSource = requireNonNull(resSource);
-    checkArgument(output == null ^ exception == null);
+    checkArgument((output == null) != (exception == null));
   }
 
   public boolean hasOutput() {
