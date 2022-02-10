07241137e1b909af30321714be3c140b801c7a25
Marcin Mikosik <marcin.mikosik@gmail.com>
2022-02-10 14:37:35 +0100

    removed dead fields from FileStruct

--- src/main/java/org/smoothbuild/eval/artifact/FileStruct.java
+++ src/main/java/org/smoothbuild/eval/artifact/FileStruct.java
@@ -8,8 +8,6 @@ public class FileStruct {
   public static final String NAME = "File";
   private static final int PATH_IDX = 0;
   private static final int CONTENT_IDX = 1;
-  public static final String PATH_FIELD_NAME = "path";
-  public static final String CONTENT_FIELD_NAME = "content";
 
   public static StringB filePath(TupleB file) {
     return (StringB) file.get(PATH_IDX);
