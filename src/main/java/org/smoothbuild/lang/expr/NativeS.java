14b2f6bf188deacbd26713fb7245571791040bba
Marcin Mikosik <marcin.mikosik@gmail.com>
2022-02-11 11:59:32 +0100

    refactored NativeS

--- src/main/java/org/smoothbuild/lang/expr/NativeS.java
+++ src/main/java/org/smoothbuild/lang/expr/NativeS.java
@@ -8,7 +8,6 @@ import org.smoothbuild.lang.base.define.Loc;
 public record NativeS(StringS path, boolean isPure, Loc loc) {
   @Override
   public String toString() {
-    return """
-        @Native("%s", %s)""".formatted(path.string(), isPure ? "PURE" : "IMPURE");
+    return "@Native(\"%s\", %s)".formatted(path.string(), isPure ? "PURE" : "IMPURE");
   }
 }
