cdbdb67f978e4be32fe695fb7346c38cc9592196
Marcin Mikosik <marcin.mikosik@gmail.com>
2022-02-17 14:32:25 +0100

    fixed naming in CompilerTest

--- src/main/test/org/smoothbuild/compile/CompilerTest.java
+++ src/main/test/org/smoothbuild/compile/CompilerTest.java
@@ -35,14 +35,14 @@ public class CompilerTest extends TestingContext {
     @Test
     public void blob() {
       var blob = blobS(37);
-      assertConversion(blob, blobB(37));
+      assertCompilation(blob, blobB(37));
     }
 
     @Test
     public void call() {
       var defFunc = defFuncS("myFunc", nList(), stringS("abc"));
       var call = callS(stringTS(), topRefS(defFunc));
-      assertConversion(defFunc, call, callB(funcB(stringB("abc"))));
+      assertCompilation(defFunc, call, callB(funcB(stringB("abc"))));
     }
 
     @Test
@@ -52,51 +52,51 @@ public class CompilerTest extends TestingContext {
       var identity = defFuncS(oa, "myIdentity", nList(itemS(oa, "p")), paramRefS(ca, "p"));
       var call = callS(stringTS(), topRefS(identity), stringS("abc"));
       var v = oVarTB("A");
-      assertConversion(identity, call,
+      assertCompilation(identity, call,
           callB(stringTB(), funcB(v, list(v), paramRefB(close(v), 0)), stringB("abc")));
     }
 
     @Test
     public void combine() {
       var combine = combineS(stringS("abc"), intS(1));
-      assertConversion(combine, combineB(stringB("abc"), intB(1)));
+      assertCompilation(combine, combineB(stringB("abc"), intB(1)));
     }
 
     @Test
     public void int_() {
       IntS int_ = intS(1);
-      assertConversion(int_, intB(1));
+      assertCompilation(int_, intB(1));
     }
 
     @Test
     public void order() {
       var order = orderS(stringTS(), stringS("abc"), stringS("def"));
-      assertConversion(order, orderB(stringB("abc"), stringB("def")));
+      assertCompilation(order, orderB(stringB("abc"), stringB("def")));
     }
 
     @Test
     public void paramRef() {
       var func = defFuncS("f", nList(itemS(intTS(), "p")), paramRefS(intTS(), "p"));
-      assertConversion(func, topRefS(func), funcB(list(intTB()), paramRefB(intTB(), 0)));
+      assertCompilation(func, topRefS(func), funcB(list(intTB()), paramRefB(intTB(), 0)));
     }
 
     @Test
     public void select() {
       var combine = combineS(stringS("abc"));
       var select = selectS(stringTS(), combine, "field0");
-      assertConversion(select, selectB(combineB(stringB("abc")), intB(0)));
+      assertCompilation(select, selectB(combineB(stringB("abc")), intB(0)));
     }
 
     @Test
     public void string() {
       var string = stringS("abc");
-      assertConversion(string, stringB("abc"));
+      assertCompilation(string, stringB("abc"));
     }
 
     @Test
     public void topRef_to_val() {
       var defVal = defValS("myVal", stringS("abc"));
-      assertConversion(defVal, topRefS(defVal), stringB("abc"));
+      assertCompilation(defVal, topRefS(defVal), stringB("abc"));
     }
 
     @Test
@@ -119,13 +119,13 @@ public class CompilerTest extends TestingContext {
     @Test
     public void topRef_to_func() {
       var defFunc = defFuncS("myFunc", nList(), stringS("abc"));
-      assertConversion(defFunc, topRefS(defFunc), funcB(stringB("abc")));
+      assertCompilation(defFunc, topRefS(defFunc), funcB(stringB("abc")));
     }
 
     @Test
     public void topRef_to_func_with_bodyT_being_subtype_of_resT() {
       var defFunc = defFuncS(arrayTS(stringTS()), "myFunc", nList(), orderS(nothingTS()));
-      assertConversion(defFunc, topRefS(defFunc),
+      assertCompilation(defFunc, topRefS(defFunc),
           funcB(arrayTB(stringTB()), list(), orderB(nothingTB())));
     }
 
@@ -173,15 +173,15 @@ public class CompilerTest extends TestingContext {
           .isEqualTo(funcB);
     }
 
-    private void assertConversion(ExprS exprS, ObjB expected) {
-      assertConversion(defs(), exprS, expected);
+    private void assertCompilation(ExprS exprS, ObjB expected) {
+      assertCompilation(defs(), exprS, expected);
     }
 
-    private void assertConversion(TopEvalS topEval, ExprS exprS, ObjB expected) {
-      assertConversion(defs(topEval), exprS, expected);
+    private void assertCompilation(TopEvalS topEval, ExprS exprS, ObjB expected) {
+      assertCompilation(defs(topEval), exprS, expected);
     }
 
-    private void assertConversion(DefsS defs, ExprS exprS, ObjB expected) {
+    private void assertCompilation(DefsS defs, ExprS exprS, ObjB expected) {
       var compiler = newCompiler(defs);
       assertThat(compiler.compileExpr(exprS))
           .isEqualTo(expected);
@@ -191,21 +191,21 @@ public class CompilerTest extends TestingContext {
   @Nested
   class _caching {
     @Test
-    public void val_conversion_result() {
-      assertConversionIsCached(defValS("myVal", stringS("abcdefghi")));
+    public void val_compilation_result() {
+      assertCompilationIsCached(defValS("myVal", stringS("abcdefghi")));
     }
 
     @Test
-    public void def_func_conversion_result() {
-      assertConversionIsCached(defFuncS("myFunc", nList(), stringS("abcdefghi")));
+    public void def_func_compilation_result() {
+      assertCompilationIsCached(defFuncS("myFunc", nList(), stringS("abcdefghi")));
     }
 
     @Test
-    public void nat_func_conversion_result() {
-      assertConversionIsCached(natFuncS(funcTS(stringTS()), "myFunc", nList()));
+    public void nat_func_compilation_result() {
+      assertCompilationIsCached(natFuncS(funcTS(stringTS()), "myFunc", nList()));
     }
 
-    private void assertConversionIsCached(TopEvalS topEval) {
+    private void assertCompilationIsCached(TopEvalS topEval) {
       var compiler = newCompiler(topEval);
       assertThat(compiler.compileExpr(topRefS(topEval)))
           .isSameInstanceAs(compiler.compileExpr(topRefS(topEval)));
