d81ad4557c436f102ebc7aac88d578d1256226fc
Marcin Mikosik <marcin.mikosik@gmail.com>
2022-02-25 15:01:35 +0100

    refactored Bounds<T extends Type> -> Bounds<T>

--- src/main/java/org/smoothbuild/lang/type/api/Bounds.java
+++ src/main/java/org/smoothbuild/lang/type/api/Bounds.java
@@ -1,6 +1,6 @@
 package org.smoothbuild.lang.type.api;
 
-public record Bounds<T extends Type>(T lower, T upper) {
+public record Bounds<T>(T lower, T upper) {
   public T get(Sides.Side<T> side) {
     return switch (side) {
       case Sides.Lower l -> lower;
