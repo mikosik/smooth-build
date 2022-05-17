eb0070c97d4d66fd94b9ca14ae059b217cf7cb00
Marcin Mikosik <marcin.mikosik@gmail.com>
2022-05-17 09:22:36 +0200

    added MergingTS.merge()

--- src/main/java/org/smoothbuild/lang/type/MergingTS.java
+++ src/main/java/org/smoothbuild/lang/type/MergingTS.java
@@ -1,7 +1,19 @@
 package org.smoothbuild.lang.type;
 
+import static org.smoothbuild.lang.type.JoinTS.join;
+import static org.smoothbuild.lang.type.MeetTS.meet;
+
+import org.smoothbuild.util.type.Side;
+
 public abstract sealed class MergingTS extends TypeS permits JoinTS, MeetTS {
   protected MergingTS(String name, VarSetS vars) {
     super(name, vars);
+  }
+
+  public static TypeS merge(TypeS a, TypeS b, Side direction) {
+    return switch (direction) {
+      case UPPER -> join(a, b);
+      case LOWER -> meet(a, b);
+    };
   }
 }
