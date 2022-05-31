65bac906c8f417531e822761606acdb20394bebf
Marcin Mikosik <marcin.mikosik@gmail.com>
2022-05-31 10:31:00 +0200

    fixed RefableObjS to implement RefableS

--- src/main/java/org/smoothbuild/lang/define/RefableObjS.java
+++ src/main/java/org/smoothbuild/lang/define/RefableObjS.java
@@ -8,7 +8,7 @@ import org.smoothbuild.lang.type.TypeS;
 /**
  * Top level evaluable.
  */
-public sealed abstract class RefableObjS extends Patanal implements RefableObj
+public sealed abstract class RefableObjS extends Patanal implements RefableObj, RefableS
     permits FuncS, ValS {
   public RefableObjS(TypeS type, ModPath modPath, String name, Loc loc) {
     super(type, modPath, name, loc);
