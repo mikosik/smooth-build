92576d63b3b8cd79fae650f7c742f4668c49ee39
Marcin Mikosik <marcin.mikosik@gmail.com>
2022-06-28 12:32:36 +0200

    added fields verification to Bounds

--- src/main/java/org/smoothbuild/util/type/Bounds.java
+++ src/main/java/org/smoothbuild/util/type/Bounds.java
@@ -1,6 +1,13 @@
 package org.smoothbuild.util.type;
 
+import static java.util.Objects.requireNonNull;
+
 public record Bounds<T>(T lower, T upper) {
+  public Bounds {
+    requireNonNull(lower);
+    requireNonNull(upper);
+  }
+
   public T get(Side side) {
     return switch (side) {
       case LOWER -> lower;
