c2c130ae7466ec4ed200728a37d7007be195ab66
Marcin Mikosik <marcin.mikosik@gmail.com>
2022-06-28 06:38:40 +0200

    removed no longer used TypeFB.edge()

--- src/main/java/org/smoothbuild/bytecode/type/TypeFB.java
+++ src/main/java/org/smoothbuild/bytecode/type/TypeFB.java
@@ -22,13 +22,6 @@ public interface TypeFB {
 
   public TupleTB tuple(List<? extends TypeB> items);
 
-  public default TypeB edge(Side side) {
-    return switch (side) {
-      case LOWER -> nothing();
-      case UPPER -> any();
-    };
-  }
-
   public default Bounds<TypeB> oneSideBound(Side side, TypeB type) {
     return switch (side) {
       case LOWER -> new Bounds<>(type, any());
