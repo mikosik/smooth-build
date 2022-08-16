4b4441be1cf4ef42c73f56f1bc1d67baf035a2f9
Marcin Mikosik <marcin.mikosik@gmail.com>
2022-08-16 10:45:09 +0200

    added Optionals.pullUp(Map<>)

--- src/main/java/org/smoothbuild/util/collect/Optionals.java
+++ src/main/java/org/smoothbuild/util/collect/Optionals.java
@@ -1,10 +1,13 @@
 package org.smoothbuild.util.collect;
 
 import static org.smoothbuild.util.collect.Lists.map;
+import static org.smoothbuild.util.collect.Maps.mapValues;
 
+import java.util.Map;
 import java.util.Optional;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Streams;
 
 public class Optionals {
@@ -14,6 +17,15 @@ public class Optionals {
       return Optional.empty();
     } else {
       return Optional.of(map(iterable, Optional::get));
+    }
+  }
+
+  public static <K, V> Optional<ImmutableMap<K, V>> pullUp(
+      Map<K, ? extends Optional<? extends V>> map) {
+    if (map.values().stream().anyMatch(Optional::isEmpty)) {
+      return Optional.empty();
+    } else {
+      return Optional.of(mapValues(map, Optional::get));
     }
   }
 }
--- src/main/test/org/smoothbuild/util/collect/OptionalsTest.java
+++ src/main/test/org/smoothbuild/util/collect/OptionalsTest.java
@@ -4,6 +4,7 @@ import static com.google.common.truth.Truth.assertThat;
 import static org.smoothbuild.util.collect.Lists.list;
 import static org.smoothbuild.util.collect.Optionals.pullUp;
 
+import java.util.Map;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Nested;
@@ -11,7 +12,7 @@ import org.junit.jupiter.api.Test;
 
 public class OptionalsTest {
   @Nested
-  class _pull_up {
+  class _pull_up_iterable {
     @Test
     public void iterable_with_zero_elems() {
       assertThat(pullUp(list()))
@@ -28,6 +29,27 @@ public class OptionalsTest {
     public void iterable_with_all_elems_present() {
       assertThat(pullUp(list(Optional.of("abc"), Optional.of("def"))))
           .isEqualTo(Optional.of(list("abc", "def")));
+    }
+  }
+
+  @Nested
+  class _pull_up_map {
+    @Test
+    public void map_with_zero_elems() {
+      assertThat(pullUp(Map.of()))
+          .isEqualTo(Optional.of(Map.of()));
+    }
+
+    @Test
+    public void map_with_empty_optional() {
+      assertThat(pullUp(Map.of("key1", Optional.of("abc"), "key2", Optional.empty())))
+          .isEqualTo(Optional.empty());
+    }
+
+    @Test
+    public void map_with_all_elems_present() {
+      assertThat(pullUp(Map.of("key1", Optional.of("abc"), "key2", Optional.of("def"))))
+          .isEqualTo(Optional.of(Map.of("key1", "abc", "key2", "def")));
     }
   }
 }
