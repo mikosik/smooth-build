a1d5ba3d591642be41057e84e8ebc26930d88d7d
Marcin Mikosik <marcin.mikosik@gmail.com>
2022-09-05 14:56:54 +0200

    removed no longer needed TestedXxxB, TestedAssignXxxS

--- src/testing/java/org/smoothbuild/testing/type/TestedAssignB.java
+++
@@ -1,8 +0,0 @@
-package org.smoothbuild.testing.type;
-
-public record TestedAssignB(TestedTB target, TestedTB source) {
-  @Override
-  public String toString() {
-    return target().name() + " <- " + source().name();
-  }
-}
