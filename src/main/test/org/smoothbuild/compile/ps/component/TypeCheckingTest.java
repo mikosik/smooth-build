ca42f5902b5eb5e8eecc02055b7c16d3e3e62322
Marcin Mikosik <marcin.mikosik@gmail.com>
2022-10-04 09:26:36 +0200

    cleaned up TypeCheckingTest, added more cases

--- src/main/test/org/smoothbuild/compile/ps/component/TypeCheckingTest.java
+++ src/main/test/org/smoothbuild/compile/ps/component/TypeCheckingTest.java
@@ -8,7 +8,7 @@ public class TypeCheckingTest extends TestContext {
   @Nested
   class _value_type_and_its_body_type {
     @Test
-    public void succeeds_when_types_are_assignable() {
+    public void mono_to_mono_success() {
       var sourceCode = """
           Int result = 7;
           """;
@@ -17,7 +17,7 @@ public class TypeCheckingTest extends TestContext {
     }
 
     @Test
-    public void fails_when_types_are_not_assignable() {
+    public void mono_to_mono_error() {
       var sourceCode = """
           Blob result = "abc";
           """;
@@ -26,7 +26,16 @@ public class TypeCheckingTest extends TestContext {
     }
 
     @Test
-    public void succeeds_when_body_type_is_more_general_than_declared_type() {
+    public void mono_to_poly_error() {
+      var sourceCode = """
+          A result = 7;
+          """;
+      module(sourceCode)
+          .loadsWithError(1, "`result` body type is not equal to declared type.");
+    }
+
+    @Test
+    public void poly_to_mono_success() {
       var sourceCode = """
           [Int] result = [];
           """;
@@ -35,9 +44,27 @@ public class TypeCheckingTest extends TestContext {
     }
 
     @Test
-    public void fails_when_body_type_is_more_specific_than_declared_type() {
+    public void poly_to_mono_error() {
       var sourceCode = """
-          A result = 7;
+          [Int] result = [[]];
+          """;
+      module(sourceCode)
+          .loadsWithError(1, "`result` body type is not equal to declared type.");
+    }
+
+    @Test
+    public void poly_to_poly_success() {
+      var sourceCode = """
+          [A] result = [];
+          """;
+      module(sourceCode)
+          .loadsWithSuccess();
+    }
+
+    @Test
+    public void poly_to_poly_error() {
+      var sourceCode = """
+          [A] result = [[]];
           """;
       module(sourceCode)
           .loadsWithError(1, "`result` body type is not equal to declared type.");
@@ -47,7 +74,7 @@ public class TypeCheckingTest extends TestContext {
   @Nested
   class _func_res_type_and_its_body_type {
     @Test
-    public void succeeds_when_types_are_assignable() {
+    public void mono_to_mono_success() {
       var sourceCode = """
           Int myFunc() = 7;
           """;
@@ -56,16 +83,25 @@ public class TypeCheckingTest extends TestContext {
     }
 
     @Test
-    public void fails_when_types_are_not_assignable() {
+    public void mono_to_mono_error() {
       var sourceCode = """
-          Int myFunc() = "abc";
+          Blob myFunc() = "abc";
           """;
       module(sourceCode)
           .loadsWithError(1, "`myFunc` body type is not equal to declared type.");
     }
 
     @Test
-    public void succeeds_when_body_type_is_more_general_than_declared_result_type() {
+    public void mono_to_poly_error() {
+      var sourceCode = """
+          A myFunc() = 7;
+          """;
+      module(sourceCode)
+          .loadsWithError(1, "`myFunc` body type is not equal to declared type.");
+    }
+
+    @Test
+    public void poly_to_mono_success() {
       var sourceCode = """
           [Int] myFunc() = [];
           """;
@@ -74,9 +110,27 @@ public class TypeCheckingTest extends TestContext {
     }
 
     @Test
-    public void fails_when_body_type_is_more_specific_than_declared_result_type() {
+    public void poly_to_mono_error() {
       var sourceCode = """
-          A myFunc() = 7;
+          [Int] myFunc() = [[]];
+          """;
+      module(sourceCode)
+          .loadsWithError(1, "`myFunc` body type is not equal to declared type.");
+    }
+
+    @Test
+    public void poly_to_poly_success() {
+      var sourceCode = """
+          [A] myFunc() = [];
+          """;
+      module(sourceCode)
+          .loadsWithSuccess();
+    }
+
+    @Test
+    public void poly_to_poly_error() {
+      var sourceCode = """
+          [A] myFunc() = [[]];
           """;
       module(sourceCode)
           .loadsWithError(1, "`myFunc` body type is not equal to declared type.");
@@ -86,145 +140,259 @@ public class TypeCheckingTest extends TestContext {
   @Nested
   class _func_param_type_and_arg_type {
     @Test
-    public void fails_when_types_are_not_assignable() {
+    public void mono_to_mono_success() {
       var sourceCode = """
-          String myFunc([Blob] blobArray) = "abc";
-          result = myFunc([1, 2, 3]);
+          String myFunc(Int int) = "abc";
+          result = myFunc(7);
+          """;
+      module(sourceCode)
+          .loadsWithSuccess();
+    }
+
+    @Test
+    public void mono_to_mono_error() {
+      var sourceCode = """
+          String myFunc(Blob blob) = "abc";
+          result = myFunc(7);
           """;
       module(sourceCode)
           .loadsWithError(2, "Illegal call.");
     }
 
     @Test
-    public void succeeds_when_types_are_assignable() {
+    public void mono_to_poly_success() {
       var sourceCode = """
-          String myFunc([[Int]] param) = "abc";
+          String myFunc(A a) = "abc";
+          result = myFunc(7);
+          """;
+      module(sourceCode)
+          .loadsWithSuccess();
+    }
+
+    @Test
+    public void mono_to_poly_error() {
+      var sourceCode = """
+          String myFunc([A] a) = "abc";
+          result = myFunc(7);
+          """;
+      module(sourceCode)
+          .loadsWithError(2, "Illegal call.");
+    }
+
+    @Test
+    public void poly_to_mono_success() {
+      var sourceCode = """
+          String myFunc([Int] param) = "abc";
           result = myFunc([]);
           """;
       module(sourceCode)
           .loadsWithSuccess();
+    }
+
+    @Test
+    public void poly_to_mono_error() {
+      var sourceCode = """
+          String myFunc([Int] param) = "abc";
+          result = myFunc([[]]);
+          """;
+      module(sourceCode)
+          .loadsWithError(2, "Illegal call.");
+    }
+
+    @Test
+    public void poly_to_poly_success() {
+      var sourceCode = """
+          String myFunc([A] param) = "abc";
+          result = myFunc([]);
+          """;
+      module(sourceCode)
+          .loadsWithSuccess();
+    }
+
+    @Test
+    public void poly_to_poly_error() {
+      var sourceCode = """
+          String myFunc(A(A) param) = "abc";
+          result = myFunc([]);
+          """;
+      module(sourceCode)
+          .loadsWithError(2, "Illegal call.");
     }
   }
 
   @Nested
   class _func_named_param_type_and_arg_type {
     @Test
-    public void fails_when_types_are_not_assignable() {
+    public void mono_to_mono_success() {
       var sourceCode = """
-          String myFunc([Blob] blobArray) = "abc";
-          result = myFunc(blobArray = [1, 2, 3]);
+          String myFunc(Int int) = "abc";
+          result = myFunc(int=7);
+          """;
+      module(sourceCode)
+          .loadsWithSuccess();
+    }
+
+    @Test
+    public void mono_to_mono_error() {
+      var sourceCode = """
+          String myFunc(Blob blob) = "abc";
+          result = myFunc(blob=7);
           """;
       module(sourceCode)
           .loadsWithError(2, "Illegal call.");
     }
 
     @Test
-    public void succeeds_when_types_are_assignable() {
+    public void mono_to_poly_success() {
       var sourceCode = """
-          String myFunc([[Int]] param) = "abc";
-          result = myFunc(param = []);
+          String myFunc(A a) = "abc";
+          result = myFunc(a=7);
           """;
       module(sourceCode)
           .loadsWithSuccess();
+    }
+
+    @Test
+    public void mono_to_poly_error() {
+      var sourceCode = """
+          String myFunc([A] a) = "abc";
+          result = myFunc(a=7);
+          """;
+      module(sourceCode)
+          .loadsWithError(2, "Illegal call.");
+    }
+
+    @Test
+    public void poly_to_mono_success() {
+      var sourceCode = """
+          String myFunc([Int] param) = "abc";
+          result = myFunc(param=[]);
+          """;
+      module(sourceCode)
+          .loadsWithSuccess();
+    }
+
+    @Test
+    public void poly_to_mono_error() {
+      var sourceCode = """
+          String myFunc([Int] param) = "abc";
+          result = myFunc(param=[[]]);
+          """;
+      module(sourceCode)
+          .loadsWithError(2, "Illegal call.");
+    }
+
+    @Test
+    public void poly_to_poly_success() {
+      var sourceCode = """
+          String myFunc([A] param) = "abc";
+          result = myFunc(param=[]);
+          """;
+      module(sourceCode)
+          .loadsWithSuccess();
+    }
+
+    @Test
+    public void poly_to_poly_error() {
+      var sourceCode = """
+          String myFunc(A(A) param) = "abc";
+          result = myFunc(param=[]);
+          """;
+      module(sourceCode)
+          .loadsWithError(2, "Illegal call.");
     }
   }
 
   @Nested
   class _func_param_type_and_param_default_val_type {
-    @Nested
-    class _fails_when_types_are_not_assignable {
-      @Test
-      public void mono_to_mono_case() {
-        var sourceCode = """
+    @Test
+    public void mono_to_mono_success() {
+      var sourceCode = """
+            Int myFunc(Int int = 7) = int;
+            """;
+      module(sourceCode)
+          .loadsWithSuccess();
+    }
+
+    @Test
+    public void mono_to_mono_error() {
+      var sourceCode = """
             String myFunc([Blob] blobArray = [1, 2, 3]) = "abc";
             """;
-        module(sourceCode)
-            .loadsWithError(1, "Parameter `blobArray` has type `[Blob]` so it cannot have default "
-                + "value with type `[Int]`.");
-      }
+      module(sourceCode)
+          .loadsWithError(1, "Parameter `blobArray` has type `[Blob]` so it cannot have default "
+              + "value with type `[Int]`.");
+    }
 
-      @Test
-      public void mono_to_poly_case() {
-        var sourceCode = """
+    @Test
+    public void mono_to_poly_success() {
+      var sourceCode = """
+            A myFunc(A a = 7) = a;
+            """;
+      module(sourceCode)
+          .loadsWithSuccess();
+    }
+
+    @Test
+    public void mono_to_poly_error() {
+      var sourceCode = """
             Int twoParamFunc(String s, Blob b) = 7;
             A(B) myFunc(A(B) funcParam = twoParamFunc) = funcParam;
             """;
-        module(sourceCode)
-            .loadsWithError(2, "Parameter `funcParam` has type `A(B)` so it cannot have default "
-                + "value with type `Int(String,Blob)`.");
-      }
-
-      @Test
-      public void poly_to_mono_case() {
-        var sourceCode = """
-            A myIdentity (A a) = a;
-            Int(Int, Int) myFunc(Int(Int, Int) funcParam = myIdentity) = funcParam;
-            """;
-        module(sourceCode)
-            .loadsWithError(2, "Parameter `funcParam` has type `Int(Int,Int)` so it cannot have"
-                + " default value with type `A(A)`.");
-      }
-
-      @Test
-      public void poly_to_poly_case() {
-        var sourceCode = """
-            A myIdentity (A a) = a;
-            A(A, A) myFunc(A(A, A) funcParam = myIdentity) = funcParam;
-            """;
-        module(sourceCode)
-            .loadsWithError(2, "Parameter `funcParam` has type `A(A,A)` so it cannot have "
-                + "default value with type `A(A)`.");
-      }
+      module(sourceCode)
+          .loadsWithError(2, "Parameter `funcParam` has type `A(B)` so it cannot have default "
+              + "value with type `Int(String,Blob)`.");
     }
 
-    @Nested
-    class _succeeds_when_types_are_assignable {
-      @Test
-      public void mono_to_mono_case() {
-        var sourceCode = """
-            Int myFunc(Int int = 7) = int;
-            """;
-        module(sourceCode)
-            .loadsWithSuccess();
-      }
-
-      @Test
-      public void mono_to_poly_case() {
-        var sourceCode = """
-            A myFunc(A a = 7) = a;
-            """;
-        module(sourceCode)
-            .loadsWithSuccess();
-      }
-
-      @Test
-      public void poly_to_mono_case1() {
-        var sourceCode = """
+    @Test
+    public void poly_to_mono_success1() {
+      var sourceCode = """
             A myIdentity (A a) = a;
             Int(Int) myFunc(Int(Int) funcParam = myIdentity) = funcParam;
             """;
-        module(sourceCode)
-            .loadsWithSuccess();
-      }
+      module(sourceCode)
+          .loadsWithSuccess();
+    }
 
-      @Test
-      public void poly_to_mono_case2() {
-        var sourceCode = """
+    @Test
+    public void poly_to_mono_success2() {
+      var sourceCode = """
             String myFunc([[Int]] blobArray = []) = "abc";
             """;
-        module(sourceCode)
-            .loadsWithSuccess();
-      }
+      module(sourceCode)
+          .loadsWithSuccess();
+    }
 
-      @Test
-      public void poly_to_poly() {
-        var sourceCode = """
+    @Test
+    public void poly_to_mono_error() {
+      var sourceCode = """
+            A myIdentity (A a) = a;
+            Int(Int, Int) myFunc(Int(Int, Int) funcParam = myIdentity) = funcParam;
+            """;
+      module(sourceCode)
+          .loadsWithError(2, "Parameter `funcParam` has type `Int(Int,Int)` so it cannot have"
+              + " default value with type `A(A)`.");
+    }
+
+    @Test
+    public void poly_to_poly_success() {
+      var sourceCode = """
             A myIdentity (A a) = a;
             B(B) myFunc(B(B) funcParam = myIdentity) = funcParam;
             """;
-        module(sourceCode)
-            .loadsWithSuccess();
-      }
+      module(sourceCode)
+          .loadsWithSuccess();
+    }
+
+    @Test
+    public void poly_to_poly_error() {
+      var sourceCode = """
+            A myIdentity (A a) = a;
+            A(A, A) myFunc(A(A, A) funcParam = myIdentity) = funcParam;
+            """;
+      module(sourceCode)
+          .loadsWithError(2, "Parameter `funcParam` has type `A(A,A)` so it cannot have "
+              + "default value with type `A(A)`.");
     }
   }
 
