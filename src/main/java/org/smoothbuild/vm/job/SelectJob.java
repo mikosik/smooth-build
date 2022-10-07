3f322b6051d84a76485969847488d81fa02ef034
Marcin Mikosik <marcin.mikosik@gmail.com>
2022-10-07 16:40:04 +0200

    refactored SelectJob

--- src/main/java/org/smoothbuild/vm/job/SelectJob.java
+++ src/main/java/org/smoothbuild/vm/job/SelectJob.java
@@ -2,12 +2,8 @@ package org.smoothbuild.vm.job;
 
 import static org.smoothbuild.util.collect.Lists.list;
 
-import org.smoothbuild.bytecode.expr.ExprB;
 import org.smoothbuild.bytecode.expr.oper.SelectB;
 import org.smoothbuild.bytecode.expr.val.InstB;
-import org.smoothbuild.bytecode.expr.val.IntB;
-import org.smoothbuild.bytecode.type.val.TupleTB;
-import org.smoothbuild.bytecode.type.val.TypeB;
 import org.smoothbuild.util.concurrent.Promise;
 import org.smoothbuild.vm.task.SelectTask;
 
@@ -24,11 +20,7 @@ public class SelectJob extends ExecutingJob {
     var data = selectB.data();
     var selectable = data.selectable();
     var index = data.index();
-    var task = new SelectTask(outputT(selectable, index), context().labeledLoc(selectB));
+    var task = new SelectTask(selectB.type(), context().labeledLoc(selectB));
     return evaluateTransitively(task, list(selectable, index));
-  }
-
-  private static TypeB outputT(ExprB selectable, IntB index) {
-    return ((TupleTB) selectable.type()).items().get(index.toJ().intValue());
   }
 }
