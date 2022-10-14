480b56dc9b03eadf7e7870dbae008cef98d412c6
Marcin Mikosik <marcin.mikosik@gmail.com>
2022-10-14 11:58:42 +0200

    refactored TaskInfo

--- src/main/java/org/smoothbuild/vm/execute/TaskInfo.java
+++ src/main/java/org/smoothbuild/vm/execute/TaskInfo.java
@@ -3,14 +3,18 @@ package org.smoothbuild.vm.execute;
 import org.smoothbuild.compile.lang.base.Loc;
 import org.smoothbuild.compile.lang.base.TagLoc;
 
-public record TaskInfo(TaskKind kind, String tag, Loc loc) {
+public record TaskInfo(TaskKind kind, TagLoc tagLoc) {
   public static final int NAME_LENGTH_LIMIT = 43;
 
-  public TaskInfo(TaskKind kind, TagLoc tagLoc) {
-    this(kind, tagLoc.tag(), tagLoc.loc());
+  public TaskInfo(TaskKind call, String tag, Loc loc) {
+    this(call, new TagLoc(tag, loc));
   }
 
-  public TagLoc tagLoc() {
-    return new TagLoc(tag, loc);
+  public String tag() {
+    return tagLoc.tag();
+  }
+
+  public Loc loc() {
+    return tagLoc.loc();
   }
 }
