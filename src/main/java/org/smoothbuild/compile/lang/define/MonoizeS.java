390525827896614c938d0b10ca1f135d17b9df66
Marcin Mikosik <marcin.mikosik@gmail.com>
2022-11-02 10:01:53 +0100

    refactored MonoizeS to java record

--- src/main/java/org/smoothbuild/compile/lang/define/MonoizeS.java
+++ src/main/java/org/smoothbuild/compile/lang/define/MonoizeS.java
@@ -1,9 +1,6 @@
 package org.smoothbuild.compile.lang.define;
 
-import java.util.Objects;
-
 import org.smoothbuild.compile.lang.base.Loc;
-import org.smoothbuild.compile.lang.base.WithLocImpl;
 import org.smoothbuild.compile.lang.type.TypeS;
 import org.smoothbuild.compile.lang.type.VarS;
 
@@ -12,45 +9,14 @@ import com.google.common.collect.ImmutableMap;
 /**
  * Monomorphization of polymorphic referencable.
  */
-public final class MonoizeS extends WithLocImpl implements OperS {
-  private final ImmutableMap<VarS, TypeS> varMap;
-  private final PolyEvaluableS polyEvaluable;
-  private final TypeS evalT;
+public record MonoizeS(
+    ImmutableMap<VarS, TypeS> varMap,
+    PolyEvaluableS polyEvaluable,
+    TypeS evalT,
+    Loc loc)
+    implements OperS {
 
   public MonoizeS(ImmutableMap<VarS, TypeS> varMap, PolyEvaluableS polyEvaluable, Loc loc) {
-    super(loc);
-    this.varMap = varMap;
-    this.polyEvaluable = polyEvaluable;
-    this.evalT = polyEvaluable.schema().monoize(varMap);
-  }
-
-  public ImmutableMap<VarS, TypeS> varMap() {
-    return varMap;
-  }
-
-  public PolyEvaluableS polyEvaluable() {
-    return polyEvaluable;
-  }
-
-  @Override
-  public TypeS evalT() {
-    return evalT;
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) {
-      return true;
-    }
-    return o instanceof MonoizeS monoizeS
-        && evalT.equals(monoizeS.evalT)
-        && varMap.equals(monoizeS.varMap)
-        && polyEvaluable.equals(monoizeS.polyEvaluable)
-        && loc().equals(monoizeS.loc());
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(evalT, varMap, polyEvaluable, loc());
+    this(varMap, polyEvaluable, polyEvaluable.schema().monoize(varMap), loc);
   }
 }
