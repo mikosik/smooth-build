925cca5a47d7475cc139ebb262deac6bb633ac91
Marcin Mikosik <marcin.mikosik@gmail.com>
2022-12-27 14:02:43 +0100

    improved API of SingleScopeBindings

--- src/main/java/org/smoothbuild/util/bindings/SingleScopeBindings.java
+++ src/main/java/org/smoothbuild/util/bindings/SingleScopeBindings.java
@@ -2,12 +2,14 @@ package org.smoothbuild.util.bindings;
 
 import java.util.Map;
 
+import com.google.common.collect.ImmutableMap;
+
 public class SingleScopeBindings<E> extends ImmutableBindings<E> {
   protected SingleScopeBindings(Map<String, ? extends E> innerScopeMap) {
     super(null, innerScopeMap);
   }
 
-  public Map<String, E> toMap() {
+  public ImmutableMap<String, E> toMap() {
     return innerScopeMap;
   }
 }
