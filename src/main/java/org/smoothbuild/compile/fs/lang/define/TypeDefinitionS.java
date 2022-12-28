2590b09611b791922e0be9fd0ac969fcc23f4738
Marcin Mikosik <marcin.mikosik@gmail.com>
2022-12-28 13:28:37 +0100

    added TypeDefinitionS.toString()

--- src/main/java/org/smoothbuild/compile/fs/lang/define/TypeDefinitionS.java
+++ src/main/java/org/smoothbuild/compile/fs/lang/define/TypeDefinitionS.java
@@ -1,5 +1,8 @@
 package org.smoothbuild.compile.fs.lang.define;
 
+import static org.smoothbuild.util.Strings.indent;
+import static org.smoothbuild.util.collect.Iterables.joinToString;
+
 import org.smoothbuild.compile.fs.lang.base.Tanal;
 import org.smoothbuild.compile.fs.lang.base.location.Location;
 import org.smoothbuild.compile.fs.lang.type.TypeS;
@@ -10,5 +13,14 @@ import org.smoothbuild.compile.fs.lang.type.TypeS;
 public class TypeDefinitionS extends Tanal {
   public TypeDefinitionS(TypeS typeS, Location location) {
     super(typeS, typeS.name(), location);
+  }
+
+  @Override
+  public String toString() {
+    var fields = joinToString("\n",
+        "type = " + type(),
+        "location = " + location()
+    );
+    return "TypeDefinitionS(\n" + indent(fields) + "\n)";
   }
 }
