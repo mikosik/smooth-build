dfe434c9c91ff9b60a07041f7c49d06255ce5b82
Marcin Mikosik <marcin.mikosik@gmail.com>
2023-01-04 10:20:51 +0100

    refactored NameablesTest to not use Labeled

--- src/main/test/org/smoothbuild/util/collect/NameablesTest.java
+++ src/main/test/org/smoothbuild/util/collect/NameablesTest.java
@@ -1,7 +1,7 @@
 package org.smoothbuild.util.collect;
 
 import static com.google.common.truth.Truth.assertThat;
-import static org.smoothbuild.util.collect.Labeled.labeled;
+import static org.smoothbuild.testing.TestContext.location;
 import static org.smoothbuild.util.collect.Nameables.toMap;
 
 import java.util.HashMap;
@@ -10,11 +10,12 @@ import java.util.Map;
 
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.smoothbuild.compile.fs.lang.base.Nal;
+import org.smoothbuild.compile.fs.lang.base.NalImpl;
 
 public class NameablesTest {
-
-  private static final Labeled<Integer> TWO = labeled("two", 2);
-  private static final Labeled<Integer> ONE = labeled("one", 1);
+  private static final Nal ONE = new NalImpl("one", location(1));
+  private static final Nal TWO = new NalImpl("two", location(2));
 
   @Nested
   class _to_map {
@@ -27,12 +28,6 @@ public class NameablesTest {
     @Test
     public void non_empty_collection() {
       assertThat(toMap(List.of(ONE, TWO)))
-          .isEqualTo(Map.of("one", ONE, "two", TWO));
-    }
-
-    @Test
-    public void collection_with_nameables_without_name() {
-      assertThat(toMap(List.of(ONE, labeled(11), TWO)))
           .isEqualTo(Map.of("one", ONE, "two", TWO));
     }
   }
