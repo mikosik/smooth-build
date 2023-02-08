b56d5daf85426e8ba512278e018dbf817a2ea8ab
Marcin Mikosik <marcin.mikosik@gmail.com>
2023-02-08 13:12:38 +0100

    changed InterfaceTS.fieldSet type to ImmutableSortedMap

--- src/main/java/org/smoothbuild/compile/fs/lang/type/InterfaceTS.java
+++ src/main/java/org/smoothbuild/compile/fs/lang/type/InterfaceTS.java
@@ -5,20 +5,21 @@ import static org.smoothbuild.compile.fs.lang.base.TypeNamesS.interfaceTypeName;
 import org.smoothbuild.compile.fs.lang.define.ItemSigS;
 
 import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSortedMap;
 
 /**
  * This class is immutable.
  */
 public final class InterfaceTS extends FieldSetTS {
-  private final ImmutableMap<String, ItemSigS> fields;
+  private final ImmutableSortedMap<String, ItemSigS> fields;
 
   public InterfaceTS(ImmutableMap<String, ItemSigS> fields) {
     super(interfaceTypeName(fields), calculateFieldSetVars(fields.values()));
-    this.fields = fields;
+    this.fields = ImmutableSortedMap.copyOf(fields);
   }
 
   @Override
-  public ImmutableMap<String, ItemSigS> fieldSet() {
+  public ImmutableSortedMap<String, ItemSigS> fieldSet() {
     return fields;
   }
 
