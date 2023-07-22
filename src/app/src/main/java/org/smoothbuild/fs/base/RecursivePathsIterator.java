fbfd3e04d514f991c897af8acee8bf1b57bf0254
Marcin Mikosik <marcin.mikosik@gmail.com>
2023-07-22 19:35:39 +0200

    Refactor RecursivePathIterator

--- src/app/src/main/java/org/smoothbuild/fs/base/RecursivePathsIterator.java
+++ src/app/src/main/java/org/smoothbuild/fs/base/RecursivePathsIterator.java
@@ -3,17 +3,17 @@ package org.smoothbuild.fs.base;
 import static com.google.common.base.Preconditions.checkState;
 import static java.lang.String.format;
 import static org.smoothbuild.fs.base.PathS.root;
-import static org.smoothbuild.util.Throwables.unexpectedCaseExc;
 
 import java.io.IOException;
 import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.NoSuchElementException;
 
 public class RecursivePathsIterator implements PathIterator {
   private final FileSystem fileSystem;
   private final PathS baseDir;
-  private final ArrayDeque<PathS> dirStack;
-  private final ArrayDeque<PathS> fileStack;
+  private final Deque<PathS> dirStack;
+  private final Deque<PathS> pathStack;
   private PathS nextFile;
 
   public static PathIterator recursivePathsIterator(FileSystem fileSystem, PathS dir)
@@ -40,7 +40,7 @@ public class RecursivePathsIterator implements PathIterator {
     this.fileSystem = fileSystem;
     this.baseDir = baseDir;
     this.dirStack = new ArrayDeque<>();
-    this.fileStack = new ArrayDeque<>();
+    this.pathStack = new ArrayDeque<>();
     this.dirStack.push(root());
     this.nextFile = fetchNextFile();
   }
@@ -59,27 +59,22 @@ public class RecursivePathsIterator implements PathIterator {
   }
 
   private PathS fetchNextFile() throws IOException {
-    while (!fileStack.isEmpty() || !dirStack.isEmpty()) {
-      if (fileStack.isEmpty()) {
-        PathS dir = dirStack.remove();
-        for (PathS name : fileSystem.files(baseDir.append(dir))) {
-          fileStack.push(dir.append(name));
+    while (!pathStack.isEmpty() || !dirStack.isEmpty()) {
+      if (pathStack.isEmpty()) {
+        var path = dirStack.remove();
+        for (PathS name : fileSystem.files(baseDir.append(path))) {
+          pathStack.push(path.append(name));
         }
       } else {
-        PathS file = fileStack.remove();
-        PathState state = fileSystem.pathState(baseDir.append(file));
-        switch (state) {
-          case FILE:
-            return file;
-          case DIR:
-            dirStack.push(file);
-            break;
-          case NOTHING:
-            throw new IOException(format(
-                "FileSystem changed when iterating tree of directory %s. Cannot find %s.",
-                baseDir.q(), baseDir.append(file).q()));
-          default:
-            throw unexpectedCaseExc(state);
+        var path = pathStack.remove();
+        switch (fileSystem.pathState(baseDir.append(path))) {
+          case FILE -> {
+            return path;
+          }
+          case DIR -> dirStack.push(path);
+          case NOTHING -> throw new IOException(format(
+              "FileSystem changed when iterating tree of directory %s. Cannot find %s.",
+              baseDir.q(), baseDir.append(path).q()));
         }
       }
     }
