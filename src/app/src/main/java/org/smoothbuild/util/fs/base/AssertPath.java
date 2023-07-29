609cbe15eae73a94f6a4bf2700c75668253a4692
Marcin Mikosik <marcin.mikosik@gmail.com>
2023-07-29 17:18:30 +0200

    Refactor AssertPath

--- src/app/src/main/java/org/smoothbuild/util/fs/base/AssertPath.java
+++ src/app/src/main/java/org/smoothbuild/util/fs/base/AssertPath.java
@@ -1,7 +1,5 @@
 package org.smoothbuild.util.fs.base;
 
-import static org.smoothbuild.util.Throwables.unexpectedCaseExc;
-
 import java.io.IOException;
 
 public class AssertPath {
@@ -9,55 +7,34 @@ public class AssertPath {
   public static void assertPathIsDir(FileSystem fileSystem, PathS path) throws IOException {
     PathState state = fileSystem.pathState(path);
     switch (state) {
-      case DIR:
-        return;
-      case FILE:
-        throw new IOException("Dir " + path.q() + " doesn't exist. It is a file.");
-      case NOTHING:
-        throw new IOException("Dir " + path.q() + " doesn't exist.");
-      default:
-        throw newUnknownPathState(state);
+      case DIR -> {}
+      case FILE -> throw new IOException("Dir " + path.q() + " doesn't exist. It is a file.");
+      case NOTHING -> throw new IOException("Dir " + path.q() + " doesn't exist.");
     }
   }
 
   public static void assertPathIsFile(FileSystem fileSystem, PathS path) throws IOException {
     PathState state = fileSystem.pathState(path);
     switch (state) {
-      case FILE:
-        return;
-      case DIR:
-        throw new IOException("File " + path.q() + " doesn't exist. It is a dir.");
-      case NOTHING:
-        throw new IOException("File " + path.q() + " doesn't exist.");
-      default:
-        throw newUnknownPathState(state);
+      case FILE -> {}
+      case DIR -> throw new IOException("File " + path.q() + " doesn't exist. It is a dir.");
+      case NOTHING -> throw new IOException("File " + path.q() + " doesn't exist.");
     }
   }
 
   public static void assertPathExists(FileSystem fileSystem, PathS path) throws IOException {
     PathState state = fileSystem.pathState(path);
     switch (state) {
-      case FILE:
-        return;
-      case DIR:
-        return;
-      case NOTHING:
-        throw new IOException("Path " + path.q() + " doesn't exist.");
-      default:
-        throw newUnknownPathState(state);
+      case FILE, DIR -> {}
+      case NOTHING -> throw new IOException("Path " + path.q() + " doesn't exist.");
     }
   }
 
   public static void assertPathIsUnused(FileSystem fileSystem, PathS path) throws IOException {
     PathState state = fileSystem.pathState(path);
     switch (state) {
-      case FILE:
-      case DIR:
-        throw new IOException("Cannot use " + path + " path. It is already taken.");
-      case NOTHING:
-        return;
-      default:
-        throw unexpectedCaseExc(state);
+      case FILE, DIR -> throw new IOException("Cannot use " + path + " path. It is already taken.");
+      case NOTHING -> {}
     }
   }
 
