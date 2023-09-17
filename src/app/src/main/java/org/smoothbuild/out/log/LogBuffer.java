5566ae48dc25ee98f70942bdecad14d81b37cddf
Marcin Mikosik <marcin.mikosik@gmail.com>
2023-09-17 12:36:21 +0200

    Move methods from LogBuffer to Logger

--- src/app/src/main/java/org/smoothbuild/out/log/LogBuffer.java
+++ src/app/src/main/java/org/smoothbuild/out/log/LogBuffer.java
@@ -1,36 +1,12 @@
 package org.smoothbuild.out.log;
 
 import static org.smoothbuild.common.collect.Iterables.joinWithCommaToString;
-import static org.smoothbuild.out.log.Level.ERROR;
-import static org.smoothbuild.out.log.Level.FATAL;
-import static org.smoothbuild.out.log.Level.INFO;
-import static org.smoothbuild.out.log.Level.WARNING;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
 
 public class LogBuffer extends ArrayList<Log> implements Logger, Logs {
-  @Override
-  public void fatal(String message) {
-    log(new Log(FATAL, message));
-  }
-
-  @Override
-  public void error(String message) {
-    log(new Log(ERROR, message));
-  }
-
-  @Override
-  public void warning(String message) {
-    log(new Log(WARNING, message));
-  }
-
-  @Override
-  public void info(String message) {
-    log(new Log(INFO, message));
-  }
-
   @Override
   public void log(Log log) {
     add(log);
--- src/app/src/main/java/org/smoothbuild/out/log/Logger.java
+++ src/app/src/main/java/org/smoothbuild/out/log/Logger.java
@@ -1,17 +1,30 @@
 package org.smoothbuild.out.log;
 
+import static org.smoothbuild.out.log.Level.ERROR;
+import static org.smoothbuild.out.log.Level.FATAL;
+import static org.smoothbuild.out.log.Level.INFO;
+import static org.smoothbuild.out.log.Level.WARNING;
+
 public interface Logger {
   public default void logAll(Iterable<? extends Log> logs) {
     logs.forEach(this::log);
   }
 
+  public default void fatal(String message) {
+    log(new Log(FATAL, message));
+  }
+
+  public default void error(String message) {
+    log(new Log(ERROR, message));
+  }
+
+  public default void warning(String message) {
+    log(new Log(WARNING, message));
+  }
+
+  public default void info(String message) {
+    log(new Log(INFO, message));
+  }
+
   public void log(Log log);
-
-  public void fatal(String message);
-
-  public void error(String message);
-
-  public void warning(String message);
-
-  public void info(String message);
 }
