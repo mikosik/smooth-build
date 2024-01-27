40f539dae3c504e4e1eaba3ff2e4cf481c5c3f65
Marcin Mikosik <marcin.mikosik@gmail.com>
2024-01-27 09:11:07 +0100

    Add Function0.memoize()

---
+++ src/app/src/main/java/org/smoothbuild/common/function/MemoizingFunction0.java
@@ -0,0 +1,31 @@
+package org.smoothbuild.common.function;
+
+import static java.util.Objects.requireNonNull;
+
+public class MemoizingFunction0<R, T extends Throwable> implements Function0<R, T> {
+  private final Function0<R, T> function0;
+  private volatile boolean computed = false;
+  private R result;
+
+  MemoizingFunction0(Function0<R, T> function0) {
+    this.function0 = requireNonNull(function0);
+  }
+
+  @Override
+  public R apply() throws T {
+    // Changes to non-volatile field `result` are safely shared between threads by piggybacking on
+    // write/reads of `computed` field. This is possible because every write to a volatile field
+    // happens-before every subsequent read of that same field.
+    if (!computed) {
+      synchronized (this) {
+        if (!computed) {
+          R localResult = function0.apply();
+          this.result = localResult;
+          this.computed = true;
+          return localResult;
+        }
+      }
+    }
+    return result;
+  }
+}
---
+++ src/app/src/test/java/org/smoothbuild/common/function/Function0Test.java
@@ -0,0 +1,77 @@
+package org.smoothbuild.common.function;
+
+import static com.google.common.truth.Truth.assertThat;
+import static java.util.concurrent.Executors.newVirtualThreadPerTaskExecutor;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
+import static org.smoothbuild.common.collect.List.list;
+import static org.smoothbuild.common.function.Function0.memoize;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Future;
+import java.util.concurrent.atomic.AtomicInteger;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.smoothbuild.common.collect.List;
+
+public class Function0Test {
+  @Nested
+  class _memoize {
+    @Test
+    void calls_function_lazily() {
+      var function0 = mock(Function0.class);
+      memoize(function0);
+      verifyNoInteractions(function0);
+    }
+
+    @Test
+    void apply_returns_value_from_wrapped_function() {
+      var memoizer = memoize(() -> "abc");
+      assertThat(memoizer.apply()).isEqualTo("abc");
+    }
+
+    @Test
+    void second_call_to_apply_does_not_call_wrapped_function() throws Throwable {
+      var function0 = mock(Function0.class);
+      var memoized = memoize(function0);
+      memoized.apply();
+      memoized.apply();
+
+      verify(function0, times(1)).apply();
+    }
+
+    @Test
+    void second_call_to_apply_returns_memoized_value() {
+      var memoizer = memoize(() -> "abc");
+      memoizer.apply();
+      assertThat(memoizer.apply()).isEqualTo("abc");
+    }
+
+    @Test
+    void multi_threaded_test() throws Exception {
+      var atomicInteger = new AtomicInteger(7);
+      var memoizer = memoize(atomicInteger::getAndIncrement);
+
+      var count = 1000;
+      var futures = invokeMemoizingFunctionFromMultipleThreads(count, memoizer);
+      assertThat(futures.map(Future::get)).isEqualTo(list(count, () -> 7));
+    }
+
+    private static List<Future<Integer>> invokeMemoizingFunctionFromMultipleThreads(
+        int count, Function0<Integer, RuntimeException> memoizingFunction) {
+      var countDownLatch = new CountDownLatch(1);
+      try (var executorService = newVirtualThreadPerTaskExecutor()) {
+        var futures = list(count, () -> (Callable<Integer>) () -> {
+              countDownLatch.await();
+              return memoizingFunction.apply();
+            })
+            .map(executorService::submit);
+        countDownLatch.countDown();
+        return futures;
+      }
+    }
+  }
+}
