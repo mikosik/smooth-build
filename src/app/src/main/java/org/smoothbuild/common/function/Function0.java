1eeba1af5eddcdda614c60186bc0c2e3f2f5b1f9
Marcin Mikosik <marcin.mikosik@gmail.com>
2024-01-27 09:11:07 +0100

    Add Function0.memoize()

--- src/app/src/main/java/org/smoothbuild/common/function/Function0.java
+++ src/app/src/main/java/org/smoothbuild/common/function/Function0.java
@@ -3,4 +3,8 @@ package org.smoothbuild.common.function;
 @FunctionalInterface
 public interface Function0<R, T extends Throwable> {
   public R apply() throws T;
+
+  public static <R, T extends Throwable> Function0<R, T> memoize(Function0<R, T> function0) {
+    return new MemoizingFunction0<>(function0);
+  }
 }
