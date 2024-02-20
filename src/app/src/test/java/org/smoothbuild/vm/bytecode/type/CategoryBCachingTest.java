b81c9af27a264f02b4e2ffdcc9f0146754dab71d
Marcin Mikosik <marcin.mikosik@gmail.com>
2024-02-20 14:59:08 +0100

    Improve naming in CategoryBCachingTest

--- src/app/src/test/java/org/smoothbuild/vm/bytecode/type/CategoryBCachingTest.java
+++ src/app/src/test/java/org/smoothbuild/vm/bytecode/type/CategoryBCachingTest.java
@@ -24,8 +24,8 @@ public class CategoryBCachingTest extends TestContext {
   public void read_type_is_cached(Function1<CategoryDb, CategoryB, BytecodeException> factory)
       throws Exception {
     var hash = factory.apply(categoryDb()).hash();
-    var catDb = categoryDbOther();
-    assertThat(catDb.get(hash)).isSameInstanceAs(catDb.get(hash));
+    var categoryDb = categoryDbOther();
+    assertThat(categoryDb.get(hash)).isSameInstanceAs(categoryDb.get(hash));
   }
 
   private static java.util.List<Function1<CategoryDb, CategoryB, BytecodeException>> factories() {
@@ -36,28 +36,28 @@ public class CategoryBCachingTest extends TestContext {
         CategoryDb::int_,
         CategoryDb::string,
         CategoryBCachingTest::tupleT,
-        catDb -> catDb.call(catDb.int_()),
-        catDb -> catDb.combine(catDb.tuple()),
-        catDb -> catDb.combine(catDb.tuple(catDb.int_())),
-        catDb -> catDb.lambda(catDb.funcT(list(), catDb.int_())),
-        catDb -> catDb.ifFunc(catDb.int_()),
-        catDb -> catDb.mapFunc(catDb.int_(), catDb.string()),
-        catDb -> catDb.order(catDb.array(catDb.int_())),
-        catDb -> catDb.pick(catDb.int_()),
-        catDb -> catDb.var(catDb.int_()),
-        catDb -> catDb.select(catDb.int_()),
-        catDb -> catDb.array(catDb.blob()),
-        catDb -> catDb.array(catDb.bool()),
-        catDb -> catDb.array(catDb.int_()),
-        catDb -> catDb.array(catDb.string()),
-        catDb -> catDb.array(tupleT(catDb)),
-        catDb -> catDb.array(funcT(catDb)),
-        catDb -> catDb.array(catDb.array(catDb.blob())),
-        catDb -> catDb.array(catDb.array(catDb.bool())),
-        catDb -> catDb.array(catDb.array(catDb.int_())),
-        catDb -> catDb.array(catDb.array(catDb.string())),
-        catDb -> catDb.array(catDb.array(tupleT(catDb))),
-        catDb -> catDb.array(catDb.array(funcT(catDb))));
+        categoryDb -> categoryDb.call(categoryDb.int_()),
+        categoryDb -> categoryDb.combine(categoryDb.tuple()),
+        categoryDb -> categoryDb.combine(categoryDb.tuple(categoryDb.int_())),
+        categoryDb -> categoryDb.lambda(categoryDb.funcT(list(), categoryDb.int_())),
+        categoryDb -> categoryDb.ifFunc(categoryDb.int_()),
+        categoryDb -> categoryDb.mapFunc(categoryDb.int_(), categoryDb.string()),
+        categoryDb -> categoryDb.order(categoryDb.array(categoryDb.int_())),
+        categoryDb -> categoryDb.pick(categoryDb.int_()),
+        categoryDb -> categoryDb.var(categoryDb.int_()),
+        categoryDb -> categoryDb.select(categoryDb.int_()),
+        categoryDb -> categoryDb.array(categoryDb.blob()),
+        categoryDb -> categoryDb.array(categoryDb.bool()),
+        categoryDb -> categoryDb.array(categoryDb.int_()),
+        categoryDb -> categoryDb.array(categoryDb.string()),
+        categoryDb -> categoryDb.array(tupleT(categoryDb)),
+        categoryDb -> categoryDb.array(funcT(categoryDb)),
+        categoryDb -> categoryDb.array(categoryDb.array(categoryDb.blob())),
+        categoryDb -> categoryDb.array(categoryDb.array(categoryDb.bool())),
+        categoryDb -> categoryDb.array(categoryDb.array(categoryDb.int_())),
+        categoryDb -> categoryDb.array(categoryDb.array(categoryDb.string())),
+        categoryDb -> categoryDb.array(categoryDb.array(tupleT(categoryDb))),
+        categoryDb -> categoryDb.array(categoryDb.array(funcT(categoryDb))));
   }
 
   private static TupleTB tupleT(CategoryDb categoryDb) throws BytecodeException {
