6e116d1356bf900f7768a3e95ac0aaf602a6316b
Marcin Mikosik <marcin.mikosik@gmail.com>
2024-02-21 15:10:18 +0100

    Improve javadocs in .bytecode.load package

--- src/app/src/main/java/org/smoothbuild/vm/bytecode/load/MethodSpec.java
+++ src/app/src/main/java/org/smoothbuild/vm/bytecode/load/MethodSpec.java
@@ -2,4 +2,8 @@ package org.smoothbuild.vm.bytecode.load;
 
 import org.smoothbuild.vm.bytecode.expr.value.BlobB;
 
+/**
+ * Specifies java method by providing method name, binary name of enclosing class and jar content as
+ * BlobB.
+ */
 public record MethodSpec(BlobB jar, String classBinaryName, String methodName) {}
