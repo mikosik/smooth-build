be799381fd04ed69793a82f187debb538c25f035
Marcin Mikosik <marcin.mikosik@gmail.com>
2024-02-29 09:29:49 +0100

    Move MemoryStepReporter to proper package

--- src/app/src/testFixtures/java/org/smoothbuild/testing/accept/MemoryReporter.java
+++ src/app/src/testFixtures/java/org/smoothbuild/testing/accept/MemoryReporter.java
@@ -1,8 +1,8 @@
 package org.smoothbuild.testing.accept;
 
-import org.smooth.build.common.step.MemoryStepReporter;
 import org.smoothbuild.common.collect.List;
 import org.smoothbuild.common.log.Log;
+import org.smoothbuild.common.testing.MemoryStepReporter;
 import org.smoothbuild.out.report.Reporter;
 
 public class MemoryReporter extends MemoryStepReporter implements Reporter {
