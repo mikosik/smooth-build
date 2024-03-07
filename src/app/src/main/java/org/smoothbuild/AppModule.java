0c08f426afb4a39d1c6a925f465e0b79c7ae8dd6
Marcin Mikosik <marcin.mikosik@gmail.com>
2024-03-07 09:39:36 +0100

    Merge VirtualMachineConfigurationModule + SandboxHashModule -> AppModule

--- src/app/src/main/java/org/smoothbuild/AppModule.java
+++ src/app/src/main/java/org/smoothbuild/AppModule.java
@@ -6,14 +6,26 @@ import static org.smoothbuild.layout.SmoothSpace.PROJECT;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
+import jakarta.inject.Singleton;
+import java.io.IOException;
+import org.smoothbuild.common.base.Hash;
 import org.smoothbuild.common.filesystem.base.FileSystem;
 import org.smoothbuild.common.filesystem.base.SubFileSystem;
 import org.smoothbuild.layout.ForSpace;
+import org.smoothbuild.layout.InstallationHashes;
 import org.smoothbuild.virtualmachine.wire.BytecodeDb;
 import org.smoothbuild.virtualmachine.wire.ComputationDb;
 import org.smoothbuild.virtualmachine.wire.Project;
+import org.smoothbuild.virtualmachine.wire.Sandbox;
 
-public class VirtualMachineConfigurationModule extends AbstractModule {
+public class AppModule extends AbstractModule {
+  @Provides
+  @Singleton
+  @Sandbox
+  public Hash provideSandboxHash(InstallationHashes installationHashes) throws IOException {
+    return installationHashes.sandboxNode().hash();
+  }
+
   @Provides
   @ComputationDb
   public FileSystem provideComputationCacheFileSystem(@ForSpace(PROJECT) FileSystem fileSystem) {
