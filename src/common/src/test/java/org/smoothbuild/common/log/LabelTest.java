b4023cf52c6de3a4dc6ae93155ef5271338dac99
Marcin Mikosik <marcin.mikosik@gmail.com>
2024-03-09 16:16:00 +0100

    Add Label.startsWith()

--- src/common/src/test/java/org/smoothbuild/common/log/LabelTest.java
+++ src/common/src/test/java/org/smoothbuild/common/log/LabelTest.java
@@ -1,10 +1,15 @@
 package org.smoothbuild.common.log;
 
 import static com.google.common.truth.Truth.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 import static org.smoothbuild.common.log.Label.label;
 
+import java.util.List;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class LabelTest {
   @Nested
@@ -26,5 +31,27 @@ public class LabelTest {
       var label = label("name").append(label("second"));
       assertThat(label.toString()).isEqualTo("::name::second");
     }
+  }
+
+  @ParameterizedTest
+  @MethodSource
+  void startsWith(Label label, Label prefix, boolean expected) {
+    assertThat(label.startsWith(prefix)).isEqualTo(expected);
+  }
+
+  public static List<Arguments> startsWith() {
+    return List.of(
+        arguments(label(), label(), true),
+        arguments(label(), label("a"), false),
+        arguments(label("a"), label(), true),
+        arguments(label("a"), label("a"), true),
+        arguments(label("a"), label("b"), false),
+        arguments(label("a"), label("a", "b"), false),
+        arguments(label("a", "b"), label(), true),
+        arguments(label("a", "b"), label("a"), true),
+        arguments(label("a", "b"), label("b"), false),
+        arguments(label("a", "b"), label("a", "b"), true),
+        arguments(label("a", "b"), label("a", "c"), false),
+        arguments(label("a", "b"), label("a", "b", "c"), false));
   }
 }
