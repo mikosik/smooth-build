3b59c945cd4f712c360bd79f9206c0ad0e56e9ee
Marcin Mikosik <marcin.mikosik@gmail.com>
2024-03-11 16:09:39 +0100

    Rename FilteringReporter -> TaskFilteringReporter

--- src/app/src/test/java/org/smoothbuild/app/report/TaskFilteringReporterTest.java
+++ src/app/src/test/java/org/smoothbuild/app/report/TaskFilteringReporterTest.java
@@ -16,7 +16,7 @@ import org.smoothbuild.app.run.eval.report.TaskMatcher;
 import org.smoothbuild.common.log.Label;
 import org.smoothbuild.common.log.Reporter;
 
-public class FilteringReporterTest {
+public class TaskFilteringReporterTest {
   @Test
   public void when_task_matcher_matches_then_report_is_forwarded() {
     testVisibility(ALL, true, EVALUATE.append(label("name")));
@@ -36,7 +36,7 @@ public class FilteringReporterTest {
     var details = "details";
     var logs = list(ERROR_LOG);
     var wrappedReporter = mock(Reporter.class);
-    var reporter = new FilteringReporter(wrappedReporter, taskMatcher);
+    var reporter = new TaskFilteringReporter(wrappedReporter, taskMatcher);
 
     reporter.report(label, details, DISK, logs);
 
