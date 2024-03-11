073ad1cd00b74c9443a372a001fef9c64b358d3d
Marcin Mikosik <marcin.mikosik@gmail.com>
2024-03-11 16:51:37 +0100

    Add LogFilteringReporter

---
+++ src/common/src/main/java/org/smoothbuild/common/log/LogFilteringReporter.java
@@ -0,0 +1,18 @@
+package org.smoothbuild.common.log;
+
+import org.smoothbuild.common.collect.List;
+
+public class LogFilteringReporter {
+  private final Reporter reporter;
+  private final Level level;
+
+  public LogFilteringReporter(Reporter reporter, Level level) {
+    this.reporter = reporter;
+    this.level = level;
+  }
+
+  public void report(Label label, String details, ResultSource source, List<Log> logs) {
+    var filtered = logs.filter(l -> l.level().hasPriorityAtLeast(level));
+    reporter.report(label, details, source, filtered);
+  }
+}
