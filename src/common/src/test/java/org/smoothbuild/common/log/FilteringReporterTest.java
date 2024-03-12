3b6769ac2d781cbd503fbd7f083fabe4d53e26f6
Marcin Mikosik <marcin.mikosik@gmail.com>
2024-03-12 09:55:11 +0100

    Move TaskFilteringReporter to common sub-project and rename to FilteringReporter

---
+++ src/common/src/test/java/org/smoothbuild/common/log/FilteringReporterTest.java
@@ -0,0 +1,35 @@
+package org.smoothbuild.common.log;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.smoothbuild.common.collect.List.list;
+import static org.smoothbuild.common.log.Label.label;
+import static org.smoothbuild.common.log.ResultSource.DISK;
+import static org.smoothbuild.common.testing.TestingLog.ERROR_LOG;
+
+import org.junit.jupiter.api.Test;
+
+public class FilteringReporterTest {
+  @Test
+  public void when_task_matcher_matches_then_report_is_forwarded() {
+    testVisibility(true);
+  }
+
+  @Test
+  public void when_task_matcher_not_matches_then_report_is_suppressed() {
+    testVisibility(false);
+  }
+
+  private void testVisibility(boolean matches) {
+    var reportLabel = label("name");
+    var details = "details";
+    var logs1 = list(ERROR_LOG);
+    var wrappedReporter = mock(Reporter.class);
+    var reporter = new FilteringReporter(wrappedReporter, (label, logs) -> matches);
+
+    reporter.report(reportLabel, details, DISK, logs1);
+
+    verify(wrappedReporter, times(matches ? 1 : 0)).report(reportLabel, details, DISK, logs1);
+  }
+}
