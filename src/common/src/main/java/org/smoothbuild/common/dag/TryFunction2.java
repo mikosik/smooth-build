7b90d9e6d85813ebc15bd7b5539eb45e571d8c17
Marcin Mikosik <marcin.mikosik@gmail.com>
2024-03-21 12:09:12 +0100

    Add .label() to TryFunctionX

--- src/common/src/main/java/org/smoothbuild/common/dag/TryFunction2.java
+++ src/common/src/main/java/org/smoothbuild/common/dag/TryFunction2.java
@@ -1,8 +1,13 @@
 package org.smoothbuild.common.dag;
 
+import org.smoothbuild.common.log.base.Label;
 import org.smoothbuild.common.log.base.Try;
 
 @FunctionalInterface
 public interface TryFunction2<A, B, R> {
   public Try<R> apply(A a, B b);
+
+  public default Label label() {
+    return Label.label();
+  }
 }
