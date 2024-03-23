88b7b5ba791a42f849f47ba7027a698f623e88ac
Marcin Mikosik <marcin.mikosik@gmail.com>
2024-03-23 14:40:09 +0100

    Get rid of @WithId

--- src/app/src/main/java/org/smoothbuild/app/AppWiring.java
+++ src/app/src/main/java/org/smoothbuild/app/AppWiring.java
@@ -9,10 +9,11 @@ import com.google.inject.Provides;
 import jakarta.inject.Singleton;
 import java.io.IOException;
 import org.smoothbuild.app.layout.InstallationHashes;
-import org.smoothbuild.app.layout.WithId;
 import org.smoothbuild.common.base.Hash;
 import org.smoothbuild.common.bucket.base.Bucket;
+import org.smoothbuild.common.bucket.base.BucketId;
 import org.smoothbuild.common.bucket.base.SubBucket;
+import org.smoothbuild.common.collect.Map;
 import org.smoothbuild.virtualmachine.wire.BytecodeDb;
 import org.smoothbuild.virtualmachine.wire.ComputationDb;
 import org.smoothbuild.virtualmachine.wire.Project;
@@ -28,19 +29,19 @@ public class AppWiring extends AbstractModule {
 
   @Provides
   @ComputationDb
-  public Bucket provideComputationCacheBucket(@WithId(PROJECT) Bucket bucket) {
+  public Bucket provideComputationCacheBucket(@Project Bucket bucket) {
     return new SubBucket(bucket, COMPUTATION_CACHE_PATH);
   }
 
   @Provides
   @BytecodeDb
-  public Bucket provideBytecodeDbBucket(@WithId(PROJECT) Bucket bucket) {
+  public Bucket provideBytecodeDbBucket(@Project Bucket bucket) {
     return new SubBucket(bucket, HASHED_DB_PATH);
   }
 
   @Provides
   @Project
-  public Bucket provideProjectBucket(@WithId(PROJECT) Bucket bucket) {
-    return bucket;
+  public Bucket provideProjectBucket(Map<BucketId, Bucket> map) {
+    return map.get(PROJECT);
   }
 }
