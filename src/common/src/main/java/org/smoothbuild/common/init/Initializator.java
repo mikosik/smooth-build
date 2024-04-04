77ebb0c0671892b2c6e164718577b780e173acf0
Marcin Mikosik <marcin.mikosik@gmail.com>
2024-04-04 10:54:43 +0200

    Fix Initializator to return logs from all initializables, not only from the last one

--- src/common/src/main/java/org/smoothbuild/common/init/Initializator.java
+++ src/common/src/main/java/org/smoothbuild/common/init/Initializator.java
@@ -1,10 +1,12 @@
 package org.smoothbuild.common.init;
 
+import static org.smoothbuild.common.log.base.Try.failure;
 import static org.smoothbuild.common.log.base.Try.success;
 
 import jakarta.inject.Inject;
 import java.util.Set;
 import org.smoothbuild.common.dag.TryFunction0;
+import org.smoothbuild.common.log.base.Logger;
 import org.smoothbuild.common.log.base.Try;
 
 public class Initializator implements TryFunction0<Void> {
@@ -17,12 +19,14 @@ public class Initializator implements TryFunction0<Void> {
 
   @Override
   public Try<Void> apply() {
+    var logger = new Logger();
     for (Initializable initializable : initializables) {
       var result = initializable.initialize();
+      logger.logAll(result.logs());
       if (result.toMaybe().isNone()) {
-        return result;
+        return failure(logger);
       }
     }
-    return success(null);
+    return success(null, logger);
   }
 }
--- src/common/src/test/java/org/smoothbuild/common/dag/InitializatorTest.java
+++ src/common/src/test/java/org/smoothbuild/common/dag/InitializatorTest.java
@@ -1,9 +1,9 @@
 package org.smoothbuild.common.dag;
 
 import static com.google.common.truth.Truth.assertThat;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.smoothbuild.common.collect.Set.set;
 import static org.smoothbuild.common.log.base.Log.error;
+import static org.smoothbuild.common.log.base.Log.info;
 import static org.smoothbuild.common.log.base.Try.failure;
 import static org.smoothbuild.common.log.base.Try.success;
 
@@ -17,8 +17,7 @@ public class InitializatorTest {
   @Test
   void fails_when_initializable_fails() {
     Try<Void> failure = failure(error("message"));
-    var initializable = mock(Initializable.class);
-    when(initializable.initialize()).thenReturn(failure);
+    Initializable initializable = () -> failure;
 
     var result = new Initializator(Set.of(initializable)).apply();
 
@@ -27,12 +26,46 @@ public class InitializatorTest {
 
   @Test
   void succeeds_when_initializable_succeeds() {
-    Try<Void> success = success(null);
-    var initializable = mock(Initializable.class);
-    when(initializable.initialize()).thenReturn(success);
+    var info = info("message");
+    Try<Void> success = success(null, info);
+    Initializable initializable = () -> success;
 
     var result = new Initializator(Set.of(initializable)).apply();
 
-    assertThat(result).isEqualTo(success(null));
+    assertThat(result).isEqualTo(success(null, info));
+  }
+
+  @Test
+  void on_success_returned_result_contains_logs_from_all_initializables() {
+    var info1 = info("message1");
+    Try<Void> success1 = success(null, info1);
+    Initializable initializable1 = () -> success1;
+    var info2 = info("message2");
+    Try<Void> success2 = success(null, info2);
+    Initializable initializable2 = () -> success2;
+
+    var result = new Initializator(set(initializable1, initializable2)).apply();
+
+    assertThat(result).isEqualTo(success(null, info1, info2));
+  }
+
+  @Test
+  void on_failure_returned_result_contains_logs_from_failed_initializable_and_all_preceding() {
+    var info1 = info("message1");
+    Try<Void> success1 = success(null, info1);
+    Initializable initializable1 = () -> success1;
+
+    var error = error("message");
+    Try<Void> failure = failure(error);
+    Initializable failedInitializable = () -> failure;
+
+    var info2 = info("message2");
+    Try<Void> success2 = success(null, info2);
+    Initializable initializable2 = () -> success2;
+
+    var result =
+        new Initializator(set(initializable1, failedInitializable, initializable2)).apply();
+
+    assertThat(result).isEqualTo(failure(info1, error));
   }
 }
