82a6e98a560cda086a3bd875fd9f15af56725cc8
Marcin Mikosik <marcin.mikosik@gmail.com>
2024-04-12 12:37:00 +0200

    Refactor MemoryReporter to store Reports instead of only Logs

--- src/common/src/testFixtures/java/org/smoothbuild/common/testing/MemoryReporter.java
+++ src/common/src/testFixtures/java/org/smoothbuild/common/testing/MemoryReporter.java
@@ -1,24 +1,32 @@
 package org.smoothbuild.common.testing;
 
+import static org.smoothbuild.common.collect.List.listOfAll;
+import static org.smoothbuild.common.log.base.Log.containsAnyFailure;
+
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.stream.Stream;
 import org.smoothbuild.common.collect.List;
 import org.smoothbuild.common.log.base.Log;
-import org.smoothbuild.common.log.base.Logger;
 import org.smoothbuild.common.log.report.Report;
 import org.smoothbuild.common.log.report.Reporter;
 
 public class MemoryReporter implements Reporter {
-  private final Logger buffer = new Logger();
+  private final CopyOnWriteArrayList<Report> reports = new CopyOnWriteArrayList<>();
 
   @Override
   public void report(Report report) {
-    buffer.logAll(report.logs());
+    reports.add(report);
   }
 
   public boolean containsFailure() {
-    return buffer.containsFailure();
+    return reports.stream().anyMatch(r -> containsAnyFailure(r.logs()));
   }
 
   public List<Log> logs() {
-    return buffer.toList();
+    return listOfAll(streamOfAllLogs().toList());
+  }
+
+  private Stream<Log> streamOfAllLogs() {
+    return reports.stream().flatMap(r -> r.logs().stream());
   }
 }
