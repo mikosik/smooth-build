33a98cdc8ed113fce48daf1a4962299093f2aa7b
Marcin Mikosik <marcin.mikosik@gmail.com>
2024-04-18 12:53:56 +0200

    Simplify SmoothBucketWiring

--- src/cli/src/main/java/org/smoothbuild/app/layout/SmoothBucketWiring.java
+++ src/cli/src/main/java/org/smoothbuild/app/layout/SmoothBucketWiring.java
@@ -1,23 +1,19 @@
 package org.smoothbuild.app.layout;
 
-import static org.smoothbuild.app.layout.BucketIds.INSTALL;
-import static org.smoothbuild.app.layout.BucketIds.LIBRARY;
-import static org.smoothbuild.app.layout.BucketIds.PROJECT;
-
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
 import com.google.inject.Singleton;
-import java.util.HashMap;
 import org.smoothbuild.common.bucket.base.Bucket;
 import org.smoothbuild.common.bucket.base.BucketId;
 import org.smoothbuild.common.bucket.wiring.BucketFactory;
 import org.smoothbuild.common.collect.Map;
+import org.smoothbuild.common.collect.Set;
 
 public class SmoothBucketWiring extends AbstractModule {
-  private final boolean withProjectBucket;
+  private final Set<BucketId> bucketIds;
 
-  public SmoothBucketWiring(boolean withProjectBucket) {
-    this.withProjectBucket = withProjectBucket;
+  public SmoothBucketWiring(Set<BucketId> bucketIds) {
+    this.bucketIds = bucketIds;
   }
 
   @Override
@@ -26,12 +22,6 @@ public class SmoothBucketWiring extends AbstractModule {
   @Provides
   @Singleton
   public Map<BucketId, Bucket> provideBucketIdToBucketMap(BucketFactory bucketFactory) {
-    java.util.Map<BucketId, Bucket> map = new HashMap<>();
-    if (withProjectBucket) {
-      map.put(PROJECT, bucketFactory.create(PROJECT));
-    }
-    map.put(LIBRARY, bucketFactory.create(LIBRARY));
-    map.put(INSTALL, bucketFactory.create(INSTALL));
-    return Map.mapOfAll(map);
+    return bucketIds.toMap(bucketFactory::create);
   }
 }
