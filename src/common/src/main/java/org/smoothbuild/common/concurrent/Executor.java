a17cad9157ad490fbf16038b5e38beb21441c806
Marcin Mikosik <marcin.mikosik@gmail.com>
2024-04-22 11:15:52 +0200

    Add common.concurrent.Executor

---
+++ src/common/src/main/java/org/smoothbuild/common/concurrent/Executor.java
@@ -0,0 +1,91 @@
+package org.smoothbuild.common.concurrent;
+
+import static java.lang.Thread.startVirtualThread;
+import static java.util.Objects.requireNonNull;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * Executes submitted Runnables in virtual threads (one new thread per Runnable).
+ * Number of threads running in parallel is limited to maxThreads passed to constructor.
+ */
+public class Executor {
+  private final int maxThreads;
+  private final Object lock;
+  private int runningThreadsCount;
+  private final Deque<Runnable> queue;
+
+  public Executor(int maxThreads) {
+    this.maxThreads = maxThreads;
+    this.lock = new Object();
+    this.runningThreadsCount = 0;
+    this.queue = new LinkedList<>();
+  }
+
+  public void submit(Runnable runnable) {
+    requireNonNull(runnable);
+    if (takePermitOrAddToQueue(runnable)) {
+      startThread(runnable);
+    }
+  }
+
+  private void startThread(Runnable runnable) {
+    startVirtualThread(runnableWrapper(runnable));
+  }
+
+  private Runnable runnableWrapper(Runnable runnable) {
+    return () -> {
+      try {
+        runnable.run();
+      } finally {
+        onThreadCompleted();
+      }
+    };
+  }
+
+  private void onThreadCompleted() {
+    var next = pollNextRunnable();
+    if (next != null) {
+      startThread(next);
+    }
+  }
+
+  public void waitUntilIdle() throws InterruptedException {
+    synchronized (lock) {
+      while (!isIdle()) {
+        lock.wait();
+      }
+    }
+  }
+
+  private boolean takePermitOrAddToQueue(Runnable runnable) {
+    synchronized (lock) {
+      if (runningThreadsCount < maxThreads) {
+        runningThreadsCount++;
+        return true;
+      } else {
+        queue.addLast(runnable);
+        return false;
+      }
+    }
+  }
+
+  private Runnable pollNextRunnable() {
+    synchronized (lock) {
+      if (queue.isEmpty()) {
+        runningThreadsCount--;
+        if (isIdle()) {
+          lock.notifyAll();
+        }
+        return null;
+      } else {
+        return queue.removeFirst();
+      }
+    }
+  }
+
+  private boolean isIdle() {
+    return runningThreadsCount == 0 && queue.isEmpty();
+  }
+}
