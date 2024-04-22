6f092355c974485dbd40d0bc172e56f175d287b5
Marcin Mikosik <marcin.mikosik@gmail.com>
2024-04-22 11:15:35 +0200

    Make Promises.runWhenAllAvailable() API more accepting

--- src/common/src/main/java/org/smoothbuild/common/concurrent/Promises.java
+++ src/common/src/main/java/org/smoothbuild/common/concurrent/Promises.java
@@ -4,7 +4,7 @@ import org.smoothbuild.common.collect.List;
 
 public class Promises {
   public static <T> void runWhenAllAvailable(
-      List<? extends Promise<T>> promises, Runnable runnable) {
+      List<? extends Promise<? extends T>> promises, Runnable runnable) {
     ThresholdRunnable latch = new ThresholdRunnable(promises.size(), runnable);
     promises.forEach(child -> child.addConsumer(v -> latch.run()));
   }
