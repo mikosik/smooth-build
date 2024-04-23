c2c2001f64ff9702e1bbd3e13bacdcbcd81fc592
Marcin Mikosik <marcin.mikosik@gmail.com>
2024-04-23 14:42:44 +0200

    Add static factory method to Output

--- src/common/src/main/java/org/smoothbuild/common/schedule/Output.java
+++ src/common/src/main/java/org/smoothbuild/common/schedule/Output.java
@@ -2,4 +2,8 @@ package org.smoothbuild.common.schedule;
 
 import org.smoothbuild.common.log.report.Report;
 
-public record Output<V>(V result, Report report) {}
+public record Output<V>(V result, Report report) {
+  public static <V> Output<V> output(V result, Report report) {
+    return new Output<>(result, report);
+  }
+}
