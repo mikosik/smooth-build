2d4a6ed185b34fc3d0559640c1927d62d64255bb
Marcin Mikosik <marcin.mikosik@gmail.com>
2024-04-24 13:12:46 +0200

    Reuse TestingThread.sleepMillis()

--- src/standard-library/src/test/java/org/smoothbuild/stdlib/compress/ZipFuncTest.java
+++ src/standard-library/src/test/java/org/smoothbuild/stdlib/compress/ZipFuncTest.java
@@ -1,6 +1,7 @@
 package org.smoothbuild.stdlib.compress;
 
 import static com.google.common.truth.Truth.assertThat;
+import static org.smoothbuild.common.testing.TestingThread.sleepMillis;
 
 import org.junit.jupiter.api.Test;
 import org.smoothbuild.virtualmachine.testing.TestingVirtualMachine;
@@ -14,13 +15,5 @@ public class ZipFuncTest extends TestingVirtualMachine {
     sleepMillis(2000);
     var zip2 = ZipFunc.func(nativeApi(), fileArray);
     assertThat(zip1.hash()).isEqualTo(zip2.hash());
-  }
-
-  private static void sleepMillis(int millis) {
-    try {
-      Thread.sleep(millis);
-    } catch (InterruptedException e) {
-      throw new RuntimeException(e);
-    }
   }
 }
--- src/standard-library/src/test/java/org/smoothbuild/stdlib/java/JarFuncTest.java
+++ src/standard-library/src/test/java/org/smoothbuild/stdlib/java/JarFuncTest.java
@@ -1,6 +1,7 @@
 package org.smoothbuild.stdlib.java;
 
 import static com.google.common.truth.Truth.assertThat;
+import static org.smoothbuild.common.testing.TestingThread.sleepMillis;
 
 import org.junit.jupiter.api.Test;
 import org.smoothbuild.virtualmachine.testing.TestingVirtualMachine;
@@ -14,13 +15,5 @@ public class JarFuncTest extends TestingVirtualMachine {
     sleepMillis(2000);
     var zip2 = JarFunc.func(nativeApi(), args);
     assertThat(zip1.hash()).isEqualTo(zip2.hash());
-  }
-
-  private static void sleepMillis(int millis) {
-    try {
-      Thread.sleep(millis);
-    } catch (InterruptedException e) {
-      throw new RuntimeException(e);
-    }
   }
 }
