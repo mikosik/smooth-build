5016cfdeca6c2653345be40f5c79ef4a37dd6b01
Marcin Mikosik <marcin.mikosik@gmail.com>
2024-04-27 10:27:30 +0200

    Remove no longer used Promise.chain()

--- src/common/src/main/java/org/smoothbuild/common/concurrent/Promise.java
+++ src/common/src/main/java/org/smoothbuild/common/concurrent/Promise.java
@@ -1,7 +1,6 @@
 package org.smoothbuild.common.concurrent;
 
 import java.util.function.Consumer;
-import java.util.function.Function;
 import org.smoothbuild.common.collect.Maybe;
 
 public interface Promise<T> {
@@ -12,6 +11,4 @@ public interface Promise<T> {
   public Maybe<T> toMaybe();
 
   public void addConsumer(Consumer<T> consumer);
-
-  public <U> Promise<U> chain(Function<T, U> func);
 }
