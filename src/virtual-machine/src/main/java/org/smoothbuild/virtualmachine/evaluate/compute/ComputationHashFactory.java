0b124ecf2310af4ca3058586e84b9c33d3d20a0c
Marcin Mikosik <marcin.mikosik@gmail.com>
2024-04-28 20:49:56 +0200

    Add ComputationHashFactory

---
+++ src/virtual-machine/src/main/java/org/smoothbuild/virtualmachine/evaluate/compute/ComputationHashFactory.java
@@ -0,0 +1,23 @@
+package org.smoothbuild.virtualmachine.evaluate.compute;
+
+import static java.util.Arrays.asList;
+import static org.smoothbuild.virtualmachine.evaluate.step.StepHashes.stepHash;
+
+import jakarta.inject.Inject;
+import org.smoothbuild.common.base.Hash;
+import org.smoothbuild.virtualmachine.bytecode.expr.base.BTuple;
+import org.smoothbuild.virtualmachine.evaluate.step.Step;
+import org.smoothbuild.virtualmachine.wire.Sandbox;
+
+public class ComputationHashFactory {
+  private final Hash sandboxHash;
+
+  @Inject
+  public ComputationHashFactory(@Sandbox Hash sandboxHash) {
+    this.sandboxHash = sandboxHash;
+  }
+
+  public Hash create(Step step, BTuple args) {
+    return Hash.of(asList(sandboxHash, stepHash(step), args.hash()));
+  }
+}
