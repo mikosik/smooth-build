7fa5f0cf268313f1901ebcf101b3f16af1668de3
Marcin Mikosik <marcin.mikosik@gmail.com>
2024-10-10 17:32:06 +0200

    Migrate Parse class from Plan to Task

--- src/compiler-frontend/src/main/java/org/smoothbuild/compilerfrontend/compile/Parse.java
+++ src/compiler-frontend/src/main/java/org/smoothbuild/compilerfrontend/compile/Parse.java
@@ -4,6 +4,8 @@ import static java.lang.String.join;
 import static org.smoothbuild.common.Antlr.errorLine;
 import static org.smoothbuild.common.Antlr.markingLine;
 import static org.smoothbuild.common.base.Strings.unlines;
+import static org.smoothbuild.common.log.base.Label.label;
+import static org.smoothbuild.common.task.Output.output;
 import static org.smoothbuild.compilerfrontend.FrontendCompilerConstants.COMPILE_PREFIX;
 import static org.smoothbuild.compilerfrontend.compile.CompileError.compileError;
 
@@ -21,21 +23,15 @@ import org.smoothbuild.antlr.lang.SmoothAntlrLexer;
 import org.smoothbuild.antlr.lang.SmoothAntlrParser;
 import org.smoothbuild.antlr.lang.SmoothAntlrParser.ModuleContext;
 import org.smoothbuild.common.bucket.base.FullPath;
-import org.smoothbuild.common.log.base.Label;
 import org.smoothbuild.common.log.base.Logger;
-import org.smoothbuild.common.log.base.Try;
-import org.smoothbuild.common.plan.TryFunction2;
+import org.smoothbuild.common.task.Output;
+import org.smoothbuild.common.task.Task2;
 import org.smoothbuild.compilerfrontend.lang.base.location.Location;
 import org.smoothbuild.compilerfrontend.lang.base.location.Locations;
 
-public class Parse implements TryFunction2<String, FullPath, ModuleContext> {
+public class Parse implements Task2<ModuleContext, String, FullPath> {
   @Override
-  public Label label() {
-    return Label.label(COMPILE_PREFIX, "parse");
-  }
-
-  @Override
-  public Try<ModuleContext> apply(String sourceCode, FullPath fullPath) {
+  public Output<ModuleContext> execute(String sourceCode, FullPath fullPath) {
     var logger = new Logger();
     var errorListener = new ErrorListener(fullPath, logger);
     var smoothAntlrLexer = new SmoothAntlrLexer(CharStreams.fromString(sourceCode));
@@ -46,7 +42,8 @@ public class Parse implements TryFunction2<String, FullPath, ModuleContext> {
     smoothAntlrParser.removeErrorListeners();
     smoothAntlrParser.addErrorListener(errorListener);
 
-    return Try.of(smoothAntlrParser.module(), logger);
+    var label = label(COMPILE_PREFIX, "parse");
+    return output(smoothAntlrParser.module(), label, logger.toList());
   }
 
   public static class ErrorListener implements ANTLRErrorListener {
