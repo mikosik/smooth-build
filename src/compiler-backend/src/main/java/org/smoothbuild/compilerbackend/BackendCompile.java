4a0290ba1e3968784455dc2d3898d4ca962aed65
Marcin Mikosik <marcin.mikosik@gmail.com>
2024-10-14 14:17:40 +0200

    Migrate BackendCompile class from Plan to Task

--- src/compiler-backend/src/main/java/org/smoothbuild/compilerbackend/BackendCompile.java
+++ src/compiler-backend/src/main/java/org/smoothbuild/compilerbackend/BackendCompile.java
@@ -1,22 +1,21 @@
 package org.smoothbuild.compilerbackend;
 
+import static org.smoothbuild.common.collect.List.list;
 import static org.smoothbuild.common.log.base.Log.fatal;
-import static org.smoothbuild.common.log.base.Try.failure;
-import static org.smoothbuild.common.log.base.Try.success;
+import static org.smoothbuild.common.task.Output.output;
 import static org.smoothbuild.compilerbackend.BackendCompilerConstants.COMPILE_BACK_LABEL;
 import static org.smoothbuild.compilerbackend.CompiledExprs.compilationResult;
 
 import jakarta.inject.Inject;
 import org.smoothbuild.common.bindings.ImmutableBindings;
 import org.smoothbuild.common.collect.List;
-import org.smoothbuild.common.log.base.Label;
-import org.smoothbuild.common.log.base.Try;
-import org.smoothbuild.common.plan.TryFunction2;
+import org.smoothbuild.common.task.Output;
+import org.smoothbuild.common.task.Task2;
 import org.smoothbuild.compilerfrontend.lang.define.SExpr;
 import org.smoothbuild.compilerfrontend.lang.define.SNamedEvaluable;
 
 public class BackendCompile
-    implements TryFunction2<List<SExpr>, ImmutableBindings<SNamedEvaluable>, CompiledExprs> {
+    implements Task2<CompiledExprs, List<SExpr>, ImmutableBindings<SNamedEvaluable>> {
   private final SbTranslatorFactory sbTranslatorFactory;
 
   @Inject
@@ -25,20 +24,17 @@ public class BackendCompile
   }
 
   @Override
-  public Label label() {
-    return COMPILE_BACK_LABEL.append("generateBytecode");
-  }
-
-  @Override
-  public Try<CompiledExprs> apply(
+  public Output<CompiledExprs> execute(
       List<SExpr> sExprs, ImmutableBindings<SNamedEvaluable> evaluables) {
+    var label = COMPILE_BACK_LABEL.append("generateBytecode");
     var sbTranslator = sbTranslatorFactory.create(evaluables);
     try {
       var bExprs = sExprs.map(sbTranslator::translateExpr);
       var bsMapping = sbTranslator.bsMapping();
-      return success(compilationResult(sExprs, bExprs, bsMapping));
+      var result = compilationResult(sExprs, bExprs, bsMapping);
+      return output(result, label, list());
     } catch (SbTranslatorException e) {
-      return failure(fatal(e.getMessage()));
+      return output(label, list(fatal(e.getMessage())));
     }
   }
 }
