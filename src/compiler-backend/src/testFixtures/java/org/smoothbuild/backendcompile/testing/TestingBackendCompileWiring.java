a905bed9ec2d30b9cddf9d2994c47669b3d753d2
Marcin Mikosik <marcin.mikosik@gmail.com>
2024-10-16 09:51:19 +0200

    Add TestingBackendCompileWiring and TestingVmWiring

---
+++ src/compiler-backend/src/testFixtures/java/org/smoothbuild/backendcompile/testing/TestingBackendCompileWiring.java
@@ -0,0 +1,14 @@
+package org.smoothbuild.backendcompile.testing;
+
+import com.google.inject.AbstractModule;
+import org.smoothbuild.compilerbackend.CompilerBackendWiring;
+import org.smoothbuild.virtualmachine.testing.TestingVmWiring;
+
+// TODO is this wiring similar to EvaluatorTestCase.TestWiring?
+public class TestingBackendCompileWiring extends AbstractModule {
+  @Override
+  protected void configure() {
+    install(new CompilerBackendWiring());
+    install(new TestingVmWiring());
+  }
+}
---
+++ src/virtual-machine/src/testFixtures/java/org/smoothbuild/virtualmachine/testing/TestingVmWiring.java
@@ -0,0 +1,67 @@
+package org.smoothbuild.virtualmachine.testing;
+
+import static org.smoothbuild.common.collect.Map.map;
+import static org.smoothbuild.common.log.base.Level.INFO;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
+import com.google.inject.Singleton;
+import java.io.PrintWriter;
+import org.smoothbuild.common.base.Hash;
+import org.smoothbuild.common.bucket.base.Bucket;
+import org.smoothbuild.common.bucket.base.BucketId;
+import org.smoothbuild.common.bucket.base.SynchronizedBucket;
+import org.smoothbuild.common.bucket.mem.MemoryBucket;
+import org.smoothbuild.common.collect.Map;
+import org.smoothbuild.common.init.InitWiring;
+import org.smoothbuild.common.log.report.ReportWiring;
+import org.smoothbuild.virtualmachine.wire.BytecodeDb;
+import org.smoothbuild.virtualmachine.wire.ComputationDb;
+import org.smoothbuild.virtualmachine.wire.Project;
+import org.smoothbuild.virtualmachine.wire.Sandbox;
+import org.smoothbuild.virtualmachine.wire.VmWiring;
+
+public class TestingVmWiring extends AbstractModule {
+  public static final BucketId PROJECT_BUCKET = new BucketId("project");
+
+  @Override
+  protected void configure() {
+    install(new VmWiring());
+    install(new InitWiring());
+    install(new ReportWiring(new PrintWriter(System.out), (label, logs) -> true, INFO));
+  }
+
+  @Provides
+  @Singleton
+  public Map<BucketId, Bucket> provideBucketMap(@Project Bucket projectBucket) {
+    return map(PROJECT_BUCKET, projectBucket);
+  }
+
+  @Provides
+  @ComputationDb
+  @Singleton
+  public Bucket provideComputationCacheBucket() {
+    return new SynchronizedBucket(new MemoryBucket());
+  }
+
+  @Provides
+  @BytecodeDb
+  @Singleton
+  public Bucket provideBytecodeDbBucket() {
+    return new SynchronizedBucket(new MemoryBucket());
+  }
+
+  @Provides
+  @Project
+  @Singleton
+  public Bucket provideProjectBucket() {
+    return new SynchronizedBucket(new MemoryBucket());
+  }
+
+  @Provides
+  @Singleton
+  @Sandbox
+  public Hash provideSandboxHash() {
+    return Hash.of(33);
+  }
+}
