98ed95386dc421967f169ad82d1177c5ba6ae57e
Marcin Mikosik <marcin.mikosik@gmail.com>
2024-10-16 15:25:44 +0200

    Migrate RemoveArtifacts class from Plan to Task

--- src/cli/src/main/java/org/smoothbuild/cli/run/RemoveArtifacts.java
+++ src/cli/src/main/java/org/smoothbuild/cli/run/RemoveArtifacts.java
@@ -1,19 +1,20 @@
 package org.smoothbuild.cli.run;
 
+import static org.smoothbuild.common.collect.List.list;
+import static org.smoothbuild.common.log.base.Label.label;
 import static org.smoothbuild.common.log.base.Log.error;
-import static org.smoothbuild.common.log.base.Try.failure;
-import static org.smoothbuild.common.log.base.Try.success;
+import static org.smoothbuild.common.task.Output.output;
 
 import jakarta.inject.Inject;
 import java.io.IOException;
 import org.smoothbuild.cli.layout.Layout;
 import org.smoothbuild.common.bucket.base.Bucket;
-import org.smoothbuild.common.log.base.Label;
-import org.smoothbuild.common.log.base.Try;
-import org.smoothbuild.common.plan.TryFunction0;
+import org.smoothbuild.common.task.Output;
+import org.smoothbuild.common.task.Task0;
+import org.smoothbuild.common.tuple.Tuple0;
 import org.smoothbuild.virtualmachine.wire.Project;
 
-public class RemoveArtifacts implements TryFunction0<Void> {
+public class RemoveArtifacts implements Task0<Tuple0> {
   private final Bucket bucket;
 
   @Inject
@@ -22,17 +23,13 @@ public class RemoveArtifacts implements TryFunction0<Void> {
   }
 
   @Override
-  public Label label() {
-    return Label.label("artifacts", "removeAll");
-  }
-
-  @Override
-  public Try<Void> apply() {
+  public Output<Tuple0> execute() {
+    var label = label("artifacts", "removeAll");
     try {
       bucket.delete(Layout.ARTIFACTS_PATH);
-      return success(null);
+      return output(label, list());
     } catch (IOException e) {
-      return failure(error(e.getMessage()));
+      return output(label, list(error(e.getMessage())));
     }
   }
 }
