fc739dfbb1178908eeaa05111ac0e2eed505b44c
Marcin Mikosik <marcin.mikosik@gmail.com>
2024-10-17 14:13:57 +0200

    Remove no longer used Plan framework

--- src/common/src/main/java/org/smoothbuild/common/plan/MaybeFunction.java
+++
@@ -1,8 +0,0 @@
-package org.smoothbuild.common.plan;
-
-import org.smoothbuild.common.collect.Maybe;
-
-@FunctionalInterface
-public interface MaybeFunction<T, R> {
-  public Maybe<R> apply(T t);
-}
