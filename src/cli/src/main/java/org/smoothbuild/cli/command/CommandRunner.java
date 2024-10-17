e22b93f1fbecdfff3a3530b8843a013b70bc5c06
Marcin Mikosik <marcin.mikosik@gmail.com>
2024-10-17 15:25:01 +0200

    Refactor CommandRunner to use CommandCompleter

--- src/cli/src/main/java/org/smoothbuild/cli/command/CommandRunner.java
+++ src/cli/src/main/java/org/smoothbuild/cli/command/CommandRunner.java
@@ -1,32 +1,22 @@
 package org.smoothbuild.cli.command;
 
-import static org.smoothbuild.cli.Main.EXIT_CODE_ERROR;
-import static org.smoothbuild.cli.Main.EXIT_CODE_SUCCESS;
-
 import com.google.inject.Key;
 import jakarta.inject.Inject;
-import org.smoothbuild.cli.report.StatusPrinter;
 import org.smoothbuild.common.task.Task0;
 import org.smoothbuild.common.task.TaskExecutor;
 
 public class CommandRunner {
   private final TaskExecutor taskExecutor;
-  private final StatusPrinter statusPrinter;
+  private final CommandCompleter commandCompleter;
 
   @Inject
-  public CommandRunner(TaskExecutor taskExecutor, StatusPrinter statusPrinter) {
+  public CommandRunner(TaskExecutor taskExecutor, CommandCompleter commandCompleter) {
     this.taskExecutor = taskExecutor;
-    this.statusPrinter = statusPrinter;
+    this.commandCompleter = commandCompleter;
   }
 
   public int run(Class<? extends Task0<Void>> taskClass) {
-    var promise = taskExecutor.submit(Key.get(taskClass));
-    try {
-      taskExecutor.waitUntilIdle();
-    } catch (InterruptedException e) {
-      throw new RuntimeException(e);
-    }
-    statusPrinter.printSummary();
-    return promise.toMaybe().map(v -> EXIT_CODE_SUCCESS).getOr(EXIT_CODE_ERROR);
+    taskExecutor.submit(Key.get(taskClass));
+    return commandCompleter.waitForCompletion();
   }
 }
