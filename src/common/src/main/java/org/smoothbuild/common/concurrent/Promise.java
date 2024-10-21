7f8b775aedc278cfcd75f75f54fdc2948f0629b1
Marcin Mikosik <marcin.mikosik@gmail.com>
2024-10-21 13:40:22 +0200

    Add Promise.map()

--- src/common/src/main/java/org/smoothbuild/common/concurrent/Promise.java
+++ src/common/src/main/java/org/smoothbuild/common/concurrent/Promise.java
@@ -1,6 +1,7 @@
 package org.smoothbuild.common.concurrent;
 
 import java.util.function.Consumer;
+import java.util.function.Function;
 import org.smoothbuild.common.collect.List;
 import org.smoothbuild.common.collect.Maybe;
 
@@ -21,6 +22,11 @@ public interface Promise<T> {
 
   public void addConsumer(Consumer<T> consumer);
 
+  public default <R> Promise<R> map(Function<T, R> mapper) {
+    MutablePromise<R> promise = promise();
+    addConsumer(v -> promise.accept(mapper.apply(v)));
+    return promise;
+  }
   public static void runWhenAllAvailable(List<? extends Promise<?>> promises, Runnable runnable) {
     ThresholdRunnable latch = new ThresholdRunnable(promises.size(), runnable);
     promises.forEach(child -> child.addConsumer(v -> latch.run()));
