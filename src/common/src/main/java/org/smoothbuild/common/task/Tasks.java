5ee3114523396dd82db9e793c6eeaa5430f0f2ee
Marcin Mikosik <marcin.mikosik@gmail.com>
2024-10-23 13:43:41 +0200

    Add Tasks.task2

--- src/common/src/main/java/org/smoothbuild/common/task/Tasks.java
+++ src/common/src/main/java/org/smoothbuild/common/task/Tasks.java
@@ -5,6 +5,7 @@ import static org.smoothbuild.common.collect.Maybe.some;
 import static org.smoothbuild.common.concurrent.Promise.promise;
 import static org.smoothbuild.common.task.Output.output;
 
+import java.util.function.BiFunction;
 import java.util.function.Function;
 import org.smoothbuild.common.collect.Maybe;
 import org.smoothbuild.common.concurrent.Promise;
@@ -15,7 +16,11 @@ public class Tasks {
     return promise(some(value));
   }
 
-  public static <R, A> Task1<R, A> task1(Label label, Function<A, R> mapper) {
-    return argument -> output(mapper.apply(argument), label, list());
+  public static <R, A1> Task1<R, A1> task1(Label label, Function<A1, R> function) {
+    return arg1 -> output(function.apply(arg1), label, list());
+  }
+
+  public static <R, A1, A2> Task2<R, A1, A2> task2(Label label, BiFunction<A1, A2, R> function) {
+    return (arg1, arg2) -> output(function.apply(arg1, arg2), label, list());
   }
 }
