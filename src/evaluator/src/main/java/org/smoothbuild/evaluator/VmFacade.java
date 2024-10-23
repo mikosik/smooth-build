a51c4b81bfe4dbb6950868653b449a2d96fec14a
Marcin Mikosik <marcin.mikosik@gmail.com>
2024-10-23 14:01:15 +0200

    Refactor VmFacade

--- src/evaluator/src/main/java/org/smoothbuild/evaluator/VmFacade.java
+++ src/evaluator/src/main/java/org/smoothbuild/evaluator/VmFacade.java
@@ -4,20 +4,18 @@ import static org.smoothbuild.common.collect.List.list;
 import static org.smoothbuild.common.log.base.Label.label;
 import static org.smoothbuild.common.log.base.ResultSource.EXECUTION;
 import static org.smoothbuild.common.log.report.Report.report;
-import static org.smoothbuild.common.task.Output.output;
 import static org.smoothbuild.common.task.Output.schedulingOutput;
+import static org.smoothbuild.common.task.Tasks.argument;
+import static org.smoothbuild.common.task.Tasks.task2;
 import static org.smoothbuild.evaluator.EvaluatedExprs.evaluatedExprs;
 import static org.smoothbuild.evaluator.EvaluatorConstants.EVALUATE_LABEL;
 
 import jakarta.inject.Inject;
-import org.smoothbuild.common.collect.List;
 import org.smoothbuild.common.log.report.Trace;
 import org.smoothbuild.common.task.Output;
 import org.smoothbuild.common.task.Task1;
 import org.smoothbuild.common.task.TaskExecutor;
-import org.smoothbuild.common.task.TaskX;
 import org.smoothbuild.compilerbackend.CompiledExprs;
-import org.smoothbuild.virtualmachine.bytecode.expr.base.BValue;
 import org.smoothbuild.virtualmachine.evaluate.execute.Vm;
 
 public class VmFacade implements Task1<EvaluatedExprs, CompiledExprs> {
@@ -32,22 +30,12 @@ public class VmFacade implements Task1<EvaluatedExprs, CompiledExprs> {
 
   @Override
   public Output<EvaluatedExprs> execute(CompiledExprs compiledExprs) {
-    var evaluated = compiledExprs.bExprs().map(vm::evaluate);
-    var evaluatedExprs = taskExecutor.submit(new Merge(compiledExprs), evaluated);
+    var evaluated = taskExecutor.join(compiledExprs.bExprs().map(vm::evaluate));
+    var mergeLabel = EVALUATE_LABEL.append("merge");
+    var evaluatedExprs = taskExecutor.submit(
+        task2(mergeLabel, (ce, ee) -> evaluatedExprs(ce.sExprs(), ee)),
+        argument(compiledExprs),
+        evaluated);
     return schedulingOutput(evaluatedExprs, report(label("??"), new Trace(), EXECUTION, list()));
-  }
-
-  public static class Merge implements TaskX<EvaluatedExprs, BValue> {
-    private final CompiledExprs compiledExprs;
-
-    public Merge(CompiledExprs compiledExprs) {
-      this.compiledExprs = compiledExprs;
-    }
-
-    @Override
-    public Output<EvaluatedExprs> execute(List<BValue> evaluatedExprs) {
-      var evaluatedExprs1 = evaluatedExprs(compiledExprs.sExprs(), evaluatedExprs);
-      return output(evaluatedExprs1, EVALUATE_LABEL.append("merge"), list());
-    }
   }
 }
