9a709c696662db9c451c3dd4eebcf4ca95c2ef02
Marcin Mikosik <marcin.mikosik@gmail.com>
2024-10-23 13:16:18 +0200

    Refactor VmFacade

--- src/evaluator/src/main/java/org/smoothbuild/evaluator/VmFacade.java
+++ src/evaluator/src/main/java/org/smoothbuild/evaluator/VmFacade.java
@@ -9,11 +9,8 @@ import static org.smoothbuild.common.task.Output.schedulingOutput;
 import static org.smoothbuild.evaluator.EvaluatedExprs.evaluatedExprs;
 import static org.smoothbuild.evaluator.EvaluatorConstants.EVALUATE_LABEL;
 
-import com.google.inject.Injector;
 import jakarta.inject.Inject;
 import org.smoothbuild.common.collect.List;
-import org.smoothbuild.common.collect.Maybe;
-import org.smoothbuild.common.concurrent.Promise;
 import org.smoothbuild.common.log.report.Trace;
 import org.smoothbuild.common.task.Output;
 import org.smoothbuild.common.task.Task1;
@@ -24,21 +21,20 @@ import org.smoothbuild.virtualmachine.bytecode.expr.base.BValue;
 import org.smoothbuild.virtualmachine.evaluate.execute.Vm;
 
 public class VmFacade implements Task1<EvaluatedExprs, CompiledExprs> {
-  private final Injector injector;
   private final TaskExecutor taskExecutor;
+  private final Vm vm;
 
   @Inject
-  public VmFacade(Injector injector, TaskExecutor taskExecutor) {
-    this.injector = injector;
+  public VmFacade(TaskExecutor taskExecutor, Vm vm) {
     this.taskExecutor = taskExecutor;
+    this.vm = vm;
   }
 
   @Override
   public Output<EvaluatedExprs> execute(CompiledExprs compiledExprs) {
-    var vm = injector.getInstance(Vm.class);
-    List<Promise<Maybe<BValue>>> evaluated = compiledExprs.bExprs().map(vm::evaluate);
-    var r = taskExecutor.submit(new Merge(compiledExprs), evaluated);
-    return schedulingOutput(r, report(label("??"), new Trace(), EXECUTION, list()));
+    var evaluated = compiledExprs.bExprs().map(vm::evaluate);
+    var evaluatedExprs = taskExecutor.submit(new Merge(compiledExprs), evaluated);
+    return schedulingOutput(evaluatedExprs, report(label("??"), new Trace(), EXECUTION, list()));
   }
 
   public static class Merge implements TaskX<EvaluatedExprs, BValue> {
