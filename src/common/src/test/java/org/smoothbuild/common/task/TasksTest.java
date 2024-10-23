9a80c34dd649f7e5fa4f51ea31454ac8f5f4e14b
Marcin Mikosik <marcin.mikosik@gmail.com>
2024-10-23 13:43:41 +0200

    Add Tasks.task2

--- src/common/src/test/java/org/smoothbuild/common/task/TasksTest.java
+++ src/common/src/test/java/org/smoothbuild/common/task/TasksTest.java
@@ -4,6 +4,8 @@ import static com.google.common.truth.Truth.assertThat;
 import static org.smoothbuild.common.collect.List.list;
 import static org.smoothbuild.common.log.base.Label.label;
 import static org.smoothbuild.common.task.Output.output;
+import static org.smoothbuild.common.task.Tasks.task1;
+import static org.smoothbuild.common.task.Tasks.task2;
 
 import org.junit.jupiter.api.Test;
 
@@ -11,7 +13,14 @@ public class TasksTest {
   @Test
   void task1_invokes_function() {
     var label = label("label");
-    var mapTask = Tasks.task1(label, (String s) -> s + "!");
+    var mapTask = task1(label, (String s) -> s + "!");
     assertThat(mapTask.execute("abc")).isEqualTo(output("abc!", label, list()));
+  }
+
+  @Test
+  void task2_invokes_function() {
+    var label = label("label");
+    var mapTask = task2(label, (String s1, String s2) -> s1 + s2);
+    assertThat(mapTask.execute("abc", "def")).isEqualTo(output("abcdef", label, list()));
   }
 }
