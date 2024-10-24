c450af7dce3efe2521bbaf33e20c2a2e6a5cc8d3
Marcin Mikosik <marcin.mikosik@gmail.com>
2024-10-24 11:36:02 +0200

    Refactor Vm, Job

--- src/virtual-machine/src/main/java/org/smoothbuild/virtualmachine/evaluate/execute/Job.java
+++ src/virtual-machine/src/main/java/org/smoothbuild/virtualmachine/evaluate/execute/Job.java
@@ -1,29 +1,17 @@
 package org.smoothbuild.virtualmachine.evaluate.execute;
 
-import com.google.common.base.Supplier;
-import com.google.common.base.Suppliers;
-import java.util.function.Function;
 import org.smoothbuild.common.collect.List;
-import org.smoothbuild.common.collect.Maybe;
-import org.smoothbuild.common.concurrent.Promise;
 import org.smoothbuild.virtualmachine.bytecode.expr.base.BExpr;
-import org.smoothbuild.virtualmachine.bytecode.expr.base.BValue;
 
 public final class Job {
   private final BExpr expr;
   private final List<Job> environment;
   private final BTrace trace;
-  private final Supplier<Promise<Maybe<BValue>>> evaluator;
 
-  public Job(
-      BExpr expr,
-      List<Job> environment,
-      BTrace trace,
-      Function<Job, Promise<Maybe<BValue>>> evaluator) {
+  public Job(BExpr expr, List<Job> environment, BTrace trace) {
     this.expr = expr;
     this.environment = environment;
     this.trace = trace;
-    this.evaluator = Suppliers.memoize(() -> evaluator.apply(this));
   }
 
   public BExpr expr() {
@@ -36,9 +24,5 @@ public final class Job {
 
   public BTrace trace() {
     return trace;
-  }
-
-  public Promise<Maybe<BValue>> evaluate() {
-    return evaluator.get();
   }
 }
