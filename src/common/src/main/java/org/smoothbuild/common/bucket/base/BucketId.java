23ad4e8bb97b3612ba6e1b73f5b51d3aac501e3a
Marcin Mikosik <marcin.mikosik@gmail.com>
2024-10-28 16:28:59 +0100

    Add helper method to BucketId

--- src/common/src/main/java/org/smoothbuild/common/bucket/base/BucketId.java
+++ src/common/src/main/java/org/smoothbuild/common/bucket/base/BucketId.java
@@ -1,7 +1,17 @@
 package org.smoothbuild.common.bucket.base;
 
+import static org.smoothbuild.common.bucket.base.FullPath.fullPath;
+
 public record BucketId(String id) {
   public static BucketId bucketId(String id) {
     return new BucketId(id);
+  }
+
+  public FullPath path(String path) {
+    return path(Path.path(path));
+  }
+
+  public FullPath path(Path path) {
+    return fullPath(this, path);
   }
 }
