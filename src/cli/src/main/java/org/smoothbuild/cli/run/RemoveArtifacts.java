a9a75e94b2468f56c7869ea817c47c3ed46187fb
Marcin Mikosik <marcin.mikosik@gmail.com>
2024-10-28 18:16:06 +0100

    Refactor RemoveArtifacts to use Filesystem

--- src/cli/src/main/java/org/smoothbuild/cli/run/RemoveArtifacts.java
+++ src/cli/src/main/java/org/smoothbuild/cli/run/RemoveArtifacts.java
@@ -8,25 +8,24 @@ import static org.smoothbuild.common.task.Output.output;
 import jakarta.inject.Inject;
 import java.io.IOException;
 import org.smoothbuild.cli.layout.Layout;
-import org.smoothbuild.common.bucket.base.Bucket;
+import org.smoothbuild.common.bucket.base.Filesystem;
 import org.smoothbuild.common.task.Output;
 import org.smoothbuild.common.task.Task0;
 import org.smoothbuild.common.tuple.Tuple0;
-import org.smoothbuild.virtualmachine.wire.Project;
 
 public class RemoveArtifacts implements Task0<Tuple0> {
-  private final Bucket bucket;
+  private final Filesystem filesystem;
 
   @Inject
-  public RemoveArtifacts(@Project Bucket bucket) {
-    this.bucket = bucket;
+  public RemoveArtifacts(Filesystem filesystem) {
+    this.filesystem = filesystem;
   }
 
   @Override
   public Output<Tuple0> execute() {
     var label = label("artifacts", "removeAll");
     try {
-      bucket.delete(Layout.ARTIFACTS_PATH);
+      filesystem.delete(Layout.ARTIFACTS);
       return output(label, list());
     } catch (IOException e) {
       return output(label, list(error(e.getMessage())));
