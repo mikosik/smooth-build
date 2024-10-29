7788095f54b629f1ee08d39c96643c184970fb3c
Marcin Mikosik <marcin.mikosik@gmail.com>
2024-10-29 09:38:39 +0100

    Fix naming in EvaluatorTestContext

--- src/cli/src/test/java/org/smoothbuild/cli/accept/NativeTest.java
+++ src/cli/src/test/java/org/smoothbuild/cli/accept/NativeTest.java
@@ -93,7 +93,7 @@ public class NativeTest extends EvaluatorTestContext {
             """);
         evaluate("result");
         assertThat(logs())
-            .contains(userFatal(1, "Error loading native jar '{module-bucket}/userModule.jar'."));
+            .contains(userFatal(1, "Error loading native jar '{modules}/userModule.jar'."));
       }
 
       @Test
