32829e74077dfe47212fea7dcbc4966afc8f7933
Marcin Mikosik <marcin.mikosik@gmail.com>
2024-10-29 09:50:49 +0100

    Refactor FullPathTest

--- src/common/src/test/java/org/smoothbuild/common/bucket/base/FullPathTest.java
+++ src/common/src/test/java/org/smoothbuild/common/bucket/base/FullPathTest.java
@@ -16,38 +16,36 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 public class FullPathTest {
+  private static final BucketId PROJECT = bucketId("project");
+  private static final BucketId LIBS = bucketId("libs");
+
   @Test
   void equals_and_hash_code() {
     String file = "abc.smooth";
 
     EqualsTester tester = new EqualsTester();
-    tester.addEqualityGroup(
-        fullPath(bucketId("project"), path(file)), fullPath(bucketId("project"), path(file)));
-    tester.addEqualityGroup(
-        fullPath(bucketId("project"), path("def")), fullPath(bucketId("project"), path("def")));
-    tester.addEqualityGroup(
-        fullPath(bucketId("sl"), path(file)), fullPath(bucketId("sl"), path(file)));
+    tester.addEqualityGroup(fullPath(PROJECT, path(file)), fullPath(PROJECT, path(file)));
+    tester.addEqualityGroup(fullPath(PROJECT, path("def")), fullPath(PROJECT, path("def")));
+    tester.addEqualityGroup(fullPath(LIBS, path(file)), fullPath(LIBS, path(file)));
 
     tester.testEquals();
   }
 
   @Test
   void append_part() {
-    var bucketId = bucketId("project");
-    var fullPath = fullPath(bucketId, path("abc"));
-    assertThat(fullPath.appendPart("def")).isEqualTo(fullPath(bucketId, path("abc/def")));
+    var fullPath = fullPath(PROJECT, path("abc"));
+    assertThat(fullPath.appendPart("def")).isEqualTo(fullPath(PROJECT, path("abc/def")));
   }
 
   @Test
   void append() {
-    var bucketId = bucketId("project");
-    var fullPath = fullPath(bucketId, path("abc"));
-    assertThat(fullPath.append(path("def/ghi"))).isEqualTo(fullPath(bucketId, path("abc/def/ghi")));
+    var fullPath = fullPath(PROJECT, path("abc"));
+    assertThat(fullPath.append(path("def/ghi"))).isEqualTo(fullPath(PROJECT, path("abc/def/ghi")));
   }
 
   @Test
   void parent_of_root_dir_throws_exception() {
-    var fullPath = fullPath(bucketId("project"), root());
+    var fullPath = fullPath(PROJECT, root());
     assertCall(() -> fullPath.parent()).throwsException(IllegalArgumentException.class);
   }
 
@@ -59,31 +57,23 @@ public class FullPathTest {
 
   public static Stream<Arguments> parentArguments() {
     return Stream.of(
+        arguments(fullPath(PROJECT, path("abc")), fullPath(PROJECT, root())),
+        arguments(fullPath(PROJECT, path(" ")), fullPath(PROJECT, root())),
+        arguments(fullPath(PROJECT, path("abc/def")), fullPath(PROJECT, path("abc"))),
+        arguments(fullPath(PROJECT, path("abc/def/ghi")), fullPath(PROJECT, path("abc/def"))),
         arguments(
-            fullPath(bucketId("bucketId"), path("abc")), fullPath(bucketId("bucketId"), root())),
-        arguments(
-            fullPath(bucketId("bucketId"), path(" ")), fullPath(bucketId("bucketId"), root())),
-        arguments(
-            fullPath(bucketId("bucketId"), path("abc/def")),
-            fullPath(bucketId("bucketId"), path("abc"))),
-        arguments(
-            fullPath(bucketId("bucketId"), path("abc/def/ghi")),
-            fullPath(bucketId("bucketId"), path("abc/def"))),
-        arguments(
-            fullPath(bucketId("bucketId"), path("abc/def/ghi/ijk")),
-            fullPath(bucketId("bucketId"), path("abc/def/ghi"))));
+            fullPath(PROJECT, path("abc/def/ghi/ijk")), fullPath(PROJECT, path("abc/def/ghi"))));
   }
 
   @Test
   void with_extension() {
-    var fullPath = fullPath(bucketId("project"), path("full/path.smooth"));
-    assertThat(fullPath.withExtension("jar"))
-        .isEqualTo(fullPath(bucketId("project"), path("full/path.jar")));
+    var fullPath = fullPath(PROJECT, path("full/path.smooth"));
+    assertThat(fullPath.withExtension("jar")).isEqualTo(fullPath(PROJECT, path("full/path.jar")));
   }
 
   @Test
   void to_string() {
-    var fullPath = fullPath(bucketId("project"), path("abc"));
+    var fullPath = fullPath(PROJECT, path("abc"));
     assertThat(fullPath.toString()).isEqualTo("{project}/abc");
   }
 }
