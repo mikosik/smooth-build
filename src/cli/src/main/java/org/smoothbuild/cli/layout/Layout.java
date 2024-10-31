7538db21b9d7c152f82ae7661c0b79935c4dc2a2
Marcin Mikosik <marcin.mikosik@gmail.com>
2024-10-31 10:53:48 +0100

    Rename BucketId.path() -> append()

--- src/cli/src/main/java/org/smoothbuild/cli/layout/Layout.java
+++ src/cli/src/main/java/org/smoothbuild/cli/layout/Layout.java
@@ -13,15 +13,15 @@ import org.smoothbuild.common.collect.List;
 
 public class Layout {
   public static final Path SMOOTH_DIR = path(".smooth");
-  public static final FullPath PROJECT_PATH = PROJECT_ALIAS.path(Path.root());
+  public static final FullPath PROJECT_PATH = PROJECT_ALIAS.append(Path.root());
   public static final FullPath SMOOTH_PATH = PROJECT_PATH.appendPart(".smooth");
   public static final FullPath ARTIFACTS_PATH = SMOOTH_PATH.appendPart("artifacts");
   public static final FullPath COMPUTATION_DB_PATH = SMOOTH_PATH.appendPart("computations");
   public static final FullPath BYTECODE_DB_PATH = SMOOTH_PATH.appendPart("bytecode");
   public static final Path SMOOTH_LOCK_PATH = SMOOTH_DIR.appendPart("lock");
   public static final Path DEFAULT_MODULE_PATH = path("build.smooth");
-  public static final FullPath DEFAULT_MODULE_FILE_PATH = PROJECT_ALIAS.path(DEFAULT_MODULE_PATH);
-  public static final FullPath STD_LIB_MODULE_PATH = LIBRARY_ALIAS.path(path("std_lib.smooth"));
+  public static final FullPath DEFAULT_MODULE_FILE_PATH = PROJECT_ALIAS.append(DEFAULT_MODULE_PATH);
+  public static final FullPath STD_LIB_MODULE_PATH = LIBRARY_ALIAS.append(path("std_lib.smooth"));
   public static final List<FullPath> STD_LIB_MODULES = list(STD_LIB_MODULE_PATH);
   public static final List<FullPath> MODULES = STD_LIB_MODULES.append(DEFAULT_MODULE_FILE_PATH);
   public static final FullPath SMOOTH_JAR_FILE_PATH = fullPath(INSTALL_ALIAS, path("smooth.jar"));
