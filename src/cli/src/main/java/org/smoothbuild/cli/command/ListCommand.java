b5562bf4732b2975a18ea9c59c3dba248c419898
Marcin Mikosik <marcin.mikosik@gmail.com>
2024-11-12 12:56:58 +0100

    Rename ListEvaluables -> ScheduleList

--- src/cli/src/main/java/org/smoothbuild/cli/command/ListCommand.java
+++ src/cli/src/main/java/org/smoothbuild/cli/command/ListCommand.java
@@ -3,7 +3,7 @@ package org.smoothbuild.cli.command;
 import static org.smoothbuild.cli.run.CreateInjector.createInjector;
 
 import java.nio.file.Path;
-import org.smoothbuild.cli.run.ListEvaluables;
+import org.smoothbuild.cli.run.ScheduleList;
 import picocli.CommandLine.Command;
 
 @Command(
@@ -15,6 +15,6 @@ public class ListCommand extends ProjectCommand {
   @Override
   protected Integer executeCommand(Path projectDir) {
     var injector = createInjector(projectDir, out(), logLevel);
-    return injector.getInstance(CommandRunner.class).run(s -> s.submit(ListEvaluables.class));
+    return injector.getInstance(CommandRunner.class).run(s -> s.submit(ScheduleList.class));
   }
 }
