0effc3b2c7cbd592b0b6144ab4726c6fa4b7bde2
Marcin Mikosik <marcin.mikosik@gmail.com>
2024-11-12 12:41:36 +0100

    Merge CommandCompleter into CommandRunner

--- src/cli/src/main/java/org/smoothbuild/cli/command/CommandRunner.java
+++ src/cli/src/main/java/org/smoothbuild/cli/command/CommandRunner.java
@@ -1,23 +1,48 @@
 package org.smoothbuild.cli.command;
 
+import static org.smoothbuild.cli.Main.EXIT_CODE_ERROR;
+import static org.smoothbuild.cli.Main.EXIT_CODE_SUCCESS;
+import static org.smoothbuild.common.collect.List.list;
+import static org.smoothbuild.common.log.base.Label.label;
+import static org.smoothbuild.common.log.base.Log.fatal;
+import static org.smoothbuild.common.log.base.ResultSource.EXECUTION;
+import static org.smoothbuild.common.log.report.Report.report;
+
 import jakarta.inject.Inject;
 import java.util.function.Function;
+import org.smoothbuild.cli.report.StatusPrinter;
 import org.smoothbuild.common.collect.Maybe;
 import org.smoothbuild.common.concurrent.Promise;
+import org.smoothbuild.common.log.report.Reporter;
+import org.smoothbuild.common.log.report.Trace;
 import org.smoothbuild.common.task.Scheduler;
 
 public class CommandRunner {
   private final Scheduler scheduler;
-  private final CommandCompleter commandCompleter;
+  private final StatusPrinter statusPrinter;
+  private final Reporter reporter;
 
   @Inject
-  public CommandRunner(Scheduler scheduler, CommandCompleter commandCompleter) {
+  public CommandRunner(Scheduler scheduler, StatusPrinter statusPrinter, Reporter reporter) {
     this.scheduler = scheduler;
-    this.commandCompleter = commandCompleter;
+    this.statusPrinter = statusPrinter;
+    this.reporter = reporter;
   }
 
   public <T extends Maybe<?>> int run(Function<Scheduler, Promise<T>> schedulingFunction) {
     Promise<T> result = schedulingFunction.apply(scheduler);
-    return commandCompleter.waitForCompletion(result);
+    return waitForCompletion(result);
+  }
+
+  private int waitForCompletion(Promise<? extends Maybe<?>> commandResult) {
+    try {
+      Maybe<?> result = commandResult.getBlocking();
+      statusPrinter.printSummary();
+      return result.isNone() ? EXIT_CODE_ERROR : EXIT_CODE_SUCCESS;
+    } catch (InterruptedException e) {
+      var fatal = fatal("main thread has been interrupted");
+      reporter.submit(report(label("smooth"), new Trace(), EXECUTION, list(fatal)));
+      return EXIT_CODE_ERROR;
+    }
   }
 }
