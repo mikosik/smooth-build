0d23eed8ba9cbb9c6a0ef0cf14a341f08f6836f2
Marcin Mikosik <marcin.mikosik@gmail.com>
2024-11-27 13:53:33 +0100

    Move STrace to common module

--- src/compiler-frontend/src/testFixtures/java/org/smoothbuild/compilerfrontend/testing/FrontendCompilerTestApi.java
+++ src/compiler-frontend/src/testFixtures/java/org/smoothbuild/compilerfrontend/testing/FrontendCompilerTestApi.java
@@ -9,11 +9,8 @@ import static org.smoothbuild.common.collect.Map.map;
 import static org.smoothbuild.common.collect.Maybe.none;
 import static org.smoothbuild.common.collect.Maybe.some;
 import static org.smoothbuild.common.collect.NList.nlist;
-import static org.smoothbuild.common.filesystem.base.FullPath.fullPath;
-import static org.smoothbuild.common.filesystem.base.Path.path;
 import static org.smoothbuild.common.io.Okios.intToByteString;
 import static org.smoothbuild.common.log.base.Log.error;
-import static org.smoothbuild.common.log.location.Locations.fileLocation;
 import static org.smoothbuild.compilerfrontend.lang.define.SItem.toTypes;
 import static org.smoothbuild.compilerfrontend.lang.type.AnnotationNames.BYTECODE;
 import static org.smoothbuild.compilerfrontend.lang.type.AnnotationNames.NATIVE_IMPURE;
@@ -28,8 +25,6 @@ import org.smoothbuild.common.collect.Map;
 import org.smoothbuild.common.collect.Maybe;
 import org.smoothbuild.common.collect.NList;
 import org.smoothbuild.common.collect.Named;
-import org.smoothbuild.common.filesystem.base.Alias;
-import org.smoothbuild.common.filesystem.base.FullPath;
 import org.smoothbuild.common.log.base.Log;
 import org.smoothbuild.common.log.location.Location;
 import org.smoothbuild.compilerfrontend.compile.ast.define.PCall;
@@ -69,7 +64,6 @@ import org.smoothbuild.compilerfrontend.lang.define.SPolymorphic;
 import org.smoothbuild.compilerfrontend.lang.define.SReference;
 import org.smoothbuild.compilerfrontend.lang.define.SSelect;
 import org.smoothbuild.compilerfrontend.lang.define.SString;
-import org.smoothbuild.compilerfrontend.lang.define.STrace;
 import org.smoothbuild.compilerfrontend.lang.type.SArrayType;
 import org.smoothbuild.compilerfrontend.lang.type.SBlobType;
 import org.smoothbuild.compilerfrontend.lang.type.SBoolType;
@@ -627,28 +621,6 @@ public interface FrontendCompilerTestApi extends VmTestApi {
     return new SItemSig(type, name);
   }
 
-  public default STrace sTrace() {
-    return new STrace();
-  }
-
-  public default STrace sTrace(String name2, int line2, String name1, int line1) {
-    return sTrace(name2, location(line2), name1, location(line1));
-  }
-
-  public default STrace sTrace(String name2, Location location2, String name1, Location location1) {
-    var element1 = new STrace.Line(name1, location1, null);
-    var element2 = new STrace.Line(name2, location2, element1);
-    return new STrace(element2);
-  }
-
-  public default STrace sTrace(String name, int line) {
-    return sTrace(name, location(line));
-  }
-
-  public default STrace sTrace(String name, Location location) {
-    return new STrace(new STrace.Line(name, location, null));
-  }
-
   public default PModule pModule(List<PStruct> structs, List<PNamedEvaluable> evaluables) {
     return new PModule("", structs, evaluables);
   }
@@ -776,13 +748,5 @@ public interface FrontendCompilerTestApi extends VmTestApi {
 
   public default Location location(int line) {
     return location(moduleFullPath(), line);
-  }
-
-  public default Location location(Alias alias) {
-    return location(fullPath(alias, path("path")), 17);
-  }
-
-  public default Location location(FullPath fullPath, int line) {
-    return fileLocation(fullPath, line);
   }
 }
