2a91e08b56f05a36c1cad67469500ab146360599
Marcin Mikosik <marcin.mikosik@gmail.com>
2024-11-27 14:47:51 +0100

    Refactor ScheduleEvaluate, BackendCompile, CompiledExprs

--- src/compiler-backend/src/main/java/org/smoothbuild/compilerbackend/CompiledExprs.java
+++ src/compiler-backend/src/main/java/org/smoothbuild/compilerbackend/CompiledExprs.java
@@ -1,19 +1,7 @@
 package org.smoothbuild.compilerbackend;
 
-import static com.google.common.base.Preconditions.checkArgument;
-
 import org.smoothbuild.common.collect.List;
 import org.smoothbuild.common.log.report.BsMapping;
-import org.smoothbuild.compilerfrontend.lang.define.SExpr;
 import org.smoothbuild.virtualmachine.bytecode.expr.base.BExpr;
 
-public record CompiledExprs(List<SExpr> sExprs, List<BExpr> bExprs, BsMapping bsMapping) {
-  public static CompiledExprs compilationResult(
-      List<SExpr> sExprs, List<BExpr> bExprs, BsMapping bsMapping) {
-    return new CompiledExprs(sExprs, bExprs, bsMapping);
-  }
-
-  public CompiledExprs {
-    checkArgument(sExprs.size() == bExprs.size());
-  }
-}
+public record CompiledExprs(List<BExpr> bExprs, BsMapping bsMapping) {}
