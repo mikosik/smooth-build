02d19671ef9e468d0341b345f44df6c2a6af97c3
Marcin Mikosik <marcin.mikosik@gmail.com>
2024-12-16 10:42:56 +0100

    Redesign PSelect to contain fieldNameText() and fieldName(), refator code to use latter

--- src/compiler-frontend/src/main/java/org/smoothbuild/compilerfrontend/compile/ast/define/PSelect.java
+++ src/compiler-frontend/src/main/java/org/smoothbuild/compilerfrontend/compile/ast/define/PSelect.java
@@ -5,23 +5,33 @@ import static org.smoothbuild.common.collect.List.list;
 
 import java.util.Objects;
 import org.smoothbuild.common.log.location.Location;
+import org.smoothbuild.compilerfrontend.lang.base.Name;
 
 public final class PSelect extends PExpr {
   private final PExpr selectable;
-  private final String field;
+  private final String fieldNameText;
+  private Name fieldName;
 
-  public PSelect(PExpr selectable, String field, Location location) {
+  public PSelect(PExpr selectable, String fieldNameText, Location location) {
     super(location);
     this.selectable = selectable;
-    this.field = field;
+    this.fieldNameText = fieldNameText;
   }
 
   public PExpr selectable() {
     return selectable;
   }
 
-  public String field() {
-    return field;
+  public String fieldNameText() {
+    return fieldNameText;
+  }
+
+  public Name fieldName() {
+    return fieldName;
+  }
+
+  public void setFieldName(Name fieldName) {
+    this.fieldName = fieldName;
   }
 
   @Override
@@ -31,18 +41,19 @@ public final class PSelect extends PExpr {
     }
     return object instanceof PSelect that
         && Objects.equals(this.selectable, that.selectable)
-        && Objects.equals(this.field, that.field)
+        && Objects.equals(this.fieldNameText, that.fieldNameText)
         && Objects.equals(this.location(), that.location());
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(selectable, field, location());
+    return Objects.hash(selectable, fieldNameText, location());
   }
 
   @Override
   public String toString() {
-    var fields = list("selectable = " + selectable, "field = " + field, "location = " + location())
+    var fields = list(
+            "selectable = " + selectable, "field = " + fieldNameText, "location = " + location())
         .toString("\n");
     return "PSelect(\n" + indent(fields) + "\n)";
   }
