9af2534af95641871e3bcccdf1fb54a05a945e1c
Marcin Mikosik <marcin.mikosik@gmail.com>
2024-12-23 13:46:22 +0100

    Add Bindings.find(Id)

--- src/compiler-frontend/src/main/java/org/smoothbuild/compilerfrontend/lang/bindings/Bindings.java
+++ src/compiler-frontend/src/main/java/org/smoothbuild/compilerfrontend/lang/bindings/Bindings.java
@@ -1,8 +1,15 @@
 package org.smoothbuild.compilerfrontend.lang.bindings;
 
+import static org.smoothbuild.common.base.Strings.q;
+import static org.smoothbuild.common.collect.Result.error;
+
+import org.smoothbuild.common.collect.List;
 import org.smoothbuild.common.collect.Map;
 import org.smoothbuild.common.collect.Maybe;
+import org.smoothbuild.common.collect.Result;
 import org.smoothbuild.common.function.Function1;
+import org.smoothbuild.compilerfrontend.lang.name.Id;
+import org.smoothbuild.compilerfrontend.lang.name.Name;
 
 public sealed interface Bindings<E> permits AbstractBindings, ImmutableBindings, MutableBindings {
   public static <E> ImmutableFlatBindings<E> immutableBindings() {
@@ -33,6 +40,19 @@ public sealed interface Bindings<E> permits AbstractBindings, ImmutableBindings,
   }
 
   public boolean contains(String name);
+
+  public default Result<E> find(Id id) {
+    var parts = id.parts();
+    if (parts.size() == 1) {
+      var result = getMaybe(parts.get(0).toString());
+      return result.toResult(() -> cannotResolveErrorMessage(parts, 1));
+    }
+    return error(cannotResolveErrorMessage(parts, 2));
+  }
+
+  private static String cannotResolveErrorMessage(List<Name> parts, int toIndex) {
+    return "Cannot resolve " + q(parts.subList(0, toIndex).toString(":")) + ".";
+  }
 
   public E get(String name);
 
