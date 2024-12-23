87e3c1d947888934a5631405063a6d2d0c9e524c
Marcin Mikosik <marcin.mikosik@gmail.com>
2024-12-23 11:13:42 +0100

    Refactor CharUtils

--- src/compiler-frontend/src/main/java/org/smoothbuild/compilerfrontend/lang/base/CharUtils.java
+++ src/compiler-frontend/src/main/java/org/smoothbuild/compilerfrontend/lang/base/CharUtils.java
@@ -2,13 +2,12 @@ package org.smoothbuild.compilerfrontend.lang.base;
 
 public class CharUtils {
   static final char SEPARATOR = ':';
-  static final String SEPARATOR_STRING = ":";
 
-  static boolean isValidFullNameCharacter(char c) {
-    return isValidShortNameCharacter(c) || c == SEPARATOR;
+  static boolean isValidFullQualifiedNameCharacter(char c) {
+    return isValidNameCharacter(c) || c == SEPARATOR;
   }
 
-  static boolean isValidShortNameCharacter(char c) {
+  static boolean isValidNameCharacter(char c) {
     return isLowerCase(c) || isUpperCase(c) || isDigit(c) || c == '_';
   }
 
