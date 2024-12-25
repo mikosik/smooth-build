d2aac92952152793a854e76bb866b01b6c672fac
Marcin Mikosik <marcin.mikosik@gmail.com>
2024-12-25 12:31:05 +0100

    Inline HasSchemaAndLocation

--- src/compiler-frontend/src/main/java/org/smoothbuild/compilerfrontend/lang/define/SEvaluable.java
+++ src/compiler-frontend/src/main/java/org/smoothbuild/compilerfrontend/lang/define/SEvaluable.java
@@ -1,9 +1,10 @@
 package org.smoothbuild.compilerfrontend.lang.define;
 
-import org.smoothbuild.compilerfrontend.lang.base.HasSchemaAndLocation;
+import org.smoothbuild.common.log.location.HasLocation;
+import org.smoothbuild.compilerfrontend.lang.base.HasSchema;
 
 /**
  * Evaluable (function or value).
  */
-public abstract sealed interface SEvaluable extends HasSchemaAndLocation
+public abstract sealed interface SEvaluable extends HasSchema, HasLocation
     permits SFunc, SNamedEvaluable, SValue {}
