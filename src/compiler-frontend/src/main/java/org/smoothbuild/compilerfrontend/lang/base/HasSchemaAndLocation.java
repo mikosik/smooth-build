d5c9a36635067da81c6b1aea422daefe83c444e1
Marcin Mikosik <marcin.mikosik@gmail.com>
2024-12-25 12:31:05 +0100

    Inline HasSchemaAndLocation

--- src/compiler-frontend/src/main/java/org/smoothbuild/compilerfrontend/lang/base/HasSchemaAndLocation.java
+++
@@ -1,5 +0,0 @@
-package org.smoothbuild.compilerfrontend.lang.base;
-
-import org.smoothbuild.common.log.location.HasLocation;
-
-public interface HasSchemaAndLocation extends HasSchema, HasLocation {}
