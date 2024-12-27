6675a009d45ad8a47e8370e85f3045af30fd7e4b
Marcin Mikosik <marcin.mikosik@gmail.com>
2024-12-27 20:33:25 +0100

    Remove unused PFunc.paramTs()

--- src/compiler-frontend/src/main/java/org/smoothbuild/compilerfrontend/compile/ast/define/PFunc.java
+++ src/compiler-frontend/src/main/java/org/smoothbuild/compilerfrontend/compile/ast/define/PFunc.java
@@ -1,19 +1,13 @@
 package org.smoothbuild.compilerfrontend.compile.ast.define;
 
-import org.smoothbuild.common.collect.List;
 import org.smoothbuild.compilerfrontend.lang.name.NList;
 import org.smoothbuild.compilerfrontend.lang.type.SFuncSchema;
 import org.smoothbuild.compilerfrontend.lang.type.SFuncType;
-import org.smoothbuild.compilerfrontend.lang.type.SType;
 
 public sealed interface PFunc extends PEvaluable permits PLambda, PNamedFunc {
   public PType resultT();
 
   public NList<PItem> params();
-
-  public default List<SType> paramTs() {
-    return PItem.toSType(params());
-  }
 
   @Override
   public SFuncType sType();
