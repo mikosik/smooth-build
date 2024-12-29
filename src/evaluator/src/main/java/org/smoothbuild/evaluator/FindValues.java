a035b5bef5e8ea7cd58b5a2ad9f400bea0940c79
Marcin Mikosik <marcin.mikosik@gmail.com>
2024-12-29 19:57:09 +0100

    Refactor FindValues to use Bindings.find() instead of getMaybe()

--- src/evaluator/src/main/java/org/smoothbuild/evaluator/FindValues.java
+++ src/evaluator/src/main/java/org/smoothbuild/evaluator/FindValues.java
@@ -1,11 +1,15 @@
 package org.smoothbuild.evaluator;
 
 import static org.smoothbuild.common.collect.List.listOfAll;
+import static org.smoothbuild.common.collect.Result.error;
+import static org.smoothbuild.common.collect.Result.ok;
 import static org.smoothbuild.common.log.location.Locations.commandLineLocation;
 import static org.smoothbuild.common.schedule.Output.output;
+import static org.smoothbuild.compilerfrontend.lang.name.Fqn.parseReference;
 import static org.smoothbuild.evaluator.EvaluatorConstants.EVALUATOR_LABEL;
 
 import java.util.ArrayList;
+import org.smoothbuild.common.collect.Either;
 import org.smoothbuild.common.collect.List;
 import org.smoothbuild.common.log.base.Logger;
 import org.smoothbuild.common.schedule.Output;
@@ -15,25 +19,18 @@ import org.smoothbuild.compilerfrontend.lang.define.SInstantiate;
 import org.smoothbuild.compilerfrontend.lang.define.SNamedValue;
 import org.smoothbuild.compilerfrontend.lang.define.SReference;
 import org.smoothbuild.compilerfrontend.lang.define.SScope;
+import org.smoothbuild.compilerfrontend.lang.name.Fqn;
 
 public class FindValues implements Task2<SScope, List<String>, List<SExpr>> {
   @Override
   public Output<List<SExpr>> execute(SScope environment, List<String> valueNames) {
     var logger = new Logger();
     var result = new ArrayList<SNamedValue>();
-    var namedEvaluables = environment.evaluables();
     for (var name : valueNames) {
-      var namedEvaluable = namedEvaluables.getMaybe(name);
-      if (namedEvaluable.isNone()) {
-        logger.error("Unknown value `" + name + "`.\n"
-            + "Try 'smooth list' to see all available values that can be calculated.");
-      } else if (!(namedEvaluable.get() instanceof SNamedValue namedValue)) {
-        logger.error("`" + name + "` cannot be calculated as it is not a value but a function.");
-      } else if (namedValue.schema().quantifiedVars().isEmpty()) {
-        result.add(namedValue);
-      } else {
-        logger.error("`" + name + "` cannot be calculated as it is a polymorphic value.");
-      }
+      parseFqn(name)
+          .flatMapRight(fqn -> getNamedEvaluable(environment, fqn))
+          .ifRight(result::add)
+          .ifLeft(logger::error);
     }
     var label = EVALUATOR_LABEL.append(":findValues");
     if (logger.containsFailure()) {
@@ -42,6 +39,33 @@ public class FindValues implements Task2<SScope, List<String>, List<SExpr>> {
     List<SExpr> exprs =
         listOfAll(result).map(v -> new SInstantiate(referenceTo(v), commandLineLocation()));
     return output(exprs, label, logger.toList());
+  }
+
+  private static Either<String, Fqn> parseFqn(String name) {
+    return parseReference(name).mapLeft(message -> "Illegal reference `" + name + "`. " + message);
+  }
+
+  private static Either<String, SNamedValue> getNamedEvaluable(SScope environment, Fqn fqn) {
+    return environment
+        .evaluables()
+        .find(fqn)
+        .mapLeft(e -> unknownFqnMessage(fqn))
+        .flatMapRight(e -> {
+          if (e instanceof SNamedValue namedValue) {
+            if (namedValue.schema().quantifiedVars().isEmpty()) {
+              return ok(namedValue);
+            } else {
+              return error(e.id().q() + " cannot be calculated as it is a polymorphic value.");
+            }
+          } else {
+            return error(e.id().q() + " cannot be calculated as it is not a value but a function.");
+          }
+        });
+  }
+
+  private static String unknownFqnMessage(Fqn fqn) {
+    return "Unknown value " + fqn.q() + ".\n"
+        + "Try 'smooth list' to see all available values that can be calculated.";
   }
 
   private static SReference referenceTo(SNamedValue sNamedValue) {
