52003231580b60e1841bcc5a0c5f6221d94db738
Marcin Mikosik <marcin.mikosik@gmail.com>
2024-12-30 10:51:03 +0100

    Restructure PEvaluable class hierarchy so HasIdAndLocation is extended by PEvaluable (and thus by all its subclasses)

--- src/compiler-frontend/src/main/java/org/smoothbuild/compilerfrontend/compile/ast/define/PEvaluable.java
+++ src/compiler-frontend/src/main/java/org/smoothbuild/compilerfrontend/compile/ast/define/PEvaluable.java
@@ -1,11 +1,12 @@
 package org.smoothbuild.compilerfrontend.compile.ast.define;
 
 import org.smoothbuild.common.collect.Maybe;
-import org.smoothbuild.common.log.location.HasLocation;
+import org.smoothbuild.compilerfrontend.lang.base.HasIdAndLocation;
 import org.smoothbuild.compilerfrontend.lang.type.SSchema;
 import org.smoothbuild.compilerfrontend.lang.type.SType;
 
-public sealed interface PEvaluable extends HasLocation, PContainer permits PFunc, PNamedEvaluable {
+public sealed interface PEvaluable extends HasIdAndLocation, PContainer
+    permits PFunc, PNamedEvaluable {
   public String nameText();
 
   public SType sType();
