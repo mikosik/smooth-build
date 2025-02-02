89a94abdbf0ce27f2d325ceafbdff2e701411305
Marcin Mikosik <marcin.mikosik@gmail.com>
2025-02-02 13:31:53 +0100

    Extract Identifiable from IdentifiableCode

---
+++ src/compiler-frontend/src/main/java/org/smoothbuild/compilerfrontend/lang/base/Identifiable.java
@@ -0,0 +1,16 @@
+package org.smoothbuild.compilerfrontend.lang.base;
+
+import org.smoothbuild.compilerfrontend.lang.name.Fqn;
+import org.smoothbuild.compilerfrontend.lang.name.Name;
+
+/**
+ * Interface marking classes that have FQN (fully qualified name).
+ */
+public interface Identifiable extends HasName {
+  public Fqn fqn();
+
+  @Override
+  public default Name name() {
+    return fqn().parts().getLast();
+  }
+}
--- src/compiler-frontend/src/main/java/org/smoothbuild/compilerfrontend/lang/base/IdentifiableCode.java
+++ src/compiler-frontend/src/main/java/org/smoothbuild/compilerfrontend/lang/base/IdentifiableCode.java
@@ -1,17 +1,8 @@
 package org.smoothbuild.compilerfrontend.lang.base;
 
 import org.smoothbuild.common.log.location.HasLocation;
-import org.smoothbuild.compilerfrontend.lang.name.Fqn;
-import org.smoothbuild.compilerfrontend.lang.name.Name;
 
 /**
- * Interface marking classes that have FQN (fully qualified name) and Location.
+ * Interface marking classes that is identifiable and has Location.
  */
-public interface IdentifiableCode extends HasName, HasLocation {
-  public Fqn fqn();
-
-  @Override
-  public default Name name() {
-    return fqn().parts().getLast();
-  }
-}
+public interface IdentifiableCode extends Identifiable, HasLocation {}
