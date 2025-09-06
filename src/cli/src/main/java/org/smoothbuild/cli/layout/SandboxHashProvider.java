b1243e5d73e569278da0eaa4869cd4e9831cdd46
Marcin Mikosik <marcin.mikosik@gmail.com>
2025-09-06 18:49:14 +0200

    Refactor SandboxHashProvider to not cause problems with nullnes analysis

--- src/cli/src/main/java/org/smoothbuild/cli/layout/SandboxHashProvider.java
+++ src/cli/src/main/java/org/smoothbuild/cli/layout/SandboxHashProvider.java
@@ -1,17 +1,22 @@
 package org.smoothbuild.cli.layout;
 
+import static com.google.common.base.Preconditions.checkState;
+
 import jakarta.inject.Inject;
+import org.jspecify.annotations.Nullable;
 import org.smoothbuild.common.base.Hash;
 import org.smoothbuild.common.dagger.PerCommand;
 
 @PerCommand
 public class SandboxHashProvider {
+  @Nullable
   private volatile Hash sandboxHash;
 
   @Inject
   public SandboxHashProvider() {}
 
   public Hash get() {
+    checkState(sandboxHash != null, "SandboxHashProvider is not initialized.");
     return sandboxHash;
   }
 
