c22d4baa652b143997ce4634dd0a1aa46d1bb262
Marcin Mikosik <marcin.mikosik@gmail.com>
2025-11-13 20:43:33 +0100

    Refactor BOutput from record to class

--- src/virtual-machine/src/main/java/org/smoothbuild/virtualmachine/evaluate/evaluator/BOutput.java
+++ src/virtual-machine/src/main/java/org/smoothbuild/virtualmachine/evaluate/evaluator/BOutput.java
@@ -5,18 +5,21 @@ import static org.smoothbuild.common.collect.Maybe.none;
 import static org.smoothbuild.common.collect.Maybe.some;
 import static org.smoothbuild.virtualmachine.bytecode.helper.StoredLogStruct.containsErrorOrAbove;
 
+import java.util.Objects;
+import org.smoothbuild.common.base.ToStringBuilder;
 import org.smoothbuild.common.collect.Maybe;
 import org.smoothbuild.virtualmachine.bytecode.BytecodeException;
 import org.smoothbuild.virtualmachine.bytecode.expr.base.BArray;
 import org.smoothbuild.virtualmachine.bytecode.expr.base.BValue;
 
-public record BOutput(Maybe<BValue> value, BArray storedLogs) {
-  /**
-   * Do not use. Use static factory methods instead which have proper checks.
-   * Record constructors cannot have `throws` clause so we cannot put checks here.
-   */
-  @Deprecated
-  public BOutput {}
+public final class BOutput {
+  private final Maybe<BValue> value;
+  private final BArray storedLogs;
+
+  private BOutput(Maybe<BValue> value, BArray storedLogs) {
+    this.value = value;
+    this.storedLogs = storedLogs;
+  }
 
   public static BOutput bOutput(BValue value, BArray storedLogs) throws BytecodeException {
     checkArgument(
@@ -30,5 +33,36 @@ public record BOutput(Maybe<BValue> value, BArray storedLogs) {
         containsErrorOrAbove(storedLogs),
         "Cannot create BOutput without a value and with logs without problem.");
     return new BOutput(none(), storedLogs);
+  }
+
+  public Maybe<BValue> value() {
+    return value;
+  }
+
+  public BArray storedLogs() {
+    return storedLogs;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (obj == this) {
+      return true;
+    }
+    return obj instanceof BOutput that
+        && Objects.equals(this.value, that.value)
+        && Objects.equals(this.storedLogs, that.storedLogs);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(value, storedLogs);
+  }
+
+  @Override
+  public String toString() {
+    return new ToStringBuilder("BOutput")
+        .addField("value", value)
+        .addField("storedLogs", storedLogs)
+        .toString();
   }
 }
