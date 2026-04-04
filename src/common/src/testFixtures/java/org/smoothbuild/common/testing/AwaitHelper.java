4f2fabe121275d4fef3356b3318196dff8496260
Marcin Mikosik <marcin.mikosik@gmail.com>
2026-04-04 20:29:54 +0200

    Increase timeout in AwaitHelper

--- src/common/src/testFixtures/java/org/smoothbuild/common/testing/AwaitHelper.java
+++ src/common/src/testFixtures/java/org/smoothbuild/common/testing/AwaitHelper.java
@@ -1,11 +1,13 @@
 package org.smoothbuild.common.testing;
 
+import static java.util.concurrent.TimeUnit.SECONDS;
+
 import java.time.Duration;
 import org.awaitility.Awaitility;
 import org.awaitility.core.ConditionFactory;
 
 public class AwaitHelper {
   public static ConditionFactory await() {
-    return Awaitility.await().pollInterval(Duration.ofMillis(5));
+    return Awaitility.await().atMost(30, SECONDS).pollInterval(Duration.ofMillis(5));
   }
 }
